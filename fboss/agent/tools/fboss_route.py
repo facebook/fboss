04ab870a9264336c959af69a08eeb0726e2133e7
Phil Dibowitz <phil@ipom.com>
2016-01-08 14:59:16 -0800

    Make the CLI print human-readable IP addresses.
    Summary:
    Thanks to simpkins for most of this.
    Closes https://github.com/facebook/fboss/pull/18
    
    Reviewed By: simpkins
    
    Differential Revision: D2816374
    
    Pulled By: oridb
    
    fb-gh-sync-id: 4c56a12462ece86d950de9625df335dbef9dfe98

--- fboss/agent/tools/fboss_route.py
+++ fboss/agent/tools/fboss_route.py
@@ -8,16 +8,19 @@ from __future__ import absolute_import
 """Add, change, or delete a route on FBOSS controller
 """
 
-from argparse import ArgumentParser
+import contextlib
 import ipaddr
+import socket
 
+from argparse import ArgumentParser
+from thrift.transport import TSocket
+from thrift.protocol import TBinaryProtocol
+from fboss.ctrl import FbossCtrl
+from fboss.ctrl.ttypes import IpPrefix
+from fboss.ctrl.ttypes import UnicastRoute
 from facebook.network.Address.ttypes import BinaryAddress
-from fboss.thrift_clients import FbossAgentClient
-from neteng.fboss.ctrl.ttypes import IpPrefix
-from neteng.fboss.ctrl.ttypes import UnicastRoute
 
 DEFAULT_CLIENTID = 1
-
 
 def parse_prefix(args):
     network = ipaddr.IPNetwork(args.prefix)
@@ -49,18 +52,89 @@ def del_route(args):
         client.deleteUnicastRoutes(args.client, [prefix])
 
 
+def list_intf(args):
+    with get_client(args) as client:
+        #for intf in client.getInterfaceList():
+        for idx, intf in client.getAllInterfaces().iteritems():
+            print ("L3 Interface %d: %s" % (idx, format_interface(intf)))
+
+def format_ip(ip):
+    family = socket.AF_INET if len(ip.addr) == 4 else socket.AF_INET6
+    return socket.inet_ntop(family, ip.addr)
+
+
+def format_route(route):
+    next_hops = ', '.join(format_ip(ip) for ip in route.nextHopAddrs)
+    return '%s --> %s' % (format_prefix(route.dest), next_hops)
+
+def format_prefix(prefix):
+    return '%s/%d' % (format_ip(prefix.ip), prefix.prefixLength)
+
+def format_interface(intf):
+    return '%s (%s)' % (
+            ', '.join(format_prefix(i) for i in intf.address), intf.mac)
+
+def format_arp(arp):
+    return '%s -> %s' % (format_ip(arp.ip), arp.mac)
+
+def list_routes(args):
+    with get_client(args) as client:
+        for route in client.getRouteTable():
+            print ("Route %s" % format_route(route))
+
+def list_optics(args):
+    with get_client(args) as client:
+        info = client.getTransceiverInfo(range(1, 64))
+        for key, val in info.iteritems():
+            print ("Optic %d: %s" % (key, str(val)))
+
+def list_ports(args):
+    details = args.details
+    with get_client(args) as client:
+        #for intf in client.getInterfaceList():
+        for idx, intf in client.getPortStatus(range(1, 64)).iteritems():
+            stats = client.getPortStats(idx) if details else ""
+            print ("Port %d: %s: %s" % (idx, str(intf), stats))
+
+def list_arps(args):
+    with get_client(args) as client:
+        #for intf in client.getInterfaceList():
+        for arp in client.getArpTable():
+            print ("Arp: %s" % (format_arp(arp)))
+
+
+def list_vlans(args):
+    with get_client(args) as client:
+        #for intf in client.getInterfaceList():
+        vlans = {}
+        for idx, intf in client.getAllInterfaces().iteritems():
+            vlans[intf.vlanId] = True
+        for vlan in vlans:
+            print("===== Vlan %d ==== " % vlan)
+            for address in client.getVlanAddresses(vlan):
+                print(address.addr)
+
+
+@contextlib.contextmanager
 def get_client(args, timeout=5.0):
-    return FbossAgentClient(args.host, args.port, timeout=timeout)
+    sock = TSocket.TSocket(args.host, args.port)
+    sock.setTimeout(timeout * 1000)  # thrift timeout is in ms
+    protocol = TBinaryProtocol.TBinaryProtocol(sock)
+    transport = protocol.trans
+    transport.open()
+    client = FbossCtrl.Client(protocol)
+    yield client
+    transport.close()
 
 
 if __name__ == '__main__':
     ap = ArgumentParser()
-    ap.add_argument('--port', '-p', type=int, default=None,
+    ap.add_argument('--port', '-p', type=int, default=5909,
                     help='the controller thrift port')
     ap.add_argument('--client', '-c', type=int, default=DEFAULT_CLIENTID,
                     help='the client ID used to manipulate the routes')
-    ap.add_argument('host',
-                    help='the controller hostname')
+    ap.add_argument('--host',
+                    help='the controller hostname', default='localhost')
     subparsers = ap.add_subparsers()
 
     flush_parser = subparsers.add_parser(
@@ -73,7 +147,7 @@ if __name__ == '__main__':
     add_parser.add_argument(
         'prefix', help='the route prefix, i.e. "1.1.1.0/24" or "2001::0/64"')
     add_parser.add_argument(
-        'nexthop', nargs='*',
+        'nexthop', nargs='+',
         help='the nexthops of the route, i.e "10.1.1.1" or "2002::1"')
 
     del_parser = subparsers.add_parser(
@@ -81,6 +155,48 @@ if __name__ == '__main__':
     del_parser.set_defaults(func=del_route)
     del_parser.add_argument(
         'prefix', help='The route prefix, i.e. "1.1.1.0/24" or "2001::0/64"')
+
+    list_parser = subparsers.add_parser(
+        'list_intf', help='list switch interfaces')
+    list_parser.set_defaults(func=list_intf)
+    list_parser.add_argument(
+        '--details', action='store_true',
+        help='List all information about the interface', default=False)
+
+    list_route_parser = subparsers.add_parser(
+        'list_routes', help='list switch routes')
+    list_route_parser.set_defaults(func=list_routes)
+    list_route_parser.add_argument(
+        '--details', action='store_true',
+        help='List all information about the routes', default=False)
+
+    list_optic_parser = subparsers.add_parser(
+        'list_optics', help='list switch optics')
+    list_optic_parser.set_defaults(func=list_optics)
+    list_optic_parser.add_argument(
+        '--details', action='store_true',
+        help='List all information about the optics', default=False)
+
+    list_port_parser = subparsers.add_parser(
+        'list_ports', help='list switch ports')
+    list_port_parser.set_defaults(func=list_ports)
+    list_port_parser.add_argument(
+        '--details', action='store_true',
+        help='List all information about the ports', default=False)
+
+    list_vlan_parser = subparsers.add_parser(
+        'list_vlans', help='list switch vlans')
+    list_vlan_parser.set_defaults(func=list_vlans)
+    list_vlan_parser.add_argument(
+        '--details', action='store_true',
+        help='List all information about the vlans', default=False)
+
+    list_arp_parser = subparsers.add_parser(
+        'list_arps', help='list switch arps')
+    list_arp_parser.set_defaults(func=list_arps)
+    list_arp_parser.add_argument(
+        '--details', action='store_true',
+        help='List all information about the arps', default=False)
 
     args = ap.parse_args()
     args.func(args)
