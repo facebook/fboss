2677ae972c70179e222b1ac3bd445d2e6ee29077
Alex Eckert <aeckert@fb.com>
2016-03-11 13:36:49 -0800

    fix handling of 0 bps values
    Summary:
    This fixes the fboss port details command to handle 0 bps
    fields properly. We assert that bps is set, but since 0 is considered
    falsy by python the assert fails.
    
    Differential Revision: D3042667
    
    fb-gh-sync-id: ff0ae9e3d5f3c3109fc7bf27ba798f1c14e50a06
    fbshipit-source-id: ff0ae9e3d5f3c3109fc7bf27ba798f1c14e50a06

--- fboss/cli/commands/port.py
+++ fboss/cli/commands/port.py
@@ -70,7 +70,7 @@ class PortDetailsCmd(cmds.FbossCmd):
                 break
             value /= 1000
 
-        assert bps_per_unit and suffix, (
+        assert bps_per_unit is not None and suffix, (
                 'Unable to convert bps to human readable format')
 
         return bps_per_unit, suffix
