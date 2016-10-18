e3dc09c811edb33deebf302d92f4417a8ac9d3a8
Nina Schiff <ninasc@fb.com>
2016-10-18 13:24:51 -0700

    Custom thrift client for qsfp service that doesn't use SR
    Summary: SR has hard dependencies on configerator and smc - this bypasses that
    
    Differential Revision: D3976664
    
    fbshipit-source-id: 252d6be3c2541c09d0455f263ad547ae669ceb77

---
+++ fboss/agent/QsfpClient.cpp
@@ -0,0 +1,44 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#include "QsfpClient.h"
+
+#include "common/network/NetworkUtil.h"
+
+namespace facebook { namespace fboss {
+
+static constexpr int kQsfpServicePort = 11111;
+static constexpr int kQsfpConnTimeoutMs = 2000;
+static constexpr int kQsfpSendTimeoutMs = 5000;
+static constexpr int kQsfpRecvTimeoutMs = 5000;
+QsfpClient::QsfpClient(folly::EventBase* eb) :
+  eb_(eb) {
+}
+
+std::unique_ptr<QsfpServiceAsyncClient> QsfpClient::createClient() {
+  // SR relies on both configerator and smcc being up
+  // use raw thrift instead
+  folly::SocketAddress addr(network::NetworkUtil::getLocalAddress(),
+      kQsfpServicePort);
+  auto socket = apache::thrift::async::TAsyncSocket::newSocket(
+      eb_, addr, kQsfpConnTimeoutMs);
+  socket->setSendTimeout(kQsfpSendTimeoutMs);
+  auto channel = apache::thrift::HeaderClientChannel::newChannel(socket);
+  return std::make_unique<QsfpServiceAsyncClient>(std::move(channel));
+}
+
+apache::thrift::RpcOptions QsfpClient::getRpcOptions(){
+  apache::thrift::RpcOptions opts;
+  opts.setTimeout(
+      std::chrono::milliseconds(kQsfpRecvTimeoutMs));
+  return opts;
+}
+
+}} // namespace facebook::fboss
---
+++ fboss/agent/QsfpClient.h
@@ -0,0 +1,42 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+#pragma once
+
+#include <map>
+#include <memory>
+#include <vector>
+
+#include <thrift/lib/cpp/ClientUtil.h>
+#include <thrift/lib/cpp2/async/HeaderClientChannel.h>
+#include <folly/io/async/EventBase.h>
+#include <folly/SocketAddress.h>
+
+#include "fboss/qsfp_service/if/gen-cpp2/QsfpService.h"
+
+namespace facebook { namespace fboss {
+
+// Create a custom client that doesn't use service router
+// Service router has hard dependencies on smcc and configerator-proxy
+// being in working order
+// To avoid being unable to configure ports, use raw thrift until they remove
+// these dependencies from their critical path
+// TODO(ninasc): t13719924
+class QsfpClient {
+ public:
+  explicit QsfpClient(folly::EventBase* eb);
+  ~QsfpClient(){}
+ private:
+  // This may throw an exception
+  std::unique_ptr<QsfpServiceAsyncClient> createClient();
+  apache::thrift::RpcOptions getRpcOptions();
+  folly::EventBase* eb_;
+};
+
+}} // namespace facebook::fboss
