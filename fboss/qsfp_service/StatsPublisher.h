86dcda033f2c3378b4ec590208a9ef0886faf7f8
Nina Schiff <ninasc@fb.com>
2016-11-22 15:48:41 -0800

    Add metrics collection
    Summary:
    Port this over from the agent.
    Need to add a monitoring config - couldn't find how the agent does it - any pointers?
    
    Reviewed By: saifhhasan
    
    Differential Revision: D4005658
    
    fbshipit-source-id: f01bf8331192c3a3b0333bc52494550fbee143ab

---
+++ fboss/qsfp_service/StatsPublisher.h
@@ -0,0 +1,26 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#pragma once
+
+#include "fboss/qsfp_service/TransceiverManager.h"
+
+namespace facebook { namespace fboss {
+class StatsPublisher {
+ public:
+  explicit StatsPublisher(TransceiverManager* transceiverManager)
+    : transceiverManager_(transceiverManager) {};
+  void init();
+  void publishStats();
+
+ private:
+  TransceiverManager* transceiverManager_{nullptr};
+};
+}}
---
+++ fboss/qsfp_service/oss/StatsPublisher.cpp
@@ -0,0 +1,19 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#include "fboss/qsfp_service/StatsPublisher.h"
+
+namespace facebook { namespace fboss {
+void StatsPublisher::init() {
+}
+
+void StatsPublisher::publishStats() {
+}
+}}
