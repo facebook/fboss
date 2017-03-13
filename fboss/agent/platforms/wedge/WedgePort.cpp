9c62794df4e36bb3970b61543fd7c8aa8507eaf5
Alex Eckert <aeckert@fb.com>
2017-03-13 15:54:32 -0700

    Don't crash on link events if port is not in port group
    Summary:
    It is apparently possible on sdk 6.4.6 that ports are not
    actually registered in port groups unless you pass in the --flexports
    flag. This is because the bcm_get_subsidiary_ports call return
    BCM_E_PORT. Luckily, we do not run with this configuration anywhere
    outside of a handful of wedge100s that should be upgraded, but let's
    still not crash in this case. Noticed this running unit tests on
    actual switch hw.
    
    Differential Revision: D4701404
    
    fbshipit-source-id: ad4170373427d90595685d46c42c3fb918582c79

--- fboss/agent/platforms/wedge/WedgePort.cpp
+++ fboss/agent/platforms/wedge/WedgePort.cpp
@@ -107,7 +107,10 @@ void WedgePort::linkSpeedChanged(const cfg::PortSpeed& speed) {
 }
 
 bool WedgePort::isControllingPort() {
-  return bcmPort_ && bcmPort_->getPortGroup()->controllingPort() == bcmPort_;
+  if (!bcmPort_ || !bcmPort_->getPortGroup()) {
+    return false;
+  }
+  return bcmPort_->getPortGroup()->controllingPort() == bcmPort_;
 }
 
 bool WedgePort::shouldCustomizeTransceiver() {
