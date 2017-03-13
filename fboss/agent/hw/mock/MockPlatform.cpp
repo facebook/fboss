3d030bbc8011faf2748cdfe4a3660a1b558399b6
Alex Eckert <aeckert@fb.com>
2017-03-13 15:54:31 -0700

    make the default MockHwSwitch a NiceMock
    Summary:
    My recent diff adding Mockables causes us to mock a lot more
    functions in HwSwitch. I tried to use NiceMock everywhere to avoid the
    warnings for unnecessary calls, but missed the default case (not
    running with --switch_hw). Hopefully we can remove this down the road
    when we lock down possible inputs, but this at least makes test output
    readable again in the meantime.
    
    Differential Revision: D4701342
    
    fbshipit-source-id: 123f5d0099d36891a07b7aff8cbf3a3740b9b3c0

--- fboss/agent/hw/mock/MockPlatform.cpp
+++ fboss/agent/hw/mock/MockPlatform.cpp
@@ -14,6 +14,8 @@
 #include "fboss/agent/ThriftHandler.h"
 #include "fboss/agent/hw/mock/MockHwSwitch.h"
 
+#include <gmock/gmock.h>
+
 using std::make_unique;
 using std::string;
 using std::unique_ptr;
@@ -26,7 +28,7 @@ MockPlatform::MockPlatform(std::unique_ptr<MockHwSwitch> hw) :
 }
 
 MockPlatform::MockPlatform() :
-    MockPlatform(make_unique<MockHwSwitch>(this)) {
+    MockPlatform(make_unique<::testing::NiceMock<MockHwSwitch>>(this)) {
 }
 
 MockPlatform::~MockPlatform() {
