dcb95837b2667c532987da87a3f07b56b3a7f5f9
Linh Bui <linhbui@fb.com>
2017-06-29 15:15:52 -0700

    test service thrift
    Summary: Thrift file of test service
    
    Reviewed By: capveg
    
    Differential Revision: D5349346
    
    fbshipit-source-id: 6251947d3398c57c21bdda0104e33614ebf24469

---
+++ fboss/system_tests/if/test.thrift
@@ -0,0 +1,8 @@
+include "common/fb303/if/fb303.thrift"
+include "common/network/if/Address.thrift"
+
+namespace py fboss.system_tests.test
+
+service TestService extends fb303.FacebookService {
+  bool ping(Address.BinaryAddress ip);
+}
