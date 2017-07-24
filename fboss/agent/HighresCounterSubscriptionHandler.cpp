a7aa77f252691cee60e0d240e6c124a3b617ab8c
Yedidya Feldblum <yfeldblum@fb.com>
2017-07-24 15:49:53 -0700

    Rename ClientReceiveState::exceptionWrapper
    Summary: [Thrift] Rename `ClientReceiveState::exceptionWrapper` to `ClientReceiveState::exception`, removing the existing `ClientReceiveState::exception`.
    
    Reviewed By: eduardo-elizondo
    
    Differential Revision: D5420370
    
    fbshipit-source-id: 55194860b61a8ccc26fcb6406d3cfc1967d85c63

--- fboss/agent/HighresCounterSubscriptionHandler.cpp
+++ fboss/agent/HighresCounterSubscriptionHandler.cpp
@@ -39,7 +39,7 @@ inline void SampleSender::publish(unique_ptr<CounterPublication> pub) {
           if (state.isException()) {
             if (!killSwitch->set()) {
               LOG(ERROR) << "Exception sending publication: "
-                         << state.exceptionWrapper();
+                         << state.exception();
             }
             // else, we were already dying so don't beat a dead horse
           } else {
