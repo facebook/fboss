00434c100256490370648bc166564fa67c46fc48
Petr Lapukhov <petr@fb.com>
2017-08-16 12:13:35 -0700

    (easy) Add sFlow-related thrift definitions
    Summary: this thrift struct describes sFlow samples emitted by fboss process.
    
    Differential Revision: D5578572
    
    fbshipit-source-id: 27d01e6c80c9ae0d0bb8ea550ca404a804c690b1

---
+++ fboss/agent/if/sflow.thrift
@@ -0,0 +1,28 @@
+namespace cpp2 facebook.fboss
+namespace d neteng.fboss.sflow
+namespace py neteng.fboss.sflow
+
+// teimstamp encoding for packet capture time
+struct SflowTimestamp {
+  1: i64 seconds,
+  2: i64 nanoseconds
+}
+
+//
+// Metadata and body of the captured packet
+//
+struct SflowPacketInfo {
+  1: SflowTimestamp timestamp,
+  2: bool ingressSampled,
+  3: bool egressSampled,
+
+  // Physical port information
+  4: i16 srcPort, //int16
+  5: i16 dstPort, //uint16
+
+  // VLAN information since it may not be in the packet
+  6: i16 vlan, //uint16
+
+  // The packet itself
+  7: binary packetData
+}
