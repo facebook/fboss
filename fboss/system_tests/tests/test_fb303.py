b4be1c2da3f88eb9a59c6b7783cb44ed62b08b4f
Rob Sherwood <rsher@fb.com>
2018-01-25 10:10:16 -0800

    Fix open source aliveSince() implementation
    Summary:
    aliveSince() has a misleading name and I misunderstood.
    The correct implementation just returns the timestamp of when
    the process started as opposed to my understanding (the number
    of seconds since the process started).
    
    Reviewed By: linhbui
    
    Differential Revision: D6789987
    
    fbshipit-source-id: db60bf00f6b07e31d91dfc8ed06d1c1cbe172a64

---
+++ fboss/system_tests/tests/test_fb303.py
@@ -0,0 +1,18 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from fboss.system_tests.system_tests import FbossBaseSystemTest, test_tags
+
+
+@test_tags("run-on-diff")
+class Fb303Tests(FbossBaseSystemTest):
+    """ Verify we receive well-formatted IPv6 route advertisments
+        On all hosts, on all interfaces
+    """
+    def test_aliveSince(self):
+        with self.test_topology.switch_thrift() as sw_client:
+            timestamp1 = sw_client.aliveSince()
+            timestamp2 = sw_client.aliveSince()
+        self.assertEqual(timestamp1, timestamp2)
