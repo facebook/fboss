2623b31362ded07c1d0089c11477bc9f772711d6
Prathyusha Peddi <prathyushapeddi@fb.com>
2018-01-31 15:25:17 -0800

    Fix calling host.thrift_client twice
    Summary:
    test test_interface_ipv4_ping is being a thrift client to get ip and we are calling _test_interface_ip_ping while test is still acting as client.
    In _test_interface_ip_ping , test is trying to be client again. Its causing socket.timeout
    ERROR: test_interface_ipv6_ping (test_l3_interface.L3InterfacesTest)
    ----------------------------------------------------------------------
    Traceback (most recent call last):
      File "thrift/transport/TSocket.py", line 293, in read
    socket.timeout: timed out
    During handling of the above exception, another exception occurred:
    Traceback (most recent call last):
      File "/mnt/btrfs/trunk-hg-fbcode-fbsource-789-1517428788/fbcode/fboss/system_tests/tests/test_l3_interface.py", line 35, in test_interface_ipv6_ping
        self._test_interface_ip_ping(host, v4=False)
      File "/mnt/btrfs/trunk-hg-fbcode-fbsource-789-1517428788/fbcode/fboss/system_tests/tests/test_l3_interface.py", line 64, in _test_interface_ip_ping
        self.assertTrue(client.ping(ip))
      File "fboss/system_tests/test/TestService.py", line 3579, in ping
      File "fboss/system_tests/test/TestService.py", line 3591, in recv_ping
      File "thrift/protocol/THeaderProtocol.py", line 160, in readMessageBegin
      File "thrift/transport/THeaderTransport.py", line 223, in _reset_protocol
      File "thrift/transport/THeaderTransport.py", line 253, in readFrame
      File "thrift/transport/TTransport.py", line 75, in readAll
      File "thrift/transport/TSocket.py", line 300, in read
    thrift.transport.TTransport.TTransportException: Socket read failed: timed out
    
    Strange thing is its not happening on v6-only servers
    
    (Note: this ignores all push blocking failures!)
    
    Reviewed By: capveg
    
    Differential Revision: D6864940
    
    fbshipit-source-id: eca5b0f616d11cb186dd3209f2dfe96876de9859

--- fboss/system_tests/tests/test_l3_interface.py
+++ fboss/system_tests/tests/test_l3_interface.py
@@ -32,8 +32,9 @@ class L3InterfacesTest(FbossBaseSystemTest):
         for host in self.test_topology.hosts():
             with host.thrift_client() as client:
                 if client.get_v6_ip('eth0'):
-                    self._test_interface_ip_ping(host, v4=False)
                     v6_capable_host_exist = True
+            if v6_capable_host_exist:
+                self._test_interface_ip_ping(host, v4=False)
         if not v6_capable_host_exist:
             raise unittest.SkipTest("None of connected servers are v6 capable")
 
@@ -46,8 +47,9 @@ class L3InterfacesTest(FbossBaseSystemTest):
         for host in self.test_topology.hosts():
             with host.thrift_client() as client:
                 if client.get_v4_ip('eth0'):
-                    self._test_interface_ip_ping(host, v4=True)
                     v4_capable_host_exist = True
+            if v4_capable_host_exist:
+                self._test_interface_ip_ping(host, v4=False)
         if not v4_capable_host_exist:
             raise unittest.SkipTest("None of connected servers are v4 capable")
 
