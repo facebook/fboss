42c47fa0323906b415057ba7ecd2aeae308d7d38
Linh Bui <linhbui@fb.com>
2018-01-31 16:23:22 -0800

    fix netlink manager deployment
    Summary:
    use options.fbcode_dir so sandcastle build has the right fbcode path
    Once approved, I will remove --deploy-netlink-manager in build steps, as it will probably add 5 mins to the test suit.
    
    Differential Revision: D6806985
    
    fbshipit-source-id: 4e1d557abc19f41386f23b53e2146e85395cc8a3

--- fboss/system_tests/tests/test_netlink_manager.py
+++ fboss/system_tests/tests/test_netlink_manager.py
@@ -6,29 +6,21 @@ from __future__ import unicode_literals
 import socket
 import unittest
 
-from fboss.system_tests.system_tests import FbossBaseSystemTest, test_tags
+from fboss.system_tests.system_tests import FbossBaseSystemTest
 from fboss.system_tests.testutils.ip_conversion import ip_str_to_addr
+from fboss.system_tests.testutils.test_ip import get_random_test_ipv6
+from fboss.system_tests.testutils.subnet import get_subnet_from_ip_and_prefixlen
 from neteng.fboss.ctrl.ttypes import IpPrefix
 from neteng.fboss.ctrl.ttypes import UnicastRoute
 from neteng.fboss.ctrl.ttypes import StdClientIds
 
 
-@unittest.skip("Test is great, deployment is not. Should be fixed with D6577165")
-@test_tags("netlink-manager")
+@unittest.skip("Test is great. Skipping until added to nightly build")
 class NetlinkManager(FbossBaseSystemTest):
     NETLINK_MANAGER_CLIENT_ID = StdClientIds.NETLINK_LISTENER
 
     def test_update_route(self):
-        dst = ip_str_to_addr("2804:6082:2040:97f::")
-        prefix = IpPrefix(
-            ip=dst,
-            prefixLength=64)
-
-        host = next(iter(self.test_topology.hosts()))
-        ip = next(host.ips())
-        nexthops = [ip_str_to_addr(ip)]
-
-        unicast_route = UnicastRoute(dest=prefix, nextHopAddrs=nexthops)
+        unicast_route = self._get_unicast_route()
 
         with self.test_topology.switch_thrift() as switch_client:
             fboss_routes = switch_client.getRouteTableByClient(
@@ -54,3 +46,22 @@ class NetlinkManager(FbossBaseSystemTest):
             fboss_routes = switch_client.getRouteTableByClient(
                 self.NETLINK_MANAGER_CLIENT_ID)
             self.assertFalse(unicast_route in fboss_routes)
+
+    def _get_unicast_route(self):
+        prefix = self._get_prefix()
+        nexthops = self._get_nexthops()
+
+        return UnicastRoute(dest=prefix, nextHopAddrs=nexthops)
+
+    def _get_prefix(self):
+        str_test_ip = get_random_test_ipv6()
+        prefixlen = 64
+        str_subnet = get_subnet_from_ip_and_prefixlen(str_test_ip, prefixlen)
+        binary_subnet = ip_str_to_addr(str_subnet)
+        return IpPrefix(ip=binary_subnet,
+                        prefixLength=prefixlen)
+
+    def _get_nexthops(self):
+        host = next(iter(self.test_topology.hosts()))
+        ip = next(host.ips())
+        return [ip_str_to_addr(ip)]
