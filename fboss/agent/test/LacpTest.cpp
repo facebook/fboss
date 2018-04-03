f280ddc810b3cdcfda8c8f212589129fb840e9fc
Saman Kazemkhani <samank@fb.com>
2018-04-03 11:37:36 -0700

    Test LACP can handle receiving a frame on a down port
    Summary:
    This adds a test for D6184510, which fixed a crash observed early on in
    the production deployment of LACP.
    
    At the time, neither reproducing the crash nor confirming the crash had been
    fixed was possible as doing so required an LACP frame reception event occuring
    close enough in time to a port down event so as to reorder the two events from
    the perspective of the agent.
    
    With the recent modifications, it is now possible to not only faithfully
    recreate the crash but also test the correctness of the bug fix, demonstrating
    that the LACP implementation organization allows for fine-grained testing of the
    entire protocol stack.
    
    [1] Corresponding to efe9853a8eff0dedee0242a4658ec365e3f5a345 on Github.
    
    Depends on D7481701.
    
    Reviewed By: boryas
    
    Differential Revision: D7482587
    
    fbshipit-source-id: cf2a05449f9661a82345eaf9f31e45525a90760e

--- fboss/agent/test/LacpTest.cpp
+++ fboss/agent/test/LacpTest.cpp
@@ -22,6 +22,7 @@
 #include "fboss/agent/LinkAggregationManager.h"
 #include "fboss/agent/types.h"
 
+#include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
 using namespace facebook::fboss;
@@ -98,6 +99,16 @@ class LacpTransmissionInterceptor : public LacpServicerIf {
   // Force users to implement the transmit method
   LacpTransmissionInterceptor();
 };
+
+class MockLacpServicer : public LacpServicerIf {
+  MOCK_METHOD2(transmit, bool(LACPDU, PortID));
+  MOCK_METHOD2(enableForwarding, void(PortID, AggregatePortID));
+  MOCK_METHOD2(disableForwarding, void(PortID, AggregatePortID));
+  MOCK_METHOD1(
+      getControllersFor,
+      std::vector<std::shared_ptr<LacpController>>(
+          folly::Range<std::vector<PortID>::const_iterator>));
+};
 } // namespace
 
 /*
@@ -151,6 +162,52 @@ TEST_F(LacpTest, nonAggregatablePortTransmitsIndividualBit) {
   // Finally, we can check that the transmitted frame did indeed signal
   // itself as not AGGREGATABLE.
   ASSERT_EQ(actorInfoTransmitted & LacpState::AGGREGATABLE, LacpState::NONE);
+
+  controllerPtr->stopMachines();
+}
+
+/*
+ * When link aggregation was first deployed to production, the following
+ * logical sequence of events was observed:
+ * 1. Port P's PHY transitions to DOWN
+ * 2. P's Receive finite-state machine transitions to ReceiveMachine::DISABLED
+ * 3. A frame is received on the downed port P
+ * 4. P's Receive finite-state machine transitions to ReceiveMachine::CURRENT
+ * 5. Port P's PHY transitions to UP
+ * 6. CHECK_EQ(state_, ReceiveState::DISABLED) in ReceiveMachine::portUp() fails
+ * because state_ == ReceiveState::CURRENT
+ *
+ * In physical reality, (3) had occured before (1), but because port event
+ * notification and LACP frame reception occur on the update thread and RX
+ * thread, respectively, and they may take different paths through lower
+ * layers (eg., Linux kernel, ASIC SDK), the port down event and frame reception
+ * had been interleaved so as to appear that a LACP frame was received on a down
+ * port, thereby causing the violation in (6).
+ *
+ */
+TEST_F(LacpTest, frameReceivedOnDownPort) {
+  MockLacpServicer lacpServicer;
+
+  // An LacpController's methods assume it is being managed by a shared_ptr
+  auto controllerPtr =
+      std::make_shared<LacpController>(PortID(1), lacpEvb(), &lacpServicer);
+
+  // At this point, the LacpController we would like to test is in place
+  controllerPtr->startMachines();
+
+  // so we can proceed to replicate the interleaving described above
+
+  // (1) induce a port down event
+  controllerPtr->portDown();
+
+  // (3) induce a frame reception event
+  controllerPtr->received(LACPDU());
+
+  // (5) induce a port up event
+  controllerPtr->portUp();
+
+  // If commit D6184510/15324eb480b1 has indeed corrected this edge-case, then
+  // we should get here without the failed CHECK in (6)
 
   controllerPtr->stopMachines();
 }
