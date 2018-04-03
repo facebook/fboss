79e2a8bcfc5f7ecf4d47b01486549a624637f403
Saman Kazemkhani <samank@fb.com>
2018-04-03 09:31:41 -0700

    Use a test fixture for LACP tests
    Summary:
    This hoists thread and EventBase accounting common to all tests
    into a test fixture.
    
    Differential Revision: D7481701
    
    fbshipit-source-id: 816bae100c611ce452cf8165b6c8e13a175c5fbd

--- fboss/agent/test/LacpTest.cpp
+++ fboss/agent/test/LacpTest.cpp
@@ -12,10 +12,10 @@
 #include <vector>
 
 #include <folly/Function.h>
-#include <folly/Range.h>
 #include <folly/Synchronized.h>
 #include <folly/synchronization/Baton.h>
 #include <folly/system/ThreadName.h>
+#include <string>
 
 #include "fboss/agent/LacpController.h"
 #include "fboss/agent/LacpTypes.h"
@@ -25,8 +25,39 @@
 #include <gtest/gtest.h>
 
 using namespace facebook::fboss;
+using namespace std::string_literals;
 
 namespace {
+
+class LacpTest : public ::testing::Test {
+ protected:
+  LacpTest() : lacpEvb_(false) {}
+
+  void SetUp() override {
+    lacpThread_.reset(new std::thread([this] {
+      static auto const name = "testLACPthread"s;
+      folly::setThreadName(name);
+      google::setThreadName(name);
+
+      // TODO(samank): why doesn't this trip the CHECK...
+      lacpEvb_.loopForever();
+    }));
+  }
+
+  folly::EventBase* lacpEvb() {
+    lacpEvb_.waitUntilRunning();
+    return &lacpEvb_;
+  }
+
+  void TearDown() override {
+    lacpEvb_.terminateLoopSoon();
+    lacpThread_->join();
+  }
+
+ private:
+  folly::EventBase lacpEvb_;
+  std::unique_ptr<std::thread> lacpThread_{nullptr};
+};
 
 // This may eventually be subsumed by an LacpBehaviorInterceptor and the
 // appropriate factory methods
@@ -73,7 +104,7 @@ class LacpTransmissionInterceptor : public LacpServicerIf {
  * If a port is not aggregatable, then the frames transmitted from the
  * corresponding LacpController must _not_ have their AGGREGATABLE bit on
  */
-TEST(LacpTest, nonAggregatablePortTransmitsIndividualBit) {
+TEST_F(LacpTest, nonAggregatablePortTransmitsIndividualBit) {
   // The LacpController constructor reserved for non-AGGREGATABLE ports takes 3
   // parameters. In what follows, we construct two of those three parameters:
   // LacpServiceIf* and folly::EventBase*.
@@ -92,28 +123,12 @@ TEST(LacpTest, nonAggregatablePortTransmitsIndividualBit) {
   // by implementing the LacpServicerIf interface.
   LacpTransmissionInterceptor transmissionInterceptor(onTransmission);
 
-  // An LacpController takes an EventBase over which to execute.
-  // TODO(samank): avoid creating a thread for each test
-  folly::EventBase* lacpEvb = nullptr;
-  folly::Baton<> lacpEvbInitialized;
-  auto lacpThread =
-      std::make_unique<std::thread>([&lacpEvb, &lacpEvbInitialized] {
-        const char* threadName = "testLacpThread";
-        folly::setThreadName(threadName);
-        google::setThreadName(threadName);
-
-        folly::EventBase evb(false);
-        lacpEvb = &evb;
-        lacpEvbInitialized.post();
-
-        evb.loopForever();
-      });
-  lacpEvbInitialized.wait();
-  lacpEvb->waitUntilRunning();
+  // An LacpController takes an EventBase over which to execute. This EventBase
+  // is provided by the test fixture's lacpEvb() method.
 
   // An LacpController's methods assume it is being managed by a shared_ptr
   auto controllerPtr = std::make_shared<LacpController>(
-      PortID(1), lacpEvb, &transmissionInterceptor);
+      PortID(1), lacpEvb(), &transmissionInterceptor);
   transmissionInterceptor.setController(controllerPtr);
 
   // At this point, the LacpController we would like to test and the
@@ -128,7 +143,7 @@ TEST(LacpTest, nonAggregatablePortTransmitsIndividualBit) {
   // is executing on, we ensure that the LacpController has finished executing.
   // This may become overly tricky with more complicated tests.
   LacpState actorInfoTransmitted;
-  lacpEvb->runInEventBaseThreadAndWait(
+  lacpEvb()->runInEventBaseThreadAndWait(
       [&actorInfoTransmitted, &lacpduTransmitted]() {
         actorInfoTransmitted = lacpduTransmitted.rlock()->actorInfo.state;
       });
@@ -138,6 +153,4 @@ TEST(LacpTest, nonAggregatablePortTransmitsIndividualBit) {
   ASSERT_EQ(actorInfoTransmitted & LacpState::AGGREGATABLE, LacpState::NONE);
 
   controllerPtr->stopMachines();
-  lacpEvb->terminateLoopSoon();
-  lacpThread->join();
 }
