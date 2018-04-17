a2d1aa9696a0ff15074ad5c3a9d48be321b18bb7
Alex Eckert <aeckert@fb.com>
2018-04-16 17:18:26 -0700

    Model PCA9548 as a class
    Summary:
    This adds a new class to model PCA9548 muxes that are part of
    an i2c bus. Having this class will make it easier to model multi-layer
    mux setups, as well as use the muxes to do more than just select qsfps
    (like in T25873711)
    
    Differential Revision: D7113815
    
    fbshipit-source-id: 577f51644e0a4bd6f053e2018e4d5bff9d0c3ff6

---
+++ fboss/lib/usb/PCA9548.cpp
@@ -0,0 +1,26 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+#include "fboss/lib/usb/PCA9548.h"
+#include "fboss/lib/usb/CP2112.h"
+
+#include <stdint.h>
+
+namespace facebook {
+namespace fboss {
+
+void PCA9548::commit(uint8_t selector) {
+  // The multiplexer address is shifted one to the left to
+  // work with the underlying I2C libraries.
+  dev_->writeByte(address_ << 1, selector);
+  selected_ = selector;
+}
+
+} // namespace fboss
+} // namespace facebook
---
+++ fboss/lib/usb/PCA9548.h
@@ -0,0 +1,73 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+#pragma once
+
+#include <stdint.h>
+#include <glog/logging.h>
+
+namespace facebook {
+namespace fboss {
+
+class CP2112;
+
+// The PCA9548 chip is a mux that allows internally connecting 0-8
+// channels over i2c. Note that this is NOT thread safe and expects
+// clients to only access the chip from a single thread.
+
+class PCA9548 {
+ public:
+  static constexpr uint8_t WIDTH = 8;
+
+  PCA9548(CP2112* dev, uint8_t address) : dev_(dev), address_(address) {}
+
+  void init(uint8_t selector = 0) {
+    commit(selector);
+  }
+
+  // selectors
+  void select(uint8_t selector) {
+    commit(selector);
+  }
+  void selectOne(uint8_t channel) {
+    CHECK_GT(WIDTH, channel);
+    commit(static_cast<uint8_t>(1 << channel));
+  }
+  void selectAll() {
+    commit(~0);
+  }
+
+  // unselection
+  void unselect(uint8_t selector) {
+    commit(selected_ & ~selector);
+  }
+  void unselectOne(uint8_t channel) {
+    CHECK_GT(WIDTH, channel);
+    commit(selected_ & ~(static_cast<uint8_t>(1 << channel)));
+  }
+  void unselectAll() {
+    commit(0);
+  }
+
+ private:
+  void commit(uint8_t selector);
+
+  CP2112* dev_{nullptr};
+
+  // i2c address of the mux
+  uint8_t address_{0};
+
+  // Which channels are selected. The semantics here are that every
+  // bit reperesents a channel and if 1, that channel is connected to
+  // the i2c bus.
+  uint8_t selected_{0};
+};
+
+} // namespace fboss
+} // namespace facebook
