96253a8e81047a6f638df2f2435967eeb2599f9d
Joseph Wu <joseph5wu@fb.com>
2018-08-31 10:37:04 -0700

    Fix error message
    Summary: As title.
    
    Differential Revision: D9560964
    
    fbshipit-source-id: 7f1aada71475597ac3a7c94ebf142e695d71f689

--- fboss/qsfp_service/sff/QsfpModule.cpp
+++ fboss/qsfp_service/sff/QsfpModule.cpp
@@ -557,7 +557,7 @@ void QsfpModule::setQsfpIdprom() {
   int dataAddress;
 
   if (!present_) {
-    throw FbossError("QSFP IDProm set failed as QSFP is not present");
+    throw FbossError("Failed setting QSFP IDProm: QSFP is not present");
   }
 
   // Check if the data is ready
@@ -566,7 +566,7 @@ void QsfpModule::setQsfpIdprom() {
   getQsfpValue(dataAddress, offset, length, status);
   if (status[1] & (1 << 0)) {
     StatsPublisher::bumpModuleErrors();
-    throw QsfpModuleError("QSFP IDProm failed as QSFP is not ready");
+    throw QsfpModuleError("Failed setting QSFP IDProm: QSFP is not ready");
   }
   flatMem_ = status[1] & (1 << 2);
   XLOG(DBG3) << "Detected QSFP " << qsfpImpl_->getName()
