ddbfb0d3213c8412b1916685e8c5db41a3e93dd6
Prathyusha Peddi <prathyushapeddi@fb.com>
2018-10-04 21:16:38 -0700

    Enable retry only for sub routines rather than whole functions
    Summary:
    We enabled retry for whole function _test_each_host which deos
    1. added loopback
    2. add unicast route
    3. Ping loopback address from other hosts
    When step3 fails , we retry whole function. This is forcing us to add loopback again and that is failing with "Device loop1 already exists"
    
    Reviewed By: jhshi
    
    Differential Revision: D10213379
    
    fbshipit-source-id: e57e3f7c1a11199719a2dc6295b08833db603604

--- fboss/system_tests/tests/test_loopback_ping.py
+++ fboss/system_tests/tests/test_loopback_ping.py
@@ -28,27 +28,38 @@ class LoopbackPing(FbossBaseSystemTest):
         for host in hosts:
             self._test_each_host(host, hosts, loopback_address)
 
-    @retryable(num_tries=3, sleep_time=10, retryable_exs=[TTransportException])
     def _test_each_host(self, host, hosts, loopback_address):
         self._setup_loopback_interface_in_host(host, loopback_address)
         unicast_route = self._add_route_to_switch(host, loopback_address)
 
-        # Make sure each other host can ping that loopback address
         for other_host in hosts:
             if other_host == host:
                 continue
-            with other_host.thrift_client() as other_host_client:
-                response = other_host_client.ping(loopback_address)
-                self.assertTrue(response)
+            self._test_ping_over_loopback(host, other_host, loopback_address)
 
         self._cleanup(host, unicast_route)
 
-    # Set up the loop1 loopback interface and add the loopback_address
+    @retryable(num_tries=3, sleep_time=10, retryable_exs=[TTransportException])
+    def _test_ping_over_loopback(self, host, other_host, loopback_address):
+        # Make sure each other host can ping that loopback address
+        with other_host.thrift_client() as other_host_client:
+            response = other_host_client.ping(loopback_address)
+            self.assertTrue(response)
+
+    @retryable(num_tries=3, sleep_time=10, retryable_exs=[TTransportException])
     def _setup_loopback_interface_in_host(self, host, loopback_address):
+        # Set up the loop1 loopback interface and add the loopback_address
         with host.thrift_client() as host_client:
             response = host_client.add_interface("loop1", DeviceType.LOOPBACK)
             self.assertTrue(response)
             response = host_client.add_address(loopback_address, "loop1")
+            self.assertTrue(response)
+
+    @retryable(num_tries=3, sleep_time=10, retryable_exs=[TTransportException])
+    def _delete_loopback_interface_in_host(self, host):
+        # Delete loop1 loopback interface
+        with host.thrift_client() as host_client:
+            response = host_client.remove_interface("loop1")
             self.assertTrue(response)
 
     # Set up and add the route to the loopback address
@@ -68,12 +79,14 @@ class LoopbackPing(FbossBaseSystemTest):
             client.addUnicastRoute(1, unicast_route)
         return unicast_route
 
-    def _cleanup(self, host, unicast_route):
-        # Clean up the newly added loopback interface
-        with host.thrift_client() as host_client:
-            response = host_client.remove_interface("loop1")
-            self.assertTrue(response)
-
-        # Remove the route
+    @retryable(num_tries=3, retryable_exs=[FbossBaseError])
+    def _delete_unicast_route(self, unicast_route):
         with self.test_topology.switch_thrift() as client:
             client.deleteUnicastRoute(1, unicast_route.dest)
+
+    def _cleanup(self, host, unicast_route):
+        # Clean up the newly added loopback interface
+        self._delete_loopback_interface_in_host(host)
+
+        # Remove the route
+        self._add_unicast_route(unicast_route)
