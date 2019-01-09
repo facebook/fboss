075114f6eb02a45c9b6671a6b417fe9e308c0962
Boris Burkov <borisb@fb.com>
2019-01-09 11:53:21 -0800

    create blank SaiSwitch
    Summary:
    This is the proposed structure within which we will implement Sai
    support via SwSwitch/HwSwitch.
    
    Differential Revision: D10501280
    
    fbshipit-source-id: 6e1700c6b72bc2c99a01e80e37f245451a007a54

---
+++ fboss/agent/hw/sai/switch/SaiSwitch.cpp
@@ -0,0 +1,80 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#include "SaiSwitch.h"
+
+#include "fboss/agent/TxPacket.h"
+#include "fboss/agent/state/SwitchState.h"
+
+#include <memory>
+
+namespace facebook {
+namespace fboss {
+
+class SaiTxPacket : public TxPacket {
+ public:
+  SaiTxPacket() {}
+};
+
+HwInitResult SaiSwitch::init(Callback* /* callback */) noexcept {
+  return HwInitResult();
+}
+void SaiSwitch::unregisterCallbacks() noexcept {}
+std::shared_ptr<SwitchState> SaiSwitch::stateChanged(
+    const StateDelta& /* delta */) {
+  return std::make_shared<SwitchState>();
+}
+bool SaiSwitch::isValidStateUpdate(const StateDelta& /* delta */) const {
+  return false;
+}
+std::unique_ptr<TxPacket> SaiSwitch::allocatePacket(uint32_t /* size */) {
+  return std::make_unique<SaiTxPacket>();
+}
+bool SaiSwitch::sendPacketSwitchedAsync(
+    std::unique_ptr<TxPacket> /* pkt */) noexcept {
+  return false;
+}
+bool SaiSwitch::sendPacketOutOfPortAsync(
+    std::unique_ptr<TxPacket> /* pkt */,
+    PortID /* portID */,
+    folly::Optional<uint8_t> /* cos */) noexcept {
+  return false;
+}
+bool SaiSwitch::sendPacketSwitchedSync(
+    std::unique_ptr<TxPacket> /* pkt */) noexcept {
+  return false;
+}
+bool SaiSwitch::sendPacketOutOfPortSync(
+    std::unique_ptr<TxPacket> /* pkt */,
+    PortID portID) noexcept {
+  return false;
+}
+void SaiSwitch::updateStats(SwitchStats* /* switchStats */) {}
+void SaiSwitch::fetchL2Table(std::vector<L2EntryThrift>* /* l2Table */) {}
+void SaiSwitch::gracefulExit(folly::dynamic& /* switchState */) {}
+folly::dynamic SaiSwitch::toFollyDynamic() const {
+  return folly::dynamic::object();
+}
+void SaiSwitch::initialConfigApplied() {}
+void SaiSwitch::clearWarmBootCache() {}
+void SaiSwitch::exitFatal() const {}
+bool SaiSwitch::isPortUp(PortID /* port */) const {
+  return false;
+}
+bool SaiSwitch::getAndClearNeighborHit(
+    RouterID /* vrf */,
+    folly::IPAddress& /* ip */) {
+  return true;
+}
+void SaiSwitch::clearPortStats(
+    const std::unique_ptr<std::vector<int32_t>>& /* ports */) {}
+
+} // namespace fboss
+} // namespace facebook
---
+++ fboss/agent/hw/sai/switch/SaiSwitch.h
@@ -0,0 +1,49 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+#pragma once
+
+#include "fboss/agent/HwSwitch.h"
+#include "fboss/agent/hw/sai/api/SaiApiTable.h"
+
+namespace facebook { namespace fboss {
+
+class SaiSwitch : public HwSwitch {
+ public:
+  HwInitResult init(Callback* callback) noexcept override;
+  void unregisterCallbacks() noexcept override;
+  std::shared_ptr<SwitchState> stateChanged(const StateDelta& delta) override;
+  bool isValidStateUpdate(const StateDelta& delta) const override;
+  std::unique_ptr<TxPacket> allocatePacket(uint32_t size) override;
+  bool sendPacketSwitchedAsync(std::unique_ptr<TxPacket> pkt) noexcept override;
+  bool sendPacketOutOfPortAsync(
+      std::unique_ptr<TxPacket> pkt,
+      PortID portID,
+      folly::Optional<uint8_t> cos) noexcept override;
+  bool sendPacketSwitchedSync(std::unique_ptr<TxPacket> pkt) noexcept override;
+  bool sendPacketOutOfPortSync(
+      std::unique_ptr<TxPacket> pkt,
+      PortID portID) noexcept override;
+  void updateStats(SwitchStats* switchStats) override;
+  void fetchL2Table(std::vector<L2EntryThrift>* l2Table) override;
+  void gracefulExit(folly::dynamic& switchState) override;
+  folly::dynamic toFollyDynamic() const override;
+  void initialConfigApplied() override;
+  void clearWarmBootCache() override;
+  void exitFatal() const override;
+  bool isPortUp(PortID port) const override;
+  bool getAndClearNeighborHit(RouterID vrf, folly::IPAddress& ip) override;
+  void clearPortStats(
+      const std::unique_ptr<std::vector<int32_t>>& ports) override;
+ private:
+  SaiApiTable saiApiTable_;
+};
+
+} // namespace fboss
+} // namespace facebook
