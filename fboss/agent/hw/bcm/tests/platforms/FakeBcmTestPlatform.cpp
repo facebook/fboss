4d48a256c7747ab42e16c2a0ab6b6dbe28f73b37
Jasmeet Bagga <jasmeetbagga@fb.com>
2019-03-15 11:15:49 -0700

    Move FakeSdkPlatform to oss as well.
    Summary:
    The platform code for Fake platforms doesn't have much that needs to
    be in facebook/. Whether we support running Fake sdk tests in oss is another
    matter though
    
    Reviewed By: shrikhare
    
    Differential Revision: D14405990
    
    fbshipit-source-id: c497023425850c26906d91eb54c44dd9fcd1fdea

---
+++ fboss/agent/hw/bcm/tests/platforms/FakeBcmTestPlatform.cpp
@@ -0,0 +1,43 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+#include "fboss/agent/hw/bcm/tests/platforms/FakeBcmTestPlatform.h"
+#include "fboss/agent/hw/bcm/tests/platforms/FakeBcmTestPort.h"
+
+using std::make_unique;
+
+namespace {
+static const std::array<int, 8> kMasterLogicalPortIds =
+    {1, 5, 9, 13, 17, 21, 25, 29};
+constexpr uint8_t kNumPortsPerTransceiver = 4;
+} // namespace
+
+namespace facebook {
+namespace fboss {
+FakeBcmTestPlatform::FakeBcmTestPlatform()
+    : BcmTestPlatform(
+          std::vector<int>(
+              kMasterLogicalPortIds.begin(),
+              kMasterLogicalPortIds.end()),
+          kNumPortsPerTransceiver) {}
+
+std::unique_ptr<BcmTestPort> FakeBcmTestPlatform::getPlatformPort(PortID id) {
+  return make_unique<FakeBcmTestPort>(id);
+}
+
+std::string FakeBcmTestPlatform::getVolatileStateDir() const {
+  return tmpDir_.path().string() + "/volatile";
+}
+
+std::string FakeBcmTestPlatform::getPersistentStateDir() const {
+  return tmpDir_.path().string() + "/persist";
+}
+
+} // namespace fboss
+} // namespace facebook
