136d1633f86e52b9ff5ab293133f801d6ac7aa5c
Srikrishna Gopu <srikrishnagopu@fb.com>
2019-04-01 15:19:22 -0700

    SaiSwitchManager
    Summary: SAI switch manager to manage Sai switch attributes. Default switch id is 0.
    
    Reviewed By: boryas
    
    Differential Revision: D14613632
    
    fbshipit-source-id: e389afe7767ee0464a61df927363964f09cd6f5c

---
+++ fboss/agent/hw/sai/switch/SaiSwitchManager.cpp
@@ -0,0 +1,79 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#include "fboss/agent/hw/sai/switch/SaiSwitchManager.h"
+
+#include "fboss/agent/FbossError.h"
+#include "fboss/agent/hw/sai/api/SwitchApi.h"
+#include "fboss/agent/hw/sai/switch/SaiManagerTable.h"
+
+#include <folly/logging/xlog.h>
+
+// (TODO: srikrishnagopu) Move this to SaiPlatform ?
+namespace {
+  std::vector<int8_t> getConnectionHandle() {
+    static const char connStr[] = "/dev/testdev/socket/0.0.0.0:40000";
+    return std::vector<int8_t> {std::begin(connStr), std::end(connStr)};
+  }
+}
+
+namespace facebook {
+namespace fboss {
+
+SaiSwitchInstance::SaiSwitchInstance(SaiApiTable* apiTable)
+    : apiTable_(apiTable), attributes_({}) {
+  SwitchApiParameters::Attributes::HwInfo hwInfo(getConnectionHandle());
+  SwitchApiParameters::Attributes attributes_{{hwInfo}};
+  auto& switchApi = apiTable_->switchApi();
+  id_ = switchApi.create2(attributes_.attrs());
+}
+
+SaiSwitchInstance::SaiSwitchInstance(
+    SaiApiTable* apiTable,
+    const SwitchApiParameters::Attributes& attributes)
+    : apiTable_(apiTable), attributes_(attributes) {
+  auto& switchApi = apiTable_->switchApi();
+  id_ = switchApi.create2(attributes_.attrs());
+}
+
+SaiSwitchInstance::~SaiSwitchInstance() {
+  auto& switchApi = apiTable_->switchApi();
+  switchApi.remove(id());
+}
+
+bool SaiSwitchInstance::operator==(const SaiSwitchInstance& other) const {
+  return attributes_ == other.attributes();
+}
+bool SaiSwitchInstance::operator!=(const SaiSwitchInstance& other) const {
+  return !(*this == other);
+}
+
+SaiSwitchManager::SaiSwitchManager(
+    SaiApiTable* apiTable,
+    SaiManagerTable* managerTable)
+    : apiTable_(apiTable), managerTable_(managerTable) {
+  auto defaultSwitch = std::make_unique<SaiSwitchInstance>(apiTable_);
+  switches_.emplace(std::make_pair(SwitchID(0), std::move(defaultSwitch)));
+}
+
+SaiSwitchInstance* SaiSwitchManager::getSwitch(
+    const SwitchID& switchId) {
+  auto itr = switches_.find(switchId);
+  if (itr == switches_.end()) {
+    return nullptr;
+  }
+  if (!itr->second) {
+    XLOG(FATAL) << "invalid null switch for switchID: " << switchId;
+  }
+  return itr->second.get();
+}
+
+} // namespace fboss
+} // namespace facebook
---
+++ fboss/agent/hw/sai/switch/SaiSwitchManager.h
@@ -0,0 +1,66 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#pragma once
+
+#include "fboss/agent/hw/sai/api/SaiApiTable.h"
+#include "fboss/agent/hw/sai/api/VirtualRouterApi.h"
+#include "fboss/agent/types.h"
+
+#include "folly/MacAddress.h"
+
+#include <memory>
+#include <unordered_map>
+
+namespace facebook {
+namespace fboss {
+
+class SaiManagerTable;
+
+class SaiSwitchInstance {
+ public:
+  explicit SaiSwitchInstance(SaiApiTable* apiTable);
+  SaiSwitchInstance(
+      SaiApiTable* apiTable,
+      const SwitchApiParameters::Attributes& attributes);
+  ~SaiSwitchInstance();
+  SaiSwitchInstance(const SaiSwitchInstance& other) = delete;
+  SaiSwitchInstance(SaiSwitchInstance&& other) = delete;
+  SaiSwitchInstance& operator=(const SaiSwitchInstance& other) = delete;
+  SaiSwitchInstance& operator=(SaiSwitchInstance&& other) = delete;
+  bool operator==(const SaiSwitchInstance& other) const;
+  bool operator!=(const SaiSwitchInstance& other) const;
+
+  const SwitchApiParameters::Attributes attributes() const {
+    return attributes_;
+  }
+  sai_object_id_t id() const {
+    return id_;
+  }
+
+ private:
+  SaiApiTable* apiTable_;
+  SwitchApiParameters::Attributes attributes_;
+  sai_object_id_t id_;
+};
+
+class SaiSwitchManager {
+ public:
+  SaiSwitchManager(SaiApiTable* apiTable, SaiManagerTable* managerTable);
+  SaiSwitchInstance* getSwitch(const SwitchID& switchId);
+
+ private:
+  SaiApiTable* apiTable_;
+  SaiManagerTable* managerTable_;
+  std::unordered_map<SwitchID, std::unique_ptr<SaiSwitchInstance>> switches_;
+};
+
+} // namespace fboss
+} // namespace facebook
