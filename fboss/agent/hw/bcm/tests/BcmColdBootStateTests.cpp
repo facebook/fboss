2e1ba0720e3c4284d204469212907f6ff2775653
Jasmeet Bagga <jasmeetbagga@fb.com>
2019-04-22 12:39:38 -0700

    Assert that all ports are disabled on cold boot
    Summary:
    As titled. We are looking to enable packet handling
    post init and not have to wait till configured state. On warm
    boot this works fine, but for cold boot it means we would be
    handling packets before config is applied and thus using cold
    boot state to do this. This is only a concern for packets coming
    from host as the front panel ports are disabled in cold boot state.
    Assert that in a bcm test
    
    Reviewed By: saifhhasan
    
    Differential Revision: D15006884
    
    fbshipit-source-id: 2c78f39bf7caafe2ce0f23e151b7b57d75add578

---
+++ fboss/agent/hw/bcm/tests/BcmColdBootStateTests.cpp
@@ -0,0 +1,31 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#include "fboss/agent/hw/bcm/tests/BcmTest.h"
+
+#include "fboss/agent/hw/bcm/BcmSwitch.h"
+#include "fboss/agent/hw/bcm/BcmPortTable.h"
+
+namespace facebook {
+namespace fboss {
+
+TEST_F(BcmTest, allPortsDisabledOnColdBoot) {
+  auto setup = [this] { return getProgrammedState(); };
+  auto verify = [this] {
+    for (auto portIdAndPort : *getHwSwitch()->getPortTable()) {
+      EXPECT_FALSE(portIdAndPort.second->isEnabled());
+    }
+  };
+  setup();
+  verify();
+}
+
+} // namespace fboss
+} // namespace facebook
