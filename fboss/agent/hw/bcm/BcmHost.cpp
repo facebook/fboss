b8749c59bcac7ffaa448bc2b29a5b82767fe306b
Parvez Shaikh <pshaikh@fb.com>
2019-05-25 10:34:30 -0700

    moving egress manager in its own file
    Summary: giving EgressManager its own file, deflating BcmHost file
    
    Differential Revision: D15443322
    
    fbshipit-source-id: bfc815df280a341273b1dc7b3d3c02aa2ed2f084

--- fboss/agent/hw/bcm/BcmHost.cpp
+++ fboss/agent/hw/bcm/BcmHost.cpp
@@ -606,53 +606,6 @@ BcmEgressBase* BcmHostTable::derefEgress(opennsl_if_t egressId) {
   return it->second.first.get();
 }
 
-void BcmEgressManager::updatePortToEgressMapping(
-    opennsl_if_t egressId,
-    opennsl_gport_t oldGPort,
-    opennsl_gport_t newGPort) {
-  auto newMapping = getPortAndEgressIdsMap()->clone();
-
-  if (BcmPort::isValidLocalPort(oldGPort) ||
-      BcmTrunk::isValidTrunkPort(oldGPort)) {
-    auto old = newMapping->getPortAndEgressIdsIf(oldGPort);
-    CHECK(old);
-    auto oldCloned = old->clone();
-    oldCloned->removeEgressId(egressId);
-    if (oldCloned->empty()) {
-      newMapping->removePort(oldGPort);
-    } else {
-      newMapping->updatePortAndEgressIds(oldCloned);
-    }
-  }
-  if (BcmPort::isValidLocalPort(newGPort) ||
-      BcmTrunk::isValidTrunkPort(newGPort)) {
-    auto existing = newMapping->getPortAndEgressIdsIf(newGPort);
-    if (existing) {
-      auto existingCloned = existing->clone();
-      existingCloned->addEgressId(egressId);
-      newMapping->updatePortAndEgressIds(existingCloned);
-    } else {
-      PortAndEgressIdsFields::EgressIdSet egressIds;
-      egressIds.insert(egressId);
-      auto toAdd = std::make_shared<PortAndEgressIds>(newGPort, egressIds);
-      newMapping->addPortAndEgressIds(toAdd);
-    }
-  }
-  // Publish and replace with the updated mapping
-  newMapping->publish();
-  setPort2EgressIdsInternal(newMapping);
-}
-
-void BcmEgressManager::setPort2EgressIdsInternal(
-    std::shared_ptr<PortAndEgressIdsMap> newMap) {
-  // This is one of the only two places that should ever directly access
-  // portAndEgressIdsDontUseDirectly_.  (getPortAndEgressIdsMap() being the
-  // other one.)
-  CHECK(newMap->isPublished());
-  folly::SpinLockGuard guard(portAndEgressIdsLock_);
-  portAndEgressIdsDontUseDirectly_.swap(newMap);
-}
-
 const BcmEgressBase* BcmHostTable::getEgressObjectIf(opennsl_if_t egress) const {
   auto it = egressMap_.find(egress);
   return it == egressMap_.end() ? nullptr : it->second.first.get();
@@ -745,58 +698,6 @@ folly::dynamic BcmHostTable::toFollyDynamic() const {
   hostTable[kHosts] = std::move(hostsJson);
   hostTable[kEcmpHosts] = std::move(ecmpHostsJson);
   return hostTable;
-}
-
-void BcmEgressManager::linkStateChangedMaybeLocked(
-    opennsl_gport_t gport,
-    bool up,
-    bool locked) {
-  auto portAndEgressIdMapping = getPortAndEgressIdsMap();
-
-  const auto portAndEgressIds =
-      portAndEgressIdMapping->getPortAndEgressIdsIf(gport);
-  if (!portAndEgressIds) {
-    return;
-  }
-  if (locked) {
-    hw_->writableHostTable()->egressResolutionChangedHwLocked(
-        portAndEgressIds->getEgressIds(),
-        up ? BcmEcmpEgress::Action::EXPAND : BcmEcmpEgress::Action::SHRINK);
-  } else {
-    CHECK(!up);
-    egressResolutionChangedHwNotLocked(
-        hw_->getUnit(), portAndEgressIds->getEgressIds(), up);
-  }
-}
-
-int BcmEgressManager::removeAllEgressesFromEcmpCallback(
-    int unit,
-    opennsl_l3_egress_ecmp_t* ecmp,
-    int intfCount,
-    opennsl_if_t* intfArray,
-    void* userData) {
-  // loop over the egresses present in the ecmp group. For each that is
-  // in the passed in list of egresses to remove (userData),
-  // remove it from the ecmp group.
-  EgressIdSet* egressesToRemove = static_cast<EgressIdSet*>(userData);
-  for (int i = 0; i < intfCount; ++i) {
-    opennsl_if_t egressInHw = intfArray[i];
-    if (egressesToRemove->find(egressInHw) != egressesToRemove->end()) {
-      BcmEcmpEgress::removeEgressIdHwNotLocked(
-          unit, ecmp->ecmp_intf, egressInHw);
-    }
-  }
-  return 0;
-}
-
-void BcmEgressManager::egressResolutionChangedHwNotLocked(
-    int unit,
-    const EgressIdSet& affectedEgressIds,
-    bool up) {
-  CHECK(!up);
-  EgressIdSet tmpEgressIds(affectedEgressIds);
-  opennsl_l3_egress_ecmp_traverse(
-      unit, removeAllEgressesFromEcmpCallback, &tmpEgressIds);
 }
 
 void BcmHostTable::egressResolutionChangedHwLocked(
