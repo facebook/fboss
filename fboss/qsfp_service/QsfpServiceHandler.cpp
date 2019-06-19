3be7573510e6f4babb21b6029b32fb90bd62702d
Tom Harbert <tdh@fb.com>
2019-06-18 20:24:53 -0700

    thrift request logging
    Summary:
    This diff:
    
    - adds a utility function for logging thrift requests
    - adds a call to this function to all handler methods
    
    Reviewed By: aeckert
    
    Differential Revision: D15684551
    
    fbshipit-source-id: 3f850c54df461ecc8264179f5ebba379025382cb

--- fboss/qsfp_service/QsfpServiceHandler.cpp
+++ fboss/qsfp_service/QsfpServiceHandler.cpp
@@ -1,4 +1,5 @@
 #include "fboss/qsfp_service/QsfpServiceHandler.h"
+#include <fboss/lib/LogThriftCall.h>
 #include <folly/logging/xlog.h>
 
 namespace facebook { namespace fboss {
@@ -18,16 +19,19 @@ facebook::fb303::cpp2::fb_status QsfpServiceHandler::getStatus() {
 }
 
 TransceiverType QsfpServiceHandler::type(int32_t /* unused */) {
+  LogThriftCall log(__func__, getConnectionContext());
   return TransceiverType::QSFP;
 }
 
 void QsfpServiceHandler::getTransceiverInfo(std::map<int32_t,
     TransceiverInfo>& info, std::unique_ptr<std::vector<int32_t>> ids) {
+  LogThriftCall log(__func__, getConnectionContext());
   manager_->getTransceiversInfo(info, std::move(ids));
 }
 
 void QsfpServiceHandler::customizeTransceiver(int32_t idx,
     cfg::PortSpeed speed) {
+  LogThriftCall log(__func__, getConnectionContext());
   XLOG(INFO) << "customizeTransceiver request for " << idx << " to speed "
              << cfg::_PortSpeed_VALUES_TO_NAMES.find(speed)->second;
   manager_->customizeTransceiver(idx, speed);
@@ -36,12 +40,14 @@ void QsfpServiceHandler::customizeTransceiver(int32_t idx,
 void QsfpServiceHandler::getTransceiverRawDOMData(
     std::map<int32_t, RawDOMData>& info,
     std::unique_ptr<std::vector<int32_t>> ids) {
+  LogThriftCall log(__func__, getConnectionContext());
   manager_->getTransceiversRawDOMData(info, std::move(ids));
 }
 
 void QsfpServiceHandler::syncPorts(
     std::map<int32_t, TransceiverInfo>& info,
     std::unique_ptr<std::map<int32_t, PortStatus>> ports) {
+  LogThriftCall log(__func__, getConnectionContext());
   manager_->syncPorts(info, std::move(ports));
 }
 
