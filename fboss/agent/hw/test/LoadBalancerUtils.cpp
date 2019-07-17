3581778016c0cf4bd0057dca088c258962bbf40a
Jasmeet Bagga <jasmeetbagga@fb.com>
2019-07-17 00:53:29 -0700

    Move LoadBalancerUtils to be under hw/test
    Summary:
    As titled. This is looking forward to be able to use
    this with any HwSwitch implementation. For now since we only
    build packet generation, load balancing tests in non-oss targets,
    I am only building closed source versions of these libs. Building
    OSS versions would be straightforward and will be done as needed
    
    Reviewed By: phshaikh
    
    Differential Revision: D16024615
    
    fbshipit-source-id: 6971a36a49b6acd1500b0b79654843eddc9e18fb

---
+++ fboss/agent/hw/test/LoadBalancerUtils.cpp
@@ -0,0 +1,183 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#include "fboss/agent/hw/test/LoadBalancerUtils.h"
+
+#include <folly/IPAddress.h>
+
+#include "fboss/agent/HwSwitch.h"
+#include "fboss/agent/LoadBalancerConfigApplier.h"
+#include "fboss/agent/hw/test/HwTest.h"
+#include "fboss/agent/hw/test/HwTestPacketUtils.h"
+#include "fboss/agent/state/LoadBalancer.h"
+#include "fboss/agent/state/SwitchState.h"
+
+#include <folly/gen/Base.h>
+
+namespace facebook {
+namespace fboss {
+namespace utility {
+
+cfg::Fields getHalfHashFields() {
+  cfg::Fields hashFields;
+  hashFields.set_ipv4Fields(std::set<cfg::IPv4Field>(
+      {cfg::IPv4Field::SOURCE_ADDRESS, cfg::IPv4Field::DESTINATION_ADDRESS}));
+  hashFields.set_ipv6Fields(std::set<cfg::IPv6Field>(
+      {cfg::IPv6Field::SOURCE_ADDRESS, cfg::IPv6Field::DESTINATION_ADDRESS}));
+
+  return hashFields;
+}
+
+cfg::Fields getFullHashFields() {
+  auto hashFields = getHalfHashFields();
+  hashFields.set_transportFields(
+      std::set<cfg::TransportField>({cfg::TransportField::SOURCE_PORT,
+                                     cfg::TransportField::DESTINATION_PORT}));
+  return hashFields;
+}
+
+cfg::LoadBalancer getHalfHashConfig(cfg::LoadBalancerID id) {
+  cfg::LoadBalancer loadBalancer;
+  loadBalancer.id = id;
+  loadBalancer.fieldSelection = getHalfHashFields();
+  loadBalancer.algorithm = cfg::HashingAlgorithm::CRC16_CCITT;
+  return loadBalancer;
+}
+cfg::LoadBalancer getFullHashConfig(cfg::LoadBalancerID id) {
+  cfg::LoadBalancer loadBalancer;
+  loadBalancer.id = id;
+  loadBalancer.fieldSelection = getFullHashFields();
+  loadBalancer.algorithm = cfg::HashingAlgorithm::CRC16_CCITT;
+  return loadBalancer;
+}
+
+cfg::LoadBalancer getEcmpHalfHashConfig() {
+  return getHalfHashConfig(cfg::LoadBalancerID::ECMP);
+}
+cfg::LoadBalancer getEcmpFullHashConfig() {
+  return getFullHashConfig(cfg::LoadBalancerID::ECMP);
+}
+cfg::LoadBalancer getTrunkHalfHashConfig() {
+  return getHalfHashConfig(cfg::LoadBalancerID::AGGREGATE_PORT);
+}
+cfg::LoadBalancer getTrunkFullHashConfig() {
+  return getFullHashConfig(cfg::LoadBalancerID::AGGREGATE_PORT);
+}
+
+std::vector<cfg::LoadBalancer> getEcmpFullTrunkHalfHashConfig() {
+  return {getEcmpFullHashConfig(), getTrunkHalfHashConfig()};
+}
+std::vector<cfg::LoadBalancer> getEcmpHalfTrunkFullHashConfig() {
+  return {getEcmpHalfHashConfig(), getTrunkFullHashConfig()};
+}
+
+std::shared_ptr<SwitchState> addLoadBalancer(
+    const Platform* platform,
+    const std::shared_ptr<SwitchState>& inputState,
+    const cfg::LoadBalancer& loadBalancerCfg) {
+  return addLoadBalancers(platform, inputState, {loadBalancerCfg});
+}
+
+std::shared_ptr<SwitchState> addLoadBalancers(
+    const Platform* platform,
+    const std::shared_ptr<SwitchState>& inputState,
+    const std::vector<cfg::LoadBalancer>& loadBalancerCfgs) {
+  auto newState{inputState->clone()};
+  auto lbMap = newState->getLoadBalancers()->clone();
+  for (const auto& loadBalancerCfg : loadBalancerCfgs) {
+    lbMap->addLoadBalancer(
+        LoadBalancerConfigParser(platform).parse(loadBalancerCfg));
+  }
+  newState->resetLoadBalancers(lbMap);
+  return newState;
+}
+
+void pumpTraffic(
+    bool isV6,
+    HwSwitch* hw,
+    folly::MacAddress cpuMac,
+    folly::Optional<PortID> frontPanelPortToLoopTraffic) {
+  for (auto i = 0; i < 100; ++i) {
+    auto srcIp = folly::IPAddress(
+        folly::sformat(isV6 ? "1001::{}" : "100.0.0.{}", i + 1));
+    for (auto j = 0; j < 100; ++j) {
+      auto dstIp = folly::IPAddress(
+          folly::sformat(isV6 ? "2001::{}" : "200.0.0.{}", j + 1));
+      auto pkt = makeUDPTxPacket(
+          hw, VlanID(1), cpuMac, cpuMac, srcIp, dstIp, 10000 + i, 20000 + j);
+      if (frontPanelPortToLoopTraffic) {
+        hw->sendPacketOutOfPortSync(
+            std::move(pkt), frontPanelPortToLoopTraffic.value());
+      } else {
+        hw->sendPacketSwitchedSync(std::move(pkt));
+      }
+    }
+  }
+}
+
+bool isLoadBalanced(
+    HwTest* hwTest,
+    const std::vector<PortDescriptor>& ecmpPorts,
+    const std::vector<NextHopWeight>& weights,
+    int maxDeviationPct,
+    bool noTrafficOk) {
+  auto portIDs = folly::gen::from(ecmpPorts) |
+      folly::gen::map([](const auto& portDesc) {
+                   CHECK(portDesc.isPhysicalPort());
+                   return portDesc.phyPortID();
+                 }) |
+      folly::gen::as<std::vector<PortID>>();
+  auto portIdToStats = hwTest->getLatestPortStats(portIDs);
+  auto portBytes = folly::gen::from(portIdToStats) |
+      folly::gen::map([](const auto& portIdAndStats) {
+                     return portIdAndStats.second.outBytes_;
+                   }) |
+      folly::gen::as<std::set<uint64_t>>();
+
+  auto lowest = *portBytes.begin();
+  auto highest = *portBytes.rbegin();
+  XLOG(DBG0) << " Highest bytes: " << highest << " lowest bytes: " << lowest;
+  if (!lowest) {
+    return !highest && noTrafficOk;
+  }
+  if (!weights.empty()) {
+    auto maxWeight = *(std::max_element(weights.begin(), weights.end()));
+    for (auto i = 0; i < ecmpPorts.size(); ++i) {
+      auto portOutBytes = portIdToStats[ecmpPorts[i].phyPortID()].outBytes_;
+      auto weightPercent = (static_cast<float>(weights[i]) / maxWeight) * 100.0;
+      auto portOutBytesPercent =
+          (static_cast<float>(portOutBytes) / highest) * 100.0;
+      auto percentDev = std::abs(weightPercent - portOutBytesPercent);
+      // Don't tolerate a deviation of more than maxDeviationPct
+      if (percentDev > maxDeviationPct) {
+        return false;
+      }
+    }
+  } else {
+    auto percentDev = (static_cast<float>(highest - lowest) / lowest) * 100.0;
+    // Don't tolerate a deviation of more than maxDeviationPct
+    if (percentDev > maxDeviationPct) {
+      return false;
+    }
+  }
+  return true;
+}
+
+bool isLoadBalanced(
+    HwTest* hwTest,
+    const std::vector<PortDescriptor>& ecmpPorts,
+    int maxDeviationPct) {
+  return isLoadBalanced(
+      hwTest, ecmpPorts, std::vector<NextHopWeight>(), maxDeviationPct);
+}
+
+} // namespace utility
+} // namespace fboss
+} // namespace facebook
---
+++ fboss/agent/hw/test/LoadBalancerUtils.h
@@ -0,0 +1,76 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+#pragma once
+
+#include "fboss/agent/gen-cpp2/switch_config_types.h"
+#include "fboss/agent/state/PortDescriptor.h"
+#include "fboss/agent/state/RouteNextHop.h"
+#include "fboss/agent/types.h"
+
+#include <folly/Optional.h>
+
+#include <vector>
+
+namespace facebook {
+namespace fboss {
+class HwSwitch;
+class Platform;
+class SwitchState;
+class LoadBalancer;
+class HwTest;
+} // namespace fboss
+} // namespace facebook
+
+namespace facebook {
+namespace fboss {
+namespace utility {
+
+cfg::Fields getHalfHashFields();
+cfg::Fields getFullHashFields();
+cfg::LoadBalancer getHalfHashConfig(cfg::LoadBalancerID id);
+cfg::LoadBalancer getFullHashConfig(cfg::LoadBalancerID id);
+cfg::LoadBalancer getEcmpHalfHashConfig();
+cfg::LoadBalancer getEcmpFullHashConfig();
+cfg::LoadBalancer getTrunkHalfHashConfig();
+cfg::LoadBalancer getTrunkFullHashConfig();
+std::vector<cfg::LoadBalancer> getEcmpFullTrunkHalfHashConfig();
+std::vector<cfg::LoadBalancer> getEcmpHalfTrunkFullHashConfig();
+
+std::shared_ptr<SwitchState> addLoadBalancer(
+    const Platform* platform,
+    const std::shared_ptr<SwitchState>& inputState,
+    const cfg::LoadBalancer& loadBalancer);
+
+std::shared_ptr<SwitchState> addLoadBalancers(
+    const Platform* platform,
+    const std::shared_ptr<SwitchState>& inputState,
+    const std::vector<cfg::LoadBalancer>& loadBalancers);
+
+void pumpTraffic(
+    bool isV6,
+    HwSwitch* hw,
+    folly::MacAddress cpuMac,
+    folly::Optional<PortID> frontPanelPortToLoopTraffic = folly::none);
+
+bool isLoadBalanced(
+    HwTest* hwTest,
+    const std::vector<PortDescriptor>& ecmpPorts,
+    const std::vector<NextHopWeight>& weights,
+    int maxDeviationPct,
+    /* Flag to control whether having no traffic on a link is ok */
+    bool noTrafficOk = false);
+
+bool isLoadBalanced(
+    HwTest* hwTest,
+    const std::vector<PortDescriptor>& ecmpPorts,
+    int maxDeviationPct);
+} // namespace utility
+} // namespace fboss
+} // namespace facebook
