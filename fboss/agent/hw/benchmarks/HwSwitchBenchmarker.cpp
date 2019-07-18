1bcbf31d0e65df9b6119928934ed581682ef5064
Jasmeet Bagga <jasmeetbagga@fb.com>
2019-07-18 00:06:38 -0700

    Introducing HwSwitchBenchmarker
    Summary:
    Introducing HwSwitchBenchmarker  - to be used in later diffs for
    benchmarking h/w switch centric operations.
    The first target is ECMP shrink operation on BRCM switches.
    
    Reviewed By: phshaikh
    
    Differential Revision: D16097282
    
    fbshipit-source-id: cc11df0e8e0c2626b2c48668190547a232baa7b9

---
+++ fboss/agent/hw/benchmarks/HwSwitchBenchmarker.cpp
@@ -0,0 +1,77 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#include "fboss/agent/hw/benchmarks/HwSwitchBenchmarker.h"
+
+#include "fboss/agent/AlpmUtils.h"
+#include "fboss/agent/ApplyThriftConfig.h"
+#include "fboss/agent/HwSwitch.h"
+#include "fboss/agent/Platform.h"
+#include "fboss/agent/hw/test/HwLinkStateToggler.h"
+#include "fboss/agent/platforms/test_platforms/CreateTestPlatform.h"
+#include "fboss/agent/state/SwitchState.h"
+
+namespace facebook {
+namespace fboss {
+
+HwSwitchBenchmarker::HwSwitchBenchmarker() {}
+
+HwSwitchBenchmarker::~HwSwitchBenchmarker() {}
+
+void HwSwitchBenchmarker::applyNewState(std::shared_ptr<SwitchState> newState) {
+  if (!newState) {
+    return;
+  }
+  StateDelta delta(programmedState_, newState);
+  programmedState_ = hwSwitch_->stateChanged(delta);
+  programmedState_->publish();
+}
+
+void HwSwitchBenchmarker::applyInitialConfigAndBringUpPorts(
+    const cfg::SwitchConfig& initCfg) {
+  CHECK(linkToggler_);
+  linkToggler_->applyInitialConfigAndBringUpPorts(
+      getProgrammedState(), getPlatform(), initCfg);
+  initCfgState_ = getProgrammedState();
+}
+
+void HwSwitchBenchmarker::linkStateChanged(PortID port, bool up) {
+  if (linkToggler_) {
+    linkToggler_->linkStateChanged(port, up);
+  }
+}
+
+void HwSwitchBenchmarker::init() {
+  platform_ = createTestPlatform();
+  hwSwitch_ = createHwSwitch(platform_.get());
+  linkToggler_ = createLinkToggler(getHwSwitch());
+  programmedState_ = hwSwitch_->init(this).switchState;
+  // HwSwitch::init() returns an unpublished programmedState_.  SwSwitch is
+  // normally responsible for publishing it.  Go ahead and call publish now.
+  // This will catch errors if test cases accidentally try to modify this
+  // programmedState_ without first cloning it.
+  programmedState_->publish();
+  // Handle ALPM state. ALPM requires that default routes be programmed
+  // before any other routes. We handle that setup here. Similarly ALPM
+  // requires that default routes be deleted last. That aspect is handled
+  // in TearDown
+  auto alpmState = setupAlpmState(programmedState_);
+  if (alpmState) {
+    applyNewState(alpmState);
+  }
+  hwSwitch_->switchRunStateChanged(SwitchRunState::INITIALIZED);
+}
+
+void HwSwitchBenchmarker::revertToInitCfgState() {
+  CHECK(initCfgState_);
+  applyNewState(initCfgState_);
+}
+} // namespace fboss
+} // namespace facebook
---
+++ fboss/agent/hw/benchmarks/HwSwitchBenchmarker.h
@@ -0,0 +1,69 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#pragma once
+
+#include "fboss/agent/HwSwitch.h"
+#include "fboss/agent/types.h"
+
+#include <memory>
+
+namespace facebook {
+namespace fboss {
+
+class Platform;
+class SwitchState;
+class HwLinkStateToggler;
+
+class HwSwitchBenchmarker : public HwSwitch::Callback {
+ public:
+  HwSwitchBenchmarker();
+  ~HwSwitchBenchmarker() override;
+  /*
+   * Setup and init platform switch
+   */
+  void init();
+  /*
+   * Revert back to post init state. Can be used by each
+   * benchmark iteration to start with a clean slate.
+   */
+  void revertToInitCfgState();
+  void applyNewState(std::shared_ptr<SwitchState> newState);
+  void applyInitialConfigAndBringUpPorts(const cfg::SwitchConfig& cfg);
+
+  std::shared_ptr<SwitchState> getProgrammedState() const {
+    return programmedState_;
+  }
+  virtual HwLinkStateToggler* getLinkToggler() {
+    return linkToggler_.get();
+  }
+  virtual Platform* getPlatform() {
+    return platform_.get();
+  }
+  virtual HwSwitch* getHwSwitch() {
+    return hwSwitch_.get();
+  }
+  void packetReceived(std::unique_ptr<RxPacket> /*pkt*/) noexcept override {}
+  void linkStateChanged(PortID port, bool up) override;
+  void exitFatal() const noexcept override {}
+
+ private:
+  virtual std::unique_ptr<HwSwitch> createHwSwitch(Platform* platform) = 0;
+  virtual std::unique_ptr<HwLinkStateToggler> createLinkToggler(
+      HwSwitch* hwSwitch) = 0;
+  std::shared_ptr<SwitchState> programmedState_;
+  std::shared_ptr<SwitchState> initCfgState_;
+  std::unique_ptr<Platform> platform_;
+  std::unique_ptr<HwSwitch> hwSwitch_;
+  std::unique_ptr<HwLinkStateToggler> linkToggler_;
+};
+
+} // namespace fboss
+} // namespace facebook
