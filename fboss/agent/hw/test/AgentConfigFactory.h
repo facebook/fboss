4482de7d5521748d4c406cee758db35f7136affb
Srikrishna Gopu <srikrishnagopu@fb.com>
2019-07-18 14:56:05 -0700

    agent config factory
    Summary:
    Config factory to generate platform ports for agent unified config.
    
    - Creates 128 ports in agent unified config.
    - Adds speed config for 10G, 25G, 50G and 100G.
    - Adds lanes as same as the port number.
    - For eg: Port 1 will have
     -- lane 1 in 10G and 25G mode
     -- lane 1 and 2 in 50G mode
     -- lane 1, 2, 3 and 4 in 100G mode.
    
    TODO:
    
     - This will work only for fake tests. Need to figure out a way to encode the actual platform port config here to run the tests on the asic.
    - To generate random agent config
    
    Reviewed By: boryas
    
    Differential Revision: D16063439
    
    fbshipit-source-id: c64b1ba63d663303d118ada04cbe4462c83296eb

---
+++ fboss/agent/hw/test/AgentConfigFactory.h
@@ -0,0 +1,28 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#pragma once
+#include "fboss/agent/gen-cpp2/agent_config_types.h"
+#include "fboss/agent/types.h"
+
+#include <vector>
+
+/*
+ * This utility is to provide utils for test.
+ */
+namespace facebook {
+namespace fboss {
+namespace utility {
+const uint16_t kMaxPorts = 128;
+
+cfg::AgentConfig getAgentConfig();
+} // namespace utility
+} // namespace fboss
+} // namespace facebook
