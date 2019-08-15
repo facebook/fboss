bf326712d7d909b7a7d64fca5d29269d8da52a34
Boris Burkov <borisb@fb.com>
2019-08-14 21:50:50 -0700

    move tuple utilites to fboss/lib
    Summary:
    tupleForEach, Map, and Projection are useful for things other than
    tuples of SaiAttributes, so move them into a generally usable fboss lib
    
    Differential Revision: D16701667
    
    fbshipit-source-id: 7086d10e836123e9442f4291e458e593a4ff3b32

---
+++ fboss/lib/TupleUtils.h
@@ -0,0 +1,95 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#pragma once
+
+#include <optional>
+#include <tuple>
+#include <type_traits>
+
+namespace facebook {
+namespace fboss {
+
+/*
+ * Regular functions
+ *
+ * This part of the library operates on runtime tuples and perform some
+ * transformation or operation like a loop, mapping, projection, etc...
+ *
+ * e.g., for each element in a tuple, call foo() on it
+ */
+
+template <typename Fn, typename TupleT>
+void tupleForEach(const Fn& fn, TupleT& tup) {
+  std::apply([&](auto&... t) { (..., fn(t)); }, tup);
+}
+
+template <typename Fn, typename TupleT>
+auto tupleMap(const Fn& fn, TupleT&& tup) {
+  auto map_helper = [&fn](auto&&... attr) { return std::tuple{fn(attr)...}; };
+  return std::apply(map_helper, tup);
+}
+
+template <typename FullTupleT, typename ProjectionTupleT>
+ProjectionTupleT tupleProjection(const FullTupleT& fullTuple) {
+  ProjectionTupleT ret;
+  auto projection_helper = [&ret, &fullTuple](auto& t) {
+    std::get<typename std::remove_reference<decltype(t)>::type>(ret) =
+        std::get<typename std::remove_reference<decltype(t)>::type>(fullTuple);
+  };
+  tupleForEach(projection_helper, ret);
+  return ret;
+}
+
+/*
+ * Meta Functions
+ *
+ * This part of the library operates on tuple types to modify them in some way
+ * For example, concatenation or filtering based on some other meta function.
+ *
+ * e.g., given two tuple types, get the type of the tuple of all the elements of
+ * both. Or, given a tuple type and a meta function, get the type of the tuple
+ * whose element types have a true value under that meta function
+ */
+
+template <typename T1, typename T2>
+struct IsElementOfTuple : std::false_type {};
+
+template <typename T, typename... Ts>
+struct IsElementOfTuple<T, std::tuple<Ts...>>
+    : std::disjunction<std::is_same<T, Ts>...> {};
+
+template <typename T1, typename T2>
+struct IsSubsetOfTuple : std::false_type {};
+
+template <typename... T1s, typename... T2s>
+struct IsSubsetOfTuple<std::tuple<T1s...>, std::tuple<T2s...>>
+    : std::conjunction<IsElementOfTuple<T1s, std::tuple<T2s...>>...> {};
+
+template <typename T1, typename T2>
+struct tupleConcat {};
+template <typename... T1s, typename... T2s>
+struct tupleConcat<std::tuple<T1s...>, std::tuple<T2s...>> {
+  using type = std::tuple<T1s..., T2s...>;
+};
+template <typename T1, typename T2>
+using tupleConcat_t = typename tupleConcat<T1, T2>::type;
+
+template <typename T>
+struct makeTupleElementsOptional {};
+template <typename... Ts>
+struct makeTupleElementsOptional<std::tuple<Ts...>> {
+  using type = std::tuple<std::optional<Ts>...>;
+};
+template <typename T>
+using makeTupleElementsOptional_t = typename makeTupleElementsOptional<T>::type;
+
+} // namespace fboss
+} // namespace facebook
---
+++ fboss/lib/test/TupleUtilsTest.cpp
@@ -0,0 +1,135 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#include "fboss/lib/TupleUtils.h"
+
+#include <gtest/gtest.h>
+
+#include <string>
+#include <tuple>
+
+using namespace facebook::fboss;
+
+struct A {
+  int a;
+};
+
+TEST(TupleForEach, oneElement) {
+  std::tuple<int> t{42};
+  int sum = 0;
+  tupleForEach([&sum](const auto& e) { sum += e; }, t);
+  EXPECT_EQ(sum, 42);
+}
+
+TEST(TupleForEach, multipleElements) {
+  std::tuple<int, double> t{42, 4.2};
+  double sum = 0;
+  tupleForEach([&sum](const auto& e) { sum += e; }, t);
+  EXPECT_EQ(sum, 46.2);
+}
+
+TEST(TupleForEach, duplicateTypes) {
+  std::tuple<int, int> t{40, 2};
+  int sum = 0;
+  tupleForEach([&sum](const auto& e) { sum += e; }, t);
+  EXPECT_EQ(sum, 42);
+}
+
+TEST(TupleForEach, sideEffect) {
+  std::tuple<A, A> t{};
+  A& a1 = std::get<0>(t);
+  A& a2 = std::get<1>(t);
+  a1.a = 40;
+  a2.a = 2;
+  int sum = 0;
+  tupleForEach(
+      [&sum](auto& e) {
+        sum += e.a;
+        e.a++;
+      },
+      t);
+  EXPECT_EQ(sum, 42);
+  EXPECT_EQ(a1.a, 41);
+  EXPECT_EQ(a2.a, 3);
+}
+
+TEST(TupleMap, oneElement) {
+  std::tuple<int> t{42};
+  auto doubled = tupleMap([](const auto& e) { return e * 2; }, t);
+  EXPECT_EQ(doubled, std::tuple{84});
+}
+
+TEST(TupleMap, multipleElements) {
+  std::tuple<int, double> t{42, 4.2};
+  auto doubled = tupleMap([](const auto& e) { return e * 2; }, t);
+  std::tuple<int, double> expected{84, 8.4};
+  EXPECT_EQ(doubled, expected);
+}
+
+TEST(TupleMap, duplicateTypes) {
+  std::tuple<int, int> t{42, 2};
+  auto doubled = tupleMap([](const auto& e) { return e * 2; }, t);
+  std::tuple<int, int> expected{84, 4};
+  EXPECT_EQ(doubled, expected);
+}
+
+TEST(TupleMap, sideEffect) {
+  std::tuple<A, A> t{};
+  A& a1 = std::get<0>(t);
+  A& a2 = std::get<1>(t);
+  a1.a = 42;
+  a2.a = 2;
+  auto doubled = tupleMap(
+      [](auto& e) {
+        e.a *= 2;
+        return e.a;
+      },
+      t);
+  std::tuple<int, int> expected{84, 4};
+  EXPECT_EQ(doubled, expected);
+  EXPECT_EQ(a1.a, 84);
+  EXPECT_EQ(a2.a, 4);
+}
+
+TEST(TupleProjection, OneToOne) {
+  std::tuple<int> t1{42};
+  auto t2 = tupleProjection<std::tuple<int>, std::tuple<int>>(t1);
+  EXPECT_EQ(t1, t2);
+}
+
+TEST(TupleProjection, OneToZero) {
+  std::tuple<int> t1{42};
+  auto t2 = tupleProjection<std::tuple<int>, std::tuple<>>(t1);
+  EXPECT_EQ(t2, std::tuple<>{});
+}
+
+TEST(TupleProjection, TwoToOne) {
+  std::tuple<int, double> t1{42, 4.2};
+  auto t2 = tupleProjection<std::tuple<int, double>, std::tuple<double>>(t1);
+  EXPECT_EQ(t2, std::tuple<double>{4.2});
+}
+
+TEST(TupleProjection, ThreeToTwo) {
+  std::tuple<int, double, std::string> t1{42, 4.2, "hello"};
+  auto t2 = tupleProjection<
+      std::tuple<int, double, std::string>,
+      std::tuple<int, std::string>>(t1);
+  std::tuple<int, std::string> expected{42, "hello"};
+  EXPECT_EQ(t2, expected);
+}
+
+TEST(TupleProjection, ChangeOrder) {
+  std::tuple<int, double, std::string> t1{42, 4.2, "hello"};
+  auto t2 = tupleProjection<
+      std::tuple<int, double, std::string>,
+      std::tuple<std::string, int>>(t1);
+  std::tuple<std::string, int> expected{"hello", 42};
+  EXPECT_EQ(t2, expected);
+}
