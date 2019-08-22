ac7f5030a833254b13d62e6154e2ae311129df85
Jasmeet Bagga <jasmeetbagga@fb.com>
2019-08-22 12:18:23 -0700

    Add route add, del benchmarks for {FSW, ThAlpm, HgridUU, HgridDU} route scale
    Summary:
    As titled. I went for one binary per benchmarks for the following
    reasons
    - Current netacastle model expects a single binary per benchmark (though
    we may be able to extend it to get multiple benchmarks per binary in future).
    - Running multiple benchmarks in a single binary requires, doing either
     - Cold reset b/w 2 benchmarks. This kind of works but is unsupported by BRCM
     - Ensuring that each benchmarks leaves switch in a state where other benchmark
     could continue its work. This is easy to make work b/w route add, del
     benchmarks, but in general will introduce coupling b/w multiple benchmarks.
    
    So go with the simplest approach of one binary per benchmark
    
    Differential Revision: D16957715
    
    fbshipit-source-id: 3905fa67055916cb42cc03388ca96a9c637a389b

---
+++ fboss/agent/hw/bcm/benchmarks/BcmFswScaleRouteDelBenchmark.cpp
@@ -0,0 +1,24 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#include "fboss/agent/hw/bcm/tests/BcmSwitchEnsemble.h"
+#include "fboss/agent/hw/benchmarks/HwRouteScaleBenchmarkHelpers.h"
+
+#include "fboss/agent/test/RouteScaleGenerators.h"
+
+namespace facebook {
+namespace fboss {
+
+ROUTE_DEL_BENCHMARK(
+    BcmFswScaleRouteDelBenchmark,
+    BcmSwitchEnsemble,
+    utility::FSWRouteScaleGenerator);
+}
+} // namespace facebook
