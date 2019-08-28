3527c8dfe0cf28519da84bbd9ebe5b1ce88a2b83
Jasmeet Bagga <jasmeetbagga@fb.com>
2019-08-27 23:35:40 -0700

    Emit cpu, rss usage for hw benchmarks
    Summary:
    As titled. For each benchmark, emit cpu time spent and max RSS
    consumed in running that benchmark. I chose to measure this over the
    entire benchmark run vs a fine grained operation like ECMP shrink.
    The idea behind this is to catch a wider swathe of BCM switch and
    SDK resource usage regressions in addition to the time based benchmarking
    of specific operations (routes add, ecmp shrink etc).
    
    As a illustration, this catches the CPU regression in 6.5.16 that
    we have been battling recently. A run of ecmp shrink benchmark with
    resource usage shows a ~30% increase in CPU time for SDK 6.5.16, even
    while the ECMP shrink time remains unchanged P106440984
    
    We would still need to carefully pick CPU, memory benchmark thresholds
    so as to not get noise. That will be determined experimentally.
    When we get a new SDK, the guideline would be to use the same thresholds
    as before and then alert on any regressions.
    
    This only covers the bottom half of our code, a similar approach should
    be extended to top half benchmarking as well.
    
    Reviewed By: shri-khare
    
    Differential Revision: D17081976
    
    fbshipit-source-id: e191980c27a6333cea3e8a992b90983f0e5939ad

--- fboss/agent/hw/benchmarks/BenchmarkMain.cpp
+++ fboss/agent/hw/benchmarks/BenchmarkMain.cpp
@@ -8,11 +8,33 @@
  *
  */
 
+#include "common/time/TimeUtil.h"
+
 #include <folly/Benchmark.h>
+#include <folly/dynamic.h>
 #include <folly/init/Init.h>
+#include <folly/json.h>
+
+#include <sys/resource.h>
+#include <sys/time.h>
+
+#include <iostream>
+
+using facebook::timevalToUsec;
 
 int main(int argc, char* argv[]) {
   folly::init(&argc, &argv, true);
+  struct rusage startUsage, endUsage;
+  getrusage(RUSAGE_SELF, &startUsage);
   folly::runBenchmarks();
+  getrusage(RUSAGE_SELF, &endUsage);
+  auto cpuTime =
+      (timevalToUsec(endUsage.ru_stime) - timevalToUsec(startUsage.ru_stime)) +
+      (timevalToUsec(endUsage.ru_utime) - timevalToUsec(endUsage.ru_utime));
+
+  folly::dynamic rusageJson = folly::dynamic::object;
+  rusageJson["cpu_time_usec"] = cpuTime;
+  rusageJson["max_rss"] = endUsage.ru_maxrss;
+  std::cout << toPrettyJson(rusageJson) << std::endl;
   return 0;
 }
