25ec4511914ca1369e08c1050fd2f36904cc9736
Parvez Shaikh <pshaikh@fb.com>
2019-09-10 10:24:17 -0700

    packet snooper
    Summary: a helper class to snoop packet (ethernet frame)
    
    Differential Revision: D17188434
    
    fbshipit-source-id: 1928ee76d6d42f7e746c606ba935561081222cfe

---
+++ fboss/agent/hw/test/HwTestPacketSnooper.cpp
@@ -0,0 +1,36 @@
+// Copyright 2004-present Facebook. All Rights Reserved.
+
+#include "fboss/agent/hw/test/HwTestPacketSnooper.h"
+#include "fboss/agent/RxPacket.h"
+
+#include <folly/logging/xlog.h>
+
+namespace facebook {
+namespace fboss {
+
+HwTestPacketSnooper::HwTestPacketSnooper(HwSwitchEnsemble* ensemble)
+    : ensemble_(ensemble) {
+  ensemble_->addHwEventObserver(this);
+}
+
+HwTestPacketSnooper::~HwTestPacketSnooper() {
+  ensemble_->removeHwEventObserver(this);
+}
+
+void HwTestPacketSnooper::packetReceived(RxPacket* pkt) noexcept {
+  std::lock_guard<std::mutex> lock(mtx_);
+  data_ = pkt->buf()->clone();
+  cv_.notify_all();
+}
+
+folly::Optional<utility::EthFrame> HwTestPacketSnooper::waitForPacket() {
+  std::unique_lock<std::mutex> lock(mtx_);
+  while (!data_) {
+    cv_.wait(lock);
+  }
+  folly::io::Cursor cursor{data_.get()};
+  return utility::EthFrame{cursor};
+}
+
+} // namespace fboss
+} // namespace facebook
---
+++ fboss/agent/hw/test/HwTestPacketSnooper.h
@@ -0,0 +1,33 @@
+// Copyright 2004-present Facebook. All Rights Reserved.
+
+#pragma once
+
+#include "fboss/agent/hw/test/HwSwitchEnsemble.h"
+#include "fboss/agent/hw/test/HwTestPacketUtils.h"
+
+#include <folly/Optional.h>
+#include <folly/io/IOBuf.h>
+#include <condition_variable>
+
+namespace facebook {
+namespace fboss {
+
+class RxPacket;
+
+class HwTestPacketSnooper : public HwSwitchEnsemble::HwSwitchEventObserverIf {
+ public:
+  explicit HwTestPacketSnooper(HwSwitchEnsemble* ensemble);
+  virtual ~HwTestPacketSnooper() override;
+  void packetReceived(RxPacket* pkt) noexcept override;
+  folly::Optional<utility::EthFrame> waitForPacket();
+
+ private:
+  void linkStateChanged(PortID /*port*/, bool /*up*/) override {}
+  HwSwitchEnsemble* ensemble_;
+  std::mutex mtx_;
+  std::condition_variable cv_;
+  std::unique_ptr<folly::IOBuf> data_;
+};
+
+} // namespace fboss
+} // namespace facebook
