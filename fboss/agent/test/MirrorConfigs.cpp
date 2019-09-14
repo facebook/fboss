fdbac1df219dfffc32bea25e97beb0308b6c58a5
Parvez Shaikh <pshaikh@fb.com>
2019-09-13 20:51:53 -0700

    mirror configs
    Summary:
    number of ways in which mirrors can be configured is getting very large
    - SPAN
    - ERSPAN
       - ERSPAN without egress port
       - ERSPAN with egress port
       - ERSPAN with DSCP
       - ERSPAN without DSCP
       - ERSPAN with source ip
       - ERSPAN without source ip
    - SFLOW
       - SFLOW without egress port
       - SFLOW with egress port
       - SFLOW with DSCP
       - SFLOW without DSCP
       - SFLOW with source ip
       - SFLOW without source ip
    
    giving a library which gives properly filled cfg::Mirror object for various combinations above.
    
    Differential Revision: D17368393
    
    fbshipit-source-id: 63c4bcceaeed122ceebc1ee5abef20bbbbe81a93

---
+++ fboss/agent/test/MirrorConfigs.cpp
@@ -0,0 +1,197 @@
+// Copyright 2004-present Facebook. All Rights Reserved.
+
+#include "fboss/agent/test/MirrorConfigs.h"
+
+namespace facebook {
+namespace fboss {
+namespace utility {
+cfg::MirrorEgressPort getMirrorEgressPort(const std::string& portName) {
+  cfg::MirrorEgressPort egressPort;
+  egressPort.set_name(portName);
+  return egressPort;
+}
+
+cfg::MirrorEgressPort getMirrorEgressPort(PortID portID) {
+  cfg::MirrorEgressPort egressPort;
+  egressPort.set_logicalID(portID);
+  return egressPort;
+}
+
+cfg::Mirror getSPANMirror(
+    const std::string& name,
+    PortID portID,
+    uint8_t dscp,
+    bool truncate) {
+  cfg::Mirror mirror;
+  mirror.set_name(name);
+  mirror.destination.set_egressPort(getMirrorEgressPort(portID));
+  mirror.set_dscp(dscp);
+  mirror.set_truncate(truncate);
+  return mirror;
+}
+
+cfg::Mirror getSPANMirror(
+    const std::string& name,
+    const std::string& portName,
+    uint8_t dscp,
+    bool truncate) {
+  cfg::Mirror mirror;
+  mirror.set_name(name);
+  mirror.destination.set_egressPort(getMirrorEgressPort(portName));
+  mirror.set_dscp(dscp);
+  mirror.set_truncate(truncate);
+  return mirror;
+}
+
+cfg::GreTunnel getGreTunnelConfig(folly::IPAddress dstAddress) {
+  cfg::GreTunnel greTunnel;
+  greTunnel.set_ip(dstAddress.str());
+  return greTunnel;
+}
+
+cfg::SflowTunnel getSflowTunnelConfig(
+    folly::IPAddress dstAddress,
+    uint16_t sPort,
+    uint16_t dPort) {
+  cfg::SflowTunnel sflowTunnel;
+  sflowTunnel.set_ip(dstAddress.str());
+  sflowTunnel.set_udpSrcPort(sPort);
+  sflowTunnel.set_udpDstPort(dPort);
+  return sflowTunnel;
+}
+
+cfg::MirrorTunnel getGREMirrorTunnelConfig(
+    folly::IPAddress dstAddress,
+    folly::Optional<folly::IPAddress> srcAddress) {
+  cfg::MirrorTunnel mirrorTunnel;
+  mirrorTunnel.set_greTunnel(getGreTunnelConfig(dstAddress));
+  if (srcAddress) {
+    mirrorTunnel.set_srcIp(srcAddress->str());
+  }
+  return mirrorTunnel;
+}
+
+cfg::MirrorTunnel getSflowMirrorTunnelConfig(
+    folly::IPAddress dstAddress,
+    uint16_t sPort,
+    uint16_t dPort,
+    folly::Optional<folly::IPAddress> srcAddress) {
+  cfg::MirrorTunnel mirrorTunnel;
+  mirrorTunnel.set_sflowTunnel(getSflowTunnelConfig(dstAddress, sPort, dPort));
+  if (srcAddress) {
+    mirrorTunnel.set_srcIp(srcAddress->str());
+  }
+  return mirrorTunnel;
+}
+
+cfg::Mirror getGREMirror(
+    const std::string& name,
+    folly::IPAddress dstAddress,
+    folly::Optional<folly::IPAddress> srcAddress,
+    uint8_t dscp,
+    bool truncate) {
+  cfg::Mirror mirror;
+  mirror.set_name(name);
+  mirror.set_dscp(dscp);
+  mirror.set_truncate(truncate);
+  mirror.destination.set_tunnel(
+      getGREMirrorTunnelConfig(dstAddress, srcAddress));
+  return mirror;
+}
+
+cfg::Mirror getGREMirrorWithPort(
+    const std::string& name,
+    PortID portID,
+    folly::IPAddress dstAddress,
+    folly::Optional<folly::IPAddress> srcAddress,
+    uint8_t dscp,
+    bool truncate) {
+  cfg::Mirror mirror;
+  mirror.set_name(name);
+  mirror.set_dscp(dscp);
+  mirror.set_truncate(truncate);
+  mirror.destination.set_egressPort(getMirrorEgressPort(portID));
+  mirror.destination.set_tunnel(
+      getGREMirrorTunnelConfig(dstAddress, srcAddress));
+  return mirror;
+}
+
+cfg::Mirror getGREMirrorWithPort(
+    const std::string& name,
+    const std::string& portName,
+    folly::IPAddress dstAddress,
+    folly::Optional<folly::IPAddress> srcAddress,
+    uint8_t dscp,
+    bool truncate) {
+  cfg::Mirror mirror;
+  mirror.set_name(name);
+  mirror.set_dscp(dscp);
+  mirror.set_truncate(truncate);
+  mirror.destination.set_egressPort(getMirrorEgressPort(portName));
+  mirror.destination.set_tunnel(
+      getGREMirrorTunnelConfig(dstAddress, srcAddress));
+  return mirror;
+}
+
+cfg::Mirror getSFlowMirror(
+    const std::string& name,
+    uint16_t sPort,
+    uint16_t dPort,
+    folly::IPAddress dstAddress,
+    folly::Optional<folly::IPAddress> srcAddress,
+    uint8_t dscp,
+    bool truncate) {
+  cfg::Mirror mirror;
+  mirror.set_name(name);
+  mirror.set_dscp(dscp);
+  mirror.set_truncate(truncate);
+  mirror.destination.set_tunnel(
+      getSflowMirrorTunnelConfig(dstAddress, sPort, dPort, srcAddress));
+  return mirror;
+}
+
+cfg::Mirror getSFlowMirrorWithPort(
+    const std::string& name,
+    PortID portID,
+    uint16_t sPort,
+    uint16_t dPort,
+    folly::IPAddress dstAddress,
+    folly::Optional<folly::IPAddress> srcAddress,
+    uint8_t dscp,
+    bool truncate
+
+) {
+  cfg::Mirror mirror;
+  mirror.set_name(name);
+  mirror.set_dscp(dscp);
+  mirror.set_truncate(truncate);
+  mirror.destination.set_egressPort(getMirrorEgressPort(portID));
+  mirror.destination.set_tunnel(
+      getSflowMirrorTunnelConfig(dstAddress, sPort, dPort, srcAddress));
+  return mirror;
+}
+
+cfg::Mirror getSFlowMirrorWithPortName(
+    const std::string& name,
+    const std::string& portName,
+    uint16_t sPort,
+    uint16_t dPort,
+    folly::IPAddress dstAddress,
+    folly::Optional<folly::IPAddress> srcAddress,
+    uint8_t dscp,
+    bool truncate
+
+) {
+  cfg::Mirror mirror;
+  mirror.set_name(name);
+  mirror.set_dscp(dscp);
+  mirror.set_truncate(truncate);
+  mirror.destination.set_egressPort(getMirrorEgressPort(portName));
+  mirror.destination.set_tunnel(
+      getSflowMirrorTunnelConfig(dstAddress, sPort, dPort, srcAddress));
+  return mirror;
+}
+
+} // namespace utility
+} // namespace fboss
+} // namespace facebook
---
+++ fboss/agent/test/MirrorConfigs.h
@@ -0,0 +1,105 @@
+// Copyright 2004-present Facebook. All Rights Reserved.
+
+#pragma once
+
+#include "fboss/agent/gen-cpp2/switch_config_constants.h"
+#include "fboss/agent/types.h"
+
+#include <string>
+#include "folly/IPAddress.h"
+#include "folly/IPAddressV4.h"
+#include "folly/IPAddressV6.h"
+
+namespace facebook {
+namespace fboss {
+namespace utility {
+
+cfg::MirrorEgressPort getMirrorEgressPort(const std::string& portName);
+
+cfg::MirrorEgressPort getMirrorEgressPort(PortID portID);
+cfg::Mirror getSPANMirror(
+    const std::string& name,
+    PortID portID,
+    uint8_t dscp = cfg::switch_config_constants::DEFAULT_MIRROR_DSCP_,
+    bool truncate = false);
+
+cfg::Mirror getSPANMirror(
+    const std::string& name,
+    const std::string& portName,
+    uint8_t dscp = cfg::switch_config_constants::DEFAULT_MIRROR_DSCP_,
+    bool truncate = false);
+
+cfg::GreTunnel getGreTunnelConfig(folly::IPAddress dstAddress);
+
+cfg::SflowTunnel getSflowTunnelConfig(
+    folly::IPAddress dstAddress,
+    uint16_t sPort,
+    uint16_t dPort);
+
+cfg::MirrorTunnel getGREMirrorTunnelConfig(
+    folly::IPAddress dstAddress,
+    folly::Optional<folly::IPAddress> srcAddress);
+
+cfg::MirrorTunnel getSflowMirrorTunnelConfig(
+    folly::IPAddress dstAddress,
+    uint16_t sPort,
+    uint16_t dPort,
+    folly::Optional<folly::IPAddress> srcAddress);
+
+cfg::Mirror getGREMirror(
+    const std::string& name,
+    folly::IPAddress dstAddress,
+    folly::Optional<folly::IPAddress> srcAddress,
+    uint8_t dscp = cfg::switch_config_constants::DEFAULT_MIRROR_DSCP_,
+    bool truncate = false);
+
+cfg::Mirror getGREMirrorWithPort(
+    const std::string& name,
+    PortID portID,
+    folly::IPAddress dstAddress,
+    folly::Optional<folly::IPAddress> srcAddress,
+    uint8_t dscp = cfg::switch_config_constants::DEFAULT_MIRROR_DSCP_,
+    bool truncate = false);
+
+cfg::Mirror getGREMirrorWithPort(
+    const std::string& name,
+    const std::string& portName,
+    folly::IPAddress dstAddress,
+    folly::Optional<folly::IPAddress> srcAddress,
+    uint8_t dscp = cfg::switch_config_constants::DEFAULT_MIRROR_DSCP_,
+    bool truncate = false);
+cfg::Mirror getSFlowMirror(
+    const std::string& name,
+    uint16_t sPort,
+    uint16_t dPort,
+    folly::IPAddress dstAddress,
+    folly::Optional<folly::IPAddress> srcAddress,
+    uint8_t dscp = cfg::switch_config_constants::DEFAULT_MIRROR_DSCP_,
+    bool truncate = false);
+
+cfg::Mirror getSFlowMirrorWithPort(
+    const std::string& name,
+    PortID portID,
+    uint16_t sPort,
+    uint16_t dPort,
+    folly::IPAddress dstAddress,
+    folly::Optional<folly::IPAddress> srcAddress,
+    uint8_t dscp = cfg::switch_config_constants::DEFAULT_MIRROR_DSCP_,
+    bool truncate = false
+
+);
+cfg::Mirror getSFlowMirrorWithPortName(
+    const std::string& name,
+    const std::string& portName,
+    uint16_t sPort,
+    uint16_t dPort,
+    folly::IPAddress dstAddress,
+    folly::Optional<folly::IPAddress> srcAddress,
+    uint8_t dscp = cfg::switch_config_constants::DEFAULT_MIRROR_DSCP_,
+    bool truncate = false
+
+);
+
+} // namespace utility
+} // namespace fboss
+} // namespace facebook
