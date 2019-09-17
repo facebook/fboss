e00b4ec041bbfcae226bd5176cdf400bd726cb49
Jasmeet Bagga <jasmeetbagga@fb.com>
2019-09-17 11:39:21 -0700

    Add HwLinkStateDependentTest on the lines of analogous class in Bcm layer
    Summary:
    Add HwLinkStateDependentTest on the lines of analogous class in Bcm layer.
    Tests which depend on link states - ecmp shrink, all dataplane tests,
    which need to send packets over UP links to derive from such a class.
    
    Initially I was thinking of merging this functionality into HwTest
    itself. I didn't go down that route since
    - LinkStateDependent tests impose interface requirements - in particular
    a starting config must be provided.
    - LinkStateDependent tests starting state is application of aforementioned
    config and getting all ports up (to synchronize all port events that may
    occur as result of initing the chip). This may not be desirable for all
    tests. For e.g. we have tests that assert the chip default state post
    a cold boot. One of the assertions there is that all ports are disabled
    on a cold boot.
    
    Reviewed By: sas
    
    Differential Revision: D17331719
    
    fbshipit-source-id: ae3795d3396bd4c01c37e8958c001f9d030429fd

---
+++ fboss/agent/hw/test/HwLinkStateDependentTest.cpp
@@ -0,0 +1,42 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+#include "fboss/agent/hw/test/HwLinkStateDependentTest.h"
+
+#include "fboss/agent/hw/test/HwLinkStateToggler.h"
+#include "fboss/agent/hw/test/HwSwitchEnsemble.h"
+#include "fboss/agent/types.h"
+
+namespace facebook {
+namespace fboss {
+
+void HwLinkStateDependentTest::SetUp() {
+  HwTest::SetUp();
+  if (getHwSwitch()->getBootType() != BootType::WARM_BOOT) {
+    // No setup beyond init required for warm boot. We should
+    // recover back to the state the switch went down with prior
+    // to warm boot
+    getHwSwitchEnsemble()->applyInitialConfigAndBringUpPorts(initialConfig());
+  }
+}
+
+HwLinkStateToggler* HwLinkStateDependentTest::getLinkToggler() {
+  return getHwSwitchEnsemble()->getLinkToggler();
+}
+
+void HwLinkStateDependentTest::bringUpPorts(const std::vector<PortID>& ports) {
+  getLinkToggler()->bringUpPorts(getProgrammedState(), ports);
+}
+
+void HwLinkStateDependentTest::bringDownPorts(
+    const std::vector<PortID>& ports) {
+  getLinkToggler()->bringDownPorts(getProgrammedState(), ports);
+}
+} // namespace fboss
+} // namespace facebook
---
+++ fboss/agent/hw/test/HwLinkStateDependentTest.h
@@ -0,0 +1,52 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#pragma once
+
+#include "fboss/agent/hw/test/HwTest.h"
+
+#include "fboss/agent/gen-cpp2/switch_config_types.h"
+#include "fboss/agent/hw/test/HwLinkStateToggler.h"
+
+namespace facebook {
+namespace fboss {
+
+class HwLinkStateDependentTest : public HwTest {
+ protected:
+  void SetUp() override;
+
+  void applyInitialConfig();
+  void bringUpPort(PortID port) {
+    bringUpPorts({port});
+  }
+  void bringDownPort(PortID port) {
+    bringDownPorts({port});
+  }
+  void bringUpPorts(const std::vector<PortID>& ports);
+  void bringDownPorts(const std::vector<PortID>& ports);
+
+ private:
+  /*
+   * All link state dependent tests must provide a starting
+   * config. This is used in setup to get switch into a state
+   * where the initial config is applied and all ports are
+   * brought up. This gives us a means to synchronize all
+   * port state events which may occurs as part of bringing
+   * up the chip. Subsequent ports state changes must be
+   * through the bringPort(s){Up,Down} APIs below
+   */
+  virtual cfg::SwitchConfig initialConfig() const = 0;
+  HwLinkStateToggler* getLinkToggler();
+  uint32_t featuresDesired() const override {
+    return (HwSwitch::LINKSCAN_DESIRED & ~HwSwitch::PACKET_RX_DESIRED);
+  }
+};
+} // namespace fboss
+} // namespace facebook
