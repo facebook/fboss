f7331f5e05ac4e5bc78f072ee829bb5db470bef0
Parvez Shaikh <pshaikh@fb.com>
2019-09-18 13:47:24 -0700

    hack to strip vlan tag while sending out of port
    Summary: strip vlan tag if packet is being sent with pipeline bypass, this is required as vlans are not stripped for such packets
    
    Differential Revision: D17429445
    
    fbshipit-source-id: 7884c06d02a59e72009857645b367bacde0bf09b

--- fboss/agent/hw/sai/switch/SaiSwitch.cpp
+++ fboss/agent/hw/sai/switch/SaiSwitch.cpp
@@ -380,6 +380,39 @@ bool SaiSwitch::sendPacketOutOfPortSyncLocked(
   if (!portHandle) {
     throw FbossError("Failed to send packet on invalid port: ", portID);
   }
+  /* TODO: this hack is required, sending packet out of port with with pipeline
+  bypass, doesn't cause vlan tag stripping. fix this once a pipeline bypass with
+  vlan stripping is available. */
+
+  folly::io::Cursor cursor(pkt->buf());
+  EthHdr ethHdr{cursor};
+  if (!ethHdr.getVlanTags().empty()) {
+    CHECK_EQ(ethHdr.getVlanTags().size(), 1)
+        << "found more than one vlan tags while sending packet";
+    /* hack to strip vlans as pipeline bypass doesn't handle this */
+    cursor.reset(pkt->buf());
+    XLOG(DBG5) << "strip vlan for packet";
+    XLOG(DBG5) << PktUtil::hexDump(cursor);
+
+    constexpr auto kVlanTagSize = 4;
+    auto ethPayload = pkt->buf()->clone();
+    // trim DA(6), SA(6) & vlan (4)
+    ethPayload->trimStart(
+        folly::MacAddress::SIZE + folly::MacAddress::SIZE + kVlanTagSize);
+
+    // trim rest of packet except DA(6), SA(6)
+    auto totalLength = pkt->buf()->length();
+    pkt->buf()->trimEnd(
+        totalLength - folly::MacAddress::SIZE - folly::MacAddress::SIZE);
+
+    // append to trimmed ethernet header remaining payload
+    pkt->buf()->appendChain(std::move(ethPayload));
+    pkt->buf()->coalesce();
+    cursor.reset(pkt->buf());
+    XLOG(DBG5) << "stripped vlan, new packet";
+    XLOG(DBG5) << PktUtil::hexDump(cursor);
+  }
+
   SaiHostifApiPacket txPacket{
       reinterpret_cast<void*>(pkt->buf()->writableData()),
       pkt->buf()->length()};
