a0c1006828a09114c002442fc7e04fa49388a135
Adam Simpkins <simpkins@fb.com>
2019-09-20 14:12:29 -0700

    update getdeps.sh to use the local sources when building from github
    Summary:
    Update `getdeps.sh` to pass in a `--src-dir` argument so that it will build
    from the local checkout rather than re-cloning a new fboss checkout in the
    getdeps scratch directory.
    
    Differential Revision: D17005419
    
    fbshipit-source-id: 4b0e9960de4aae3240775578b574d32745a0ce4d

--- getdeps.sh
+++ getdeps.sh
@@ -2,10 +2,21 @@
 
 # FBOSS is now built using fbcode_builder. Thus, just invoke fbcode_builder
 # getdeps.py to build fboss here.
-for getdeps in build/fbcode_builder/getdeps.py opensource/fbcode_builder/getdeps.py ; do
-    if [ -f $getdeps ] ; then
-        exec env PATH=/opt/rh/devtoolset-8/root/usr/bin/:$PATH $getdeps build fboss
+
+TOOLCHAIN_DIR=/opt/rh/devtoolset-8/root/usr/bin
+if [[ -d "$TOOLCHAIN_DIR" ]]; then
+    PATH="$TOOLCHAIN_DIR:$PATH"
+fi
+
+FBOSS_DIR=$(dirname "$0")
+GETDEPS_PATHS=(
+    "$FBOSS_DIR/build/fbcode_builder/getdeps.py"
+    "$FBOSS_DIR/../../opensource/fbcode_builder/getdeps.py"
+)
+for getdeps in "${GETDEPS_PATHS[@]}"; do
+    if [[ -x "$getdeps" ]]; then
+        exec "$getdeps" build fboss --current-project fboss "$@"
     fi
 done
-echo "Could not find getdeps.py!?"
+echo "Could not find getdeps.py!?" >&2
 exit 1
