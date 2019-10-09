618b7f8fefd5024243c9e1c4de2ad8d71cd94b46
Chenfei Lin <clin82@fb.com>
2019-10-09 16:50:25 -0700

    Enable FPGA mem space access through binary
    Summary: The FPGA on Wedge400DQ does not enable memory space access by default. So here we do it within the binary.
    
    Reviewed By: xzhu70
    
    Differential Revision: D17806592
    
    fbshipit-source-id: 4ecb1bbaee5be00598e3a743806d323294192ca4

---
+++ fboss/lib/PciAccess.cpp
@@ -0,0 +1,38 @@
+/*
+ *  Copyright (c) 2018-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+#include "fboss/lib/PciAccess.h"
+
+#include <fstream>
+#include <string>
+
+namespace {
+constexpr uint8_t kMemSpaceAccess = 0x42;
+}
+
+namespace facebook {
+namespace fboss {
+
+PciAccess::PciAccess(std::string devicePath) : devicePath_(devicePath) {}
+
+PciAccess::~PciAccess() {}
+
+void PciAccess::enableMemSpaceAccess() {
+  std::string configPath(devicePath_);
+  configPath.append("/config");
+  std::fstream confFile(
+      configPath.c_str(), std::fstream::binary | std::fstream::out);
+  confFile.seekp(4, confFile.beg);
+  confFile.put((char)kMemSpaceAccess);
+  confFile.flush();
+  confFile.close();
+}
+
+} // namespace fboss
+} // namespace facebook
---
+++ fboss/lib/PciAccess.h
@@ -0,0 +1,32 @@
+/*
+ *  Copyright (c) 2018-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+#pragma once
+
+#include <string>
+
+namespace facebook {
+namespace fboss {
+
+/**
+ * Class for managing pci device access.
+ */
+class PciAccess {
+ public:
+  explicit PciAccess(std::string devicePath);
+  ~PciAccess();
+
+  void enableMemSpaceAccess();
+
+ private:
+  std::string devicePath_;
+};
+
+} // namespace fboss
+} // namespace facebook
