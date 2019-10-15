02bea2bb9e520bea163cdb5ac0e5e9966511b0b3
Jasmeet Bagga <jasmeetbagga@fb.com>
2019-10-14 17:15:13 -0700

    Add a Bcm Tx slow path benchmark
    Summary:
    This is the flip side of rx slow path benchmark
    added in D17785294. Methodology here is
    - Program a route
    - Start a thread and have it pump traffic till its stopped
    - Get port out bytes, pkts.
    - Burn 5 secs
    - Get port out bytes, pkts.
    - Calculate rate.
    
    Interestingly the rate on TX is only 15K pps compared to 100K+
    on the RX side. This is what routing folks were seeing when they
    measured the slow path performance and got a cap at ~11K.
    
    Reviewed By: saifhhasan
    
    Differential Revision: D17839621
    
    fbshipit-source-id: bc2e86301f1f445c0aa81bb866f410b49fa10ba1

---
+++ fboss/agent/hw/bcm/benchmarks/BcmTxSlowPathBenchmark.cpp
@@ -0,0 +1,112 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#include "fboss/agent/hw/bcm/tests/BcmSwitchEnsemble.h"
+#include "fboss/agent/hw/test/ConfigFactory.h"
+#include "fboss/agent/hw/test/HwTestPacketUtils.h"
+#include "fboss/agent/test/EcmpSetupHelper.h"
+
+#include "fboss/agent/hw/bcm/BcmControlPlane.h"
+#include "fboss/agent/hw/bcm/BcmControlPlaneQueueManager.h"
+
+#include <folly/IPAddressV6.h>
+#include <folly/dynamic.h>
+#include <folly/init/Init.h>
+#include <folly/json.h>
+#include "common/time/Time.h"
+#include "fboss/agent/hw/bcm/BcmError.h"
+
+#include <chrono>
+#include <iostream>
+#include <thread>
+
+DEFINE_bool(json, true, "Output in json form");
+
+namespace facebook::fboss {
+
+std::pair<uint64_t, uint64_t> getOutPktsAndBytes(
+    BcmSwitchEnsemble& ensemble,
+    PortID port) {
+  auto stats = ensemble.getLatestPortStats({port})[port];
+  return {stats.outUnicastPkts_, stats.outBytes_};
+}
+
+void runTxSlowPathBenchmark() {
+  constexpr int kEcmpWidth = 1;
+  BcmSwitchEnsemble ensemble;
+  auto bcmSwitch = ensemble.getHwSwitch();
+  auto portUsed = ensemble.getPlatform()->masterLogicalPortIds()[0];
+  auto config = utility::oneL3IntfConfig(bcmSwitch, portUsed);
+  ensemble.applyInitialConfigAndBringUpPorts(config);
+  auto ecmpHelper = utility::EcmpSetupAnyNPorts6(ensemble.getProgrammedState());
+  auto ecmpRouteState = ecmpHelper.setupECMPForwarding(
+      ecmpHelper.resolveNextHops(ensemble.getProgrammedState(), kEcmpWidth),
+      kEcmpWidth);
+  ensemble.applyNewState(ecmpRouteState);
+
+  std::atomic<bool> packetTxDone{false};
+  std::thread t([bcmSwitch, &config, &packetTxDone]() {
+    auto cpuMac = bcmSwitch->getPlatform()->getLocalMac();
+    const auto kSrcIp = folly::IPAddressV6("2620:0:1cfe:face:b00c::3");
+    const auto kDstIp = folly::IPAddressV6("2620:0:1cfe:face:b00c::4");
+    while (!packetTxDone) {
+      for (auto i = 0; i < 1'000; ++i) {
+        // Send packet
+        auto txPacket = utility::makeUDPTxPacket(
+            bcmSwitch,
+            VlanID(config.vlanPorts[0].vlanID),
+            cpuMac,
+            cpuMac,
+            kSrcIp,
+            kDstIp,
+            8000,
+            8001);
+        bcmSwitch->sendPacketSwitchedAsync(std::move(txPacket));
+      }
+    }
+  });
+
+  auto [pktsBefore, bytesBefore] =
+      getOutPktsAndBytes(ensemble, PortID(portUsed));
+  auto timeBefore = std::chrono::steady_clock::now();
+  constexpr auto kBurnIntevalMs = 5000;
+  // Let the packet flood warm up
+  WallClockMs::Burn(kBurnIntevalMs);
+  auto [pktsAfter, bytesAfter] = getOutPktsAndBytes(ensemble, PortID(portUsed));
+  auto timeAfter = std::chrono::steady_clock::now();
+  packetTxDone = true;
+  t.join();
+  std::chrono::duration<double, std::milli> durationMillseconds =
+      timeAfter - timeBefore;
+  uint32_t pps = (static_cast<double>(pktsAfter - pktsBefore) /
+                  durationMillseconds.count()) *
+      1000;
+  uint32_t bytesPerSec = (static_cast<double>(bytesAfter - bytesBefore) /
+                          durationMillseconds.count()) *
+      1000;
+
+  if (FLAGS_json) {
+    folly::dynamic cpuTxRateJson = folly::dynamic::object;
+    cpuTxRateJson["cpu_tx_pps"] = pps;
+    cpuTxRateJson["cpu_tx_bytes_per_sec"] = bytesPerSec;
+    std::cout << toPrettyJson(cpuTxRateJson) << std::endl;
+  } else {
+    XLOG(INFO) << " Pkts before: " << pktsBefore << " Pkts after: " << pktsAfter
+               << " interval ms: " << durationMillseconds.count()
+               << " pps: " << pps << " bytes per sec: " << bytesPerSec;
+  }
+}
+} // namespace facebook::fboss
+
+int main(int argc, char* argv[]) {
+  folly::init(&argc, &argv, true);
+  facebook::fboss::runTxSlowPathBenchmark();
+  return 0;
+}
