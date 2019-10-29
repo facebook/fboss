3aa159f709155adb187f430cc4fa61026d6cb0f1
Jingyi Yang <girasoley@fb.com>
2019-10-29 00:04:13 -0700

    interface drain
    Summary:
    add interface drain for SPR
    
    Alex Jose Needs help on how to run a test drain here.
    
    I tried `drainer --addr localhost:9090 drain  fsw001.p007.f01.frc3:eth5/7/1 --task T56264994`
    
    https://our.intern.facebook.com/intern/network/drainer/job/587527?tier=girasoley.drainer
    
    But looking at the log this still does not run through my changes.
    
    Reviewed By: saifhhasan
    
    Differential Revision: D18125687
    
    fbshipit-source-id: 5d727b7a2caf9f231b7412b334f7a273cdb7cdac

--- fboss/py/fboss/cli/commands/interface.py
+++ fboss/py/fboss/cli/commands/interface.py
@@ -68,16 +68,66 @@ class InterfaceShowCmd(cmds.FbossCmd):
                                    addr.prefixLength))
 
 
+def convert_address(addr: bytes) -> str:
+    ''' convert binary address to human readable format '''
+    if len(addr) == 4:
+        return socket.inet_ntop(socket.AF_INET, addr)
+    elif len(addr) == 16:
+        return socket.inet_ntop(socket.AF_INET6, addr)
+    raise ValueError("bad binary address {0}".gdlluivnhbufformat(repr(addr)))
+
+
+def sort_key(port: str) -> Any:
+    ''' function to return X+Y when given ethX/Y '''
+    port_re = re.compile(r'eth(\d+)/(\d+)')
+    match = port_re.match(port)
+    if match:
+        return int(match.group(1) + match.group(2))
+    return port
+
+
+def get_interface_summary(agent_client, qsfp_client) -> List[Interface]:
+    # Getting the port/agg to VLAN map in order to display them
+    vlan_port_map = utils.get_vlan_port_map(agent_client,
+            qsfp_client=qsfp_client)
+    vlan_aggregate_port_map = utils.get_vlan_aggregate_port_map(agent_client)
+
+    interface_summary: List[Interface] = []
+    for interface in agent_client.getAllInterfaces().values():
+        # build the addresses variable for this interface
+        addresses = '\n'.join([
+            convert_address(address.ip.addr)
+            + '/' + str(address.prefixLength)
+            for address in interface.address
+        ])
+        # build the ports variable for this interface
+        ports: str = ''
+        if interface.vlanId in vlan_port_map.keys():
+            for root_port in sorted(
+                vlan_port_map[interface.vlanId].keys(), key=sort_key,
+            ):
+                port_list = vlan_port_map[interface.vlanId][root_port]
+                ports += ' '.join(port_list) + '\n'
+        vlan = interface.vlanId
+        interface_name = interface.interfaceName
+        mtu = interface.mtu
+        # check if aggregate
+        if vlan in vlan_aggregate_port_map.keys():
+            interface_name = (
+                f'{interface.interfaceName}\n({vlan_aggregate_port_map[vlan]})'
+            )
+        interface_summary.append(Interface(
+            vlan=vlan,
+            name=interface_name,
+            mtu=mtu,
+            addresses=addresses,
+            ports=ports,
+        ))
+    return interface_summary
+
+
 class InterfaceSummaryCmd(cmds.FbossCmd):
     ''' Show interface summary '''
-
-    def convert_address(self, addr: bytes) -> str:
-        ''' convert binary address to human readable format '''
-        if len(addr) == 4:
-            return socket.inet_ntop(socket.AF_INET, addr)
-        elif len(addr) == 16:
-            return socket.inet_ntop(socket.AF_INET6, addr)
-        raise ValueError("bad binary address {0}".format(repr(addr)))
 
     def print_table(self, interface_summary: List[Interface]) -> None:
         ''' build and output a table with interface summary data '''
@@ -95,53 +145,8 @@ class InterfaceSummaryCmd(cmds.FbossCmd):
             ])
         print(table)
 
-    def sort_key(self, port: str) -> Any:
-        ''' function to return X+Y when given ethX/Y '''
-        port_re = re.compile(r'eth(\d+)/(\d+)')
-        match = port_re.match(port)
-        if match:
-            return int(match.group(1) + match.group(2))
-        return port
-
     def run(self) -> None:
         ''' fetch data and populate interface summary list '''
         with self._create_agent_client() as agent_client, \
                 self._create_qsfp_client() as qsfp_client:
-
-            # Getting the port/agg to VLAN map in order to display them
-            vlan_port_map = utils.get_vlan_port_map(agent_client,
-                    qsfp_client=qsfp_client)
-            vlan_aggregate_port_map = utils.get_vlan_aggregate_port_map(agent_client)
-
-            interface_summary: List[Interface] = []
-            for interface in agent_client.getAllInterfaces().values():
-                # build the addresses variable for this interface
-                addresses = '\n'.join([
-                    self.convert_address(address.ip.addr)
-                    + '/' + str(address.prefixLength)
-                    for address in interface.address
-                ])
-                # build the ports variable for this interface
-                ports: str = ''
-                if interface.vlanId in vlan_port_map.keys():
-                    for root_port in sorted(
-                        vlan_port_map[interface.vlanId].keys(), key=self.sort_key,
-                    ):
-                        port_list = vlan_port_map[interface.vlanId][root_port]
-                        ports += ' '.join(port_list) + '\n'
-                vlan = interface.vlanId
-                interface_name = interface.interfaceName
-                mtu = interface.mtu
-                # check if aggregate
-                if vlan in vlan_aggregate_port_map.keys():
-                    interface_name = (
-                        f'{interface.interfaceName}\n({vlan_aggregate_port_map[vlan]})'
-                    )
-                interface_summary.append(Interface(
-                    vlan=vlan,
-                    name=interface_name,
-                    mtu=mtu,
-                    addresses=addresses,
-                    ports=ports,
-                ))
-            self.print_table(interface_summary)
+            self.print_table(get_interface_summary(agent_client, qsfp_client))
