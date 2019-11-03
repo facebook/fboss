40d0ba0927e45c0ebac14d7cf07c640c56e3a092
Jasmeet Bagga <jasmeetbagga@fb.com>
2019-11-03 15:07:54 -0800

    Add route overflow tests
    Summary:
    Add tests to overflow route tables on h/w and test
    that we are able to handle such a state update w/o crashing.
    
    TODO: Pull these up to hw/test layer
    
    Reviewed By: phshaikh
    
    Differential Revision: D18219842
    
    fbshipit-source-id: 6e48ffca9533b1933f8f6ccf26c9e994835771bf

---
+++ fboss/agent/hw/bcm/tests/BcmRouteOverflowTest.cpp
@@ -0,0 +1,70 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#include "fboss/agent/HwSwitch.h"
+#include "fboss/agent/hw/bcm/tests/BcmTest.h"
+#include "fboss/agent/hw/test/ConfigFactory.h"
+#include "fboss/agent/test/RouteScaleGenerators.h"
+
+namespace facebook {
+namespace fboss {
+
+TEST_F(BcmTest, overflowRoutes) {
+  applyNewConfig(
+      utility::onePortPerVlanConfig(getHwSwitch(), masterLogicalPortIds()));
+  std::shared_ptr<SwitchState> desiredState;
+  switch (getPlatform()->getMode()) {
+    case PlatformMode::WEDGE:
+      desiredState = utility::FSWRouteScaleGenerator(getProgrammedState())
+                         .getSwitchStates()
+                         .back();
+      break;
+
+    case PlatformMode::GALAXY_FC:
+    case PlatformMode::GALAXY_LC:
+    case PlatformMode::WEDGE100:
+      desiredState = utility::HgridUuRouteScaleGenerator(getProgrammedState())
+                         .getSwitchStates()
+                         .back();
+      break;
+    case PlatformMode::WEDGE400:
+    case PlatformMode::MINIPACK:
+    case PlatformMode::YAMP:
+      /*
+       * A route distribution 200,000 /128 does overflow the ASIC tables
+       * but it takes 15min to generate, program and clean up such a
+       * distribution. Since the code paths for overflow in our implementation
+       * are platform independent, skip overflow on TH3 in the interest
+       * of time.
+       * Caveat: if for whatever reason, SDK stopped returning E_FULL on
+       * table full on TH3, skipping overflow test here would hide such
+       * a bug.
+       */
+      break;
+
+    case PlatformMode::FAKE_WEDGE:
+    case PlatformMode::FAKE_WEDGE40:
+      // No limits to overflow for fakes
+      break;
+    case PlatformMode::WEDGE400C:
+    case PlatformMode::WEDGE400C_SIM:
+      CHECK(0) << " TBD";
+
+      break;
+  }
+  if (!desiredState) {
+    return;
+  }
+  getHwSwitchEnsemble()->setAllowPartialStateApplication(true);
+  applyNewState(desiredState);
+  EXPECT_NE(getProgrammedState(), desiredState);
+}
+} // namespace fboss
+} // namespace facebook
