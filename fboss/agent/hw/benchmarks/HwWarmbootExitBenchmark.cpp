b243b39c423d9872cd034ceef2774a737b8434e3
Jasmeet Bagga <jasmeetbagga@fb.com>
2019-11-04 15:41:05 -0800

    Hw warm boot exit time benchmark
    Summary:
    Start benchmarking the time we take in HwSwitch
    during warm boot exit. While this is not a perfect match for
    how long it takes its close. It includes
    - Time it take to dump switch state, sdk state
    - Time it takes to stop hw switch threads, handlers, get ASIC ready
    for shutdown
    It does not include
    - SwSwitch thread stop time
    - Any additional SwSwitch cleanup
    
    We should attack the missing pieces but the current diff gets us quite
    a ways there in benchmarking our warm boot exit path.
    
    Methodology
    - For all but wedge40 platforms, program the worst case FSW route scale
    distribution and initiate warm boot exit.
    - For wedge40, program distribution taken from a random wedge40 in fleet
    and initiate warm boot exit.
    
    Reviewed By: boryas
    
    Differential Revision: D18231531
    
    fbshipit-source-id: 4d3d5fc43a58cf6e76c13bbd3a0f905ba742a43c

---
+++ fboss/agent/hw/benchmarks/HwWarmbootExitBenchmark.cpp
@@ -0,0 +1,114 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#include "fboss/agent/Platform.h"
+#include "fboss/agent/hw/test/ConfigFactory.h"
+#include "fboss/agent/hw/test/HwSwitchEnsemble.h"
+#include "fboss/agent/hw/test/HwSwitchEnsembleFactory.h"
+#include "fboss/agent/hw/test/HwTestPacketUtils.h"
+#include "fboss/agent/platforms/common/PlatformProductInfo.h"
+#include "fboss/agent/test/EcmpSetupHelper.h"
+#include "fboss/agent/test/RouteScaleGenerators.h"
+
+#include <folly/IPAddressV6.h>
+#include <folly/dynamic.h>
+#include <folly/init/Init.h>
+#include <folly/json.h>
+
+#include <chrono>
+#include <iostream>
+
+DEFINE_bool(json, true, "Output in json form");
+
+namespace {
+class StopWatch {
+ public:
+  StopWatch() : startTime_(std::chrono::steady_clock::now()) {}
+  ~StopWatch() {
+    std::chrono::duration<double, std::milli> durationMillseconds =
+        std::chrono::steady_clock::now() - startTime_;
+    if (FLAGS_json) {
+      folly::dynamic warmBootTime = folly::dynamic::object;
+      warmBootTime["warm_boot_msecs"] = durationMillseconds.count();
+      std::cout << warmBootTime << std::endl;
+    } else {
+      XLOG(INFO) << " warm boot msecs: " << durationMillseconds.count();
+    }
+  }
+
+ private:
+  std::chrono::time_point<std::chrono::steady_clock> startTime_;
+};
+} // namespace
+namespace facebook::fboss {
+
+void runBenchmark() {
+  auto ensemble = createHwEnsemble(
+      HwSwitch::FeaturesDesired::LINKSCAN_DESIRED |
+      HwSwitch::FeaturesDesired::PACKET_RX_DESIRED);
+  auto hwSwitch = ensemble->getHwSwitch();
+  auto config =
+      utility::onePortPerVlanConfig(hwSwitch, ensemble->masterLogicalPortIds());
+  ensemble->applyInitialConfigAndBringUpPorts(config);
+
+  std::shared_ptr<SwitchState> toApply;
+  if (ensemble->getPlatform()->getMode() == PlatformMode::WEDGE) {
+    toApply = utility::RouteDistributionGenerator(
+                  ensemble->getProgrammedState(),
+                  // Distribution taken from a random wedge in fleet
+                  {
+                      // v6 distribution - mask to num unique prefixes
+                      {37, 7},
+                      {44, 11},
+                      {47, 6},
+                      {48, 3},
+                      {52, 61},
+                      {54, 340},
+                      {64, 1542},
+                      {80, 1},
+                      {127, 4},
+                  },
+                  {// v4 distribution - mask to num unique prefixes
+                   {15, 4},
+                   {16, 1},
+                   {17, 1},
+                   {21, 10},
+                   {24, 31},
+                   {26, 6},
+                   {27, 26},
+                   {31, 40}},
+                  4000,
+                  4)
+                  .getSwitchStates()
+                  .back();
+  } else {
+    toApply = utility::FSWRouteScaleGenerator(ensemble->getProgrammedState())
+                  .getSwitchStates()
+                  .back();
+  }
+  ensemble->applyNewState(toApply);
+  // Static such that the object destructor runs as late as possible. In
+  // Static such that the object destructor runs as late as possible. In
+  // particular in this case, destructor (and thus the duration calculation)
+  // will run at the time of program exit when static variable destructors run
+  static StopWatch timer;
+  ensemble->gracefulExit();
+  // Leak HwSwitchEnsemble for warmboot, so that
+  // we don't run destructors and unprogram h/w. We are
+  // going to exit the process anyways.
+  __attribute__((unused)) auto leakedHwEnsemble = ensemble.release();
+}
+} // namespace facebook::fboss
+
+int main(int argc, char* argv[]) {
+  folly::init(&argc, &argv, true);
+  facebook::fboss::runBenchmark();
+  return 0;
+}
