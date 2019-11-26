ee66278eb8ecce6a4557e021cad52ebc6a6d3fc5
Boris Burkov <borisb@fb.com>
2019-11-26 08:28:25 -0800

    library to run a python interpreter
    Summary:
    This library will be wrapped by a thrift diag shell higher in the
    stack, so it is primarily concerned with only the "run a python repl"
    aspect of things. Since we will end up with different REPLs, this also
    represents a proto-interface. I suspect the exact standard stream redirections
    will need to be configurable so I went ahead and made that an interface of
    the repl.
    
    A few important notes, mostly to do with deficiencies in python:
    
    AFAIK, there is no way to exit PyMain() without crashing the parent program,
    so we just keep it running forever. On destruction, leak the thread, assuming
    that there is no long-running use case for destroying and re-creating the
    thrift handler (it's not so bad to leak one or two threads during a short
    lived test, for example)
    
    The alternative, PyRun_InteractiveLoop, seems to be completely busted, with
    regard to redirection: https://bugs.python.org/issue14916 (unfixed bug with
    patch from community member provided in 2013, last pinged in 2015)
    
    If we want to make this perfect (rather than "working" which is the state in
    this diff), we can:
    * use lower level PyRun_X primitives to implement the loop
    * get the patch prescribed in that bug patched in
    * figure out how to exit from PyMain safely (lol)
    
    Differential Revision: D18314226
    
    fbshipit-source-id: 655268b351abc5934e89243f4e22ff10c221e032

---
+++ fboss/agent/hw/sai/diag/PythonRepl.cpp
@@ -0,0 +1,76 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#include "fboss/agent/hw/sai/diag/PythonRepl.h"
+
+namespace facebook::fboss {
+
+PythonRepl::PythonRepl(int fd) : fd_(fd) {}
+
+PythonRepl::~PythonRepl() noexcept {
+  /*
+   * Due to a deficiency in Py_Main (and its alternative, PyRun_InteractiveLoop)
+   * we cannot cleanly terminate the infinite loops in the pyThread nor the
+   * producerThread. For that reason, DiagRepl has the same lifetime as
+   * SaiHandler, so it is only destroyed when FBOSS is exiting and it is safe
+   * to detach the threads instead of joining them.
+   */
+  try {
+    pyThread_->detach();
+  } catch (...) {
+    XLOG(WARNING) << "Failed to detach python thread during repl tear down";
+  }
+}
+
+void PythonRepl::run() {
+  pyThread_ = std::make_unique<std::thread>(
+      [this]() mutable { runPythonInterpreter(); });
+  running_ = true;
+}
+
+bool PythonRepl::running() const {
+  return running_;
+}
+
+void PythonRepl::runPythonInterpreter() {
+  // TODO: what to put in argc/argv?!?
+  int argc = 1;
+  std::wstring arg = L"SAI-Diag-Repl";
+  wchar_t* argp = arg.data();
+  wchar_t** argv = &argp;
+  XLOG(INFO) << "Starting Python interpreter";
+  Py_Initialize();
+  /* hack: point sys.stderr at the pty slave fd so that it is sent to the user
+   *       but without spoiling stderr for logging. For some reason, the same
+   *       trick does not work with stdin/stdout...
+   */
+  PyRun_SimpleString("import os");
+  PyRun_SimpleString("import sys");
+  auto fdopenCmd = folly::sformat("f = os.fdopen({}, 'w')", fd_);
+  PyRun_SimpleString(fdopenCmd.c_str());
+  PyRun_SimpleString("sys.stderr = f");
+  XLOG(INFO) << "Starting interactive Python session";
+  Py_Main(argc, argv);
+  Py_Finalize();
+}
+
+std::string PythonRepl::getPrompt() const {
+  return ">>> ";
+}
+
+std::vector<folly::File> PythonRepl::getStreams() const {
+  std::vector<folly::File> ret;
+  ret.reserve(2);
+  ret.emplace_back(folly::File(STDIN_FILENO));
+  ret.emplace_back(folly::File(STDOUT_FILENO));
+  return ret;
+}
+
+} // namespace facebook::fboss
---
+++ fboss/agent/hw/sai/diag/PythonRepl.h
@@ -0,0 +1,41 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+#pragma once
+
+#include <Python.h>
+
+#include <folly/File.h>
+#include <folly/Format.h>
+#include <folly/logging/xlog.h>
+
+#include <thread>
+#include <vector>
+
+namespace facebook::fboss {
+
+class PythonRepl {
+ public:
+  explicit PythonRepl(int fd);
+  ~PythonRepl() noexcept;
+  void run();
+  bool running() const;
+  std::string getPrompt() const;
+  // Get the list of streams the repl expects the shell to dup2 with
+  // the pty slave fd passed into the repl constructor
+  std::vector<folly::File> getStreams() const;
+
+ private:
+  void runPythonInterpreter();
+  std::unique_ptr<std::thread> pyThread_;
+  int fd_;
+  bool running_{false};
+};
+
+} // namespace facebook::fboss
