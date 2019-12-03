f6a6a74d900cb5b2b63fc92c19b37b078b7123f1
Srikrishna Gopu <srikrishnagopu@fb.com>
2019-12-03 14:11:14 -0800

    get cpu queue out packets
    Summary: Updating Copp Utils getCpuQueueOutPackets to fetch the cpu queue counters
    
    Differential Revision: D18583409
    
    fbshipit-source-id: 44e16080a7b84f7495093f5aa4861da5a2377c4f

--- fboss/agent/hw/sai/hw_test/HwTestCoppUtils.cpp
+++ fboss/agent/hw/sai/hw_test/HwTestCoppUtils.cpp
@@ -9,13 +9,25 @@
  */
 #include "fboss/agent/hw/test/HwTestCoppUtils.h"
 
+#include "fboss/agent/hw/sai/switch/SaiHostifManager.h"
+#include "fboss/agent/hw/sai/switch/SaiManagerTable.h"
+#include "fboss/agent/hw/sai/switch/SaiSwitch.h"
+
+#include "fboss/agent/SwitchStats.h"
+
 namespace facebook {
 namespace fboss {
 namespace utility {
 
-uint64_t getCpuQueueOutPackets(HwSwitch* /*hwSwitch*/, int /*queueId*/) {
-  // TODO
-  return 0;
+uint64_t getCpuQueueOutPackets(HwSwitch* hwSwitch, int queueId) {
+  auto saiSwitch = static_cast<SaiSwitch*>(hwSwitch);
+  SwitchStats dummy;
+  saiSwitch->updateStats(&dummy);
+  auto hwPortStats =
+      saiSwitch->managerTable()->hostifManager().getCpuPortStats();
+  auto queueIter = hwPortStats.queueOutPackets_.find(queueId);
+  return (queueIter != hwPortStats.queueOutPackets_.end()) ? queueIter->second
+                                                           : 0;
 }
 } // namespace utility
 } // namespace fboss
