892f016059517c8d5d568973f0b00401b6a66890
Shrikrishna Khare <skhare@fb.com>
2019-12-11 13:05:41 -0800

    Unit tests for MacTableUtils
    Summary: As titled. Unit tests for CLASSID based APIs would soon follow.
    
    Differential Revision: D18926441
    
    fbshipit-source-id: 8ec35a44f701da1dbfb221275c714898f6e42708

---
+++ fboss/agent/test/MacTableUtilsTests.cpp
@@ -0,0 +1,104 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#include <gtest/gtest.h>
+
+#include "fboss/agent/L2Entry.h"
+#include "fboss/agent/MacTableUtils.h"
+#include "fboss/agent/state/Port.h"
+#include "fboss/agent/state/SwitchState.h"
+#include "fboss/agent/state/Vlan.h"
+#include "fboss/agent/test/TestUtils.h"
+
+#include <folly/MacAddress.h>
+
+using folly::MacAddress;
+
+namespace facebook {
+namespace fboss {
+
+class MacTableUtilsTest : public ::testing::Test {
+ public:
+  VlanID kVlan() const {
+    return VlanID(1);
+  }
+
+  PortID kPortID() const {
+    return PortID(1);
+  }
+
+  folly::MacAddress kMacAddress() const {
+    return MacAddress("01:02:03:04:05:06");
+  }
+
+  L2Entry kL2Entry() {
+    return L2Entry(
+        kMacAddress(),
+        kVlan(),
+        PortDescriptor(kPortID()),
+        L2Entry::L2EntryType::L2_ENTRY_TYPE_VALIDATED);
+  }
+
+  std::shared_ptr<SwitchState> verifyAddMac(
+      const std::shared_ptr<SwitchState>& state) {
+    return verifyAddAndDeleteMacHelper(
+        state, L2EntryUpdateType::L2_ENTRY_UPDATE_TYPE_ADD);
+  }
+
+  std::shared_ptr<SwitchState> verifyDeleteMac(
+      const std::shared_ptr<SwitchState>& state) {
+    return verifyAddAndDeleteMacHelper(
+        state, L2EntryUpdateType::L2_ENTRY_UPDATE_TYPE_DELETE);
+  }
+
+ private:
+  std::shared_ptr<SwitchState> verifyAddAndDeleteMacHelper(
+      const std::shared_ptr<SwitchState>& state,
+      L2EntryUpdateType l2EntryUpdateType) {
+    auto newState =
+        MacTableUtils::updateMacTable(state, kL2Entry(), l2EntryUpdateType);
+
+    auto vlan = newState->getVlans()->getVlanIf(kVlan()).get();
+    auto* macTable = vlan->getMacTable().get();
+    auto node = macTable->getNodeIf(kL2Entry().getMac());
+
+    if (l2EntryUpdateType == L2EntryUpdateType::L2_ENTRY_UPDATE_TYPE_ADD) {
+      EXPECT_NE(nullptr, node);
+    } else {
+      EXPECT_EQ(nullptr, node);
+    }
+
+    return newState;
+  }
+
+};
+
+TEST_F(MacTableUtilsTest, VerifyUpdateMacTableAddMac) {
+  verifyAddMac(testStateA());
+}
+
+TEST_F(MacTableUtilsTest, VerifyUpdateMacTableAddMacTwice) {
+  auto state = verifyAddMac(testStateA());
+  verifyAddMac(state);
+}
+
+TEST_F(MacTableUtilsTest, VerifyUpdateMacTableDeleteMac) {
+  auto state = verifyAddMac(testStateA());
+  verifyDeleteMac(state);
+}
+
+TEST_F(MacTableUtilsTest, VerifyUpdateMacTableDeleteMacTwice) {
+  auto state1 = verifyAddMac(testStateA());
+  auto state2 = verifyDeleteMac(state1);
+  verifyDeleteMac(state2);
+}
+
+} // namespace fboss
+} // namespace facebook
