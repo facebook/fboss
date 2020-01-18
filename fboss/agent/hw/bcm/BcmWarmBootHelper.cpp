09f0e67c1625c3dd1c176aece87657a98681f9ac
Shrikrishna Khare <skhare@fb.com>
2020-01-18 10:52:30 -0800

    open source BcmWarmBootHelper.cpp and dependencies
    Summary:
    With the landing of D16401189, FBOSS OSS no longer depends on OpenNSL,
    but instead links with OpenNSA.  All the BCM API is available in OpenNSA.
    
    Thus, start moving agent/hw/bcm/facebook/* to open source.
    
    This patch open sources BcmWarmBootHelper.cpp and dependencies
    
    Differential Revision: D19462220
    
    fbshipit-source-id: 4f0c69dc5cf756dfe0da73111bcdf545a8f8badf

--- fboss/agent/hw/bcm/BcmWarmBootHelper.cpp
+++ fboss/agent/hw/bcm/BcmWarmBootHelper.cpp
@@ -3,16 +3,37 @@
 
 #include "fboss/agent/SysError.h"
 #include "fboss/agent/Utils.h"
+#include "fboss/agent/hw/bcm/BcmAPI.h"
+#include "fboss/agent/hw/bcm/BcmError.h"
+#include "fboss/agent/hw/bcm/BcmFacebookAPI.h"
+#include "fboss/agent/hw/bcm/BcmUnit.h"
 
 #include <fcntl.h>
 #include <sys/stat.h>
+#include <chrono>
 
 #include <folly/FileUtil.h>
 #include <folly/json.h>
 #include <folly/logging/xlog.h>
 #include <glog/logging.h>
 
+extern "C" {
+#include <bcm/switch.h>
+#if (!defined(BCM_VER_MAJOR))
+#include <soc/opensoc.h>
+#else
+#include <soc/scache.h>
+#endif
+} // extern "C"
+
 using std::string;
+
+using facebook::fboss::BcmAPI;
+
+using std::chrono::duration;
+using std::chrono::duration_cast;
+using std::chrono::seconds;
+using std::chrono::steady_clock;
 
 DEFINE_bool(can_warm_boot, true, "Enable/disable warm boot functionality");
 DEFINE_string(
@@ -141,6 +162,129 @@ folly::dynamic DiscBackedBcmWarmBootHelper::getWarmBootState() const {
   sysCheckError(
       ret, "Unable to read switch state from : ", warmBootSwitchStateFile());
   return folly::parseJson(warmBootJson);
+}
+
+int BcmWarmBootHelper::warmBootReadCallback(
+    int unitNumber,
+    uint8_t* buf,
+    int offset,
+    int nbytes) {
+  try {
+    auto wbHelper = BcmAPI::getUnit(unitNumber)->warmBootHelper();
+    wbHelper->warmBootRead(buf, offset, nbytes);
+    return nbytes;
+  } catch (const std::exception& ex) {
+    XLOG(ERR) << "error performing warm boot read of " << nbytes
+              << " bytes for unit " << unitNumber << ": "
+              << folly::exceptionStr(ex);
+    return BCM_E_FAIL;
+  }
+}
+
+int BcmWarmBootHelper::warmBootWriteCallback(
+    int unitNumber,
+    uint8_t* buf,
+    int offset,
+    int nbytes) {
+  try {
+    auto wbHelper = BcmAPI::getUnit(unitNumber)->warmBootHelper();
+    wbHelper->warmBootWrite(buf, offset, nbytes);
+    return nbytes;
+  } catch (const std::exception& ex) {
+    XLOG(ERR) << "error performing warm boot write of " << nbytes
+              << " bytes for unit " << unitNumber << ": "
+              << folly::exceptionStr(ex);
+    // Ugh.  Unfortunately the Broadcom SDK code doesn't appear to
+    // check the return value from the warm boot write callback, so
+    // it won't correctly handle errors at this point.
+    return BCM_E_FAIL;
+  }
+}
+
+void DiscBackedBcmWarmBootHelper::setupWarmBootFile() {
+  auto warmBootPath = warmBootDataPath();
+  warmBootFd_ = open(warmBootPath.c_str(), O_RDWR | O_CREAT, 0600);
+  if (warmBootFd_ < 0) {
+    throw SysError(errno, "failed to open warm boot data file ", warmBootPath);
+  }
+
+  auto rv = soc_stable_set(unit_, BCM_SWITCH_STABLE_APPLICATION, 0);
+  bcmCheckError(rv, "unable to configure for warm boot for unit ", unit_);
+
+  rv = soc_switch_stable_register(
+      unit_, &warmBootReadCallback, &warmBootWriteCallback, nullptr, nullptr);
+  bcmCheckError(
+      rv,
+      "unable to register read, write callbacks for warm boot "
+      "on unit ",
+      unit_);
+
+  auto configStableSize = BcmAPI::getConfigValue("stable_size");
+  auto stableSize = (configStableSize)
+      ? std::stoul(configStableSize, nullptr, 0)
+      : SOC_DEFAULT_LVL2_STABLE_SIZE;
+// Remove this code once the new values for stable are propogated
+// to all switches
+#define STABLE_SIZE_MINIMUM 0x6000000
+  if (stableSize < STABLE_SIZE_MINIMUM) {
+    stableSize = STABLE_SIZE_MINIMUM;
+  }
+  XLOG(DBG0) << "Initializing sdk stable storage with max size of "
+             << stableSize << " bytes.";
+  rv = soc_stable_size_set(unit_, stableSize);
+  bcmCheckError(
+      rv, "unable to set size for warm boot storage for unit ", unit_);
+}
+
+void DiscBackedBcmWarmBootHelper::warmBootRead(
+    uint8_t* buf,
+    int offset,
+    int nbytes) {
+  if (warmBootFd_ < 0) {
+    // This shouldn't ever happen.  We only register the warm boot
+    // callbacks after opening the fd.
+    throw FbossError(
+        "attempted warm boot read on unit ",
+        unit_,
+        " but warm boot not configured");
+  }
+
+  // The Broadcom code assumes that the read callback always returns
+  // exactly as much data as requested, so use folly::preadFull().
+  // This should always return the full amount read, or an error.
+  auto bytesRead = folly::preadFull(warmBootFd_, buf, nbytes, offset);
+  if (bytesRead < 0) {
+    throw SysError(
+        errno,
+        "error reading ",
+        nbytes,
+        " bytes from warm boot file for unit ",
+        unit_);
+  }
+}
+
+void DiscBackedBcmWarmBootHelper::warmBootWrite(
+    const uint8_t* buf,
+    int offset,
+    int nbytes) {
+  if (warmBootFd_ < 0) {
+    // This shouldn't ever happen.  We only register the warm boot
+    // callbacks after opening the fd.
+    throw FbossError(
+        "attempted warm boot write on unit ",
+        unit_,
+        " but warm boot not configured");
+  }
+
+  auto bytesWritten = folly::pwriteFull(warmBootFd_, buf, nbytes, offset);
+  if (bytesWritten < 0) {
+    throw SysError(
+        errno,
+        "error writing ",
+        nbytes,
+        " bytes to warm boot file for unit ",
+        unit_);
+  }
 }
 
 } // namespace facebook::fboss
