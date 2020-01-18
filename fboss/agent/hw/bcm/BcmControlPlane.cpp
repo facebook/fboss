33f0be3a26fddb1a0da139f4ed62ba12ee3f570d
Shrikrishna Khare <skhare@fb.com>
2020-01-18 10:52:30 -0800

    open source BcmControlPlane and dependencies
    Summary:
    With the landing of D16401189, FBOSS OSS no longer depends on OpenNSL,
    but instead links with OpenNSA.  All the BCM API is available in OpenNSA.
    
    Thus, start moving agent/hw/bcm/facebook/* to open source.
    
    This patch open sources BcmControlPlane and dependencies
    
    Differential Revision: D19460550
    
    fbshipit-source-id: 2ad465213d170523570080be7c41b84bb02a95ec

--- fboss/agent/hw/bcm/BcmControlPlane.cpp
+++ fboss/agent/hw/bcm/BcmControlPlane.cpp
@@ -8,11 +8,23 @@
  *
  */
 #include "fboss/agent/hw/bcm/BcmControlPlane.h"
+
 #include "fboss/agent/hw/bcm/BcmControlPlaneQueueManager.h"
+#include "fboss/agent/hw/bcm/BcmError.h"
+#include "fboss/agent/hw/bcm/BcmQosPolicyTable.h"
+#include "fboss/agent/hw/bcm/BcmSwitch.h"
+#include "fboss/agent/state/PortQueue.h"
 
 extern "C" {
+#include <bcm/cosq.h>
+#include <bcm/qos.h>
 #include <bcm/types.h>
 }
+
+using facebook::stats::MonotonicCounter;
+using std::chrono::duration_cast;
+using std::chrono::seconds;
+using std::chrono::system_clock;
 
 namespace {
 constexpr auto kCPUName = "cpu";
@@ -32,6 +44,24 @@ void BcmControlPlane::setupQueue(const PortQueue& queue) {
 void BcmControlPlane::setupRxReasonToQueue(
     const ControlPlane::RxReasonToQueue& /*reasonToQueue*/) {
   // TODO(joseph5wu) Implement the logic of setting reason mapping to bcm
+}
+
+void BcmControlPlane::setupIngressQosPolicy(
+    const std::optional<std::string>& qosPolicyName) {
+  int ingressHandle = 0; // 0 means 'detach the current policy'
+  int egressHandle = -1; // -1 means do not change the current policy
+  if (qosPolicyName) {
+    auto qosPolicy = hw_->getQosPolicyTable()->getQosPolicy(*qosPolicyName);
+    ingressHandle = qosPolicy->getHandle(BcmQosMap::Type::IP_INGRESS);
+  }
+  auto rv =
+      bcm_qos_port_map_set(hw_->getUnit(), 0, ingressHandle, egressHandle);
+  bcmCheckError(rv, "failed to set the qos map on CPU port");
+}
+
+void BcmControlPlane::updateQueueCounters() {
+  auto now = duration_cast<seconds>(system_clock::now().time_since_epoch());
+  queueManager_->updateQueueStats(now);
 }
 
 } // namespace facebook::fboss
