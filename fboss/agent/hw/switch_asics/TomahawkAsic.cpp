502521836a75b53d750101a72181e08f244b9995
Jasmeet Bagga <jasmeetbagga@fb.com>
2020-01-23 14:02:32 -0800

    Open source ASIC features
    Summary:
    TD2, TH and TH3 asic usage is all in public domain. No need
    to hide them. There might be a concern for hiding features of the
    ASIC, but that doesn't seem too concerning, since
    - ASIC capabilities are well advertised by companies
    - For fine grained capabiliies (MPLS ECMP), a user could always take
    our test cases and run them on a OCP box to figure this out.
    
    Further, since we have opensourced bcm_tests - we don't want to
    paralyze them by hiding information about ASICs
    
    Reviewed By: shri-khare
    
    Differential Revision: D19530291
    
    fbshipit-source-id: f173e4203d7a35a229272aa6e0c92a0f4afb0800

---
+++ fboss/agent/hw/switch_asics/TomahawkAsic.cpp
@@ -0,0 +1,27 @@
+// Copyright 2004-present Facebook. All Rights Reserved.
+
+#include "fboss/agent/hw/switch_asics/TomahawkAsic.h"
+
+namespace facebook::fboss {
+
+bool TomahawkAsic::isSupported(Feature feature) const {
+  switch (feature) {
+    case HwAsic::Feature::SPAN:
+    case HwAsic::Feature::ERSPANv4:
+    case HwAsic::Feature::SFLOWv4:
+    case HwAsic::Feature::MPLS:
+    case HwAsic::Feature::MPLS_ECMP:
+    case HwAsic::Feature::HOT_SWAP:
+      return true;
+
+    case HwAsic::Feature::HOSTTABLE_FOR_HOSTROUTES:
+    case HwAsic::Feature::TRUNCATE_MIRROR_PACKET:
+    case HwAsic::Feature::ERSPANv6:
+    case HwAsic::Feature::SFLOWv6:
+    case HwAsic::Feature::TX_VLAN_STRIPPING_ON_PORT:
+      return false;
+  }
+  return false;
+}
+
+} // namespace facebook::fboss
