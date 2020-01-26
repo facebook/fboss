54106fb2592332c7f575fad068bd2b548a15ce9e
Jasmeet Bagga <jasmeetbagga@fb.com>
2020-01-25 18:15:12 -0800

    Kill all opennsl references in fboss external deps
    Summary: As titled
    
    Reviewed By: shri-khare
    
    Differential Revision: D19568586
    
    fbshipit-source-id: aa66b3c860f5436931804926b0ba29ea5b1cf1e9

--- fboss/agent/hw/bcm/oss/BcmPort.cpp
+++ fboss/agent/hw/bcm/oss/BcmPort.cpp
@@ -19,23 +19,11 @@ extern "C" {
 namespace facebook::fboss {
 
 cfg::PortSpeed BcmPort::getMaxSpeed() const {
-#if (!defined(OPENNSL_VERSION_3_7_0_1_ODP)) && \
-    (!defined(OPENNSL_VERSION_3_9_0_1_ODP))
-#include <soc/opensoc.h>
-  uint32 flags;
-  bcm_port_interface_info_t interface_info;
-  bcm_port_mapping_info_t mapping_info;
-
-  auto ret = bcm_port_get(unit_, port_, &flags, &interface_info, &mapping_info);
-  bcmCheckError(ret, "Failed to get speed info for port ", port_);
-  return cfg::PortSpeed(interface_info.max_speed);
-#else
   int speed;
   auto unit = hw_->getUnit();
   auto rv = bcm_port_speed_max(hw_->getUnit(), port_, &speed);
   bcmCheckError(rv, "Failed to get max speed for port ", port_);
   return cfg::PortSpeed(speed);
-#endif
 }
 
 } // namespace facebook::fboss
