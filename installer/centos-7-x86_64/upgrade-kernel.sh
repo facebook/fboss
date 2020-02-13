26c1bf33e7e30a894f074c55401d1ab812a5925b
Shrikrishna Khare <skhare@fb.com>
2020-02-13 13:06:37 -0800

    When upgrading kernel, install headers too
    Summary: As titled.
    
    Differential Revision: D19868393
    
    fbshipit-source-id: 967e4624a0cf34e6b23f098b738b152d2ad8d062

--- installer/centos-7-x86_64/upgrade-kernel.sh
+++ installer/centos-7-x86_64/upgrade-kernel.sh
@@ -2,4 +2,4 @@
 
 rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
 rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-3.el7.elrepo.noarch.rpm
-yum --enablerepo=elrepo-kernel install kernel-lt
+yum --enablerepo=elrepo-kernel install kernel-lt kernel-lt-devel
