35cadb5fba7b41b635a6d2aaf224ddceb0269efe
Jasmeet Bagga <jasmeetbagga@fb.com>
2020-02-14 00:56:09 -0800

    Tests to assert that port, queue rename doesn't cause stats to be lost
    Summary: As titled
    
    Differential Revision: D19802012
    
    fbshipit-source-id: fc539f9d53dae8d795aa00927ad99a76afd1327f

--- fboss/agent/hw/test/HwPortFb303StatsTests.cpp
+++ fboss/agent/hw/test/HwPortFb303StatsTests.cpp
@@ -25,6 +25,71 @@ HwPortFb303Stats::QueueId2Name kQueue2Name = {
     {1, "gold"},
     {2, "silver"},
 };
+
+HwPortStats getInitedStats() {
+  return {
+      apache::thrift::FragileConstructor(),
+      1, // inBytes
+      2, // inUcastPackets
+      3, // inMulticastPkts
+      4, // inBroadcastPkts
+      5, // inDiscards
+      6, // inErrors
+      7, // inPause
+      8, // inIpv4HdrErrors
+      9, // inIpv6HdrErrors
+      10, // inDstNullDiscards
+      11, // inDiscardsRaw
+      12, // outBytes
+      13, // outUnicastPkts
+      14, // outMulticastPkts
+      15, // outBroadcastPkts
+      16, // outDiscards
+      17, // outErrors
+      18, // outPause
+      19, // outCongestionDiscardPkts
+      {{1, 1}, {2, 1}}, // queueOutDiscards
+      {{1, 2}, {2, 2}}, // queueOutBytes
+      20, // outEcnCounter
+      {{1, 3}, {2, 3}}, // queueOutPackets
+  };
+}
+
+void updateStats(HwPortFb303Stats& portStats) {
+  auto now = duration_cast<seconds>(system_clock::now().time_since_epoch());
+  // To get last increment from monotonic counter we need to update it twice
+  HwPortStats empty{};
+  // Need to populate queue stats, since by default these
+  // maps are empty
+  empty.queueOutDiscardBytes_ = empty.queueOutBytes_ =
+      empty.queueOutPackets_ = {{1, 0}, {2, 0}};
+  portStats.updateStats(empty, now);
+  portStats.updateStats(getInitedStats(), now);
+}
+
+void verifyUpdatedStats(const HwPortFb303Stats& portStats) {
+  auto curValue{1};
+  for (auto counterName : HwPortFb303Stats::kPortStatKeys()) {
+    // +1 because first initialization is to -1
+    EXPECT_EQ(
+        portStats.getCounterLastIncrement(
+            HwPortFb303Stats::statName(counterName, kPortName)),
+        curValue++ + 1);
+  }
+  curValue = 1;
+  for (auto counterName : HwPortFb303Stats::kQueueStatKeys()) {
+    for (const auto& queueIdAndName : kQueue2Name) {
+      EXPECT_EQ(
+          portStats.getCounterLastIncrement(HwPortFb303Stats::statName(
+              counterName,
+              kPortName,
+              queueIdAndName.first,
+              queueIdAndName.second)),
+          curValue);
+    }
+    ++curValue;
+  }
+}
 } // namespace
 
 TEST(HwPortFb303StatsTest, StatName) {
@@ -80,63 +145,9 @@ TEST(HwPortFb303StatsTest, ReInit) {
 }
 
 TEST(HwPortFb303Stats, UpdateStats) {
-  HwPortStats stats{
-      apache::thrift::FragileConstructor(),
-      1, // inBytes
-      2, // inUcastPackets
-      3, // inMulticastPkts
-      4, // inBroadcastPkts
-      5, // inDiscards
-      6, // inErrors
-      7, // inPause
-      8, // inIpv4HdrErrors
-      9, // inIpv6HdrErrors
-      10, // inDstNullDiscards
-      11, // inDiscardsRaw
-      12, // outBytes
-      13, // outUnicastPkts
-      14, // outMulticastPkts
-      15, // outBroadcastPkts
-      16, // outDiscards
-      17, // outErrors
-      18, // outPause
-      19, // outCongestionDiscardPkts
-      {{1, 1}, {2, 1}}, // queueOutDiscards
-      {{1, 2}, {2, 2}}, // queueOutBytes
-      20, // outEcnCounter
-      {{1, 3}, {2, 3}}, // queueOutPackets
-  };
   HwPortFb303Stats portStats(kPortName, kQueue2Name);
-  auto now = duration_cast<seconds>(system_clock::now().time_since_epoch());
-  // To get last increment from monotonic counter we need to update it twice
-  HwPortStats empty{};
-  // Need to populate queue stats, since by default these
-  // maps are empty
-  empty.queueOutDiscardBytes_ = empty.queueOutBytes_ =
-      empty.queueOutPackets_ = {{1, 0}, {2, 0}};
-  portStats.updateStats(empty, now);
-  portStats.updateStats(stats, now);
-  auto curValue{1};
-  for (auto counterName : HwPortFb303Stats::kPortStatKeys()) {
-    // +1 because first initialization is to -1
-    EXPECT_EQ(
-        portStats.getCounterLastIncrement(
-            HwPortFb303Stats::statName(counterName, kPortName)),
-        curValue++ + 1);
-  }
-  curValue = 1;
-  for (auto counterName : HwPortFb303Stats::kQueueStatKeys()) {
-    for (const auto& queueIdAndName : kQueue2Name) {
-      EXPECT_EQ(
-          portStats.getCounterLastIncrement(HwPortFb303Stats::statName(
-              counterName,
-              kPortName,
-              queueIdAndName.first,
-              queueIdAndName.second)),
-          curValue);
-    }
-    ++curValue;
-  }
+  updateStats(portStats);
+  verifyUpdatedStats(portStats);
 }
 
 TEST(HwPortFb303StatsTest, RenameQueue) {
@@ -178,4 +189,18 @@ TEST(HwPortFb303StatsTest, RemoveQueue) {
     EXPECT_TRUE(fbData->getStatMap()->contains(
         HwPortFb303Stats::statName(statKey, kPortName, 2, "silver")));
   }
+}
+
+TEST(HwPortFb303Stats, portNameChangeStatsRetainValue) {
+  HwPortFb303Stats portStats(kPortName, kQueue2Name);
+  updateStats(portStats);
+  portStats.portNameChanged("fab1/1/1");
+  verifyUpdatedStats(portStats);
+}
+
+TEST(HwPortFb303Stats, queueNameChangeStatsRetainValue) {
+  HwPortFb303Stats portStats(kPortName, kQueue2Name);
+  updateStats(portStats);
+  portStats.addOrUpdateQueue(1, "platinium");
+  verifyUpdatedStats(portStats);
 }
