83dce0b5d0d54b449931b6c3e94f28d00d4df3fa
Parvez Shaikh <pshaikh@fb.com>
2020-02-16 10:17:02 -0800

    next hop handle
    Summary:
    Sai Next Hop Handle contains reference to either MPLS next hop or IP next hop.
    
    Only public function is to get the adapter key. No other details of next hop are needed by clients.
    
    Accordingly provided functions in Next Hop manager to provide this handle based on resolved next hop. Next Hop manager creates Sai Next Hop Handle to appropriate next hop
    
    Reviewed By: boryas
    
    Differential Revision: D19837995
    
    fbshipit-source-id: b2f66eb5e0be219068cd36d4c01be38140f90a8e

--- fboss/agent/hw/sai/switch/SaiNextHopManager.cpp
+++ fboss/agent/hw/sai/switch/SaiNextHopManager.cpp
@@ -13,9 +13,12 @@
 #include "fboss/agent/FbossError.h"
 #include "fboss/agent/hw/sai/store/SaiStore.h"
 #include "fboss/agent/hw/sai/switch/SaiManagerTable.h"
+#include "fboss/agent/hw/sai/switch/SaiRouterInterfaceManager.h"
 #include "fboss/agent/hw/sai/switch/SaiSwitchManager.h"
 
 #include <folly/logging/xlog.h>
+
+#include "fboss/agent/state/RouteNextHop.h"
 
 namespace facebook::fboss {
 
@@ -33,5 +36,82 @@ SaiNextHopManager::SaiNextHopManager(
     SaiManagerTable* managerTable,
     const SaiPlatform* platform)
     : managerTable_(managerTable), platform_(platform) {}
+
+SaiNextHopHandle::SaiNextHopHandle(
+    const SaiIpNextHopTraits::AdapterHostKey& key) {
+  const auto& [intf, ip] = key;
+  auto& store = SaiStore::getInstance()->get<SaiIpNextHopTraits>();
+  nexthop_ = store.setObject(key, {SAI_NEXT_HOP_TYPE_IP, intf, ip});
+  // TODO(pshaikh): define a way to access variant member based on object traits
+  id_ = std::get<0>(nexthop_)->adapterKey();
+}
+
+SaiNextHopHandle::SaiNextHopHandle(
+    const SaiMplsNextHopTraits::AdapterHostKey& key) {
+  const auto& [intf, ip, labelStack] = key;
+  SaiMplsNextHopTraits::CreateAttributes attributes;
+  auto& store = SaiStore::getInstance()->get<SaiMplsNextHopTraits>();
+  nexthop_ =
+      store.setObject(key, {SAI_NEXT_HOP_TYPE_MPLS, intf, ip, labelStack});
+  // TODO(pshaikh): define a way to access variant member based on object traits
+  id_ = std::get<1>(nexthop_)->adapterKey();
+}
+
+std::shared_ptr<SaiNextHopHandle> SaiNextHopManager::refOrEmplace(
+    const ResolvedNextHop& swNextHop) {
+  auto interfaceId = swNextHop.intfID().value();
+  auto routerInterfaceHandle =
+      managerTable_->routerInterfaceManager().getRouterInterfaceHandle(
+          interfaceId);
+  if (!routerInterfaceHandle) {
+    throw FbossError("Missing SAI router interface for ", interfaceId);
+  }
+  auto rifId = routerInterfaceHandle->routerInterface->adapterKey();
+  folly::IPAddress ip = swNextHop.addr();
+  if (!swNextHop.labelForwardingAction() ||
+      LabelForwardingAction::LabelForwardingType::PHP ==
+          swNextHop.labelForwardingAction()->type()) {
+    return refOrEmplace(rifId, ip);
+  }
+  CHECK(
+      swNextHop.labelForwardingAction()->type() !=
+      LabelForwardingAction::LabelForwardingType::POP_AND_LOOKUP)
+      << "action pop is not supported";
+  if (LabelForwardingAction::LabelForwardingType::SWAP ==
+      swNextHop.labelForwardingAction()->type()) {
+    return refOrEmplace(
+        rifId, ip, swNextHop.labelForwardingAction()->swapWith().value());
+  }
+  return refOrEmplace(
+      rifId, ip, swNextHop.labelForwardingAction()->pushStack().value());
+}
+
+std::shared_ptr<SaiNextHopHandle> SaiNextHopManager::refOrEmplace(
+    SaiRouterInterfaceTraits::AdapterKey interface,
+    const folly::IPAddress& ip) {
+  SaiIpNextHopTraits::AdapterHostKey k{interface, ip};
+  auto emplaced = ipNextHops_.refOrEmplace(k, k);
+  return emplaced.first;
+}
+
+std::shared_ptr<SaiNextHopHandle> SaiNextHopManager::refOrEmplace(
+    SaiRouterInterfaceTraits::AdapterKey interface,
+    const folly::IPAddress& ip,
+    LabelForwardingAction::Label label) {
+  return refOrEmplace(interface, ip, LabelForwardingAction::LabelStack{label});
+}
+
+std::shared_ptr<SaiNextHopHandle> SaiNextHopManager::refOrEmplace(
+    SaiRouterInterfaceTraits::AdapterKey interface,
+    const folly::IPAddress& ip,
+    const LabelForwardingAction::LabelStack& stack) {
+  std::vector<sai_uint32_t> labels;
+  for (auto label : stack) {
+    labels.push_back(label);
+  }
+  SaiMplsNextHopTraits::AdapterHostKey k{interface, ip, std::move(labels)};
+  auto emplaced = mplsNextHops_.refOrEmplace(k, k);
+  return emplaced.first;
+}
 
 } // namespace facebook::fboss
--- fboss/agent/hw/sai/switch/SaiNextHopManager.h
+++ fboss/agent/hw/sai/switch/SaiNextHopManager.h
@@ -11,20 +11,56 @@
 #pragma once
 
 #include "fboss/agent/hw/sai/api/NextHopApi.h"
+#include "fboss/agent/hw/sai/api/RouterInterfaceApi.h"
 #include "fboss/agent/hw/sai/store/SaiObject.h"
+#include "fboss/agent/state/LabelForwardingAction.h"
 #include "fboss/agent/types.h"
+#include "fboss/lib/RefMap.h"
 
 #include "folly/IPAddress.h"
 
 #include <memory>
-#include <unordered_map>
 
 namespace facebook::fboss {
 
 class SaiManagerTable;
 class SaiPlatform;
+class ResolvedNextHop;
 
 using SaiIpNextHop = SaiObject<SaiIpNextHopTraits>;
+using SaiMplsNextHop = SaiObject<SaiMplsNextHopTraits>;
+
+/*
+ * SaiNextHopHandle holds reference to either ip next hop or mpls next hop
+ * This is done to simplify handling of multipath groups which may need to hold
+ * both ip next hops or mpls next hops or both together. This is tightly wrapped
+ * class, and clients of this class need not make any distinction between ip
+ * next hop and mpls next hop. It only provides handle to underlying adapter
+ * key.
+ * The only operations this class should allow are creation, destruction and
+ * fetching an underlying SAI object id. This means that one of the two next hop
+ * SAI objects either exist in SDK/ASIC or they don't.
+ */
+class SaiNextHopHandle {
+ public:
+  explicit SaiNextHopHandle(const SaiIpNextHopTraits::AdapterHostKey& key);
+  explicit SaiNextHopHandle(const SaiMplsNextHopTraits::AdapterHostKey& key);
+  NextHopSaiId adapterKey() const {
+    return id_;
+  }
+  SaiNextHopHandle(const SaiNextHopHandle&) = delete;
+  SaiNextHopHandle& operator=(const SaiNextHopHandle&) = delete;
+  SaiNextHopHandle(SaiNextHopHandle&&) = delete;
+  SaiNextHopHandle& operator==(SaiNextHopHandle&&) = delete;
+
+ private:
+  // TODO(pshaikh): define object type for object with condition attribute and
+  // its adapter host key
+  using SaiNextHop = std::
+      variant<std::shared_ptr<SaiIpNextHop>, std::shared_ptr<SaiMplsNextHop>>;
+  SaiNextHop nexthop_;
+  NextHopSaiId id_{0};
+};
 
 class SaiNextHopManager {
  public:
@@ -33,9 +69,34 @@ class SaiNextHopManager {
       RouterInterfaceSaiId routerInterfaceId,
       const folly::IPAddress& ip);
 
+  /* return next hop handle here, based on resolved next hop. resulting next hop
+   * handle will be either ip next hop or mpls next hop depending on properties
+   * of resolved next hop */
+  std::shared_ptr<SaiNextHopHandle> refOrEmplace(
+      const ResolvedNextHop& swNextHop);
+
  private:
+  /* emplace ip next hop */
+  std::shared_ptr<SaiNextHopHandle> refOrEmplace(
+      SaiRouterInterfaceTraits::AdapterKey interface,
+      const folly::IPAddress& ip);
+  /* emplace mpls next hop with single label */
+  std::shared_ptr<SaiNextHopHandle> refOrEmplace(
+      SaiRouterInterfaceTraits::AdapterKey interface,
+      const folly::IPAddress& ip,
+      LabelForwardingAction::Label label);
+  /* emplace mpls next hop with label stack */
+  std::shared_ptr<SaiNextHopHandle> refOrEmplace(
+      SaiRouterInterfaceTraits::AdapterKey interface,
+      const folly::IPAddress& ip,
+      const LabelForwardingAction::LabelStack& stack);
+
   SaiManagerTable* managerTable_;
   const SaiPlatform* platform_;
+  UnorderedRefMap<SaiIpNextHopTraits::AdapterHostKey, SaiNextHopHandle>
+      ipNextHops_;
+  UnorderedRefMap<SaiMplsNextHopTraits::AdapterHostKey, SaiNextHopHandle>
+      mplsNextHops_;
 };
 
 } // namespace facebook::fboss
