0a2a115e6eac656235665881a2be58dee5fef21d
Shrikrishna Khare <skhare@fb.com>
2020-02-22 13:07:16 -0800

    Open source SAI dataplane tests
    Summary:
    The source files were already open sourced but were not being built as part of
    the hw tests. Fix it.
    
    Would script to find out:
    - files that are open sourced but not built by CMake
    - built by CMake but tests are not linked.
    
    But that is for follow up diff(s).
    
    Differential Revision: D20056657
    
    fbshipit-source-id: f5c8a18c3a3068bcc4421a76933918abff5e2aeb

--- cmake/AgentHwTest.cmake
+++ cmake/AgentHwTest.cmake
@@ -23,6 +23,18 @@ target_link_libraries(hw_test_main
   Folly::folly
 )
 
+add_library(hw_packet_utils
+  fboss/agent/hw/test/HwTestLearningUpdateObserver.cpp
+  fboss/agent/hw/test/HwTestPacketSnooper.cpp
+  fboss/agent/hw/test/HwTestPacketUtils.cpp
+)
+
+target_link_libraries(hw_packet_utils
+  hw_switch_ensemble
+  packet_factory
+  Folly::folly
+)
+
 add_library(hw_link_state_toggler
   fboss/agent/hw/test/HwLinkStateToggler.cpp
 )
@@ -43,6 +55,21 @@ target_link_libraries(hw_switch_ensemble
   core
 )
 
+add_library(load_balancer_utils
+  fboss/agent/hw/test/LoadBalancerUtils.cpp
+)
+
+target_link_libraries(load_balancer_utils
+  hw_packet_utils
+  core
+  fboss_types
+  switch_config_cpp2
+  hw_switch_ensemble
+  packet_factory
+  state
+  Folly::folly
+)
+
 add_library(hw_switch_test
   fboss/agent/hw/test/HwLinkStateDependentTest.cpp
   fboss/agent/hw/test/HwTest.cpp
@@ -53,14 +80,35 @@ add_library(hw_switch_test
   fboss/agent/hw/test/HwRouteOverflowTest.cpp
   fboss/agent/hw/test/HwVlanTests.cpp
   fboss/agent/hw/test/HwL2ClassIDTests.cpp
+
+  fboss/agent/hw/test/dataplane_tests/HwCoppTests.cpp
+  fboss/agent/hw/test/dataplane_tests/HwEcmpDataPlaneTestUtil.cpp
+  fboss/agent/hw/test/dataplane_tests/HwJumboFramesTests.cpp
+  fboss/agent/hw/test/dataplane_tests/HwInDiscardCounterTests.cpp
+  fboss/agent/hw/test/dataplane_tests/HwInPauseDiscardsTests.cpp
+  fboss/agent/hw/test/dataplane_tests/HwL4PortBlackholingTests.cpp
+  fboss/agent/hw/test/dataplane_tests/HwMacLearningTests.cpp
+  fboss/agent/hw/test/dataplane_tests/HwLoadBalancerTests.cpp
+  fboss/agent/hw/test/dataplane_tests/HwLoadBalancerTestsV4.cpp
+  fboss/agent/hw/test/dataplane_tests/HwLoadBalancerTestsV6.cpp
+  fboss/agent/hw/test/dataplane_tests/HwLoadBalancerTestsV4ToMpls.cpp
+  fboss/agent/hw/test/dataplane_tests/HwLoadBalancerTestsV6ToMpls.cpp
+  fboss/agent/hw/test/dataplane_tests/HwLoadBalancerTestsV4InMplsPhp.cpp
+  fboss/agent/hw/test/dataplane_tests/HwLoadBalancerTestsV6InMplsPhp.cpp
+  fboss/agent/hw/test/dataplane_tests/HwLoadBalancerTestsV4InMplsSwap.cpp
+  fboss/agent/hw/test/dataplane_tests/HwLoadBalancerTestsV6InMplsSwap.cpp
+  fboss/agent/hw/test/dataplane_tests/HwTrunkLoadBalancerTests.cpp
 )
 
 target_link_libraries(hw_switch_test
   config_factory
+  hw_packet_utils
   hw_switch_ensemble
+  load_balancer_utils
   core
   hardware_stats_cpp2
   route_distribution_gen
   route_scale_gen
+  trunk_utils
   Folly::folly
 )
