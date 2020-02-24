68522f72956bdcea78f5bf794a961d845dfc1458
Shrikrishna Khare <skhare@fb.com>
2020-02-24 15:16:38 -0800

    Faster packaging FBOSS OSS binaries and deps
    Summary:
    The current build-rpm.py script takes > 30 minutes.
    Even simply tar'ing/gziping takes > 7 minutes.
    
    In most cases, it would be sufficient to copy FBOSS binaries and deps. This
    patch adds package-fboss.py script that would do exactly that. It creates a
    directory in /tmp/ and copies all FBOSS OSS binaries and deps. This could be
    easily scp -r'ed to the test switch.
    
    This approach takes approx 5 seconds vs. build-rpm (> 30 mins) or tar'ing (> 7
    minutes). The tar reduces the size by half (these are compiled binaries), but
    it is significantly faster to copy the non-tar'ed files over network than wait
    for 7 minutes to tar and then copy.
    
    The original rpm build functionality is retained by --rpm option.
    
    Differential Revision: D20071401
    
    fbshipit-source-id: df56a4dde9af7f9a4510d02678acc99bc5992ee7

--- installer/centos-7-x86_64/package-fboss.py
+++ installer/centos-7-x86_64/package-fboss.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python3
 # Copyright 2004-present Facebook. All Rights Reserved.
 
+import argparse
 import os
 import shutil
 import subprocess
 import tarfile
+import tempfile
 
 
-class BuildRpm:
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--rpm", help="Builds RPM", action="store_true")
+    return parser.parse_args()
+
+
+class PackageFboss:
     FBOSS_BINS = "fboss_bins-1"
     FBOSS_BINS_SPEC = FBOSS_BINS + ".spec"
 
@@ -38,14 +46,35 @@ class BuildRpm:
     }
 
     def __init__(self):
-        self.rpm_dir_abs = os.path.join(BuildRpm.HOME_DIR_ABS, "rpmbuild")
+        self.tmp_dir_name = tempfile.mkdtemp(prefix=PackageFboss.FBOSS_BINS)
+
+        self.rpm_dir_abs = os.path.join(PackageFboss.HOME_DIR_ABS, "rpmbuild")
         self.rpm_src_dir_abs = os.path.join(self.rpm_dir_abs, "SOURCES")
         self.rpm_spec_dir_abs = os.path.join(self.rpm_dir_abs, "SPECS")
 
         self.rpm_src_fboss_dir_abs = os.path.join(
-            self.rpm_src_dir_abs, BuildRpm.FBOSS_BINS
+            self.rpm_src_dir_abs, PackageFboss.FBOSS_BINS
         )
         self.rpm_src_fboss_tar_abs = self.rpm_src_fboss_dir_abs + ".tar.gz"
+
+    def _get_install_dir_for(self, name):
+        return (
+            subprocess.check_output([PackageFboss.GETDEPS, "show-inst-dir", name])
+            .decode("utf-8")
+            .strip()
+            .split("\n")[-1]
+        )
+
+    def _copy_binaries(self, tmp_dir_name):
+        print(f"Copying binaries...")
+
+        for name, exec_type_and_execs in PackageFboss.NAME_TO_EXECUTABLES.items():
+            executable_path = self._get_install_dir_for(name)
+            executable_type, executables = exec_type_and_execs
+            for e in executables:
+                abs_path = os.path.join(executable_path, executable_type, e)
+                print(f"Copying {abs_path} to {tmp_dir_name}")
+                shutil.copy(abs_path, tmp_dir_name)
 
     def _setup_for_rpmbuild(self):
         print(f"Setup for rpmbuild...")
@@ -54,25 +83,6 @@ class BuildRpm:
         os.makedirs(self.rpm_dir_abs)
         subprocess.run("rpmdev-setuptree")
         os.makedirs(self.rpm_src_fboss_dir_abs)
-
-    def _get_install_dir_for(self, name):
-        return (
-            subprocess.check_output([BuildRpm.GETDEPS, "show-inst-dir", name])
-            .decode("utf-8")
-            .strip()
-            .split("\n")[-1]
-        )
-
-    def _copy_binaries(self):
-        print(f"Copying binaries...")
-
-        for name, exec_type_and_execs in BuildRpm.NAME_TO_EXECUTABLES.items():
-            executable_path = self._get_install_dir_for(name)
-            executable_type, executables = exec_type_and_execs
-            for e in executables:
-                abs_path = os.path.join(executable_path, executable_type, e)
-                print(f"Copying {abs_path} to {self.rpm_src_fboss_dir_abs}")
-                shutil.copy(abs_path, self.rpm_src_fboss_dir_abs)
 
     def _prepare_for_build(self):
         print(f"Preparing for build...")
@@ -87,24 +97,32 @@ class BuildRpm:
 
         # TODO use rpmdev-newspec $FBOSS_BINS to create spec and edit it.
         # For now, copy pre-created SPEC file.
-        shutil.copy(BuildRpm.RPM_SPEC_ABS, self.rpm_spec_dir_abs)
+        shutil.copy(PackageFboss.RPM_SPEC_ABS, self.rpm_spec_dir_abs)
 
-    def _build_rpm(self):
+    def _build_rpm_helper(self):
         print(f"Building rpm...")
         env = dict(os.environ)
-        env["LD_LIBRARY_PATH"] = BuildRpm.DEVTOOLS_LIBRARY_PATH
+        env["LD_LIBRARY_PATH"] = PackageFboss.DEVTOOLS_LIBRARY_PATH
         subprocess.run(
-            ["rpmbuild", "-ba", BuildRpm.FBOSS_BINS_SPEC],
+            ["rpmbuild", "-ba", PackageFboss.FBOSS_BINS_SPEC],
             cwd=self.rpm_spec_dir_abs,
             env=env,
         )
 
-    def run(self):
+    def _build_rpm(self):
         self._setup_for_rpmbuild()
-        self._copy_binaries()
+        self._copy_binaries(self.rpm_src_fboss_dir_abs)
         self._prepare_for_build()
-        self._build_rpm()
+        self._build_rpm_helper()
+
+    def run(self, args):
+        if args.rpm:
+            print(f"Building RPM...")
+            self._build_rpm()
+        else:
+            self._copy_binaries(self.tmp_dir_name)
 
 
 if __name__ == "__main__":
-    BuildRpm().run()
+    args = parse_args()
+    PackageFboss().run(args)
