a4be85f9fed5a58c100d2d898b34b142ba3a3dac
Jasmeet Bagga <jasmeetbagga@fb.com>
2020-02-28 21:24:54 -0800

    Start building Adapter key serializers
    Summary:
    To be used for warm boots for serializing
    adapter keys.
    
    Reviewed By: phshaikh
    
    Differential Revision: D20154799
    
    fbshipit-source-id: 0f7750d93bccb53fbe05c04735cf8790f13c7cdb

---
+++ fboss/agent/hw/sai/api/AdapterKeySerializers.h
@@ -0,0 +1,34 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+#pragma once
+
+#include "fboss/agent/hw/sai/api/Traits.h"
+
+#include <folly/dynamic.h>
+
+namespace facebook::fboss {
+constexpr auto kKey = "adapterkey";
+
+template <typename SaiObjectTraits>
+std::enable_if_t<AdapterKeyIsObjectId<SaiObjectTraits>::value, folly::dynamic>
+toFollyDynamic(const typename SaiObjectTraits::AdapterKey& adapterKey) {
+  folly::dynamic key = folly::dynamic::object;
+  key[kKey] = static_cast<long>(adapterKey);
+  return key;
+}
+
+template <typename SaiObjectTraits>
+std::enable_if_t<
+    AdapterKeyIsObjectId<SaiObjectTraits>::value,
+    typename SaiObjectTraits::AdapterKey>
+fromFollyDynamic(const folly::dynamic& obj) {
+  return typename SaiObjectTraits::AdapterKey(obj[kKey].asInt());
+}
+} // namespace facebook::fboss
---
+++ fboss/agent/hw/sai/api/tests/AdapterKeySerializerTest.cpp
@@ -0,0 +1,23 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#include "fboss/agent/hw/sai/api/AdapterKeySerializers.h"
+#include "fboss/agent/hw/sai/api/VlanApi.h"
+
+#include <gtest/gtest.h>
+
+using namespace facebook::fboss;
+
+TEST(AdapterKeySerializerTest, toFromFollyDynamicVlan) {
+  SaiVlanTraits::AdapterKey vlan{42};
+  EXPECT_EQ(
+      vlan,
+      fromFollyDynamic<SaiVlanTraits>(toFollyDynamic<SaiVlanTraits>(vlan)));
+}
