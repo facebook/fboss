4c53a8fd212afd22f1c27eeb563826ba19f1108b
Parvez Shaikh <pshaikh@fb.com>
2020-03-16 09:04:27 -0700

    tests for mpls next hop tests
    Summary: testing mpls attribute creation
    
    Reviewed By: boryas
    
    Differential Revision: D20317966
    
    fbshipit-source-id: 628572a9463da21fb5189d669972c75c1a01c3d9

--- fboss/agent/hw/sai/api/tests/NextHopApiTest.cpp
+++ fboss/agent/hw/sai/api/tests/NextHopApiTest.cpp
@@ -40,6 +40,27 @@ class NextHopApiTest : public ::testing::Test {
     EXPECT_EQ(0, fnh.routerInterfaceId);
     return nextHopId;
   }
+
+  NextHopSaiId createMplsNextHop(
+      folly::IPAddress ip,
+      std::vector<sai_uint32_t> stack) {
+    SaiMplsNextHopTraits::Attributes::Type typeAttribute(
+        SAI_NEXT_HOP_TYPE_MPLS);
+    SaiMplsNextHopTraits::Attributes::RouterInterfaceId
+        routerInterfaceIdAttribute(0);
+    SaiMplsNextHopTraits::Attributes::Ip ipAttribute(ip4);
+    SaiMplsNextHopTraits::Attributes::LabelStack labelStack{stack};
+    auto nextHopId = nextHopApi->create<SaiMplsNextHopTraits>(
+        {typeAttribute, routerInterfaceIdAttribute, ipAttribute, labelStack},
+        0);
+    auto fnh = fs->nextHopManager.get(nextHopId);
+    EXPECT_EQ(SAI_NEXT_HOP_TYPE_MPLS, fnh.type);
+    EXPECT_EQ(ip, fnh.ip);
+    EXPECT_EQ(0, fnh.routerInterfaceId);
+    EXPECT_EQ(stack, fnh.labelStack);
+
+    return nextHopId;
+  }
   std::shared_ptr<FakeSai> fs;
   std::unique_ptr<NextHopApi> nextHopApi;
   folly::IPAddress ip4{str4};
@@ -47,6 +68,10 @@ class NextHopApiTest : public ::testing::Test {
 
 TEST_F(NextHopApiTest, createNextHop) {
   createNextHop(ip4);
+}
+
+TEST_F(NextHopApiTest, createMplsNextHop) {
+  createMplsNextHop(ip4, {1001, 2001, 3001});
 }
 
 TEST_F(NextHopApiTest, removeNextHop) {
@@ -60,6 +85,17 @@ TEST_F(NextHopApiTest, getIp) {
   auto nextHopId = createNextHop(ip4);
   SaiIpNextHopTraits::Attributes::Ip ipAttribute;
   EXPECT_EQ(ip4, nextHopApi->getAttribute(nextHopId, ipAttribute));
+}
+
+TEST_F(NextHopApiTest, getLabelStack) {
+  auto nextHopId = createMplsNextHop(ip4, {1001, 2001, 3001});
+  SaiMplsNextHopTraits::Attributes::Ip ipAttribute;
+  EXPECT_EQ(ip4, nextHopApi->getAttribute(nextHopId, ipAttribute));
+  SaiMplsNextHopTraits::Attributes::LabelStack labelStackAttribute;
+
+  EXPECT_EQ(
+      std::vector<sai_uint32_t>({1001, 2001, 3001}),
+      nextHopApi->getAttribute(nextHopId, labelStackAttribute));
 }
 
 // IP is create only, so if we try to set it, we expect to fail
