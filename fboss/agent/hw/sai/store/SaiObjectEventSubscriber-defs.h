c411f1519474610ba4e67a02194ca5ebd8dd7698
Parvez Shaikh <pshaikh@fb.com>
2020-03-18 23:39:44 -0700

    sai object event subscriber
    Summary:
    Boiler plate templates for
    1) subscriber interface
    2) single event subscriber
    3) aggregate event subscriber
    
    Subscriber interface: This is used by publisher, it provides minimum things such as
    1) notify subscriber object of create or delete of publisher
    2) save opaque subscription token by publisher
    
    This also provides a utility methods to subclasses to
    1) store weak reference to publisher
    2) maintain publisher attributes (that govern subscriber)
    3) accessors to both
    
    single event subscriber: This is an implementation of interface. It is a facade to more complex aggregate subscriber, which is used by publisher to dispatch notifications as above
    
    aggregate subscriber:  a collection or mixin of  one or more single event subscribers. a facade to more complex subscriber implementation which can be arbitrarily complex. A subscriber implementation can subscribe to more than one publishers using aggregate subscriber, leading to N-to-N relationship between publishers and subscribers. aggregate subscribers main jobs involve
    1) aggregating events from all subscribers
    2) informing subscriber implementation about these events if
    2.1) all publishers are ready
    2.2) any one publisher has died
    
    Additionally aggregate subscriber gives "boiler plate" methods to set and reset subscriber implementation's "SAI" object.
    
    A subclass of this class is a  subscriber implementation which must follow some "protocol" as expected by this  class
    1) aggregate subscriber  informs "subscriber impl object" if all publishers are available
    2) "subscriber impl object" must prepare create attributes & adapter host keys depending on which subscriber's SAI object is being implemented
    3) "subscriber impl object" then invokes "setObject" this constructs a SAI object
    4) aggregate subscriber  informs "subscriber impl object" if any publisher is dying
    5) "subscriber impl object" can update attributes and either update "SAI object" or destroy it.
    
    Reviewed By: boryas
    
    Differential Revision: D20150049
    
    fbshipit-source-id: 9c529d5d64064affcc120b2fb5295c84be90b3dc

---
+++ fboss/agent/hw/sai/store/SaiObjectEventSubscriber-defs.h
@@ -0,0 +1,69 @@
+// Copyright 2004-present Facebook. All Rights Reserved.
+
+#pragma once
+
+#include "fboss/agent/hw/sai/store/SaiObject.h"
+#include "fboss/agent/hw/sai/store/SaiStore.h"
+
+namespace facebook::fboss {
+
+namespace detail {
+template <typename PublishedObjectTrait>
+SaiObjectEventSubscriber<PublishedObjectTrait>::SaiObjectEventSubscriber(
+    typename PublishedObjectTrait::PublisherAttributes attr)
+    : publisherAttrs_(attr) {}
+
+template <typename PublishedObjectTrait>
+SaiObjectEventSubscriber<PublishedObjectTrait>::~SaiObjectEventSubscriber() {}
+
+template <typename PublishedObjectTrait>
+typename SaiObjectEventSubscriber<PublishedObjectTrait>::PublisherObjectWeakPtr
+SaiObjectEventSubscriber<PublishedObjectTrait>::getPublisherObject() const {
+  return publisherObject_;
+}
+
+template <typename PublishedObjectTrait>
+void SaiObjectEventSubscriber<PublishedObjectTrait>::setPublisherObject(
+    PublisherObjectSharedPtr object) {
+  publisherObject_ = object;
+}
+} // namespace detail
+
+template <
+    typename SubscriberImpl,
+    typename SubscriberTraits,
+    typename... PublisherObjectTraits>
+bool SaiObjectEventAggregateSubscriber<
+    SubscriberImpl,
+    SubscriberTraits,
+    PublisherObjectTraits...>::isAlive() const {
+  return object_.get();
+}
+
+template <
+    typename SubscriberImpl,
+    typename SubscriberTraits,
+    typename... PublisherObjectTraits>
+void SaiObjectEventAggregateSubscriber<
+    SubscriberImpl,
+    SubscriberTraits,
+    PublisherObjectTraits...>::resetObject() {
+  object_.reset();
+}
+
+template <
+    typename SubscriberImpl,
+    typename SubscriberTraits,
+    typename... PublisherObjectTraits>
+void SaiObjectEventAggregateSubscriber<
+    SubscriberImpl,
+    SubscriberTraits,
+    PublisherObjectTraits...>::
+    setObject(
+        typename SubscriberTraits::AdapterHostKey key,
+        typename SubscriberTraits::CreateAttributes attr) {
+  auto& store = SaiStore::getInstance()->get<SubscriberTraits>();
+  object_ = store.setObject(key, attr);
+}
+
+} // namespace facebook::fboss
---
+++ fboss/agent/hw/sai/store/SaiObjectEventSubscriber.h
@@ -0,0 +1,196 @@
+// Copyright 2004-present Facebook. All Rights Reserved.
+
+#pragma once
+
+#include <algorithm>
+#include <any>
+#include <memory>
+
+#include "fboss/agent/hw/sai/api/Traits.h"
+#include "fboss/lib/TupleUtils.h"
+
+namespace facebook::fboss {
+
+template <typename>
+class SaiObject;
+
+namespace detail {
+/*
+ * A subscriber interface as used by  publisher
+ * afterCreate and beforeRemove methods are invoked by publishers after and
+ * before publishers are created or removed respectively.
+ * saveSubscription  is invoked by publisher to save subscription.
+ */
+template <typename PublisherObjectTraits>
+struct SaiObjectEventSubscriber {
+  using PublisherObjectSharedPtr =
+      std::shared_ptr<const SaiObject<PublisherObjectTraits>>;
+  using PublisherObjectWeakPtr =
+      std::weak_ptr<const SaiObject<PublisherObjectTraits>>;
+
+  using PublisherAttributes =
+      typename PublisherObjectTraits::PublisherAttributes;
+
+  SaiObjectEventSubscriber(PublisherAttributes attr);
+  virtual ~SaiObjectEventSubscriber();
+  PublisherAttributes getPublisherAttributes() const {
+    return publisherAttrs_;
+  }
+
+  /* return non-owning reference to monitored object */
+  PublisherObjectWeakPtr getPublisherObject() const;
+
+  virtual void afterCreate(PublisherObjectSharedPtr) = 0;
+  virtual void beforeRemove() = 0;
+
+  void saveSubscription(std::any subscription) {
+    subscription_ = std::move(subscription);
+  }
+
+ protected:
+  void setPublisherObject(PublisherObjectSharedPtr object = nullptr);
+
+ private:
+  PublisherAttributes publisherAttrs_;
+  PublisherObjectWeakPtr publisherObject_;
+  // TODO(pshaikh): this is currently maintained as any to break circular
+  // dependencies in object, publisher, and subscriber types investigate and
+  // eliminate this any type with proper type
+  std::any subscription_;
+};
+
+/* A single subscriber for a publisher using particular published object trait.
+ * A single subscriber implements methods of afterCreate and beforeRemove.
+ * Both these methods simply dispatch events from publisher to aggregate
+ * subscriber. In doing so, single subscriber is presenting a facade to a
+ * publisher with published trait to notify an aggregate subscriber.  */
+template <typename AggregateSubscriber, typename PublishedObjectTrait>
+class SaiObjectEventSingleSubscriber
+    : public SaiObjectEventSubscriber<PublishedObjectTrait> {
+ public:
+  using PublisherObjectSharedPtr =
+      std::shared_ptr<const SaiObject<PublishedObjectTrait>>;
+  using PublisherAttributes =
+      typename PublishedObjectTrait::PublisherAttributes;
+  using Base = SaiObjectEventSubscriber<PublishedObjectTrait>;
+  /* Publisher object with PublishedObjectTrait and can be identified by
+   * PublisherAttributes */
+  SaiObjectEventSingleSubscriber(PublisherAttributes monitoredAttrs)
+      : Base{monitoredAttrs} {}
+
+  /* Publisher object created, dispatch call to aggregate subscriber */
+  void afterCreate(PublisherObjectSharedPtr object) override {
+    this->setPublisherObject(object);
+    auto* aggregateSubscriber = static_cast<AggregateSubscriber*>(this);
+    aggregateSubscriber
+        ->template afterCreateNotifyAggregateSubscriber<PublishedObjectTrait>();
+  }
+
+  /* Publisher object removed, dispatch call  to aggregate subscriber */
+  void beforeRemove() override {
+    auto* aggregateSubscriber = static_cast<AggregateSubscriber*>(this);
+    aggregateSubscriber->template beforeRemoveNotifyAggregateSubscriber<
+        PublishedObjectTrait>();
+    this->setPublisherObject(nullptr);
+  }
+};
+} // namespace detail
+
+/*
+ * An aggregate subscriber to aggregate events from more than one publishers
+ * It aggregates all events from all publishers, manages some book keeping about
+ * which publisher are alive. The notification is further dispatched to
+ * implementation of subscribers.
+ * An aggregate subscriber implements following methods
+ *  1) afterCreateNotifyAggregateSubscriber
+ *   - notify subscriber implementation if all publishers are alive
+ *  2) beforeRemoveNotifyAggregateSubscriber
+ *   - notify subscriber implementation if any publisher is dead
+ *  3) setObject
+ *    - A method for subscriber to setup SAI object
+ *  4) resetObject
+ *    - A method for subscriber to remove SAI objet
+ *
+ * A subscriber implementation in implements following methods
+ *   1) createObject
+ *    - create SAI object by assembling attributes from all publishers and
+ *      invoke setObject
+ * 2) removeObject
+ *    - either call createObject with new attributes or reset object
+ */
+template <
+    typename SubscriberImpl,
+    typename SubscriberTraits,
+    typename... PublishedObjectTraits>
+class SaiObjectEventAggregateSubscriber
+    : public detail::SaiObjectEventSingleSubscriber<
+          SaiObjectEventAggregateSubscriber<
+              SubscriberImpl,
+              SubscriberTraits,
+              PublishedObjectTraits...>,
+          PublishedObjectTraits>... {
+ public:
+  static_assert(
+      (IsObjectPublisher<PublishedObjectTraits>::value && ... && true),
+      "requested object is not monitored");
+
+  static constexpr size_t Size = sizeof...(PublishedObjectTraits);
+  using AggregateType = std::tuple<PublishedObjectTraits...>;
+  using AdapterHostKey = typename SubscriberTraits::AdapterHostKey;
+  using CreateAttributes = typename SubscriberTraits::CreateAttributes;
+  using Class = SaiObjectEventAggregateSubscriber<
+      SubscriberImpl,
+      SubscriberTraits,
+      PublishedObjectTraits...>;
+  using SubscriberSharedPtr = std::shared_ptr<SaiObject<SubscriberTraits>>;
+
+  SaiObjectEventAggregateSubscriber(
+      typename PublishedObjectTraits::PublisherAttributes... attrs)
+      : detail::SaiObjectEventSingleSubscriber<Class, PublishedObjectTraits>(
+            attrs)... {}
+
+  template <typename PublishedObjectTrait>
+  void afterCreateNotifyAggregateSubscriber() {
+    auto index = TupleIndex<AggregateType, PublishedObjectTrait>::value;
+    livePublisherObjects_[index] = true;
+    if (allPublishedObjectsAlive()) {
+      // trigger all creation
+      auto tuple = std::make_tuple(
+          detail::SaiObjectEventSingleSubscriber<Class, PublishedObjectTraits>::
+              getPublisherObject()...);
+      auto* subscriber = static_cast<SubscriberImpl*>(this);
+      subscriber->createObject(tuple);
+    }
+  }
+
+  template <typename PublishedObjectTrait>
+  void beforeRemoveNotifyAggregateSubscriber() {
+    auto index = TupleIndex<AggregateType, PublishedObjectTrait>::value;
+    livePublisherObjects_[index] = false;
+    auto tuple = std::make_tuple(
+        detail::SaiObjectEventSingleSubscriber<Class, PublishedObjectTraits>::
+            getPublisherObject()...);
+    auto* subscriber = static_cast<SubscriberImpl*>(this);
+    subscriber->removeObject(index, tuple);
+  }
+
+  bool allPublishedObjectsAlive() const {
+    return std::all_of(
+        livePublisherObjects_.begin(),
+        livePublisherObjects_.end(),
+        [](auto live) { return live; });
+  }
+
+  bool isAlive() const;
+
+ protected:
+  void setObject(AdapterHostKey key, CreateAttributes attr);
+
+  void resetObject();
+
+ private:
+  std::vector<bool> livePublisherObjects_{Size};
+  SubscriberSharedPtr object_;
+};
+
+} // namespace facebook::fboss
