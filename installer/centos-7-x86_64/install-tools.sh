27084fcffdd4f02f42439ee8bfc18bffe85a00b9
Shrikrishna Khare <skhare@fb.com>
2020-04-27 15:19:52 -0700

    Minor fixes to install-tools
    Summary: As titled.
    
    Reviewed By: bkoray
    
    Differential Revision: D21262045
    
    fbshipit-source-id: 30cae8e81ecb2f1f3a992af8020fcbb3582208f3

--- installer/centos-7-x86_64/install-tools.sh
+++ installer/centos-7-x86_64/install-tools.sh
@@ -2,6 +2,7 @@
 
 sudo yum install -y git gcc g++ python-devel rh-python36 openssl*
 sudo yum install -y centos-release-scl-rh devtoolset-8-toolchain
-sudo yum group install "Development Tools"
-sudo yum install rpm-build rpmdevtools
+sudo yum group install "Development Tools" -y
+sudo yum install rpm-build rpmdevtools -y
+sudo yum install python-pip -y
 sudo pip install gitpython
--- installer/centos-7-x86_64/upgrade-kernel.sh
+++ installer/centos-7-x86_64/upgrade-kernel.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
-rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-3.el7.elrepo.noarch.rpm
-yum --enablerepo=elrepo-kernel install kernel-lt kernel-lt-devel
+sudo rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
+sudo rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-3.el7.elrepo.noarch.rpm
+sudo yum --enablerepo=elrepo-kernel install kernel-lt kernel-lt-devel
