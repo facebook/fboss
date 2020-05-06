7ee17593bde1b11e673be6b6330c097d77250d2f
Shrikrishna Khare <skhare@fb.com>
2020-05-05 22:42:38 -0700

    LookupNextHopsAndRouteManager: data structures for classID computation
    Summary:
    Queue-per-host for l2 entry and neighbor entry is already implemented.
    This is towards supporting queue-per-host for route entries (e.g. ip-per-task).
    
    In order to implement 'route inherits classid of one of its reachable next
    hops', we need to maintain several data structures. This diff introduces that.
    See comments in the diff for detailed explanation.
    
    Differential Revision: D21106095
    
    fbshipit-source-id: a5f80454d6b4ce4417434eef96d7db74e3e5201b

--- fboss/agent/LookupClassRouteUpdater.h
+++ fboss/agent/LookupClassRouteUpdater.h
@@ -12,6 +12,9 @@
 #include "fboss/agent/StateObserver.h"
 #include "fboss/agent/state/StateDelta.h"
 
+#include <folly/container/F14Map.h>
+#include <folly/container/F14Set.h>
+
 namespace facebook::fboss {
 
 class LookupClassRouteUpdater : public AutoRegisterStateObserver {
@@ -78,6 +81,95 @@ class LookupClassRouteUpdater : public AutoRegisterStateObserver {
 
   template <typename AddrT>
   void processRouteUpdates(const StateDelta& stateDelta);
+
+  using RidAndCidr = std::pair<RouterID, folly::CIDRNetwork>;
+  using NextHopAndVlan = std::pair<folly::IPAddress, VlanID>;
+  using WithAndWithoutClassIDPrefixes =
+      std::pair<folly::F14FastSet<RidAndCidr>, folly::F14FastSet<RidAndCidr>>;
+
+  /*
+   * We need to maintain nexthop to route mapping so that when a nexthop is
+   * resolved (gets classID), the same classID could be associated with
+   * corresponding routes. However, we are only interested in nexthops that are
+   * part of 'certain' subnets. vlan2SubnetsCache_ maintains that list of
+   * subnets.
+   *   - ports needing queue-per-host have non-empty lookupClasses list.
+   *   - each port belongs to an interface.
+   *   - each interface is part of certain subnet.
+   *   - nexthop IP that would use such a port for egress, would belong to the
+   *     interface subnet.
+   * Thus, we discover and maintain a list of subnets for ports that have
+   * non-emptry lookupClasses list.
+   *
+   * Today, the list of subnets we need to cache is very small. Thus,
+   * folly::F14FastSet is good enough. In future, if we need to cache a large
+   * number of subnets, we could use a Radix tree.
+   */
+  boost::container::flat_map<VlanID, folly::F14FastSet<folly::CIDRNetwork>>
+      vlan2SubnetsCache_;
+
+  /*
+   * Route inherits classID of one of its reachable next hops.
+   *
+   * A route entry points to an egress object, which corresponds to a next hop.
+   * If a route has multiple next hops:
+   *  - route entry points to an ECMP egress object.
+   *  - ECMP egress object points to a list of egress objects.
+   *  - Each egress object in the list corresponds to one next hop.
+   *
+   * However, a classID could only be associated with a route entry, and not
+   * with an egress object (hardware limitation). Thus, if a route has multiple
+   * next hops, we must pick one of the next hops and inherit its classID for
+   * the route.
+   *
+   * Only reachable next hops have classID. Thus, a Route inherits classID of
+   * one of its *reachable* next hops. In the current implementation, we pick
+   * any one reachable next hop and inherit its classID, in future this could
+   * be modified to some other scheme.
+   *
+   * There are several cases to consider:
+   * (1) When a route is resolved:
+   *     (1.1) None of its next hops are reachable: No classID for route.
+   *     (1.2) At least one next hop is reachable: Inherit its classID.
+   * (2) When a route is deleted, clear local data structures.
+   * (3) When route's nexthopset changes:
+   *         Remove oldRoute and add newRoute.
+   * (4) When a next hop's classID is updated:
+   *     (4.1) next hop gets classID (neighbor becomes reachable):
+   *           For every route that has this next hop, if the route does not
+   *           already have a classID, route inherits this classID.
+   *     (4.2) next hop loses classID (neighbor becomes unreachable):
+   *           For every route that has this next hop, find another next hop
+   *           that already has classID, route inherits that classID. If such a
+   *           next hop does not exist, route loses classID.
+   *
+   * This is implemented by maintaining following data structures.
+   * TODO(skhare) add note on how these data structures handle all cases.
+   *
+   * Note that this class computes classIDs for the routes, state update is
+   * scheduled by the caller (LookupClassRouteUpdater).
+   */
+
+  /*
+   * NextHop to prefixes map.
+   *
+   * In theory, same IP may exist in different Vlans, thus maintain IP + Vlan
+   * to prefixes mapping.
+   *
+   * Today, the list of subnets we need to cache is very small. Thus,
+   * folly::F14FastSet is good enough. In future, if we need to cache a large
+   * number of subnets, we could use Radix tree.
+   *
+   * This maintains the list of prefixes that inherit classID from this
+   * [nexthop, vlan] separately from the list of prefixes that don't.
+   */
+  folly::F14FastMap<NextHopAndVlan, WithAndWithoutClassIDPrefixes>
+      nextHopAndVlanToPrefixes_;
+
+  /*
+   * Set of prefixes with classID (from any [nexthop + vlan]).
+   */
+  folly::F14FastSet<RidAndCidr> allPrefixesWithClassID_;
 };
 
 } // namespace facebook::fboss
