cd5774aa08de2e3817f6110cc3ce16d744e49bc8
Ravindra Sunkad <rsunkad@fb.com>
2020-05-12 07:23:41 -0700

    Download kernel RPM from a mirror that doest purge kernel-lt versions
    Summary:
    elrepo.org maintains only the most recent two versions of kernel-lt RPMs
    Change the upgrade script to download from mirror.pit.teraswitch.com instead
    
    Reviewed By: shri-khare
    
    Differential Revision: D21506038
    
    fbshipit-source-id: 9ec5ed67a646e12ede615910627285b4e0388256

--- installer/centos-7-x86_64/upgrade-kernel.sh
+++ installer/centos-7-x86_64/upgrade-kernel.sh
@@ -3,6 +3,7 @@
 # Kernel version for FBOSS build
 LTKERNELVER=4.4.219-1.el7.elrepo.x86_64
 
+# Download kernel RPMs from one of the archives @ https://elrepo.org/tiki/Download
 sudo rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
-sudo rpm -Uvh https://elrepo.org/linux/kernel/el7/x86_64/RPMS/kernel-lt-$LTKERNELVER.rpm
-sudo rpm -Uvh https://elrepo.org/linux/kernel/el7/x86_64/RPMS/kernel-lt-devel-$LTKERNELVER.rpm
+sudo rpm -Uvh http://mirror.pit.teraswitch.com/elrepo/kernel/el7/x86_64/RPMS/kernel-lt-$LTKERNELVER.rpm
+sudo rpm -Uvh http://mirror.pit.teraswitch.com/elrepo/kernel/el7/x86_64/RPMS/kernel-lt-devel-$LTKERNELVER.rpm
