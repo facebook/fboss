78c627c42880450c97cdfe30d55273f30086199a
Jasmeet Bagga <jasmeetbagga@fb.com>
2020-05-13 12:50:50 -0700

    From BcmEcmpGroupShrink to HwEcmpGroupShrink benchmark
    Summary:
    As titled. Building on top of the previous diffs
    move this benchmark up to hw/benchmarks folder.
    
    TODO: get this running for SAI
    
    Differential Revision: D21530228
    
    fbshipit-source-id: 723f245024b017fa20f951e1085a0ece65300885

---
+++ fboss/agent/hw/benchmarks/HwEcmpShrinkSpeedBenchmark.cpp
@@ -0,0 +1,75 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#include "fboss/agent/hw/test/ConfigFactory.h"
+#include "fboss/agent/hw/test/HwLinkStateToggler.h"
+#include "fboss/agent/hw/test/HwSwitchEnsemble.h"
+#include "fboss/agent/hw/test/HwSwitchEnsembleFactory.h"
+#include "fboss/agent/hw/test/HwTestEcmpUtils.h"
+#include "fboss/agent/state/Port.h"
+#include "fboss/agent/test/EcmpSetupHelper.h"
+
+#include <folly/Benchmark.h>
+#include <folly/IPAddress.h>
+
+namespace facebook::fboss {
+
+using utility::getEcmpSizeInHw;
+
+BENCHMARK(HwEcmpGroupShrink) {
+  folly::BenchmarkSuspender suspender;
+  constexpr int kEcmpWidth = 4;
+  static auto ensemble = createHwEnsemble(
+      HwSwitch::PACKET_RX_DESIRED | HwSwitch::LINKSCAN_DESIRED);
+  auto hwSwitch = ensemble->getHwSwitch();
+  auto config =
+      utility::onePortPerVlanConfig(hwSwitch, ensemble->masterLogicalPortIds());
+  ensemble->applyInitialConfig(config);
+  auto ecmpHelper =
+      utility::EcmpSetupAnyNPorts6(ensemble->getProgrammedState());
+  auto ecmpRouteState = ecmpHelper.setupECMPForwarding(
+      ecmpHelper.resolveNextHops(ensemble->getProgrammedState(), kEcmpWidth),
+      kEcmpWidth);
+  ensemble->applyNewState(ecmpRouteState);
+  auto prefix = folly::CIDRNetwork(folly::IPAddress("::"), 0);
+  CHECK_EQ(
+      kEcmpWidth,
+      getEcmpSizeInHw(hwSwitch, prefix, ecmpHelper.getRouterId(), kEcmpWidth));
+
+  auto newState = ensemble->getProgrammedState()->clone();
+  auto newPort = newState->getPorts()
+                     ->getPort(ecmpHelper.ecmpPortDescriptorAt(0).phyPortID())
+                     ->modify(&newState);
+  newPort->setLoopbackMode(cfg::PortLoopbackMode::NONE);
+  ensemble->applyNewState(newState);
+  // We restart benchmarking ASAP *after* we have triggered port down
+  // event above. This is analogous to starting a timer immediately
+  // after a event that triggers a port down event. However, with such
+  // a low interval benchmark its possible that link down handling and
+  // ecmp shrink already occurs while we are restarting the timer. However,
+  // if we ever introduce slowness in ecmp shrink path, we should still
+  // notice it since restarting the timer/benchmark is happening at program
+  // execution speed.
+  // An alternative approach would be to start the timer/benchmark, before
+  // triggering the link down event. Unfortunately, that's not feasible, since
+  // the executing the API call to trigger link down above is relatively slow -
+  // starting the timer before link down increases the benchmark time by order
+  // of magnitude.
+  suspender.dismiss();
+  // Busy loop to see how soon after port down do we shrink ECMP group
+  while (
+      getEcmpSizeInHw(hwSwitch, prefix, ecmpHelper.getRouterId(), kEcmpWidth) !=
+      kEcmpWidth - 1)
+    ;
+  suspender.rehire();
+  ensemble->revertToInitCfgState();
+}
+
+} // namespace facebook::fboss
