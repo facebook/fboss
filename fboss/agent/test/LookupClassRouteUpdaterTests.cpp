275d1b90611b76c2c7684f98684f0142ff94e128
Shrikrishna Khare <skhare@fb.com>
2020-05-15 22:58:42 -0700

    LookupClassRouteUpdaterTests: verify route updates
    Summary:
    Queue-per-host for l2 entry and neighbor entry is already implemented.
    This is towards supporting queue-per-host for route entries (e.g. ip-per-task).
    
    As titled.
    
    Differential Revision: D21558687
    
    fbshipit-source-id: 94bc5441a6798dda796848a006171fbf2dc0252d

--- fboss/agent/test/LookupClassRouteUpdaterTests.cpp
+++ fboss/agent/test/LookupClassRouteUpdaterTests.cpp
@@ -65,14 +65,6 @@ class LookupClassRouteUpdaterTest : public ::testing::Test {
     return PortID(1);
   }
 
-  MacAddress kMacAddress() const {
-    return MacAddress("01:02:03:04:05:06");
-  }
-
-  MacAddress kMacAddress2() const {
-    return MacAddress("01:02:03:04:05:07");
-  }
-
   ClientID kClientID() const {
     return ClientID(1001);
   }
@@ -81,7 +73,19 @@ class LookupClassRouteUpdaterTest : public ::testing::Test {
     return RouterID{0};
   }
 
-  AddrT kGetIpAddress() {
+  InterfaceID kInterfaceID() const {
+    return InterfaceID(1);
+  }
+
+  MacAddress kMacAddressA() const {
+    return MacAddress("01:02:03:04:05:06");
+  }
+
+  MacAddress kMacAddressB() const {
+    return MacAddress("01:02:03:04:05:07");
+  }
+
+  AddrT kIpAddressA() {
     if constexpr (std::is_same_v<AddrT, folly::IPAddressV4>) {
       return IPAddressV4("10.0.0.2");
     } else {
@@ -89,7 +93,7 @@ class LookupClassRouteUpdaterTest : public ::testing::Test {
     }
   }
 
-  AddrT kGetIpAddress2() {
+  AddrT kIpAddressB() {
     if constexpr (std::is_same_v<AddrT, folly::IPAddressV4>) {
       return IPAddressV4("10.0.0.3");
     } else {
@@ -97,11 +101,28 @@ class LookupClassRouteUpdaterTest : public ::testing::Test {
     }
   }
 
-  RoutePrefix<AddrT> kGetRoutePrefix() const {
+  AddrT kIpAddressC() {
+    if constexpr (std::is_same_v<AddrT, folly::IPAddressV4>) {
+      return IPAddressV4("10.0.0.4");
+    } else {
+      return IPAddressV6("2401:db00:2110:3001::0004");
+    }
+  }
+
+  RoutePrefix<AddrT> kroutePrefix1() const {
     if constexpr (std::is_same_v<AddrT, folly::IPAddressV4>) {
       return RoutePrefix<AddrT>{folly::IPAddressV4{"10.1.4.0"}, 24};
     } else {
       return RoutePrefix<AddrT>{folly::IPAddressV6{"2803:6080:d038:3066::"},
+                                64};
+    }
+  }
+
+  RoutePrefix<AddrT> kroutePrefix2() const {
+    if constexpr (std::is_same_v<AddrT, folly::IPAddressV4>) {
+      return RoutePrefix<AddrT>{folly::IPAddressV4{"11.1.4.0"}, 24};
+    } else {
+      return RoutePrefix<AddrT>{folly::IPAddressV6{"2803:6080:d038:3067::"},
                                 64};
     }
   }
@@ -135,18 +156,28 @@ class LookupClassRouteUpdaterTest : public ::testing::Test {
     waitForStateUpdates(sw_);
   }
 
+  void unresolveNeighbor(folly::IPAddress ipAddress) {
+    sw_->getNeighborUpdater()->flushEntry(kVlan(), ipAddress);
+
+    sw_->getNeighborUpdater()->waitForPendingUpdates();
+    waitForBackgroundThread(sw_);
+    waitForStateUpdates(sw_);
+  }
+
   void verifyClassIDHelper(
       RoutePrefix<AddrT> routePrefix,
       std::optional<cfg ::AclLookupClass> classID) {
-    auto state = sw_->getState();
-    auto vlan = state->getVlans()->getVlan(kVlan());
-    auto routeTableRib = state->getRouteTables()
-                             ->getRouteTable(kRid())
-                             ->template getRib<AddrT>();
+    this->verifyStateUpdateAfterNeighborCachePropagation([=]() {
+      auto state = sw_->getState();
+      auto vlan = state->getVlans()->getVlan(kVlan());
+      auto routeTableRib = state->getRouteTables()
+                               ->getRouteTable(kRid())
+                               ->template getRib<AddrT>();
 
-    auto route = routeTableRib->routes()->getRouteIf(routePrefix);
-    XLOG(DBG) << route->str();
-    EXPECT_EQ(route->getClassID(), classID);
+      auto route = routeTableRib->routes()->getRouteIf(routePrefix);
+      XLOG(DBG) << route->str();
+      EXPECT_EQ(route->getClassID(), classID);
+    });
   }
 
   void addRoute(RoutePrefix<AddrT> routePrefix, std::vector<AddrT> nextHops) {
@@ -181,6 +212,27 @@ class LookupClassRouteUpdaterTest : public ::testing::Test {
     waitForStateUpdates(this->sw_);
   }
 
+  void removeNeighbor(const AddrT& ip) {
+    this->updateState(
+        "Add new route", [=](const std::shared_ptr<SwitchState>& state) {
+          auto newState = state->clone();
+
+          VlanID vlanId = newState->getInterfaces()
+                              ->getInterface(this->kInterfaceID())
+                              ->getVlanID();
+          Vlan* vlan = newState->getVlans()->getVlanIf(VlanID(vlanId)).get();
+          auto* neighborTable =
+              vlan->template getNeighborEntryTable<AddrT>().get()->modify(
+                  &vlan, &newState);
+          neighborTable->removeEntry(ip);
+          return newState;
+        });
+    waitForStateUpdates(this->sw_);
+    this->sw_->getNeighborUpdater()->waitForPendingUpdates();
+    waitForBackgroundThread(this->sw_);
+    waitForStateUpdates(this->sw_);
+  }
+
  protected:
   void runInUpdateEventBaseAndWait(Func func) {
     auto* evb = sw_->getUpdateEvb();
@@ -205,49 +257,129 @@ using TestTypes = ::testing::Types<folly::IPAddressV4, folly::IPAddressV6>;
 
 TYPED_TEST_CASE(LookupClassRouteUpdaterTest, TestTypes);
 
-TYPED_TEST(LookupClassRouteUpdaterTest, AddRouteThenResolveNextHop) {
-  this->addRoute(this->kGetRoutePrefix(), {this->kGetIpAddress()});
-  this->resolveNeighbor(this->kGetIpAddress(), this->kMacAddress());
+// Test cases verifying Route changes
 
-  this->verifyStateUpdateAfterNeighborCachePropagation([=]() {
-    this->verifyClassIDHelper(
-        this->kGetRoutePrefix(),
-        cfg::AclLookupClass::CLASS_QUEUE_PER_HOST_QUEUE_0);
-  });
+TYPED_TEST(LookupClassRouteUpdaterTest, AddRouteThenResolveNextHop) {
+  this->addRoute(this->kroutePrefix1(), {this->kIpAddressA()});
+  this->resolveNeighbor(this->kIpAddressA(), this->kMacAddressA());
+
+  this->verifyClassIDHelper(
+      this->kroutePrefix1(), cfg::AclLookupClass::CLASS_QUEUE_PER_HOST_QUEUE_0);
 }
 
 TYPED_TEST(LookupClassRouteUpdaterTest, ResolveNextHopThenAddRoute) {
-  this->resolveNeighbor(this->kGetIpAddress(), this->kMacAddress());
-  this->addRoute(this->kGetRoutePrefix(), {this->kGetIpAddress()});
+  this->resolveNeighbor(this->kIpAddressA(), this->kMacAddressA());
+  this->addRoute(this->kroutePrefix1(), {this->kIpAddressA()});
 
-  this->verifyStateUpdateAfterNeighborCachePropagation([=]() {
-    this->verifyClassIDHelper(
-        this->kGetRoutePrefix(),
-        cfg::AclLookupClass::CLASS_QUEUE_PER_HOST_QUEUE_0);
-  });
+  this->verifyClassIDHelper(
+      this->kroutePrefix1(), cfg::AclLookupClass::CLASS_QUEUE_PER_HOST_QUEUE_0);
+}
+
+TYPED_TEST(LookupClassRouteUpdaterTest, AddRouteThenResolveTwoNextHop) {
+  this->addRoute(
+      this->kroutePrefix1(), {this->kIpAddressA(), this->kIpAddressB()});
+  this->resolveNeighbor(this->kIpAddressA(), this->kMacAddressA());
+  this->resolveNeighbor(this->kIpAddressB(), this->kMacAddressB());
+
+  /*
+   * In the current implementation, route inherits classID of the first
+   * reachable nexthop. This test verifies that.
+   * An accidental change in those semantics would be caught by this test
+   * failure.
+   */
+  this->verifyClassIDHelper(
+      this->kroutePrefix1(), cfg::AclLookupClass::CLASS_QUEUE_PER_HOST_QUEUE_0);
+}
+
+TYPED_TEST(LookupClassRouteUpdaterTest, ResolveTwoNextHopThenAddRoute) {
+  this->resolveNeighbor(this->kIpAddressA(), this->kMacAddressA());
+  this->resolveNeighbor(this->kIpAddressB(), this->kMacAddressB());
+  this->addRoute(
+      this->kroutePrefix1(), {this->kIpAddressA(), this->kIpAddressB()});
+
+  /*
+   * In the current implementation, route inherits classID of the first
+   * reachable nexthop. This test verifies that.
+   * An accidental change in those semantics would be caught by this test
+   * failure.
+   */
+  this->verifyClassIDHelper(
+      this->kroutePrefix1(), cfg::AclLookupClass::CLASS_QUEUE_PER_HOST_QUEUE_0);
 }
 
 TYPED_TEST(LookupClassRouteUpdaterTest, AddRouteThenResolveSecondNextHopOnly) {
   this->addRoute(
-      this->kGetRoutePrefix(), {this->kGetIpAddress(), this->kGetIpAddress2()});
-  this->resolveNeighbor(this->kGetIpAddress2(), this->kMacAddress2());
+      this->kroutePrefix1(), {this->kIpAddressA(), this->kIpAddressB()});
+  this->resolveNeighbor(this->kIpAddressB(), this->kMacAddressB());
 
-  this->verifyStateUpdateAfterNeighborCachePropagation([=]() {
-    this->verifyClassIDHelper(
-        this->kGetRoutePrefix(),
-        cfg::AclLookupClass::CLASS_QUEUE_PER_HOST_QUEUE_0);
-  });
+  this->verifyClassIDHelper(
+      this->kroutePrefix1(), cfg::AclLookupClass::CLASS_QUEUE_PER_HOST_QUEUE_0);
 }
 
 TYPED_TEST(LookupClassRouteUpdaterTest, ResolveSecondNextHopOnlyThenAddRoute) {
-  this->resolveNeighbor(this->kGetIpAddress2(), this->kMacAddress2());
+  this->resolveNeighbor(this->kIpAddressB(), this->kMacAddressB());
   this->addRoute(
-      this->kGetRoutePrefix(), {this->kGetIpAddress(), this->kGetIpAddress2()});
+      this->kroutePrefix1(), {this->kIpAddressA(), this->kIpAddressB()});
 
-  this->verifyStateUpdateAfterNeighborCachePropagation([=]() {
-    this->verifyClassIDHelper(
-        this->kGetRoutePrefix(),
-        cfg::AclLookupClass::CLASS_QUEUE_PER_HOST_QUEUE_0);
-  });
+  this->verifyClassIDHelper(
+      this->kroutePrefix1(), cfg::AclLookupClass::CLASS_QUEUE_PER_HOST_QUEUE_0);
 }
+
+TYPED_TEST(
+    LookupClassRouteUpdaterTest,
+    AddRouteWithTwoNextHopsThenUnresolveFirstNextHop) {
+  this->addRoute(
+      this->kroutePrefix1(), {this->kIpAddressA(), this->kIpAddressB()});
+  this->resolveNeighbor(this->kIpAddressA(), this->kMacAddressA());
+  this->resolveNeighbor(this->kIpAddressB(), this->kMacAddressB());
+
+  /*
+   * In the current implementation, route inherits classID of the first
+   * reachable nexthop. This test verifies that.
+   * An accidental change in those semantics would be caught by this test
+   * failure.
+   */
+  this->verifyClassIDHelper(
+      this->kroutePrefix1(), cfg::AclLookupClass::CLASS_QUEUE_PER_HOST_QUEUE_0);
+
+  this->unresolveNeighbor(this->kIpAddressA());
+  this->verifyClassIDHelper(
+      this->kroutePrefix1(), cfg::AclLookupClass::CLASS_QUEUE_PER_HOST_QUEUE_1);
+}
+
+TYPED_TEST(
+    LookupClassRouteUpdaterTest,
+    AddRouteWithTwoNextHopsThenRemoveFirstNextHop) {
+  this->addRoute(
+      this->kroutePrefix1(), {this->kIpAddressA(), this->kIpAddressB()});
+  this->resolveNeighbor(this->kIpAddressA(), this->kMacAddressA());
+  this->resolveNeighbor(this->kIpAddressB(), this->kMacAddressB());
+
+  /*
+   * In the current implementation, route inherits classID of the first
+   * reachable nexthop. This test verifies that.
+   * An accidental change in those semantics would be caught by this test
+   * failure.
+   */
+  this->verifyClassIDHelper(
+      this->kroutePrefix1(), cfg::AclLookupClass::CLASS_QUEUE_PER_HOST_QUEUE_0);
+
+  this->removeNeighbor(this->kIpAddressA());
+  this->verifyClassIDHelper(
+      this->kroutePrefix1(), cfg::AclLookupClass::CLASS_QUEUE_PER_HOST_QUEUE_1);
+}
+
+TYPED_TEST(LookupClassRouteUpdaterTest, ChangeNextHopSet) {
+  this->addRoute(this->kroutePrefix1(), {this->kIpAddressA()});
+  this->verifyClassIDHelper(this->kroutePrefix1(), std::nullopt);
+
+  this->resolveNeighbor(this->kIpAddressB(), this->kMacAddressB());
+
+  this->addRoute(
+      this->kroutePrefix1(), {this->kIpAddressA(), this->kIpAddressB()});
+
+  this->verifyClassIDHelper(
+      this->kroutePrefix1(), cfg::AclLookupClass::CLASS_QUEUE_PER_HOST_QUEUE_0);
+}
+
 } // namespace facebook::fboss
