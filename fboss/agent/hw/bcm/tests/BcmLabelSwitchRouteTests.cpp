404f638a72b16cf7d2b6a15b3f488a62c1821a36
Sean Choi <schoi@fb.com>
2020-05-28 00:07:23 -0700

    Updating BCM Label Switch Route Test to use utility functions
    Summary: In order to abstract BCMLabelSwitchRouteTest, this diff utilizes the newly added utility functions made in D21440072.
    
    Reviewed By: phshaikh
    
    Differential Revision: D21455948
    
    fbshipit-source-id: 58b5ee70177b3f5c3816d7c43a2c82bda7aaf8fb

--- fboss/agent/hw/bcm/tests/BcmLabelSwitchRouteTests.cpp
+++ fboss/agent/hw/bcm/tests/BcmLabelSwitchRouteTests.cpp
@@ -1,13 +1,9 @@
 // Copyright 2004-present Facebook. All Rights Reserved.
 
-#include "fboss/agent/hw/bcm/BcmAddressFBConvertors.h"
-#include "fboss/agent/hw/bcm/BcmIntf.h"
-#include "fboss/agent/hw/bcm/BcmLabelSwitchingUtils.h"
-#include "fboss/agent/hw/bcm/BcmPortTable.h"
 #include "fboss/agent/hw/bcm/tests/BcmLinkStateDependentTests.h"
-#include "fboss/agent/hw/bcm/tests/BcmMplsTestUtils.h"
 #include "fboss/agent/hw/switch_asics/HwAsic.h"
 #include "fboss/agent/hw/test/ConfigFactory.h"
+#include "fboss/agent/hw/test/HwTestMplsUtils.h"
 #include "fboss/agent/test/EcmpSetupHelper.h"
 
 using namespace ::testing;
@@ -100,151 +96,19 @@ class BcmLabelSwitchRouteTest : public BcmLinkStateDependentTests {
   void verifyLabelSwitchAction(
       LabelForwardingAction::LabelForwardingType action) {
     setupECMPHelper(kTopLabel, action);
-    bcm_mpls_tunnel_switch_t info;
-    bcm_mpls_tunnel_switch_t_init(&info);
-    info.label = kTopLabel;
-    info.port = BCM_GPORT_INVALID;
-    auto rv = bcm_mpls_tunnel_switch_get(getHwSwitch()->getUnit(), &info);
-    ASSERT_EQ(rv, 0);
-    EXPECT_EQ(
-        info.action,
-        utility::getLabelSwitchAction(
-            LabelNextHopEntry::Action::NEXTHOPS, action));
-    auto nexthop = this->getNextHop(0);
-    bcm_mac_t mac;
-    macToBcm(nexthop.mac, &mac);
-    bcm_if_t labelActionIntf;
-    switch (action) {
-      case LabelForwardingAction::LabelForwardingType::SWAP:
-        utility::verifyLabeledEgress(
-            info.egress_if, nexthop.action.swapWith().value());
-        labelActionIntf = getHwSwitch()
-                              ->getIntfTable()
-                              ->getBcmIntf(InterfaceID(utility::kBaseVlanId))
-                              ->getBcmIfId();
-        break;
-
-      case LabelForwardingAction::LabelForwardingType::PUSH: {
-        auto stack = nexthop.action.pushStack().value();
-        auto tunnelStack =
-            LabelForwardingAction::LabelStack{stack.begin() + 1, stack.end()};
-        utility::verifyTunneledEgress(info.egress_if, stack[0], tunnelStack);
-        auto tunnel = getHwSwitch()
-                          ->getIntfTable()
-                          ->getBcmIntf(InterfaceID(utility::kBaseVlanId))
-                          ->getBcmLabeledTunnel(tunnelStack);
-        labelActionIntf = tunnel->getTunnelInterface();
-
-      } break;
-      case LabelForwardingAction::LabelForwardingType::PHP: {
-        labelActionIntf = getHwSwitch()
-                              ->getIntfTable()
-                              ->getBcmIntf(InterfaceID(utility::kBaseVlanId))
-                              ->getBcmIfId();
-      } break;
-      case LabelForwardingAction::LabelForwardingType::POP_AND_LOOKUP:
-      case LabelForwardingAction::LabelForwardingType::NOOP:
-        break;
-    }
-
-    if (action != LabelForwardingAction::LabelForwardingType::POP_AND_LOOKUP &&
-        action != LabelForwardingAction::LabelForwardingType::NOOP) {
-      utility::verifyEgress(
-          info.egress_if,
-          getHwSwitch()->getPortTable()->getBcmPortId(
-              nexthop.portDesc.phyPortID()),
-          mac,
-          labelActionIntf);
-    }
-  }
-
-  void verifyLabeledMultiPath(
-      bcm_if_t egress_if,
-      LabelForwardingAction::LabelForwardingType action) {
-    setupECMPHelper(kTopLabel, action);
-    std::map<
-        bcm_port_t,
-        std::pair<bcm_mpls_label_t, LabelForwardingAction::LabelStack>>
-        stacks;
-    for (auto i = 0; i < kWidth; i++) {
-      auto port = getHwSwitch()->getPortTable()->getBcmPortId(
-          masterLogicalPortIds()[i]);
-      auto nexthop = getNextHop(i);
-      if (!nexthop.action.pushStack()) {
-        stacks.emplace(
-            port,
-            std::make_pair(
-                nexthop.action.swapWith().value(),
-                LabelForwardingAction::LabelStack()));
-      } else {
-        stacks.emplace(
-            port,
-            utility::getEgressLabelAndTunnelStackFromPushStack(
-                nexthop.action.pushStack().value()));
-      }
-    }
-    utility::verifyLabeledMultiPathEgress(0, kWidth, egress_if, stacks);
-  }
-
-  void verifyUnlabeledMultiPath(
-      bcm_if_t egress_if,
-      LabelForwardingAction::LabelForwardingType action) {
-    setupECMPHelper(kTopLabel, action);
-
-    std::vector<bcm_port_t> ports{kWidth};
-    std::vector<bcm_mac_t> macs{kWidth};
-    std::vector<bcm_if_t> intfs{kWidth};
-
-    for (auto i = 0; i < kWidth; i++) {
-      auto nexthop = getNextHop(i);
-      ports[i] = getHwSwitch()->getPortTable()->getBcmPortId(
-          nexthop.portDesc.phyPortID());
-      macToBcm(nexthop.mac, &macs[i]);
-      if (nexthop.action.type() ==
-          LabelForwardingAction::LabelForwardingType::PUSH) {
-        auto stack = nexthop.action.pushStack().value();
-
-        auto tunnelStack =
-            LabelForwardingAction::LabelStack{stack.begin() + 1, stack.end()};
-        intfs[i] = getHwSwitch()
-                       ->getIntfTable()
-                       ->getBcmIntf(InterfaceID(utility::kBaseVlanId))
-                       ->getBcmLabeledTunnel(tunnelStack)
-                       ->getTunnelInterface();
-      } else {
-        intfs[i] = getHwSwitch()
-                       ->getIntfTable()
-                       ->getBcmIntf(InterfaceID(utility::kBaseVlanId))
-                       ->getBcmIfId();
-      }
-    }
-    utility::verifyMultiPathEgress(egress_if, ports, macs, intfs);
+    utility::verifyLabelSwitchAction(
+        getHwSwitch(), kTopLabel, action, this->getNextHop(0));
   }
 
   void verifyMultiPathLabelSwitchAction(
       LabelForwardingAction::LabelForwardingType action) {
     setupECMPHelper(kTopLabel, action);
-    bcm_mpls_tunnel_switch_t info;
-    bcm_mpls_tunnel_switch_t_init(&info);
-    info.label = kTopLabel;
-    info.port = BCM_GPORT_INVALID;
-    auto rv = bcm_mpls_tunnel_switch_get(getHwSwitch()->getUnit(), &info);
-    ASSERT_EQ(rv, 0);
-    EXPECT_EQ(
-        info.action,
-        utility::getLabelSwitchAction(
-            LabelNextHopEntry::Action::NEXTHOPS, action));
-    auto nexthop = this->getNextHop(0);
-    switch (action) {
-      case LabelForwardingAction::LabelForwardingType::SWAP:
-      case LabelForwardingAction::LabelForwardingType::PUSH: {
-        verifyLabeledMultiPath(info.egress_if, action);
-      } break;
-      case LabelForwardingAction::LabelForwardingType::PHP:
-      case LabelForwardingAction::LabelForwardingType::POP_AND_LOOKUP:
-      case LabelForwardingAction::LabelForwardingType::NOOP:
-        break;
+    std::vector<EcmpNextHop> nexthops;
+    for (auto i = 0; i < kWidth; i++) {
+      nexthops.push_back(this->getNextHop(i));
     }
+    utility::verifyMultiPathLabelSwitchAction(
+        getHwSwitch(), kTopLabel, action, nexthops);
   }
 
  private:
