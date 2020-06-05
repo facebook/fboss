28c341c5a30ea20aae85ab132a7966ab91d5bc4e
Alex Eckert <aeckert@fb.com>
2020-06-04 17:25:11 -0700

    add config for qsfp_service
    Summary:
    Preliminary config for qsfp_service. Has ability to control
    command line args and override tx pre-emphasis settings per part
    number.
    
    Differential Revision: D20996153
    
    fbshipit-source-id: d5091baff5b7261f0978a4308edea6acee6134a8

---
+++ fboss/qsfp_service/if/qsfp_service_config.thrift
@@ -0,0 +1,27 @@
+#
+# Copyright 2004-present Facebook. All Rights Reserved.
+#
+namespace cpp2 facebook.fboss.cfg
+namespace py neteng.fboss.qsfp_service_config
+namespace py3 neteng.fboss
+namespace rust facebook.fboss
+
+struct Sff8636Overrides {
+  1: optional i16 rxPreemphasis
+}
+
+struct CmisOverrides {}
+
+union TransceiverOverrides {
+  1: Sff8636Overrides sff,
+  2: CmisOverrides cmis,
+}
+
+struct QsfpServiceConfig {
+  // This is used to override the default command line arguments we
+  // pass to qsfp service.
+  1: map<string, string> defaultCommandLineArgs = {},
+
+  // This has overrides from part number to settings on the optic.
+  2: map<string, TransceiverOverrides> pnOverrides = {},
+}
