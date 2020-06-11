81030ea0bad57f74daa0acd6d13fd4b07f7b5cee
Jasmeet Bagga <jasmeetbagga@fb.com>
2020-06-10 23:14:39 -0700

    For qos transition tests verify both front panel and cpu traffic
    Summary: As titled
    
    Reviewed By: shri-khare
    
    Differential Revision: D21949030
    
    fbshipit-source-id: 2861fd5a603724fcc3301d039c7cc9ac8d19f70b

--- fboss/agent/hw/test/dataplane_tests/Hw2QueueToOlympicQoSTests.cpp
+++ fboss/agent/hw/test/dataplane_tests/Hw2QueueToOlympicQoSTests.cpp
@@ -14,6 +14,7 @@
 #include "fboss/agent/hw/test/dataplane_tests/HwTest2QueueUtils.h"
 #include "fboss/agent/hw/test/dataplane_tests/HwTestOlympicUtils.h"
 #include "fboss/agent/test/EcmpSetupHelper.h"
+#include "fboss/agent/test/ResourceLibUtil.h"
 
 #include "fboss/agent/hw/test/ConfigFactory.h"
 
@@ -22,10 +23,18 @@
 namespace facebook::fboss {
 
 class Hw2QueueToOlympicQoSTest : public HwLinkStateDependentTest {
- protected:
+ private:
+  void SetUp() override {
+    HwLinkStateDependentTest::SetUp();
+    helper_ = std::make_unique<utility::EcmpSetupAnyNPorts6>(
+        getProgrammedState(), RouterID(0));
+  }
   cfg::SwitchConfig initialConfig() const override {
-    auto cfg = utility::oneL3IntfConfig(
-        getHwSwitch(), masterLogicalPortIds()[0], cfg::PortLoopbackMode::MAC);
+    auto cfg = utility::twoL3IntfConfig(
+        getHwSwitch(),
+        masterLogicalPortIds()[0],
+        masterLogicalPortIds()[1],
+        cfg::PortLoopbackMode::MAC);
     /*
      * N.B., On one platform, we have to program qos maps before we program l3
      * interfaces. Even if we enforce that ordering in SaiSwitch, we must still
@@ -39,23 +48,21 @@ class Hw2QueueToOlympicQoSTest : public HwLinkStateDependentTest {
     return cfg;
   }
 
-  template <typename ECMP_HELPER>
-  void setupECMPForwarding(const ECMP_HELPER& ecmpHelper) {
-    auto kEcmpWidthForTest = 1;
-    auto newState = ecmpHelper.setupECMPForwarding(
-        ecmpHelper.resolveNextHops(getProgrammedState(), kEcmpWidthForTest),
-        kEcmpWidthForTest);
+  void setupECMPForwarding() {
+    auto newState = helper_->setupECMPForwarding(
+        helper_->resolveNextHops(getProgrammedState(), kEcmpWidth), kEcmpWidth);
     applyNewState(newState);
   }
 
   std::unique_ptr<facebook::fboss::TxPacket> createUdpPkt(
       uint8_t dscpVal) const {
     auto cpuMac = getPlatform()->getLocalMac();
+    auto srcMac = utility::MacAddressGenerator().get(cpuMac.u64NBO() + 1);
 
     return utility::makeUDPTxPacket(
         getHwSwitch(),
         VlanID(*initialConfig().vlanPorts[0].vlanID_ref()),
-        cpuMac,
+        srcMac,
         cpuMac,
         folly::IPAddressV6("2620:0:1cfe:face:b00c::3"),
         folly::IPAddressV6("2620:0:1cfe:face:b00c::4"),
@@ -64,12 +71,22 @@ class Hw2QueueToOlympicQoSTest : public HwLinkStateDependentTest {
         static_cast<uint8_t>(dscpVal << 2)); // Trailing 2 bits are for ECN
   }
 
-  void sendUdpPkt(int dscpVal) {
-    getHwSwitch()->sendPacketSwitchedSync(createUdpPkt(dscpVal));
+  void sendUdpPkt(int dscpVal, bool frontPanel) {
+    auto txPacket = createUdpPkt(dscpVal);
+    // port is in LB mode, so it will egress and immediately loop back.
+    // Since it is not re-written, it should hit the pipeline as if it
+    // ingressed on the port, and be properly queued.
+    if (frontPanel) {
+      auto outPort = helper_->ecmpPortDescriptorAt(kEcmpWidth).phyPortID();
+      getHwSwitchEnsemble()->ensureSendPacketOutOfPort(
+          std::move(txPacket), outPort);
+    } else {
+      getHwSwitchEnsemble()->ensureSendPacketSwitched(std::move(txPacket));
+    }
   }
-
   void _verifyDscpQueueMappingHelper(
-      const std::map<int, std::vector<uint8_t>>& queueToDscp) {
+      const std::map<int, std::vector<uint8_t>>& queueToDscp,
+      bool frontPanel) {
     for (const auto& entry : queueToDscp) {
       auto queueId = entry.first;
       auto dscpVals = entry.second;
@@ -78,7 +95,7 @@ class Hw2QueueToOlympicQoSTest : public HwLinkStateDependentTest {
         auto beforeQueueOutPkts = getLatestPortStats(masterLogicalPortIds()[0])
                                       .get_queueOutPackets_()
                                       .at(queueId);
-        sendUdpPkt(dscpVal);
+        sendUdpPkt(dscpVal, frontPanel);
         auto afterQueueOutPkts = getLatestPortStats(masterLogicalPortIds()[0])
                                      .get_queueOutPackets_()
                                      .at(queueId);
@@ -87,34 +104,42 @@ class Hw2QueueToOlympicQoSTest : public HwLinkStateDependentTest {
       }
     }
   }
+
+ protected:
+  void runTest(bool frontPanel) {
+    if (!isSupported(HwAsic::Feature::L3_QOS)) {
+      return;
+    }
+
+    auto setup = [=]() { setupECMPForwarding(); };
+
+    auto verify = [=]() {
+      _verifyDscpQueueMappingHelper(utility::k2QueueToDscp(), frontPanel);
+    };
+
+    auto setupPostWarmboot = [=]() {
+      auto newCfg{initialConfig()};
+      utility::addOlympicQueueConfig(&newCfg);
+      utility::addOlympicQosMaps(newCfg);
+      applyNewConfig(newCfg);
+    };
+
+    auto verifyPostWarmboot = [=]() {
+      _verifyDscpQueueMappingHelper(utility::kOlympicQueueToDscp(), frontPanel);
+    };
+
+    verifyAcrossWarmBoots(setup, verify, setupPostWarmboot, verifyPostWarmboot);
+  }
+  static inline constexpr auto kEcmpWidth = 1;
+  std::unique_ptr<utility::EcmpSetupAnyNPorts6> helper_;
 };
 
-TEST_F(Hw2QueueToOlympicQoSTest, VerifyDscpQueueMapping) {
-  if (!isSupported(HwAsic::Feature::L3_QOS)) {
-    return;
-  }
+TEST_F(Hw2QueueToOlympicQoSTest, verifyDscpToQueueMappingCpu) {
+  runTest(false);
+}
 
-  auto setup = [=]() {
-    utility::EcmpSetupAnyNPorts6 ecmpHelper6{getProgrammedState()};
-    setupECMPForwarding(ecmpHelper6);
-  };
-
-  auto verify = [=]() {
-    _verifyDscpQueueMappingHelper(utility::k2QueueToDscp());
-  };
-
-  auto setupPostWarmboot = [=]() {
-    auto newCfg{initialConfig()};
-    utility::addOlympicQueueConfig(&newCfg);
-    utility::addOlympicQosMaps(newCfg);
-    applyNewConfig(newCfg);
-  };
-
-  auto verifyPostWarmboot = [=]() {
-    _verifyDscpQueueMappingHelper(utility::kOlympicQueueToDscp());
-  };
-
-  verifyAcrossWarmBoots(setup, verify, setupPostWarmboot, verifyPostWarmboot);
+TEST_F(Hw2QueueToOlympicQoSTest, verifyDscpToQueueMappingFrontPanel) {
+  runTest(true);
 }
 
 } // namespace facebook::fboss
