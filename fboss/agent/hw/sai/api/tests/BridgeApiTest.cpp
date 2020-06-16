a392815a70721e40496dc796a54b3384b191a85b
Ron He <zecheng@fb.com>
2020-06-16 14:48:25 -0700

    Verify get/set attributes for Bridge APIs
    Summary:
    Verify all attributes available to FBOSS for Bridge entities APIs
    (Bridge, BridgePort)
    
    Reviewed By: shri-khare
    
    Differential Revision: D22007267
    
    fbshipit-source-id: c7e7cc3c5282e06df04c99e8a3248288e0659437

--- fboss/agent/hw/sai/api/tests/BridgeApiTest.cpp
+++ fboss/agent/hw/sai/api/tests/BridgeApiTest.cpp
@@ -25,39 +25,51 @@ class BridgeApiTest : public ::testing::Test {
     sai_api_initialize(0, nullptr);
     bridgeApi = std::make_unique<BridgeApi>();
   }
-  std::shared_ptr<FakeSai> fs;
-  std::unique_ptr<BridgeApi> bridgeApi;
-  void checkBridge(BridgeSaiId bridgeId) {
+
+  BridgeSaiId createBridge(const sai_bridge_type_t bridgeType) const {
+    return bridgeApi->create<SaiBridgeTraits>({bridgeType}, 0);
+  }
+
+  BridgePortSaiId createBridgePort(
+      const sai_bridge_port_type_t bridgePortType,
+      const sai_bridge_port_fdb_learning_mode_t bridgePortFdbLearningMode)
+      const {
+    SaiBridgePortTraits::Attributes::PortId bridgePortId{42};
+    SaiBridgePortTraits::Attributes::AdminState bridgePortAdminState{true};
+    SaiBridgePortTraits::CreateAttributes c{bridgePortType,
+                                            bridgePortId,
+                                            bridgePortAdminState,
+                                            bridgePortFdbLearningMode};
+    return bridgeApi->create<SaiBridgePortTraits>(c, 0);
+  }
+
+  void checkBridge(BridgeSaiId bridgeId) const {
     auto fb = fs->bridgeManager.get(bridgeId);
     EXPECT_EQ(fb.id, bridgeId);
   }
-  void checkBridgePort(BridgePortSaiId bridgePortId) {
+  void checkBridgePort(BridgePortSaiId bridgePortId) const {
     auto fbp = fs->bridgeManager.getMember(bridgePortId);
     EXPECT_EQ(fbp.id, bridgePortId);
   }
+
+  std::shared_ptr<FakeSai> fs;
+  std::unique_ptr<BridgeApi> bridgeApi;
 };
 
 TEST_F(BridgeApiTest, createBridge) {
-  SaiBridgeTraits::Attributes::Type bridgeType(SAI_BRIDGE_TYPE_1Q);
-  auto bridgeId = bridgeApi->create<SaiBridgeTraits>({bridgeType}, 0);
+  auto bridgeId = createBridge(SAI_BRIDGE_TYPE_1Q);
   checkBridge(bridgeId);
 }
 
 TEST_F(BridgeApiTest, createBridgePort) {
-  SaiBridgePortTraits::CreateAttributes c{SAI_BRIDGE_PORT_TYPE_PORT,
-                                          42,
-                                          true,
-                                          SAI_BRIDGE_PORT_FDB_LEARNING_MODE_HW};
-  auto bridgePortId = bridgeApi->create<SaiBridgePortTraits>(c, 0);
+  auto bridgePortId = createBridgePort(
+      SAI_BRIDGE_PORT_TYPE_PORT, SAI_BRIDGE_PORT_FDB_LEARNING_MODE_HW);
   checkBridgePort(bridgePortId);
 }
 
 TEST_F(BridgeApiTest, removeBridgePort) {
-  SaiBridgePortTraits::CreateAttributes c{SAI_BRIDGE_PORT_TYPE_PORT,
-                                          42,
-                                          true,
-                                          SAI_BRIDGE_PORT_FDB_LEARNING_MODE_HW};
-  auto bridgePortId = bridgeApi->create<SaiBridgePortTraits>(c, 0);
+  auto bridgePortId = createBridgePort(
+      SAI_BRIDGE_PORT_TYPE_PORT, SAI_BRIDGE_PORT_FDB_LEARNING_MODE_HW);
   checkBridgePort(bridgePortId);
   EXPECT_EQ(fs->bridgeManager.get(0).fm().map().size(), 1);
   bridgeApi->remove(bridgePortId);
@@ -68,8 +80,7 @@ TEST_F(BridgeApiTest, bridgeCount) {
   uint32_t count = getObjectCount<SaiBridgeTraits>(0);
   // default bridge
   EXPECT_EQ(count, 1);
-  SaiBridgeTraits::Attributes::Type bridgeType(SAI_BRIDGE_TYPE_1Q);
-  bridgeApi->create<SaiBridgeTraits>({bridgeType}, 0);
+  createBridge(SAI_BRIDGE_TYPE_1Q);
   count = getObjectCount<SaiBridgeTraits>(0);
   EXPECT_EQ(count, 2);
 }
@@ -77,11 +88,89 @@ TEST_F(BridgeApiTest, bridgeCount) {
 TEST_F(BridgeApiTest, bridgePortCount) {
   uint32_t count = getObjectCount<SaiBridgePortTraits>(0);
   EXPECT_EQ(count, 0);
-  SaiBridgePortTraits::CreateAttributes c{SAI_BRIDGE_PORT_TYPE_PORT,
-                                          42,
-                                          true,
-                                          SAI_BRIDGE_PORT_FDB_LEARNING_MODE_HW};
-  bridgeApi->create<SaiBridgePortTraits>(c, 0);
+  createBridgePort(
+      SAI_BRIDGE_PORT_TYPE_PORT, SAI_BRIDGE_PORT_FDB_LEARNING_MODE_HW);
   count = getObjectCount<SaiBridgePortTraits>(0);
   EXPECT_EQ(count, 1);
+}
+
+TEST_F(BridgeApiTest, getBridgeAttribute) {
+  auto bridgeId = createBridge(SAI_BRIDGE_TYPE_1Q);
+  checkBridge(bridgeId);
+
+  auto bridgeTypeGot =
+      bridgeApi->getAttribute(bridgeId, SaiBridgeTraits::Attributes::Type());
+  EXPECT_EQ(bridgeTypeGot, SAI_BRIDGE_TYPE_1Q);
+
+  EXPECT_THROW(
+      bridgeApi->getAttribute(
+          bridgeId, SaiBridgeTraits::Attributes::PortList()),
+      SaiApiError);
+}
+
+TEST_F(BridgeApiTest, setBridgeAttribute) {
+  auto bridgeId = createBridge(SAI_BRIDGE_TYPE_1Q);
+  checkBridge(bridgeId);
+
+  SaiBridgeTraits::Attributes::Type type{SAI_BRIDGE_TYPE_1Q};
+  SaiBridgeTraits::Attributes::PortList portList{};
+
+  // SAI spec does not support setting any attribute for bridge post
+  // creation.
+  EXPECT_THROW(bridgeApi->setAttribute(bridgeId, type), SaiApiError);
+  EXPECT_THROW(bridgeApi->setAttribute(bridgeId, portList), SaiApiError);
+}
+
+TEST_F(BridgeApiTest, getBridgePortAttribute) {
+  auto bridgePortId = createBridgePort(
+      SAI_BRIDGE_PORT_TYPE_PORT, SAI_BRIDGE_PORT_FDB_LEARNING_MODE_HW);
+  checkBridgePort(bridgePortId);
+
+  auto bridgePortBridgeIdGot = bridgeApi->getAttribute(
+      bridgePortId, SaiBridgePortTraits::Attributes::BridgeId());
+  auto bridgePortIdGot = bridgeApi->getAttribute(
+      bridgePortId, SaiBridgePortTraits::Attributes::PortId());
+  auto bridgePortTypeGot = bridgeApi->getAttribute(
+      bridgePortId, SaiBridgePortTraits::Attributes::Type());
+  auto bridgePortFdbLearningModeGot = bridgeApi->getAttribute(
+      bridgePortId, SaiBridgePortTraits::Attributes::FdbLearningMode());
+  auto bridgePortAdminStateGot = bridgeApi->getAttribute(
+      bridgePortId, SaiBridgePortTraits::Attributes::AdminState());
+
+  EXPECT_EQ(bridgePortBridgeIdGot, 0);
+  EXPECT_EQ(bridgePortIdGot, 42);
+  EXPECT_EQ(bridgePortTypeGot, SAI_BRIDGE_PORT_TYPE_PORT);
+  EXPECT_EQ(bridgePortFdbLearningModeGot, SAI_BRIDGE_PORT_FDB_LEARNING_MODE_HW);
+  EXPECT_EQ(bridgePortAdminStateGot, true);
+}
+
+TEST_F(BridgeApiTest, setBridgePortAttribute) {
+  auto bridgePortId = createBridgePort(
+      SAI_BRIDGE_PORT_TYPE_PORT, SAI_BRIDGE_PORT_FDB_LEARNING_MODE_HW);
+  checkBridgePort(bridgePortId);
+
+  SaiBridgePortTraits::Attributes::FdbLearningMode fdbLearningMode{
+      SAI_BRIDGE_PORT_FDB_LEARNING_MODE_DISABLE};
+  SaiBridgePortTraits::Attributes::AdminState adminState{false};
+
+  bridgeApi->setAttribute(bridgePortId, fdbLearningMode);
+  bridgeApi->setAttribute(bridgePortId, adminState);
+
+  auto bridgePortFdbLearningModeGot = bridgeApi->getAttribute(
+      bridgePortId, SaiBridgePortTraits::Attributes::FdbLearningMode());
+  auto bridgePortAdminStateGot = bridgeApi->getAttribute(
+      bridgePortId, SaiBridgePortTraits::Attributes::AdminState());
+
+  EXPECT_EQ(
+      bridgePortFdbLearningModeGot, SAI_BRIDGE_PORT_FDB_LEARNING_MODE_DISABLE);
+  EXPECT_EQ(bridgePortAdminStateGot, false);
+
+  // SAI spec does not support setting portId and type for bridge port post
+  // creation.
+
+  SaiBridgePortTraits::Attributes::PortId portId{101010}; // 42 in binary
+  SaiBridgePortTraits::Attributes::Type type{SAI_BRIDGE_PORT_TYPE_SUB_PORT};
+
+  EXPECT_THROW(bridgeApi->setAttribute(bridgePortId, portId), SaiApiError);
+  EXPECT_THROW(bridgeApi->setAttribute(bridgePortId, type), SaiApiError);
 }
