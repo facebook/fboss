91fe7a2a5346b1eb25193fc9496977f8b6fb6dec
Jasmeet Bagga <jasmeetbagga@fb.com>
2020-06-22 22:30:34 -0700

    Support disableTtl decrement utility when on SAI >= 1.6.0
    Summary: As titled
    
    Reviewed By: shri-khare
    
    Differential Revision: D22155951
    
    fbshipit-source-id: 341187d4f2ca6bed15b0e6f2e7b7f86fea92bf84

--- fboss/agent/hw/sai/hw_test/HwTestQosUtils.cpp
+++ fboss/agent/hw/sai/hw_test/HwTestQosUtils.cpp
@@ -12,13 +12,32 @@
 
 #include "fboss/agent/FbossError.h"
 
+#include "fboss/agent/hw/sai/api/NextHopApi.h"
+#include "fboss/agent/hw/sai/api/SaiApiTable.h"
+#include "fboss/agent/hw/sai/switch/SaiManagerTable.h"
+#include "fboss/agent/hw/sai/switch/SaiNextHopManager.h"
+#include "fboss/agent/hw/sai/switch/SaiRouterInterfaceManager.h"
+#include "fboss/agent/hw/sai/switch/SaiSwitch.h"
+
 namespace facebook::fboss::utility {
 
 void disableTTLDecrements(
-    HwSwitch* /*hw*/,
-    RouterID /*routerId*/,
-    InterfaceID /*intf*/,
-    const folly::IPAddress& /*nhop*/) {
-  throw FbossError("Disable TTL decrement not iimplemented");
+    HwSwitch* hw,
+    RouterID routerId,
+    InterfaceID intf,
+    const folly::IPAddress& nhopIp) {
+#if SAI_API_VERSION >= SAI_VERSION(1, 6, 0)
+  auto managerTable = static_cast<SaiSwitch*>(hw)->managerTable();
+  auto rintfHandle =
+      managerTable->routerInterfaceManager().getRouterInterfaceHandle(intf);
+  SaiIpNextHopTraits::AdapterHostKey key(
+      rintfHandle->routerInterface->adapterKey(), nhopIp);
+  auto nhop = managerTable->nextHopManager().getNextHop(key);
+  SaiIpNextHopTraits::Attributes::DisableTtlDecrement disableTtl{true};
+  SaiApiTable::getInstance()->nextHopApi().setAttribute(
+      nhop->getSaiObject()->adapterKey(), disableTtl);
+#else
+  throw FbossError("Disable TTL decrement for nhops is not supported");
+#endif
 }
 } // namespace facebook::fboss::utility
