032144b8789e405aa7dc506aa7601f5f04e79343
Jasmeet Bagga <jasmeetbagga@fb.com>
2020-07-10 19:22:27 -0700

    Wrap buffer profile API
    Summary: As titled
    
    Reviewed By: shri-khare
    
    Differential Revision: D22438388
    
    fbshipit-source-id: e5831b5f6eb5489ab1e27a0ade1830d90d72b716

--- fboss/agent/hw/sai/api/BufferApi.h
+++ fboss/agent/hw/sai/api/BufferApi.h
@@ -149,6 +149,27 @@ class BufferApi : public SaiApi<BufferApi> {
       const sai_stat_id_t* counter_ids) const {
     return api_->clear_buffer_pool_stats(key, num_of_counters, counter_ids);
   }
+
+  sai_status_t _create(
+      BufferProfileSaiId* id,
+      sai_object_id_t switch_id,
+      size_t count,
+      sai_attribute_t* attr_list) {
+    return api_->create_buffer_profile(
+        rawSaiId(id), switch_id, count, attr_list);
+  }
+  sai_status_t _remove(BufferProfileSaiId id) {
+    return api_->remove_buffer_profile(id);
+  }
+  sai_status_t _getAttribute(BufferProfileSaiId key, sai_attribute_t* attr)
+      const {
+    return api_->get_buffer_profile_attribute(key, 1, attr);
+  }
+  sai_status_t _setAttribute(
+      BufferProfileSaiId key,
+      const sai_attribute_t* attr) {
+    return api_->set_buffer_profile_attribute(key, attr);
+  }
   sai_buffer_api_t* api_;
   friend class SaiApi<BufferApi>;
 };
