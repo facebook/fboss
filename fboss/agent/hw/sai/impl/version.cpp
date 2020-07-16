e267576e476b6a2e8a97b9ee3baa8393ddf7b3da
Parvez Shaikh <pshaikh@fb.com>
2020-07-16 14:42:37 -0700

    getSaiImplVersion a method to get sai version
    Summary: Providing a method in SAI implementation libraries which will provide more detailed versioning information for SAI.
    
    Differential Revision: D22543172
    
    fbshipit-source-id: f56f936e0e1e12ccc1af35bdd8afc993dac2bb8d

---
+++ fboss/agent/hw/sai/impl/version.cpp
@@ -0,0 +1,9 @@
+// Copyright 2004-present Facebook. All Rights Reserved.
+
+#include "fboss/agent/hw/sai/impl/version.h"
+
+namespace facebook::fboss {
+const char* getSaiImpVersion() {
+  return "sai";
+}
+} // namespace facebook::fboss
---
+++ fboss/agent/hw/sai/impl/version.h
@@ -0,0 +1,9 @@
+// Copyright 2004-present Facebook. All Rights Reserved.
+
+#pragma once
+
+namespace facebook::fboss {
+
+const char* getSaiImpVersion();
+
+} // namespace facebook::fboss
