005881756d85c760e8302ebb1f80bef99821dfc3
Ron He <zecheng@fb.com>
2020-07-17 09:24:22 -0700

    Use buck to build the generated C code
    Summary: Use buck to build the sai replayer generated C code.
    
    Reviewed By: phshaikh
    
    Differential Revision: D22511615
    
    fbshipit-source-id: a7018a8ae0f0f2bab31b620f8f8e055ccff7e445

---
+++ fboss/agent/hw/sai/tracer/run/saiLog.cpp
@@ -0,0 +1,21 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#include "fboss/agent/hw/sai/tracer/run/saiLog.h"
+
+// This file will be replaced by the generated code from SAI tracer.
+// It will contain API initialization steps as well as the SAI API calls.
+// Once replaced, we'll build the target and then copy it to switch
+// so that it can replay all SAI API calls.
+namespace facebook::fboss {
+
+void run_trace() {}
+
+} // namespace facebook::fboss
---
+++ fboss/agent/hw/sai/tracer/run/saiLog.h
@@ -0,0 +1,28 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+extern "C" {
+#include <sai.h>
+}
+
+namespace facebook::fboss {
+
+// The run_tracer function in saiLog.cpp will be replaced by the generated
+// code captured by sai tracer. It will auto generate API calls and
+// initialization steps to be replayed.
+
+void run_trace();
+
+} // namespace facebook::fboss
