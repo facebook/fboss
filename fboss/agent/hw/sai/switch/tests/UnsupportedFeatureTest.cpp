9f9f3427eeee023672e8619200e44743f58accc9
Jasmeet Bagga <jasmeetbagga@fb.com>
2020-07-22 13:51:52 -0700

    UTs for processing unsupported feature delta in SaiSwitch
    Summary: As titled. Start with QCM, more to come
    
    Reviewed By: shri-khare
    
    Differential Revision: D22618631
    
    fbshipit-source-id: aa4618d63af528dc1d6d5db9d7e8307d95a0cda5

---
+++ fboss/agent/hw/sai/switch/tests/UnsupportedFeatureTest.cpp
@@ -0,0 +1,30 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+#include "fboss/agent/FbossError.h"
+#include "fboss/agent/hw/sai/switch/tests/ManagerTestBase.h"
+#include "fboss/agent/state/StateDelta.h"
+#include "fboss/agent/state/SwitchSettings.h"
+#include "fboss/agent/state/SwitchState.h"
+
+#include "fboss/agent/types.h"
+
+#include <string>
+
+using namespace facebook::fboss;
+
+TEST_F(ManagerTestBase, checkQcmSupport) {
+  auto oldState = std::make_shared<SwitchState>();
+  oldState->publish();
+  auto newState = oldState->clone();
+  auto newSwitchSettings = newState->getSwitchSettings()->clone();
+  newSwitchSettings->setQcmEnable(true);
+  newState->resetSwitchSettings(newSwitchSettings);
+  EXPECT_THROW(stateChanged(oldState, newState), FbossError);
+}
