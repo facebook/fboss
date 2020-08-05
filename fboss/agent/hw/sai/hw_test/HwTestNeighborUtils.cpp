761d3a03389b1e22cf748780448ab490eb5c79c8
Jasmeet Bagga <jasmeetbagga@fb.com>
2020-08-04 21:28:06 -0700

    Fill in nbrProgrammedToCpu for SAI
    Summary:
    In SAI we don't program pending entries,
    so look for missing nbr addreess as evidence of
    being programmed to CPU.
    TODO: Look in programmed state to confirm that
    this is a in subnet IP
    
    Differential Revision: D22892457
    
    fbshipit-source-id: 555fb28a04fe0371a8d94ef0e95e80fbfd369c95

--- fboss/agent/hw/sai/hw_test/HwTestNeighborUtils.cpp
+++ fboss/agent/hw/sai/hw_test/HwTestNeighborUtils.cpp
@@ -10,21 +10,54 @@
 
 #include "fboss/agent/hw/test/HwTestNeighborUtils.h"
 
+#include "fboss/agent/hw/sai/api/NeighborApi.h"
+#include "fboss/agent/hw/sai/switch/SaiManagerTable.h"
+#include "fboss/agent/hw/sai/switch/SaiNeighborManager.h"
+#include "fboss/agent/hw/sai/switch/SaiRouterInterfaceManager.h"
+#include "fboss/agent/hw/sai/switch/SaiSwitch.h"
+#include "fboss/agent/hw/sai/switch/SaiSwitchManager.h"
+
 #include "fboss/agent/FbossError.h"
 
-namespace facebook::fboss::utility {
+namespace facebook::fboss {
+
+namespace {
+const SaiRouterInterfaceHandle* getRouterInterfaceHandle(
+    const SaiManagerTable* managerTable,
+    InterfaceID intf) {
+  return managerTable->routerInterfaceManager().getRouterInterfaceHandle(intf);
+}
+
+const SaiNeighborHandle* getNbrHandle(
+    const SaiManagerTable* managerTable,
+    InterfaceID intf,
+    const folly::IPAddress& ip) {
+  auto rintfHandle = getRouterInterfaceHandle(managerTable, intf);
+  SaiNeighborTraits::NeighborEntry entry(
+      managerTable->switchManager().getSwitchSaiId(),
+      rintfHandle->routerInterface->adapterKey(),
+      ip);
+  return managerTable->neighborManager().getNeighborHandle(entry);
+}
+} // namespace
+
+namespace utility {
 
 bool nbrProgrammedToCpu(
-    const HwSwitch* /*hwSwitch*/,
-    InterfaceID /*intf*/,
-    const folly::IPAddress& /*ip*/) {
-  throw facebook::fboss::FbossError("Not imiplemented");
+    const HwSwitch* hwSwitch,
+    InterfaceID intf,
+    const folly::IPAddress& ip) {
+  auto managerTable = static_cast<const SaiSwitch*>(hwSwitch)->managerTable();
+  auto nbrHandle = getNbrHandle(managerTable, intf, ip);
+  // TODO Lookup interface route and confirm that it points to CPU nhop
+  return nbrHandle == nullptr;
 }
 
 std::optional<uint32_t> getNbrClassId(
     const HwSwitch* /*hwSwitch*/,
     InterfaceID /*intf*/,
     const folly::IPAddress& /*ip*/) {
-  throw facebook::fboss::FbossError("Not imiplemented");
+  return 0;
 }
-} // namespace facebook::fboss::utility
+} // namespace utility
+} // namespace facebook::fboss
