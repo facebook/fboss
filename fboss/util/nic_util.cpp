2b9ce6e6f5acbb068253672a7f4b273425959fbc
Rajan Kumar <rajank@fb.com>
2020-08-19 13:24:16 -0700

    Adding nic_util utility for debugging eth0 NIC (Intel i210) in FBOSS switch
    Summary:
    These are the CLI to read/write the Intel NIC i210 MAC and PHY register read/writes. These CLI can run on platforms which have Intel i210 NIC like: Wedge100, Minipack etc.
    
    nic_util --nic_read_reg --reg_addr <register-address>
    
    nic_util --nic_write_reg --reg_addr <register-address> --reg_val <data-value>
    
    nic_util --nic_phy_read_reg --phy_reg_addr <register-address> [--phy_page <phy-page-id>]
    
    nic_util --nic_phy_write_reg --phy_reg_addr <register-address> --phy_reg_val <data-value> [--phy_page <phy-page-id>]
    
    nic_util --nic_link_show_config
    
    nic_util --nic_link_show_status
    
    Differential Revision: D22280004
    
    fbshipit-source-id: 5d14b710dfb022075cde10cbe8ad8743fdbc9571

---
+++ fboss/util/nic_util.cpp
@@ -0,0 +1,454 @@
+// Copyright 2004-present Facebook. All Rights Reserved.
+
+#include <fcntl.h>
+#include <folly/init/Init.h>
+#include <gflags/gflags.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sysexits.h>
+#include <unistd.h>
+
+DEFINE_bool(nic_read_reg, false,
+  "Read register from NIC i210, use with --reg_addr");
+DEFINE_int32(reg_addr, 0,
+  "NIC i210 register address");
+DEFINE_bool(nic_write_reg, false,
+  "Write register from NIC i210, use with --reg_addr and --reg_val");
+DEFINE_uint32(reg_val, 0,
+  "NIC i210 register value");
+
+DEFINE_bool(nic_phy_read_reg, false,
+  "Read register from NIC i210 PHY, use with --phy_reg_addr and --phy_page");
+DEFINE_int32(phy_reg_addr, 0,
+  "NIC i210 PHY register address");
+DEFINE_bool(nic_phy_write_reg, false,
+  "Write register from NIC i210 PHY, use with --phy_reg_addr, --phy_page and phy_reg_val");
+DEFINE_uint32(phy_reg_val, 0,
+  "NIC i210 PHY register value");
+DEFINE_uint32(phy_page, -1,
+  "NIC i210 PHY Page id");
+
+DEFINE_bool(nic_link_show_config, false,
+  "Read link config from NIC i210");
+DEFINE_bool(nic_link_show_status, false,
+  "Read link status from NIC i210");
+
+constexpr uint16_t kMdiControlRegister            = 0x0020;
+
+/*
+ * map_info_s
+ *
+ * Structure to keep NIC i210 device address mapping to user address space
+ */
+struct nic_info_s {
+  unsigned int mapSize;
+  unsigned char *mapAddr;
+};
+
+/*
+ * nicReadRegInternal
+ *
+ * Read the NIC i210 register. The register address is 16 bit value, the data
+ * read is 32 bit value. These could be MAC, PCS, PCIe block registers.
+ */
+unsigned int nicReadRegInternal(struct nic_info_s *pNic, uint32_t regAddr) {
+
+  unsigned char *ptr = pNic->mapAddr;
+  if (regAddr >= pNic->mapSize) {
+    return 0;
+  }
+  ptr += regAddr;
+  return *(unsigned int *) ptr;
+}
+
+/*
+ * nicWriteRegInternal
+ *
+ * Write the NIC i210 register. The register address is 16 bit value, the data
+ * is 32 bit value. These could be MAC, PCS, PCIe block registers.
+ */
+void nicWriteRegInternal(struct nic_info_s *pNic, uint32_t regAddr, uint32_t regVal) {
+
+  unsigned char *ptr = pNic->mapAddr;
+  if (regAddr >= pNic->mapSize) {
+    return;
+  }
+  ptr += regAddr;
+  *(unsigned int*)ptr = regVal;
+}
+
+/*
+ * nicShowReg
+ *
+ * Read and displays the NIC i210 register. The register address is 16 bit
+ * value, the data read is 32 bit value.
+ */
+bool nicShowReg(struct nic_info_s *pNic, uint32_t regAddr) {
+
+  unsigned char *ptr = pNic->mapAddr;
+  printf("Map address: 0x%lx\n", (unsigned long)ptr);
+  unsigned int regVal = nicReadRegInternal(pNic, regAddr);
+  printf("Reg 0x%x = 0x%.8x\n",  regAddr, regVal);
+  return true;
+}
+
+/*
+ * nicWriteReg
+ *
+ * Writes the NIC i210 register. The register address is 16 bit value, the data
+ * read is 32 bit value. These could be MAC, PCS, PCIe block registers.
+ */
+bool nicWriteReg(struct nic_info_s *pNic, uint32_t regAddr, uint32_t regVal) {
+
+  unsigned char *ptr = pNic->mapAddr;
+  printf("Map address: 0x%lx\n", (unsigned long)ptr);
+  nicWriteRegInternal(pNic, regAddr, regVal);
+  regVal = nicReadRegInternal(pNic, regAddr);
+  printf("Reg 0x%x = 0x%.8x\n",  regAddr, regVal);
+  return true;
+}
+
+/*
+ * waitForMdioCompletion
+ *
+ * This function waits for the MDIO transaction completion and then returns
+ * the MDIO control register word
+ */
+uint32_t waitForMdioCompletion(struct nic_info_s *pNic) {
+
+  uint32_t mdioRegVal;
+  uint32_t iteration=0, maxIterations=100;
+
+  unsigned char *ptr = pNic->mapAddr + 0x20;
+
+  // Keep reading MDIO control register till bit 28 (Ready Bit) becomes 1
+  mdioRegVal = *(unsigned int*)ptr;
+  while((mdioRegVal & (1<<28))==0 && (iteration < maxIterations)) {
+    //printf("mdioRegVal 0x%x 0x%x\n", mdioRegVal, (mdioRegVal & (1<<28)));
+    usleep(10000);
+    mdioRegVal = *(unsigned int*)ptr;
+    iteration++;
+  }
+  return mdioRegVal;
+}
+
+/*
+ * nicPhyReadReg
+ *
+ * Reads the NIC i210 internal Phy register. The Phy register is internally
+ * connected using MDIO. The phy registers are standard clause 22 page based
+ * registers. The phy register address is 8 bit value, the data read is 16 bit
+ * value. The 'phyPage' is optional parameter and if user does not tell the
+ * phy page id then the read is done from existing page register.
+ */
+bool nicPhyReadReg(
+  struct nic_info_s *pNic,
+  uint32_t phyRegAddr,
+  uint32_t phyPage) {
+
+  uint32_t mdioRegVal;
+
+  unsigned char *ptr = pNic->mapAddr;
+  printf("Map address: 0x%lx\n", (unsigned long)ptr);
+
+  // NIC register 0x20 is MDIO control register
+  ptr += kMdiControlRegister;
+
+  // If phy_page is specified then first set the page id in phy register 22
+  if (phyPage >= 0) {
+    // OP (27..26): 1 (MDIO write)
+    // PhyReg (20..16): 22 (page id)
+    // DataVal (15..0): phyPage
+    mdioRegVal = (1 << 26) | (22 << 16) | phyPage;
+    // Write MDIO control register
+    *(unsigned int*)ptr = mdioRegVal;
+
+    // Keep reading MDIO control register till bit 28 (Ready Bit) becomes 1
+    mdioRegVal = waitForMdioCompletion(pNic);
+  }
+
+  // Now read the actual PHY rgister
+
+  // OP (27..26): 2 (MDIO read)
+  // PhyReg (20..16): phyRegAddr
+  mdioRegVal = (2 << 26) | (phyRegAddr << 16);
+  // Write MDIO control register
+  *(unsigned int*)ptr = mdioRegVal;
+
+  // Keep reading MDIO control register till bit 28 (Ready Bit) becomes 1
+  mdioRegVal = waitForMdioCompletion(pNic);
+
+  printf("PHY Reg 0x%x = 0x%.4x\n",
+         (mdioRegVal>>16) & 0x1f, mdioRegVal & 0xffff);
+  return true;
+}
+
+/*
+ * nicPhyWriteReg
+ *
+ * Writes the NIC i210 internal Phy register. The Phy register is internally
+ * connected using MDIO. The phy registers are standard clause 22 page based
+ * registers. The phy register address is 8 bit value, the data read is 16 bit
+ * value. The 'phyPage' is optional parameter and if user does not tell the
+ * phy page id then the read is done from existing page register.
+ */
+bool nicPhyWriteReg(
+    struct nic_info_s *pNic,
+    uint32_t phyRegAddr,
+    uint32_t phyRegVal,
+    uint32_t phyPage) {
+
+  uint32_t mdioRegVal;
+  uint32_t iteration=0, maxIterations=100;
+
+  unsigned char *ptr = pNic->mapAddr;
+  printf("Map address: 0x%lx\n", (unsigned long)ptr);
+  // NIC register 0x20 is MDIO control register
+  ptr += kMdiControlRegister;
+
+  // If phy_page is specified then first set the page id in phy register 22
+  if (phyPage >= 0) {
+    // OP (27..26): 1 (MDIO write)
+    // PhyReg (20..16): 22 (page id)
+    // DataVal (15..0): phyPage
+    mdioRegVal = (1 << 26) | (22 << 16) | phyPage;
+    // Write MDIO control register
+    *(unsigned int*)ptr = mdioRegVal;
+
+    // Keep reading MDIO control register till bit 28 (Ready Bit) becomes 1
+    mdioRegVal = waitForMdioCompletion(pNic);
+  }
+
+  // Now write the actual PHY rgister
+
+  // OP (27..26): 1 (MDIO write)
+  // PhyReg (20..16): phyRegAddr
+  // DataVal (15..0): phyRegVal
+  mdioRegVal = (1 << 26) | (phyRegAddr << 16) | phyRegVal;
+  *(unsigned int*)ptr = mdioRegVal;
+
+  // Keep reading MDIO control register till bit 28 (Ready Bit) becomes 1
+  mdioRegVal = waitForMdioCompletion(pNic);
+  while((mdioRegVal & (1<<28))==0 && (iteration < maxIterations)) {
+    usleep(10000);
+    mdioRegVal = *(unsigned int*)ptr;
+    iteration++;
+  }
+
+  printf("PHY Reg 0x%x = 0x%.4x\n",
+         (mdioRegVal>>16) & 0x1f, mdioRegVal & 0xffff);
+  return true;
+}
+
+/*
+ * nicLinkShowConfig
+ *
+ * Shows the NIC i210 link configuration from these blocks:
+ *  MAC block
+ *  PCS block
+ *  Internal PHY block
+ */
+bool nicLinkShowConfig(struct nic_info_s *pNic) {
+
+  uint32_t regVal, mdioRegVal;
+  std::string speedString[] = {"10 Mbps", "100 Mbps", "1 Gbps", "None"};
+  std::string linkMode[] = {"Internal PHY (1000-BASE-T)", "1000-Base-KX", "SGMII", "Serdes"};
+
+  unsigned char *ptr = pNic->mapAddr;
+  printf("Map address: 0x%lx\n", (unsigned long)ptr);
+  printf("i210 MAC Config:\n");
+  // Read Device Control Register
+  regVal = nicReadRegInternal(pNic, 0);
+  printf("  Set Link Up:         %s\n", (regVal & 0x40) ? "Up" : "Down");
+  printf("  Link speed:          %s\n", speedString[((regVal>>8) & 0x3)].c_str());
+  printf("  Duplex:              %s\n", (regVal & 0x1) ? "Full" : "Half");
+  // Read Extended Device Control Register
+  regVal = nicReadRegInternal(pNic, 0x18);
+  printf("  Link Mode:           %s\n", linkMode[((regVal>>22) & 0x3)].c_str());
+  printf("  Driver Loaded:       %s\n", (regVal & 0x10000000) ? "Yes" : "No");
+
+  printf("i210 PCS Config:\n");
+  // Read PCS Configuration register
+  regVal = nicReadRegInternal(pNic, 0x4200);
+  printf("  PCS Enabled:         %s\n", (regVal & 0x8) ? "Enabled" : "Disabled");
+
+  printf("i210 PHY Config:\n");
+
+  // Set Phy page 0
+  mdioRegVal = (1 << 26) | (22 << 16);
+  nicWriteRegInternal(pNic, 0x20, mdioRegVal);
+  mdioRegVal = waitForMdioCompletion(pNic);
+
+  // Read reg 0
+  mdioRegVal = (2 << 26) | (0 << 16);
+  nicWriteRegInternal(pNic, 0x20, mdioRegVal);
+  mdioRegVal = waitForMdioCompletion(pNic);
+
+  printf("  Power Down:          %s\n", (mdioRegVal & 0x800) ? "Yes" : "No");
+  printf("  AN Enabled:          %s\n", (mdioRegVal & 0x1000) ? "Yes" : "No");
+  regVal = ((mdioRegVal>>5) & 0x2) | ((mdioRegVal>>13) & 0x1);
+  printf("  Speed:               %s\n", speedString[regVal].c_str());
+  printf("  Duplex:              %s\n", (mdioRegVal & 0x100) ? "Full" : "Half" );
+
+  return true;
+}
+
+/*
+ * nicLinkShowStatus
+ *
+ * Shows the NIC i210 link status from these blocks:
+ *  MAC block
+ *  PCS block
+ *  Internal PHY block
+ */
+bool nicLinkShowStatus(struct nic_info_s *pNic) {
+
+  uint32_t regVal, mdioRegVal;
+  std::string speedString[] = {"10 Mbps", "100 Mbps", "1 Gbps", "None"};
+
+  unsigned char *ptr = pNic->mapAddr;
+  printf("Map address: 0x%lx\n", (unsigned long)ptr);
+  printf("i210 MAC Status:\n");
+  // Read Device Status Register
+  regVal = nicReadRegInternal(pNic, 0x8);
+  printf("  Link:                %s\n", (regVal & 0x2) ? "Up" : "Down");
+  printf("  Link speed:          %s\n", speedString[((regVal>>6) & 0x3)].c_str());
+  printf("  Duplex:              %s\n", (regVal & 0x1) ? "Full" : "Half");
+
+  // Read Copper switch control register
+  regVal = nicReadRegInternal(pNic, 0x34);
+  printf("  PHY Signal:          %s\n", (regVal & 0x400) ? "Detected" : "Not detected");
+  printf("  Serdes Signal:       %s\n", (regVal & 0x200) ? "Detected" : "Not detected");
+  printf("  Phy power:           %s\n", (regVal & 0x800) ? "Down" : "Up");
+
+  printf("i210 PCS Status:\n");
+  // Read PCS status register
+  regVal = nicReadRegInternal(pNic, 0x420C);
+  printf("  PCS Link:            %s\n", (regVal & 0x1) ? "Up" : "Down");
+  printf("  Link speed:          %s\n", speedString[((regVal>>1) & 0x3)].c_str());
+  printf("  Duplex:              %s\n", (regVal & 0x8) ? "Full" : "Half");
+
+  printf("i210 PHY Status:\n");
+
+  // Set Phy page 0
+  mdioRegVal = (1 << 26) | (22 << 16);
+  nicWriteRegInternal(pNic, 0x20, mdioRegVal);
+  mdioRegVal = waitForMdioCompletion(pNic);
+
+  // Read reg 1
+  mdioRegVal = (2 << 26) | (0x1 << 16);
+  nicWriteRegInternal(pNic, 0x20, mdioRegVal);
+  mdioRegVal = waitForMdioCompletion(pNic);
+  printf("  Link:                %s\n", (mdioRegVal & 0x4) ? "Up" : "Down");
+  printf("  AN Status:           %s\n", (mdioRegVal & 0x20) ? "Completed" : "Incomplete");
+
+  // Read reg 17
+  mdioRegVal = (2 << 26) | (0x11 << 16);
+  nicWriteRegInternal(pNic, 0x20, mdioRegVal);
+  mdioRegVal = waitForMdioCompletion(pNic);
+  printf("  Speed:               %s\n", speedString[((mdioRegVal>>14) & 0x3)].c_str());
+  printf("  Duplex:              %s\n", (mdioRegVal & 0x2000) ? "Full" : "Half");
+  printf("  Link Energy:         %s\n", (mdioRegVal & 0x10) ? "Not detected" : "Detected");
+
+  return true;
+}
+
+/*
+ * This utility program provides the access CLI for Intel NIC i210
+ * which is used in most of the FBOSS platform. This NIC provides
+ * eth0 management interface in our platform on uServer side.
+ * This CLI is supported for all platforms where i210 NIC is
+ * there. It picks up the PCI configuration of NIC and then does
+ * read/write to its registers: MAC, PCS, PHY. The MAC, PCS are
+ * direct access whereas internal PHY registers are accessed via
+ * internal MDIO bus.
+ */
+int main(int argc, char* argv[]) {
+  folly::init(&argc, &argv, true);
+  gflags::SetCommandLineOptionWithMode(
+      "minloglevel", "0", gflags::SET_FLAGS_DEFAULT);
+
+  char sysfsPath[256];
+  int nChars;
+
+  // Find the PCI bus:slot:function for the Intel NIC i210
+  // For Intel NIC i210:
+  //   vendor id = 8086 (Intel)
+  //   device id = 1533 (i210)
+  FILE *fp = popen("lspci -d 8086:1533", "r");
+  if (!fp) {
+    printf("NIC i210 not found on PCI bus\n");
+    return EX_SOFTWARE;
+  }
+
+  // Build the sysfs file path for this NIC device resource0
+  // where all registers are mapped
+  nChars = sprintf(sysfsPath, "/sys/bus/pci/devices/0000:");
+  fscanf(fp, "%s", &sysfsPath[nChars]);
+  strcat(sysfsPath, "/resource0");
+
+  // Open the sysfs file to access the device
+  int fd = open(sysfsPath, O_RDWR | O_SYNC);
+  if (fd == -1) {
+    printf("Unable to open up the sysfs file %s\n", sysfsPath);
+    return EX_SOFTWARE;
+  }
+
+  // Mmeory map the device's 64KB memory register space to our
+  // user space
+  struct nic_info_s nicInfo;
+  nicInfo.mapSize = 0x10000;
+  nicInfo.mapAddr = (unsigned char*)mmap(0, 0x10000,
+                        PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+  if (nicInfo.mapAddr == MAP_FAILED) {
+    printf("mmap failed for resource\n");
+    close(fd);
+    return EX_SOFTWARE;
+  }
+
+  // CLI: nic_util --nic_read_reg --reg_addr <register-address>
+  if (FLAGS_nic_read_reg) {
+    printf("Calling nicShowReg with address 0x%x\n", FLAGS_reg_addr);
+    nicShowReg(&nicInfo, FLAGS_reg_addr);
+  }
+
+  // CLI: nic_util --nic_write_reg --reg_addr <register-address>
+  //               --reg_val <data-value>
+  if (FLAGS_nic_write_reg) {
+    printf("Calling nicWriteReg with address 0x%x value 0x%x\n",
+            FLAGS_reg_addr, FLAGS_reg_val);
+    nicWriteReg(&nicInfo, FLAGS_reg_addr, FLAGS_reg_val);
+  }
+
+  // CLI: nic_util --nic_phy_read_reg --phy_reg_addr <register-address>
+  //              [--phy_page <phy-page-id>]
+  if (FLAGS_nic_phy_read_reg) {
+    printf("Calling nicPhyReadReg with address 0x%x (page %d)\n",
+            FLAGS_phy_reg_addr, FLAGS_phy_page);
+    nicPhyReadReg(&nicInfo, FLAGS_phy_reg_addr, FLAGS_phy_page);
+  }
+
+  // CLI: nic_util --nic_phy_write_reg --phy_reg_addr <register-address>
+  //               --phy_reg_val <data-value> [--phy_page <phy-page-id>]
+  if (FLAGS_nic_phy_write_reg) {
+    printf("Calling nicPhyWriteReg with address 0x%x value 0x%x (page %d)\n",
+            FLAGS_phy_reg_addr, FLAGS_phy_reg_val, FLAGS_phy_page);
+    nicPhyWriteReg(&nicInfo, FLAGS_phy_reg_addr, FLAGS_phy_reg_val, FLAGS_phy_page);
+  }
+
+  // CLI: nic_util --nic_link_show_config
+  if (FLAGS_nic_link_show_config) {
+    printf("Calling nicLinkConfig to get link configuration\n");
+    nicLinkShowConfig(&nicInfo);
+  }
+
+  // CLI: nic_util --nic_link_show_status
+  if (FLAGS_nic_link_show_status) {
+    printf("Calling nicLinkStatus to get link status\n");
+    nicLinkShowStatus(&nicInfo);
+  }
+
+  munmap(nicInfo.mapAddr, 0x10000);
+  close(fd);
+  return EX_OK;
+}
