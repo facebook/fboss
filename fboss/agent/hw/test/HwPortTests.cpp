e0ae5d238ce296e77393a02572e35eda6c54c955
Parvez Shaikh <pshaikh@fb.com>
2020-09-01 17:56:16 -0700

    test port profiles
    Summary:
    writing tests for each profile. verify tx setting and interface mode for each profile.
    test skips if profile is unsupported for a test platform. if profile is supported, it sets that profile and speed for a port. for SAI, disabled ports are 'cleaned away' i.e. removed from configuration. test also provides mock media types as per profile (profiles now have media type info).
    
    next, verify interface mode and Tx Settings for each profile.  the test can be (and shall be) extended to verify
    - speed
    - pause settings (if required)
    - verify fec settings
    - verify rx settings (if applicable)
    - verify lanes (if applicable)
    
    For every new profile added or being supported, idea is that this test can be extended to verify required attributes as above.
    
    test currently ignores 'deprecated' profiles.
    
    Reviewed By: birdsoup
    
    Differential Revision: D23067431
    
    fbshipit-source-id: f8c6b75e59c2396433f7cff8cb02458d080f6dff

--- fboss/agent/hw/test/HwPortTests.cpp
+++ fboss/agent/hw/test/HwPortTests.cpp
@@ -13,7 +13,111 @@ class HwPortTest : public HwTest {
     return utility::oneL3IntfTwoPortConfig(
         getHwSwitch(), masterLogicalPortIds()[0], masterLogicalPortIds()[1]);
   }
+
+  void testProfile(cfg::PortProfileID profile) {
+    auto profiles = getPlatform()->getSupportedProfiles();
+    if (profiles.find(profile) == profiles.end()) {
+      return;
+    }
+    auto setup = []() {
+      // TODO
+    };
+    auto verify = []() {
+      // TODO
+    };
+    verifyAcrossWarmBoots(setup, verify);
+  }
 };
+
+TEST_F(HwPortTest, PROFILE_10G_1_NRZ_NOFEC_COPPER) {
+  testProfile(cfg::PortProfileID::PROFILE_10G_1_NRZ_NOFEC_COPPER);
+}
+
+TEST_F(HwPortTest, PROFILE_10G_1_NRZ_NOFEC_OPTICAL) {
+  testProfile(cfg::PortProfileID::PROFILE_10G_1_NRZ_NOFEC_OPTICAL);
+}
+
+TEST_F(HwPortTest, PROFILE_20G_2_NRZ_NOFEC_COPPER) {
+  testProfile(cfg::PortProfileID::PROFILE_20G_2_NRZ_NOFEC_COPPER);
+}
+
+TEST_F(HwPortTest, PROFILE_25G_1_NRZ_NOFEC_COPPER) {
+  testProfile(cfg::PortProfileID::PROFILE_25G_1_NRZ_NOFEC_COPPER);
+}
+
+TEST_F(HwPortTest, PROFILE_25G_1_NRZ_CL74_COPPER) {
+  testProfile(cfg::PortProfileID::PROFILE_25G_1_NRZ_CL74_COPPER);
+}
+
+TEST_F(HwPortTest, PROFILE_25G_1_NRZ_RS528_COPPER) {
+  testProfile(cfg::PortProfileID::PROFILE_25G_1_NRZ_RS528_COPPER);
+}
+
+TEST_F(HwPortTest, PROFILE_40G_4_NRZ_NOFEC_COPPER) {
+  testProfile(cfg::PortProfileID::PROFILE_40G_4_NRZ_NOFEC_COPPER);
+}
+
+TEST_F(HwPortTest, PROFILE_40G_4_NRZ_NOFEC_OPTICAL) {
+  testProfile(cfg::PortProfileID::PROFILE_40G_4_NRZ_NOFEC_OPTICAL);
+}
+
+TEST_F(HwPortTest, PROFILE_50G_2_NRZ_NOFEC_COPPER) {
+  testProfile(cfg::PortProfileID::PROFILE_50G_2_NRZ_NOFEC_COPPER);
+}
+
+TEST_F(HwPortTest, PROFILE_50G_2_NRZ_CL74_COPPER) {
+  testProfile(cfg::PortProfileID::PROFILE_50G_2_NRZ_CL74_COPPER);
+}
+
+TEST_F(HwPortTest, PROFILE_50G_2_NRZ_RS528_COPPER) {
+  testProfile(cfg::PortProfileID::PROFILE_50G_2_NRZ_RS528_COPPER);
+}
+
+TEST_F(HwPortTest, PROFILE_100G_4_NRZ_RS528_COPPER) {
+  testProfile(cfg::PortProfileID::PROFILE_100G_4_NRZ_RS528_COPPER);
+}
+
+TEST_F(HwPortTest, PROFILE_100G_4_NRZ_RS528_OPTICAL) {
+  testProfile(cfg::PortProfileID::PROFILE_100G_4_NRZ_RS528_OPTICAL);
+}
+
+/*
+TEST_F(HwPortTest, PROFILE_200G_4_PAM4_RS544X2N_COPPER) {
+  testProfile(cfg::PortProfileID::PROFILE_200G_4_PAM4_RS544X2N_COPPER);
+}
+
+TEST_F(HwPortTest, PROFILE_200G_4_PAM4_RS544X2N_OPTICAL) {
+  testProfile(cfg::PortProfileID::PROFILE_200G_4_PAM4_RS544X2N_OPTICAL);
+}
+
+TEST_F(HwPortTest, PROFILE_400G_8_PAM4_RS544X2N_OPTICAL) {
+  testProfile(cfg::PortProfileID::PROFILE_400G_8_PAM4_RS544X2N_OPTICAL);
+}
+*/
+
+TEST_F(HwPortTest, PROFILE_100G_4_NRZ_CL91_COPPER) {
+  testProfile(cfg::PortProfileID::PROFILE_100G_4_NRZ_CL91_COPPER);
+}
+
+TEST_F(HwPortTest, PROFILE_100G_4_NRZ_CL91_OPTICAL) {
+  testProfile(cfg::PortProfileID::PROFILE_100G_4_NRZ_CL91_OPTICAL);
+}
+
+TEST_F(HwPortTest, PROFILE_20G_2_NRZ_NOFEC_OPTICAL) {
+  testProfile(cfg::PortProfileID::PROFILE_20G_2_NRZ_NOFEC_OPTICAL);
+}
+
+TEST_F(HwPortTest, PROFILE_25G_1_NRZ_NOFEC_OPTICAL) {
+  testProfile(cfg::PortProfileID::PROFILE_25G_1_NRZ_NOFEC_OPTICAL);
+}
+
+TEST_F(HwPortTest, PROFILE_50G_2_NRZ_NOFEC_OPTICAL) {
+  testProfile(cfg::PortProfileID::PROFILE_50G_2_NRZ_NOFEC_OPTICAL);
+}
+
+TEST_F(HwPortTest, PROFILE_100G_4_NRZ_NOFEC_COPPER) {
+  testProfile(cfg::PortProfileID::PROFILE_100G_4_NRZ_NOFEC_COPPER);
+}
 
 TEST_F(HwPortTest, AssertMode) {
   auto setup = [this]() { applyNewConfig(initialConfig()); };
