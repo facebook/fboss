3570861e6ab0b340cb246e38d19a55e58b655dc5
Parvez Shaikh <pshaikh@fb.com>
2020-09-09 10:58:36 -0700

    set port LED status on wedge40 for HW Tests
    Summary: As titled,  this is copy paste from wedge40 port LED programming
    
    Differential Revision: D23493527
    
    fbshipit-source-id: e08fd70215c65b1d4d8fd9e8ea20bdaab2b403b8

--- fboss/agent/platforms/tests/utils/BcmTestWedge40Port.cpp
+++ fboss/agent/platforms/tests/utils/BcmTestWedge40Port.cpp
@@ -9,7 +9,10 @@
  */
 #include "fboss/agent/platforms/tests/utils/BcmTestWedge40Port.h"
 
+#include "fboss/agent/hw/bcm/BcmPort.h"
+#include "fboss/agent/platforms/common/utils/Wedge40LedUtils.h"
 #include "fboss/agent/platforms/tests/utils/BcmTestWedge40Platform.h"
+#include "fboss/agent/platforms/wedge/utils/BcmLedUtils.h"
 
 namespace facebook::fboss {
 
@@ -18,4 +21,10 @@ BcmTestWedge40Port::BcmTestWedge40Port(
     BcmTestWedge40Platform* platform)
     : BcmTestPort(id, platform) {}
 
+void BcmTestWedge40Port::linkStatusChanged(bool up, bool adminUp) {
+  auto bcmPort = getBcmPort();
+  uint32_t value =
+      static_cast<uint32_t>(Wedge40LedUtils::getLEDState(up, adminUp));
+  BcmLedUtils::setWedge40PortStatus(0, bcmPort->getPortID(), value);
+}
 } // namespace facebook::fboss
