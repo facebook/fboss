cbceab6c40419452465191c60bd1bd24ed7f7276
Parvez Shaikh <pshaikh@fb.com>
2020-09-09 10:58:36 -0700

    utils methods to get and set LED status of ports
    Summary: Stub methods in BCM LED utils.
    
    Differential Revision: D23581763
    
    fbshipit-source-id: 878212928ff605e6048c2ece7be4209f770a1622

--- fboss/agent/platforms/wedge/utils/oss/BcmLedUtils.cpp
+++ fboss/agent/platforms/wedge/utils/oss/BcmLedUtils.cpp
@@ -6,5 +6,25 @@ namespace facebook::fboss {
 
 void BcmLedUtils::initLED0(int /*unit*/, folly::ByteRange /*code*/) {}
 void BcmLedUtils::initLED1(int /*unit*/, folly::ByteRange /*code*/) {}
-
+void BcmLedUtils::setGalaxyPortStatus(
+    int /*unit*/,
+    int /*port*/,
+    uint32_t /*status*/) {}
+uint32_t BcmLedUtils::getGalaxyPortStatus(int /*unit*/, int /*port*/) {
+  return 0;
+}
+void BcmLedUtils::setWedge100PortStatus(
+    int /*unit*/,
+    int /*port*/,
+    uint32_t /*status*/) {}
+uint32_t BcmLedUtils::getWedge100PortStatus(int /*unit*/, int /*port*/) {
+  return 0;
+}
+void BcmLedUtils::setWedge40PortStatus(
+    int /*unit*/,
+    int /*port*/,
+    uint32_t /*status*/) {}
+uint32_t BcmLedUtils::getWedge40PortStatus(int /*unit*/, int /*port*/) {
+  return 0;
+}
 } // namespace facebook::fboss
