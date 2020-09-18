ba30f0f12459c326b19722b300f765654a274e71
Jasmeet Bagga <jasmeetbagga@fb.com>
2020-09-17 18:13:59 -0700

    Fill in APIs for creating ECN/Wred profiles
    Summary: As titled
    
    Differential Revision: D23697200
    
    fbshipit-source-id: 70c0d600751486001e426f509fabe6c13b4afbe0

--- fboss/agent/hw/sai/switch/SaiWredManager.cpp
+++ fboss/agent/hw/sai/switch/SaiWredManager.cpp
@@ -10,11 +10,47 @@
 
 #include "fboss/agent/hw/sai/switch/SaiWredManager.h"
 
+#include "fboss/agent/hw/sai/switch/SaiManagerTable.h"
+#include "fboss/agent/hw/sai/switch/SaiSwitchManager.h"
+#include "fboss/agent/state/PortQueue.h"
+
 namespace facebook::fboss {
 
 std::shared_ptr<SaiWred> SaiWredManager::getOrCreateProfile(
-    const PortQueue& /*queue*/) {
-  // TODO
-  return nullptr;
+    const PortQueue& queue) {
+  if (!queue.getAqms().size()) {
+    return nullptr;
+  }
+  auto c = profileCreateAttrs(queue);
+  return wredProfiles_
+      .refOrEmplace(c, c, c, managerTable_->switchManager().getSwitchSaiId())
+      .first;
+}
+
+SaiWredTraits::CreateAttributes SaiWredManager::profileCreateAttrs(
+    const PortQueue& queue) const {
+  SaiWredTraits::CreateAttributes attrs;
+  using Attributes = SaiWredTraits::Attributes;
+  std::get<Attributes::GreenEnable>(attrs) = false;
+  std::get<Attributes::EcnMarkMode>(attrs) = SAI_ECN_MARK_MODE_NONE;
+
+  for (const auto& [type, aqm] : queue.getAqms()) {
+    auto thresholds = (*aqm.detection_ref()).get_linear();
+    auto [minLen, maxLen] = std::make_pair(
+        *thresholds.minimumLength_ref(), *thresholds.maximumLength_ref());
+    switch (type) {
+      case cfg::QueueCongestionBehavior::EARLY_DROP:
+        std::get<Attributes::GreenEnable>(attrs) = true;
+        std::get<Attributes::GreenMinThreshold>(attrs) = minLen;
+        std::get<Attributes::GreenMaxThreshold>(attrs) = maxLen;
+        break;
+      case cfg::QueueCongestionBehavior::ECN:
+        std::get<Attributes::EcnMarkMode>(attrs) = SAI_ECN_MARK_MODE_GREEN;
+        std::get<Attributes::EcnGreenMinThreshold>(attrs) = minLen;
+        std::get<Attributes::EcnGreenMaxThreshold>(attrs) = maxLen;
+        break;
+    }
+  }
+  return attrs;
 }
 } // namespace facebook::fboss
