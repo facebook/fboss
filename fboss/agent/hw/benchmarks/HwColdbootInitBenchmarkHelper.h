1684f07b45d6cb89da79791328ff4f0346c32886
Srikrishna Gopu <srikrishnagopu@fb.com>
2020-09-24 03:08:39 -0700

    coldboot init benchmark helper
    Summary: As titled, benchmark helper for cold boot init. This measures the performance time from INITIALIZED to CONFIGURED during cold boot.
    
    Reviewed By: phshaikh
    
    Differential Revision: D23635046
    
    fbshipit-source-id: db9355e9f034b4d3e952b0168707ba9e764d67cd

---
+++ fboss/agent/hw/benchmarks/HwColdbootInitBenchmarkHelper.h
@@ -0,0 +1,46 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#include "fboss/agent/FbossError.h"
+#include "fboss/agent/hw/test/ConfigFactory.h"
+#include "fboss/agent/hw/test/HwLinkStateToggler.h"
+#include "fboss/agent/hw/test/HwSwitchEnsemble.h"
+#include "fboss/agent/hw/test/HwSwitchEnsembleFactory.h"
+
+#include <folly/Benchmark.h>
+#include "fboss/agent/gen-cpp2/switch_config_types.h"
+
+using namespace facebook::fboss;
+
+namespace facebook::fboss {
+
+void initColdBootInitBenchmarker(
+    cfg::PortSpeed uplinkPortSpeed,
+    cfg::PortSpeed downlinkPortSpeed) {
+  auto ensemble = createHwEnsemble(HwSwitchEnsemble::getAllFeatures());
+  folly::BenchmarkSuspender suspender;
+  auto hwSwitch = ensemble->getHwSwitch();
+  auto config = utility::createUplinkDownlinkConfig(
+      hwSwitch,
+      ensemble->masterLogicalPortIds(),
+      uplinkPortSpeed,
+      downlinkPortSpeed,
+      cfg::PortLoopbackMode::MAC);
+  suspender.dismiss();
+  ensemble->applyInitialConfig(config);
+  suspender.rehire();
+}
+
+#define COLDBOOT_INIT_BENCHMARK(name, uplinkSpeed, downlinkSpeed) \
+  BENCHMARK(name) {                                               \
+    initColdBootInitBenchmarker(uplinkSpeed, downlinkSpeed);      \
+  }
+
+} // namespace facebook::fboss
