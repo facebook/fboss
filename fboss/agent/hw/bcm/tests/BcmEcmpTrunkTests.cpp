304d625cd6aa391b180b702315809120f2669499
Midhun Somasundaran <msomasundaran@fb.com>
2020-10-05 17:56:47 -0700

    Add test cases for link toggle during warm boot
    Summary: Test cases for changes in D23936563 (https://github.com/facebook/fboss/commit/7f4b0b085bb58044304259cd409c9e11852aa335)
    
    Reviewed By: jasmeetbagga
    
    Differential Revision: D23945566
    
    fbshipit-source-id: 48a5a1dcddada257e2f3036fed44286ea0c5054a

--- fboss/agent/hw/bcm/tests/BcmEcmpTrunkTests.cpp
+++ fboss/agent/hw/bcm/tests/BcmEcmpTrunkTests.cpp
@@ -65,7 +65,6 @@ class BcmEcmpTrunkTest : public BcmLinkStateDependentTests {
   }
 
   void runEcmpWithTrunkMinLinks(uint8_t minlinks) {
-    auto ecmpShrinkTest = (minlinks >= 2) ? true : false;
     auto setup = [=]() {
       auto state = enableTrunkPorts(getProgrammedState());
       applyNewState(utility::setTrunkMinLinkCount(state, minlinks));
@@ -109,13 +108,7 @@ class BcmEcmpTrunkTest : public BcmLinkStateDependentTests {
                   getHwSwitch(), kDefaultRoute.network, kDefaultRoute.mask),
               kEcmpWidth));
     };
-    // TODO - Enable warm boot verification in ECMP shrink case
-    if (ecmpShrinkTest) {
-      setup();
-      verify();
-    } else {
-      verifyAcrossWarmBoots(setup, verify);
-    }
+    verifyAcrossWarmBoots(setup, verify);
   }
 
   const int kEcmpWidth = 7;
@@ -123,6 +116,9 @@ class BcmEcmpTrunkTest : public BcmLinkStateDependentTests {
   std::unique_ptr<EcmpSetupTargetedPorts6> ecmpHelper_;
 };
 
+// ECMP with trunk as member. One trunk member transitions to
+// down state but no ECMP shrink happens because minlinks
+// condition is not violated.
 TEST_F(BcmEcmpTrunkTest, TrunkNotRemovedFromEcmpWithMinLinks) {
   runEcmpWithTrunkMinLinks(1);
 }
@@ -184,6 +180,388 @@ TEST_F(BcmEcmpTrunkTest, TrunkMemberRemoveWithRouteTest) {
             getUnit(), trunkTable->getBcmTrunkId(kAggId), trunkMemberCount));
   };
   verifyAcrossWarmBoots(setup, verify);
+}
+
+// ECMP with trunk as member. One trunk member transitions to
+// Down state before warmboot resulting in ECMP shrink. Post warmboot,
+// ECMP stays shrunk. Port transitions to Up after warm boot
+// but ECMP stays shrunk until LACP brings it up.
+TEST_F(
+    BcmEcmpTrunkTest,
+    TrunkL2ResolveNhopThenLinkAndLACPDownThenUpThenStateUp) {
+  uint8_t minlinks = 2;
+  uint8_t numEcmpMembersToExclude = 1;
+  auto setup = [=]() {
+    auto state = enableTrunkPorts(getProgrammedState());
+    applyNewState(utility::setTrunkMinLinkCount(state, minlinks));
+    applyNewState(ecmpHelper_->resolveNextHops(
+        ecmpHelper_->setupECMPForwarding(getProgrammedState(), getEcmpPorts()),
+        getEcmpPorts()));
+    // We programmed the default route picked by EcmpSetupAnyNPorts so lookup
+    // the ECMP group for it
+    ASSERT_EQ(
+        kEcmpWidth,
+        getEcmpSizeInHw(
+            getUnit(),
+            getEgressIdForRoute(
+                getHwSwitch(), kDefaultRoute.network, kDefaultRoute.mask),
+            kEcmpWidth));
+
+    auto trunkTable = getHwSwitch()->getTrunkTable();
+    EXPECT_NO_THROW(trunkTable->getBcmTrunkId(kAggId));
+    EXPECT_EQ(
+        2,
+        utility::getBcmTrunkMemberCount(
+            getUnit(), trunkTable->getBcmTrunkId(kAggId), 2));
+    // link down on member port resulting in ECMP shrink
+    bringDownPort(PortID(getTrunkMemberPorts()[0]));
+    EXPECT_EQ(
+        1,
+        utility::getBcmTrunkMemberCount(
+            getUnit(), trunkTable->getBcmTrunkId(kAggId), 2));
+    // LACP watches on port down and change state
+    state = disableTrunkPort(
+        getProgrammedState(), kAggId, PortID(getTrunkMemberPorts()[0]));
+    applyNewState(state);
+  };
+
+  auto verify = [=]() {
+    // Ecmp should shrink based on the aggCount since the number of ports
+    // in Agg is less than min-links.
+    ASSERT_EQ(
+        kEcmpWidth - numEcmpMembersToExclude,
+        getEcmpSizeInHw(
+            getUnit(),
+            getEgressIdForRoute(
+                getHwSwitch(), kDefaultRoute.network, kDefaultRoute.mask),
+            kEcmpWidth));
+    auto trunkTable = getHwSwitch()->getTrunkTable();
+    EXPECT_NO_THROW(trunkTable->getBcmTrunkId(kAggId));
+    EXPECT_EQ(
+        1,
+        utility::getBcmTrunkMemberCount(
+            getUnit(), trunkTable->getBcmTrunkId(kAggId), 2));
+  };
+
+  auto setupPostWarmboot = [=]() {
+    // bring up the member link
+    bringUpPort(PortID(getTrunkMemberPorts()[0]));
+    // ECMP should stay shrunk
+    ASSERT_EQ(
+        kEcmpWidth - numEcmpMembersToExclude,
+        getEcmpSizeInHw(
+            getUnit(),
+            getEgressIdForRoute(
+                getHwSwitch(), kDefaultRoute.network, kDefaultRoute.mask),
+            kEcmpWidth));
+    auto trunkTable = getHwSwitch()->getTrunkTable();
+    EXPECT_NO_THROW(trunkTable->getBcmTrunkId(kAggId));
+    EXPECT_EQ(
+        1,
+        utility::getBcmTrunkMemberCount(
+            getUnit(), trunkTable->getBcmTrunkId(kAggId), 2));
+  };
+
+  auto verifyPostWarmboot = [=]() {
+    // LACP enables link post warm boot
+    auto state = enableTrunkPorts(getProgrammedState());
+    applyNewState(state);
+    applyNewState(
+        ecmpHelper_->resolveNextHops(getProgrammedState(), getEcmpPorts()));
+    // ECMP should expand
+    ASSERT_EQ(
+        kEcmpWidth,
+        getEcmpSizeInHw(
+            getUnit(),
+            getEgressIdForRoute(
+                getHwSwitch(), kDefaultRoute.network, kDefaultRoute.mask),
+            kEcmpWidth));
+    auto trunkTable = getHwSwitch()->getTrunkTable();
+    EXPECT_NO_THROW(trunkTable->getBcmTrunkId(kAggId));
+    EXPECT_EQ(
+        2,
+        utility::getBcmTrunkMemberCount(
+            getUnit(), trunkTable->getBcmTrunkId(kAggId), 2));
+  };
+
+  verifyAcrossWarmBoots(setup, verify, setupPostWarmboot, verifyPostWarmboot);
+}
+
+// ECMP with trunk as member. One trunk member transitions to
+// Down state before warmboot resulting in ECMP shrink.
+// Warmboot happens before LACP reacts to link down resulting
+// in mismatched trunk membership in hardware/software.
+// During warm boot init, the trunk is add to ECMP for a breif
+// time period till port states are re-applied. Post warmboot,
+// ECMP stays shrunk. Port transitions to Up after warm boot
+// but ECMP stays shrunk until LACP brings it up.
+// For details regarding traffic loss during short period
+// of time during warmboot init, please check the wiki -
+// https://github.com/facebook/fboss/blob/master/fboss/agent/wiki/warmboot_link_flaps.rst
+TEST_F(BcmEcmpTrunkTest, TrunkL2ResolveNhopThenLinkDownThenUpThenStateUp) {
+  uint8_t minlinks = 2;
+  uint8_t numEcmpMembersToExclude = 1;
+  auto setup = [=]() {
+    auto state = enableTrunkPorts(getProgrammedState());
+    applyNewState(utility::setTrunkMinLinkCount(state, minlinks));
+    applyNewState(ecmpHelper_->resolveNextHops(
+        ecmpHelper_->setupECMPForwarding(getProgrammedState(), getEcmpPorts()),
+        getEcmpPorts()));
+    // We programmed the default route picked by EcmpSetupAnyNPorts so lookup
+    // the ECMP group for it
+    ASSERT_EQ(
+        kEcmpWidth,
+        getEcmpSizeInHw(
+            getUnit(),
+            getEgressIdForRoute(
+                getHwSwitch(), kDefaultRoute.network, kDefaultRoute.mask),
+            kEcmpWidth));
+    auto trunkTable = getHwSwitch()->getTrunkTable();
+    EXPECT_NO_THROW(trunkTable->getBcmTrunkId(kAggId));
+    EXPECT_EQ(
+        2,
+        utility::getBcmTrunkMemberCount(
+            getUnit(), trunkTable->getBcmTrunkId(kAggId), 2));
+    // link down on member port resulting in ECMP shrink
+    // LACP does not get a chance to react to port down before warm boot
+    bringDownPort(PortID(getTrunkMemberPorts()[0]));
+  };
+
+  auto verify = [=]() {
+    // Ecmp should shrink based on the aggCount since the number of ports
+    // in Agg is less than min-links.
+    ASSERT_EQ(
+        kEcmpWidth - numEcmpMembersToExclude,
+        getEcmpSizeInHw(
+            getUnit(),
+            getEgressIdForRoute(
+                getHwSwitch(), kDefaultRoute.network, kDefaultRoute.mask),
+            kEcmpWidth));
+    auto trunkTable = getHwSwitch()->getTrunkTable();
+    EXPECT_NO_THROW(trunkTable->getBcmTrunkId(kAggId));
+    EXPECT_EQ(
+        1,
+        utility::getBcmTrunkMemberCount(
+            getUnit(), trunkTable->getBcmTrunkId(kAggId), 2));
+  };
+
+  auto setupPostWarmboot = [=]() {
+    // LACP reacts to link down notification at end of warm boot init
+    auto state = disableTrunkPort(
+        getProgrammedState(), kAggId, PortID(getTrunkMemberPorts()[0]));
+    applyNewState(state);
+
+    // bring up the member link
+    bringUpPort(PortID(getTrunkMemberPorts()[0]));
+    // ECMP should stay shrunk
+    ASSERT_EQ(
+        kEcmpWidth - numEcmpMembersToExclude,
+        getEcmpSizeInHw(
+            getUnit(),
+            getEgressIdForRoute(
+                getHwSwitch(), kDefaultRoute.network, kDefaultRoute.mask),
+            kEcmpWidth));
+    auto trunkTable = getHwSwitch()->getTrunkTable();
+    EXPECT_NO_THROW(trunkTable->getBcmTrunkId(kAggId));
+    EXPECT_EQ(
+        1,
+        utility::getBcmTrunkMemberCount(
+            getUnit(), trunkTable->getBcmTrunkId(kAggId), 2));
+  };
+
+  auto verifyPostWarmboot = [=]() {
+    // LACP enables link when state machine converges
+    auto state = enableTrunkPorts(getProgrammedState());
+    applyNewState(state);
+    applyNewState(
+        ecmpHelper_->resolveNextHops(getProgrammedState(), getEcmpPorts()));
+    // ECMP should expand
+    ASSERT_EQ(
+        kEcmpWidth,
+        getEcmpSizeInHw(
+            getUnit(),
+            getEgressIdForRoute(
+                getHwSwitch(), kDefaultRoute.network, kDefaultRoute.mask),
+            kEcmpWidth));
+    auto trunkTable = getHwSwitch()->getTrunkTable();
+    EXPECT_NO_THROW(trunkTable->getBcmTrunkId(kAggId));
+    EXPECT_EQ(
+        2,
+        utility::getBcmTrunkMemberCount(
+            getUnit(), trunkTable->getBcmTrunkId(kAggId), 2));
+  };
+
+  verifyAcrossWarmBoots(setup, verify, setupPostWarmboot, verifyPostWarmboot);
+}
+
+// ECMP with trunk as member. One trunk member undergo
+// quick port Up/Down transition resulting in ECMP shrink.
+// LACP listens to port down and removes member from Aggport
+// but warm boot shut down happens before ARP/ND reacts.
+// On warm boot initialization, restoring neighbor entry
+// should not expand ECMP since minlink condition is not met.
+// LACP enables link post warm boot resulting in ECMP expansion.
+TEST_F(
+    BcmEcmpTrunkTest,
+    TrunkL2ResolveNhopThenLinkAndLACPDownThenUpBeforeReinit) {
+  uint8_t minlinks = 2;
+  uint8_t numEcmpMembersToExclude = 1;
+  auto setup = [=]() {
+    auto state = enableTrunkPorts(getProgrammedState());
+    applyNewState(utility::setTrunkMinLinkCount(state, minlinks));
+    applyNewState(ecmpHelper_->resolveNextHops(
+        ecmpHelper_->setupECMPForwarding(getProgrammedState(), getEcmpPorts()),
+        getEcmpPorts()));
+    // We programmed the default route picked by EcmpSetupAnyNPorts so lookup
+    // the ECMP group for it
+    ASSERT_EQ(
+        kEcmpWidth,
+        getEcmpSizeInHw(
+            getUnit(),
+            getEgressIdForRoute(
+                getHwSwitch(), kDefaultRoute.network, kDefaultRoute.mask),
+            kEcmpWidth));
+    auto trunkTable = getHwSwitch()->getTrunkTable();
+    EXPECT_NO_THROW(trunkTable->getBcmTrunkId(kAggId));
+    EXPECT_EQ(
+        2,
+        utility::getBcmTrunkMemberCount(
+            getUnit(), trunkTable->getBcmTrunkId(kAggId), 2));
+    // link down on member port resulting in ECMP shrink
+    bringDownPort(PortID(getTrunkMemberPorts()[0]));
+    EXPECT_EQ(
+        1,
+        utility::getBcmTrunkMemberCount(
+            getUnit(), trunkTable->getBcmTrunkId(kAggId), 2));
+    // LACP watches on port down and change state
+    state = disableTrunkPort(
+        getProgrammedState(), kAggId, PortID(getTrunkMemberPorts()[0]));
+    applyNewState(state);
+    // bring up the member link
+    bringUpPort(PortID(getTrunkMemberPorts()[0]));
+  };
+
+  auto verify = [=]() {
+    // Ecmp should shrink based on the aggCount since the number of ports
+    // in Agg is less than min-links.
+    ASSERT_EQ(
+        kEcmpWidth - numEcmpMembersToExclude,
+        getEcmpSizeInHw(
+            getUnit(),
+            getEgressIdForRoute(
+                getHwSwitch(), kDefaultRoute.network, kDefaultRoute.mask),
+            kEcmpWidth));
+    auto trunkTable = getHwSwitch()->getTrunkTable();
+    EXPECT_NO_THROW(trunkTable->getBcmTrunkId(kAggId));
+    EXPECT_EQ(
+        1,
+        utility::getBcmTrunkMemberCount(
+            getUnit(), trunkTable->getBcmTrunkId(kAggId), 2));
+  };
+
+  auto setupPostWarmboot = [=]() {};
+
+  auto verifyPostWarmboot = [=]() {
+    // LACP enables link post warm boot
+    auto state = enableTrunkPorts(getProgrammedState());
+    applyNewState(state);
+    applyNewState(
+        ecmpHelper_->resolveNextHops(getProgrammedState(), getEcmpPorts()));
+    // ECMP should expand
+    ASSERT_EQ(
+        kEcmpWidth,
+        getEcmpSizeInHw(
+            getUnit(),
+            getEgressIdForRoute(
+                getHwSwitch(), kDefaultRoute.network, kDefaultRoute.mask),
+            kEcmpWidth));
+    auto trunkTable = getHwSwitch()->getTrunkTable();
+    EXPECT_NO_THROW(trunkTable->getBcmTrunkId(kAggId));
+    EXPECT_EQ(
+        2,
+        utility::getBcmTrunkMemberCount(
+            getUnit(), trunkTable->getBcmTrunkId(kAggId), 2));
+  };
+
+  verifyAcrossWarmBoots(setup, verify, setupPostWarmboot, verifyPostWarmboot);
+}
+
+// ECMP with trunk as member. One trunk member experience
+// quick link Down/Up transition resulting in ECMP shrink.
+// Warmboot happens before LACP reacts to link down resulting
+// in mismatched trunk membership in hardware/software.
+// Warm boot restore results in ECMP expanding since LACP and
+// ARP did not get any down event.
+TEST_F(BcmEcmpTrunkTest, TrunkL2ResolveNhopThenLinkUpDownUpBeforeReinit) {
+  uint8_t minlinks = 2;
+  uint8_t numEcmpMembersToExclude = 1;
+  auto setup = [=]() {
+    auto state = enableTrunkPorts(getProgrammedState());
+    applyNewState(utility::setTrunkMinLinkCount(state, minlinks));
+    applyNewState(ecmpHelper_->resolveNextHops(
+        ecmpHelper_->setupECMPForwarding(getProgrammedState(), getEcmpPorts()),
+        getEcmpPorts()));
+    // We programmed the default route picked by EcmpSetupAnyNPorts so lookup
+    // the ECMP group for it
+    ASSERT_EQ(
+        kEcmpWidth,
+        getEcmpSizeInHw(
+            getUnit(),
+            getEgressIdForRoute(
+                getHwSwitch(), kDefaultRoute.network, kDefaultRoute.mask),
+            kEcmpWidth));
+    auto trunkTable = getHwSwitch()->getTrunkTable();
+    EXPECT_NO_THROW(trunkTable->getBcmTrunkId(kAggId));
+    EXPECT_EQ(
+        2,
+        utility::getBcmTrunkMemberCount(
+            getUnit(), trunkTable->getBcmTrunkId(kAggId), 2));
+    // link down on member port resulting in ECMP shrink
+    bringDownPort(PortID(getTrunkMemberPorts()[0]));
+    EXPECT_EQ(
+        1,
+        utility::getBcmTrunkMemberCount(
+            getUnit(), trunkTable->getBcmTrunkId(kAggId), 2));
+    // bring up the member link
+    bringUpPort(PortID(getTrunkMemberPorts()[0]));
+    // Ecmp should shrink based on the aggCount since the number of ports
+    // in Agg is less than min-links.
+    ASSERT_EQ(
+        kEcmpWidth - numEcmpMembersToExclude,
+        getEcmpSizeInHw(
+            getUnit(),
+            getEgressIdForRoute(
+                getHwSwitch(), kDefaultRoute.network, kDefaultRoute.mask),
+            kEcmpWidth));
+    EXPECT_EQ(
+        1,
+        utility::getBcmTrunkMemberCount(
+            getUnit(), trunkTable->getBcmTrunkId(kAggId), 2));
+  };
+
+  auto verify = [=]() {};
+
+  auto setupPostWarmboot = [=]() {};
+
+  auto verifyPostWarmboot = [=]() {
+    // ECMP should expand on warmboot restore since ARP/ND entry is
+    // present and port is up.
+    ASSERT_EQ(
+        kEcmpWidth,
+        getEcmpSizeInHw(
+            getUnit(),
+            getEgressIdForRoute(
+                getHwSwitch(), kDefaultRoute.network, kDefaultRoute.mask),
+            kEcmpWidth));
+    auto trunkTable = getHwSwitch()->getTrunkTable();
+    EXPECT_NO_THROW(trunkTable->getBcmTrunkId(kAggId));
+    EXPECT_EQ(
+        2,
+        utility::getBcmTrunkMemberCount(
+            getUnit(), trunkTable->getBcmTrunkId(kAggId), 2));
+  };
+
+  verifyAcrossWarmBoots(setup, verify, setupPostWarmboot, verifyPostWarmboot);
 }
 
 } // namespace facebook::fboss
