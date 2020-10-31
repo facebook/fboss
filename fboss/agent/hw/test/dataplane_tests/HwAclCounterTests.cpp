3de050611532539d11c47665c2a1d650ba7131a5
Shrikrishna Khare <skhare@fb.com>
2020-10-31 15:59:07 -0700

    Add front panel ports for HwAclCounterTests
    Summary:
    This is towards supporting ACLs in SAI implementation.
    
    Today, AclCounter tests verify packets from CPU only. Add another flavor of tests that verify packets from the front panel ports. We have often seen difference in behavior/bugs between CPU originated and front panel originated packets. These tests would catch that.
    
    Reviewed By: jasmeetbagga
    
    Differential Revision: D24661460
    
    fbshipit-source-id: d78b1c8be5823678e0284d7a0a800e7535423df0

--- fboss/agent/hw/test/dataplane_tests/HwAclCounterTests.cpp
+++ fboss/agent/hw/test/dataplane_tests/HwAclCounterTests.cpp
@@ -7,66 +7,100 @@
  *  of patent rights can be found in the PATENTS file in the same directory.
  *
  */
+
 #include "fboss/agent/hw/test/ConfigFactory.h"
 #include "fboss/agent/hw/test/HwLinkStateDependentTest.h"
-#include "fboss/agent/hw/test/HwPortUtils.h"
 #include "fboss/agent/hw/test/HwTestAclUtils.h"
 #include "fboss/agent/hw/test/HwTestPacketUtils.h"
-#include "fboss/agent/hw/test/TrafficPolicyUtils.h"
 #include "fboss/agent/test/EcmpSetupHelper.h"
-
-#include "fboss/agent/state/Interface.h"
-#include "fboss/agent/state/SwitchState.h"
-
-#include <folly/IPAddressV6.h>
-
-#include "fboss/agent/gen-cpp2/switch_config_types.h"
-
-using folly::IPAddressV6;
+#include "fboss/agent/test/ResourceLibUtil.h"
 
 namespace facebook::fboss {
 
 class HwAclCounterTest : public HwLinkStateDependentTest {
  protected:
+  void SetUp() override {
+    HwLinkStateDependentTest::SetUp();
+    helper_ = std::make_unique<utility::EcmpSetupAnyNPorts6>(
+        getProgrammedState(), RouterID(0));
+  }
   cfg::SwitchConfig initialConfig() const override {
     auto cfg = utility::onePortPerVlanConfig(
         getHwSwitch(), masterLogicalPortIds(), cfg::PortLoopbackMode::MAC);
     return cfg;
   }
 
-  void sendPkt(uint8_t ttl) {
-    auto vlanId = utility::firstVlanID(initialConfig());
-    auto intfMac = utility::getInterfaceMac(getProgrammedState(), vlanId);
-    auto pkt = utility::makeUDPTxPacket(
-        getHwSwitch(),
-        vlanId,
-        intfMac,
-        intfMac,
-        IPAddressV6("2001::1"),
-        IPAddressV6("2800::1"),
-        10000,
-        20000,
-        0,
-        ttl);
-    getHwSwitch()->sendPacketSwitchedSync(std::move(pkt));
-  }
+  void counterBumpOnHitHelper(bool bumpOnHit, bool frontPanel) {
+    auto setup = [this]() {
+      applyNewState(helper_->setupECMPForwarding(
+          helper_->resolveNextHops(getProgrammedState(), 2), kEcmpWidth));
+      auto newCfg{initialConfig()};
+      addTtlAclStat(&newCfg);
+      applyNewConfig(newCfg);
+    };
 
-  static constexpr auto kAclName = "ttld-acl";
-  static constexpr auto kCounterName = "ttld-stat";
+    auto verify = [this, bumpOnHit, frontPanel]() {
+      auto statBefore = utility::getAclInOutPackets(
+          getHwSwitch(), getProgrammedState(), kAclName, kCounterName);
 
-  void setupAclStat() {
-    auto initialCfg = initialConfig();
-    addTtlAclStat(&initialCfg);
-    applyNewConfig(initialCfg);
-    ecmpHelper_ = std::make_unique<utility::EcmpSetupAnyNPorts6>(
-        getProgrammedState(), kRid);
-    setupECMPForwarding();
+      // TTL is configured for value >= 128
+      auto ttl = bumpOnHit ? 200 : 10;
+      sendPacket(frontPanel, ttl);
+
+      auto statAfter = utility::getAclInOutPackets(
+          getHwSwitch(), getProgrammedState(), kAclName, kCounterName);
+
+      if (bumpOnHit) {
+        // Bump by 2, once on the way out and once when it loops back in
+        EXPECT_EQ(statBefore + 2, statAfter);
+      } else {
+        EXPECT_EQ(statBefore, statAfter);
+      }
+    };
+
+    verifyAcrossWarmBoots(setup, verify);
   }
 
  private:
+  void sendPacket(bool frontPanel, uint8_t ttl) {
+    auto vlanId = VlanID(*initialConfig().vlanPorts_ref()[0].vlanID_ref());
+    auto intfMac = utility::getInterfaceMac(getProgrammedState(), vlanId);
+    auto srcMac = utility::MacAddressGenerator().get(intfMac.u64NBO() + 1);
+    auto txPacket = utility::makeUDPTxPacket(
+        getHwSwitch(),
+        vlanId,
+        srcMac, // src mac
+        intfMac, // dst mac
+        kSrcIP(),
+        kDstIP(),
+        8000, // l4 src port
+        8001, // l4 dst port
+        0,
+        ttl);
+
+    // port is in LB mode, so it will egress and immediately loop back.
+    // Since it is not re-written, it should hit the pipeline as if it
+    // ingressed on the port, and be properly queued.
+    if (frontPanel) {
+      auto outPort = helper_->ecmpPortDescriptorAt(kEcmpWidth).phyPortID();
+      getHwSwitchEnsemble()->ensureSendPacketOutOfPort(
+          std::move(txPacket), outPort);
+    } else {
+      getHwSwitchEnsemble()->ensureSendPacketSwitched(std::move(txPacket));
+    }
+  }
+
+  folly::IPAddressV6 kSrcIP() {
+    return folly::IPAddressV6("2620:0:1cfe:face:b00c::1");
+  }
+
+  folly::IPAddressV6 kDstIP() {
+    return folly::IPAddressV6("2620:0:1cfe:face:b00c::10");
+  }
+
   void addTtlAclStat(cfg::SwitchConfig* config) const {
     auto acl = utility::addAcl(config, kAclName);
-    acl->srcIp_ref() = "2001::/64";
+    acl->srcIp_ref() = "2620:0:1cfe:face:b00c::/64";
     acl->proto_ref() = 0x11;
     acl->ipType_ref() = cfg::IpType::IP6;
     acl->ttl_ref() = cfg::Ttl();
@@ -74,43 +108,34 @@ class HwAclCounterTest : public HwLinkStateDependentTest {
     *acl->ttl_ref()->mask_ref() = 128;
     utility::addAclStat(config, kAclName, kCounterName);
   }
-  void setupECMPForwarding() {
-    auto constexpr kEcmpWidth = 1;
-    auto newState = ecmpHelper_->setupECMPForwarding(
-        ecmpHelper_->resolveNextHops(getProgrammedState(), kEcmpWidth),
-        kEcmpWidth);
-    applyNewState(newState);
-  }
 
-  const RouterID kRid{0};
-  std::unique_ptr<utility::EcmpSetupAnyNPorts6> ecmpHelper_;
+  static inline constexpr auto kEcmpWidth = 1;
+  const VlanID kVlanID{utility::kBaseVlanId};
+  const InterfaceID kIntfID{utility::kBaseVlanId};
+  std::unique_ptr<utility::EcmpSetupAnyNPorts6> helper_;
+  static constexpr auto kAclName = "ttld-acl";
+  static constexpr auto kCounterName = "ttld-stat";
 };
 
-TEST_F(HwAclCounterTest, TestCounterBumpOnHit) {
-  auto setup = [this]() { setupAclStat(); };
-  auto verify = [this]() {
-    auto statBefore = utility::getAclInOutPackets(
-        getHwSwitch(), getProgrammedState(), kAclName, kCounterName);
-    sendPkt(200);
-    auto statAfter = utility::getAclInOutPackets(
-        getHwSwitch(), getProgrammedState(), kAclName, kCounterName);
-    // Bump by 2, once on the way out and once when it loops back in
-    EXPECT_EQ(statBefore + 2, statAfter);
-  };
-  verifyAcrossWarmBoots(setup, verify);
+// Verify that traffic arrive on a front panel port increments ACL counter.
+TEST_F(HwAclCounterTest, VerifyCounterBumpOnHitFrontPanel) {
+  counterBumpOnHitHelper(true /* bump on hit */, true /* front panel port */);
 }
 
-TEST_F(HwAclCounterTest, TestCounterNoHitNoBump) {
-  auto setup = [this]() { setupAclStat(); };
-  auto verify = [=]() {
-    auto statBefore = utility::getAclInOutPackets(
-        getHwSwitch(), getProgrammedState(), kAclName, kCounterName);
-    sendPkt(10);
-    auto statAfter = utility::getAclInOutPackets(
-        getHwSwitch(), getProgrammedState(), kAclName, kCounterName);
-    EXPECT_EQ(statBefore, statAfter);
-  };
-  verifyAcrossWarmBoots(setup, verify);
+// Verify that traffic originating on the CPU increments ACL counter.
+TEST_F(HwAclCounterTest, VerifyCounterBumpOnHitCpu) {
+  counterBumpOnHitHelper(true /* bump on hit */, false /* cpu port */);
+}
+
+// Verify that traffic arrive on a front panel port increments ACL counter.
+TEST_F(HwAclCounterTest, VerifyCounterNoHitNoBumpFrontPanel) {
+  counterBumpOnHitHelper(
+      false /* no hit, no bump */, true /* front panel port */);
+}
+
+// Verify that traffic originating on the CPU increments ACL counter.
+TEST_F(HwAclCounterTest, VerifyCounterNoHitNoBumpCpu) {
+  counterBumpOnHitHelper(false /* no hit, no bump */, false /* cpu port */);
 }
 
 } // namespace facebook::fboss
