ad8a93d2ec9073e684ca0bc2a5118daf607ed25d
Shrikrishna Khare <skhare@fb.com>
2020-10-31 15:59:07 -0700

    Add front panel ports for HwDscpQueueMappingTests
    Summary:
    This is towards supporting ACLs in SAI implementation.
    
    Today, HwDscpQueueMappingTest tests verify packets from CPU only. Add another flavor of
    tests that verify packets from the front panel ports. We have often seen
    difference in behavior/bugs between CPU originated and front panel originated
    packets. These tests would catch that.
    
    Reviewed By: jasmeetbagga
    
    Differential Revision: D24668012
    
    fbshipit-source-id: dd8282806de8471c3fc4b54a57bff3e3f2c5208b

--- fboss/agent/hw/test/dataplane_tests/HwDscpQueueMappingTests.cpp
+++ fboss/agent/hw/test/dataplane_tests/HwDscpQueueMappingTests.cpp
@@ -10,28 +10,206 @@
 
 #include "fboss/agent/hw/test/ConfigFactory.h"
 #include "fboss/agent/hw/test/HwLinkStateDependentTest.h"
-#include "fboss/agent/hw/test/HwPortUtils.h"
 #include "fboss/agent/hw/test/HwTestAclUtils.h"
 #include "fboss/agent/hw/test/HwTestPacketUtils.h"
 #include "fboss/agent/hw/test/TrafficPolicyUtils.h"
 #include "fboss/agent/test/EcmpSetupHelper.h"
-
-#include <folly/IPAddress.h>
-
-#include "fboss/agent/gen-cpp2/switch_config_types.h"
+#include "fboss/agent/test/ResourceLibUtil.h"
 
 namespace facebook::fboss {
 
 class HwDscpQueueMappingTest : public HwLinkStateDependentTest {
  protected:
+  void SetUp() override {
+    HwLinkStateDependentTest::SetUp();
+    helper_ = std::make_unique<utility::EcmpSetupAnyNPorts6>(
+        getProgrammedState(), RouterID(0));
+  }
   cfg::SwitchConfig initialConfig() const override {
-    auto cfg = utility::oneL3IntfConfig(
-        getHwSwitch(), masterLogicalPortIds()[0], cfg::PortLoopbackMode::MAC);
+    auto cfg = utility::onePortPerVlanConfig(
+        getHwSwitch(), masterLogicalPortIds(), cfg::PortLoopbackMode::MAC);
     return cfg;
   }
 
-  int kEcmpWidthForTest() const {
-    return 1;
+  void verifyDscpQueueMappingHelper(bool frontPanel) {
+    if (!isSupported(HwAsic::Feature::L3_QOS)) {
+      return;
+    }
+
+    auto setup = [this]() {
+      applyNewState(helper_->setupECMPForwarding(
+          helper_->resolveNextHops(getProgrammedState(), 2), kEcmpWidth));
+
+      auto kAclName = "acl1";
+      auto newCfg{initialConfig()};
+      utility::addDscpAclToCfg(&newCfg, kAclName, kDscp());
+      utility::addTrafficCounter(&newCfg, kCounterName());
+      utility::addQueueMatcher(&newCfg, kAclName, kQueueId(), kCounterName());
+
+      applyNewConfig(newCfg);
+    };
+
+    auto verify = [this, frontPanel]() {
+      auto beforeQueueOutPkts = getLatestPortStats(masterLogicalPortIds()[0])
+                                    .get_queueOutPackets_()
+                                    .at(kQueueId());
+
+      sendPacket(frontPanel);
+
+      auto afterQueueOutPkts = getLatestPortStats(masterLogicalPortIds()[0])
+                                   .get_queueOutPackets_()
+                                   .at(kQueueId());
+
+      EXPECT_EQ(1, afterQueueOutPkts - beforeQueueOutPkts);
+    };
+
+    verifyAcrossWarmBoots(setup, verify);
+  }
+
+  void verifyAclAndQosMapHelper(bool frontPanel) {
+    if (!isSupported(HwAsic::Feature::L3_QOS)) {
+      return;
+    }
+
+    auto setup = [this]() {
+      applyNewState(helper_->setupECMPForwarding(
+          helper_->resolveNextHops(getProgrammedState(), 2), kEcmpWidth));
+
+      auto newCfg{initialConfig()};
+
+      // QosMap
+      utility::addDscpQosPolicy(&newCfg, "qp1", {{kQueueId(), {kDscp()}}});
+      utility::setCPUQosPolicy(&newCfg, "qp1");
+      utility::setDefaultQosPolicy(&newCfg, "qp1");
+
+      // ACL
+      auto* acl = utility::addAcl(&newCfg, "acl0");
+      cfg::Ttl ttl; // Match packets with hop limit > 127
+      std::tie(*ttl.value_ref(), *ttl.mask_ref()) = std::make_tuple(0x80, 0x80);
+      acl->ttl_ref() = ttl;
+      utility::addAclStat(&newCfg, "acl0", kCounterName());
+
+      applyNewConfig(newCfg);
+    };
+
+    auto verify = [this, frontPanel]() {
+      auto beforeQueueOutPkts = getLatestPortStats(masterLogicalPortIds()[0])
+                                    .get_queueOutPackets_()
+                                    .at(kQueueId());
+      auto beforeAclInOutPkts = utility::getAclInOutPackets(
+          getHwSwitch(), getProgrammedState(), "acl0", kCounterName());
+
+      sendPacket(frontPanel, 255 /* ttl, > 127 to match ACL */);
+
+      auto afterQueueOutPkts = getLatestPortStats(masterLogicalPortIds()[0])
+                                   .get_queueOutPackets_()
+                                   .at(kQueueId());
+      auto afterAclInOutPkts = utility::getAclInOutPackets(
+          getHwSwitch(), getProgrammedState(), "acl0", kCounterName());
+
+      EXPECT_EQ(1, afterQueueOutPkts - beforeQueueOutPkts);
+      EXPECT_EQ(2, afterAclInOutPkts - beforeAclInOutPkts);
+    };
+
+    verifyAcrossWarmBoots(setup, verify);
+  }
+
+  void verifyAclAndQosMapConflictHelper(bool frontPanel) {
+    if (!isSupported(HwAsic::Feature::L3_QOS)) {
+      return;
+    }
+
+    auto setup = [this]() {
+      applyNewState(helper_->setupECMPForwarding(
+          helper_->resolveNextHops(getProgrammedState(), 2), kEcmpWidth));
+
+      auto newCfg{initialConfig()};
+
+      // The QoS map sends packets to queue kQueueIdQosMap() i.e. 7,
+      // The ACL sends them to queue kQueueIdAcl() i.e. 2.
+
+      // QosMap
+      utility::addDscpQosPolicy(
+          &newCfg, "qp1", {{kQueueIdQosMap(), {kDscp()}}});
+      utility::setCPUQosPolicy(&newCfg, "qp1");
+      utility::setDefaultQosPolicy(&newCfg, "qp1");
+
+      // ACL
+      utility::addDscpAclToCfg(&newCfg, "acl0", kDscp());
+      utility::addTrafficCounter(&newCfg, kCounterName());
+      utility::addQueueMatcher(&newCfg, "acl0", kQueueIdAcl(), kCounterName());
+
+      applyNewConfig(newCfg);
+    };
+
+    auto verify = [this, frontPanel]() {
+      auto beforeQueueOutPktsAcl = getLatestPortStats(masterLogicalPortIds()[0])
+                                       .get_queueOutPackets_()
+                                       .at(kQueueIdAcl());
+      auto beforeQueueOutPktsQosMap =
+          getLatestPortStats(masterLogicalPortIds()[0])
+              .get_queueOutPackets_()
+              .at(kQueueIdQosMap());
+      auto beforeAclInOutPkts = utility::getAclInOutPackets(
+          getHwSwitch(), getProgrammedState(), "acl0", kCounterName());
+
+      sendPacket(frontPanel);
+
+      auto afterQueueOutPktsAcl = getLatestPortStats(masterLogicalPortIds()[0])
+                                      .get_queueOutPackets_()
+                                      .at(kQueueIdAcl());
+      auto afterQueueOutPktsQosMap =
+          getLatestPortStats(masterLogicalPortIds()[0])
+              .get_queueOutPackets_()
+              .at(kQueueIdQosMap());
+
+      auto afterAclInOutPkts = utility::getAclInOutPackets(
+          getHwSwitch(), getProgrammedState(), "acl0", kCounterName());
+
+      // The ACL overrides the decision of the QoS map
+      EXPECT_EQ(0, afterQueueOutPktsQosMap - beforeQueueOutPktsQosMap);
+      EXPECT_EQ(1, afterQueueOutPktsAcl - beforeQueueOutPktsAcl);
+      EXPECT_EQ(2, afterAclInOutPkts - beforeAclInOutPkts);
+    };
+
+    verifyAcrossWarmBoots(setup, verify);
+  }
+
+ private:
+  void sendPacket(bool frontPanel, uint8_t ttl = 64) {
+    auto vlanId = VlanID(*initialConfig().vlanPorts_ref()[0].vlanID_ref());
+    auto intfMac = utility::getInterfaceMac(getProgrammedState(), vlanId);
+    auto srcMac = utility::MacAddressGenerator().get(intfMac.u64NBO() + 1);
+    auto txPacket = utility::makeUDPTxPacket(
+        getHwSwitch(),
+        vlanId,
+        srcMac, // src mac
+        intfMac, // dst mac
+        kSrcIP(),
+        kDstIP(),
+        8000, // l4 src port
+        8001, // l4 dst port
+        kDscp() << 2, // Trailing 2 bits are for ECN
+        ttl);
+
+    // port is in LB mode, so it will egress and immediately loop back.
+    // Since it is not re-written, it should hit the pipeline as if it
+    // ingressed on the port, and be properly queued.
+    if (frontPanel) {
+      auto outPort = helper_->ecmpPortDescriptorAt(kEcmpWidth).phyPortID();
+      getHwSwitchEnsemble()->ensureSendPacketOutOfPort(
+          std::move(txPacket), outPort);
+    } else {
+      getHwSwitchEnsemble()->ensureSendPacketSwitched(std::move(txPacket));
+    }
+  }
+
+  folly::IPAddressV6 kSrcIP() {
+    return folly::IPAddressV6("2620:0:1cfe:face:b00c::1");
+  }
+
+  folly::IPAddressV6 kDstIP() {
+    return folly::IPAddressV6("2620:0:1cfe:face:b00c::10");
   }
 
   int16_t kDscp() const {
@@ -46,155 +224,53 @@ class HwDscpQueueMappingTest : public HwLinkStateDependentTest {
     return folly::to<std::string>("dscp", kQueueId(), "_counter");
   }
 
-  MacAddress kDstMac() const {
-    return getPlatform()->getLocalMac();
+  int kQueueIdQosMap() const {
+    return 7;
   }
 
-  void setupECMPForwarding(int ecmpWidth) {
-    utility::EcmpSetupAnyNPorts6 ecmpHelper(getProgrammedState(), kRid);
-    auto newState = ecmpHelper.setupECMPForwarding(
-        ecmpHelper.resolveNextHops(getProgrammedState(), ecmpWidth), ecmpWidth);
-    applyNewState(newState);
+  int kQueueIdAcl() const {
+    return 2;
   }
 
-  std::unique_ptr<facebook::fboss::TxPacket> createUdpPkt(
-      int hopLimit = 64) const {
-    auto vlanId = utility::firstVlanID(initialConfig());
-    auto intfMac = utility::getInterfaceMac(getProgrammedState(), vlanId);
-
-    return utility::makeUDPTxPacket(
-        getHwSwitch(),
-        vlanId,
-        intfMac,
-        intfMac,
-        folly::IPAddressV6("2620:0:1cfe:face:b00c::3"),
-        folly::IPAddressV6("2620:0:1cfe:face:b00c::4"),
-        8000,
-        8001,
-        kDscp() << 2, // Trailing 2 bits are for ECN
-        hopLimit);
-  }
-
-  void sendUdpPkt(int hopLimit = 64) {
-    getHwSwitchEnsemble()->ensureSendPacketSwitched(createUdpPkt(hopLimit));
-  }
-
- private:
-  const RouterID kRid{0};
+  static inline constexpr auto kEcmpWidth = 1;
+  const VlanID kVlanID{utility::kBaseVlanId};
+  const InterfaceID kIntfID{utility::kBaseVlanId};
+  std::unique_ptr<utility::EcmpSetupAnyNPorts6> helper_;
 };
 
-TEST_F(HwDscpQueueMappingTest, CheckDscpQueueMapping) {
-  if (!isSupported(HwAsic::Feature::L3_QOS)) {
-    return;
-  }
-  auto setup = [=]() {
-    setupECMPForwarding(kEcmpWidthForTest());
-    auto kAclName = "acl1";
-    auto newCfg{initialConfig()};
-    utility::addDscpAclToCfg(&newCfg, kAclName, kDscp());
-    utility::addTrafficCounter(&newCfg, kCounterName());
-    utility::addQueueMatcher(&newCfg, kAclName, kQueueId(), kCounterName());
-    applyNewConfig(newCfg);
-  };
-
-  auto verify = [=]() {
-    auto beforeQueueOutPkts = getLatestPortStats(masterLogicalPortIds()[0])
-                                  .get_queueOutPackets_()
-                                  .at(kQueueId());
-    sendUdpPkt();
-    auto afterQueueOutPkts = getLatestPortStats(masterLogicalPortIds()[0])
-                                 .get_queueOutPackets_()
-                                 .at(kQueueId());
-
-    EXPECT_EQ(1, afterQueueOutPkts - beforeQueueOutPkts);
-  };
-
-  verifyAcrossWarmBoots(setup, verify);
+// Verify that traffic arriving on front panel port egresses via queue based on
+// DSCP
+TEST_F(HwDscpQueueMappingTest, VerifyDscpQueueMappingFrontPanel) {
+  verifyDscpQueueMappingHelper(true /* front panel port */);
 }
 
-TEST_F(HwDscpQueueMappingTest, AclAndQosMap) {
-  if (!isSupported(HwAsic::Feature::L3_QOS)) {
-    return;
-  }
-  auto setup = [=]() {
-    setupECMPForwarding(kEcmpWidthForTest());
-    auto newCfg{initialConfig()};
-    utility::addDscpQosPolicy(&newCfg, "qp1", {{kQueueId(), {kDscp()}}});
-    utility::setCPUQosPolicy(&newCfg, "qp1");
-
-    auto* acl = utility::addAcl(&newCfg, "acl0");
-    cfg::Ttl ttl; // Match packets with hop limit > 127
-    std::tie(*ttl.value_ref(), *ttl.mask_ref()) = std::make_tuple(0x80, 0x80);
-    acl->ttl_ref() = ttl;
-    utility::addAclStat(&newCfg, "acl0", kCounterName());
-    applyNewConfig(newCfg);
-  };
-
-  auto verify = [=]() {
-    auto beforeQueueOutPkts = getLatestPortStats(masterLogicalPortIds()[0])
-                                  .get_queueOutPackets_()
-                                  .at(kQueueId());
-    auto beforeAclInOutPkts = utility::getAclInOutPackets(
-        getHwSwitch(), getProgrammedState(), "acl0", kCounterName());
-    sendUdpPkt(255);
-    auto afterQueueOutPkts = getLatestPortStats(masterLogicalPortIds()[0])
-                                 .get_queueOutPackets_()
-                                 .at(kQueueId());
-    auto afterAclInOutPkts = utility::getAclInOutPackets(
-        getHwSwitch(), getProgrammedState(), "acl0", kCounterName());
-    EXPECT_EQ(1, afterQueueOutPkts - beforeQueueOutPkts);
-    EXPECT_EQ(2, afterAclInOutPkts - beforeAclInOutPkts);
-  };
-
-  verifyAcrossWarmBoots(setup, verify);
+// Verify that traffic originating from CPU egresses via queue based on DSCP
+TEST_F(HwDscpQueueMappingTest, VerifyDscpQueueMappingCpu) {
+  verifyDscpQueueMappingHelper(false /* CPU port */);
 }
 
-TEST_F(HwDscpQueueMappingTest, AclAndQosMapConflict) {
-  if (!isSupported(HwAsic::Feature::L3_QOS)) {
-    return;
-  }
-  // The QoS map sends packets to queue 7 while the ACL sends them to queue 2
-  constexpr auto kQueueIdAcl = 2;
-  constexpr auto kQueueIdQosMap = 7;
-  auto setup = [=]() {
-    setupECMPForwarding(kEcmpWidthForTest());
-    auto newCfg{initialConfig()};
-    utility::addDscpQosPolicy(&newCfg, "qp1", {{kQueueIdQosMap, {kDscp()}}});
-    utility::setCPUQosPolicy(&newCfg, "qp1");
-    utility::addDscpAclToCfg(&newCfg, "acl0", kDscp());
-    utility::addTrafficCounter(&newCfg, kCounterName());
-    utility::addQueueMatcher(&newCfg, "acl0", kQueueIdAcl, kCounterName());
-    applyNewConfig(newCfg);
-  };
+// Verify that traffic arriving on front panel port with non-conflicting ACL
+// (counter based on ttl) and QoS Map (DSCP to Queue)
+TEST_F(HwDscpQueueMappingTest, VerifyAclAndQosMapFrontPanel) {
+  verifyAclAndQosMapHelper(true /* front panel port */);
+}
 
-  auto verify = [=]() {
-    auto beforeQueueOutPktsAcl = getLatestPortStats(masterLogicalPortIds()[0])
-                                     .get_queueOutPackets_()
-                                     .at(kQueueIdAcl);
-    auto beforeQueueOutPktsQosMap =
-        getLatestPortStats(masterLogicalPortIds()[0])
-            .get_queueOutPackets_()
-            .at(kQueueIdQosMap);
-    auto beforeAclInOutPkts = utility::getAclInOutPackets(
-        getHwSwitch(), getProgrammedState(), "acl0", kCounterName());
-    sendUdpPkt();
-    auto afterQueueOutPktsAcl = getLatestPortStats(masterLogicalPortIds()[0])
-                                    .get_queueOutPackets_()
-                                    .at(kQueueIdAcl);
-    auto afterQueueOutPktsQosMap = getLatestPortStats(masterLogicalPortIds()[0])
-                                       .get_queueOutPackets_()
-                                       .at(kQueueIdQosMap);
+// Verify that traffic originating from CPU with non-conflicting ACL (counter
+// based on ttl) and QoS Map (DSCP to Queue)
+TEST_F(HwDscpQueueMappingTest, VerifyAclAndQosMapCpu) {
+  verifyAclAndQosMapHelper(false /* CPU port */);
+}
 
-    auto afterAclInOutPkts = utility::getAclInOutPackets(
-        getHwSwitch(), getProgrammedState(), "acl0", kCounterName());
+// Verify that traffic arriving on front panel port with conflicting ACL (DSCP
+// to queue) and QoS Map (DSCP to Queue)
+TEST_F(HwDscpQueueMappingTest, VerifyAclAndQosMapConflictFrontPanel) {
+  verifyAclAndQosMapConflictHelper(true /* front panel port */);
+}
 
-    // The ACL overrides the decision of the QoS map
-    EXPECT_EQ(0, afterQueueOutPktsQosMap - beforeQueueOutPktsQosMap);
-    EXPECT_EQ(1, afterQueueOutPktsAcl - beforeQueueOutPktsAcl);
-    EXPECT_EQ(2, afterAclInOutPkts - beforeAclInOutPkts);
-  };
-
-  verifyAcrossWarmBoots(setup, verify);
+// Verify that traffic originating from CPU port with conflicting ACL (DSCP
+// to queue) and QoS Map (DSCP to Queue)
+TEST_F(HwDscpQueueMappingTest, VerifyAclAndQosMapConflictCpu) {
+  verifyAclAndQosMapConflictHelper(false /* CPU port */);
 }
 
 } // namespace facebook::fboss
