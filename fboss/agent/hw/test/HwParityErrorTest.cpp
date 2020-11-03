31e20931998cbd37c6cc388d93804f0cbffe5e3f
Parvez Shaikh <pshaikh@fb.com>
2020-11-03 12:07:32 -0800

    remove hw test switch utils
    Summary: as titled, instead of this, using methods defined in HwTest itself
    
    Reviewed By: jasmeetbagga
    
    Differential Revision: D24690668
    
    fbshipit-source-id: b269e131f1917a7e2738c8a196cbbb98ff85ef9c

--- fboss/agent/hw/test/HwParityErrorTest.cpp
+++ fboss/agent/hw/test/HwParityErrorTest.cpp
@@ -4,7 +4,6 @@
 
 #include "fboss/agent/hw/HwSwitchStats.h"
 #include "fboss/agent/hw/test/ConfigFactory.h"
-#include "fboss/agent/hw/test/HwTestSwitchUtils.h"
 
 namespace facebook::fboss {
 
@@ -17,15 +16,77 @@ class HwParityErrorTest : public HwLinkStateDependentTest {
   HwSwitchEnsemble::Features featuresDesired() const override {
     return {HwSwitchEnsemble::LINKSCAN, HwSwitchEnsemble::TAM_NOTIFY};
   }
+
+  bool skipTest() {
+    auto asic = getPlatform()->getAsic()->getAsicType();
+    switch (asic) {
+      case HwAsic::AsicType::ASIC_TYPE_FAKE:
+      case HwAsic::AsicType::ASIC_TYPE_MOCK:
+      case HwAsic::AsicType::ASIC_TYPE_TAJO:
+        break;
+      case HwAsic::AsicType::ASIC_TYPE_TRIDENT2:
+      case HwAsic::AsicType::ASIC_TYPE_TOMAHAWK:
+      case HwAsic::AsicType::ASIC_TYPE_TOMAHAWK3:
+      case HwAsic::AsicType::ASIC_TYPE_TOMAHAWK4:
+        return false;
+    }
+    return true;
+  }
+
+  void generateBcmParityError() {
+    std::string out;
+    auto ensemble = getHwSwitchEnsemble();
+    ensemble->runDiagCommand("\n", out);
+    ensemble->runDiagCommand(
+        "ser INJECT memory=L2_ENTRY index=10 pipe=pipe_x\n", out);
+    ensemble->runDiagCommand("d chg L2_ENTRY 10 1\n", out);
+    ensemble->runDiagCommand("quit\n", out);
+    std::ignore = out;
+  }
+
+  void generateParityError() {
+    auto asic = getPlatform()->getAsic()->getAsicType();
+    switch (asic) {
+      case HwAsic::AsicType::ASIC_TYPE_FAKE:
+      case HwAsic::AsicType::ASIC_TYPE_MOCK:
+      case HwAsic::AsicType::ASIC_TYPE_TAJO:
+        XLOG(FATAL) << "Unsupported HwAsic";
+        break;
+      case HwAsic::AsicType::ASIC_TYPE_TRIDENT2:
+      case HwAsic::AsicType::ASIC_TYPE_TOMAHAWK:
+      case HwAsic::AsicType::ASIC_TYPE_TOMAHAWK3:
+      case HwAsic::AsicType::ASIC_TYPE_TOMAHAWK4:
+        generateBcmParityError();
+    }
+  }
+
+  void verifyParityError() {
+    auto stats = getHwSwitchEnsemble()->getHwSwitch()->getSwitchStats();
+    EXPECT_GT(stats->getCorrParityErrorCount(), 0);
+  }
 };
 
 TEST_F(HwParityErrorTest, verifyParityError) {
+  if (skipTest()) {
+// profile is not supported.
+#if defined(GTEST_SKIP)
+    GTEST_SKIP();
+#endif
+    return;
+  }
   auto setup = [=]() { applyNewConfig(initialConfig()); };
   auto verify = [=]() {
-    auto stats = getHwSwitchEnsemble()->getHwSwitch()->getSwitchStats();
+    auto ensemble = getHwSwitchEnsemble();
+    auto stats = ensemble->getHwSwitch()->getSwitchStats();
     EXPECT_EQ(stats->getCorrParityErrorCount(), 0);
-    utility::generateParityError(getHwSwitchEnsemble());
-    utility::verifyParityError(getHwSwitchEnsemble());
+    generateParityError();
+    auto retries = 3;
+    stats = ensemble->getHwSwitch()->getSwitchStats();
+    while (retries-- && stats->getCorrParityErrorCount() == 0) {
+      std::this_thread::sleep_for(std::chrono::milliseconds(100));
+      stats = ensemble->getHwSwitch()->getSwitchStats();
+    }
+    verifyParityError();
   };
   setup();
   verify();
