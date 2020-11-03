2b1e8c58e4749951ad24e670d3333b99b23c335b
Parvez Shaikh <pshaikh@fb.com>
2020-11-03 12:07:32 -0800

    method to generate and verify parity error
    Summary: As titled, implementing stubs defined previously
    
    Reviewed By: jasmeetbagga
    
    Differential Revision: D24629273
    
    fbshipit-source-id: 2531567bce69d60be6d1413e998c906c88c4882f

--- fboss/agent/hw/bcm/tests/HwTestSwitchUtils.cpp
+++ fboss/agent/hw/bcm/tests/HwTestSwitchUtils.cpp
@@ -2,12 +2,25 @@
 
 #include "fboss/agent/hw/test/HwTestSwitchUtils.h"
 
+#include "fboss/agent/hw/HwSwitchStats.h"
+#include "fboss/agent/hw/test/HwSwitchEnsemble.h"
+
+#include <gtest/gtest.h>
+
 namespace facebook::fboss::utility {
-void generateParityError(HwSwitchEnsemble* /*enselble*/) {
-  // TODO: implement this
+void generateParityError(HwSwitchEnsemble* ensemble) {
+  auto stats = ensemble->getHwSwitch()->getSwitchStats();
+  EXPECT_EQ(stats->getCorrParityErrorCount(), 0);
+  std::string out;
+  ensemble->runDiagCommand(
+      "ser INJECT memory=L2_ENTRY index=10 pipe=pipe_x\n", out);
+  ensemble->runDiagCommand("d chg L2_ENTRY 10 1\n", out);
+  std::this_thread::sleep_for(std::chrono::milliseconds(500));
+  std::ignore = out;
 }
 
-void verifyParityError(HwSwitchEnsemble* /*enselble*/) {
-  // TODO: implement this
+void verifyParityError(HwSwitchEnsemble* ensemble) {
+  auto stats = ensemble->getHwSwitch()->getSwitchStats();
+  EXPECT_GT(stats->getCorrParityErrorCount(), 0);
 }
 } // namespace facebook::fboss::utility
--- fboss/agent/hw/test/HwParityErrorTest.cpp
+++ fboss/agent/hw/test/HwParityErrorTest.cpp
@@ -1,26 +1,29 @@
 // Copyright 2004-present Facebook. All Rights Reserved.
 
-#include "fboss/agent/hw/test/HwTest.h"
+#include "fboss/agent/hw/test/HwLinkStateDependentTest.h"
 
+#include "fboss/agent/hw/HwSwitchStats.h"
 #include "fboss/agent/hw/test/ConfigFactory.h"
 #include "fboss/agent/hw/test/HwTestSwitchUtils.h"
 
 namespace facebook::fboss {
 
-class HwParityErrorTest : public HwTest {
+class HwParityErrorTest : public HwLinkStateDependentTest {
  protected:
-  cfg::SwitchConfig initialConfig() const {
+  cfg::SwitchConfig initialConfig() const override {
     return utility::onePortPerVlanConfig(getHwSwitch(), masterLogicalPortIds());
   }
 
   HwSwitchEnsemble::Features featuresDesired() const override {
-    return {HwSwitchEnsemble::TAM_NOTIFY};
+    return {HwSwitchEnsemble::LINKSCAN, HwSwitchEnsemble::TAM_NOTIFY};
   }
 };
 
 TEST_F(HwParityErrorTest, verifyParityError) {
   auto setup = [=]() { applyNewConfig(initialConfig()); };
   auto verify = [=]() {
+    auto stats = getHwSwitchEnsemble()->getHwSwitch()->getSwitchStats();
+    EXPECT_EQ(stats->getCorrParityErrorCount(), 0);
     utility::generateParityError(getHwSwitchEnsemble());
     utility::verifyParityError(getHwSwitchEnsemble());
   };
