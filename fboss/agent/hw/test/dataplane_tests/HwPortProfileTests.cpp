9ab9cd005ffa35d5104f7f41d261e6f52a37e351
Parvez Shaikh <pshaikh@fb.com>
2020-11-06 08:29:36 -0800

    refactoring profile test for qsfp override
    Summary:
    refactored test code to support warm boot verification.
    
    note that this test relies on override transceiver information which should be available across warm boot. this can be enabled by overriding port2transceiverInfoMap method
    
    But this method does not take profile argument. But the returned transceiver information map depends on profile argument. So shifted the profile argument from method to test fixture.
    
    So HwPortProfileTest is now template
    
    Defined a macro to give provide tests for different profiles.
    
    Reviewed By: jasmeetbagga
    
    Differential Revision: D24656102
    
    fbshipit-source-id: 31b799263c3acda4d95f0508868c7c3f2fbbf61a

--- fboss/agent/hw/test/dataplane_tests/HwPortProfileTests.cpp
+++ fboss/agent/hw/test/dataplane_tests/HwPortProfileTests.cpp
@@ -8,6 +8,7 @@
 #include "fboss/agent/hw/test/ConfigFactory.h"
 
 namespace facebook::fboss {
+template <cfg::PortProfileID Profile>
 class HwPortProfileTest : public HwLinkStateDependentTest {
  protected:
   cfg::SwitchConfig initialConfig() const override {
@@ -19,8 +20,8 @@ class HwPortProfileTest : public HwLinkStateDependentTest {
         lbMode);
   }
 
-  bool skipTest(cfg::PortProfileID profile) {
-    if (!getPlatform()->getPortProfileConfig(profile)) {
+  bool skipTest() {
+    if (!getPlatform()->getPortProfileConfig(Profile)) {
       return true;
     }
     auto& platformPorts = getPlatform()->getPlatformPorts();
@@ -29,7 +30,7 @@ class HwPortProfileTest : public HwLinkStateDependentTest {
       if (iter == platformPorts.end()) {
         return true;
       }
-      if (iter->second.supportedProfiles_ref()->find(profile) ==
+      if (iter->second.supportedProfiles_ref()->find(Profile) ==
           iter->second.supportedProfiles_ref()->end()) {
         return true;
       }
@@ -53,13 +54,8 @@ class HwPortProfileTest : public HwLinkStateDependentTest {
     // (TODO): verify lane count (for sai)
   }
 
-  void setupPort2OverrideTransceiverInfo(cfg::PortProfileID profile) {
-    auto tech = utility::getMediaType(profile);
-    getPlatform()->setPort2OverrideTransceiverInfo(port2transceiverInfo(tech));
-  }
-
-  void testProfile(cfg::PortProfileID profile) {
-    if (skipTest(profile)) {
+  void runTest() {
+    if (skipTest()) {
 // profile is not supported.
 #if defined(GTEST_SKIP)
       GTEST_SKIP();
@@ -67,12 +63,11 @@ class HwPortProfileTest : public HwLinkStateDependentTest {
       return;
     }
     auto setup = [=]() {
-      setupPort2OverrideTransceiverInfo(profile);
       auto config = initialConfig();
       for (auto port : {masterLogicalPortIds()[0], masterLogicalPortIds()[1]}) {
         auto hwSwitch = getHwSwitch();
         utility::configurePortProfile(
-            *hwSwitch, config, profile, getAllPortsInGroup(port));
+            *hwSwitch, config, Profile, getAllPortsInGroup(port));
       }
       applyNewConfig(config);
     };
@@ -87,16 +82,14 @@ class HwPortProfileTest : public HwLinkStateDependentTest {
         utility::verifyLedStatus(getHwSwitchEnsemble(), portID, up);
       }
     };
-    // TODO: fix warm boot.
-    // auto setupPostWb = [=]() { setupPort2OverrideTransceiverInfo(profile); };
-    // verifyAcrossWarmBoots(setup, verify, setupPostWb, verify);
     setup();
     verify();
   }
 
-  std::map<PortID, TransceiverInfo> port2transceiverInfo(
-      TransmitterTechnology tech) const {
+  std::optional<std::map<PortID, TransceiverInfo>> port2transceiverInfoMap()
+      const override {
     std::map<PortID, TransceiverInfo> result{};
+    auto tech = utility::getMediaType(Profile);
     result.emplace(
         masterLogicalPortIds()[0],
         utility::getTransceiverInfo(masterLogicalPortIds()[0], tech));
@@ -107,96 +100,55 @@ class HwPortProfileTest : public HwLinkStateDependentTest {
   }
 };
 
-TEST_F(HwPortProfileTest, PROFILE_10G_1_NRZ_NOFEC_COPPER) {
-  testProfile(cfg::PortProfileID::PROFILE_10G_1_NRZ_NOFEC_COPPER);
-}
+#define TEST_PROFILE(PROFILE)                                     \
+  struct HwTest_##PROFILE                                         \
+      : public HwPortProfileTest<cfg::PortProfileID::PROFILE> {}; \
+  TEST_F(HwTest_##PROFILE, TestProfile) {                         \
+    runTest();                                                    \
+  }
 
-TEST_F(HwPortProfileTest, PROFILE_10G_1_NRZ_NOFEC_OPTICAL) {
-  testProfile(cfg::PortProfileID::PROFILE_10G_1_NRZ_NOFEC_OPTICAL);
-}
+TEST_PROFILE(PROFILE_10G_1_NRZ_NOFEC_COPPER)
 
-TEST_F(HwPortProfileTest, PROFILE_25G_1_NRZ_NOFEC_COPPER) {
-  testProfile(cfg::PortProfileID::PROFILE_25G_1_NRZ_NOFEC_COPPER);
-}
+TEST_PROFILE(PROFILE_10G_1_NRZ_NOFEC_OPTICAL)
 
-TEST_F(HwPortProfileTest, PROFILE_25G_1_NRZ_CL74_COPPER) {
-  testProfile(cfg::PortProfileID::PROFILE_25G_1_NRZ_CL74_COPPER);
-}
+TEST_PROFILE(PROFILE_25G_1_NRZ_NOFEC_COPPER)
 
-TEST_F(HwPortProfileTest, PROFILE_25G_1_NRZ_RS528_COPPER) {
-  testProfile(cfg::PortProfileID::PROFILE_25G_1_NRZ_RS528_COPPER);
-}
+TEST_PROFILE(PROFILE_25G_1_NRZ_CL74_COPPER)
 
-TEST_F(HwPortProfileTest, PROFILE_40G_4_NRZ_NOFEC_COPPER) {
-  testProfile(cfg::PortProfileID::PROFILE_40G_4_NRZ_NOFEC_COPPER);
-}
+TEST_PROFILE(PROFILE_25G_1_NRZ_RS528_COPPER)
 
-TEST_F(HwPortProfileTest, PROFILE_40G_4_NRZ_NOFEC_OPTICAL) {
-  testProfile(cfg::PortProfileID::PROFILE_40G_4_NRZ_NOFEC_OPTICAL);
-}
+TEST_PROFILE(PROFILE_40G_4_NRZ_NOFEC_COPPER)
 
-TEST_F(HwPortProfileTest, PROFILE_50G_2_NRZ_NOFEC_COPPER) {
-  testProfile(cfg::PortProfileID::PROFILE_50G_2_NRZ_NOFEC_COPPER);
-}
+TEST_PROFILE(PROFILE_40G_4_NRZ_NOFEC_OPTICAL)
 
-TEST_F(HwPortProfileTest, PROFILE_50G_2_NRZ_CL74_COPPER) {
-  testProfile(cfg::PortProfileID::PROFILE_50G_2_NRZ_CL74_COPPER);
-}
+TEST_PROFILE(PROFILE_50G_2_NRZ_NOFEC_COPPER)
 
-TEST_F(HwPortProfileTest, PROFILE_50G_2_NRZ_RS528_COPPER) {
-  testProfile(cfg::PortProfileID::PROFILE_50G_2_NRZ_RS528_COPPER);
-}
+TEST_PROFILE(PROFILE_50G_2_NRZ_CL74_COPPER)
 
-TEST_F(HwPortProfileTest, PROFILE_100G_4_NRZ_RS528_COPPER) {
-  testProfile(cfg::PortProfileID::PROFILE_100G_4_NRZ_RS528_COPPER);
-}
+TEST_PROFILE(PROFILE_50G_2_NRZ_RS528_COPPER)
 
-TEST_F(HwPortProfileTest, PROFILE_100G_4_NRZ_RS528_OPTICAL) {
-  testProfile(cfg::PortProfileID::PROFILE_100G_4_NRZ_RS528_OPTICAL);
-}
+TEST_PROFILE(PROFILE_100G_4_NRZ_RS528_COPPER)
 
-TEST_F(HwPortProfileTest, PROFILE_100G_4_NRZ_CL91_COPPER) {
-  testProfile(cfg::PortProfileID::PROFILE_100G_4_NRZ_CL91_COPPER);
-}
+TEST_PROFILE(PROFILE_100G_4_NRZ_RS528_OPTICAL)
 
-TEST_F(HwPortProfileTest, PROFILE_100G_4_NRZ_CL91_OPTICAL) {
-  testProfile(cfg::PortProfileID::PROFILE_100G_4_NRZ_CL91_OPTICAL);
-}
+TEST_PROFILE(PROFILE_100G_4_NRZ_CL91_COPPER)
 
-TEST_F(HwPortProfileTest, PROFILE_25G_1_NRZ_NOFEC_OPTICAL) {
-  testProfile(cfg::PortProfileID::PROFILE_25G_1_NRZ_NOFEC_OPTICAL);
-}
+TEST_PROFILE(PROFILE_100G_4_NRZ_CL91_OPTICAL)
 
-TEST_F(HwPortProfileTest, PROFILE_50G_2_NRZ_NOFEC_OPTICAL) {
-  testProfile(cfg::PortProfileID::PROFILE_50G_2_NRZ_NOFEC_OPTICAL);
-}
+TEST_PROFILE(PROFILE_25G_1_NRZ_NOFEC_OPTICAL)
 
-TEST_F(HwPortProfileTest, PROFILE_100G_4_NRZ_NOFEC_COPPER) {
-  testProfile(cfg::PortProfileID::PROFILE_100G_4_NRZ_NOFEC_COPPER);
-}
+TEST_PROFILE(PROFILE_50G_2_NRZ_NOFEC_OPTICAL)
 
-/*
-TEST_F(HwPortProfileTest, PROFILE_20G_2_NRZ_NOFEC_COPPER) {
-  // TODO: investigate and fix failures
-  testProfile(cfg::PortProfileID::PROFILE_20G_2_NRZ_NOFEC_COPPER);
-}
+TEST_PROFILE(PROFILE_100G_4_NRZ_NOFEC_COPPER)
 
-TEST_F(HwPortProfileTest, PROFILE_20G_2_NRZ_NOFEC_OPTICAL) {
-  // TODO: investigate and fix failures
-  testProfile(cfg::PortProfileID::PROFILE_20G_2_NRZ_NOFEC_OPTICAL);
-}
-*/
+// TODO: investigate and fix failures
+// TEST_PROFILE(PROFILE_20G_2_NRZ_NOFEC_COPPER)
+// TEST_PROFILE(PROFILE_20G_2_NRZ_NOFEC_OPTICAL)
 
-TEST_F(HwPortProfileTest, PROFILE_200G_4_PAM4_RS544X2N_COPPER) {
-  testProfile(cfg::PortProfileID::PROFILE_200G_4_PAM4_RS544X2N_COPPER);
-}
+TEST_PROFILE(PROFILE_200G_4_PAM4_RS544X2N_COPPER)
 
-TEST_F(HwPortProfileTest, PROFILE_200G_4_PAM4_RS544X2N_OPTICAL) {
-  testProfile(cfg::PortProfileID::PROFILE_200G_4_PAM4_RS544X2N_OPTICAL);
-}
+TEST_PROFILE(PROFILE_200G_4_PAM4_RS544X2N_OPTICAL)
 
-TEST_F(HwPortProfileTest, PROFILE_400G_8_PAM4_RS544X2N_OPTICAL) {
-  testProfile(cfg::PortProfileID::PROFILE_400G_8_PAM4_RS544X2N_OPTICAL);
-}
+TEST_PROFILE(PROFILE_400G_8_PAM4_RS544X2N_OPTICAL)
 
 } // namespace facebook::fboss
