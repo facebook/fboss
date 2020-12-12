c1b8e2057f4e508f795ad28becb5521235f6dece
Jasmeet Bagga <jasmeetbagga@fb.com>
2020-12-11 16:45:53 -0800

    Rollback non ecmp and rollback many times test cases
    Summary:
    As tittled. Code refactor for better reuse.
    Rollback many times to stress test rollbacks
    
    Differential Revision: D25364085
    
    fbshipit-source-id: 223eed3e6167dc21a6e0905d086668bff6d941d0

--- fboss/agent/hw/sai/hw_test/SaiRouteRollbackTests.cpp
+++ fboss/agent/hw/sai/hw_test/SaiRouteRollbackTests.cpp
@@ -23,7 +23,7 @@ constexpr auto kEcmpWidth = 2;
 
 namespace facebook::fboss {
 class SaiRouteRollbackTest : public SaiRollbackTest {
- protected:
+ private:
   RoutePrefixV4 kV4Prefix1() {
     return RoutePrefixV4{folly::IPAddressV4("1.0.0.0"), 24};
   }
@@ -40,23 +40,29 @@ class SaiRouteRollbackTest : public SaiRollbackTest {
     applyNewState(v4EcmpHelper_->resolveNextHops(getProgrammedState(), width));
     applyNewState(v6EcmpHelper_->resolveNextHops(getProgrammedState(), width));
   }
+  std::shared_ptr<SwitchState> ecmpRoutesState(
+      const std::shared_ptr<SwitchState>& in) {
+    auto newState =
+        v4EcmpHelper_->setupECMPForwarding(in, kEcmpWidth, {kV4Prefix1()});
+    return v6EcmpHelper_->setupECMPForwarding(
+        newState, kEcmpWidth, {kV6Prefix1()});
+  }
+  std::shared_ptr<SwitchState> nonEcmpRoutesState(
+      const std::shared_ptr<SwitchState>& in) {
+    auto newState = v4EcmpHelper_->setupECMPForwarding(in, 1, {kV4Prefix2()});
+    return v6EcmpHelper_->setupECMPForwarding(newState, 1, {kV6Prefix2()});
+  }
+
   void setupEcmpRoutes() {
-    resolveNextHops(kEcmpWidth);
     // ECMP routes
-    applyNewState(v4EcmpHelper_->setupECMPForwarding(
-        getProgrammedState(), kEcmpWidth, {kV4Prefix1()}));
-    applyNewState(v6EcmpHelper_->setupECMPForwarding(
-        getProgrammedState(), kEcmpWidth, {kV6Prefix1()}));
+    applyNewState(ecmpRoutesState(getProgrammedState()));
   }
 
   void setupNonEcmpRoutes() {
-    resolveNextHops(1);
     // Non ECMP routes
-    applyNewState(v4EcmpHelper_->setupECMPForwarding(
-        getProgrammedState(), 1, {kV4Prefix2()}));
-    applyNewState(v6EcmpHelper_->setupECMPForwarding(
-        getProgrammedState(), 1, {kV6Prefix2()}));
+    applyNewState(nonEcmpRoutesState(getProgrammedState()));
   }
+
   void SetUp() override {
     SaiRollbackTest::SetUp();
     v4EcmpHelper_ = std::make_unique<utility::EcmpSetupAnyNPorts4>(
@@ -64,28 +70,51 @@ class SaiRouteRollbackTest : public SaiRollbackTest {
     v6EcmpHelper_ = std::make_unique<utility::EcmpSetupAnyNPorts6>(
         getProgrammedState(), RouterID(0));
   }
+
+ protected:
+  void runTest(bool rollbackEcmp, bool rollbackNonEcmp, int numIters) {
+    auto verify = [=]() {
+      resolveNextHops(kEcmpWidth);
+      for (auto i = 0; i < numIters; ++i) {
+        // Cache rollback states
+        auto noRouteState = getProgrammedState();
+        auto ecmpRoutesOnlyState = ecmpRoutesState(getProgrammedState());
+        auto nonEcmpRoutesOnlyState = nonEcmpRoutesState(getProgrammedState());
+        // Program Routes
+        setupEcmpRoutes();
+        setupNonEcmpRoutes();
+        // Rollback
+        // TODO verify routess post rollback
+        if (rollbackEcmp && rollbackNonEcmp) {
+          rollback(noRouteState);
+        } else if (rollbackEcmp) {
+          rollback(nonEcmpRoutesOnlyState);
+        } else if (rollbackNonEcmp) {
+          rollback(ecmpRoutesOnlyState);
+        }
+      }
+    };
+    verifyAcrossWarmBoots([]() {}, verify);
+  }
+
+ private:
   std::unique_ptr<utility::EcmpSetupAnyNPorts4> v4EcmpHelper_;
   std::unique_ptr<utility::EcmpSetupAnyNPorts6> v6EcmpHelper_;
 };
 
 TEST_F(SaiRouteRollbackTest, rollbackAll) {
-  auto verify = [this]() {
-    auto noRouteState = getProgrammedState();
-    setupEcmpRoutes();
-    setupNonEcmpRoutes();
-    rollback(noRouteState);
-  };
-  verifyAcrossWarmBoots([]() {}, verify);
+  runTest(true, true, 1);
 }
 
 TEST_F(SaiRouteRollbackTest, rollbackNonEcmp) {
-  auto verify = [this]() {
-    setupEcmpRoutes();
-    auto rollbackState = getProgrammedState();
-    setupNonEcmpRoutes();
-    rollback(rollbackState);
-  };
-  verifyAcrossWarmBoots([]() {}, verify);
+  runTest(false, true, 1);
 }
 
+TEST_F(SaiRouteRollbackTest, rollbackEcmp) {
+  runTest(true, false, 1);
+}
+
+TEST_F(SaiRouteRollbackTest, rollbackManyTimes) {
+  runTest(true, true, 10);
+}
 } // namespace facebook::fboss
