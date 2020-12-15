65e1417a64fafe38fabf58081b7820b574c76b19
Chenfei Lin <clin82@fb.com>
2020-12-14 23:06:54 -0800

    Add MinipackBaseSystemContainer class
    Summary: As titled. Align with the design of our brother platform family.
    
    Differential Revision: D24107390
    
    fbshipit-source-id: 21edcb8362d170845abcaecbc226a815f83d134d

---
+++ fboss/lib/fpga/MinipackBaseSystemContainer.cpp
@@ -0,0 +1,36 @@
+// Copyright 2004-present Facebook. All Rights Reserved.
+
+#include "fboss/lib/fpga/MinipackBaseSystemContainer.h"
+
+namespace facebook::fboss {
+
+void MinipackBaseSystemContainer::initHW() {
+  if (isHwInitialized_) {
+    return;
+  }
+  fpgaDevice_->mmap();
+  isHwInitialized_ = true;
+}
+
+uint32_t MinipackBaseSystemContainer::getPimOffset(int pim) {
+  // To avoid ambiguity, we explicitly decided the pim number starts from 2.
+  return kFacebookFpgaPimBase + kFacebookFpgaPimSize * (pim - 2);
+}
+
+MinipackBasePimContainer* MinipackBaseSystemContainer::getPimContainer(
+    int pim) const {
+  // To avoid ambiguity, we explicitly decided the pim number starts from 2.
+  CHECK(pim >= 2 && pim <= kNumberPim + 1);
+  return pims_[pim - 2].get();
+}
+
+std::vector<MinipackBasePimContainer*>
+MinipackBaseSystemContainer::getAllPimContainers() {
+  std::vector<MinipackBasePimContainer*> pims;
+  for (const auto& pim : pims_) {
+    pims.push_back(pim.get());
+  }
+  return pims;
+}
+
+} // namespace facebook::fboss
---
+++ fboss/lib/fpga/MinipackBaseSystemContainer.h
@@ -0,0 +1,39 @@
+// Copyright 2004-present Facebook. All Rights Reserved.
+
+#pragma once
+
+#include "fboss/lib/fpga/FpgaDevice.h"
+#include "fboss/lib/fpga/MinipackBasePimContainer.h"
+
+namespace facebook::fboss {
+// The base system container for Minipack switch family.
+class MinipackBaseSystemContainer {
+ public:
+  virtual ~MinipackBaseSystemContainer() {}
+  const static int kPimStartNum = 2;
+  const static int kNumberPim = 8;
+  /*
+   * This function should be called before any access attempt
+   * to any HW devices.
+   * For now, it just allocate FPGA memory map to get it ready.
+   */
+  void initHW();
+
+  // To avoid ambiguity, we explicitly decided the pim number starts from 2.
+  uint32_t getPimOffset(int pim);
+  virtual MinipackBasePimContainer* getPimContainer(int pim) const;
+  virtual std::vector<MinipackBasePimContainer*> getAllPimContainers();
+
+ protected:
+  // FPGA Device Info
+  static constexpr uint32_t kFacebookFpgaVendorID = 0x1d9b;
+  static constexpr uint32_t kFacebookFpgaSmbSize = 0x40000;
+  static constexpr uint32_t kFacebookFpgaPimBase = 0x40000;
+  static constexpr uint32_t kFacebookFpgaPimSize = 0x8000;
+
+  std::array<std::unique_ptr<MinipackBasePimContainer>, kNumberPim> pims_;
+  std::unique_ptr<FpgaDevice> fpgaDevice_;
+  bool isHwInitialized_{false};
+};
+
+} // namespace facebook::fboss
