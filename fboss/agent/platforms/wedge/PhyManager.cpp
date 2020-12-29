0bdaae6f9e695faac2287f81694a5f70ca2fbf9c
Chet Powers <ccpowers@fb.com>
2020-12-29 11:02:21 -0800

    move phymanager out of facebook dir
    Summary: In a later diff I wanted to use PhyManager in Transceivermanager, but that breaks the oss build until it's moved out of the facebook/ dir
    
    Differential Revision: D25717269
    
    fbshipit-source-id: 4032cf3e8defb7922f09b9d8747ce17d35de1eab

---
+++ fboss/agent/platforms/wedge/PhyManager.cpp
@@ -0,0 +1,122 @@
+// Copyright 2004-present Facebook. All Rights Reserved.
+
+#include "fboss/agent/platforms/wedge/PhyManager.h"
+#include <folly/logging/xlog.h>
+
+namespace facebook {
+namespace fboss {
+
+/*
+ * getExternalPhy
+ *
+ * This function returns the ExternalPhy object for the given slot number,
+ * mdio controller id and phy id.
+ */
+phy::ExternalPhy*
+PhyManager::getExternalPhy(int slotId, int mdioId, int phyId) {
+  // Check if the slot id is valid
+  if (slotId >= numOfSlot_ || slotId >= externalPhyMap_.size()) {
+    throw FbossError(
+        folly::sformat("getExternalPhy: Invalid Slot Id {:d}", slotId));
+  }
+  // Check if within the slot, the MDIO based phy list is populated
+  if (mdioId >= numMdioController_[slotId] ||
+      mdioId >= externalPhyMap_[slotId].size()) {
+    throw FbossError(
+        folly::sformat("getExternalPhy: Invalid Mdio Id {:d}", mdioId));
+  }
+  // Check if within the slot and mdio id, the phy object for the phy id exist
+  if (phyId >= externalPhyMap_[slotId][mdioId].size()) {
+    throw FbossError(
+        folly::sformat("getExternalPhy: Invalid Phy Id {:d}", phyId));
+  }
+  // Return the externalPhy object for this slot, mdio, phy
+  return externalPhyMap_[slotId][mdioId][phyId].get();
+}
+
+/*
+ * programOnePort
+ *
+ * Calls the ExternalPhy programOnePort function. This function will be called
+ * by application like agent.
+ */
+void PhyManager::programOnePort(
+    int slotId,
+    int mdioId,
+    int phyId,
+    phy::PhyPortConfig config) {
+  auto xphy = getExternalPhy(slotId, mdioId, phyId);
+  // Call the ExternalPhy function
+  xphy->programOnePort(config);
+};
+
+/*
+ * setPortPrbs
+ *
+ * Calls the ExternalPhy setPortPrbs function. This function will be called
+ * by application like agent.
+ */
+void PhyManager::setPortPrbs(
+    int slotId,
+    int mdioId,
+    int phyId,
+    phy::PhyPortConfig config,
+    phy::Side side,
+    bool enable,
+    int32_t polynominal) {
+  auto xphy = getExternalPhy(slotId, mdioId, phyId);
+  // Call the ExternalPhy function
+  xphy->setPortPrbs(config, side, enable, polynominal);
+};
+
+/*
+ * getPortStats
+ *
+ * Calls the ExternalPhy getPortStats function. This function will be called
+ * by application like agent.
+ */
+phy::ExternalPhyPortStats PhyManager::getPortStats(
+    int slotId,
+    int mdioId,
+    int phyId,
+    phy::PhyPortConfig config) {
+  auto xphy = getExternalPhy(slotId, mdioId, phyId);
+  // Call the ExternalPhy function
+  return xphy->getPortStats(config);
+};
+
+/*
+ * getPortPrbsStats
+ *
+ * Calls the ExternalPhy getPortPrbsStats function. This function will be called
+ * by application like agent.
+ */
+phy::ExternalPhyPortStats PhyManager::getPortPrbsStats(
+    int slotId,
+    int mdioId,
+    int phyId,
+    phy::PhyPortConfig config) {
+  auto xphy = getExternalPhy(slotId, mdioId, phyId);
+  // Call the ExternalPhy function
+  return xphy->getPortPrbsStats(config);
+};
+
+/*
+ * getLaneSpeed
+ *
+ * Calls the ExternalPhy getLaneSpeed function. This function will be called
+ * by application like agent.
+ */
+float_t PhyManager::getLaneSpeed(
+    int slotId,
+    int mdioId,
+    int phyId,
+    phy::PhyPortConfig config,
+    phy::Side side) {
+  auto xphy = getExternalPhy(slotId, mdioId, phyId);
+  // Call the ExternalPhy function
+  return xphy->getLaneSpeed(config, side);
+};
+
+} // namespace fboss
+} // namespace facebook
---
+++ fboss/agent/platforms/wedge/PhyManager.h
@@ -0,0 +1,102 @@
+// Copyright 2004-present Facebook. All Rights Reserved.
+
+#pragma once
+
+#include <map>
+#include <vector>
+#include "fboss/agent/FbossError.h"
+#include "fboss/lib/phy/ExternalPhy.h"
+#include "fboss/mdio/Mdio.h"
+
+namespace facebook {
+namespace fboss {
+
+class PhyManager {
+ public:
+  virtual ~PhyManager() {}
+  /*
+   * This function initializes all the PHY objects for a given chassis. The PHY
+   * objects are kept per slot, per MDIO controller, per phy address. This
+   * needs to be defined by inheriting classes.
+   */
+  virtual bool initExternalPhyMap() {
+    return false;
+  }
+  /*
+   * This function returns the ExternalPhy object for the given slot number,
+   * mdio controller id and phy id.
+   */
+  phy::ExternalPhy* getExternalPhy(int slotId, int mdioId, int phyId);
+
+  /*
+   * This function calls ExternalPhy function programOnePort for the given
+   * slot id, mdio id, phy id
+   */
+  void
+  programOnePort(int slotId, int mdioId, int phyId, phy::PhyPortConfig config);
+
+  /*
+   * This function calls ExternalPhy function setPortPrbs for the given
+   * slot id, mdio id, phy id
+   */
+  void setPortPrbs(
+      int slotId,
+      int mdioId,
+      int phyId,
+      phy::PhyPortConfig config,
+      phy::Side side,
+      bool enable,
+      int32_t polynominal);
+
+  /*
+   * This function calls ExternalPhy function getPortStats for the given
+   * slot id, mdio id, phy id
+   */
+  phy::ExternalPhyPortStats
+  getPortStats(int slotId, int mdioId, int phyId, phy::PhyPortConfig config);
+
+  /*
+   * This function calls ExternalPhy function getPortPrbsStats for the given
+   * slot id, mdio id, phy id
+   */
+  phy::ExternalPhyPortStats getPortPrbsStats(
+      int slotId,
+      int mdioId,
+      int phyId,
+      phy::PhyPortConfig config);
+
+  /*
+   * This function calls ExternalPhy function getLaneSpeed for the given
+   * slot id, mdio id, phy id
+   */
+  float_t getLaneSpeed(
+      int slotId,
+      int mdioId,
+      int phyId,
+      phy::PhyPortConfig config,
+      phy::Side side);
+
+  /*
+   * A virtual function for the ExternalPhy obejcts. The sub-class needs to
+   * implement this function. The implementation will be different for
+   * Minipack abd Yamp. If the Phy code is in same process then that should
+   * called PhyManager function otherwise it should  be a thrift call to port
+   * service process
+   */
+  virtual void initializeSlotPhys(int /* slotId */, bool /* warmboot */) {}
+
+ protected:
+  // Number of slot in the platform
+  int numOfSlot_;
+  // Number of MDIO controller in each slot
+  std::map<int, int> numMdioController_;
+
+  // List of ExternalPhy objects for the chassis
+  // This is 3D array of ExternalPhy objects which is per PIM, per MDIO
+  // controller, Per PHY endpoint
+  std::map<int, std::vector<std::vector<std::unique_ptr<phy::ExternalPhy>>>>
+      externalPhyMap_;
+};
+
+} // namespace fboss
+} // namespace facebook
