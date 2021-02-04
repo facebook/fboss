ebcd31f2a2094885b13b21f30b394c199e100777
Parvez Shaikh <pshaikh@fb.com>
2021-02-04 15:42:27 -0800

    LAG store tests
    Summary: As titled
    
    Differential Revision: D26232565
    
    fbshipit-source-id: 41fa68d6afd6468364c9ea5b65942375fa245715

---
+++ fboss/agent/hw/sai/store/tests/LagStoreTest.cpp
@@ -0,0 +1,142 @@
+// Copyright 2004-present Facebook. All Rights Reserved.
+
+#include "fboss/agent/hw/sai/api/LagApi.h"
+#include "fboss/agent/hw/sai/fake/FakeSai.h"
+#include "fboss/agent/hw/sai/store/SaiObject.h"
+#include "fboss/agent/hw/sai/store/SaiStore.h"
+#include "fboss/agent/hw/sai/store/tests/SaiStoreTest.h"
+
+class LagStoreTest : public SaiStoreTest {
+ public:
+  void SetUp() override {
+    SaiStoreTest::SetUp();
+  }
+  bool hasMember(LagSaiId lag, LagMemberSaiId member) {
+    auto members = saiApiTable->lagApi().getAttribute(
+        lag, SaiLagTraits::Attributes::PortList{});
+    for (auto entry : members) {
+      if (member == entry) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  LagSaiId createLag(std::string label) {
+    auto lagId = saiApiTable->lagApi().create<SaiLagTraits>({}, 0);
+    std::array<char, 32> data{};
+    std::copy(std::begin(label), std::end(label), std::begin(data));
+    saiApiTable->lagApi().setAttribute(
+        lagId, SaiLagTraits::Attributes::Label(data));
+    return lagId;
+  }
+
+  LagMemberSaiId createLagMember(LagSaiId lagId, sai_object_id_t port) {
+    return saiApiTable->lagApi().create<SaiLagMemberTraits>(
+        {static_cast<sai_object_id_t>(lagId), port}, 0);
+  }
+};
+
+TEST_F(LagStoreTest, setObject) {
+  SaiStore s(0);
+  s.reload();
+  std::array<char, 32> labelValue{"lag0"};
+  SaiLagTraits::Attributes::Label label{labelValue};
+  auto lag = s.get<SaiLagTraits>().setObject(label, {});
+  std::vector<std::shared_ptr<SaiObject<SaiLagMemberTraits>>> members;
+  for (auto i : {1, 2, 3, 4}) {
+    SaiLagMemberTraits::CreateAttributes createAttrs{lag->adapterKey(), i};
+    members.push_back(
+        s.get<SaiLagMemberTraits>().setObject(createAttrs, createAttrs));
+  }
+  for (auto member : members) {
+    EXPECT_TRUE(hasMember(lag->adapterKey(), member->adapterKey()));
+  }
+}
+
+TEST_F(LagStoreTest, updateObject) {
+  SaiStore s(0);
+  s.reload();
+  std::array<char, 32> labelValue{"lag0"};
+  SaiLagTraits::Attributes::Label label{labelValue};
+  auto lag = s.get<SaiLagTraits>().setObject(label, {});
+  std::vector<std::shared_ptr<SaiObject<SaiLagMemberTraits>>> members;
+  std::vector<LagMemberSaiId> memberIds;
+  for (auto i : {1, 2, 3, 4}) {
+    SaiLagMemberTraits::CreateAttributes createAttrs{lag->adapterKey(), i};
+    auto member =
+        s.get<SaiLagMemberTraits>().setObject(createAttrs, createAttrs);
+    memberIds.push_back(member->adapterKey());
+    members.push_back(std::move(member));
+  }
+  for (auto memberId : memberIds) {
+    EXPECT_TRUE(hasMember(lag->adapterKey(), memberId));
+  }
+  members.pop_back();
+  auto i = 0;
+  for (auto memberId : memberIds) {
+    if (i < 3) {
+      EXPECT_TRUE(hasMember(lag->adapterKey(), memberIds[i++]));
+    } else {
+      EXPECT_FALSE(hasMember(lag->adapterKey(), memberIds[i++]));
+    }
+  }
+}
+
+TEST_F(LagStoreTest, loadLags) {
+  auto id0 = createLag("a");
+  auto id1 = createLag("b");
+
+  SaiStore s(0);
+  s.reload();
+  auto& store = s.get<SaiLagTraits>();
+
+  auto got =
+      store.get(SaiLagTraits::Attributes::Label{std::array<char, 32>({"a"})});
+  EXPECT_EQ(got->adapterKey(), id0);
+  got = store.get(SaiLagTraits::Attributes::Label{std::array<char, 32>({"b"})});
+  EXPECT_EQ(got->adapterKey(), id1);
+}
+
+TEST_F(LagStoreTest, loadLagMembers) {
+  auto id0 = createLag("a");
+  auto member0 = createLagMember(id0, 1);
+  auto member1 = createLagMember(id0, 2);
+  SaiStore s(0);
+  s.reload();
+  auto& store = s.get<SaiLagMemberTraits>();
+  SaiLagMemberTraits::AdapterHostKey ahk0{
+      static_cast<sai_object_id_t>(id0), static_cast<sai_object_id_t>(1)};
+  auto got = store.get(ahk0);
+  EXPECT_EQ(got->adapterKey(), member0);
+  SaiLagMemberTraits::AdapterHostKey ahk1{
+      static_cast<sai_object_id_t>(id0), static_cast<sai_object_id_t>(2)};
+  got = store.get(ahk1);
+  EXPECT_EQ(got->adapterKey(), member1);
+}
+
+TEST_F(LagStoreTest, toAndFromDynamic) {
+  auto id0 = createLag("a");
+  createLagMember(id0, 1);
+  createLagMember(id0, 2);
+  SaiStore s(0);
+  s.reload();
+  auto& lagStore = s.get<SaiLagTraits>();
+
+  auto lagAdapterHostKey =
+      SaiLagTraits::Attributes::Label{std::array<char, 32>({"a"})};
+  auto got = lagStore.get(lagAdapterHostKey);
+
+  auto k0 = got->adapterHostKeyToFollyDynamic();
+  auto k1 = SaiObject<SaiLagTraits>::follyDynamicToAdapterHostKey(k0);
+  EXPECT_EQ(k1, lagAdapterHostKey.value());
+
+  auto ak2AhkJson = s.adapterKeys2AdapterHostKeysFollyDynamic();
+  auto& lagAk2AhkJson = ak2AhkJson[saiObjectTypeToString(SAI_OBJECT_TYPE_LAG)];
+  EXPECT_TRUE(!lagAk2AhkJson.empty());
+  EXPECT_EQ(lagAk2AhkJson.size(), 1);
+
+  auto iter = lagAk2AhkJson.find(folly::to<std::string>(got->adapterKey()));
+  EXPECT_FALSE(lagAk2AhkJson.items().end() == iter);
+  EXPECT_EQ(iter->second, k0);
+}
