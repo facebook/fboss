b08312b64c0813981b913e5e3e5ebe9632ea0c00
Parvez Shaikh <pshaikh@fb.com>
2021-02-17 15:11:13 -0800

    lag manager
    Summary: as titled. this is stub
    
    Differential Revision: D26463580
    
    fbshipit-source-id: ac841668ffc535d0f3152774123e0d55c88839f5

---
+++ fboss/agent/hw/sai/switch/SaiLagManager.cpp
@@ -0,0 +1,17 @@
+// Copyright 2004-present Facebook. All Rights Reserved.
+
+#include "fboss/agent/hw/sai/switch/SaiLagManager.h"
+
+namespace facebook::fboss {
+
+void SaiLagManager::addLag(
+    const std::shared_ptr<AggregatePort>& /*aggregatePort*/) {}
+
+void SaiLagManager::removeLag(
+    const std::shared_ptr<AggregatePort>& /*aggregatePort*/) {}
+
+void SaiLagManager::changeLag(
+    const std::shared_ptr<AggregatePort>& /*oldAggregatePort*/,
+    const std::shared_ptr<AggregatePort>& /*newAggregatePort*/) {}
+
+} // namespace facebook::fboss
---
+++ fboss/agent/hw/sai/switch/SaiLagManager.h
@@ -0,0 +1,38 @@
+// Copyright 2004-present Facebook. All Rights Reserved.
+
+#pragma once
+
+#include "fboss/agent/hw/sai/api/LagApi.h"
+#include "fboss/agent/hw/sai/store/SaiObjectWithCounters.h"
+#include "fboss/agent/state/AggregatePort.h"
+
+namespace facebook::fboss {
+
+using SaiLag = SaiObject<SaiLagTraits>;
+
+struct SaiLagHandle {
+  std::shared_ptr<SaiLag> lag;
+};
+
+class SaiManagerTable;
+class SaiPlatform;
+
+class SaiLagManager {
+ public:
+  using Handles =
+      folly::F14FastMap<AggregatePortID, std::unique_ptr<SaiLagHandle>>;
+  SaiLagManager(SaiManagerTable* managerTable, SaiPlatform* platform)
+      : managerTable_(managerTable), platform_(platform) {}
+  void addLag(const std::shared_ptr<AggregatePort>& aggregatePort);
+  void removeLag(const std::shared_ptr<AggregatePort>& aggregatePort);
+  void changeLag(
+      const std::shared_ptr<AggregatePort>& oldAggregatePort,
+      const std::shared_ptr<AggregatePort>& newAggregatePort);
+
+ private:
+  SaiManagerTable* managerTable_;
+  SaiPlatform* platform_;
+  Handles handles_;
+};
+
+} // namespace facebook::fboss
