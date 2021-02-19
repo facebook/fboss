66469902680586c7068ec20af57a62333c8ff4a9
Peyman Gardideh <pgardideh@fb.com>
2021-02-19 04:38:29 -0800

    add yamp and elbert led tests
    Summary: Add tests to verify led controller set/get colors for Yamp/Elbert.
    
    Differential Revision: D26346721
    
    fbshipit-source-id: 1583d2a706575c81309236c9737ba8a9632f32be

---
+++ fboss/lib/fpga/tests/BaseSystemContainerTests.h
@@ -0,0 +1,60 @@
+// Copyright 2004-present Facebook. All Rights Reserved.
+
+#pragma once
+
+#include <gtest/gtest.h>
+#include "fboss/lib/fpga/facebook/tests/FakeFpgaDevice.h"
+
+namespace facebook::fboss {
+
+class BaseSystemContainerTests : public ::testing::Test {
+ public:
+  template <typename SystemContainer, typename Color>
+  void testPimLeds(
+      SystemContainer* systemContainer,
+      int numPims,
+      int pimStart,
+      int numLeds,
+      Color onColor,
+      Color offColor) {
+    for (auto pim = pimStart; pim < numPims + pimStart; pim++) {
+      auto pimContainer = systemContainer->getPimContainer(pim);
+      for (auto led = 0; led < numLeds; led++) {
+        auto ledController = pimContainer->getLedController(led);
+        auto ledAddr = getLedAddress(pim, led);
+        testLedOnOff(ledController, onColor, offColor, ledAddr);
+
+        // manually set register to yellow and verify reading from fpga
+        fpgaDevice_->write(ledAddr, static_cast<uint32_t>(onColor));
+        EXPECT_EQ(ledController->getColor(), onColor);
+      }
+    }
+  }
+
+ protected:
+  virtual uint32_t getLedAddress(int pim, int led) const = 0;
+  FakeFpgaDevice* fpgaDevice_;
+
+ private:
+  template <typename LedController, typename Color>
+  void setAndVerifyLed(
+      LedController* ledController,
+      Color color,
+      uint32_t ledAddress) {
+    ledController->setColor(color);
+    EXPECT_EQ(ledController->getColor(), color);
+    EXPECT_EQ(fpgaDevice_->read(ledAddress), static_cast<uint32_t>(color));
+  }
+
+  template <typename LedController, typename Color>
+  void testLedOnOff(
+      LedController* ledController,
+      Color onColor,
+      Color offColor,
+      uint32_t ledAddress) {
+    setAndVerifyLed(ledController, onColor, ledAddress);
+    setAndVerifyLed(ledController, offColor, ledAddress);
+  }
+};
+
+} // namespace facebook::fboss
