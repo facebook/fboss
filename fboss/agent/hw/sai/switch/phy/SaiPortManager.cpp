fd9b2d989c7787bb4cdc884b080d74a2568027ec
Joseph Wu <joseph5wu@fb.com>
2021-03-16 19:06:10 -0700

    Move facebook/SaiBufferManager.cpp under sai/switch/npu/
    Summary:
    This diff introduced sai/switch/phy directory to keep all the SaiXXXManager for phy related implementation.
    After having these two new directories:
    ```
    sai/switch/npu
    sai/switch/phy
    ```
    I moved the following:
    ```
    SaiBufferManager.cpp
    SaiPortManager.cpp
    SaiSwitch.cpp
    SaiTamManager.cpp
    ```
    into these two directory to make both npu(tajo/bcm) and phy can use their own implementation of these SaiManager.
    
    Reviewed By: shri-khare
    
    Differential Revision: D27036225
    
    fbshipit-source-id: caa182036182b2edb31f246783e1b634ecde574d

---
+++ fboss/agent/hw/sai/switch/phy/SaiPortManager.cpp
@@ -0,0 +1,19 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#include "fboss/agent/hw/sai/switch/SaiPortManager.h"
+
+namespace facebook::fboss {
+
+void SaiPortManager::addRemovedHandle(PortID /*portID*/) {}
+
+void SaiPortManager::removeRemovedHandleIf(PortID /*portID*/) {}
+
+} // namespace facebook::fboss
