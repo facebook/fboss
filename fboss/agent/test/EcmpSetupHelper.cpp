05e9374d16d46a6fcb868f4a6216baea00b370bb
Jasmeet Bagga <jasmeetbagga@fb.com>
2021-03-17 10:05:13 -0700

    Include needed headers
    Summary:
    RouteTables get used in EcmpSetupHelper, so include the header
    file. Else we get build failures when some transitively included files
    sheds its header includes
    
    Reviewed By: shri-khare
    
    Differential Revision: D27030130
    
    fbshipit-source-id: bbd644437a5fb4d37685e43e27025770172631fc

--- fboss/agent/test/EcmpSetupHelper.cpp
+++ fboss/agent/test/EcmpSetupHelper.cpp
@@ -17,6 +17,7 @@
 #include "fboss/agent/state/Interface.h"
 #include "fboss/agent/state/Port.h"
 #include "fboss/agent/state/PortDescriptor.h"
+#include "fboss/agent/state/RouteTable.h"
 #include "fboss/agent/state/RouteUpdater.h"
 #include "fboss/agent/state/SwitchState.h"
 #include "fboss/agent/state/Vlan.h"
