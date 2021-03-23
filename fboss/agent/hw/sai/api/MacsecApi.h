2cc2f911c35ac6d58c540b33c82d45c3492736e5
Chet Powers <ccpowers@fb.com>
2021-03-23 13:50:50 -0700

    add macsecSA to macsecApi
    Differential Revision: D27142493
    
    fbshipit-source-id: e7543ad61cd4c5ddc707a608e5928501c3dd683b

--- fboss/agent/hw/sai/api/MacsecApi.h
+++ fboss/agent/hw/sai/api/MacsecApi.h
@@ -68,6 +68,85 @@ struct SaiMacsecPortTraits {
 SAI_ATTRIBUTE_NAME(MacsecPort, PortID)
 SAI_ATTRIBUTE_NAME(MacsecPort, MacsecDirection)
 
+struct SaiMacsecSATraits {
+  static constexpr sai_object_type_t ObjectType = SAI_OBJECT_TYPE_MACSEC_SA;
+  using SaiApiT = MacsecApi;
+  struct Attributes {
+    using EnumType = sai_macsec_sa_attr_t;
+    using SCID =
+        SaiAttribute<EnumType, SAI_MACSEC_SA_ATTR_SC_ID, sai_object_id_t>;
+    using AuthNum = SaiAttribute<EnumType, SAI_MACSEC_SA_ATTR_AN, sai_uint8_t>;
+    using AuthKey = SaiAttribute<
+        EnumType,
+        SAI_MACSEC_SA_ATTR_AUTH_KEY,
+        std::array<uint8_t, sizeof(sai_macsec_auth_key_t)>>;
+    using MacsecDirection = SaiAttribute<
+        EnumType,
+        SAI_MACSEC_SA_ATTR_MACSEC_DIRECTION,
+        sai_int32_t>;
+#if SAI_API_VERSION >= SAI_VERSION(1, 7, 1)
+    using SSCI =
+        SaiAttribute<EnumType, SAI_MACSEC_SA_ATTR_MACSEC_SSCI, sai_uint32_t>;
+#endif
+    using MinimumXpn =
+        SaiAttribute<EnumType, SAI_MACSEC_SA_ATTR_MINIMUM_XPN, sai_uint64_t>;
+    using SAK = SaiAttribute<
+        EnumType,
+        SAI_MACSEC_SA_ATTR_SAK,
+        std::array<uint8_t, sizeof(sai_macsec_sak_t)>>;
+    using Salt = SaiAttribute<
+        EnumType,
+        SAI_MACSEC_SA_ATTR_SALT,
+        std::array<uint8_t, sizeof(sai_macsec_salt_t)>>;
+  };
+
+  using AdapterKey = MacsecSASaiId;
+  using AdapterHostKey = std::
+      tuple<Attributes::SCID, Attributes::AuthNum, Attributes::MacsecDirection>;
+  using CreateAttributes = std::tuple<
+      Attributes::SCID,
+      Attributes::AuthNum,
+      Attributes::AuthKey,
+      Attributes::MacsecDirection,
+#if SAI_API_VERSION >= SAI_VERSION(1, 7, 1)
+      Attributes::SSCI,
+#endif
+      Attributes::SAK,
+      Attributes::Salt,
+      std::optional<Attributes::MinimumXpn>>;
+
+  // TODO(ccpowers): in our first implementation (without sai wrappers), we read
+  // different stats depending on the direction. Is it OK to just read them all?
+  static constexpr std::array<sai_stat_id_t, 12> CounterIdsToRead = {
+      SAI_MACSEC_SA_STAT_OCTETS_ENCRYPTED,
+      SAI_MACSEC_SA_STAT_OCTETS_PROTECTED,
+      SAI_MACSEC_SA_STAT_OUT_PKTS_ENCRYPTED,
+      SAI_MACSEC_SA_STAT_OUT_PKTS_PROTECTED,
+      SAI_MACSEC_SA_STAT_IN_PKTS_UNCHECKED,
+      SAI_MACSEC_SA_STAT_IN_PKTS_DELAYED,
+      SAI_MACSEC_SA_STAT_IN_PKTS_LATE,
+      SAI_MACSEC_SA_STAT_IN_PKTS_INVALID,
+      SAI_MACSEC_SA_STAT_IN_PKTS_NOT_VALID,
+      SAI_MACSEC_SA_STAT_IN_PKTS_NOT_USING_SA,
+      SAI_MACSEC_SA_STAT_IN_PKTS_UNUSED_SA,
+      SAI_MACSEC_SA_STAT_IN_PKTS_OK,
+  };
+};
+
+SAI_ATTRIBUTE_NAME(MacsecSA, AuthNum)
+SAI_ATTRIBUTE_NAME(MacsecSA, AuthKey)
+SAI_ATTRIBUTE_NAME(MacsecSA, MacsecDirection)
+#if SAI_API_VERSION >= SAI_VERSION(1, 7, 1)
+SAI_ATTRIBUTE_NAME(MacsecSA, SSCI)
+#endif
+SAI_ATTRIBUTE_NAME(MacsecSA, MinimumXpn)
+SAI_ATTRIBUTE_NAME(MacsecSA, SAK)
+SAI_ATTRIBUTE_NAME(MacsecSA, Salt)
+SAI_ATTRIBUTE_NAME(MacsecSA, SCID)
+
+template <>
+struct SaiObjectHasStats<SaiMacsecSATraits> : public std::true_type {};
+
 class MacsecApi : public SaiApi<MacsecApi> {
  public:
   static constexpr sai_api_t ApiType = SAI_API_MACSEC;
@@ -94,12 +173,24 @@ class MacsecApi : public SaiApi<MacsecApi> {
     return api_->create_macsec_port(rawSaiId(id), switch_id, count, attr_list);
   }
 
+  sai_status_t _create(
+      MacsecSASaiId* id,
+      sai_object_id_t switch_id,
+      size_t count,
+      sai_attribute_t* attr_list) const {
+    return api_->create_macsec_sa(rawSaiId(id), switch_id, count, attr_list);
+  }
+
   sai_status_t _remove(MacsecSaiId key) {
     return api_->remove_macsec(key);
   }
 
   sai_status_t _remove(MacsecPortSaiId key) {
     return api_->remove_macsec_port(key);
+  }
+
+  sai_status_t _remove(MacsecSASaiId key) {
+    return api_->remove_macsec_sa(key);
   }
 
   sai_status_t _getAttribute(MacsecSaiId key, sai_attribute_t* attr) const {
@@ -110,12 +201,30 @@ class MacsecApi : public SaiApi<MacsecApi> {
     return api_->get_macsec_port_attribute(key, 1, attr);
   }
 
+  sai_status_t _getAttribute(MacsecSASaiId key, sai_attribute_t* attr) const {
+    return api_->get_macsec_sa_attribute(key, 1, attr);
+  }
+
   sai_status_t _setAttribute(MacsecSaiId key, const sai_attribute_t* attr) {
     return api_->set_macsec_attribute(key, attr);
   }
 
   sai_status_t _setAttribute(MacsecPortSaiId key, const sai_attribute_t* attr) {
     return api_->set_macsec_port_attribute(key, attr);
+  }
+
+  sai_status_t _setAttribute(MacsecSASaiId key, const sai_attribute_t* attr) {
+    return api_->set_macsec_sa_attribute(key, attr);
+  }
+
+  sai_status_t _getStats(
+      MacsecSASaiId key,
+      uint32_t num_of_counters,
+      const sai_stat_id_t* counter_ids,
+      sai_stats_mode_t mode,
+      uint64_t* counters) const {
+    return api_->get_macsec_sa_stats(
+        key, num_of_counters, counter_ids, counters);
   }
 
   sai_macsec_api_t* api_;
