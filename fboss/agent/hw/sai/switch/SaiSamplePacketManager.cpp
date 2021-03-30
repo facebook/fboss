896abc3e6356f2aed3d9d5f07ba6157415da0f5c
Srikrishna Gopu <srikrishnagopu@fb.com>
2021-03-29 21:28:10 -0700

    make tajo to use exclusive samplepacket mode.
    Summary:
    As titled, make tajo use exclusive mode for samplepacket.
    
    - Following up with bcm on hostif table error.
    - Following up with Tajo on SHARED mode
    
    Reviewed By: jasmeetbagga
    
    Differential Revision: D27287790
    
    fbshipit-source-id: ebd477fb987172c9132c98f62343bbb03cebac4c

--- fboss/agent/hw/sai/switch/SaiSamplePacketManager.cpp
+++ fboss/agent/hw/sai/switch/SaiSamplePacketManager.cpp
@@ -15,6 +15,7 @@
 #include "fboss/agent/hw/sai/switch/SaiManagerTable.h"
 #include "fboss/agent/hw/sai/switch/SaiPortManager.h"
 #include "fboss/agent/hw/sai/switch/SaiSwitchManager.h"
+#include "fboss/agent/hw/switch_asics/HwAsic.h"
 
 #include "fboss/agent/platforms/sai/SaiPlatform.h"
 
@@ -32,6 +33,15 @@ sai_samplepacket_type_t getSamplePacketType(
       throw facebook::fboss::FbossError("sample destination is invalid");
   }
 }
+
+sai_samplepacket_mode_t getSamplePacketMode(
+    const facebook::fboss::SaiPlatform* platform) {
+  // TODO: Tajo will be moving to shared mode and we can get rid of this.
+  return platform->getAsic()->getAsicType() ==
+          facebook::fboss::HwAsic::AsicType::ASIC_TYPE_TAJO
+      ? SAI_SAMPLEPACKET_MODE_EXCLUSIVE
+      : SAI_SAMPLEPACKET_MODE_SHARED;
+}
 } // namespace
 namespace facebook::fboss {
 
@@ -41,10 +51,7 @@ SaiSamplePacketManager::getOrCreateSamplePacket(
     cfg::SampleDestination sampleDestination) {
   auto type = getSamplePacketType(sampleDestination);
   SaiSamplePacketTraits::AdapterHostKey k{
-      sampleRate,
-      type,
-      SAI_SAMPLEPACKET_MODE_SHARED // TODO: Decide by asic support
-  };
+      sampleRate, type, getSamplePacketMode(platform_)};
   SaiSamplePacketTraits::CreateAttributes attributes = k;
   auto& store = saiStore_->get<SaiSamplePacketTraits>();
   return store.setObject(k, attributes);
