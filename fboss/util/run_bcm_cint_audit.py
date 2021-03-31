16edfa672371a5ba80855eec66f02157fe20af03
Ron He <zecheng@fb.com>
2021-03-31 14:24:53 -0700

    On-diff cint audit to block when new Bcm API is introduced
    Summary:
    Similar to netcastle tests, create a list of known_missing_apis that are excluded from the audit. The list will be shorten once more API support is added to Bcm Cinter.
    
    Then for new APIs introduced in Bcm, the audit should fail, prompting user to add Bcm support.
    
    Reviewed By: shri-khare
    
    Differential Revision: D27410375
    
    fbshipit-source-id: 0214e1b6acdb9da377b6314ec3219c5cc836b9d5

--- fboss/util/run_bcm_cint_audit.py
+++ fboss/util/run_bcm_cint_audit.py
@@ -11,6 +11,7 @@
 #   - diff with wrapped symbols (APIs logged by Bcm Cinter) and print output.
 
 import subprocess
+import sys
 
 ROOT_DIR_TO_SEARCH = "fboss/agent/hw/bcm"
 PATTERNS_TO_SEARCH = [r"'bcm_\S*[a-z,A-Z,0-9]('"]
@@ -36,6 +37,98 @@ WRAPPED_SYMBOLS_TO_SEARCH = [
     "'wrap=" + search_pattern[1:-2] + "'" for search_pattern in PATTERNS_TO_SEARCH
 ]  # Remove trailing close bracket
 WRAPPED_SYMBOLS_TO_REPLACE = ["'s/.*--wrap=//'", "'s/\",//'"]
+
+KNOWN_MISSING_APIS = {
+    "bcm_cosq_burst_monitor_detach",
+    "bcm_cosq_burst_monitor_flow_view_config_set",
+    "bcm_cosq_burst_monitor_init",
+    "bcm_cosq_burst_monitor_set",
+    "bcm_cosq_pfc_class_map_config_t_init",
+    "bcm_errmsg",
+    "bcm_field_entry_config_t_init",
+    "bcm_field_entry_flexctr_attach",
+    "bcm_field_entry_flexctr_detach",
+    "bcm_field_entry_policer_attach",
+    "bcm_field_entry_policer_detach",
+    "bcm_field_entry_remove",
+    "bcm_field_group_config_t_init",
+    "bcm_field_group_oper_mode_set",
+    "bcm_field_qualify_ExactMatchHitStatus",
+    "bcm_flexctr_action_create",
+    "bcm_flexctr_action_destroy",
+    "bcm_flexctr_action_t_init",
+    "bcm_flowtracker_export_template_create",
+    "bcm_flowtracker_export_template_destroy",
+    "bcm_flowtracker_group_age_timer_set",
+    "bcm_flowtracker_group_collector_attach",
+    "bcm_flowtracker_group_collector_detach",
+    "bcm_flowtracker_group_create",
+    "bcm_flowtracker_group_destroy",
+    "bcm_flowtracker_group_export_trigger_set",
+    "bcm_flowtracker_group_flow_limit_set",
+    "bcm_flowtracker_group_info_t_init",
+    "bcm_flowtracker_group_tracking_params_set",
+    "bcm_flowtracker_tracking_param_info_t_init",
+    "bcm_l2_addr_delete_by_mac_port",
+    "bcm_l2_addr_register",
+    "bcm_l2_addr_t_init",
+    "bcm_l2_addr_unregister",
+    "bcm_l2_station_t_init",
+    "bcm_l3_ecmp_member_t_init",
+    "bcm_l3_enable_set",
+    "bcm_l3_host_t_init",
+    "bcm_l3_info_t_init",
+    "bcm_l3_ingress_t_init",
+    "bcm_l3_intf_t_init",
+    "bcm_l3_route_t_init",
+    "bcm_mirror_destination_t_init",
+    "bcm_mpls_egress_label_t_init",
+    "bcm_mpls_tunnel_switch_t_init",
+    "bcm_pkt_rx_free",
+    "bcm_pktio_alloc",
+    "bcm_pktio_cleanup",
+    "bcm_pktio_free",
+    "bcm_pktio_init",
+    "bcm_pktio_pmd_set",
+    "bcm_pktio_pull",
+    "bcm_pktio_put",
+    "bcm_pktio_rx_register",
+    "bcm_pktio_rx_unregister",
+    "bcm_pktio_trim",
+    "bcm_pktio_tx",
+    "bcm_policer_config_t_init",
+    "bcm_policer_create",
+    "bcm_policer_destroy",
+    "bcm_port_control_phy_timesync_set",
+    "bcm_port_flood_block_set",
+    "bcm_port_if_t",
+    "bcm_port_phy_set",
+    "bcm_port_phy_timesync_config_set",
+    "bcm_port_phy_timesync_config_t_init",
+    "bcm_port_phy_tx_t_init",
+    "bcm_port_priority_group_config_t_init",
+    "bcm_port_resource_t_init",
+    "bcm_port_stat_attach",
+    "bcm_port_stat_detach_with_id",
+    "bcm_port_timesync_config_set",
+    "bcm_port_timesync_config_t_init",
+    "bcm_qos_map_t_init",
+    "bcm_rtag7_control_set",
+    "bcm_rx_cosq_mapping_extended_add",
+    "bcm_rx_cosq_mapping_extended_delete",
+    "bcm_rx_cosq_mapping_t_init",
+    "bcm_rx_queue_channel_set",
+    "bcm_rx_reasons_t_init",
+    "bcm_shutdown",
+    "bcm_stat_sync",
+    "bcm_time_interface_add",
+    "bcm_time_interface_delete_all",
+    "bcm_time_interface_t_init",
+    "bcm_trunk_info_t_init",
+    "bcm_trunk_member_t_init",
+    "bcm_vlan_control_vlan_t_init",
+    "bcm_warmboot_set",
+}
 
 
 def get_used_apis():
@@ -85,6 +178,14 @@ def get_wrapped_apis():
 if __name__ == "__main__":
     used_apis = get_used_apis()
     wrapped_apis = get_wrapped_apis()
-    diff = used_apis - wrapped_apis
-    print(f"There're {len(diff)} Bcm APIs that are not supported by Bcm Cinter.\n")
-    print(*sorted(used_apis - wrapped_apis), sep="\n")
+    diff = used_apis - wrapped_apis - KNOWN_MISSING_APIS
+    if len(diff) > 0:
+        linking_verb = "is" if len(diff) == 1 else "are"
+        print(
+            f"There {linking_verb} {len(diff)} Bcm API(s) not supported by Bcm Cinter.\n"
+            "Please make sure to log the API in Bcm Cinter when introducing new Bcm APIs.\n"
+        )
+        print(*sorted(diff), sep="\n")
+        sys.exit(1)
+    else:
+        print("No missing API in Bcm Cinter.")
