1f6182abf57a0f43e8003e296d85aba76bd43078
Shrikrishna Khare <skhare@fb.com>
2021-04-14 21:38:03 -0700

    Build FBOSS CLI in OSS
    Reviewed By: hegara
    
    Differential Revision: D27491660
    
    fbshipit-source-id: 7394bfbb1cbc5893bc278f0970d816bd65c507e8

---
+++ cmake/CliFboss2.cmake
@@ -0,0 +1,35 @@
+# CMake to build libraries and binaries in fboss/cli/fboss2
+
+# In general, libraries and binaries in fboss/foo/bar are built by
+# cmake/FooBar.cmake
+
+add_executable(fboss2
+  fboss/cli/fboss2/Main.cpp
+)
+
+target_link_libraries(fboss2
+  CLI11
+  Folly::folly
+)
+
+add_library(CLI11
+  fboss/cli/fboss2/CLI11/App.hpp
+  fboss/cli/fboss2/CLI11/CLI.hpp
+  fboss/cli/fboss2/CLI11/Config.hpp
+  fboss/cli/fboss2/CLI11/ConfigFwd.hpp
+  fboss/cli/fboss2/CLI11/Error.hpp
+  fboss/cli/fboss2/CLI11/Formatter.hpp
+  fboss/cli/fboss2/CLI11/FormatterFwd.hpp
+  fboss/cli/fboss2/CLI11/Macros.hpp
+  fboss/cli/fboss2/CLI11/Option.hpp
+  fboss/cli/fboss2/CLI11/Split.hpp
+  fboss/cli/fboss2/CLI11/StringTools.hpp
+  fboss/cli/fboss2/CLI11/Timer.hpp
+  fboss/cli/fboss2/CLI11/TypeTools.hpp
+  fboss/cli/fboss2/CLI11/Validators.hpp
+  fboss/cli/fboss2/CLI11/Version.hpp
+)
+
+set_target_properties(CLI11 PROPERTIES LINKER_LANGUAGE CXX)
+
+install(TARGETS fboss2)
