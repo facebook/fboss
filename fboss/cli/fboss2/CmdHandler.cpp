902b948a762a9978880bbe55420f4990e1cec361
Shrikrishna Khare <skhare@fb.com>
2021-04-14 21:38:03 -0700

    Introduce singleton for command handlers
    Summary: As titled.
    
    Differential Revision: D27491678
    
    fbshipit-source-id: 3ddba46f3b043aae8cfa2cd580ed97c630f62087

---
+++ fboss/cli/fboss2/CmdHandler.cpp
@@ -0,0 +1,24 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#include "fboss/cli/fboss2/CmdHandler.h"
+
+#include <folly/Singleton.h>
+
+namespace {
+struct singleton_tag_type {};
+} // namespace
+
+using facebook::fboss::CmdHandler;
+static folly::Singleton<CmdHandler, singleton_tag_type>
+    cmdHandler{};
+std::shared_ptr<CmdHandler> CmdHandler::getInstance() {
+  return cmdHandler.try_get();
+}
