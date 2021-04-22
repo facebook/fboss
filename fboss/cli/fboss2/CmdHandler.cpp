e1240f6162830acc393e7fa3498e9ea24cedebd9
Shrikrishna Khare <skhare@fb.com>
2021-04-21 22:04:29 -0700

    Introduce support for show port queues
    Summary: As titled.
    
    Differential Revision: D27699459
    
    fbshipit-source-id: 84b2a83e7dcf8046e109d65a8bc673e33ef02b78

--- fboss/cli/fboss2/CmdHandler.cpp
+++ fboss/cli/fboss2/CmdHandler.cpp
@@ -19,6 +19,7 @@
 #include "fboss/cli/fboss2/CmdShowLldp.h"
 #include "fboss/cli/fboss2/CmdShowNdp.h"
 #include "fboss/cli/fboss2/CmdShowPort.h"
+#include "fboss/cli/fboss2/CmdShowPortQueue.h"
 #include "fboss/cli/fboss2/CmdUtils.h"
 
 #include <folly/Singleton.h>
@@ -34,6 +35,7 @@ template void CmdHandler<CmdShowArp, CmdShowArpTraits>::run();
 template void CmdHandler<CmdShowLldp, CmdShowLldpTraits>::run();
 template void CmdHandler<CmdShowNdp, CmdShowNdpTraits>::run();
 template void CmdHandler<CmdShowPort, CmdShowPortTraits>::run();
+template void CmdHandler<CmdShowPortQueue, CmdShowPortQueueTraits>::run();
 
 template void CmdHandler<CmdClearArp, CmdClearArpTraits>::run();
 template void CmdHandler<CmdClearNdp, CmdClearNdpTraits>::run();
---
+++ fboss/cli/fboss2/CmdShowPortQueue.h
@@ -0,0 +1,89 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#pragma once
+
+#include "fboss/cli/fboss2/CmdHandler.h"
+
+namespace facebook::fboss {
+
+struct CmdShowPortQueueTraits {
+  static constexpr utils::ObjectArgTypeId ObjectArgTypeId =
+      utils::ObjectArgTypeId::OBJECT_ARG_TYPE_ID_PORT_LIST;
+  using ObjectArgType = std::vector<std::string>;
+  using RetType = std::map<int32_t, facebook::fboss::PortInfoThrift>;
+};
+
+class CmdShowPortQueue : public CmdHandler<CmdShowPortQueue, CmdShowPortQueueTraits> {
+ public:
+  using ObjectArgType = CmdShowPortQueueTraits::ObjectArgType;
+  using RetType = CmdShowPortQueueTraits::RetType;
+
+  RetType queryClient(
+      const folly::IPAddress& hostIp,
+      const ObjectArgType& queriedPorts) {
+    RetType portEntries;
+
+    auto client = utils::createClient<facebook::fboss::FbossCtrlAsyncClient>(
+        hostIp.str());
+
+    client->sync_getAllPortInfo(portEntries);
+
+    if (queriedPorts.size() == 0) {
+      return portEntries;
+    }
+
+    RetType retVal;
+    for (auto const&[portId, portInfo] : portEntries) {
+      for (auto const& queriedPort : queriedPorts) {
+        if (portInfo.get_name() == queriedPort) {
+          retVal.insert(std::make_pair(portId, portInfo));
+        }
+      }
+    }
+
+    return retVal;
+  }
+
+  void printOutput(const RetType& portId2PortInfoThrift) {
+    std::string fmtString = "{:<7}{:<20}{:<25}{:<10}{:<15}{:<15}\n";
+
+    for (auto const&[portId, portInfo] : portId2PortInfoThrift) {
+
+      std::cout << portInfo.get_name() << "\n";
+      std::cout << std::string(10, '=') << std::endl;
+
+      std::cout << fmt::format(
+          fmtString,
+          "ID",
+          "Name",
+          "Mode",
+          "Weight",
+          "ReservedBytes",
+          "SharedBytes");
+      std::cout << std::string(90, '-') << std::endl;
+
+      for (auto const& queue : portInfo.get_portQueues()) {
+        std::cout << fmt::format(
+            fmtString,
+            queue.get_id(),
+            queue.get_name(),
+            queue.get_mode(),
+            queue.weight_ref() ? *queue.weight_ref() : 0,
+            queue.reservedBytes_ref() ? *queue.reservedBytes_ref() : 0,
+            queue.scalingFactor_ref() ? *queue.scalingFactor_ref() : 0);
+      }
+
+      std::cout << "\n";
+    }
+  }
+};
+
+} // namespace facebook::fboss
