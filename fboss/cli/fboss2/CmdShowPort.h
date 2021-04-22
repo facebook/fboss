58a7434eb8b1e269737a7276439fa39513bc5295
Shrikrishna Khare <skhare@fb.com>
2021-04-22 07:43:26 -0700

    Only print colored output for tty
    Summary:
    As titled.
    
    Piping to a differnt command disables colors (avoids messy asci codes in
    output).
    
    Differential Revision: D27803371
    
    fbshipit-source-id: f5e7f24754c347fb74615c592e2f3bc53e5945f5

--- fboss/cli/fboss2/CmdShowPort.h
+++ fboss/cli/fboss2/CmdShowPort.h
@@ -12,6 +12,8 @@
 
 #include "fboss/cli/fboss2/CmdHandler.h"
 
+#include <unistd.h>
+
 namespace facebook::fboss {
 
 struct CmdShowPortTraits {
@@ -78,10 +80,16 @@ class CmdShowPort : public CmdHandler<CmdShowPort, CmdShowPortTraits> {
           portInfo.get_name(),
           getAdminStateStr(portInfo.get_adminState()));
 
-      fmt::print(
-          fmt::emphasis::bold | fg(operColor),
-          "{0:<15}",
-          operState);
+      if (isatty(fileno(stdout))) {
+          fmt::print(
+              fmt::emphasis::bold | fg(operColor),
+              "{0:<15}",
+              operState);
+        } else {
+          fmt::print(
+              "{0:<15}",
+              operState);
+        }
 
       fmt::print(
           "{:<10}{:<20}\n",
