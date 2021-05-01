bd909f4456ba09e6e20c75cced5e36057a7893c0
Joseph Wu <joseph5wu@fb.com>
2021-05-01 16:18:45 -0700

    Introduce SaiPhySwitchEnsemble for qsfp_service hw_test
    Summary:
    Introduce SaiPhySwitchEnsemble extending from HwPhyEnsemble created in last diff.
    
    SaiPhySwitchEnsemble will use PhyManager, especially ElbertPhyManager, to program phy.
    
    Reviewed By: jasmeetbagga
    
    Differential Revision: D28050032
    
    fbshipit-source-id: 9fe4bfbd7529ce5836f835c0675e8185b574c827

---
+++ fboss/qsfp_service/test/hw_test/phy/sai/SaiPhyEnsemble.h
@@ -0,0 +1,27 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#pragma once
+
+#include "fboss/qsfp_service/test/hw_test/phy/HwPhyEnsemble.h"
+
+namespace facebook::fboss {
+
+class PhyManager;
+
+class SaiPhyEnsemble : public HwPhyEnsemble {
+ public:
+  SaiPhyEnsemble() {}
+
+  ~SaiPhyEnsemble() override {}
+
+  void init(const HwPhyEnsemble::HwPhyEnsembleInitInfo& info) override;
+};
+} // namespace facebook::fboss
