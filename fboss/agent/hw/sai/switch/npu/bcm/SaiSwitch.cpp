0892c241cd843409f31f3391273890d66658d448
Ron He <zecheng@fb.com>
2021-05-17 11:39:53 -0700

    Prepare to build brcm-sai 5.0.0.3
    Summary: As titled. Update compiler flag from `SAI_VERSION_5_0_0_2_ODP` to `SAI_VERSION_5_0_0_3_ODP`
    
    Reviewed By: jasmeetbagga
    
    Differential Revision: D28431968
    
    fbshipit-source-id: 4ca52fd27376d1ad62a4b1cb8d7944fb6d478e6d

--- fboss/agent/hw/sai/switch/npu/bcm/SaiSwitch.cpp
+++ fboss/agent/hw/sai/switch/npu/bcm/SaiSwitch.cpp
@@ -7,7 +7,7 @@
 #include <folly/String.h>
 
 extern "C" {
-#if !defined(SAI_VERSION_5_0_0_2_ODP)
+#if !defined(SAI_VERSION_5_0_0_3_ODP)
 #include <experimental/saiexperimentalswitch.h>
 #include <experimental/saitamextensions.h>
 #else
