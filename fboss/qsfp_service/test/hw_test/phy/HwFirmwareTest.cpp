1359b396e1d2cac0a0b3b3899b517f1db76cf2ad
Jasmeet Bagga <jasmeetbagga@fb.com>
2021-05-24 10:32:39 -0700

    Assert FW version for all xphys
    Summary: As titled. Todo - maintain a mapping of platform ->default FW version
    
    Differential Revision: D28564003
    
    fbshipit-source-id: b9fd4a09915ea244bedc263b1dd50d97e6795022

--- fboss/qsfp_service/test/hw_test/phy/HwFirmwareTest.cpp
+++ fboss/qsfp_service/test/hw_test/phy/HwFirmwareTest.cpp
@@ -9,6 +9,7 @@
  */
 #include "fboss/qsfp_service/test/hw_test/HwTest.h"
 
+#include "fboss/agent/platforms/common/PlatformMapping.h"
 #include "fboss/lib/phy/ExternalPhy.h"
 #include "fboss/lib/phy/PhyManager.h"
 #include "fboss/qsfp_service/test/hw_test/phy/HwPhyEnsemble.h"
@@ -16,8 +17,14 @@
 namespace facebook::fboss {
 
 TEST_F(HwTest, CheckDefaultFirmwareVersion) {
-  EXPECT_EQ(
-      getHwPhyEnsemble()->getTargetExternalPhy()->fwVersion(),
-      getHwPhyEnsemble()->getInitInfo().fwVersion);
+  auto chips = getHwPhyEnsemble()->getPlatformMapping()->getChips();
+  for (auto chip : chips) {
+    if (chip.second.get_type() != phy::DataPlanePhyChipType::XPHY) {
+      continue;
+    }
+    auto xphy = getHwPhyEnsemble()->getPhyManager()->getExternalPhy(
+        GlobalXphyID(chip.second.get_physicalID()));
+    EXPECT_EQ(xphy->fwVersion(), getHwPhyEnsemble()->getInitInfo().fwVersion);
+  }
 }
 } // namespace facebook::fboss
