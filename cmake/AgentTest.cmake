b1c4742847e894d3bdf7d328d9a02eee723cd56b
Jasmeet Bagga <jasmeetbagga@fb.com>
2021-05-25 14:11:02 -0700

    Build sai_link-test in OSS
    Summary: As titled
    
    Differential Revision: D28681630
    
    fbshipit-source-id: a7b86e679a7d10bdd4c390329f04a90f0e4ec95d

--- cmake/AgentTest.cmake
+++ cmake/AgentTest.cmake
@@ -120,3 +120,36 @@ target_link_libraries(bcm_link_test
   platform
   bcm
 )
+
+function(BUILD_SAI_LINK_TEST SAI_IMPL_NAME SAI_IMPL_ARG)
+
+  message(STATUS "Building SAI_IMPL_NAME: ${SAI_IMPL_NAME} SAI_IMPL_ARG: ${SAI_IMPL_ARG}")
+
+  add_executable(sai_link_test-${SAI_IMPL_NAME}-${SAI_VER_SUFFIX}
+    fboss/agent/test/link_tests/SaiLinkTest.cpp
+  )
+
+  target_link_libraries(sai_link_test-${SAI_IMPL_NAME}-${SAI_VER_SUFFIX}
+    # --whole-archive is needed for gtest to find these tests
+    -Wl,--whole-archive
+    ${SAI_IMPL_ARG}
+    sai_switch
+    link_tests
+    agent_config_cpp2
+    sai_platform
+    -Wl,--no-whole-archive
+    ref_map
+    ${GTEST}
+    ${LIBGMOCK_LIBRARIES}
+  )
+
+  set_target_properties(sai_link_test-${SAI_IMPL_NAME}-${SAI_VER_SUFFIX}
+      PROPERTIES COMPILE_FLAGS
+      "-DSAI_VER_MAJOR=${SAI_VER_MAJOR} \
+      -DSAI_VER_MINOR=${SAI_VER_MINOR}  \
+      -DSAI_VER_RELEASE=${SAI_VER_RELEASE}"
+    )
+
+endfunction()
+
+BUILD_SAI_LINK_TEST("fake" fake_sai)
