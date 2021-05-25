e444389b245fd497a71b78aff0535f19c31e6cbd
Jasmeet Bagga <jasmeetbagga@fb.com>
2021-05-25 00:23:36 -0700

    Verify link sanity across warm boots
    Summary: As titled
    
    Differential Revision: D28663425
    
    fbshipit-source-id: e521069f90c272c08e5b02ed07c0f1da4c9b0988

--- fboss/agent/test/link_tests/LinkSanityTests.cpp
+++ fboss/agent/test/link_tests/LinkSanityTests.cpp
@@ -6,21 +6,23 @@
 using namespace ::testing;
 using namespace facebook::fboss;
 
-class LinkSanityTests : public LinkTest {};
-
 // Tests that the link comes up after a flap on the ASIC
-TEST_F(LinkSanityTests, asicLinkFlap) {
-  auto ports = getCabledPorts();
-  // Set the port status on all cabled ports to false. The link should go down
-  for (const auto& port : ports) {
-    setPortStatus(port, false);
-  }
-  EXPECT_NO_THROW(waitForAllCabledPorts(false));
+TEST_F(LinkTest, asicLinkFlap) {
+  auto verify = [this]() {
+    auto ports = getCabledPorts();
+    // Set the port status on all cabled ports to false. The link should go down
+    for (const auto& port : ports) {
+      setPortStatus(port, false);
+    }
+    EXPECT_NO_THROW(waitForAllCabledPorts(false));
 
-  // Set the port status on all cabled ports to true. The link should come back
-  // up
-  for (const auto& port : ports) {
-    setPortStatus(port, true);
-  }
-  EXPECT_NO_THROW(waitForAllCabledPorts(true));
+    // Set the port status on all cabled ports to true. The link should come
+    // back up
+    for (const auto& port : ports) {
+      setPortStatus(port, true);
+    }
+    EXPECT_NO_THROW(waitForAllCabledPorts(true));
+  };
+
+  verifyAcrossWarmBoots([]() {}, verify);
 }
