48d89be081a28b2243ddb51760412bcb68802bcf
Jasmeet Bagga <jasmeetbagga@fb.com>
2021-06-01 23:28:29 -0700

    Pull up HwFirmwareTest
    Summary:
    As titled. Working towards just having a HwQfpTest binary where
    individual tests apply or not based on the setup
    
    Differential Revision: D28660029
    
    fbshipit-source-id: b7c85c6e4d5ba12923ec05b2eeb71f6b775600ea

--- fboss/qsfp_service/test/hw_test/HwFirmwareTest.cpp
+++ fboss/qsfp_service/test/hw_test/HwFirmwareTest.cpp
@@ -23,16 +23,32 @@ TEST_F(HwTest, CheckDefaultFirmwareVersion) {
     return;
   }
 
-  phy::PhyFwVersion desiredFw;
+  std::optional<phy::PhyFwVersion> desiredFw;
   auto platformMode = getHwQsfpEnsemble()->getWedgeManager()->getPlatformMode();
   switch (platformMode) {
     case PlatformMode::ELBERT:
-      desiredFw.version_ref() = 91;
-      desiredFw.versionStr_ref() = "91.1";
-      desiredFw.minorVersion_ref() = 1;
+      desiredFw->version_ref() = 91;
+      desiredFw->versionStr_ref() = "91.1";
+      desiredFw->minorVersion_ref() = 1;
       break;
-    default:
+    case PlatformMode::MINIPACK:
+    case PlatformMode::FUJI:
+    case PlatformMode::CLOUDRIPPER:
+    case PlatformMode::YAMP:
       throw FbossError("Fill in desired f/w version for: ", platformMode);
+    case PlatformMode::WEDGE:
+    case PlatformMode::WEDGE100:
+    case PlatformMode::GALAXY_LC:
+    case PlatformMode::GALAXY_FC:
+    case PlatformMode::FAKE_WEDGE:
+    case PlatformMode::FAKE_WEDGE40:
+    case PlatformMode::WEDGE400C:
+    case PlatformMode::WEDGE400C_SIM:
+    case PlatformMode::WEDGE400:
+      break;
+  }
+  if (!desiredFw) {
+    return;
   }
   for (auto chip : chips) {
     if (chip.second.get_type() != phy::DataPlanePhyChipType::XPHY) {
@@ -40,7 +56,7 @@ TEST_F(HwTest, CheckDefaultFirmwareVersion) {
     }
     auto xphy = getHwQsfpEnsemble()->getPhyManager()->getExternalPhy(
         GlobalXphyID(chip.second.get_physicalID()));
-    EXPECT_EQ(xphy->fwVersion(), desiredFw);
+    EXPECT_EQ(xphy->fwVersion(), *desiredFw);
   }
 }
 } // namespace facebook::fboss
