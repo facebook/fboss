c8ad667767a7303ff87fc5560ba514ee2d4ada28
Rajan Kumar <rajank@fb.com>
2021-06-05 01:44:52 -0700

    credo_macsec_util cli: dump port map command client side
    Summary:
    In this diff:
    Added client side code from credo_macsec_util to trigger the dump phy port map info command
    
    Reviewed By: birdsoup
    
    Differential Revision: D28053679
    
    fbshipit-source-id: 0bf3f98e384ade63c4a762fad71f3f2138af42d3

--- fboss/util/credo_macsec_util.cpp
+++ fboss/util/credo_macsec_util.cpp
@@ -16,6 +16,34 @@ using namespace facebook::fboss;
 using namespace facebook::fboss::mka;
 using namespace apache::thrift;
 
+DEFINE_bool(phy_port_map, false, "Print Phy Port map for this platform");
+
+/*
+ * printPhyPortMap()
+ * Print the Phy port mapping information from the macsec handling process.
+ * This function will make the thrift call to the qsfp_service (or phy_service)
+ * process to get this phy port mapping information. ie:
+ * PORT    SLOT    MDIO    PHYID   NAME
+ *  1       2       1       0      eth4/2/1
+ *  3       0       0       0      eth2/1/1
+ *  5       3       4       0      eth5/5/1
+ */
+void printPhyPortMap(QsfpServiceAsyncClient* fbMacsecHandler) {
+  MacsecPortPhyMap macsecPortPhyMap{};
+  fbMacsecHandler->sync_macsecGetPhyPortInfo(macsecPortPhyMap);
+
+  printf("Printing the Phy port info map:\n");
+  printf("PORT    SLOT    MDIO    PHYID   NAME\n");
+  for (auto it : *macsecPortPhyMap.macsecPortPhyMap_ref()) {
+      int port = it.first;
+      int slot = *it.second.slotId_ref();
+      int mdio = *it.second.mdioId_ref();
+      int phy = *it.second.phyId_ref();
+
+      printf("%4d    %4d    %4d    %4d    eth%d/%d/1\n",  port, slot, mdio, phy, slot, mdio+1);
+  }
+}
+
 int main(int argc, char* argv[]) {
   folly::init(&argc, &argv, true);
   gflags::SetCommandLineOptionWithMode(
@@ -24,6 +52,11 @@ int main(int argc, char* argv[]) {
 
   std::unique_ptr<QsfpServiceAsyncClient> client;
   client = QsfpClient::createClient(&evb).getVia(&evb);
+
+  if (FLAGS_phy_port_map) {
+    printPhyPortMap(client.get());
+    return 0;
+  }
 
   return 0;
 }
