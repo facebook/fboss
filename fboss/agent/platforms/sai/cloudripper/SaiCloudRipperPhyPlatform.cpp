5ea327a56c50e9aacda33781414476e6d2fa0cc6
Vasant Patil <vsp@fb.com>
2021-06-09 19:51:36 -0700

    Introducing Cloud Ripper PHY platform
    Summary:
    As titled
    
    - Not yet instantiating this class.
    - This code will be tested along with CloudRipperPhyManager
    
    Differential Revision: D28843555
    
    fbshipit-source-id: baebe5a366b29d40b48125a78b56712eb357b401

---
+++ fboss/agent/platforms/sai/cloudripper/SaiCloudRipperPhyPlatform.cpp
@@ -0,0 +1,154 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#include "fboss/agent/platforms/sai/cloudripper/SaiCloudRipperPhyPlatform.h"
+
+#include "fboss/agent/hw/sai/switch/SaiSwitch.h"
+#include "fboss/agent/hw/switch_asics/CredoF104Asic.h"
+#include "fboss/agent/platforms/common/cloud_ripper/CloudRipperPlatformMapping.h"
+
+sai_status_t credo_library_initialize(void);
+
+namespace facebook::fboss {
+
+const std::string& SaiCloudRipperPhyPlatform::getFirmwareDirectory() {
+  static const std::string kFirmwareDir = "/lib/firmware/fboss/credo/f104/";
+  return kFirmwareDir;
+}
+
+namespace {
+static auto constexpr kSaiBootType = "SAI_KEY_BOOT_TYPE";
+static auto constexpr kSaiConfigFile = "SAI_KEY_INIT_CONFIG_FILE";
+
+const std::string kPhyConfigProfile =
+    SaiCloudRipperPhyPlatform::getFirmwareDirectory() + "Cloud_Ripper.xml";
+
+/*
+ * saiProfileGetValue
+ *
+ * This function returns some key values to the SAI while doing
+ * sai_api_initialize.
+ * For SAI_KEY_BOOT_TYPE, currently we only return the cold boot type.
+ * For SAI_KEY_INIT_CONFIG_FILE, the profile id tells SAI which default
+ * configuration to pick up for the Phy.
+ */
+const char* FOLLY_NULLABLE saiProfileGetValue(
+    sai_switch_profile_id_t /*profileId*/,
+    const char* variable) {
+  if (strcmp(variable, kSaiBootType) == 0) {
+    // TODO(vsp) Support warmboot
+    return "cold";
+  } else if (strcmp(variable, kSaiConfigFile) == 0) {
+    return kPhyConfigProfile.c_str();
+  }
+  return nullptr;
+}
+
+/*
+ * saiProfileGetNextValue
+ *
+ * This function lets SAI pick up next value for a given key. Currently this
+ * returns null
+ */
+int saiProfileGetNextValue(
+    sai_switch_profile_id_t /* profile_id */,
+    const char** /* variable */,
+    const char** /* value */) {
+  return -1;
+}
+
+sai_service_method_table_t kSaiServiceMethodTable = {
+    .profile_get_value = saiProfileGetValue,
+    .profile_get_next_value = saiProfileGetNextValue,
+};
+} // namespace
+
+SaiCloudRipperPhyPlatform::SaiCloudRipperPhyPlatform(
+    std::unique_ptr<PlatformProductInfo> productInfo,
+    int phyId)
+    : SaiHwPlatform(
+          std::move(productInfo),
+          std::make_unique<CloudRipperPlatformMapping>()),
+      phyId_(phyId) {
+  asic_ = std::make_unique<CredoF104Asic>();
+}
+
+SaiCloudRipperPhyPlatform::~SaiCloudRipperPhyPlatform() {}
+
+std::string SaiCloudRipperPhyPlatform::getHwConfig() {
+  throw FbossError("SaiCloudRipperPhyPlatform doesn't support getHwConfig()");
+}
+HwAsic* SaiCloudRipperPhyPlatform::getAsic() const {
+  return asic_.get();
+}
+
+uint32_t SaiCloudRipperPhyPlatform::numLanesPerCore() const {
+  // TODO(vsp): Each core in GB has 16 or 24 lanes.
+  // IFG 3, 4, 7, and 8 have 16 lanes and rest have 24 lanes.
+  return 24;
+}
+
+std::vector<PortID> SaiCloudRipperPhyPlatform::getAllPortsInGroup(
+    PortID /* portID */) const {
+  throw FbossError("SaiCloudRipperPhyPlatform doesn't support FlexPort");
+}
+std::vector<FlexPortMode> SaiCloudRipperPhyPlatform::getSupportedFlexPortModes()
+    const {
+  throw FbossError("SaiCloudRipperPhyPlatform doesn't support FlexPort");
+}
+std::optional<sai_port_interface_type_t>
+SaiCloudRipperPhyPlatform::getInterfaceType(
+    TransmitterTechnology /* transmitterTech */,
+    cfg::PortSpeed /* speed */) const {
+  throw FbossError(
+      "SaiCloudRipperPhyPlatform doesn't support getInterfaceType()");
+}
+bool SaiCloudRipperPhyPlatform::isSerdesApiSupported() const {
+  return true;
+}
+bool SaiCloudRipperPhyPlatform::supportInterfaceType() const {
+  return false;
+}
+void SaiCloudRipperPhyPlatform::initLEDs() {
+  throw FbossError("SaiCloudRipperPhyPlatform doesn't support initLEDs()");
+}
+
+sai_service_method_table_t* SaiCloudRipperPhyPlatform::getServiceMethodTable()
+    const {
+  return &kSaiServiceMethodTable;
+}
+
+const std::set<sai_api_t>& SaiCloudRipperPhyPlatform::getSupportedApiList()
+    const {
+  auto getApiList = [this]() {
+    std::set<sai_api_t> apis(getDefaultPhyAsicSupportedApis());
+    apis.insert(facebook::fboss::MacsecApi::ApiType);
+    return apis;
+  };
+
+  static const std::set<sai_api_t> kSupportedMacsecPhyApiList = getApiList();
+  return kSupportedMacsecPhyApiList;
+}
+
+void SaiCloudRipperPhyPlatform::preHwInitialized() {
+  // Credo library needs to be initialized when the library is attached as a
+  // shared library
+  credo_library_initialize();
+  // Call SaiSwitch::initSaiApis before creating SaiSwitch.
+  // Only call this function once to make sure we only initialize sai apis
+  // once even we'll create multiple SaiSwitch based on how many Cloud Ripper
+  // PHYs in the system.
+  SaiSwitch::initSaiApis(getServiceMethodTable(), getSupportedApiList());
+}
+
+void SaiCloudRipperPhyPlatform::initImpl(uint32_t hwFeaturesDesired) {
+  saiSwitch_ = std::make_unique<SaiSwitch>(this, hwFeaturesDesired);
+}
+} // namespace facebook::fboss
---
+++ fboss/agent/platforms/sai/cloudripper/SaiCloudRipperPhyPlatform.h
@@ -0,0 +1,61 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+#pragma once
+
+#include "fboss/agent/platforms/sai/SaiHwPlatform.h"
+
+namespace facebook::fboss {
+
+class CredoF104Asic;
+
+class SaiCloudRipperPhyPlatform : public SaiHwPlatform {
+ public:
+  static const std::string& getFirmwareDirectory();
+
+  SaiCloudRipperPhyPlatform(
+      std::unique_ptr<PlatformProductInfo> productInfo,
+      int phyId);
+  ~SaiCloudRipperPhyPlatform() override;
+
+  std::string getHwConfig() override;
+  HwAsic* getAsic() const override;
+  uint32_t numLanesPerCore() const override;
+  std::vector<PortID> getAllPortsInGroup(PortID portID) const override;
+  std::vector<FlexPortMode> getSupportedFlexPortModes() const override;
+  std::optional<sai_port_interface_type_t> getInterfaceType(
+      TransmitterTechnology transmitterTech,
+      cfg::PortSpeed speed) const override;
+  bool isSerdesApiSupported() const override;
+  bool supportInterfaceType() const override;
+  void initLEDs() override;
+
+  sai_service_method_table_t* getServiceMethodTable() const override;
+
+  const std::set<sai_api_t>& getSupportedApiList() const override;
+
+  void preHwInitialized() override;
+
+  SaiSwitchTraits::CreateAttributes getSwitchAttributes(
+      bool /* mandatoryOnly */) override {
+    CHECK(switchCreateAttrs_);
+    return *switchCreateAttrs_;
+  }
+  void setSwitchAttributes(SaiSwitchTraits::CreateAttributes attrs) {
+    switchCreateAttrs_ = attrs;
+  }
+
+ private:
+  int phyId_{0};
+  std::unique_ptr<CredoF104Asic> asic_;
+  std::optional<SaiSwitchTraits::CreateAttributes> switchCreateAttrs_;
+
+  void initImpl(uint32_t hwFeaturesDesired) override;
+};
+} // namespace facebook::fboss
