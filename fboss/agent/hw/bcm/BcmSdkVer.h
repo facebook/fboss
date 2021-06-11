2558f859840f32194bfa5112ff04a64895806709
Srilakshmi Adusumalli <sadusuma@fb.com>
2021-06-11 10:40:31 -0700

    Add a macro for open nsa build
    Summary: 15 APIs needs to be moved to OSS as part of task T92033512. This is the first commit to add a macro to identify OPEN_NSA build and move BcmSdkVer.h to common location since we would need these macros when we open source the APIs
    
    Reviewed By: shri-khare
    
    Differential Revision: D29010297
    
    fbshipit-source-id: da1faf5d1b4ccc0c5f413222cc90617612d81835

---
+++ fboss/agent/hw/bcm/BcmSdkVer.h
@@ -0,0 +1,21 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#pragma once
+
+#if (!defined(BCM_VER_MAJOR)) || (!defined(BCM_VER_MINOR)) || \
+    (!defined(BCM_VER_RELEASE))
+#define IS_OPENNSA
+#else
+#define BCM_VERSION(major, minor, micro) \
+  (100000 * (major) + 1000 * (minor) + (micro))
+#define BCM_SDK_VERSION \
+  BCM_VERSION(BCM_VER_MAJOR, BCM_VER_MINOR, BCM_VER_RELEASE)
+#endif
