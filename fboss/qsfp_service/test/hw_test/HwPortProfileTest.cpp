2cfbfddadf80ed2c89b9a7a9a28426943398d498
Joseph Wu <joseph5wu@fb.com>
2021-06-11 19:22:38 -0700

    Support Minipack/Yamp profile tests
    Summary:
    Since we're still using the following PortProfileID:
    ```
    PROFILE_40G_4_NRZ_NOFEC
    PROFILE_100G_4_NRZ_RS528
    PROFILE_200G_4_PAM4_RS544X2N
    ```
    on Minipack and YAMP, we need to add these profiles in the HwPortProfileTest for these two platforms.
    
    Reviewed By: jasmeetbagga
    
    Differential Revision: D29052253
    
    fbshipit-source-id: 1f26416031571f69b1fdf970a055d7ee7f00947a

--- fboss/qsfp_service/test/hw_test/HwPortProfileTest.cpp
+++ fboss/qsfp_service/test/hw_test/HwPortProfileTest.cpp
@@ -25,25 +25,6 @@ namespace facebook::fboss {
 template <cfg::PortProfileID Profile>
 class HwPortProfileTest : public HwTest {
  private:
-  cfg::PortProfileID getMatchingIphyProfile() {
-    if (!getHwQsfpEnsemble()->isXphyPlatform()) {
-      return Profile;
-    }
-    // Xphy platforms have non optical settings on the iphy side
-    // vs optical on the xphy line side
-    switch (Profile) {
-      case cfg::PortProfileID::PROFILE_100G_4_NRZ_RS528_OPTICAL:
-        return cfg::PortProfileID::PROFILE_100G_4_NRZ_RS528;
-      case cfg::PortProfileID::PROFILE_200G_4_PAM4_RS544X2N_OPTICAL:
-        return cfg::PortProfileID::PROFILE_200G_4_PAM4_RS544X2N;
-      case cfg::PortProfileID::PROFILE_400G_8_PAM4_RS544X2N_OPTICAL:
-        return cfg::PortProfileID::PROFILE_400G_8_PAM4_RS544X2N;
-      default:
-        // Add switch cases as we add more tests
-        throw FbossError("Missing iphy profile for profile id: ", Profile);
-    };
-    return Profile;
-  }
   struct Ports {
     std::vector<PortID> xphyPorts;
     std::vector<PortID> iphyPorts;
@@ -57,8 +38,7 @@ class HwPortProfileTest : public HwTest {
     auto agentConfig = getHwQsfpEnsemble()->getWedgeManager()->getAgentConfig();
     auto& swConfig = *agentConfig->thrift.sw_ref();
     for (auto& port : *swConfig.ports_ref()) {
-      if ((*port.profileID_ref() != Profile &&
-           *port.profileID_ref() != getMatchingIphyProfile()) ||
+      if (*port.profileID_ref() != Profile ||
           *port.state_ref() != cfg::PortState::ENABLED) {
         continue;
       }
@@ -181,13 +161,9 @@ class HwPortProfileTest : public HwTest {
     auto ports = findAvailablePorts();
     EXPECT_TRUE(!(ports.xphyPorts.empty() && ports.iphyPorts.empty()));
     auto portMap = std::make_unique<WedgeManager::PortMap>();
+    // Program xphy
     for (auto port : ports.xphyPorts) {
-      if (platformMode == PlatformMode::ELBERT ||
-          platformMode == PlatformMode::FUJI) {
-        // Right now only Elbert supports programming PHYs via phy manager in
-        // qsfp service.
-        getHwQsfpEnsemble()->getWedgeManager()->programXphyPort(port, Profile);
-      }
+      getHwQsfpEnsemble()->getWedgeManager()->programXphyPort(port, Profile);
       portMap->emplace(port, utility::getPortStatus(port, getHwQsfpEnsemble()));
     }
     for (auto port : ports.iphyPorts) {
@@ -252,4 +228,16 @@ TEST_PROFILE(PROFILE_100G_4_NRZ_RS528_OPTICAL)
 TEST_PROFILE(PROFILE_200G_4_PAM4_RS544X2N_OPTICAL)
 
 TEST_PROFILE(PROFILE_400G_8_PAM4_RS544X2N_OPTICAL)
+
+// The following profiles have been used by Minipack/Yamp.
+// But we prefer a more detailed profile with the optics type in the name
+// like PROFILE_100G_4_NRZ_RS528_OPTICAL
+// For now, we keep support the following profiles until we can deprecate
+// these old profiles.
+TEST_PROFILE(PROFILE_40G_4_NRZ_NOFEC)
+
+TEST_PROFILE(PROFILE_100G_4_NRZ_RS528)
+
+TEST_PROFILE(PROFILE_200G_4_PAM4_RS544X2N)
+
 } // namespace facebook::fboss
