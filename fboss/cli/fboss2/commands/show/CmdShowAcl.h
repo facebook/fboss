95047b97f78dea679ff595eb10926d83aecadf6d
Sara Nicholas <saranicholas@fb.com>
2021-06-15 15:13:46 -0700

    Improve show acl support
    Summary:
    Expand show acl support on fboss2 cli to show more information about each acl in addition to name
    
    Builds on D27491674 (https://github.com/facebook/fboss/commit/1173f199f134a8ac3404f5b4e4074f702b9ed81e)
    
    Reviewed By: shri-khare
    
    Differential Revision: D29072055
    
    fbshipit-source-id: e0eb471035529c4dee9ebc5391811eaae7a89890

--- fboss/cli/fboss2/commands/show/CmdShowAcl.h
+++ fboss/cli/fboss2/commands/show/CmdShowAcl.h
@@ -36,7 +36,52 @@ class CmdShowAcl : public CmdHandler<CmdShowAcl, CmdShowAclTraits> {
 
   void printOutput(const RetType& aclTable) {
     for (auto const& aclEntry : aclTable) {
-      std::cout << aclEntry.get_name() << std::endl;
+      std::cout << "Acl: " << aclEntry.get_name() << std::endl;
+      std::cout << "   priority: " << aclEntry.get_priority() << std::endl;
+      if (aclEntry.get_proto()) {
+        std::cout << "   proto: " << *aclEntry.get_proto() << std::endl;
+      }
+      if (aclEntry.get_srcPort()) {
+        std::cout << "   src port: " << *aclEntry.get_srcPort() << std::endl;
+      }
+      if (aclEntry.get_dstPort()) {
+        std::cout << "   dst port: " << *aclEntry.get_dstPort() << std::endl;
+      }
+      if (aclEntry.get_ipFrag()) {
+        std::cout << "   ip fragment: " << *aclEntry.get_ipFrag() << std::endl;
+      }
+      if (aclEntry.get_dscp()) {
+        std::cout << "   dscp: " << *aclEntry.get_dscp() << std::endl;
+      }
+      if (aclEntry.get_ipType()) {
+        std::cout << "   ip type: " << *aclEntry.get_ipType() << std::endl;
+      }
+      if (aclEntry.get_icmpType()) {
+        std::cout << "   icmp type: " << *aclEntry.get_icmpType() << std::endl;
+      }
+      if (aclEntry.get_icmpCode()) {
+        std::cout << "   icmp code: " << *aclEntry.get_icmpCode() << std::endl;
+      }
+      if (aclEntry.get_ttl()) {
+        std::cout << "   ttl: " << *aclEntry.get_ttl() << std::endl;
+      }
+      if (aclEntry.get_l4SrcPort()) {
+        std::cout << "   L4 src port: " << *aclEntry.get_l4SrcPort()
+                  << std::endl;
+      }
+      if (aclEntry.get_l4DstPort()) {
+        std::cout << "   L4 dst port: " << *aclEntry.get_l4DstPort()
+                  << std::endl;
+      }
+      if (aclEntry.get_dstMac()) {
+        std::cout << "   dst mac: " << *aclEntry.get_dstMac() << std::endl;
+      }
+      if (aclEntry.get_lookupClassL2()) {
+        std::cout << "   lookup class L2: " << *aclEntry.get_lookupClassL2()
+                  << std::endl;
+      }
+      std::cout << "   action: " << aclEntry.get_actionType() << std::endl;
+      std::cout << std::endl;
     }
   }
 };
