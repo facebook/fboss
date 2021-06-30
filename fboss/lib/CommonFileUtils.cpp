bff1986c16b786516c28c96e9d9545a5d9907d87
Jasmeet Bagga <jasmeetbagga@fb.com>
2021-06-29 21:03:10 -0700

    Add a createDir helper
    Summary:
    As titled. Will use this to create volatile state
    dir for qsfp_service. The code here is taken from AgentUtils.cpp,
    will change it in stacked diff to share this code
    
    Reviewed By: rajank7
    
    Differential Revision: D29385115
    
    fbshipit-source-id: e2bae208dc3492afdcaea0dc3df8142ccc31dcff

--- fboss/lib/CommonFileUtils.cpp
+++ fboss/lib/CommonFileUtils.cpp
@@ -1,16 +1,13 @@
 // (c) Facebook, Inc. and its affiliates. Confidential and proprietary.
 
 #include "fboss/lib/CommonFileUtils.h"
+#include <boost/filesystem/operations.hpp>
 #include "fboss/agent/SysError.h"
 
 #include <fcntl.h>
 
 namespace facebook::fboss {
 
-/*
- * Remove the given file. Return true if file exists and
- * we were able to remove it, false otherwise
- */
 bool removeFile(const std::string& filename) {
   int rv = unlink(filename.c_str());
   if (rv == 0) {
@@ -22,22 +19,24 @@ bool removeFile(const std::string& filename) {
     return false;
   }
   // Some other unexpected error.
-  throw facebook::fboss::SysError(
-      errno, "error while trying to remove file: ", filename);
+  throw SysError(errno, "error while trying to remove file: ", filename);
 }
 
-/*
- * Create given file. Throw if file could not be created else
- * return fd
- */
 int createFile(const std::string& filename) {
   mode_t mode = S_IRUSR | S_IWUSR | S_IROTH | S_IRGRP;
   auto fd = open(filename.c_str(), O_RDWR | O_CREAT, mode);
   if (fd < 0) {
     // Some other unexpected error.
-    throw facebook::fboss::SysError(
-        errno, "error while trying to create file ", filename);
+    throw SysError(errno, "error while trying to create file ", filename);
   }
   return fd;
+}
+
+void createDir(folly::StringPiece path) {
+  try {
+    boost::filesystem::create_directories(path.str());
+  } catch (...) {
+    throw SysError(errno, "failed to create directory \"", path, "\"");
+  }
 }
 } // namespace facebook::fboss
--- fboss/lib/CommonFileUtils.h
+++ fboss/lib/CommonFileUtils.h
@@ -2,12 +2,29 @@
 
 #pragma once
 
+#include <folly/Range.h>
 #include <string>
 
 namespace facebook::fboss {
 
+/*
+ * Remove the given file. Return true if file exists and
+ * we were able to remove it, false otherwise
+ */
 bool removeFile(const std::string& filename);
 
+/*
+ * Create given file. Throw if file could not be created else
+ * return fd
+ */
 int createFile(const std::string& filename);
+
+/*
+ * Create a directory, recursively creating all parents if necessary.
+ *
+ * If the directory already exists this function is a no-op.
+ * Throws an exception on error.
+ */
+void createDir(folly::StringPiece path);
 
 } // namespace facebook::fboss
