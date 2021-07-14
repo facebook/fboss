1c6e0be19fe393d57a6590ebd085f4d7eb3af5f3
Ron He <zecheng@fb.com>
2021-07-14 02:52:15 -0700

    Prepare to build brcm-sai 5.0.0.6
    Summary: As titled. Update compiler flag from SAI_VERSION_5_0_0_3_ODP to SAI_VERSION_5_0_0_6_ODP
    
    Differential Revision: D29691239
    
    fbshipit-source-id: e54dd8e5de812190f7c07e76672b4a18766f069b

--- fboss/agent/hw/sai/switch/SaiSwitch.cpp
+++ fboss/agent/hw/sai/switch/SaiSwitch.cpp
@@ -1355,7 +1355,7 @@ void SaiSwitch::unregisterCallbacksLocked(
     switchApi.unregisterRxCallback(switchId_);
   }
   if (isFeatureSetupLocked(FeaturesDesired::TAM_EVENT_NOTIFY_DESIRED, lock)) {
-#if defined(SAI_VERSION_5_0_0_3_ODP)
+#if defined(SAI_VERSION_5_0_0_6_ODP)
     switchApi.unregisterParityErrorSwitchEventCallback(switchId_);
 #else
     switchApi.unregisterTamEventCallback(switchId_);
@@ -1627,7 +1627,7 @@ void SaiSwitch::switchRunStateChangedImplLocked(
       }
       if (getFeaturesDesired() & FeaturesDesired::TAM_EVENT_NOTIFY_DESIRED) {
         auto& switchApi = SaiApiTable::getInstance()->switchApi();
-#if defined(SAI_VERSION_5_0_0_3_ODP)
+#if defined(SAI_VERSION_5_0_0_6_ODP)
         switchApi.registerParityErrorSwitchEventCallback(
             switchId_, (void*)__gParityErrorSwitchEventCallback);
 #else
