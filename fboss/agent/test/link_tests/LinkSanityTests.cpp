381a85a17b2888b97e1b8c1ac45c9cd1b320dca0
Jasmeet Bagga <jasmeetbagga@fb.com>
2021-07-28 00:55:58 -0700

    Test to assert that warm boot is hitless on link tests
    Summary:
    - Create a data plane loop, via the familiar technique
    of using router mac as next hop mac and disabling TTL decrements.
    Now each packet sent to the configured route, will loop to the
    other cabled port, only to be routed again endlessly.
    This is sufficient to create a pretty high rate of traffic.
    - Assert that no in discards are seen on WB here. If there
    are port flaps, we would end up hitting in discards.
    
    TODO:
    - Configure LB in config so traffic is distributed evenly and
    all ports are see sufficient traffic to detect in discards on
    flap
    
    Depends on : D29921010
    
    Differential Revision: D29925381
    
    fbshipit-source-id: d101e8d91fba3232ef7434f82a4c4efa7d6e6679

--- fboss/agent/test/link_tests/LinkSanityTests.cpp
+++ fboss/agent/test/link_tests/LinkSanityTests.cpp
@@ -4,6 +4,10 @@
 #include "fboss/agent/LldpManager.h"
 #include "fboss/agent/PlatformPort.h"
 #include "fboss/agent/SwSwitch.h"
+#include "fboss/agent/hw/gen-cpp2/hardware_stats_types.h"
+#include "fboss/agent/hw/test/LoadBalancerUtils.h"
+#include "fboss/agent/hw/test/dataplane_tests/HwTestQosUtils.h"
+#include "fboss/agent/test/EcmpSetupHelper.h"
 #include "fboss/agent/test/link_tests/LinkTest.h"
 #include "fboss/qsfp_service/lib/QsfpCache.h"
 
@@ -71,4 +75,54 @@ TEST_F(LinkTest, trafficRxTx) {
       [packetsFlowingOnAllPorts, this]() {
         checkWithRetry(packetsFlowingOnAllPorts);
       });
+}
+
+TEST_F(LinkTest, warmbootIsHitLess) {
+  boost::container::flat_set<PortDescriptor> ecmpPorts;
+  auto vlanOwningPorts =
+      utility::getPortsWithExclusiveVlanMembership(sw()->getState());
+  for (auto port : getCabledPorts()) {
+    if (vlanOwningPorts.find(PortDescriptor(port)) != vlanOwningPorts.end()) {
+      ecmpPorts.insert(PortDescriptor(port));
+    }
+  }
+  ASSERT_GT(ecmpPorts.size(), 0);
+  auto setupTraffic = [this, &ecmpPorts]() {
+    sw()->updateStateBlocking("Resolve nhops", [this, ecmpPorts](auto state) {
+      utility::EcmpSetupTargetedPorts6 ecmp6(
+          state, sw()->getPlatform()->getLocalMac());
+      return ecmp6.resolveNextHops(state, ecmpPorts);
+    });
+    utility::EcmpSetupTargetedPorts6 ecmp6(
+        sw()->getState(), sw()->getPlatform()->getLocalMac());
+    ecmp6.programRoutes(
+        std::make_unique<SwSwitchRouteUpdateWrapper>(sw()->getRouteUpdater()),
+        ecmpPorts);
+    for (const auto& nextHop : ecmp6.getNextHops()) {
+      if (ecmpPorts.find(nextHop.portDesc) != ecmpPorts.end()) {
+        utility::disableTTLDecrements(
+            sw()->getHw(), ecmp6.getRouterId(), nextHop);
+      }
+    }
+    utility::pumpTraffic(
+        true,
+        sw()->getHw(),
+        sw()->getPlatform()->getLocalMac(),
+        (*sw()->getState()->getVlans()->begin())->getID());
+    // TODO: Assert that traffic reached a certain rate
+  };
+
+  auto verifyInDiscards = [this]() {
+    for (auto i = 0; i < 2; ++i) {
+      auto portStats = sw()->getHw()->getPortStats();
+      for (auto [port, stats] : portStats) {
+        auto inDiscards = *stats.inDiscards__ref();
+        XLOG(INFO) << "Port: " << port << " in discards: " << inDiscards;
+        EXPECT_EQ(inDiscards, 0);
+      }
+      // Allow for a few rounds of stat collection
+      sleep(1);
+    }
+  };
+  verifyAcrossWarmBoots(setupTraffic, verifyInDiscards);
 }
