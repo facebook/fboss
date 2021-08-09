6dd274b2de8d89afe97df647e5887b24e1590b22
Harshit Gulati <hgulati@fb.com>
2021-08-08 22:03:49 -0700

    Pull macsec tests into its own file
    Summary: as titled. Also making "MacsecTest" as the handler class for macsec related tests. Easier to mark all future macsec tests as known bad in cfgr if all tests start with "MacsecTest."
    
    Reviewed By: jasmeetbagga
    
    Differential Revision: D30179293
    
    fbshipit-source-id: a2f63845aca65175fe5bd6ad991a90d9ff32a363

---
+++ fboss/agent/test/link_tests/MacsecTest.cpp
@@ -0,0 +1,84 @@
+#include <fboss/mka_service/if/gen-cpp2/MKAService.h>
+#include <folly/dynamic.h>
+#include <folly/experimental/coro/BlockingWait.h>
+#include <folly/json.h>
+#include <gtest/gtest.h>
+#include <thrift/lib/cpp2/async/RocketClientChannel.h>
+#include "fboss/agent/LldpManager.h"
+#include "fboss/agent/PlatformPort.h"
+#include "fboss/agent/SwSwitch.h"
+#include "fboss/agent/test/link_tests/LinkTest.h"
+#include "fboss/mka_service/if/gen-cpp2/mka_config_constants.h"
+#include "fboss/qsfp_service/lib/QsfpCache.h"
+
+using namespace ::testing;
+using namespace facebook::fboss;
+using namespace facebook::fboss::mka;
+
+class MacsecTest : public LinkTest {};
+
+TEST_F(MacsecTest, setupMkaSession) {
+  auto verify = [this]() {
+    checkWithRetry([this] { return lldpNeighborsOnAllCabledPorts(); });
+    auto port = getCabledPorts()[0];
+    std::optional<PortID> neighborPort;
+    auto lldpNeighbor = sw()->getLldpMgr()->getDB()->getNeighbors(port)[0];
+    for (auto port : *sw()->getState()->getPorts()) {
+      if (port->getName() == lldpNeighbor.getPortId()) {
+        neighborPort = port->getID();
+        break;
+      }
+    }
+    CHECK(neighborPort);
+    XLOG(INFO) << " Port: " << port << " neighbor: " << *neighborPort;
+#if FOLLY_HAS_COROUTINES
+    auto evbThread = std::make_unique<folly::ScopedEventBaseThread>();
+    std::unique_ptr<facebook::fboss::mka::MKAServiceAsyncClient> client;
+    evbThread->getEventBase()->runImmediatelyOrRunInEventBaseThreadAndWait(
+        [&]() {
+          folly::SocketAddress addr("::1", 5920);
+          auto socket = folly::AsyncSocket::newSocket(
+              folly::EventBaseManager::get()->getEventBase(), addr, 5000);
+          auto channel = apache::thrift::RocketClientChannel::newChannel(
+              std::move(socket));
+          client =
+              std::make_unique<facebook::fboss::mka::MKAServiceAsyncClient>(
+                  std::move(channel));
+        });
+
+    SCOPE_EXIT {
+      evbThread->getEventBase()->runImmediatelyOrRunInEventBaseThreadAndWait(
+          [&]() { client.reset(); });
+      evbThread.reset();
+    };
+    auto ckn = "2b7e151628aed2a6abf7158809cf4f3c";
+    auto priority = mka_config_constants::DEFAULT_KEYSERVER_PRIORITY();
+    for (auto port : {port, *neighborPort}) {
+      MKAConfig config;
+      config.l2Port_ref() = folly::to<std::string>(port);
+      config.transport_ref() = MKATransport::THRIFT_TRANSPORT;
+      config.capability_ref() = MACSecCapability::CAPABILITY_INGTY_CONF;
+      config.srcMac_ref() = sw()->getPlatform()->getLocalMac().toString();
+      config.primaryCak_ref()->key_ref() = "135bd758b0ee5c11c55ff6ab19fdb199";
+      config.primaryCak_ref()->ckn_ref() = ckn;
+      // Different priorities to allow for key server election
+      config.priority_ref() = priority--;
+      auto result = folly::coro::blockingWait(client->co_provisionCAK(config));
+      ASSERT_EQ(result, MKAResponse::SUCCESS);
+    }
+    checkWithRetry([&client, port, neighborPort, ckn]() {
+      auto activeSessions =
+          folly::coro::blockingWait(client->co_numActiveMKASessions());
+      auto portCkn = folly::coro::blockingWait(
+          client->co_getActiveCKN(folly::to<std::string>(port)));
+      auto nbrCkn = folly::coro::blockingWait(
+          client->co_getActiveCKN(folly::to<std::string>(*neighborPort)));
+      XLOG(INFO) << " Active sessions: " << activeSessions
+                 << " port ckn: " << portCkn << " nbr ckn: " << nbrCkn;
+      return activeSessions == 2 && portCkn == nbrCkn && portCkn == ckn;
+    });
+#endif
+  };
+
+  verifyAcrossWarmBoots([]() {}, verify);
+}
