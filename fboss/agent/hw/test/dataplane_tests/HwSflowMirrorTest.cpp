422bf21bdc06e7bf0dd1c494b829c39e767d2c50
Srikrishna Gopu <srikrishnagopu@fb.com>
2021-08-09 23:00:39 -0700

    update shim header length based on the platform
    Summary:
    Different vendors provide different shim header formats. Tajo provides a 9 bytes header with the following format and hence updating the test to reflect it.
    
    ```
         * sflow shim format for Tajo:
         *
         * Source system port GID - 16 bits
         * Destination system port GID - 16 bits
         * Source logical port GID - 20 bits
         * Destination logical port GID - 20 bits
    ```
    
    Reviewed By: jasmeetbagga
    
    Differential Revision: D28015106
    
    fbshipit-source-id: 1c5451c61c140ff7a6fe6af071d38c09ecce66b4

--- fboss/agent/hw/test/dataplane_tests/HwSflowMirrorTest.cpp
+++ fboss/agent/hw/test/dataplane_tests/HwSflowMirrorTest.cpp
@@ -73,10 +73,16 @@ class HwSflowMirrorTest : public HwLinkStateDependentTest {
 
   int getSflowPacketHeaderLength(bool isV6 = false) {
     auto ipHeader = isV6 ? 40 : 20;
-    auto slfowShimHeaderLength = 8;
-    if (getPlatform()->getAsic()->isSupported(
-            HwAsic::Feature::SFLOW_SHIM_VERSION_FIELD)) {
-      slfowShimHeaderLength += 4;
+    int slfowShimHeaderLength;
+    if (getPlatform()->getAsic()->getAsicType() ==
+        HwAsic::AsicType::ASIC_TYPE_TAJO) {
+      slfowShimHeaderLength = 9;
+    } else {
+      slfowShimHeaderLength = 8;
+      if (getPlatform()->getAsic()->isSupported(
+              HwAsic::Feature::SFLOW_SHIM_VERSION_FIELD)) {
+        slfowShimHeaderLength += 4;
+      }
     }
     return 18 /* ethernet header */ + ipHeader + 8 /* udp header */ +
         slfowShimHeaderLength;
