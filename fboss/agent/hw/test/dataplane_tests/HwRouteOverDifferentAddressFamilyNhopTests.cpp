31f1e4f4c2a16f900186c60f7bcd6a80d87089ce
Jasmeet Bagga <jasmeetbagga@fb.com>
2021-08-30 12:31:57 -0700

    Test for route and nhop with distinct address families - ecmp + non Ecmp
    Summary: Test both ecmp and non ecmp use cases
    
    Reviewed By: shri-khare
    
    Differential Revision: D30632807
    
    fbshipit-source-id: 0b0af2a35583c436893fbcd7bb107b21349fb65a

--- fboss/agent/hw/test/dataplane_tests/HwRouteOverDifferentAddressFamilyNhopTests.cpp
+++ fboss/agent/hw/test/dataplane_tests/HwRouteOverDifferentAddressFamilyNhopTests.cpp
@@ -27,32 +27,33 @@ class HwRouteOverDifferentAddressFamilyNhopTest
         getHwSwitch(), masterLogicalPortIds(), cfg::PortLoopbackMode::MAC);
     return cfg;
   }
-  folly::IPAddressV6 kIpv6() const {
-    return folly::IPAddressV6{"200::1"};
+  folly::IPAddressV6 kIpv6(bool ecmp) const {
+    return ecmp ? folly::IPAddressV6{"200::1"} : folly::IPAddressV6{"201::1"};
   }
-  folly::IPAddressV4 kIpv4() const {
-    return folly::IPAddressV4{"200.0.0.1"};
+  folly::IPAddressV4 kIpv4(bool ecmp) const {
+    return ecmp ? folly::IPAddressV4{"200.0.0.1"}
+                : folly::IPAddressV4{"201.0.0.1"};
   }
-  folly::CIDRNetwork kPrefixV6() const {
-    return {kIpv6(), 64};
+  folly::CIDRNetwork kPrefixV6(bool ecmp) const {
+    return {kIpv6(ecmp), 64};
   }
-  folly::CIDRNetwork kPrefixV4() const {
-    return {kIpv4(), 24};
+  folly::CIDRNetwork kPrefixV4(bool ecmp) const {
+    return {kIpv4(ecmp), 24};
   }
   template <typename AddrT>
-  folly::CIDRNetwork getPrefix() const {
+  folly::CIDRNetwork getPrefix(bool ecmp) const {
     if constexpr (std::is_same_v<AddrT, folly::IPAddressV6>) {
-      return kPrefixV6();
+      return kPrefixV6(ecmp);
     } else {
-      return kPrefixV4();
+      return kPrefixV4(ecmp);
     }
   }
   template <typename AddrT>
-  AddrT getDstIp() const {
+  AddrT getDstIp(bool ecmp) const {
     if constexpr (std::is_same_v<AddrT, folly::IPAddressV6>) {
-      return kIpv6();
+      return kIpv6(ecmp);
     } else {
-      return kIpv4();
+      return kIpv4(ecmp);
     }
   }
   std::unique_ptr<TxPacket> makeTxPacket(const folly::IPAddress& dstIp) const {
@@ -77,27 +78,39 @@ class HwRouteOverDifferentAddressFamilyNhopTest
   template <typename RouteAddrT, typename NhopAddrT>
   void runTest() {
     auto setup = [this] {
-      auto addRoute = [this](auto nw, const auto& nhops) {
+      utility::EcmpSetupTargetedPorts<NhopAddrT> ecmpHelper(
+          getProgrammedState());
+      auto addRoute = [this, &ecmpHelper](auto nw, const auto& nhopPorts) {
+        std::vector<NhopAddrT> nhops;
+        for (auto nhopPort : nhopPorts) {
+          nhops.push_back(ecmpHelper.nhop(nhopPort).ip);
+        }
         auto updater = getRouteUpdater();
         updater->addRoute(
             RouterID(0),
             nw.first,
             nw.second,
             ClientID::BGPD,
-            RouteNextHopEntry(nhops, AdminDistance::EBGP));
+            RouteNextHopEntry(makeNextHops(nhops), AdminDistance::EBGP));
         updater->program();
       };
-      utility::EcmpSetupTargetedPorts<NhopAddrT> ecmpHelper(
-          getProgrammedState());
-      PortDescriptor nhopPort(masterLogicalPortIds()[0]);
-      applyNewState(
-          ecmpHelper.resolveNextHops(getProgrammedState(), {nhopPort}));
-      auto nhops = makeNextHops<NhopAddrT>({ecmpHelper.nhop(nhopPort).ip});
-      addRoute(getPrefix<RouteAddrT>(), nhops);
+      boost::container::flat_set<PortDescriptor> ecmpNhopPorts(
+          {PortDescriptor(masterLogicalPortIds()[0]),
+           PortDescriptor(masterLogicalPortIds()[1])});
+      boost::container::flat_set<PortDescriptor> nonEcmpNhopPorts(
+          {PortDescriptor(masterLogicalPortIds()[2])});
+      for (auto ecmp : {true, false}) {
+        auto nhopPorts = ecmp ? ecmpNhopPorts : nonEcmpNhopPorts;
+        applyNewState(
+            ecmpHelper.resolveNextHops(getProgrammedState(), nhopPorts));
+        addRoute(getPrefix<RouteAddrT>(ecmp), nhopPorts);
+      }
     };
     auto verify = [this]() {
-      getHwSwitchEnsemble()->ensureSendPacketSwitched(
-          makeTxPacket(getDstIp<RouteAddrT>()));
+      for (auto ecmp : {true, false}) {
+        getHwSwitchEnsemble()->ensureSendPacketSwitched(
+            makeTxPacket(getDstIp<RouteAddrT>(ecmp)));
+      }
     };
     verifyAcrossWarmBoots(setup, verify);
   }
