631472057f77c07305d1ab365ab532390ab55176
Jasmeet Bagga <jasmeetbagga@fb.com>
2021-09-07 18:28:32 -0700

    Pull up checkWithRetry to AgentInitializer
    Summary: As titled. Will use it in prod agent init too.
    
    Reviewed By: rajank7
    
    Differential Revision: D30787868
    
    fbshipit-source-id: bad31fcb30b42792f2e7543847dad0389d8e82e0

--- fboss/agent/test/AgentTest.h
+++ fboss/agent/test/AgentTest.h
@@ -91,21 +91,6 @@ class AgentTest : public ::testing::Test, public AgentInitializer {
     verifyAcrossWarmBoots([]() {}, verify, []() {}, []() {});
   }
 
-  template <typename CONDITION_FN>
-  void checkWithRetry(
-      CONDITION_FN condition,
-      int retries = 10,
-      std::chrono::duration<uint32_t, std::milli> msBetweenRetry =
-          std::chrono::milliseconds(1000)) const {
-    while (retries--) {
-      if (condition()) {
-        return;
-      }
-      std::this_thread::sleep_for(msBetweenRetry);
-    }
-    throw FbossError("Verify with retry failed, condition was never satisfied");
-  }
-
  private:
   template <typename AddrT>
   void resolveNeighbor(
