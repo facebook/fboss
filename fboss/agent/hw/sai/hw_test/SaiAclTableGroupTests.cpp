2c1bb3c509e41f1b660d253a03d82d2cfe10cbf2
Shrikrishna (Shri) Khare <skhare@fb.com>
2021-09-20 15:47:03 -0700

    HwTests for SaiAclTableGroup: multiple ACL Tables no entries
    Summary:
    This is towards supporting Multiple ACL Table in FBOSS.
    
    As titled.
    
    Reviewed By: jasmeetbagga
    
    Differential Revision: D30767313
    
    fbshipit-source-id: 5b5ead85915f0e20d48fe452889f22de982dd653

--- fboss/agent/hw/sai/hw_test/SaiAclTableGroupTests.cpp
+++ fboss/agent/hw/sai/hw_test/SaiAclTableGroupTests.cpp
@@ -54,6 +54,34 @@ class SaiAclTableGroupTest : public HwTest {
   bool isSupported() const {
     return HwTest::isSupported(HwAsic::Feature::MULTIPLE_ACL_TABLES);
   }
+
+  void addAclTable1(cfg::SwitchConfig& cfg) {
+    // table1 with dscp matcher ACL entry
+    utility::addAclTable(
+        &cfg,
+        kAclTable1(),
+        1 /* priority */,
+        {cfg::AclTableActionType::PACKET_ACTION},
+        {cfg::AclTableQualifier::DSCP});
+  }
+
+  void addAclTable2(cfg::SwitchConfig& cfg) {
+    // table2 with ttl matcher ACL entry
+    utility::addAclTable(
+        &cfg,
+        kAclTable2(),
+        2 /* priority */,
+        {cfg::AclTableActionType::COUNTER},
+        {cfg::AclTableQualifier::TTL});
+  }
+
+  std::string kAclTable1() const {
+    return "table1";
+  }
+
+  std::string kAclTable2() const {
+    return "table2";
+  }
 };
 
 TEST_F(SaiAclTableGroupTest, SingleAclTableGroup) {
@@ -70,6 +98,29 @@ TEST_F(SaiAclTableGroupTest, SingleAclTableGroup) {
 
   auto verify = [=]() {
     ASSERT_TRUE(isAclTableGroupEnabled(getHwSwitch(), SAI_ACL_STAGE_INGRESS));
+  };
+
+  verifyAcrossWarmBoots(setup, verify);
+}
+
+TEST_F(SaiAclTableGroupTest, MultipleTablesNoEntries) {
+  ASSERT_TRUE(isSupported());
+
+  auto setup = [this]() {
+    auto newCfg = initialConfig();
+
+    utility::addAclTableGroup(
+        &newCfg, cfg::AclStage::INGRESS, "Ingress Table Group");
+    addAclTable1(newCfg);
+    addAclTable2(newCfg);
+
+    applyNewConfig(newCfg);
+  };
+
+  auto verify = [=]() {
+    ASSERT_TRUE(isAclTableGroupEnabled(getHwSwitch(), SAI_ACL_STAGE_INGRESS));
+    ASSERT_TRUE(utility::isAclTableEnabled(getHwSwitch(), kAclTable1()));
+    ASSERT_TRUE(utility::isAclTableEnabled(getHwSwitch(), kAclTable2()));
   };
 
   verifyAcrossWarmBoots(setup, verify);
