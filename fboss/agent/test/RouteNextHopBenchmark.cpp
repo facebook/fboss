b93ae2959a0bfda9a70f1b97ab38def6796977b3
Midhun Somasundaran <msomasundaran@fb.com>
2021-09-20 18:53:32 -0700

    Add benchmark test for route nexthop normalization
    Summary: Adding a benchmark for the normalization of ucmp weights in nexthop entry. The test checks for RSW scale (10k) and FSW (30k) with 8 and 36 paths respectively. Weight of each path is randomly set and the weights are different for each route.
    
    Reviewed By: saifhhasan
    
    Differential Revision: D31039166
    
    fbshipit-source-id: 80a2b152c5beffec8101633d74cf199f75bcdd13

---
+++ fboss/agent/test/RouteNextHopBenchmark.cpp
@@ -0,0 +1,78 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+#include <folly/Benchmark.h>
+#include <folly/Random.h>
+#include "fboss/agent/state/RouteNextHopEntry.h"
+
+using namespace facebook::fboss;
+using folly::IPAddress;
+
+namespace {
+const AdminDistance kDefaultAdminDistance = AdminDistance::EBGP;
+static constexpr int kNumSSWs = 36;
+static constexpr int kRSWEcmpWidth = 64;
+static constexpr int kFSWEcmpWidth = 128;
+static constexpr int kRSWNumPaths = 8;
+static constexpr int kFSWNumPaths = 36;
+static constexpr int kRSWNumRoutes = 10000;
+static constexpr int kFSWNumRoutes = 30000;
+} // namespace
+
+void RouteNextHopEntryScaleOptimized(
+    bool optimized,
+    int ecmpWidth,
+    int numPaths,
+    int numRoutes) {
+  std::vector<RouteNextHopEntry> rNhops;
+  FLAGS_optimized_ucmp = optimized;
+  FLAGS_ecmp_width = ecmpWidth;
+  BENCHMARK_SUSPEND {
+    // limiting for ease of generating nh address
+    CHECK(10 + numPaths < 100);
+    for (auto routeIndex = 0; routeIndex < numRoutes; ++routeIndex) {
+      RouteNextHopSet nhops;
+      for (auto pathIndex = 10; pathIndex < 10 + numPaths; ++pathIndex) {
+        std::string nhAddrStr =
+            fmt::format("2401:db{}:e112:9103:1028::01", pathIndex);
+        nhops.emplace(ResolvedNextHop(
+            folly::IPAddress(nhAddrStr),
+            InterfaceID(pathIndex),
+            folly::Random::rand32() % kNumSSWs));
+      }
+      auto nh = RouteNextHopEntry(nhops, kDefaultAdminDistance);
+      rNhops.emplace_back(nh);
+    }
+  }
+
+  for (auto& nh : rNhops) {
+    auto normalizedNhops = nh.normalizedNextHops();
+  }
+}
+
+void RouteNextHopEntryScaleOptimizedRSW(uint32_t /* iters */, bool optimized) {
+  RouteNextHopEntryScaleOptimized(
+      optimized, kRSWEcmpWidth, kRSWNumPaths, kRSWNumRoutes);
+}
+
+void RouteNextHopEntryScaleOptimizedFSW(uint32_t /* iters */, bool optimized) {
+  RouteNextHopEntryScaleOptimized(
+      optimized, kFSWEcmpWidth, kFSWNumPaths, kFSWNumRoutes);
+}
+
+BENCHMARK_PARAM(RouteNextHopEntryScaleOptimizedRSW, true);
+BENCHMARK_PARAM(RouteNextHopEntryScaleOptimizedRSW, false);
+BENCHMARK_PARAM(RouteNextHopEntryScaleOptimizedFSW, true);
+BENCHMARK_PARAM(RouteNextHopEntryScaleOptimizedFSW, false);
+
+int main(int argc, char** argv) {
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+  folly::runBenchmarks();
+  return 0;
+}
