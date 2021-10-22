6741ee7774494d4d55177c90614f6bff81e78b8d
Peyman Gardideh <pgardideh@fb.com>
2021-10-22 11:40:41 -0700

    Subcommand args [4/7] - Add logic to resolve subcommand argument type
    Summary: Add resolve_arg_types to recursively traverse up the command tree starting from the leaf and construct a tuple type for all arguments, in order. resolve_arg_types will filter out all std::monostate since those are for subcommands that don't take any arguments. It also keeps a nonfiltered version which will be used to figure out which runtime args to keep
    
    Differential Revision: D31462718
    
    fbshipit-source-id: 4c1b32ea884bc8759486c2eb25a9f28d79a21a78

--- fboss/cli/fboss2/CmdHandler.h
+++ fboss/cli/fboss2/CmdHandler.h
@@ -22,14 +22,65 @@
 #include <fmt/color.h>
 #include <fmt/format.h>
 #include <folly/logging/xlog.h>
+#include <type_traits>
+#include <variant>
 
 namespace facebook::fboss {
+
+template <typename Cmd>
+class resolve_arg_types {
+  template <typename Front, typename Tuple>
+  struct tuple_push_back {};
+
+  // Unwrap tuple and create a new type with our new type at the end
+  template <typename Front, typename... Ts>
+  struct tuple_push_back<Front, std::tuple<Ts...>> {
+    using type = std::tuple<Ts..., Front>;
+  };
+
+  // recurse up the linked list of classes
+  using parent = resolve_arg_types<typename Cmd::Traits::ParentCmd>;
+  using appended_type = typename tuple_push_back<
+      typename Cmd::Traits::ObjectArgType,
+      typename parent::filtered_type>::type;
+
+  template <typename T>
+  using is_monostate = std::is_same<T, std::monostate>;
+
+ public:
+  // filter out std::monostate
+  using filtered_type = std::conditional_t<
+      !is_monostate<typename Cmd::Traits::ObjectArgType>::value,
+      appended_type,
+      typename parent::filtered_type>;
+  // Used at run time to determine which index args we need and which to get rid
+  // of (i.e. which positions are std::monostate)
+  using unfiltered_type = typename tuple_push_back<
+      typename Cmd::Traits::ObjectArgType,
+      typename parent::unfiltered_type>::type;
+};
+
+// Base case for recursion (when ParentCmd is void)
+template <>
+struct resolve_arg_types<void> {
+  using filtered_type = std::tuple<>;
+  using unfiltered_type = std::tuple<>;
+};
+
+struct BaseCommandTraits {
+  // Only for top level commands, nested subcommands will override this
+  using ParentCmd = void;
+  static constexpr utils::ObjectArgTypeId ObjectArgTypeId =
+      utils::ObjectArgTypeId::OBJECT_ARG_TYPE_ID_NONE;
+  using ObjectArgType = std::monostate;
+};
 
 template <typename CmdTypeT, typename CmdTypeTraits>
 class CmdHandler {
  public:
   static constexpr utils::ObjectArgTypeId ObjectArgTypeId =
       CmdTypeTraits::ObjectArgTypeId;
+  using Traits = CmdTypeTraits;
   using ObjectArgType = typename CmdTypeTraits::ObjectArgType;
   using RetType = typename CmdTypeTraits::RetType;
 
