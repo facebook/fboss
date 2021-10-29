697af6d9867d27009f3b1a9205ca4af5a551125c
Joseph Wu <joseph5wu@fb.com>
2021-10-29 12:19:40 -0700

    Switch to use /etc/coop/qsfp.conf for qsfp_service
    Summary:
    Before we can have the new coop to generate the symlink for all FBOSS devices,
    we need to make current QsfpConfig to read from these two different paths but using the same content
    
    Differential Revision: D32009570
    
    fbshipit-source-id: f2562b219ca14f8f17dfa2065dd9986cf82694c4

--- fboss/qsfp_service/QsfpConfig.cpp
+++ fboss/qsfp_service/QsfpConfig.cpp
@@ -4,17 +4,31 @@
 #include "fboss/agent/FbossError.h"
 
 #include <folly/FileUtil.h>
+#include <folly/logging/xlog.h>
 #include <thrift/lib/cpp2/protocol/Serializer.h>
 
 DEFINE_string(
     qsfp_config,
-    "/etc/coop/qsfp/current",
+    "/etc/coop/qsfp.conf",
     "The path to the local JSON configuration file");
+
+// TODO(joseph5wu) Will deprecate this file and use symlink directly
+DEFINE_string(
+    qsfp_config_current,
+    "/etc/coop/qsfp/current",
+    "[Deprecated] The path to the local JSON configuration file");
 
 namespace facebook::fboss {
 
 std::unique_ptr<QsfpConfig> QsfpConfig::fromDefaultFile() {
-  return fromFile(FLAGS_qsfp_config);
+  try {
+    return fromFile(FLAGS_qsfp_config);
+  } catch (const FbossError& err) {
+    XLOG(WARN) << " Can't get qsfp config from " << FLAGS_qsfp_config << ": "
+               << *err.message_ref() << ". Switch back to use "
+               << FLAGS_qsfp_config_current;
+    return fromFile(FLAGS_qsfp_config_current);
+  }
 }
 
 std::unique_ptr<QsfpConfig> QsfpConfig::fromFile(folly::StringPiece path) {
@@ -22,6 +36,7 @@ std::unique_ptr<QsfpConfig> QsfpConfig::fromFile(folly::StringPiece path) {
   if (!folly::readFile(path.data(), configStr)) {
     throw FbossError("unable to read ", path);
   }
+  XLOG(DBG2) << "Read qsfp config from " << path;
   return fromRawConfig(std::move(configStr));
 }
 
