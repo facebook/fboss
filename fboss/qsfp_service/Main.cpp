d424744bf10da4e101143809cb3a31af49842327
Joseph Wu <joseph5wu@fb.com>
2021-10-29 15:32:30 -0700

    Init flag defaults from config
    Summary: This will enable qsfp_service to use the defaultCommandLineArgs from qsfp.conf to initialize flag defaults
    
    Reviewed By: jasmeetbagga
    
    Differential Revision: D32013499
    
    fbshipit-source-id: 703f6be8c90f6c681ca5561dbeadd1b6fce88a90

--- fboss/qsfp_service/Main.cpp
+++ fboss/qsfp_service/Main.cpp
@@ -32,7 +32,24 @@ DECLARE_int32(port);
 
 FOLLY_INIT_LOGGING_CONFIG("fboss=DBG2; default:async=true");
 
+void initFlagDefaults(int argc, char** argv) {
+  // one pass over flags, but don't clear argc/argv. We only do this
+  // to extract the 'qsfp_config' arg.
+  gflags::ParseCommandLineFlags(&argc, &argv, false);
+  auto qsfpConfig = QsfpConfig::fromDefaultFile();
+  for (auto item : *qsfpConfig->thrift.defaultCommandLineArgs_ref()) {
+    // logging not initialized yet, need to use std::cerr
+    std::cerr << "Overriding default flag from config: " << item.first.c_str()
+              << "=" << item.second.c_str() << std::endl;
+    gflags::SetCommandLineOptionWithMode(
+        item.first.c_str(), item.second.c_str(), gflags::SET_FLAGS_DEFAULT);
+  }
+}
+
 int main(int argc, char** argv) {
+  // Read the config and set default command line arguments
+  initFlagDefaults(argc, argv);
+
   qsfpServiceInit(&argc, &argv);
 
   auto transceiverManager = createWedgeManager();
