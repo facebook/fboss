3d710ec2ccc5de1bf60c400f9eaae8da05d79d5b
Mike Choi <mikechoi@fb.com>
2021-11-01 19:52:38 -0700

    Initial checkin for Fan Service main class.
    Summary:
    [FBOSS/FanService]: Initial checkin for Fan Service main class.
    This class integrates all other classes of Fan Services defined in other diffs.
    
    Reviewed By: binhuang00
    
    Differential Revision: D31869569
    
    fbshipit-source-id: 250ade702e16e417ea74973edcec13a863b65fed

---
+++ fboss/platform/fan_service/FanService.cpp
@@ -0,0 +1,197 @@
+// Copyright 2021- Facebook. All rights reserved.
+
+// Implementation of FanService class. Refer to .h file
+// for functional description
+#include "fboss/platform/fan_service/FanService.h"
+
+FanService::FanService(std::string configFileName) {
+  lastControlExecutionTimeSec_ = 0;
+  lastSensorFetchTimeSec_ = 0;
+
+  pBsp_ = NULL;
+  pSensorData_ = NULL;
+
+  odsTier_ = "";
+
+  cfgFile_ = configFileName;
+  enableOdsStreamer_ = true;
+
+  return;
+}
+
+bool FanService::getOdsStreamerEnable() {
+  return enableOdsStreamer_;
+}
+
+void FanService::setOdsStreamerEnable(bool enable) {
+  enableOdsStreamer_ = enable;
+}
+
+void FanService::setControlFrequency(uint64_t sec) {
+  controlFrequencySec_ = sec;
+}
+
+unsigned int FanService::getControlFrequency() {
+  return controlFrequencySec_;
+}
+
+void FanService::setSensorFetchFrequency(uint64_t sec) {
+  sensorFetchFrequencySec_ = sec;
+}
+
+unsigned int FanService::getSensorFetchFrequency() {
+  return sensorFetchFrequencySec_;
+}
+
+void FanService::setOdsTier(std::string oT) {
+  odsTier_ = oT;
+}
+std::shared_ptr<Bsp> FanService::BspFactory() {
+  Bsp* returnVal = NULL;
+  switch (pConfig_->bspType) {
+    // In many cases, generic BSP is enough.
+    case kBspGeneric:
+    case kBspDarwin:
+    case kBspLassen:
+    case kBspMinipack3:
+      returnVal = new Bsp();
+      break;
+    // For unit testing, we use Mock (Mokujin) BSP.
+    case kBspMokujin:
+      returnVal = static_cast<Bsp*>(new Mokujin());
+      break;
+
+    default:
+      facebook::fboss::FbossError("Invalid BSP Type given to BSP Factory!");
+      break;
+  }
+  std::shared_ptr<Bsp> returnValShared(returnVal);
+  return returnValShared;
+}
+
+void FanService::kickstart() {
+  int rc = 0;
+
+  // Read Config
+  pConfig_ = std::make_shared<ServiceConfig>();
+  pConfig_->parse(cfgFile_);
+
+  // Get the proper BSP object from BSP factory,
+  // according to the parsed config, then run init routine.
+  pBsp_ = BspFactory();
+  // Initialize SensorData
+  pSensorData_ = std::make_shared<SensorData>();
+
+  // Start ODS Streamer and attach sensors
+  setOdsStreamerEnable(true);
+  pOdsStreamer_ = std::make_shared<OdsStreamer>(pSensorData_, odsTier_);
+
+  // Start control logic, and attach bsp and sensors
+  pControlLogic_ = std::make_shared<ControlLogic>(pConfig_, pBsp_);
+}
+
+int FanService::controlFan(/*folly::EventBase* evb*/) {
+  int rc = 0;
+  bool hasSensorDataUpdate = false;
+  uint64_t currentTimeSec = facebook::WallClockUtil::NowInSecFast();
+  // Update Sensor Value based according to fetch frequency
+  if ((currentTimeSec - lastSensorFetchTimeSec_) >=
+      pConfig_->getSensorFetchFrequency()) {
+    lastSensorFetchTimeSec_ = currentTimeSec;
+    // Get the updated sensor data
+    pBsp_->getSensorData(pConfig_, pSensorData_);
+    hasSensorDataUpdate = true;
+  }
+  // Change Fan PWM as needed according to control execution frequency
+  if ((currentTimeSec - lastControlExecutionTimeSec_) >=
+      pConfig_->getControlFrequency()) {
+    lastControlExecutionTimeSec_ = currentTimeSec;
+    pControlLogic_->updateControl(pSensorData_);
+  }
+
+  return rc;
+}
+
+OdsStreamer* FanService::getOdsStreamer() {
+  return pOdsStreamer_.get();
+}
+
+int FanService::runMock(std::string mockInputFile, std::string mockOutputFile) {
+  int rc = 0;
+  uint64_t timeToAdvanceSec = 0, currentTimeSec = 0;
+  bool loopControl = true;
+  std::string simulationSensorName;
+  float simulationSensorValue;
+  // Make sure BSP is a mock bsp type
+  if (pConfig_->bspType != kBspMokujin) {
+    XLOG(ERR) << "Mock mode is enabled, but BSP is not a Mock BSP!";
+    return -1;
+  }
+  Mokujin* pMokujin = static_cast<Mokujin*>(pBsp_.get());
+  pMokujin->openIOFiles(mockInputFile, mockOutputFile);
+  int sensorIntervalSec = (int)(pConfig_->getSensorFetchFrequency());
+  int fanIntervalSec = (int)(pConfig_->getControlFrequency());
+
+  while (loopControl) {
+    // Read and Parse Mock data as needed
+    XLOG(INFO) << "Current Time : " << currentTimeSec;
+    pMokujin->setTimeStamp(currentTimeSec);
+    while ((!pMokujin->isEof()) &&
+           (pMokujin->hasAnyMoreEvent(currentTimeSec))) {
+      XLOG(INFO) << "Processing system state change event.";
+      pMokujin->getNextSensorEvent(
+          currentTimeSec, simulationSensorName, simulationSensorValue);
+      XLOG(INFO) << "Read System Event for " << simulationSensorName
+                 << " set to " << simulationSensorValue;
+      if (simulationSensorName == "end") {
+        XLOG(INFO) << "No more event data. Time to end the simulation";
+        loopControl = false;
+        break;
+      } else if (simulationSensorName != "") {
+        pMokujin->updateSimulationData(
+            simulationSensorName, simulationSensorValue);
+        XLOG(INFO) << "Updated system status.";
+      }
+    }
+    XLOG(INFO)
+        << "Done changing system state according to simulation scenario.";
+    // Read sensor if needed
+    if ((currentTimeSec - lastSensorFetchTimeSec_) >=
+        pConfig_->getSensorFetchFrequency()) {
+      lastSensorFetchTimeSec_ = currentTimeSec;
+      XLOG(INFO) << "Time to read sensor data";
+      pBsp_->getSensorData(pConfig_, pSensorData_);
+      XLOG(INFO) << "Done reading sensor data";
+    }
+    // Update fan as needed
+    if ((currentTimeSec - lastControlExecutionTimeSec_) >=
+        pConfig_->getControlFrequency()) {
+      lastControlExecutionTimeSec_ = currentTimeSec;
+      XLOG(INFO) << "Time for running fan control logic";
+      pControlLogic_->updateControl(pSensorData_);
+      XLOG(INFO) << "Done adjusting fan speed";
+    }
+
+    // Figure out when is the next event,
+    // so that we can jump to that time in the future.
+    timeToAdvanceSec =
+        lastSensorFetchTimeSec_ + pConfig_->getSensorFetchFrequency();
+    if (timeToAdvanceSec >
+        lastControlExecutionTimeSec_ + pConfig_->getControlFrequency()) {
+      timeToAdvanceSec =
+          lastControlExecutionTimeSec_ + pConfig_->getControlFrequency();
+    }
+    if (timeToAdvanceSec > pMokujin->getNextEventTime()) {
+      timeToAdvanceSec = pMokujin->getNextEventTime();
+    }
+    if (timeToAdvanceSec < currentTimeSec + 1) {
+      timeToAdvanceSec = currentTimeSec + 1;
+    }
+    // Update current time with the next event time (future)
+    currentTimeSec = timeToAdvanceSec;
+  }
+  XLOG(INFO) << "Done running simulation.";
+  pMokujin->closeFiles();
+  XLOG(INFO) << "File closed. Exiting...";
+  return rc;
+}
---
+++ fboss/platform/fan_service/FanService.h
@@ -0,0 +1,84 @@
+// Copyright 2021- Facebook. All rights reserved.
+#pragma once
+
+#include <gflags/gflags.h>
+#include <string>
+#include "Bsp.h"
+#include "ControlLogic.h"
+#include "Mokujin.h"
+#include "OdsStreamer.h"
+#include "SensorData.h"
+#include "ServiceConfig.h"
+
+// FanService    : The main Class the represent the fan_service
+//                 Instantiates the following classes.
+//
+//               - ControlLogic class : PWM control logic
+//               - ServiceConfig class : parse config file, and keep the info
+//               - Bsp class : All I/O functions including Thrift handler
+//                    - Mokujin class : A mock of Bsp class for unit testing
+//               - SensorData class : Stores sensor data and the timestamps
+//               - OdsStreamer class : Logic to stream sensor data to ODS
+
+class FanService {
+ public:
+  // Constructor / destructor
+  FanService(std::string configFileName);
+  ~FanService() {} // Make compiler happy in handling smart pointers
+  // Instantiates all classes used by Fan Service
+  void kickstart();
+  // Runs Fan PWM control logic
+  int controlFan();
+  // A special function to run Fan Service as a Mock
+  // (simulation for unit testing)
+  int runMock(std::string mockInputFile, std::string mockOutputFile);
+  // Public function to expose ODS streamer. Needed by Handler class
+  OdsStreamer* getOdsStreamer();
+  // Set / Change ODS Tier based on config file
+  void setOdsTier(std::string oT);
+
+ private:
+  // Attributes
+  // BSP contains platform specific I/O methonds
+  std::shared_ptr<Bsp> pBsp_;
+  // ServiceConfig parses the configuration, and keep the data
+  std::shared_ptr<ServiceConfig> pConfig_;
+  // Control logic determines fan pwm based on config and sensor read
+  std::shared_ptr<ControlLogic> pControlLogic_;
+  // ODS streamer streams sensor data to ODS (optional)
+  std::shared_ptr<OdsStreamer> pOdsStreamer_;
+  // SensorData keeps all the latest sensor reading. Also provides
+  // metadata that describes how to read that sensor data
+  std::shared_ptr<SensorData> pSensorData_;
+  // Knob to enable ODS streamer. Default On.
+  bool enableOdsStreamer_;
+  // Config File Name
+  std::string cfgFile_;
+  // ODS Tier to stream data
+  std::string odsTier_;
+  // The timestamp of the last PWM control logic execution
+  uint64_t lastControlExecutionTimeSec_;
+  // The timestamp of the last sensor data fetch
+  uint64_t lastSensorFetchTimeSec_;
+  // How often we run fan control logic?
+  uint64_t controlFrequencySec_;
+  // How often do we fetch sensor data?
+  uint64_t sensorFetchFrequencySec_;
+
+  // Methods
+  // ODS Streamer enable
+  void setOdsStreamerEnable(bool enable);
+  // Check if ods streamer enabled
+  bool getOdsStreamerEnable();
+  // Control Logic Execution Frequency in seconds
+  void setControlFrequency(uint64_t sec);
+  unsigned int getControlFrequency();
+  // Sensor Fetch Frequency in seconds
+  void setSensorFetchFrequency(uint64_t sec);
+  unsigned int getSensorFetchFrequency();
+  // The factory method to return the proper BSP object,
+  // based on the platform type specified in config file
+  std::shared_ptr<Bsp> BspFactory();
+  // Main Loop for standalone execution
+  int mainLoop();
+};
