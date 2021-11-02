fb437b3ee50ad6acd25d49a10f426394281d66cc
Mike Choi <mikechoi@fb.com>
2021-11-02 09:47:27 -0700

    Initial Checkin of FanService's ODS Streamer Class
    Summary: Ods Streamer capable of sending sensor data to ODS without using Central Proxy
    
    Reviewed By: tao-ren
    
    Differential Revision: D31872757
    
    fbshipit-source-id: 8926d04cafb571dc85a90e6ec91d43734ae58dd1

---
+++ fboss/platform/fan_service/OdsStreamer.cpp
@@ -0,0 +1,80 @@
+// Copyright 2021- Facebook. All rights reserved.
+
+// Implementation of OdsStreamer class. Refer to .h
+// for functional description
+#include "fboss/platform/fan_service/OdsStreamer.h"
+
+DECLARE_string(ods_tier);
+
+OdsStreamer::OdsStreamer(std::shared_ptr<SensorData> pSd, std::string oT) {
+  pSensorData_ = pSd;
+  odsTier_ = oT;
+}
+
+OdsStreamer::~OdsStreamer() {}
+
+int OdsStreamer::publishToOds(
+    folly::EventBase* evb,
+    std::vector<facebook::maestro::ODSAppValue> values,
+    std::string odsTier) {
+  int rc = 0;
+  XLOG(INFO) << "ODS Streamer : Publisher : Entered. ";
+  try {
+    facebook::maestro::SetOdsRawValuesRequest request;
+    *request.dataPoints_ref() = std::move(values);
+    /* logging counters of type double */
+    /* prepare the client */
+    auto odsClient = folly::to_shared_ptr(
+        facebook::servicerouter::cpp2::getClientFactory()
+            .getSRClientUnique<facebook::maestro::OdsRouterAsyncClient>(
+                odsTier, evb));
+    /* log data points to ODS */
+    odsClient->sync_setOdsRawValues(request);
+  } catch (const std::exception& ex) {
+    XLOG(ERR) << "Error while publishing to ODS : " << folly::exceptionStr(ex);
+    rc = -1;
+  }
+  XLOG(INFO) << "ODS Streamer : Publisher : Exiting with rc=" << rc;
+  return rc;
+}
+
+facebook::maestro::ODSAppValue OdsStreamer::getOdsAppValue(
+    std::string key,
+    int64_t value,
+    uint64_t timeStampSec) {
+  facebook::maestro::ODSAppValue retVal;
+  retVal.key_ref() = key;
+  retVal.value_ref() = value;
+  retVal.unixTime_ref() = (int64_t)timeStampSec;
+  retVal.entity_ref() = facebook::network::NetworkUtil::getLocalHost(true);
+  retVal.category_id_ref() =
+      folly::to_signed(int32_t(facebook::monitoring::OdsCategoryId::ODS_FBOSS));
+  return retVal;
+}
+
+int OdsStreamer::postData(folly::EventBase* evb) {
+  int rc = 0;
+  std::vector<facebook::maestro::ODSAppValue> dataToStream;
+  float value = 0;
+  int64_t int64Value;
+  XLOG(INFO) << "ODS Streamer : Started";
+  std::vector<std::string> keyList = pSensorData_->getKeyLists();
+  for (auto sensorName = keyList.begin(); sensorName != keyList.end();
+       ++sensorName) {
+    if (pSensorData_->getSensorEntryType(*sensorName) == kSensorEntryInt) {
+      value = (float)pSensorData_->getSensorDataInt(*sensorName);
+    } else {
+      value = pSensorData_->getSensorDataFloat(*sensorName);
+    }
+    int64Value = static_cast<int64_t>(value * 1000.0);
+    XLOG(INFO) << "ODS Streamer : Packing " << *sensorName << " : " << value;
+    dataToStream.push_back(getOdsAppValue(
+        *sensorName,
+        value,
+        (uint64_t)pSensorData_->getLastUpdated(*sensorName)));
+  }
+  XLOG(INFO) << "ODS Streamer : Data packed. Publishing";
+  rc = publishToOds(evb, dataToStream, odsTier_);
+  XLOG(INFO) << "ODS Streamer : Done publishing with rc : " << rc;
+  return rc;
+}
---
+++ fboss/platform/fan_service/OdsStreamer.h
@@ -0,0 +1,43 @@
+// Copyright 2021- Facebook. All rights reserved.
+
+#pragma once
+// OdsStreamer is a part of Fan Service
+// Role : To stream the sensor data to ODS.
+
+// Folly library header file for conversion and log
+#include <folly/Conv.h>
+#include <folly/logging/xlog.h>
+// FB service routines for streaming ODS data
+#include <fb303/ThreadCachedServiceData.h>
+#include "common/fbwhoami/FbWhoAmI.h"
+#include "common/network/NetworkUtil.h"
+#include "common/strings/UUID.h"
+#include "common/time/Time.h"
+#include "common/time/TimeUtil.h"
+#include "maestro/if/OdsRouter/gen-cpp2/OdsRouter.h"
+#include "monitoring/common/OdsCategoryId.h"
+#include "scribe/client/ScribeClient.h"
+#include "servicerouter/client/cpp2/ServiceRouter.h"
+// Headerfile for SensorData class
+#include "SensorData.h"
+
+class OdsStreamer {
+ public:
+  // Constructor / Destructor
+  OdsStreamer(std::shared_ptr<SensorData> pSd, std::string oT);
+  ~OdsStreamer();
+  // Main entry to initiate the ODS data send
+  int postData(folly::EventBase* evb);
+
+ private:
+  // Internal states
+  std::shared_ptr<SensorData> pSensorData_;
+  std::string odsTier_;
+  // Internal helper functions
+  int publishToOds(
+      folly::EventBase* evb,
+      std::vector<facebook::maestro::ODSAppValue> values,
+      std::string odsTier);
+  facebook::maestro::ODSAppValue
+  getOdsAppValue(std::string key, int64_t value, uint64_t timeStampSec);
+};
