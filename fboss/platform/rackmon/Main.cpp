89cb5beb13837d1f108e24de4bf38e3afde7da7c
Jasmeet Bagga <jasmeetbagga@fb.com>
2021-11-10 10:45:07 -0800

    Set version info and enable async logging for rackmon svc
    Summary: As titled
    
    Reviewed By: tao-ren
    
    Differential Revision: D32301742
    
    fbshipit-source-id: f32375fd12f3033d92aa490347613f31feda90ac

--- fboss/platform/rackmon/Main.cpp
+++ fboss/platform/rackmon/Main.cpp
@@ -1,5 +1,27 @@
 // Copyright 2004-present Facebook. All Rights Reserved.
 
-int main(int /*argc*/, char** /*argv*/) {
+#include "common/base/BuildInfo.h"
+#include "common/init/Init.h"
+#include "common/services/cpp/BuildModule.h"
+#include "common/services/cpp/BuildValues.h"
+#include "common/services/cpp/Fb303Module.h"
+
+#include <fb303/FollyLoggingHandler.h>
+#include <folly/logging/Init.h>
+
+using namespace facebook;
+using namespace facebook::services;
+
+FOLLY_INIT_LOGGING_CONFIG("fboss=DBG2; default:async=true");
+
+int main(int argc, char** argv) {
+  // Set version info
+  gflags::SetVersionString(BuildInfo::toDebugString());
+
+  // Init FB and export build values
+  initFacebook(&argc, &argv);
+  services::BuildValues::setExportedValues();
+  fb303::registerFollyLoggingOptionHandlers();
+
   return 0;
 }
