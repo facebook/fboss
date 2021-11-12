09ecd232ccaf42d6c4e8538d5e288bdd62957416
Joseph Wu <joseph5wu@fb.com>
2021-11-11 19:30:24 -0800

    Move skip_xphy_programming to Platform
    Summary:
    Right now only BCM platforms include Pim.h, that's why when the link_test running on SAI SDK, it will miss this flag.
    To fix that, move the flag define to a broadly used class Platform.h
    
    Differential Revision: D32364863
    
    fbshipit-source-id: b4ec40fb3a329b0f09e4069edbde43ba830d3030

--- fboss/agent/Platform.cpp
+++ fboss/agent/Platform.cpp
@@ -51,6 +51,19 @@ DEFINE_string(
     "/var/facebook/fboss/qsfp_service/phy",
     "Directory for storing phy persistent state");
 
+// Eventually we will switch the whole xphy programming to qsfp_service.
+// Now this flag will be used to bypass the xphy programming when
+// qsfo_service switches the two flags;
+// - `init_pim_xphys`
+// - `use_new_state_machine`
+// to be true, and then wedge_agent will set `skip_xphy_programming` to be true.
+// Both services will use "defaultCommandLineArgs" to control the flags from
+// their own configs
+DEFINE_bool(
+    skip_xphy_programming,
+    false,
+    "Skip all xphy programming in wedge_agent");
+
 namespace facebook::fboss {
 
 Platform::Platform(
