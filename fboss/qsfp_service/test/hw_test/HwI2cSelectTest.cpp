a645a317b1ef49317279a1c652d4282774ac8f79
Sam Pham <sampham@fb.com>
2021-12-10 09:08:50 -0800

    Darwin optics/I2C QSFP I2C select test
    Summary: Add a QSFP test to verify the QSFP service properly uses its I2C controllers to access its transceivers. Currently, the test simply reads the serial numbers of all connected transceivers and verify that they are unique. If they are not, the test will fail. Note an exception is connected DACs because the transceivers from both ends will have the same serial number.
    
    Reviewed By: birdsoup
    
    Differential Revision: D32535275
    
    fbshipit-source-id: 17753be33934f7907a148cbf9b45ffc239604699

---
+++ fboss/qsfp_service/test/hw_test/HwI2cSelectTest.cpp
@@ -0,0 +1,90 @@
+// (c) Facebook, Inc. and its affiliates. Confidential and proprietary.
+
+#include "fboss/qsfp_service/test/hw_test/HwTest.h"
+
+#include "fboss/agent/AgentConfig.h"
+#include "fboss/agent/platforms/common/PlatformMapping.h"
+
+#include "fboss/qsfp_service/test/hw_test/HwPortUtils.h"
+#include "fboss/qsfp_service/test/hw_test/HwQsfpEnsemble.h"
+#include "thrift/lib/cpp/util/EnumUtils.h"
+
+#include <folly/gen/Base.h>
+#include <folly/logging/xlog.h>
+
+namespace facebook::fboss {
+
+/*
+ * This test reads the serial numbers of all connected transceivers and verify
+ * that they are unique. If they are not, the test will fail. Note an exception
+ * is connected DACs because the transceivers from both ends will have the same
+ * serial number.
+ */
+TEST_F(HwTest, i2cUniqueSerialNumbers) {
+  // Get the IDs of all connected transceivers
+  auto ensemble = getHwQsfpEnsemble();
+  WedgeManager* wedgeManager = ensemble->getWedgeManager();
+  auto agentConfig = wedgeManager->getAgentConfig();
+  auto transceivers = utility::legacyTransceiverIds(
+      utility::getCabledPortTranceivers(*agentConfig, ensemble));
+
+  // Get information of all connected transceivers
+  std::map<int32_t, TransceiverInfo> transceiverInfo;
+  wedgeManager->getTransceiversInfo(
+      transceiverInfo, std::make_unique<std::vector<int32_t>>(transceivers));
+
+  // Build a dictionary of transceiver ID, its ethernet name (eg eth1/1/1), and
+  // the ethernet name of the connected transceiver from the other end
+  std::unordered_map<int32_t, std::pair<std::string, std::string>> cabledNames;
+  auto& swConfig = *(agentConfig->thrift.sw_ref());
+
+  for (auto& port : *swConfig.ports_ref()) {
+    if (!port.expectedLLDPValues_ref()->empty()) {
+      auto portName = *port.Port::name_ref();
+      auto neighborName = port.expectedLLDPValues_ref()->at(cfg::LLDPTag::PORT);
+      auto portId = *port.logicalID_ref();
+      const int32_t id = static_cast<int32_t>(
+          *utility::getTranscieverIdx(PortID(portId), ensemble));
+      cabledNames[id] = {portName, neighborName};
+      XLOG(INFO) << fmt::format(
+          "Transceiver {:d} has name: {:s} and neighbor's name: {:s}",
+          id,
+          portName,
+          neighborName);
+    }
+  }
+
+  // Get the serial numbers of all connected transceivers and insert them into
+  // a dictionary if they are unique
+  std::unordered_map<std::string, int32_t> snIds;
+
+  for (auto tcvrId : transceivers) {
+    auto vendor = transceiverInfo[tcvrId].vendor_ref();
+    CHECK(vendor);
+    auto vendorInfo = *vendor;
+    auto sn = *(vendorInfo.serialNumber_ref());
+    XLOG(INFO) << fmt::format(
+        "Transceiver {:d} has serial number: {:s}", tcvrId, sn);
+
+    if (snIds.find(sn) == snIds.end()) {
+      snIds[sn] = tcvrId;
+    } else {
+      // Found duplicated serial numbers, module must be DAC and connected to
+      // another DAC on the other end
+      auto neighborId = snIds[sn];
+      CHECK(cabledNames.find(tcvrId) != cabledNames.end());
+      CHECK(cabledNames.find(neighborId) != cabledNames.end());
+      EXPECT_EQ(cabledNames[tcvrId].first, cabledNames[neighborId].second);
+      EXPECT_EQ(cabledNames[tcvrId].second, cabledNames[neighborId].first);
+
+      auto transmitterTech =
+          *(transceiverInfo[tcvrId].cable_ref()->transmitterTech_ref());
+      EXPECT_TRUE(TransmitterTechnology::COPPER == transmitterTech);
+
+      transmitterTech =
+          *(transceiverInfo[neighborId].cable_ref()->transmitterTech_ref());
+      EXPECT_TRUE(TransmitterTechnology::COPPER == transmitterTech);
+    }
+  }
+}
+} // namespace facebook::fboss
