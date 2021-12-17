e80c4d5dd0b1a09e6e9e00e687f2a8fe9dcd6868
Rajan Kumar <rajank@fb.com>
2021-12-17 10:11:45 -0800

    Open source utility to control Wedge400 optics modules
    Summary: This diff is to add the open source utility to enable/disable all the optics modules on Wedge400 platform. With this utility the optics modules can be made to work on wedge400 platform using open source agent and qsfp binary
    
    Differential Revision: D33054165
    
    fbshipit-source-id: 2572252dfba84f6001dcc4e029b143c06c043f95

---
+++ fboss/util/qsfp/wedge400_qsfp_util.py
@@ -0,0 +1,64 @@
+"""
+This utility provides access to QSFP modules in the Wedge400 platform. All the
+optics modules can be enabled/disabled through the controlling FPGA using this
+utility
+"""
+
+import mmap
+import subprocess
+from ctypes import c_uint32
+
+
+class FpgaDevice:
+    def __init__(self):
+        pciBusStr = subprocess.check_output(
+            "lspci -d 1d9b:7012 | sed -e 's/ .*//'", shell=True, text=True
+        )
+        pciBusList = pciBusStr.strip().split("\n")
+        if len(pciBusList) != 2:
+            print("The FPGA not detected correctly, exiting")
+            exit()
+
+        # Memory map for first FPGA
+        resourceFile1 = "/sys/bus/pci/devices/0000:" + pciBusList[0] + "/resource0"
+        print(resourceFile1)
+        self.f1 = open(resourceFile1, "rb+")
+        fpgaMap1 = mmap.mmap(
+            self.f1.fileno(),
+            0x8000,
+            flags=mmap.MAP_SHARED,
+            prot=mmap.PROT_WRITE | mmap.PROT_READ,
+            offset=0,
+        )
+        self.memStart1 = (c_uint32 * 0x2000).from_buffer(fpgaMap1)
+
+        # Memory map for second FPGA
+        resourceFile2 = "/sys/bus/pci/devices/0000:" + pciBusList[1] + "/resource0"
+        print(resourceFile2)
+        self.f2 = open(resourceFile2, "rb+")
+        fpgaMap2 = mmap.mmap(
+            self.f2.fileno(),
+            0x8000,
+            flags=mmap.MAP_SHARED,
+            prot=mmap.PROT_WRITE | mmap.PROT_READ,
+            offset=0,
+        )
+        self.memStart2 = (c_uint32 * 0x2000).from_buffer(fpgaMap2)
+
+    def __del__(self):
+        self.f1.close()
+        self.f2.close()
+
+    def read_fpga(self, fpgaId, regOffset) -> c_uint32:
+        offset = regOffset >> 2
+        if fpgaId == 0:
+            return self.memStart1[offset]
+        else:
+            return self.memStart2[offset]
+
+    def write_fpga(self, fpgaId, regOffset, regVal):
+        offset = regOffset >> 2
+        if fpgaId == 0:
+            self.memStart1[offset] = regVal
+        else:
+            self.memStart2[offset] = regVal
