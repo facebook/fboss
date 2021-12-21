7c4c1ccb945b8a16a0666e34677475362fa869b3
Ron He <zecheng@fb.com>
2021-12-21 13:59:23 -0800

    Rearrange sai replayer util method for each type
    Summary:
    Rearranging sai replayer util methods to the following category -
    1. Primitive types
    2. Various attribute types (ip, mac etc.)
    3. List types
    4. Other types (requires special case handling)
    
    This is towards supporting sai replayer refactoring & attribute id name in later diffs.
    
    Reviewed By: shri-khare
    
    Differential Revision: D33241445
    
    fbshipit-source-id: 4193a699211c650f598a36ee1cff874758b26b35

--- fboss/agent/hw/sai/tracer/Utils.h
+++ fboss/agent/hw/sai/tracer/Utils.h
@@ -19,25 +19,31 @@ namespace facebook::fboss {
 
 // Helper methods to setup attributes
 
+/* Primitive Types */
+
 // OidAttr not only serializes oid, but also look into the variable mappings
 // to check if the oid matches any previously declared variables
 std::string oidAttr(const sai_attribute_t* attr_list, int i);
 
-void oidListAttr(
-    const sai_attribute_t* attr_list,
-    int i,
-    uint32_t listIndex,
-    std::vector<std::string>& attrLines);
+std::string boolAttr(const sai_attribute_t* attr_list, int i);
+
+std::string s8Attr(const sai_attribute_t* attr_list, int i);
+
+std::string u8Attr(const sai_attribute_t* attr_list, int i);
+
+std::string u16Attr(const sai_attribute_t* attr_list, int i);
+
+std::string s32Attr(const sai_attribute_t* attr_list, int i);
+
+std::string u32Attr(const sai_attribute_t* attr_list, int i);
+
+std::string u64Attr(const sai_attribute_t* attr_list, int i);
+
+/* Various Attribute Types */
 
 void aclEntryActionSaiObjectIdAttr(
     const sai_attribute_t* attr_list,
     int i,
-    std::vector<std::string>& attrLines);
-
-void aclEntryActionSaiObjectIdListAttr(
-    const sai_attribute_t* attr_list,
-    int i,
-    uint32_t listIndex,
     std::vector<std::string>& attrLines);
 
 void aclEntryActionU8Attr(
@@ -85,55 +91,6 @@ void aclEntryFieldMacAttr(
     int i,
     std::vector<std::string>& attrLines);
 
-std::string boolAttr(const sai_attribute_t* attr_list, int i);
-
-std::string s8Attr(const sai_attribute_t* attr_list, int i);
-
-std::string u8Attr(const sai_attribute_t* attr_list, int i);
-
-std::string u16Attr(const sai_attribute_t* attr_list, int i);
-
-std::string s32Attr(const sai_attribute_t* attr_list, int i);
-
-std::string u32Attr(const sai_attribute_t* attr_list, int i);
-
-std::string u64Attr(const sai_attribute_t* attr_list, int i);
-
-void s8ListAttr(
-    const sai_attribute_t* attr_list,
-    int i,
-    uint32_t listIndex,
-    std::vector<std::string>& attrLines,
-    bool nullable = false);
-
-void s32ListAttr(
-    const sai_attribute_t* attr_list,
-    int i,
-    uint32_t listIndex,
-    std::vector<std::string>& attrLines);
-
-void u32ListAttr(
-    const sai_attribute_t* attr_list,
-    int i,
-    uint32_t listIndex,
-    std::vector<std::string>& attrLines);
-
-void s32RangeAttr(
-    const sai_attribute_t* attr_list,
-    int i,
-    std::vector<std::string>& attrLines);
-
-void u32RangeAttr(
-    const sai_attribute_t* attr_list,
-    int i,
-    std::vector<std::string>& attrLines);
-
-void qosMapListAttr(
-    const sai_attribute_t* attr_list,
-    int i,
-    uint32_t listIndex,
-    std::vector<std::string>& attrLines);
-
 void charDataAttr(
     const sai_attribute_t* attr_list,
     int i,
@@ -148,6 +105,56 @@ void ipAttr(
     const sai_attribute_t* attr_list,
     int i,
     std::vector<std::string>& attrLines);
+
+void s32RangeAttr(
+    const sai_attribute_t* attr_list,
+    int i,
+    std::vector<std::string>& attrLines);
+
+void u32RangeAttr(
+    const sai_attribute_t* attr_list,
+    int i,
+    std::vector<std::string>& attrLines);
+
+/* List Types */
+
+void oidListAttr(
+    const sai_attribute_t* attr_list,
+    int i,
+    uint32_t listIndex,
+    std::vector<std::string>& attrLines);
+
+void aclEntryActionSaiObjectIdListAttr(
+    const sai_attribute_t* attr_list,
+    int i,
+    uint32_t listIndex,
+    std::vector<std::string>& attrLines);
+
+void s32ListAttr(
+    const sai_attribute_t* attr_list,
+    int i,
+    uint32_t listIndex,
+    std::vector<std::string>& attrLines);
+
+void u32ListAttr(
+    const sai_attribute_t* attr_list,
+    int i,
+    uint32_t listIndex,
+    std::vector<std::string>& attrLines);
+
+void qosMapListAttr(
+    const sai_attribute_t* attr_list,
+    int i,
+    uint32_t listIndex,
+    std::vector<std::string>& attrLines);
+
+/* Other types */
+void s8ListAttr(
+    const sai_attribute_t* attr_list,
+    int i,
+    uint32_t listIndex,
+    std::vector<std::string>& attrLines,
+    bool nullable = false);
 
 void u8ArrGenericAttr(
     const uint8_t* array,
