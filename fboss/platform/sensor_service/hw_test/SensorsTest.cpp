6c6eafb3f3f19231e6fcb7244241fb053f4fdbe9
Jasmeet Bagga <jasmeetbagga@fb.com>
2022-01-04 11:18:56 -0800

    Sensor service HwTest scaffolding
    Summary:
    As titled. Starting with SensorServiceImpl testing. Later
    we can also pull in SensorService thrift sever under the purview of
    testing.
    
    Reviewed By: binhuang00
    
    Differential Revision: D33299792
    
    fbshipit-source-id: 88da4b9a71053b0239c4e59d0e44c398c2c071db

---
+++ fboss/platform/sensor_service/hw_test/SensorsTest.cpp
@@ -0,0 +1,23 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#include "fboss/platform/sensor_service/hw_test/SensorsTest.h"
+
+DEFINE_string(
+    config,
+    "/etc/sensor_service/darwin_sensor_config.json",
+    "Platform Sensor Configuration File Path, e.g. /etc/sensor_service/darwin_sensor_config.json");
+
+namespace facebook::fboss::platform::sensor_service {
+
+void SensorsTest::SetUp() {
+  sensorServiceImpl_ = std::make_unique<SensorServiceImpl>(FLAGS_config);
+}
+} // namespace facebook::fboss::platform::sensor_service
---
+++ fboss/platform/sensor_service/hw_test/SensorsTest.h
@@ -0,0 +1,25 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+#pragma once
+
+#include <gtest/gtest.h>
+
+#include "fboss/platform/sensor_service/SensorServiceImpl.h"
+
+namespace facebook::fboss::platform::sensor_service {
+
+class SensorsTest : public ::testing::Test {
+ public:
+  void SetUp() override;
+
+ protected:
+  std::unique_ptr<SensorServiceImpl> sensorServiceImpl_;
+};
+} // namespace facebook::fboss::platform::sensor_service
