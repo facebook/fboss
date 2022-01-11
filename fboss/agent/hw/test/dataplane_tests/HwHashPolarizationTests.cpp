14b7634d53710e258d822f9f6d7f4e017385dba3
Parvez Shaikh <pshaikh@fb.com>
2022-01-10 20:19:56 -0800

    cross platform hash polarization test for TH
    Summary: As titled
    
    Reviewed By: zechengh09
    
    Differential Revision: D32782401
    
    fbshipit-source-id: 94d9c14f851421dbde9ec1df7543dfe750f7c1ea

--- fboss/agent/hw/test/dataplane_tests/HwHashPolarizationTests.cpp
+++ fboss/agent/hw/test/dataplane_tests/HwHashPolarizationTests.cpp
@@ -297,4 +297,24 @@ TEST_F(HwHashPolarizationTestForSAITD2, With_TH) {
   runTest(HwAsic::AsicType::ASIC_TYPE_TOMAHAWK, false);
 }
 
+struct HwHashPolarizationTestForTH : HwHashPolarizationTestForAsic<
+                                         HwAsic::AsicType::ASIC_TYPE_TOMAHAWK,
+                                         false> {};
+
+TEST_F(HwHashPolarizationTestForTH, With_TH) {
+  runTest(HwAsic::AsicType::ASIC_TYPE_TOMAHAWK, false);
+}
+
+TEST_F(HwHashPolarizationTestForTH, With_SAI_TH) {
+  runTest(HwAsic::AsicType::ASIC_TYPE_TOMAHAWK, true);
+}
+
+TEST_F(HwHashPolarizationTestForTH, With_SAI_TD2) {
+  runTest(HwAsic::AsicType::ASIC_TYPE_TRIDENT2, true);
+}
+
+TEST_F(HwHashPolarizationTestForTH, With_TD2) {
+  runTest(HwAsic::AsicType::ASIC_TYPE_TRIDENT2, false);
+}
+
 } // namespace facebook::fboss
