24815cf33fcc0d3ddd49539b9f7ea08063f707fd
Peyman Gardideh <pgardideh@fb.com>
2022-01-31 15:31:31 -0800

    Use clearAllPortStats in clear interface counters
    Summary:
    Incorporate new api clearAllPortStats from D33641994 (https://github.com/facebook/fboss/commit/d5d502584397b18fc6dfd37884cd4936e42f8a62) in fboss2. Got ~20% speed gain from this. Will need to wait for an agent push before landing this.
    Flipped intf name check so user gets a signal in case of incorrect param. Also removed wild card try catch since api shouldn't return any errors and any thrift transport level exceptions should be bubbled up and handled by framework
    
    Reviewed By: jasmeetbagga
    
    Differential Revision: D33642003
    
    fbshipit-source-id: 1c49e56af3f2425452678f804dcaa2577263f187

--- fboss/cli/fboss2/commands/clear/CmdClearInterfaceCounters.h
+++ fboss/cli/fboss2/commands/clear/CmdClearInterfaceCounters.h
@@ -21,7 +21,7 @@ struct CmdClearInterfaceCountersTraits : public BaseCommandTraits {
   static constexpr utils::ObjectArgTypeId ObjectArgTypeId =
       utils::ObjectArgTypeId::OBJECT_ARG_TYPE_ID_PORT_LIST;
   using ObjectArgType = std::vector<std::string>;
-  using RetType = int32_t;
+  using RetType = std::string;
 };
 
 class CmdClearInterfaceCounters : public CmdHandler<
@@ -29,9 +29,8 @@ class CmdClearInterfaceCounters : public CmdHandler<
                                       CmdClearInterfaceCountersTraits> {
  public:
   using RetType = CmdClearInterfaceCountersTraits::RetType;
-  // I tried bool as the return type here but bool does not have a "write"
-  // method so it wouldn't compile
-  int32_t queryClient(
+
+  std::string queryClient(
       const HostInfo& hostInfo,
       std::vector<std::string> queriedIfs) {
     std::map<int32_t, facebook::fboss::PortInfoThrift> portEntries;
@@ -39,36 +38,41 @@ class CmdClearInterfaceCounters : public CmdHandler<
         utils::createClient<facebook::fboss::FbossCtrlAsyncClient>(hostInfo);
 
     std::vector<int32_t> portsToClear;
-    client->sync_getAllPortInfo(portEntries);
-
     /*
-    Populate list of ports to clear.  The function in agent thrift does not
-    accept wild cards.  This loop looks to see if the user has provided specific
-    interfaces to clear.  If so only get the portId of those specific
-    interfaces. Otherwise  populate all portIds and clear them.
+      if the user has provided specific interfaces to clear then find the
+      corresponding port names, otherwise empty vector means clear all counters
     */
-    for (auto port : portEntries) {
-      int32_t numberOfQueries = queriedIfs.size();
-      if (numberOfQueries == 0 ||
-          std::find(
-              queriedIfs.begin(), queriedIfs.end(), port.second.get_name()) !=
-              queriedIfs.end()) {
-        portsToClear.push_back(port.first);
+    if (queriedIfs.size()) {
+      client->sync_getAllPortInfo(portEntries);
+      for (auto queriedIf : queriedIfs) {
+        auto it = std::find_if(
+            portEntries.begin(),
+            portEntries.end(),
+            [&queriedIf](const auto& port) {
+              return port.second.get_name() == queriedIf;
+            });
+        if (it != portEntries.end()) {
+          portsToClear.push_back(it->first);
+        } else {
+          throw std::runtime_error(fmt::format(
+              "{} is not a valid interface name on this device", queriedIf));
+        }
       }
     }
-    try {
-      client->sync_clearPortStats(portsToClear);
-      return 1;
-    } catch (...) {
-      return 0;
-    }
+    clearCounters(*client, portsToClear);
+
+    return "Counters cleared.";
   }
 
-  void printOutput(int32_t result) {
-    if (result == 1) {
-      std::cout << "Counters cleared." << std::endl;
+  void printOutput(const RetType& logMsg) {
+    std::cout << logMsg << std::endl;
+  }
+
+  void clearCounters(FbossCtrlAsyncClient& client, std::vector<int32_t> ports) {
+    if (ports.size()) {
+      client.sync_clearPortStats(ports);
     } else {
-      std::cout << "Something went wrong.  Counters not cleared." << std::endl;
+      client.sync_clearAllPortStats();
     }
   }
 };
