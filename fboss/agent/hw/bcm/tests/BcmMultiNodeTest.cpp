f5672fd6feca39d2ea268db05c06fdd01c72397d
Midhun Somasundaran <msomasundaran@fb.com>
2022-02-10 10:11:36 -0800

    Remove pktio disable flag in multinode test
    Summary: Renable pktio in multi node tests
    
    Differential Revision: D34119503
    
    fbshipit-source-id: 73b66b26c55b05c88c48adfaf90822d5055af01b

--- fboss/agent/hw/bcm/tests/BcmMultiNodeTest.cpp
+++ fboss/agent/hw/bcm/tests/BcmMultiNodeTest.cpp
@@ -12,11 +12,6 @@
 #include "fboss/agent/test/MultiNodeTest.h"
 
 int main(int argc, char* argv[]) {
-  // temporarly set pktio to false to prevent crash in shutdown path
-  std::vector<const char*> newArgv(argv, argv + argc);
-  newArgv.push_back("--use_pktio=false");
-  newArgv.push_back("nullptr");
-  argc++;
   return facebook::fboss::mulitNodeTestMain(
-      argc, (char**)newArgv.data(), facebook::fboss::initWedgePlatform);
+      argc, argv, facebook::fboss::initWedgePlatform);
 }
