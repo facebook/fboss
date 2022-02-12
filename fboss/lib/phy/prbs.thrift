2aad7c71e5821e0f1af691d84d341a872febee20
Harshit Gulati <hgulati@fb.com>
2022-02-11 21:57:12 -0800

    Created prbs.thrift and added enum for prbs polynomials
    Summary:
    as titled. Both agent and qsfp_service will use it in subsequent diffs. Couldn't add it in phy.thrift because of a cyclic dependency. phy.thrift -> transceiver.thrift -> phy.thrift.
    
    Later, i will also move out the other prbs related structs and enums from phy.thrift to prbs.thrift
    
    Reviewed By: birdsoup
    
    Differential Revision: D34163093
    
    fbshipit-source-id: fb81f768111d75496a9d838df62d7cda85bf4eab

---
+++ fboss/lib/phy/prbs.thrift
@@ -0,0 +1,23 @@
+#
+# Copyright 2004-present Facebook. All Rights Reserved.
+#
+namespace py neteng.fboss.prbs
+namespace py3 neteng.fboss.prbs
+namespace py.asyncio neteng.fboss.asyncio.prbs
+namespace cpp2 facebook.fboss.prbs
+namespace go neteng.fboss.prbs
+namespace php fboss_prbs
+
+enum PrbsPolynomial {
+  PRBS7 = 7,
+  PRBS9 = 9,
+  PRBS10 = 10,
+  PRBS11 = 11,
+  PRBS13 = 13,
+  PRBS15 = 15,
+  PRBS20 = 20,
+  PRBS23 = 23,
+  PRBS31 = 31,
+  PRBS49 = 49,
+  PRBS58 = 58,
+}
