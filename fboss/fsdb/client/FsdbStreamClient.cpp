de7c54080c04c8e2f369a74b491d7b61f453a219
Jasmeet Bagga <jasmeetbagga@fb.com>
2022-02-26 03:13:51 -0800

    Pull up FsdbStreamClient
    Summary:
    As titled. Working towards getting these accessible from
    OSS code
    
    Differential Revision: D34408653
    
    fbshipit-source-id: 73c26652c2075d35d35527f08556d2db77892681

---
+++ fboss/fsdb/client/FsdbStreamClient.cpp
@@ -0,0 +1,117 @@
+// Copyright 2004-present Facebook. All Rights Reserved.
+
+#include "fboss/fsdb/client/FsdbStreamClient.h"
+
+#include <folly/CancellationToken.h>
+#include <folly/experimental/coro/BlockingWait.h>
+#include <folly/logging/xlog.h>
+
+DEFINE_int32(
+    fsdb_reconnect_ms,
+    1000,
+    "re-connect to fsdbm timer in milliseconds");
+
+namespace facebook::fboss::fsdb {
+FsdbStreamClient::FsdbStreamClient(
+    const std::string& clientId,
+    folly::EventBase* streamEvb,
+    folly::EventBase* connRetryEvb)
+    : folly::AsyncTimeout(connRetryEvb),
+      clientId_(clientId),
+      streamEvb_(streamEvb),
+      connRetryEvb_(connRetryEvb),
+      clientEvbThread_(
+          std::make_unique<folly::ScopedEventBaseThread>(clientId)) {
+  if (!streamEvb_ || !connRetryEvb) {
+    throw std::runtime_error(
+        "Must pass valid stream, connRetry evbs to ctor, but passed null");
+  }
+
+  cancelSource_ = std::make_unique<folly::CancellationSource>();
+  connRetryEvb->runInEventBaseThread(
+      [this] { scheduleTimeout(FLAGS_fsdb_reconnect_ms); });
+}
+
+FsdbStreamClient::~FsdbStreamClient() {
+  XLOG(DBG2) << "Destroying FsdbStreamClient";
+  cancel();
+}
+
+void FsdbStreamClient::setServerToConnect(
+    const std::string& ip,
+    uint16_t port,
+    bool allowReset) {
+  if (!allowReset && serverAddress_) {
+    throw std::runtime_error("Cannot reset server address");
+  }
+  serverAddress_ = folly::SocketAddress(ip, port);
+}
+
+void FsdbStreamClient::timeoutExpired() noexcept {
+  if (state_.load() == State::DISCONNECTED && serverAddress_) {
+    connectToServer(
+        serverAddress_->getIPAddress().str(), serverAddress_->getPort());
+  }
+  scheduleTimeout(FLAGS_fsdb_reconnect_ms);
+}
+
+bool FsdbStreamClient::isConnectedToServer() const {
+  return (state_.load() == State::CONNECTED);
+}
+
+void FsdbStreamClient::createClient(const std::string& ip, uint16_t port) {
+  clientEvbThread_->getEventBase()->runInEventBaseThreadAndWait(
+      [this] { client_.reset(); });
+  client_ = Client::getClient(ip, port, clientEvbThread_->getEventBase());
+}
+
+void FsdbStreamClient::connectToServer(const std::string& ip, uint16_t port) {
+  auto state = state_.load();
+  if (state == State::CONNECTING) {
+    XLOG(DBG2) << "Connection already in progress";
+    return;
+  }
+  CHECK(state == State::DISCONNECTED);
+  state_.store(State::CONNECTING);
+  streamEvb_->runInEventBaseThread([this, ip, port] {
+    try {
+      createClient(ip, port);
+      state_.store(State::CONNECTED);
+      folly::coro::blockingWait(serviceLoop());
+    } catch (const std::exception& ex) {
+      XLOG(ERR) << "Connect to server failed with ex:" << ex.what();
+      state_.store(State::DISCONNECTED);
+    }
+  });
+}
+
+// TODO derived classes to override this
+
+void FsdbStreamClient::cancel() {
+  XLOG(DBG2) << "Cancel FsdbStreamClient";
+  cancelSource_.withWLock([](auto& cancelSource) {
+    if (cancelSource) {
+      XLOG(DBG2) << "Request FsdbStreamClient Cancellation";
+      cancelSource->requestCancellation();
+    }
+  });
+
+  // already disconnected;
+  if (state_.load() == State::CANCELLED) {
+    XLOG(WARNING) << clientId_ << " already disconnected";
+    return;
+  }
+  serverAddress_.reset();
+  connRetryEvb_->runInEventBaseThreadAndWait([this] { cancelTimeout(); });
+  state_.store(State::CANCELLED);
+  clientEvbThread_->getEventBase()->runImmediatelyOrRunInEventBaseThreadAndWait(
+      [this]() { client_.reset(); });
+  // terminate event base getting ready for clean-up
+  clientEvbThread_->getEventBase()->terminateLoopSoon();
+}
+
+bool FsdbStreamClient::isCancelled() const {
+  return (state_.load() == State::CANCELLED);
+}
+
+} // namespace facebook::fboss::fsdb
---
+++ fboss/fsdb/client/FsdbStreamClient.h
@@ -0,0 +1,69 @@
+// Copyright 2004-present Facebook. All Rights Reserved.
+
+#pragma once
+
+#include <folly/SocketAddress.h>
+#include <folly/Synchronized.h>
+#include <folly/io/async/ScopedEventBaseThread.h>
+#include "fboss/fsdb/client/facebook/Client.h"
+
+namespace folly {
+class CancellationToken;
+}
+
+namespace facebook::fboss::fsdb {
+class FsdbStreamClient : public folly::AsyncTimeout {
+ public:
+  FsdbStreamClient(
+      const std::string& clientId,
+      folly::EventBase* streamEvb,
+      folly::EventBase* connRetryEvb);
+
+  void setServerToConnect(
+      const std::string& ip,
+      uint16_t port,
+      /* allow reset for use in tests*/
+      bool allowReset = false);
+  virtual ~FsdbStreamClient() override;
+  bool isConnectedToServer() const;
+  void cancel();
+
+  enum class State : uint16_t {
+    DISCONNECTED,
+    CONNECTING,
+    CONNECTED,
+    CANCELLED
+  };
+  bool isCancelled() const;
+  const std::string& clientId() const {
+    return clientId_;
+  }
+  State getState() const {
+    return state_.load();
+  }
+
+ private:
+  void createClient(const std::string& ip, uint16_t port);
+  void connectToServer(const std::string& ip, uint16_t port);
+
+  void timeoutExpired() noexcept override;
+
+  virtual folly::coro::Task<void> serviceLoop() = 0;
+
+ protected:
+  void setState(State state) {
+    state_.store(state);
+  }
+  folly::Synchronized<std::unique_ptr<folly::CancellationSource>> cancelSource_;
+  std::unique_ptr<FsdbServiceAsyncClient> client_;
+
+ private:
+  std::string clientId_;
+  folly::EventBase* streamEvb_;
+  folly::EventBase* connRetryEvb_;
+  std::atomic<State> state_{State::DISCONNECTED};
+  std::unique_ptr<folly::ScopedEventBaseThread> clientEvbThread_;
+  std::optional<folly::SocketAddress> serverAddress_;
+};
+
+} // namespace facebook::fboss::fsdb
