0ef0e71a077f70cf3a19194f03fe5ec8779a0391
Amithash Prasad <amithash@fb.com>
2022-03-02 12:06:33 -0800

    rackmon: Add Msg support
    Summary:
    This provides the basic modbus message object used to
    transfer and receive Modbus messages. It abstracts
    the checksum handling common to all messages.
    
    Reviewed By: jasmeetbagga
    
    Differential Revision: D34093250
    
    fbshipit-source-id: b58ecab7599a45c74e5212f3a8d8e0334f9ba834

---
+++ fboss/platform/rackmon/Msg.h
@@ -0,0 +1,207 @@
+// Copyright 2021-present Facebook. All Rights Reserved.
+#pragma once
+#include <array>
+#include <exception>
+#include <memory>
+#include <stdexcept>
+#include <vector>
+
+namespace rackmon {
+
+struct CRCError : public std::runtime_error {
+  CRCError(uint16_t exp, uint16_t got)
+      : std::runtime_error(
+            "CRC Exception: " + std::to_string(got) +
+            " != " + std::to_string(exp)) {}
+};
+
+struct Msg {
+  // Modbus defines a max size of 253
+  static constexpr size_t kMaxModbusLength = 253;
+  std::array<uint8_t, kMaxModbusLength> raw;
+  size_t len = 0;
+  uint8_t& addr = raw[0];
+
+  virtual ~Msg() {}
+
+  void clear() {
+    len = 0;
+  }
+  // Push to the end
+  Msg& operator<<(uint8_t d);
+  Msg& operator<<(uint16_t d);
+  Msg& operator<<(uint32_t d);
+  template <typename T>
+  Msg& operator<<(std::vector<T>& d) {
+    for (T v : d) {
+      *this << v;
+    }
+    return *this;
+  }
+
+  // Pop from the end.
+  Msg& operator>>(uint8_t& d);
+  Msg& operator>>(uint16_t& d);
+  Msg& operator>>(uint32_t& d);
+  template <typename T>
+  Msg& operator>>(std::vector<T>& d) {
+    for (auto it = d.rbegin(); it != d.rend(); it++) {
+      *this >> *it;
+    }
+    return *this;
+  }
+
+  constexpr auto begin() noexcept {
+    return raw.begin();
+  }
+  auto end() noexcept {
+    return raw.begin() + len;
+  }
+  constexpr auto begin() const noexcept {
+    return raw.cbegin();
+  }
+  auto end() const noexcept {
+    return raw.cbegin() + len;
+  }
+  Msg() {}
+  Msg(const Msg& other) {
+    len = other.len;
+    std::copy(other.begin(), other.end(), begin());
+  }
+  template <size_t size>
+  explicit Msg(const std::array<uint8_t, size>& other) {
+    if (size > raw.size()) {
+      throw std::overflow_error("array too big");
+    }
+    len = size;
+    std::copy(other.begin(), other.end(), raw.begin());
+  }
+  Msg& operator=(const Msg& other) {
+    len = other.len;
+    std::copy(other.begin(), other.end(), begin());
+    return *this;
+  }
+  bool operator==(const Msg& other) const {
+    if (len != other.len) {
+      return false;
+    }
+    return std::equal(begin(), end(), other.raw.begin());
+  }
+  bool operator!=(const Msg& other) const {
+    return !(*this == other);
+  }
+
+ protected:
+  void finalize();
+  void validate();
+
+  // Truely private. Used by finalize() and validate().
+  uint16_t crc16();
+
+  // Modbus will call encode before transmitting and
+  // decode right after receiving a response. Marking
+  // this private and having Modbus as a friend to
+  // avoid someone accidentally calling encode() or
+  // decode() from the upper layers which can cause
+  // checksums etc to be recomputed thus destroying
+  // the message.
+  virtual void encode() {
+    finalize();
+  }
+  virtual void decode() {
+    validate();
+  }
+  friend class Modbus;
+  friend class Encoder;
+};
+
+// This template eventually allows us to use constant expressions
+// for Msg, example: `Msg msg = 0x12ab_M;`
+// We use this as a intermediate form of the constant expression
+// to convert the chars into a byte array.
+template <char... cs>
+struct ConstMsg {
+  template <typename In, typename Out>
+  static constexpr auto parse_hex(In begin, In end, Out out) {
+    auto hexByte = [](char u, char l) constexpr {
+      auto hexNibble = [](char b) constexpr {
+        if (b >= '0' && b <= '9') {
+          return b - '0';
+        }
+        if (b >= 'A' && b <= 'F') {
+          return 10 + (b - 'A');
+        }
+        if (b >= 'a' && b <= 'f') {
+          return 10 + (b - 'a');
+        }
+        return 0;
+      };
+      return hexNibble(u) << 4 | hexNibble(l);
+    };
+    if (end - begin <= 2) {
+      throw std::underflow_error("Literal too short");
+    }
+    if (begin[0] != '0' || begin[1] != 'x') {
+      throw std::invalid_argument("literal does not start with 0x");
+    }
+    if ((end - begin) % 2 != 0) {
+      throw std::invalid_argument("literal has odd length");
+    }
+    begin += 2;
+    while (begin != end) {
+      *out = hexByte(*begin, *(begin + 1));
+      begin += 2;
+      ++out;
+    }
+    return out;
+  }
+  static constexpr auto to_array() {
+    constexpr std::array<char, sizeof...(cs)> data{cs...};
+    std::array<std::uint8_t, (sizeof...(cs) / 2 - 1)> result{};
+    parse_hex(data.begin(), data.end(), result.begin());
+    return result;
+  }
+  constexpr operator std::array<std::uint8_t, (sizeof...(cs) / 2)>() const {
+    return to_array();
+  }
+  operator Msg() const {
+    constexpr auto tmp = to_array();
+    return Msg(tmp);
+  }
+};
+
+// User defined constant expression. Use ConstMsg as an intermediate
+// store to hold the bytes and then convert to Msg.
+template <char... cs>
+auto operator"" _M() {
+  static_assert(sizeof...(cs) % 2 == 0, "Must be an even number of chars");
+  Msg msg = ConstMsg<cs...>{};
+  return msg;
+}
+
+#ifdef __TEST__
+class Encoder {
+ public:
+  Encoder() {}
+  static void encode(Msg& msg) {
+    msg.encode();
+  }
+  static void decode(Msg& msg) {
+    msg.decode();
+  }
+  static void finalize(Msg& msg) {
+    msg.finalize();
+  }
+};
+
+// User defined constant expression. Use ConstMsg as an intermediate
+// store to hold the bytes and then convert to Msg.
+template <char... cs>
+auto operator"" _EM() {
+  Msg msg = ConstMsg<cs...>{};
+  Encoder::encode(msg);
+  return msg;
+}
+#endif
+
+} // namespace rackmon
---
+++ fboss/platform/rackmon/tests/MsgTest.cpp
@@ -0,0 +1,222 @@
+// Copyright 2021-present Facebook. All Rights Reserved.
+#include "Msg.h"
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+using namespace std;
+using namespace testing;
+using namespace rackmon;
+
+TEST(Msg, SaneInits) {
+  Msg msg;
+  EXPECT_EQ(msg.len, 0);
+  // This is defined in the protocol.
+  // Do not let anyone change this!
+  EXPECT_EQ(msg.raw.size(), 253);
+  EXPECT_EQ(msg.begin(), msg.end());
+}
+
+TEST(Msg, TestMsgStream) {
+  Msg msg;
+  uint8_t v8 = 0xf0;
+  uint16_t v16 = 0x1234;
+  msg << v8;
+  EXPECT_EQ(msg.len, 1);
+  EXPECT_EQ(msg.raw[0], v8);
+  EXPECT_EQ(msg.addr, v8);
+  EXPECT_NE(msg.begin(), msg.end());
+  EXPECT_EQ(*msg.begin(), v8);
+  msg << v16;
+  EXPECT_EQ(msg.len, 3);
+  EXPECT_EQ(msg.raw[0], v8);
+  EXPECT_EQ(msg.raw[1], 0x12);
+  EXPECT_EQ(msg.raw[2], 0x34);
+  EXPECT_EQ(msg.addr, v8);
+  EXPECT_NE(msg.begin(), msg.end());
+  EXPECT_EQ(*(msg.begin() + 0), 0xf0);
+  EXPECT_EQ(*(msg.begin() + 1), 0x12);
+  EXPECT_EQ(*(msg.begin() + 2), 0x34);
+  uint16_t o16;
+  msg >> o16;
+  EXPECT_EQ(o16, v16);
+  EXPECT_EQ(msg.len, 1);
+  uint8_t o8;
+  msg >> o8;
+  EXPECT_EQ(o8, v8);
+  EXPECT_EQ(msg.len, 0);
+  msg << v8;
+  msg.clear();
+  EXPECT_EQ(msg.len, 0);
+  uint32_t v32 = 0x12345678;
+  msg << v32;
+  EXPECT_EQ(msg.len, 4);
+  EXPECT_EQ(msg.raw[0], 0x12);
+  EXPECT_EQ(msg.raw[1], 0x34);
+  EXPECT_EQ(msg.raw[2], 0x56);
+  EXPECT_EQ(msg.raw[3], 0x78);
+  uint32_t e32;
+  msg >> e32;
+  EXPECT_EQ(v32, e32);
+  EXPECT_EQ(msg.len, 0);
+}
+
+TEST(Msg, TestStreamVectors) {
+  Msg msg;
+  std::vector<uint8_t> v8{1, 2, 3};
+  msg << v8;
+  EXPECT_EQ(msg.len, 3);
+  EXPECT_EQ(msg.raw[0], 0x1);
+  EXPECT_EQ(msg.raw[1], 0x2);
+  EXPECT_EQ(msg.raw[2], 0x3);
+  std::fill(v8.begin(), v8.end(), 0);
+  msg >> v8;
+  EXPECT_EQ(v8[0], 1);
+  EXPECT_EQ(v8[1], 2);
+  EXPECT_EQ(v8[2], 3);
+  msg.clear();
+
+  std::vector<uint16_t> v16{0x1122, 0x3344};
+  msg << v16;
+  EXPECT_EQ(msg.len, 4);
+  EXPECT_EQ(msg.raw[0], 0x11);
+  EXPECT_EQ(msg.raw[1], 0x22);
+  EXPECT_EQ(msg.raw[2], 0x33);
+  EXPECT_EQ(msg.raw[3], 0x44);
+  std::fill(v16.begin(), v16.end(), 0);
+  msg >> v16;
+  EXPECT_EQ(v16[0], 0x1122);
+  EXPECT_EQ(v16[1], 0x3344);
+  msg.clear();
+
+  std::vector<uint32_t> v32{0x11223344};
+  msg << v32;
+  EXPECT_EQ(msg.len, 4);
+  EXPECT_EQ(msg.raw[0], 0x11);
+  EXPECT_EQ(msg.raw[1], 0x22);
+  EXPECT_EQ(msg.raw[2], 0x33);
+  EXPECT_EQ(msg.raw[3], 0x44);
+  std::fill(v32.begin(), v32.end(), 0);
+  msg >> v32;
+  EXPECT_EQ(v32[0], 0x11223344);
+}
+
+TEST(Msg, TestUnderflow) {
+  Msg msg;
+  uint8_t v8 = 0x1;
+  uint16_t v16;
+  // Underflow trying to get anything out of an empty msg
+  EXPECT_THROW(msg >> v8, std::underflow_error);
+  // Undeflow when trying to get 2 bytes on something with 1.
+  msg << v8++;
+  EXPECT_THROW(msg >> v16, std::underflow_error);
+  msg << v8;
+  msg >> v16;
+  EXPECT_EQ(v16, 0x0102);
+}
+
+TEST(Msg, TestOverflow) {
+  Msg msg;
+  // Almost fill it out with just 1 byte to spare.
+  // all this should work.
+  for (size_t i = 0; i < 252; i++)
+    msg << uint8_t(0xff);
+  // Try to push a 16bit value, it should throw but
+  // not change the state of msg.
+  EXPECT_THROW(msg << uint16_t(0xffff), std::overflow_error);
+  // push the last byte, should succeed.
+  msg << uint8_t(0xff);
+  // Anymore pushes should fail.
+  EXPECT_THROW(msg << uint8_t(0xff), std::overflow_error);
+}
+
+class TestMsg : public Msg {
+ public:
+  void wrap_finalize() {
+    finalize();
+  }
+  void wrap_encode() {
+    encode();
+  }
+  void wrap_validate() {
+    validate();
+  }
+  void wrap_decode() {
+    decode();
+  }
+};
+
+TEST(Msg, TestCRC) {
+  TestMsg msg;
+  msg << uint8_t(1);
+  msg << uint8_t(2);
+  msg << uint8_t(3);
+  EXPECT_EQ(msg.len, 3);
+  msg.wrap_finalize();
+  EXPECT_EQ(msg.len, 5);
+  // Pop CRC and inspect.
+  uint16_t crc;
+  msg >> crc;
+  EXPECT_EQ(msg.len, 3);
+  EXPECT_EQ(crc, 0x6161);
+  // check validate does not throw
+  msg.wrap_encode();
+  msg.wrap_validate();
+  EXPECT_EQ(msg.len, 3);
+  // Reset len to include CRC and check decode() does not throw.
+  msg.len += 2;
+  msg.wrap_decode();
+  EXPECT_EQ(msg.len, 3);
+
+  // Reset len and corrupt one byte, check both validate and
+  // decode throws.
+  msg.len += 2;
+  msg.raw[0] = 4;
+  EXPECT_THROW(msg.wrap_validate(), CRCError);
+  EXPECT_THROW(msg.wrap_decode(), CRCError);
+}
+
+TEST(Msg, ConstantLiteral) {
+  Msg msg = 0x123456789abcdef012_M;
+  EXPECT_EQ(msg.len, 9);
+  EXPECT_EQ(msg.raw[0], 0x12);
+  EXPECT_EQ(msg.raw[1], 0x34);
+  EXPECT_EQ(msg.raw[2], 0x56);
+  EXPECT_EQ(msg.raw[3], 0x78);
+  EXPECT_EQ(msg.raw[4], 0x9a);
+  EXPECT_EQ(msg.raw[5], 0xbc);
+  EXPECT_EQ(msg.raw[6], 0xde);
+  EXPECT_EQ(msg.raw[7], 0xf0);
+  EXPECT_EQ(msg.raw[8], 0x12);
+}
+
+TEST(Msg, constIter) {
+  const Msg msg = 0x1234_M;
+  auto it = msg.begin();
+  EXPECT_EQ(*it, 0x12);
+  it++;
+  EXPECT_EQ(*it, 0x34);
+  it++;
+  EXPECT_EQ(it, msg.end());
+}
+
+TEST(Msg, EqualCompare) {
+  Msg msg1 = 0x12345678_M;
+  Msg msg2 = 0x123456_M;
+  EXPECT_NE(msg1, msg2);
+  msg2 << uint8_t(0x78);
+  EXPECT_EQ(msg1, msg2);
+}
+
+TEST(Msg, CopyConstructor) {
+  Msg msg1 = 0x12345678_M;
+  Msg msg2(msg1);
+  EXPECT_EQ(msg1, msg2);
+}
+
+TEST(Msg, AssignmentOperator) {
+  Msg msg1 = 0x12345678_M;
+  Msg msg2 = 0x1234_M;
+  EXPECT_NE(msg1, msg2);
+  msg2 = msg1;
+  EXPECT_EQ(msg1, msg2);
+}
