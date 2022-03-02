1e718ebc4c247937543cbcb1c94d11c9064cd0e2
Joseph Wu <joseph5wu@fb.com>
2022-03-01 23:56:39 -0800

    Move xphy init benchmark test to qsfp
    Summary:
    Move the previous agent platform xphy init benchmark tests to qsfp_service directory since we now rely on qsfp to initialize xphy.
    Also get rid of the platform dependency as we should just use the same WedgeManager pointer to call xphy initialization, like what qsfp_service does.
    
    Reviewed By: jasmeetbagga
    
    Differential Revision: D34571916
    
    fbshipit-source-id: 5a44e7507bf43d7fb20b0c9fb901e14c43e30aa6

---
+++ fboss/qsfp_service/test/benchmarks/PhyInitBenchmark.cpp
@@ -0,0 +1,60 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+#include <folly/Benchmark.h>
+
+#include "fboss/lib/CommonFileUtils.h"
+#include "fboss/qsfp_service/platforms/wedge/WedgeManager.h"
+#include "fboss/qsfp_service/platforms/wedge/WedgeManagerInit.h"
+
+namespace facebook::fboss {
+
+std::unique_ptr<WedgeManager> setupForColdboot() {
+  // Once we setup for cold boot, WedgeManager will always reload xphy firmware
+  // when initExternalPhyMap() is called
+  createDir(FLAGS_qsfp_service_volatile_dir);
+  auto fd = createFile(TransceiverManager::forceColdBootFileName());
+  close(fd);
+
+  // Set initializing pim xphys to 1 so we always initialize xphy chips
+  gflags::SetCommandLineOptionWithMode(
+      "init_pim_xphys", "1", gflags::SET_FLAGS_DEFAULT);
+  gflags::SetCommandLineOptionWithMode(
+      "force_reload_gearbox_fw", "1", gflags::SET_FLAGS_DEFAULT);
+
+  return createWedgeManager();
+}
+
+std::unique_ptr<WedgeManager> setupForWarmboot() {
+  // Use cold boot to force download xphy
+  auto wedgeMgr = setupForColdboot();
+  wedgeMgr->initExternalPhyMap();
+  // Use gracefulExit so the next initExternalPhyMap() will be using warm boot
+  wedgeMgr->gracefulExit();
+
+  return createWedgeManager();
+}
+
+BENCHMARK(PhyInitAllColdBoot) {
+  folly::BenchmarkSuspender suspender;
+  auto wedgeMgr = setupForColdboot();
+  suspender.dismiss();
+
+  wedgeMgr->initExternalPhyMap();
+}
+
+BENCHMARK(PhyInitAllWarmBoot) {
+  folly::BenchmarkSuspender suspender;
+  auto wedgeMgr = setupForWarmboot();
+  suspender.dismiss();
+
+  wedgeMgr->initExternalPhyMap();
+}
+
+} // namespace facebook::fboss
