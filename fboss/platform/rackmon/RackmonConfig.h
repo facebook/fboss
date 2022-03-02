fda12c1d7d53b3ebb7cb515b788b383536d32f0d
Amithash Prasad <amithash@fb.com>
2022-03-02 12:06:33 -0800

    rackmon: Add static configuration
    Summary:
    This reads in the *.json configuration files (Used in openBMC)
    and generates a cpp file which contains the JSON as a rawstring.
    
    Reviewed By: tao-ren
    
    Differential Revision: D34096956
    
    fbshipit-source-id: ec69f7c76d74902494373b8aae5cd5fb9cf2445d

---
+++ fboss/platform/rackmon/RackmonConfig.h
@@ -0,0 +1,8 @@
+// Copyright 2021-present Facebook. All Rights Reserved.
+#include <string>
+#include <vector>
+
+namespace rackmonsvc {
+const std::string getInterfaceConfig();
+const std::vector<std::string> getRegisterMapConfig();
+} // namespace rackmonsvc
---
+++ fboss/platform/rackmon/tests/RackmonConfigTest.cpp
@@ -0,0 +1,25 @@
+#include "RackmonConfig.h"
+#include <gtest/gtest.h>
+#include <nlohmann/json.hpp>
+
+using namespace rackmonsvc;
+using nlohmann::json;
+
+TEST(RackmonConfig, Interface) {
+  json ifaceConfig = json::parse(getInterfaceConfig());
+  ASSERT_TRUE(ifaceConfig.contains("interfaces"));
+  ASSERT_EQ(ifaceConfig["interfaces"].size(), 3);
+  EXPECT_EQ(ifaceConfig["interfaces"][0]["device_path"], "/dev/ttyUSB1");
+  EXPECT_EQ(ifaceConfig["interfaces"][1]["device_path"], "/dev/ttyUSB2");
+  EXPECT_EQ(ifaceConfig["interfaces"][2]["device_path"], "/dev/ttyUSB3");
+}
+
+TEST(RackmonConfig, RegisterMap) {
+  const std::vector<std::string> mapsConfig = getRegisterMapConfig();
+  ASSERT_EQ(mapsConfig.size(), 1);
+  json maps = json::parse(mapsConfig.at(0));
+  ASSERT_EQ(maps["name"], "orv2_psu");
+  ASSERT_EQ(maps["probe_register"], 104);
+  ASSERT_GE(maps["registers"].size(), 30);
+  ASSERT_EQ(maps["registers"][0]["name"], "PSU MFR MODEL");
+}
