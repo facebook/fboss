ab18896b204f641651734dfed617ff138fbbda6e
Jasmeet Bagga <jasmeetbagga@fb.com>
2022-03-03 03:05:05 -0800

    Fill in code to add delta subscriptions via FsdbPubSubManager
    Summary: As titled
    
    Differential Revision: D34530127
    
    fbshipit-source-id: 8abd9f65f819a649744f8ffc221a87561b343607

--- fboss/fsdb/client/FsdbPubSubManager.cpp
+++ fboss/fsdb/client/FsdbPubSubManager.cpp
@@ -2,9 +2,19 @@
 
 #include "fboss/fsdb/client/FsdbPubSubManager.h"
 
+#include <folly/String.h>
+#include <folly/logging/xlog.h>
 #include <string>
 #include "fboss/fsdb/client/FsdbDeltaPublisher.h"
 
+namespace {
+
+std::string toPathStr(
+    const std::string& fsdbHost,
+    const std::vector<std::string>& path) {
+  return fsdbHost + ":/" + folly::join('/', path.begin(), path.end());
+}
+} // namespace
 namespace facebook::fboss::fsdb {
 
 FsdbPubSubManager::FsdbPubSubManager(const std::string& clientId)
@@ -44,5 +54,40 @@ void FsdbPubSubManager::publish(const OperDelta& pubUnit) {
     CHECK(deltaPublisher);
     deltaPublisher->write(pubUnit);
   });
+}
+
+void FsdbPubSubManager::addSubscription(
+    const std::vector<std::string>& subscribePath,
+    FsdbStreamClient::FsdbStreamStateChangeCb stateChangeCb,
+    FsdbDeltaSubscriber::FsdbOperDeltaUpdateCb operDeltaCb,
+    const std::string& fsdbHost,
+    int32_t fsdbPort) {
+  auto pathStr = toPathStr(fsdbHost, subscribePath);
+  path2Subscriber_.withWLock([&](auto& path2Subscriber) {
+    auto [itr, inserted] = path2Subscriber.emplace(std::make_pair(
+        pathStr,
+        std::make_unique<FsdbDeltaSubscriber>(
+            clientId_,
+            subscribePath,
+            subscribersStreamEvbThread_.getEventBase(),
+            reconnectThread_.getEventBase(),
+            operDeltaCb,
+            stateChangeCb)));
+    if (!inserted) {
+      throw std::runtime_error(
+          "Subscription at : " + pathStr + " already exists");
+    }
+    XLOG(DBG2) << " Added subscription for: " << pathStr;
+    itr->second->setServerToConnect(fsdbHost, fsdbPort);
+  });
+}
+
+void FsdbPubSubManager::removeSubscription(
+    const std::vector<std::string>& subscribePath,
+    const std::string& fsdbHost) {
+  auto pathStr = toPathStr(fsdbHost, subscribePath);
+  if (path2Subscriber_.wlock()->erase(pathStr)) {
+    XLOG(DBG2) << "Erased subscription for : " << pathStr;
+  }
 }
 } // namespace facebook::fboss::fsdb
--- fboss/fsdb/client/FsdbPubSubManager.h
+++ fboss/fsdb/client/FsdbPubSubManager.h
@@ -4,6 +4,7 @@
 #include <folly/Synchronized.h>
 #include <folly/io/async/ScopedEventBaseThread.h>
 #include "fboss/fsdb/Flags.h"
+#include "fboss/fsdb/client/FsdbDeltaSubscriber.h"
 #include "fboss/fsdb/client/FsdbStreamClient.h"
 #include "fboss/fsdb/if/gen-cpp2/fsdb_oper_types.h"
 
@@ -22,13 +23,28 @@ class FsdbPubSubManager {
       FsdbStreamClient::FsdbStreamStateChangeCb publisherStateChangeCb,
       int32_t fsdbPort = FLAGS_fsdbPort);
   void publish(const OperDelta& pubUnit);
+  void addSubscription(
+      const std::vector<std::string>& subscribePath,
+      FsdbStreamClient::FsdbStreamStateChangeCb stateChangeCb,
+      FsdbDeltaSubscriber::FsdbOperDeltaUpdateCb operDeltaCb,
+      const std::string& fsdbHost = "::1",
+      int32_t fsdbPort = FLAGS_fsdbPort);
+
+  void removeSubscription(
+      const std::vector<std::string>& subscribePath,
+      const std::string& fsdbHost = "::1");
 
  private:
   void stopDeltaPublisher(std::unique_ptr<FsdbDeltaPublisher>& deltaPublisher);
+
   folly::ScopedEventBaseThread reconnectThread_;
   folly::ScopedEventBaseThread publisherStreamEvbThread_;
+  folly::ScopedEventBaseThread subscribersStreamEvbThread_;
   const std::string clientId_;
   // TODO - support multiple publishers?
   folly::Synchronized<std::unique_ptr<FsdbDeltaPublisher>> deltaPublisher_;
+  folly::Synchronized<
+      std::unordered_map<std::string, std::unique_ptr<FsdbDeltaSubscriber>>>
+      path2Subscriber_;
 };
 } // namespace facebook::fboss::fsdb
