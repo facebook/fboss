e99db36bbebf389b8b399d9cf5a4a5f8f70d4ea8
Siva Muthusamy <simuthus@fb.com>
2022-03-06 14:29:55 -0800

    Fix OSS build
    Summary: Fix OSS build
    
    Differential Revision: D34635709
    
    fbshipit-source-id: 3303bf2dfa10ea3fe0ee68904f3d5fc340d74f8c

--- cmake/AgentHwTest.cmake
+++ cmake/AgentHwTest.cmake
@@ -273,6 +273,7 @@ add_library(hw_switch_test
   fboss/agent/hw/test/dataplane_tests/HwTestQueuePerHostUtils.cpp
   fboss/agent/hw/test/dataplane_tests/HwTestPfcUtils.cpp
   fboss/agent/hw/test/dataplane_tests/HwTestUtils.cpp
+  fboss/agent/hw/test/dataplane_tests/HwTestWatermarkUtils.cpp
   fboss/agent/hw/test/dataplane_tests/HwTrunkLoadBalancerTests.cpp
   fboss/agent/hw/test/dataplane_tests/HwWatermarkTests.cpp
   fboss/agent/hw/test/dataplane_tests/HwRouteStatTests.cpp
