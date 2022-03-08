38ab0c7e311116efa37731cddd808754ad8cf3bf
Harshit Gulati <hgulati@fb.com>
2022-03-07 21:54:24 -0800

    Move common FieldInfo code into a base class
    Summary:
    We currently have a SffFieldInfo, Sff8472FieldInfo, CmisFieldInfo, SffFr1FieldInfo, which pretty much have the same implementation to return the page, offset, length of a field.
    
    This diff creates a template base class for this implementation, the next 3 diffs will change the existing *FieldInfos to use this base class
    
    Differential Revision: D34676577
    
    fbshipit-source-id: 21df35f751f8bfc36ee0bb7eecb487eea5675175

---
+++ fboss/qsfp_service/module/QsfpFieldInfo.h
@@ -0,0 +1,50 @@
+// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+#pragma once
+
+#include <unordered_map>
+#include "fboss/agent/FbossError.h"
+#include "fboss/lib/usb/TransceiverI2CApi.h"
+
+namespace facebook {
+namespace fboss {
+
+template <typename QsfpField, typename Pages>
+class QsfpFieldInfo {
+ public:
+  uint8_t transceiverI2CAddress;
+  int dataAddress; // TODO: rename to page
+  int offset;
+  int length;
+
+  QsfpFieldInfo(
+      uint8_t transceiverI2CAddress,
+      Pages page,
+      int offset,
+      int length)
+      : transceiverI2CAddress(transceiverI2CAddress),
+        dataAddress(static_cast<int>(page)),
+        offset(offset),
+        length(length) {}
+
+  QsfpFieldInfo(Pages page, int offset, int length)
+      : transceiverI2CAddress(TransceiverI2CApi::ADDR_QSFP),
+        dataAddress(static_cast<int>(page)),
+        offset(offset),
+        length(length) {}
+
+  typedef std::unordered_map<QsfpField, QsfpFieldInfo> QsfpFieldMap;
+
+  static QsfpFieldInfo getQsfpFieldAddress(
+      const QsfpFieldMap& map,
+      QsfpField field) {
+    auto info = map.find(field);
+    if (info == map.end()) {
+      throw FbossError("Invalid Field ID");
+    }
+    return info->second;
+  }
+};
+
+} // namespace fboss
+} // namespace facebook
