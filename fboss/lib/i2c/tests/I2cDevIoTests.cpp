bfb041cbfff0996a8639a3cc39d0969b493d5078
Sam Pham <sampham@fb.com>
2022-04-14 11:24:07 -0700

    Create the I2cDevIoTests and LassenQsfpI2cTests classes to contain unit tests for the I2cDevIo and LassenQsfpI2c classes
    Summary:
    The I2cDevIoTests and LassenQsfpI2cTests classes verify that the I2cDevIo and LassenQsfpI2c classes can handle errors properly (invalid I2C device files in this case because the unit tests will run on a devserver and not an actual Lassen switch). Read and write statistics are also tested for the LassenQsfpI2c class.
    
    The MockI2cDevIo class will be created in the next diff to test valid read and write in the LassenQsfpI2c class.
    
    Differential Revision: D35094347
    
    fbshipit-source-id: c1b5b6937a33bbbf71b339ed372b0949b91116b7

---
+++ fboss/lib/i2c/tests/I2cDevIoTests.cpp
@@ -0,0 +1,68 @@
+// Copyright 2004-present Facebook. All Rights Reserved.
+
+#include <folly/FileUtil.h>
+#include <folly/experimental/TestUtil.h>
+#include <folly/logging/xlog.h>
+#include <gtest/gtest.h>
+#include "fboss/lib/i2c/I2cDevIo.h"
+
+namespace facebook::fboss {
+
+class I2cDevIoTests : public ::testing::Test {
+ public:
+  /* Verify the ctor I2cDevIo() throws I2cDevIoError exception for non-existent
+   * file
+   */
+  void testInvalidFile() {
+    EXPECT_THROW(
+        std::unique_ptr<I2cDevIo> i2cDev =
+            std::make_unique<I2cDevIo>("/invalidFile/tmp"),
+        I2cDevIoError);
+  }
+
+  /* Verify the I2cDevIo::read() method throws I2cDevIoError exception if it
+   * cannot read from a regular non-i2cDev file
+   */
+  void testInvalidRead() {
+    std::unique_ptr<I2cDevIo> i2cDev = std::make_unique<I2cDevIo>(filePath_);
+    uint8_t addr = 0x50;
+    uint8_t offset = 127;
+    uint8_t buf;
+    EXPECT_THROW(i2cDev->read(addr, offset, &buf, 1), I2cDevIoError);
+  }
+
+  /* Verify the I2cDevIo::write() method throws I2cDevIoError exception if it
+   * cannot write to a regular non-i2cDev file
+   */
+  void testInvalidWrite() {
+    std::unique_ptr<I2cDevIo> i2cDev = std::make_unique<I2cDevIo>(filePath_);
+    uint8_t addr = 0x50;
+    uint8_t offset = 127;
+    const uint8_t buf = 1;
+    EXPECT_THROW(i2cDev->write(addr, offset, &buf, 1), I2cDevIoError);
+  }
+
+ protected:
+  void SetUp() override {
+    std::string content = "123456789";
+    folly::writeFile(content, filePath_.c_str());
+  }
+
+ private:
+  folly::test::TemporaryDirectory tmpDir_;
+  std::string filePath_ = tmpDir_.path().string() + "/invalidI2cDev";
+};
+
+TEST_F(I2cDevIoTests, TestInvalidFile) {
+  testInvalidFile();
+}
+
+TEST_F(I2cDevIoTests, TestInvalidRead) {
+  testInvalidRead();
+}
+
+TEST_F(I2cDevIoTests, TestInvalidWrite) {
+  testInvalidWrite();
+}
+
+} // namespace facebook::fboss
