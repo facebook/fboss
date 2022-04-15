fb093547275bd7421ace3cb28bb2edad0ad5aef6
Nivin Lawrence <nivinl@fb.com>
2022-04-15 12:16:56 -0700

    Introduce base Bitsflow ACL test class
    Summary:
    Introduce base test class for Bitsflow ACLs, need to have agent
    started with config and also a thrift server so as to validate thrif
    connections to wedge_agent with ACLs.
    
    Reviewed By: jasmeetbagga
    
    Differential Revision: D35516782
    
    fbshipit-source-id: f0aee2dd093101fe687850cd8dfebab64acc2ef6

---
+++ fboss/agent/test/BitsflowAclTest.cpp
@@ -0,0 +1,63 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+#include "fboss/agent/ApplyThriftConfig.h"
+#include "fboss/agent/SetupThrift.h"
+#include "fboss/agent/SwSwitch.h"
+#include "fboss/agent/ThriftHandler.h"
+#include "fboss/agent/test/HwTestHandle.h"
+#include "fboss/agent/test/TestUtils.h"
+
+#include <gtest/gtest.h>
+
+using namespace facebook::fboss;
+
+class BitsflowAclTest : public ::testing::Test {
+ public:
+  void initialSetup() {
+    auto config = testConfigA();
+    handle_ = createTestHandle(&config);
+    sw_ = handle_->getSw();
+    sw_->initialConfigApplied(std::chrono::steady_clock::now());
+    createThriftThread();
+  }
+
+  void TearDown() override {
+    if (thriftServer_) {
+      thriftServer_->stop();
+    }
+    if (thriftThread_.has_value()) {
+      (*thriftThread_).join();
+    }
+    if (thriftServer_) {
+      thriftServer_.reset();
+    }
+  }
+
+ private:
+  void createThriftThread() {
+    uint16_t anyPort = 0;
+    auto eventBase = new folly::EventBase();
+    auto handler = std::make_shared<ThriftHandler>(sw_);
+    folly::EventBaseManager::get()->clearEventBase();
+    thriftServer_ = setupThriftServer(
+        *eventBase, handler, anyPort, false /* isDuplex */, true /* setupSSL*/);
+    thriftThread_ = std::thread([&]() noexcept {
+      XLOG(DBG0) << "Starting thrift server thread ...";
+      folly::setThreadName("agent-ThriftServer");
+      thriftServer_->serve();
+      XLOG(DBG0) << "Stopping thrift server thread ...";
+    });
+  }
+
+  SwSwitch* sw_;
+  std::optional<std::thread> thriftThread_;
+  std::unique_ptr<HwTestHandle> handle_{nullptr};
+  std::shared_ptr<apache::thrift::ThriftServer> thriftServer_{nullptr};
+};
