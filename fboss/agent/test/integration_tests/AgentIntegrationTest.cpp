fc23c200970b940b37f28e6d916531e677c01438
Midhun Somasundaran <msomasundaran@fb.com>
2022-05-10 16:19:35 -0700

    Initial routing and agent integration test
    Summary: Adding an initial version of agent-routing integration test. The agent test binary brings up the ports on a standalone switch and verifies that BGP sessions are established in the next stacked diffs. More tests to be added
    
    Reviewed By: jasmeetbagga, xiangxu1121
    
    Differential Revision: D35739212
    
    fbshipit-source-id: 113565810820e2fa49d38575ff97e261677daa7b

---
+++ fboss/agent/test/integration_tests/AgentIntegrationTest.cpp
@@ -0,0 +1,53 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#include <gflags/gflags.h>
+
+#include "fboss/agent/AgentConfig.h"
+#include "fboss/agent/hw/test/ConfigFactory.h"
+#include "fboss/agent/hw/test/HwTestPacketUtils.h"
+#include "fboss/agent/hw/test/LoadBalancerUtils.h"
+#include "fboss/agent/platforms/wedge/WedgePlatformInit.h"
+#include "fboss/agent/state/SwitchState.h"
+#include "fboss/agent/test/EcmpSetupHelper.h"
+#include "fboss/agent/test/integration_tests/AgentIntegrationTest.h"
+#include "fboss/lib/config/PlatformConfigUtils.h"
+
+namespace facebook::fboss {
+
+cfg::SwitchConfig AgentIntegrationTest::initialConfig() const {
+  cfg::SwitchConfig cfg;
+  std::vector<PortID> ports;
+
+  auto subsidiaryPortMap =
+      utility::getSubsidiaryPortIDs(platform()->getPlatformPorts());
+  for (auto& port : subsidiaryPortMap) {
+    ports.emplace_back(port.first);
+  }
+  utility::setPortToDefaultProfileIDMap(
+      std::make_shared<PortMap>(), sw()->getPlatform());
+  cfg = utility::onePortPerVlanConfig(
+      platform()->getHwSwitch(), ports, cfg::PortLoopbackMode::MAC, true, true);
+
+  cfg.switchSettings()->maxRouteCounterIDs() = 1;
+  return cfg;
+}
+
+int agentIntegrationTestMain(
+    int argc,
+    char** argv,
+    facebook::fboss::PlatformInitFn initPlatformFn) {
+  ::testing::InitGoogleTest(&argc, argv);
+
+  initAgentTest(argc, argv, initPlatformFn);
+
+  return RUN_ALL_TESTS();
+}
+} // namespace facebook::fboss
---
+++ fboss/agent/test/integration_tests/AgentIntegrationTest.h
@@ -0,0 +1,27 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#include "fboss/agent/Main.h"
+#include "fboss/agent/test/AgentHwTest.h"
+
+DECLARE_string(config);
+
+namespace facebook::fboss {
+class AgentIntegrationTest : public AgentHwTest {
+ protected:
+  cfg::SwitchConfig initialConfig() const override;
+};
+
+int agentIntegrationTestMain(
+    int argc,
+    char** argv,
+    facebook::fboss::PlatformInitFn initPlatformFn);
+
+} // namespace facebook::fboss
