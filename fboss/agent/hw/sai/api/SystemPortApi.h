b557e825eb5b6bdc881d263186e16a0d25eec5f2
Jasmeet Bagga <jasmeetbagga@fb.com>
2022-05-25 15:17:24 -0700

    SystemPortApi support, new concept for DSF to map scheduled ports
    Summary: As titled
    
    Differential Revision: D36668386
    
    fbshipit-source-id: ca5f78ce5f52e40e58e680573ccf77d06924e67e

---
+++ fboss/agent/hw/sai/api/SystemPortApi.h
@@ -0,0 +1,106 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+#pragma once
+
+#include "fboss/agent/hw/sai/api/SaiApi.h"
+#include "fboss/agent/hw/sai/api/SaiAttribute.h"
+#include "fboss/agent/hw/sai/api/SaiAttributeDataTypes.h"
+#include "fboss/agent/hw/sai/api/SaiVersion.h"
+#include "fboss/agent/hw/sai/api/Types.h"
+
+#include <folly/logging/xlog.h>
+
+#include <optional>
+#include <tuple>
+
+extern "C" {
+#include <sai.h>
+}
+
+namespace facebook::fboss {
+
+class SystemPortApi;
+
+struct SaiSystemPortTraits {
+  static constexpr sai_object_type_t ObjectType = SAI_OBJECT_TYPE_SYSTEM_PORT;
+  using SaiApiT = SystemPortApi;
+  struct Attributes {
+    using EnumType = sai_system_port_attr_t;
+    using Type = SaiAttribute<EnumType, SAI_SYSTEM_PORT_ATTR_TYPE, sai_int32_t>;
+    using QosNumberOfVoqs = SaiAttribute<
+        EnumType,
+        SAI_SYSTEM_PORT_ATTR_QOS_NUMBER_OF_VOQS,
+        sai_uint32_t>;
+    using QosVoqList = SaiAttribute<
+        EnumType,
+        SAI_SYSTEM_PORT_ATTR_QOS_VOQ_LIST,
+        std::vector<SaiObjectIdT>>;
+    using Port =
+        SaiAttribute<EnumType, SAI_SYSTEM_PORT_ATTR_PORT, SaiObjectIdT>;
+    using AdminState =
+        SaiAttribute<EnumType, SAI_SYSTEM_PORT_ATTR_ADMIN_STATE, bool>;
+    using ConfigInfo = SaiAttribute<
+        EnumType,
+        SAI_SYSTEM_PORT_ATTR_CONFIG_INFO,
+        sai_system_port_config_t>;
+    using QosTcToQueueMap = SaiAttribute<
+        EnumType,
+        SAI_SYSTEM_PORT_ATTR_QOS_TC_TO_QUEUE_MAP,
+        SaiObjectIdT>;
+  };
+  using AdapterKey = SystemPortSaiId;
+  using AdapterHostKey = Attributes::ConfigInfo;
+
+  using CreateAttributes = std::tuple<
+      Attributes::ConfigInfo,
+      std::optional<Attributes::AdminState>,
+      std::optional<Attributes::QosTcToQueueMap>>;
+};
+
+SAI_ATTRIBUTE_NAME(SystemPort, Type)
+SAI_ATTRIBUTE_NAME(SystemPort, QosNumberOfVoqs)
+SAI_ATTRIBUTE_NAME(SystemPort, QosVoqList)
+SAI_ATTRIBUTE_NAME(SystemPort, Port)
+SAI_ATTRIBUTE_NAME(SystemPort, AdminState)
+SAI_ATTRIBUTE_NAME(SystemPort, ConfigInfo)
+SAI_ATTRIBUTE_NAME(SystemPort, QosTcToQueueMap)
+
+class SystemPortApi : public SaiApi<SystemPortApi> {
+ public:
+  static constexpr sai_api_t ApiType = SAI_API_SYSTEM_PORT;
+  SystemPortApi() {
+    sai_status_t status =
+        sai_api_query(ApiType, reinterpret_cast<void**>(&api_));
+    saiApiCheckError(status, ApiType, "Failed to query for port api");
+  }
+
+ private:
+  sai_status_t _create(
+      SystemPortSaiId* id,
+      sai_object_id_t switch_id,
+      size_t count,
+      sai_attribute_t* attr_list) const {
+    return api_->create_port(rawSaiId(id), switch_id, count, attr_list);
+  }
+  sai_status_t _remove(SystemPortSaiId key) const {
+    return api_->remove_port(key);
+  }
+  sai_status_t _getAttribute(SystemPortSaiId key, sai_attribute_t* attr) const {
+    return api_->get_port_attribute(key, 1, attr);
+  }
+  sai_status_t _setAttribute(SystemPortSaiId key, const sai_attribute_t* attr)
+      const {
+    return api_->set_port_attribute(key, attr);
+  }
+  sai_port_api_t* api_;
+  friend class SaiApi<SystemPortApi>;
+};
+
+} // namespace facebook::fboss
--- fboss/agent/hw/sai/api/Types.h
+++ fboss/agent/hw/sai/api/Types.h
@@ -46,6 +46,7 @@ FBOSS_STRONG_TYPE(sai_object_id_t, RouterInterfaceSaiId);
 FBOSS_STRONG_TYPE(sai_object_id_t, SamplePacketSaiId);
 FBOSS_STRONG_TYPE(sai_object_id_t, SchedulerSaiId);
 FBOSS_STRONG_TYPE(sai_object_id_t, SwitchSaiId);
+FBOSS_STRONG_TYPE(sai_object_id_t, SystemPortSaiId);
 FBOSS_STRONG_TYPE(sai_object_id_t, VirtualRouterSaiId);
 FBOSS_STRONG_TYPE(sai_object_id_t, VlanSaiId);
 FBOSS_STRONG_TYPE(sai_object_id_t, VlanMemberSaiId);
