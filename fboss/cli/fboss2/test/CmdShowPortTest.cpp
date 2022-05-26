21607602db38bbdb91295d6fb86e9625e4ed7538
Alicia Luo <alicialuo@fb.com>
2022-05-26 16:40:25 -0700

    Augment `fboss2 show port` to sort by name
    Summary:
    The output of current `fboss2 show port` command is not sorted yet.
    
    We want to display the result according to the order of the port name.
    
    Below is an explanation of how the sorting algorithm is performed between two port names:
    
    For example, `eth1/5/3` will be parsed to four parts: `eth`(module name), `1`(module number), `5`(port number), `3` (subport number).
    
    Two port names will first be compared by the alphabetical order of their module names. If the module namea are the same, then the order will be decied by the numerical values of their module number, port number, subport number in order.
    
    Therefore, `eth420/5/1` comes before `fab3/9/1` as the module name of the former one is alphabetically smaller. However, `eth420/5/1` comes after `eth1/5/3` as the module number `420` is larger than `1`. Accordingly, `eth1/5/3` comes after `eth1/5/1` as its subport is larger. With input array
     `["eth1/5/1", "eth1/5/2", "eth1/5/3", "fab402/20/1", "eth1/10/1", eth1/4/1"]`, the expected returned order of this comparator will be `["eth1/4/1", "eth1/5/1", "eth1/5/2", "eth1/5/3",  "eth1/10/1", "fab402/20/1"]`.
    
    Differential Revision: D36561435
    
    fbshipit-source-id: 322a9bb24403d0b95a1e72518b4eebc959d0d9ed

--- fboss/cli/fboss2/test/CmdShowPortTest.cpp
+++ fboss/cli/fboss2/test/CmdShowPortTest.cpp
@@ -57,16 +57,71 @@ std::map<int32_t, PortInfoThrift> createPortEntries() {
   tcvr3.transceiverId() = 2;
   portEntry3.transceiverIdx() = tcvr3;
 
+  PortInfoThrift portEntry4;
+  portEntry4.portId() = 8;
+  portEntry4.name() = "fab402/9/1";
+  portEntry4.adminState() = PortAdminState::ENABLED;
+  portEntry4.operState() = PortOperState::UP;
+  portEntry4.speedMbps() = 100000;
+  portEntry4.profileID() = "PROFILE_100G_4_NRZ_NOFEC_COPPER";
+  TransceiverIdxThrift tcvr4;
+  tcvr4.transceiverId() = 9;
+  portEntry4.transceiverIdx() = tcvr4;
+
+  PortInfoThrift portEntry5;
+  portEntry5.portId() = 7;
+  portEntry5.name() = "eth1/10/2";
+  portEntry5.adminState() = PortAdminState::ENABLED;
+  portEntry5.operState() = PortOperState::UP;
+  portEntry5.speedMbps() = 100000;
+  portEntry5.profileID() = "PROFILE_100G_4_NRZ_CL91_OPTICAL";
+  TransceiverIdxThrift tcvr5;
+  tcvr5.transceiverId() = 9;
+  portEntry5.transceiverIdx() = tcvr5;
+
+  PortInfoThrift portEntry6;
+  portEntry6.portId() = 9;
+  portEntry6.name() = "eth1/4/1";
+  portEntry6.adminState() = PortAdminState::ENABLED;
+  portEntry6.operState() = PortOperState::UP;
+  portEntry6.speedMbps() = 100000;
+  portEntry6.profileID() = "PROFILE_100G_4_NRZ_CL91_OPTICAL";
+  TransceiverIdxThrift tcvr6;
+  tcvr6.transceiverId() = 10;
+  portEntry6.transceiverIdx() = tcvr6;
+
   portMap[portEntry1.get_portId()] = portEntry1;
   portMap[portEntry2.get_portId()] = portEntry2;
   portMap[portEntry3.get_portId()] = portEntry3;
+  portMap[portEntry4.get_portId()] = portEntry4;
+  portMap[portEntry5.get_portId()] = portEntry5;
+  portMap[portEntry6.get_portId()] = portEntry6;
+  return portMap;
+}
+
+std::map<int32_t, PortInfoThrift> createInvalidPortEntries() {
+  std::map<int32_t, PortInfoThrift> portMap;
+
+  // port name format <module name><module number>/<port number>/<subport
+  // number>
+  // missing <module number>
+
+  PortInfoThrift portEntry1, portEntry2;
+  portEntry1.portId() = 1;
+  portEntry1.name() = "eth/5/1";
+
+  portEntry2.portId() = 2;
+  portEntry2.name() = "eth/5/1";
+
+  portMap[portEntry1.get_portId()] = portEntry1;
+  portMap[portEntry2.get_portId()] = portEntry2;
   return portMap;
 }
 
 cli::ShowPortModel createPortModel() {
   cli::ShowPortModel model;
 
-  cli::PortEntry entry1, entry2, entry3;
+  cli::PortEntry entry1, entry2, entry3, entry4, entry5, entry6;
   entry1.id() = 1;
   entry1.name() = "eth1/5/1";
   entry1.adminState() = "Enabled";
@@ -91,13 +146,39 @@ cli::ShowPortModel createPortModel() {
   entry3.profileId() = "PROFILE_100G_4_NRZ_CL91_COPPER";
   entry3.tcvrID() = 2;
 
-  model.portEntries() = {entry1, entry2, entry3};
+  entry4.id() = 8;
+  entry4.name() = "fab402/9/1";
+  entry4.adminState() = "Enabled";
+  entry4.operState() = "Up";
+  entry4.speed() = "100G";
+  entry4.profileId() = "PROFILE_100G_4_NRZ_NOFEC_COPPER";
+  entry4.tcvrID() = 9;
+
+  entry5.id() = 7;
+  entry5.name() = "eth1/10/2";
+  entry5.adminState() = "Enabled";
+  entry5.operState() = "Up";
+  entry5.speed() = "100G";
+  entry5.profileId() = "PROFILE_100G_4_NRZ_CL91_OPTICAL";
+  entry5.tcvrID() = 9;
+
+  entry6.id() = 9;
+  entry6.name() = "eth1/4/1";
+  entry6.adminState() = "Enabled";
+  entry6.operState() = "Up";
+  entry6.speed() = "100G";
+  entry6.profileId() = "PROFILE_100G_4_NRZ_CL91_OPTICAL";
+  entry6.tcvrID() = 10;
+
+  // sorted by name
+  model.portEntries() = {entry6, entry1, entry2, entry3, entry5, entry4};
   return model;
 }
 
 class CmdShowPortTestFixture : public CmdHandlerTestBase {
  public:
   std::map<int32_t, facebook::fboss::PortInfoThrift> portEntries;
+  CmdShowPortTraits::ObjectArgType queriedEntries;
   cli::ShowPortModel normalizedModel;
 
   void SetUp() override {
@@ -106,6 +187,26 @@ class CmdShowPortTestFixture : public CmdHandlerTestBase {
     normalizedModel = createPortModel();
   }
 };
+
+TEST_F(CmdShowPortTestFixture, sortByName) {
+  auto model = CmdShowPort().createModel(portEntries, queriedEntries);
+
+  EXPECT_THRIFT_EQ(model, normalizedModel);
+}
+
+TEST_F(CmdShowPortTestFixture, invalidPortName) {
+  auto invalidPortEntries = createInvalidPortEntries();
+
+  try {
+    CmdShowPort().createModel(invalidPortEntries, queriedEntries);
+    FAIL();
+  } catch (const std::invalid_argument& expected) {
+    ASSERT_STREQ(
+        "Invalid port name: eth/5/1\n"
+        "Port name must match 'moduleNum/port/subport' pattern",
+        expected.what());
+  }
+}
 
 TEST_F(CmdShowPortTestFixture, queryClient) {
   setupMockedAgentServer();
@@ -125,11 +226,14 @@ TEST_F(CmdShowPortTestFixture, printOutput) {
 
   std::string output = ss.str();
   std::string expectOutput =
-      " ID  Name      AdminState  LinkState  TcvrID  Speed  ProfileID                      \n"
-      "--------------------------------------------------------------------------------------------\n"
-      " 1   eth1/5/1  Enabled     Down       0       100G   PROFILE_100G_4_NRZ_CL91_COPPER \n"
-      " 2   eth1/5/2  Disabled    Down       1       25G    PROFILE_25G_1_NRZ_CL74_COPPER  \n"
-      " 3   eth1/5/3  Enabled     Up         2       100G   PROFILE_100G_4_NRZ_CL91_COPPER \n\n";
+      " ID  Name        AdminState  LinkState  TcvrID  Speed  ProfileID                       \n"
+      "-----------------------------------------------------------------------------------------------\n"
+      " 9   eth1/4/1    Enabled     Up         10      100G   PROFILE_100G_4_NRZ_CL91_OPTICAL \n"
+      " 1   eth1/5/1    Enabled     Down       0       100G   PROFILE_100G_4_NRZ_CL91_COPPER  \n"
+      " 2   eth1/5/2    Disabled    Down       1       25G    PROFILE_25G_1_NRZ_CL74_COPPER   \n"
+      " 3   eth1/5/3    Enabled     Up         2       100G   PROFILE_100G_4_NRZ_CL91_COPPER  \n"
+      " 7   eth1/10/2   Enabled     Up         9       100G   PROFILE_100G_4_NRZ_CL91_OPTICAL \n"
+      " 8   fab402/9/1  Enabled     Up         9       100G   PROFILE_100G_4_NRZ_NOFEC_COPPER \n\n";
   EXPECT_EQ(output, expectOutput);
 }
 
