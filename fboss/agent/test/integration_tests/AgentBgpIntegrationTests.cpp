eb0e648caccf7c4b8d152fae72ef5ded4282d949
Midhun Somasundaran <msomasundaran@fb.com>
2022-06-02 19:09:34 -0700

    Add a test to exercise agent notifcation to bgp
    Summary: Add a test to exercise thrift notification from agent to bgp. The test sets up an NDP entry and flushes it by bringing down the ports which trigger a notification to bgp.
    
    Reviewed By: jasmeetbagga
    
    Differential Revision: D36618544
    
    fbshipit-source-id: 37d7aac2178007f5b6d229025828999ec46b994b

--- fboss/agent/test/integration_tests/AgentBgpIntegrationTests.cpp
+++ fboss/agent/test/integration_tests/AgentBgpIntegrationTests.cpp
@@ -9,10 +9,10 @@
  */
 
 #include <fb303/ServiceData.h>
+#include "fboss/agent/IPv6Handler.h"
 #include "fboss/agent/gen-cpp2/switch_config_types.h"
 #include "fboss/agent/hw/test/ConfigFactory.h"
 #include "fboss/agent/state/SwitchState.h"
-#include "fboss/agent/test/TestUtils.h"
 #include "fboss/agent/test/integration_tests/AgentIntegrationTest.h"
 #include "fboss/lib/CommonUtils.h"
 #include "neteng/fboss/bgp/cpp/BgpServiceUtil.h"
@@ -25,6 +25,7 @@ namespace {
 static constexpr int kNumBgpSessions{4}; // ipv4 and ipv6 with one for each end
 static constexpr int kBgpThriftPort{6909};
 auto constexpr kMaxRetries = 30;
+auto constexpr kMaxFastNotificationRetries = 2;
 auto constexpr kTestPrefix = "100::";
 auto constexpr kTestPrefixLength = 120;
 } // namespace
@@ -38,7 +39,8 @@ class BgpIntegrationTest : public AgentIntegrationTest {
  protected:
   void checkBgpState(
       TBgpPeerState state,
-      std::set<std::string> sessionsToCheck) {
+      std::set<std::string> sessionsToCheck,
+      int retries = kMaxRetries) {
     auto clientParams = servicerouter::ClientParams();
     clientParams.setSingleHost("::1", kBgpThriftPort);
     auto client =
@@ -59,7 +61,7 @@ class BgpIntegrationTest : public AgentIntegrationTest {
             }
           }
         },
-        kMaxRetries);
+        retries);
   }
 
   void checkAgentState() {
@@ -83,12 +85,66 @@ class BgpIntegrationTest : public AgentIntegrationTest {
     EXPECT_NE(testRoute, nullptr);
     EXPECT_TRUE(testRoute->getEntryForClient(ClientID::BGPD));
   }
+
+  void updateState(folly::StringPiece name, StateUpdateFn func) {
+    sw()->updateStateBlocking(name, func);
+  }
+
+  void setPortState(PortID port, bool up) {
+    updateState(up ? "set port up" : "set port down", [&](const auto& state) {
+      auto newState = state->clone();
+      auto newPort = newState->getPorts()->getPort(port)->modify(&newState);
+      newPort->setLoopbackMode(
+          up ? cfg::PortLoopbackMode::MAC : cfg::PortLoopbackMode::NONE);
+      newPort->setAdminState(
+          up ? cfg::PortState::ENABLED : cfg::PortState::DISABLED);
+      return newState;
+    });
+  }
+
+  void addNDPEntry(
+      folly::IPAddressV6 srcIpV6,
+      folly::IPAddressV6 dstIpV6,
+      PortDescriptor portDesc,
+      VlanID vlan) {
+    auto srcMac = utility::kLocalCpuMac(), dstMac = srcMac;
+    sw()->getIPv6Handler()->sendNeighborAdvertisement(
+        vlan, srcMac, srcIpV6, dstMac, dstIpV6, portDesc);
+  }
 };
 
 TEST_F(BgpIntegrationTest, bgpSesionsEst) {
   auto verify = [&]() {
     checkBgpState(TBgpPeerState::ESTABLISHED, {"1::", "2::"});
     checkAgentState();
+  };
+  verifyAcrossWarmBoots(verify);
+}
+
+TEST_F(BgpIntegrationTest, bgpNotifyPortDown) {
+  auto verify = [&]() {
+    // Bring the port up and verify
+    setPortState(masterLogicalPortIds()[0], true);
+    setPortState(masterLogicalPortIds()[1], true);
+    checkBgpState(TBgpPeerState::ESTABLISHED, {"1::", "2::"});
+    checkAgentState();
+    // add an NDP entres to trigger update to bgp on flush
+    addNDPEntry(
+        folly::IPAddressV6("1::"),
+        folly::IPAddressV6("1::"),
+        PortDescriptor(masterLogicalPortIds()[0]),
+        VlanID(utility::kBaseVlanId));
+    addNDPEntry(
+        folly::IPAddressV6("2::"),
+        folly::IPAddressV6("2::"),
+        PortDescriptor(masterLogicalPortIds()[1]),
+        VlanID(utility::kBaseVlanId + 1));
+    // Bring down a port and ensure that BGP session goes idle
+    setPortState(masterLogicalPortIds()[0], false);
+    setPortState(masterLogicalPortIds()[1], false);
+    // BGP session should transition to idle through fast notification
+    checkBgpState(
+        TBgpPeerState::IDLE, {"1::", "2::"}, kMaxFastNotificationRetries);
   };
   verifyAcrossWarmBoots(verify);
 }
