3f0b038076dc292cdef613e8e931d34ddcc5e21d
Alicia Luo <alicialuo@fb.com>
2022-06-22 08:58:20 -0700

    Implement and verify `doTyping` for argument type conversion
    Summary:
    ### Context
    Currently all fboss2 subcommands accept arguments as a string vector and the framework does not do any argument parsing/validation automatically. We can use the enum type of the argument to do some of this at the framework level and pass a fully typed struct to the command handler.
    
    Based upon the single instance type conversion, we hope to support the type conversion with templates so that the implementation will not be restricted by various forms of the actual input
    
    ### Changes
    - Implement a tuple version `DoTyping` and verify with test cases
    - Implement and verify the template version of `DoTyping`
    
    ### Next Steps
    - Create argument type class
    - Replace them in actual implementation
    
    Reviewed By: peygar
    
    Differential Revision: D37228864
    
    fbshipit-source-id: a7e9773aad3b8f82f9c9ab66f4e9d8734f4b685a

--- fboss/cli/fboss2/test/CmdArgsTest.cpp
+++ fboss/cli/fboss2/test/CmdArgsTest.cpp
@@ -16,19 +16,34 @@ using namespace ::testing;
 namespace facebook::fboss {
 namespace {
 
+std::vector<std::string> getPortList() {
+  return {"eth1/1/1", "eth1/20/3", "eth1/32/4"};
+}
+
+std::vector<std::string> getPrbsComponent() {
+  return {
+      "asic",
+      "xphy_system",
+      "xphy_line",
+      "transceiver_system",
+      "transceiver_line"};
+}
+
 utils::PortList doTypingImplicitPortList() {
-  std::vector<std::string> portList({"eth1/1/1", "eth1/20/3", "eth1/32/4"});
-  return portList;
+  return getPortList();
 }
 
 utils::PrbsComponent doTypingImplicitPrbsComponent() {
-  std::vector<std::string> prbsComponent(
-      {"asic",
-       "xphy_system",
-       "xphy_line",
-       "transceiver_system",
-       "transceiver_line"});
-  return prbsComponent;
+  return getPrbsComponent();
+}
+
+std::tuple<utils::PortList, utils::PrbsComponent> doTypingImplicitTuple() {
+  return std::make_tuple(getPortList(), getPrbsComponent());
+}
+
+template <typename TypedArgs, typename UnTypedArgs>
+TypedArgs doTyping(UnTypedArgs& unTypedArgs) {
+  return unTypedArgs;
 }
 
 } // namespace
@@ -38,5 +53,31 @@ TEST(CmdArgsTest, doTypingImplicitSingle) {
   auto typedPrbsComponent = doTypingImplicitPrbsComponent();
   EXPECT_EQ(typeid(typedPortList), typeid(utils::PortList));
   EXPECT_EQ(typeid(typedPrbsComponent), typeid(utils::PrbsComponent));
+  static_assert(std::is_same_v<utils::PortList, decltype(typedPortList)>);
+  static_assert(
+      std::is_same_v<utils::PrbsComponent, decltype(typedPrbsComponent)>);
 }
+
+TEST(CmdArgsTest, doTypingImplicitTuple) {
+  auto typedArgs = doTypingImplicitTuple();
+  static_assert(std::is_same_v<
+                utils::PortList,
+                std::tuple_element_t<0, decltype(typedArgs)>>);
+  static_assert(std::is_same_v<
+                utils::PrbsComponent,
+                std::tuple_element_t<1, decltype(typedArgs)>>);
+}
+
+TEST(CmdArgsTest, doTypingTemplating) {
+  using TypedArgsTest = std::tuple<utils::PortList, utils::PrbsComponent>;
+  auto untypedArgs = std::make_tuple(getPortList(), getPrbsComponent());
+  auto typedArgs = doTyping<TypedArgsTest>(untypedArgs);
+  static_assert(std::is_same_v<
+                utils::PortList,
+                std::tuple_element_t<0, decltype(typedArgs)>>);
+  static_assert(std::is_same_v<
+                utils::PrbsComponent,
+                std::tuple_element_t<1, decltype(typedArgs)>>);
+}
+
 } // namespace facebook::fboss
