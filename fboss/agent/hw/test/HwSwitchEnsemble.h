92d41bc58ce8554097e03cc53afa4138ef139782
Jasmeet Bagga <jasmeetbagga@fb.com>
2022-06-30 08:06:41 -0700

    Add SwitchType and id to HwSwitchEnsemble init params
    Summary: As titled. To be used in VOQ, Fabric switch tests
    
    Differential Revision: D37454656
    
    fbshipit-source-id: 62dc4997a812cd2e4409c52b559055d5350e85c0

--- fboss/agent/hw/test/HwSwitchEnsemble.h
+++ fboss/agent/hw/test/HwSwitchEnsemble.h
@@ -76,6 +76,8 @@ class HwSwitchEnsemble : public HwSwitch::Callback {
   void stopObservers();
   struct HwSwitchEnsembleInitInfo {
     std::optional<TransceiverInfo> overrideTransceiverInfo;
+    cfg::SwitchType switchType = cfg::SwitchType::NPU;
+    std::optional<int64_t> switchId;
   };
   enum Feature : uint32_t {
     PACKET_RX,
