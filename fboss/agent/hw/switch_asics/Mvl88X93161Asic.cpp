fdf6b6e7515d1dbce43a147e8391598dcd5d588a
Rajan Kumar <rajank@fb.com>
2022-06-30 23:56:39 -0700

    Added Marvell 88X93161 ASIC to platform code
    Summary: Added Marvell 88X93161 ASIC to platform code
    
    Differential Revision: D37321443
    
    fbshipit-source-id: 85a84acdcce73ec972b90df8da671b55cd0fe6c6

---
+++ fboss/agent/hw/switch_asics/Mvl88X93161Asic.cpp
@@ -0,0 +1,86 @@
+// Copyright 2004-present Facebook. All Rights Reserved.
+
+#include "fboss/agent/hw/switch_asics/Mvl88X93161Asic.h"
+#include "fboss/agent/FbossError.h"
+
+namespace facebook::fboss {
+bool Mvl88X93161Asic::isSupported(Feature feature) const {
+  switch (feature) {
+    case HwAsic::Feature::REMOVE_PORTS_FOR_COLDBOOT:
+      return true;
+    default:
+      return false;
+  }
+  return false;
+}
+
+std::set<cfg::StreamType> Mvl88X93161Asic::getQueueStreamTypes(
+    bool /* cpu */) const {
+  throw FbossError("Mvl88X93161Asic doesn't support queue feature");
+}
+int Mvl88X93161Asic::getDefaultNumPortQueues(
+    cfg::StreamType /* streamType */,
+    bool /* cpu */) const {
+  throw FbossError("Mvl88X93161Asic doesn't support queue feature");
+}
+uint64_t Mvl88X93161Asic::getDefaultReservedBytes(
+    cfg::StreamType /* streamType */,
+    bool /* cpu */) const {
+  throw FbossError("Mvl88X93161Asic doesn't support queue feature");
+}
+cfg::MMUScalingFactor Mvl88X93161Asic::getDefaultScalingFactor(
+    cfg::StreamType /* streamType */,
+    bool /* cpu */) const {
+  throw FbossError("Mvl88X93161Asic doesn't support queue feature");
+}
+
+uint32_t Mvl88X93161Asic::getMaxMirrors() const {
+  throw FbossError("Mvl88X93161Asic doesn't support mirror feature");
+}
+uint16_t Mvl88X93161Asic::getMirrorTruncateSize() const {
+  throw FbossError("Mvl88X93161Asic doesn't support mirror feature");
+}
+
+uint32_t Mvl88X93161Asic::getMaxLabelStackDepth() const {
+  throw FbossError("Mvl88X93161Asic doesn't support label feature");
+};
+uint64_t Mvl88X93161Asic::getMMUSizeBytes() const {
+  throw FbossError("Mvl88X93161Asic doesn't support MMU feature");
+};
+int Mvl88X93161Asic::getMaxNumLogicalPorts() const {
+  throw FbossError("Mvl88X93161Asic doesn't support logical ports feature");
+}
+uint32_t Mvl88X93161Asic::getMaxWideEcmpSize() const {
+  throw FbossError("Mvl88X93161Asic doesn't support ecmp feature");
+}
+uint32_t Mvl88X93161Asic::getMaxLagMemberSize() const {
+  throw FbossError("Mvl88X93161Asic doesn't support lag feature");
+}
+uint32_t Mvl88X93161Asic::getPacketBufferUnitSize() const {
+  throw FbossError("Mvl88X93161Asic doesn't support MMU feature");
+}
+uint32_t Mvl88X93161Asic::getPacketBufferDescriptorSize() const {
+  throw FbossError("Mvl88X93161Asic doesn't support MMU feature");
+}
+uint32_t Mvl88X93161Asic::getMaxVariableWidthEcmpSize() const {
+  return 512;
+}
+uint32_t Mvl88X93161Asic::getMaxEcmpSize() const {
+  return 4096;
+}
+
+uint32_t Mvl88X93161Asic::getNumCores() const {
+  throw FbossError("Num cores API not supported");
+}
+
+bool Mvl88X93161Asic::scalingFactorBasedDynamicThresholdSupported() const {
+  throw FbossError("Mvl88X93161Asic doesn't support MMU feature");
+}
+int Mvl88X93161Asic::getBufferDynThreshFromScalingFactor(
+    cfg::MMUScalingFactor /* scalingFactor */) const {
+  throw FbossError("Mvl88X93161Asic doesn't support MMU feature");
+}
+uint32_t Mvl88X93161Asic::getStaticQueueLimitBytes() const {
+  throw FbossError("Mvl88X93161Asic doesn't support MMU feature");
+}
+}; // namespace facebook::fboss
---
+++ fboss/agent/hw/switch_asics/Mvl88X93161Asic.h
@@ -0,0 +1,61 @@
+// Copyright 2004-present Facebook. All Rights Reserved.
+
+#pragma once
+
+#include "fboss/agent/hw/switch_asics/HwAsic.h"
+
+namespace facebook::fboss {
+
+class Mvl88X93161Asic : public HwAsic {
+ public:
+  bool isSupported(Feature feature) const override;
+  AsicType getAsicType() const override {
+    return AsicType::ASIC_TYPE_SANDIA_PHY;
+  }
+  std::string getVendor() const override {
+    return "marvell";
+  }
+
+  phy::DataPlanePhyChipType getDataPlanePhyChipType() const override {
+    return phy::DataPlanePhyChipType::XPHY;
+  }
+
+  cfg::PortSpeed getMaxPortSpeed() const override {
+    return cfg::PortSpeed::FOURHUNDREDG;
+  }
+
+  std::set<cfg::StreamType> getQueueStreamTypes(bool cpu) const override;
+  int getDefaultNumPortQueues(cfg::StreamType streamType, bool cpu)
+      const override;
+  uint32_t getMaxLabelStackDepth() const override;
+  uint64_t getMMUSizeBytes() const override;
+  uint32_t getMaxMirrors() const override;
+  uint64_t getDefaultReservedBytes(cfg::StreamType streamType, bool cpu)
+      const override;
+  cfg::MMUScalingFactor getDefaultScalingFactor(
+      cfg::StreamType streamType,
+      bool cpu) const override;
+  int getMaxNumLogicalPorts() const override;
+  uint16_t getMirrorTruncateSize() const override;
+  uint32_t getMaxWideEcmpSize() const override;
+  uint32_t getMaxLagMemberSize() const override;
+  uint32_t getSflowShimHeaderSize() const override {
+    return 0;
+  }
+  std::optional<uint32_t> getPortSerdesPreemphasis() const override {
+    return std::nullopt;
+  }
+  uint32_t getPacketBufferUnitSize() const override;
+  uint32_t getPacketBufferDescriptorSize() const override;
+  AsicVendor getAsicVendor() const override {
+    return HwAsic::AsicVendor::ASIC_VENDOR_MARVELL;
+  }
+  uint32_t getMaxVariableWidthEcmpSize() const override;
+  uint32_t getMaxEcmpSize() const override;
+  uint32_t getNumCores() const override;
+  bool scalingFactorBasedDynamicThresholdSupported() const override;
+  int getBufferDynThreshFromScalingFactor(
+      cfg::MMUScalingFactor scalingFactor) const override;
+  uint32_t getStaticQueueLimitBytes() const override;
+};
+} // namespace facebook::fboss
