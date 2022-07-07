5b78fb50d08ff2fe50c4a4e97c1bd03351d9107a
Yijun Li <yijunli@fb.com>
2022-07-07 14:55:34 -0700

    wrap api tracer
    Summary:
    Context: We need to impl a tunnel in SaiSwitch. The tunnel termination module terminates the outer tunnel since encapsulation type is IPinIP and destination is SSW loopback address.
    Create TunnelApi.h, include attributes for Tunnel Decap (Tunnel and Tunnel Term Objects)
    
    # This Stack
    1-4 api related files
    5.Create file for Tunnel Tracer and add it in the build system
    6.**Add trace function call wrappers**
    7.Add to store, create manager
    
    Reviewed By: shri-khare
    
    Differential Revision: D37576329
    
    fbshipit-source-id: 228702b511ce712dc77df076ce297026a0489346

--- fboss/agent/hw/sai/tracer/TunnelApiTracer.cpp
+++ fboss/agent/hw/sai/tracer/TunnelApiTracer.cpp
@@ -32,8 +32,45 @@ std::map<int32_t, std::pair<std::string, std::size_t>> _TunnelTermMap{
 } // namespace
 
 namespace facebook::fboss {
+
+WRAP_CREATE_FUNC(tunnel, SAI_OBJECT_TYPE_TUNNEL, tunnel);
+WRAP_REMOVE_FUNC(tunnel, SAI_OBJECT_TYPE_TUNNEL, tunnel);
+WRAP_SET_ATTR_FUNC(tunnel, SAI_OBJECT_TYPE_TUNNEL, tunnel);
+WRAP_GET_ATTR_FUNC(tunnel, SAI_OBJECT_TYPE_TUNNEL, tunnel);
+
+WRAP_CREATE_FUNC(
+    tunnel_term_table_entry,
+    SAI_OBJECT_TYPE_TUNNEL_TERM_TABLE_ENTRY,
+    tunnel);
+WRAP_REMOVE_FUNC(
+    tunnel_term_table_entry,
+    SAI_OBJECT_TYPE_TUNNEL_TERM_TABLE_ENTRY,
+    tunnel);
+WRAP_SET_ATTR_FUNC(
+    tunnel_term_table_entry,
+    SAI_OBJECT_TYPE_TUNNEL_TERM_TABLE_ENTRY,
+    tunnel);
+WRAP_GET_ATTR_FUNC(
+    tunnel_term_table_entry,
+    SAI_OBJECT_TYPE_TUNNEL_TERM_TABLE_ENTRY,
+    tunnel);
+
 sai_tunnel_api_t* wrappedTunnelApi() {
   static sai_tunnel_api_t tunnelWrappers;
+
+  tunnelWrappers.create_tunnel = &wrap_create_tunnel;
+  tunnelWrappers.remove_tunnel = &wrap_remove_tunnel;
+  tunnelWrappers.set_tunnel_attribute = &wrap_set_tunnel_attribute;
+  tunnelWrappers.get_tunnel_attribute = &wrap_get_tunnel_attribute;
+
+  tunnelWrappers.create_tunnel_term_table_entry =
+      &wrap_create_tunnel_term_table_entry;
+  tunnelWrappers.remove_tunnel_term_table_entry =
+      &wrap_remove_tunnel_term_table_entry;
+  tunnelWrappers.get_tunnel_term_table_entry_attribute =
+      &wrap_get_tunnel_term_table_entry_attribute;
+  tunnelWrappers.set_tunnel_term_table_entry_attribute =
+      &wrap_set_tunnel_term_table_entry_attribute;
 
   return &tunnelWrappers;
 }
