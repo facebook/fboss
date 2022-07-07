1f8d566c6d10a899f836da4a542f6901ca3896e0
Harshit Gulati <hgulati@fb.com>
2022-07-06 22:04:57 -0700

    Print PhyInfo for show interface phy CLI
    Summary: Added logic to print the PhyInfos
    
    Reviewed By: rajank7
    
    Differential Revision: D37631757
    
    fbshipit-source-id: 7bef1a1015851b533d276dc223717bd77b4b0b33

--- fboss/cli/fboss2/commands/show/interface/phy/CmdShowInterfacePhy.h
+++ fboss/cli/fboss2/commands/show/interface/phy/CmdShowInterfacePhy.h
@@ -35,8 +35,148 @@ class CmdShowInterfacePhy
     return createModel(hostInfo, queriedIfs, phyChipType);
   }
 
-  void printOutput(const RetType& /* model */, std::ostream& out = std::cout) {
-    out << std::endl;
+  void printOutput(const RetType& model, std::ostream& out = std::cout) {
+    for (const auto& info : *model.phyInfo_ref()) {
+      auto ifName = info.first;
+      auto phyInfo = info.second;
+      if (phyInfo.find(phy::DataPlanePhyChipType::IPHY) != phyInfo.end()) {
+        printPhyInfo(out, phyInfo[phy::DataPlanePhyChipType::IPHY], ifName);
+      }
+      if (phyInfo.find(phy::DataPlanePhyChipType::XPHY) != phyInfo.end()) {
+        printPhyInfo(out, phyInfo[phy::DataPlanePhyChipType::XPHY], ifName);
+      }
+      out << std::endl;
+    }
+  }
+
+  void printPhyInfo(
+      std::ostream& out,
+      phy::PhyInfo& phyInfo,
+      const std::string& ifName) {
+    auto phyType =
+        apache::thrift::util::enumNameSafe(*phyInfo.phyChip()->type());
+    std::string prefix = phyType + "-";
+    Table table;
+    table.setHeader({"Interface", ifName});
+    table.addRow({"PhyChipType", phyType});
+    if (auto linkState = phyInfo.linkState()) {
+      if (*linkState) {
+        table.addRow(
+            {"Link State", Table::StyledCell("UP", Table::Style::GOOD)});
+      } else {
+        table.addRow(
+            {"Link State", Table::StyledCell("DOWN", Table::Style::ERROR)});
+      }
+    }
+    if (auto linkFlapCount = phyInfo.linkFlapCount()) {
+      table.addRow({"Link Flap Count", std::to_string(*linkFlapCount)});
+    }
+    table.addRow(
+        {"Speed", apache::thrift::util::enumNameSafe(*phyInfo.speed())});
+    table.addRow(
+        {phyType + " Data Collected",
+         utils::getPrettyElapsedTime(*phyInfo.timeCollected()) + " ago"});
+    out << table;
+    if (auto systemSide = phyInfo.system()) {
+      printSideInfo(out, *systemSide, prefix + "System ");
+    }
+    printSideInfo(out, *phyInfo.line(), prefix + "Line ");
+  }
+
+  void printSideInfo(
+      std::ostream& out,
+      phy::PhySideInfo& sideInfo,
+      const std::string& prefix) {
+    if (auto rs = sideInfo.rs()) {
+      Table rsTable;
+      rsTable.setHeader({prefix + "Reconciliation Sublayer", ""});
+      rsTable.addRow(
+          {prefix + "Local Fault Live",
+           std::to_string(*(*rs).faultStatus()->localFault())});
+      rsTable.addRow(
+          {prefix + "Remote Fault Live",
+           std::to_string(*(*rs).faultStatus()->remoteFault())});
+      out << rsTable;
+    }
+    if (auto pcs = sideInfo.pcs()) {
+      Table pcsTable;
+      Table rsFecTable;
+      bool hasPcsData{false}, hasRsFecData{false};
+      if (auto pcsRxStatusLive = (*pcs).pcsRxStatusLive()) {
+        pcsTable.addRow(
+            {prefix + "PCS RX Link Status Live",
+             makeColorCellForLiveFlag(std::to_string(*pcsRxStatusLive))});
+        hasPcsData = true;
+      }
+      if (auto rsFec = (*pcs).rsFec()) {
+        rsFecTable.setHeader({prefix + "RS FEC", ""});
+        rsFecTable.addRow(
+            {prefix + "Corrected codewords",
+             std::to_string(*(rsFec->correctedCodewords()))});
+        rsFecTable.addRow(
+            {prefix + "Uncorrected codewords",
+             std::to_string(*(rsFec->uncorrectedCodewords()))});
+        hasRsFecData = true;
+      }
+      if (hasPcsData) {
+        pcsTable.setHeader({prefix + "PCS", ""});
+        out << pcsTable;
+      }
+      if (hasRsFecData) {
+        out << rsFecTable;
+      }
+    }
+
+    if (!sideInfo.pmd()->lanes()->empty()) {
+      Table pmdTable;
+      pmdTable.setHeader(
+          {prefix + "PMD",
+           "Lane",
+           "RX Signal Detect Live",
+           "RX CDR Lock Live",
+           "Eye Heights",
+           "Eye Widths"});
+      for (const auto& laneInfo : *sideInfo.pmd()->lanes()) {
+        auto lane = laneInfo.second;
+        std::string sigDetLive = "N/A";
+        std::string cdrLockLive = "N/A";
+        std::vector<float> eyeHeights = {};
+        std::vector<float> eyeWidths = {};
+        if (auto rxSigDetLive = lane.signalDetectLive()) {
+          sigDetLive = std::to_string(*rxSigDetLive);
+        }
+        if (auto rxCdrLockLive = lane.cdrLockLive()) {
+          cdrLockLive = std::to_string(*rxCdrLockLive);
+        }
+        if (auto eyes = lane.eyes()) {
+          for (const auto& eye : *eyes) {
+            if (auto eyeW = eye.width()) {
+              eyeWidths.push_back(*eyeW);
+            }
+            if (auto eyeH = eye.height()) {
+              eyeHeights.push_back(*eyeH);
+            }
+          }
+        }
+        pmdTable.addRow(
+            {"",
+             std::to_string(*lane.lane()),
+             makeColorCellForLiveFlag(sigDetLive),
+             makeColorCellForLiveFlag(cdrLockLive),
+             folly::join(",", eyeHeights),
+             folly::join(",", eyeWidths)});
+      }
+      out << pmdTable;
+    }
+  }
+
+  Table::StyledCell makeColorCellForLiveFlag(const std::string& flag) {
+    if (flag == "0") {
+      return Table::StyledCell("False", Table::Style::ERROR);
+    } else if (flag == "1") {
+      return Table::StyledCell("True", Table::Style::GOOD);
+    }
+    return Table::StyledCell(flag, Table::Style::INFO);
   }
 
   RetType createModel(
