a8a2e737a7cd3d6984c6a18ea7c9f86676239fb8
Shrikrishna (Shri) Khare <skhare@fb.com>
2022-08-10 18:35:52 -0700

    Start adding platform mapping for Makalu
    Summary:
    As titled.
    
    Adds placeholder data structures. Actual platform mapping will be added later.
    
    Differential Revision: D38529034
    
    fbshipit-source-id: ca46df8571664d0f665386aa54103edde562c613

---
+++ fboss/agent/platforms/common/makalu/MakaluPlatformMapping.h
@@ -0,0 +1,27 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+#pragma once
+
+#include "fboss/agent/platforms/common/PlatformMapping.h"
+
+namespace facebook {
+namespace fboss {
+
+class MakaluPlatformMapping : public PlatformMapping {
+ public:
+  MakaluPlatformMapping();
+
+ private:
+  // Forbidden copy constructor and assignment operator
+  MakaluPlatformMapping(MakaluPlatformMapping const&) = delete;
+  MakaluPlatformMapping& operator=(MakaluPlatformMapping const&) = delete;
+};
+} // namespace fboss
+} // namespace facebook
