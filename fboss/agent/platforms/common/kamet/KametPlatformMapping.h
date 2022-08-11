9f8a92180ad95151d32de4fb114fe9b4bb3a840b
Shrikrishna (Shri) Khare <skhare@fb.com>
2022-08-10 18:35:52 -0700

    Start adding platform mapping for Kamet
    Summary:
    As titled.
    
    Adds placeholder data structures. Actual platform mapping will be added later.
    
    Differential Revision: D38561652
    
    fbshipit-source-id: 5b43253a45b0b0420022057c17a85e55cb3c15f9

---
+++ fboss/agent/platforms/common/kamet/KametPlatformMapping.h
@@ -0,0 +1,27 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+#pragma once
+
+#include "fboss/agent/platforms/common/PlatformMapping.h"
+
+namespace facebook {
+namespace fboss {
+
+class KametPlatformMapping : public PlatformMapping {
+ public:
+  KametPlatformMapping();
+
+ private:
+  // Forbidden copy constructor and assignment operator
+  KametPlatformMapping(KametPlatformMapping const&) = delete;
+  KametPlatformMapping& operator=(KametPlatformMapping const&) = delete;
+};
+} // namespace fboss
+} // namespace facebook
