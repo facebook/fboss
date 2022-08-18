5070b534568d4e0d946cfd0cb346055bd3d81b31
Jasmeet Bagga <jasmeetbagga@fb.com>
2022-08-18 14:35:51 -0700

    Fabric switch test - verify we can set loopback mode on ports
    Summary: As titled
    
    Reviewed By: shri-khare
    
    Differential Revision: D38793459
    
    fbshipit-source-id: 381e784dcb37195739021ef8f2228eb35de28b5f

--- fboss/agent/hw/test/HwFabricSwitchTests.cpp
+++ fboss/agent/hw/test/HwFabricSwitchTests.cpp
@@ -16,4 +16,23 @@ TEST_F(HwFabricSwitchTest, init) {
   verifyAcrossWarmBoots([]() {}, []() {});
 }
 
+TEST_F(HwFabricSwitchTest, loopbackMode) {
+  auto setup = [this]() {
+    auto newState = getProgrammedState()->clone();
+    for (auto& port : *newState->getPorts()) {
+      auto newPort = port->modify(&newState);
+      newPort->setLoopbackMode(getAsic()->desiredLoopbackMode());
+    }
+    applyNewState(newState);
+  };
+
+  auto verify = [this]() {
+    auto state = getProgrammedState();
+    for (auto& port : *state->getPorts()) {
+      EXPECT_EQ(getAsic()->desiredLoopbackMode(), port->getLoopbackMode());
+    }
+  };
+  verifyAcrossWarmBoots(setup, verify);
+}
+
 } // namespace facebook::fboss
