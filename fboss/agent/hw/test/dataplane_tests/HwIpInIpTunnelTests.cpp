b0468f5593307c49bc46dd5f9d5ff712db7420f7
Yijun Li <yijunli@fb.com>
2022-08-19 12:31:17 -0700

    HwTest: successful tunnel decap
    Summary:
    End to end test: tunnel decapsulation
    will only run on 7.2.0.0 and non-TH3 devices
    
    Reviewed By: msomasundaran
    
    Differential Revision: D38415168
    
    fbshipit-source-id: bf039a3b244f11f3e000e7c6979265fa2c13b930

--- fboss/agent/hw/test/dataplane_tests/HwIpInIpTunnelTests.cpp
+++ fboss/agent/hw/test/dataplane_tests/HwIpInIpTunnelTests.cpp
@@ -1,5 +1,7 @@
 // (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
 
+#include <cstdint>
+#include <string>
 #include <vector>
 #include "fboss/agent/gen-cpp2/switch_config_types.h"
 #include "fboss/agent/hw/test/ConfigFactory.h"
@@ -7,6 +9,7 @@
 #include "fboss/agent/hw/test/HwTestAclUtils.h"
 #include "fboss/agent/hw/test/HwTestPacketUtils.h"
 #include "fboss/agent/hw/test/TrafficPolicyUtils.h"
+#include "fboss/agent/test/EcmpSetupHelper.h"
 #include "fboss/agent/test/ResourceLibUtil.h"
 #include "folly/logging/xlog.h"
 
@@ -14,6 +17,7 @@ namespace facebook::fboss {
 
 class HwIpInIpTunnelTest : public HwLinkStateDependentTest {
  protected:
+  std::string kTunnelTermDstIp = "2000::1";
   void SetUp() override {
     HwLinkStateDependentTest::SetUp();
   }
@@ -42,19 +46,63 @@ class HwIpInIpTunnelTest : public HwLinkStateDependentTest {
 
     return tunnel;
   }
+  void sendIpInIpPacket(std::string dstIpAddr) {
+    auto vlanId = VlanID(*initialConfig().vlanPorts()[1].vlanID());
+    auto intfMac = utility::getInterfaceMac(getProgrammedState(), vlanId);
+    auto srcMac = utility::MacAddressGenerator().get(intfMac.u64NBO() + 1);
+    const folly::IPAddressV6 outerSrcIp("1000::1");
+    const folly::IPAddressV6 outerDstIp(dstIpAddr);
+    const folly::IPAddressV6 innerSrcIp("4004::23");
+    const folly::IPAddressV6 innerDstIp("3000::3");
+    auto payload = std::vector<uint8_t>(10, 0xff);
+    auto txPacket = utility::makeIpInIpTxPacket(
+        getHwSwitch(),
+        vlanId,
+        srcMac, // src mac
+        intfMac, // dst mac
+        outerSrcIp,
+        outerDstIp,
+        innerSrcIp,
+        innerDstIp,
+        10000, // udp src port
+        20000, // udp dst port
+        0,
+        255,
+        payload);
+    getHwSwitchEnsemble()->ensureSendPacketOutOfPort(
+        std::move(txPacket), masterLogicalPortIds()[1]);
+  }
 };
 
 TEST_F(HwIpInIpTunnelTest, TunnelDecapForwarding) {
   auto setup = [=]() {
     std::vector<cfg::IpInIpTunnel> tunnelList;
     auto cfg{this->initialConfig()};
-    tunnelList.push_back(
-        makeTunnelConfig("hwTestTunnel", "2401:db00:11c:8202:0:0:0:104"));
+    tunnelList.push_back(makeTunnelConfig("hwTestTunnel", kTunnelTermDstIp));
     cfg.ipInIpTunnels() = tunnelList;
     this->applyNewConfig(cfg);
+    utility::EcmpSetupAnyNPorts6 ecmpHelper(
+        getProgrammedState(), getPlatform()->getLocalMac());
+    resolveNeigborAndProgramRoutes(
+        ecmpHelper, 1); // forwarding takes the first port: 0
   };
 
-  auto verify = [=]() {};
+  auto verify = [=]() {
+    // src is in; dest is out
+    auto beforeSrcBytes =
+        getLatestPortStats(masterLogicalPortIds()[1]).get_inBytes_();
+    auto beforeDstBytes =
+        getLatestPortStats(masterLogicalPortIds()[0]).get_outBytes_();
+    sendIpInIpPacket(kTunnelTermDstIp);
+    auto afterSrcBytes =
+        getLatestPortStats(masterLogicalPortIds()[1]).get_inBytes_();
+    auto afterDstBytes =
+        getLatestPortStats(masterLogicalPortIds()[0]).get_outBytes_();
+
+    EXPECT_EQ(
+        afterSrcBytes - beforeSrcBytes - IPv6Hdr::SIZE,
+        afterDstBytes - beforeDstBytes);
+  };
   this->verifyAcrossWarmBoots(setup, verify);
 }
 
