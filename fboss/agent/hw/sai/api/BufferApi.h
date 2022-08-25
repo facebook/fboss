7f0d12efd51dac6ffbc93e5a0d8fd266fcdfd035
Nivin Lawrence <nivinl@fb.com>
2022-08-24 21:49:46 -0700

    Wrap IngressPriorityGroup API
    Summary: As titled
    
    Reviewed By: shri-khare
    
    Differential Revision: D38876810
    
    fbshipit-source-id: 6c21bcff58c882661865a6814837f37adc813ac0

--- fboss/agent/hw/sai/api/BufferApi.h
+++ fboss/agent/hw/sai/api/BufferApi.h
@@ -229,6 +229,29 @@ class BufferApi : public SaiApi<BufferApi> {
       const sai_attribute_t* attr) const {
     return api_->set_buffer_profile_attribute(key, attr);
   }
+
+  sai_status_t _create(
+      IngressPriorityGroupSaiId* id,
+      sai_object_id_t switch_id,
+      size_t count,
+      sai_attribute_t* attr_list) const {
+    return api_->create_ingress_priority_group(
+        rawSaiId(id), switch_id, count, attr_list);
+  }
+  sai_status_t _remove(IngressPriorityGroupSaiId id) const {
+    return api_->remove_ingress_priority_group(id);
+  }
+  sai_status_t _getAttribute(
+      IngressPriorityGroupSaiId key,
+      sai_attribute_t* attr) const {
+    return api_->get_ingress_priority_group_attribute(key, 1, attr);
+  }
+  sai_status_t _setAttribute(
+      IngressPriorityGroupSaiId key,
+      const sai_attribute_t* attr) const {
+    return api_->set_ingress_priority_group_attribute(key, attr);
+  }
+
   sai_buffer_api_t* api_;
   friend class SaiApi<BufferApi>;
 };
