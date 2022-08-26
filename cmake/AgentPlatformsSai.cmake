f53d41592b43eeaacb52e3797cb8a0ffb896dda4
Jasmeet Bagga <jasmeetbagga@fb.com>
2022-08-26 11:32:33 -0700

    Use SaiCloudripperPlatform.cpp in oss builds instead file under oss dir
    Summary:
    The file under oss dir was nearly identical to one in parent
    dir. Just use the one from parent dir, that file is already in open
    source
    
    Reviewed By: nivinl
    
    Differential Revision: D39046767
    
    fbshipit-source-id: 2c508112ef320ef76937cb2f03c191d5fcbae7bd

--- cmake/AgentPlatformsSai.cmake
+++ cmake/AgentPlatformsSai.cmake
@@ -30,7 +30,7 @@ add_library(sai_platform
   fboss/agent/platforms/sai/SaiSandiaPlatform.cpp
   fboss/agent/platforms/sai/SaiSandiaPlatformPort.cpp
   fboss/agent/platforms/sai/SaiWedge400CPlatform.cpp
-  fboss/agent/platforms/sai/oss/SaiCloudRipperPlatform.cpp
+  fboss/agent/platforms/sai/SaiCloudRipperPlatform.cpp
   fboss/agent/platforms/sai/SaiCloudRipperPlatformPort.cpp
   fboss/agent/platforms/sai/SaiWedge400CPlatformPort.cpp
   fboss/agent/platforms/sai/SaiTajoPlatform.cpp
