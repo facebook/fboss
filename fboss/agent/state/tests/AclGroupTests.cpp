a05ef6b5d19567340d71f521e3fc5eb3e7439728
Ron He <zecheng@fb.com>
2022-08-30 11:00:20 -0700

    Back out "Back out "[fboss/state] Unit test for AclTableGroupMap serialization""
    Summary:
    Original commit changeset: b8e52cf20432
    
    Original Phabricator Diff: D38958515 (https://github.com/facebook/fboss/commit/a6442286a3d2a42439255f592bcb6766d5bc401c)
    
    Reviewed By: peygar
    
    Differential Revision: D39112481
    
    fbshipit-source-id: a45fe035f423fd480014bc2f4b723236c451e41b

--- fboss/agent/state/tests/AclGroupTests.cpp
+++ fboss/agent/state/tests/AclGroupTests.cpp
@@ -497,6 +497,7 @@ TEST(AclGroup, ApplyConfigWarmbootMultipleAclTable) {
 
   auto tableGroups = make_shared<AclTableGroupMap>();
   tableGroups->addAclTableGroup(tableGroup);
+  validateNodeMapSerialization(*tableGroups);
 
   cfg::AclTable cfgTable1;
   cfgTable1.name_ref() = kTable1;
