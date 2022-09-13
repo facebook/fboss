f24c8feb73bdcb5f81ad884f07eeedbaa0809019
Jasmeet Bagga <jasmeetbagga@fb.com>
2022-09-13 08:48:44 -0700

    Start building platform mappings for Makalu
    Summary:
    Start with fabric port platform mappings.
    These are very similar to Kamet, except that fabri
    ports start with id=256.
    
    Reviewed By: harshitgulati18
    
    Differential Revision: D39451478
    
    fbshipit-source-id: 2020296fc1fcc942ca3c0ebd2af695b2baf07248

--- fboss/agent/platforms/common/makalu/MakaluPlatformMapping.cpp
+++ fboss/agent/platforms/common/makalu/MakaluPlatformMapping.cpp
@@ -10,19 +10,76 @@
 
 #include "fboss/agent/platforms/common/makalu/MakaluPlatformMapping.h"
 
-// TODO(skhare) add platform mapping
-
+namespace facebook::fboss {
 namespace {
-constexpr auto kJsonPlatformMappingStr = R"(
-{
+cfg::PlatformMapping buildMapping() {
+  cfg::PlatformMapping platformMapping;
+  // Fabric port mapping. For fabric ports only 40 out of the 48
+  // backplane ports are routed to front panel. However the ASIC
+  // returns all ports so we need to create lane mappings for them
+  for (auto lane = 0, port = 256; lane < 192; ++lane, ++port) {
+    // 4 lanes of each Blackhawk core are routed to one of the
+    // 2 Indus chips on Makalu platform
+    if (lane % 4 == 0) {
+      phy::DataPlanePhyChip chip;
+      // TODO - Make BC core id map front panel mapping. So BC0 becomes
+      // first front panel port
+      chip.name() = folly::to<std::string>("BC", lane / 4);
+      chip.type() = phy::DataPlanePhyChipType::IPHY;
+      chip.physicalID() = lane / 4;
+      platformMapping.chips()->push_back(chip);
+    }
+    cfg::PlatformPortEntry portEntry;
+    portEntry.mapping()->id() = port;
+    // FIXME - fab port id does not match front panel port
+    portEntry.mapping()->name() = folly::to<std::string>("fab1/", port, "/1");
+    portEntry.mapping()->controllingPort() = port;
+    phy::PinConnection pinConnect;
+    phy::PinID asicSerdesPinId;
+    asicSerdesPinId.chip() = *platformMapping.chips()->back().name();
+    asicSerdesPinId.lane() = lane % 4;
+    pinConnect.a() = asicSerdesPinId;
+    // FIXME - get z end and polarity swap info
+    portEntry.mapping()->pins()->push_back(pinConnect);
+    portEntry.mapping()->portType() = cfg::PortType::FABRIC_PORT;
+    cfg::PlatformPortConfig portConfig;
+    portConfig.subsumedPorts() = std::vector<int32_t>({port});
+    phy::PinConfig pinConfig;
+    pinConfig.id() = asicSerdesPinId;
+    phy::PortPinConfig portPinConfig;
+    portPinConfig.iphy()->push_back(pinConfig);
+    portConfig.pins() = portPinConfig;
+    portEntry.supportedProfiles()->insert(
+        {cfg::PortProfileID::PROFILE_53POINT125G_1_PAM4_RS545_COPPER,
+         portConfig});
+    platformMapping.ports()->emplace(port, std::move(portEntry));
+  }
+  // Fill in supported profiles
+  cfg::PlatformPortConfigFactor configFactor;
+  configFactor.profileID() =
+      cfg::PortProfileID::PROFILE_53POINT125G_1_PAM4_RS545_COPPER;
+  phy::ProfileSideConfig profileSideConfig;
+  profileSideConfig.numLanes() = 1;
+  profileSideConfig.modulation() = phy::IpModulation::PAM4;
+  // TODO - set to RS545 fec
+  profileSideConfig.fec() = phy::FecMode::NONE;
+  profileSideConfig.medium() = TransmitterTechnology::COPPER;
+
+  phy::PortProfileConfig profileConfig;
+  profileConfig.speed() = cfg::PortSpeed::FIFTYTHREEPOINTONETWOFIVEG;
+  profileConfig.iphy() = profileSideConfig;
+
+  cfg::PlatformPortProfileConfigEntry portProfileConfigEntry;
+  portProfileConfigEntry.factor() = configFactor;
+  portProfileConfigEntry.profile() = profileConfig;
+  platformMapping.platformSupportedProfiles()->push_back(
+      portProfileConfigEntry);
+
+  return platformMapping;
 }
-)";
 } // namespace
 
-namespace facebook {
-namespace fboss {
 MakaluPlatformMapping::MakaluPlatformMapping()
-    : PlatformMapping(kJsonPlatformMappingStr) {}
+    : PlatformMapping(buildMapping()) {}
 
-} // namespace fboss
-} // namespace facebook
+} // namespace facebook::fboss
