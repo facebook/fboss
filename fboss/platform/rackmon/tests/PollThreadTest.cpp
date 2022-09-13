da8a6f14dc78723d3de50c3fa1aaf5d07d76bd20
Amithash Prasad <amithash@fb.com>
2022-09-12 18:10:53 -0700

    rackmon2: Fix rare unit test failure.
    Summary:
    Yeah, dont write unit tests which depend on timing
    or system load. But thats the only way to test this
    one unit after which all things depending on it
    can be tested using mocks.
    
    So, make the expectation between(2,3) instead of exactly
    2. This allows us wiggle room when the test took just
    a bit longer to stop and we had an additional call.
    
    Imported from: D39297407
    
    Reviewed By: somasun
    
    Differential Revision: D39323246
    
    fbshipit-source-id: cc9e61a5306f14c7b77b5f950b39150be918491e

--- fboss/platform/rackmon/tests/PollThreadTest.cpp
+++ fboss/platform/rackmon/tests/PollThreadTest.cpp
@@ -13,7 +13,7 @@ struct Tester {
 
 TEST(PollThreadTest, basic) {
   Tester test_obj;
-  EXPECT_CALL(test_obj, do_stuff()).Times(2);
+  EXPECT_CALL(test_obj, do_stuff()).Times(Between(2, 3));
   PollThread<Tester> thr(&Tester::do_stuff, &test_obj, 1s);
   thr.start();
   // This is a polling function, so we need to sleep to check
