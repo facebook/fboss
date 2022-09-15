2e1e0067446d42c3537e989a3ac31d1ba8a4fdd7
Joan Olguy Canéus <joancaneus@fb.com>
2022-09-15 00:35:32 -0700

    Modify version logic in kmod script
    Summary:
    we had a request from DC push team stating that they must have a way
    to pull existing kernel version. Therefore, we modify the kmod script so that
    we can read the kmod version on the fly from the remote server
    
    Reviewed By: tao-ren
    
    Differential Revision: D39486583
    
    fbshipit-source-id: 161ce1187c38739cf2fa956c0457078e0c130718

--- fboss/platform/scripts/kmods_upgrade.sh
+++ fboss/platform/scripts/kmods_upgrade.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 # Copyright 2022-present Facebook. All Rights Reserved.
 
+# contruct the RPM remote server URL
+KERNEL_VER=$(uname -r)
+YUM_KERNEL_DIR=$(echo "${KERNEL_VER}" | cut -d '_' -f 1,2)
+RPM_REMOTE_SERVER_URL="https://yum.rftw0.facebook.com/yum/centos/kernel/${YUM_KERNEL_DIR}/x86_64/RPMS/"
+
 stop_chef(){
   # Stopping chef to perform kernel upgrades. Default is at least 1 hour
 
@@ -63,8 +68,24 @@ install_bsp_kmods(){
   old_rpm=$(rpm -qa | grep arista_darwin_bsp)
   rpm -e "$old_rpm"
 
-  fbk_ver=$(uname -r)
-  dnf install -y "arista_darwin_bsp-${fbk_ver}-${KMOD_RPM_VER}"
+  # Pull the Latest kmod rpm for the current running kernel version
+
+  # Get the context of the remote server into a file
+  wget "${RPM_REMOTE_SERVER_URL}" -q -O - | grep "arista_darwin" > /tmp/remote_server_output.txt
+
+  # remove the .x86_64.rpm since it won't be part of the rpm command
+  sed -i -e 's/.x86_64.rpm//' /tmp/remote_server_output.txt
+
+  # Sort the content of the file per date (both calendar date and time) and pull the rpm
+  # for the sorting one k2 full the column with the day, month, year first
+  # then  k3 pick the column with the hours and mins
+  # then the most recent rpm file will be at the bottom
+  rpm=$(sort -k2 -k3 /tmp/remote_server_output.txt | cut -d '>' -f 1 | cut -d '"' -f 2 | tail -1)
+
+  #remove the tmp file since it's no longer needed
+  rm -rf /tmp/remote_server_output.txt
+
+  dnf install -y "${rpm}"
 }
 
 reload_bsp_kmods(){
@@ -112,52 +133,44 @@ start_chef(){
 }
 
 
-if [ "$#" -eq 1 ]; then
-  KMOD_RPM_VER="$1"
+# Drain the swich since Kernel upgrade is being treated as disruptive
+# TODO: Figure out what is the command to drain switch on Darwin
 
-  # Drain the swich since Kernel upgrade is being treated as disruptive
-  # TODO: Figure out what is the command to drain switch on Darwin
+# stop chef so that it doesn't restart platform services
+stop_chef
 
-  # stop chef so that it doesn't restart platform services
-  stop_chef
+# Stop platform services
+stop_platform_services
 
-  # Stop platform services
-  stop_platform_services
+# increase fan_speed
+fan_speed_increase
 
-  # increase fan_speed
-  fan_speed_increase
+# stop forwarding stack services
+stop_forwarding_stack_services
 
-  # stop forwarding stack services
-  stop_forwarding_stack_services
+# stop watchdog
+stop_watchdog
 
-  # stop watchdog
-  stop_watchdog
+# unload bsp kernel Modules
+unload_bsp_kmods
 
-  # unload bsp kernel Modules
-  unload_bsp_kmods
+# install new bsp kmods
+install_bsp_kmods "$KMOD_RPM_VER"
 
-  # install new bsp kmods
-  install_bsp_kmods "$KMOD_RPM_VER"
+# reload bsp_kmods
+reload_bsp_kmods
 
-  # reload bsp_kmods
-  reload_bsp_kmods
+# start watchdog service
+start_watchdog
 
-  # start watchdog service
-  start_watchdog
+# start fowarding stack services
+start_forwarding_stack_services
 
-  # start fowarding stack services
-  start_forwarding_stack_services
+# start platform services
+start_platform_services
 
-  # start platform services
-  start_platform_services
+# start chef
+start_chef
 
-  # start chef
-  start_chef
-
-  # undrain the switch
-  # TODO: Figure out the command to undrain the switch
-else
-  echo "Wrong usage"
-  echo "proper usage: kmods-updade.sh <KMOD_RPM_VER>"
-  echo "where KMOD_RPM_VER is the latest kmod rpm version"
-fi
+# undrain the switch
+# TODO: Figure out the command to undrain the switch
