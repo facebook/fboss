7e042fb7f06c84a067f2643723ace3f8b24e0d5f
Rajan Kumar <rajank@fb.com>
2022-09-19 14:20:18 -0700

    Added sai_phy_util debug CLI for PHY register read/writes
    Summary:
    Added this debug CLI
    
      sai_phy_util --read_reg --port <portName> --mdio <mdioAddr> --dev <deviceId> --offset <registerOffset>
    
      sai_phy_util --write_reg --port <portName> --mdio <mdioAddr> --dev <deviceId> --offset <registerOffset> --val <dataValue>
    
    Differential Revision: D39092963
    
    fbshipit-source-id: 900b906d30fd01b8186a82d7bf1bfa01cd4bfbd8

--- fboss/util/sai_phy_util.cpp
+++ fboss/util/sai_phy_util.cpp
@@ -8,6 +8,18 @@ using namespace apache::thrift;
 
 DEFINE_bool(phy_port_info, false, "Print PHY port SAI info, use with --port");
 DEFINE_string(port, "", "Switch port");
+DEFINE_bool(
+    read_reg,
+    false,
+    "Read PHY register, use with --port --mdio --dev --offset");
+DEFINE_bool(
+    write_reg,
+    false,
+    "Write PHY register, use with --port --mdio --dev --offset --val");
+DEFINE_int32(mdio, -1, "PHY Mdio address");
+DEFINE_int32(dev, -1, "Clause45 Device Id for register access");
+DEFINE_int32(offset, -1, "Register offset");
+DEFINE_int32(val, -1, "Value to be written");
 
 void printPhyPortInfo(QsfpServiceAsyncClient* qsfpHandler) {
   if (FLAGS_port == "") {
@@ -16,6 +28,33 @@ void printPhyPortInfo(QsfpServiceAsyncClient* qsfpHandler) {
   }
   std::string output;
   qsfpHandler->sync_getSaiPortInfo(output, FLAGS_port);
+
+  printf("%s\n", output.c_str());
+}
+
+void saiPhyRegisterAccess(QsfpServiceAsyncClient* qsfpHandler, bool opRead) {
+  if (FLAGS_port == "") {
+    printf("Port name is required\n");
+    return;
+  }
+  if (FLAGS_mdio == -1 || FLAGS_dev == -1 || FLAGS_offset == -1) {
+    printf("MDIO address, Device Id and Register offset is required\n");
+    return;
+  }
+  if (!opRead && FLAGS_val == -1) {
+    printf("Register value is required\n");
+    return;
+  }
+
+  std::string output;
+  qsfpHandler->sync_saiPhyRegisterAccess(
+      output,
+      FLAGS_port,
+      opRead,
+      FLAGS_mdio,
+      FLAGS_dev,
+      FLAGS_offset,
+      FLAGS_val);
 
   printf("%s\n", output.c_str());
 }
@@ -32,6 +71,13 @@ int main(int argc, char* argv[]) {
     printPhyPortInfo(client.get());
     return 0;
   }
-
+  if (FLAGS_read_reg) {
+    saiPhyRegisterAccess(client.get(), true);
+    return 0;
+  }
+  if (FLAGS_write_reg) {
+    saiPhyRegisterAccess(client.get(), false);
+    return 0;
+  }
   return 0;
 }
