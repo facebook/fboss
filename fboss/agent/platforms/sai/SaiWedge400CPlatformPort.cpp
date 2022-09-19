ff791e9ad6bb170a7b40c947145d94b2ac4e7c64
Jasmeet Bagga <jasmeetbagga@fb.com>
2022-09-19 11:15:03 -0700

    Pull attached core and port index from 400C platform port
    Summary:
    We need to get this info into platform mapping,
    but till then pull it out from ifg and lane numbers
    
    Differential Revision: D39611834
    
    fbshipit-source-id: 8932ed9fc4f69cb5b15f0629e782f13a202c4f3e

--- fboss/agent/platforms/sai/SaiWedge400CPlatformPort.cpp
+++ fboss/agent/platforms/sai/SaiWedge400CPlatformPort.cpp
@@ -10,6 +10,8 @@
 #include "fboss/agent/platforms/sai/SaiWedge400CPlatformPort.h"
 #include "fboss/agent/platforms/common/utils/Wedge400LedUtils.h"
 
+#include <folly/Conv.h>
+
 DEFINE_bool(skip_led_programming, false, "Skip programming LED");
 
 namespace facebook::fboss {
@@ -41,4 +43,23 @@ uint32_t SaiWedge400CPlatformPort::getCurrentLedState() const {
   return static_cast<uint32_t>(currentLedState_);
 }
 
+std::optional<int> SaiWedge400CPlatformPort::getAttachedCoreId() const {
+  return getFirstLaneAndChip().second;
+}
+std::optional<int> SaiWedge400CPlatformPort::getCorePortIndex() const {
+  return getFirstLaneAndChip().first;
+}
+
+std::pair<int32_t, int32_t> SaiWedge400CPlatformPort::getFirstLaneAndChip()
+    const {
+  const auto& mapping = getPlatformPortEntry().mapping();
+  std::map<int32_t, std::string> lane2Chip;
+  for (const auto& pin : *mapping->pins()) {
+    lane2Chip.emplace(*pin.a()->lane(), *pin.a()->chip());
+  }
+  auto [firstLane, chipStr] = *lane2Chip.begin();
+  // Move past "IFG"
+  chipStr = chipStr.substr(std::string("IFG").size());
+  return {firstLane, folly::to<int32_t>(chipStr)};
+}
 } // namespace facebook::fboss
