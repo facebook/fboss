ba425eccbc2597da0bb6c160107ccd38c644eda3
Harshit Gulati <hgulati@fb.com>
2022-09-20 08:21:20 -0700

    Added a BspPlatformMapping for Kamet
    Summary: This diff is the first diff to support Kamet, a new platform. This diff creates a KametBspPlatformMapping class that derives off the base class BspPlatformMapping which stores all the information about how a transceiver needs to be managed from BSP.
    
    Differential Revision: D39605696
    
    fbshipit-source-id: c4bdd838d0841beb7f89bd912e348520b6897fc2

---
+++ fboss/lib/bsp/kamet/KametBspPlatformMapping.cpp
@@ -0,0 +1,72 @@
+// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+#include "fboss/lib/bsp/kamet/KametBspPlatformMapping.h"
+#include "fboss/lib/bsp/BspPlatformMapping.h"
+#include "fboss/lib/bsp/gen-cpp2/bsp_platform_mapping_types.h"
+
+using namespace facebook::fboss;
+
+namespace {
+// TODO: Move this mapping generation to cfgr
+static BspPlatformMappingThrift buildKametPlatformMapping() {
+  BspPlatformMappingThrift mapping;
+  BspPimMapping pimMapping;
+  pimMapping.pimID() = 1;
+
+  for (int tcvr = 1; tcvr <= 48; tcvr++) {
+    BspTransceiverMapping tcvrMapping;
+    tcvrMapping.tcvrId() = tcvr;
+
+    BspResetPinInfo resetInfo;
+    BspPresencePinInfo presenceInfo;
+    std::string resetPath = "";
+    std::string presencePath = "";
+    if (tcvr <= 12) {
+      presencePath = "/sys/bus/i2c/devices/2-0032/cpld_qsfpdd_port_status_";
+      resetPath = "/sys/bus/i2c/devices/2-0032/cpld_qsfpdd_port_config_";
+    } else if (tcvr <= 24) {
+      presencePath = "/sys/bus/i2c/devices/2-0033/cpld_qsfpdd_port_status_";
+      resetPath = "/sys/bus/i2c/devices/2-0033/cpld_qsfpdd_port_config_";
+    } else if (tcvr <= 36) {
+      presencePath = "/sys/bus/i2c/devices/2-0030/cpld_qsfpdd_port_status_";
+      resetPath = "/sys/bus/i2c/devices/2-0030/cpld_qsfpdd_port_config_";
+    } else {
+      presencePath = "/sys/bus/i2c/devices/2-0031/cpld_qsfpdd_port_status_";
+      resetPath = "/sys/bus/i2c/devices/2-0031/cpld_qsfpdd_port_config_";
+    }
+    resetPath += std::to_string((tcvr - 1) % 12);
+    presencePath += std::to_string((tcvr - 1) % 12);
+    resetInfo.sysfsPath() = resetPath;
+    presenceInfo.sysfsPath() = presencePath;
+    resetInfo.mask() = 0x1;
+    presenceInfo.mask() = 0x2;
+    BspTransceiverAccessControllerInfo accessControl;
+    accessControl.controllerId() = fmt::format("accessController-{}", tcvr);
+    accessControl.type() = ResetAndPresenceAccessType::CPLD;
+    accessControl.reset() = resetInfo;
+    accessControl.presence() = presenceInfo;
+    tcvrMapping.accessControl() = accessControl;
+
+    BspTransceiverIOControllerInfo tcvrIO;
+    tcvrIO.controllerId() = fmt::format("ioController-{}", tcvr);
+    tcvrIO.type() = TransceiverIOType::I2C;
+    tcvrIO.devicePath() = "/dev/i2c-" + std::to_string(tcvr + 20);
+    tcvrMapping.io() = tcvrIO;
+
+    pimMapping.tcvrMapping()[tcvr] = tcvrMapping;
+  }
+  mapping.pimMapping()[1] = pimMapping;
+  return mapping;
+}
+
+} // namespace
+
+namespace facebook {
+namespace fboss {
+
+// TODO: Use pre generated bsp platform mapping from cfgr
+KametBspPlatformMapping::KametBspPlatformMapping()
+    : BspPlatformMapping(buildKametPlatformMapping()) {}
+
+} // namespace fboss
+} // namespace facebook
---
+++ fboss/lib/bsp/kamet/KametBspPlatformMapping.h
@@ -0,0 +1,14 @@
+// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+#pragma once
+
+#include "fboss/lib/bsp/BspPlatformMapping.h"
+
+namespace facebook::fboss {
+
+class KametBspPlatformMapping : public BspPlatformMapping {
+ public:
+  KametBspPlatformMapping();
+};
+
+} // namespace facebook::fboss
