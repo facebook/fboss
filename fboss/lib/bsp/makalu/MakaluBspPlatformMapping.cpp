208f783f98cb2e1ca3ee70b4a1c980e2f26495a1
Harshit Gulati <hgulati@fb.com>
2022-09-22 17:14:21 -0700

    Create a BspPlatformMapping for Makalu
    Summary: as titled
    
    Differential Revision: D39721959
    
    fbshipit-source-id: 73b3e6bd2a5f50e60016a0b647956e2138654483

---
+++ fboss/lib/bsp/makalu/MakaluBspPlatformMapping.cpp
@@ -0,0 +1,101 @@
+// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+#include "fboss/lib/bsp/makalu/MakaluBspPlatformMapping.h"
+#include "fboss/lib/bsp/BspPlatformMapping.h"
+#include "fboss/lib/bsp/gen-cpp2/bsp_platform_mapping_types.h"
+
+using namespace facebook::fboss;
+
+namespace {
+// TODO: Move this mapping generation to cfgr
+static BspPlatformMappingThrift buildMakaluPlatformMapping() {
+  BspPlatformMappingThrift mapping;
+  BspPimMapping pimMapping;
+  pimMapping.pimID() = 1;
+
+  for (int tcvr = 1; tcvr <= 76; tcvr++) {
+    BspTransceiverMapping tcvrMapping;
+    tcvrMapping.tcvrId() = tcvr;
+
+    BspResetPinInfo resetInfo;
+    BspPresencePinInfo presenceInfo;
+    std::string resetPath = "";
+    std::string presencePath = "";
+    if (tcvr <= 8) {
+      presencePath = "/sys/bus/i2c/devices/1-0031/cpld_qsfpdd_intr_present_0";
+      resetPath = "/sys/bus/i2c/devices/1-0031/cpld_qsfpdd_reset_0";
+    } else if (tcvr <= 16) {
+      presencePath = "/sys/bus/i2c/devices/1-0031/cpld_qsfpdd_intr_present_1";
+      resetPath = "/sys/bus/i2c/devices/1-0031/cpld_qsfpdd_reset_1";
+    } else if (tcvr <= 18) {
+      presencePath = "/sys/bus/i2c/devices/1-0031/cpld_qsfpdd_intr_present_2";
+      resetPath = "/sys/bus/i2c/devices/1-0031/cpld_qsfpdd_reset_2";
+    } else if (tcvr <= 26) {
+      presencePath = "/sys/bus/i2c/devices/1-0032/cpld_qsfpdd_intr_present_0";
+      resetPath = "/sys/bus/i2c/devices/1-0032/cpld_qsfpdd_reset_0";
+    } else if (tcvr <= 34) {
+      presencePath = "/sys/bus/i2c/devices/1-0032/cpld_qsfpdd_intr_present_1";
+      resetPath = "/sys/bus/i2c/devices/1-0032/cpld_qsfpdd_reset_1";
+    } else if (tcvr <= 36) {
+      presencePath = "/sys/bus/i2c/devices/1-0032/cpld_qsfpdd_intr_present_2";
+      resetPath = "/sys/bus/i2c/devices/1-0032/cpld_qsfpdd_reset_2";
+    } else if (tcvr <= 44) {
+      presencePath = "/sys/bus/i2c/devices/30-0033/cpld_qsfpdd_intr_present_0";
+      resetPath = "/sys/bus/i2c/devices/30-0033/cpld_qsfpdd_reset_0";
+    } else if (tcvr <= 52) {
+      presencePath = "/sys/bus/i2c/devices/30-0033/cpld_qsfpdd_intr_present_1";
+      resetPath = "/sys/bus/i2c/devices/30-0033/cpld_qsfpdd_reset_1";
+    } else if (tcvr <= 56) {
+      presencePath = "/sys/bus/i2c/devices/30-0033/cpld_qsfpdd_intr_present_2";
+      resetPath = "/sys/bus/i2c/devices/30-0033/cpld_qsfpdd_reset_2";
+    } else if (tcvr <= 64) {
+      presencePath = "/sys/bus/i2c/devices/30-0034/cpld_qsfpdd_intr_present_0";
+      resetPath = "/sys/bus/i2c/devices/30-0034/cpld_qsfpdd_reset_0";
+    } else if (tcvr <= 72) {
+      presencePath = "/sys/bus/i2c/devices/30-0034/cpld_qsfpdd_intr_present_1";
+      resetPath = "/sys/bus/i2c/devices/30-0034/cpld_qsfpdd_reset_1";
+    } else {
+      presencePath = "/sys/bus/i2c/devices/30-0034/cpld_qsfpdd_intr_present_2";
+      resetPath = "/sys/bus/i2c/devices/30-0034/cpld_qsfpdd_reset_2";
+    }
+    int tcvrIDForMask = tcvr >= 37 ? (tcvr - 37) : tcvr;
+    resetInfo.sysfsPath() = resetPath;
+    presenceInfo.sysfsPath() = presencePath;
+    resetInfo.mask() = ~(1 << ((tcvrIDForMask - 1) % 8));
+    presenceInfo.mask() = 1 << (tcvrIDForMask % 8);
+    BspTransceiverAccessControllerInfo accessControl;
+    accessControl.controllerId() = fmt::format("accessController-{}", tcvr);
+    accessControl.type() = ResetAndPresenceAccessType::CPLD;
+    accessControl.reset() = resetInfo;
+    accessControl.presence() = presenceInfo;
+    tcvrMapping.accessControl() = accessControl;
+
+    BspTransceiverIOControllerInfo tcvrIO;
+    tcvrIO.controllerId() = fmt::format("ioController-{}", tcvr);
+    tcvrIO.type() = TransceiverIOType::I2C;
+    std::string devPath = "";
+    if (tcvr <= 36) {
+      devPath = "/dev/i2c-" + std::to_string(tcvr + 72);
+    } else {
+      devPath = "/dev/i2c-" + std::to_string(tcvr - 37 + 33);
+    }
+    tcvrIO.devicePath() = devPath;
+    tcvrMapping.io() = tcvrIO;
+
+    pimMapping.tcvrMapping()[tcvr] = tcvrMapping;
+  }
+  mapping.pimMapping()[1] = pimMapping;
+  return mapping;
+}
+
+} // namespace
+
+namespace facebook {
+namespace fboss {
+
+// TODO: Use pre generated bsp platform mapping from cfgr
+MakaluBspPlatformMapping::MakaluBspPlatformMapping()
+    : BspPlatformMapping(buildMakaluPlatformMapping()) {}
+
+} // namespace fboss
+} // namespace facebook
---
+++ fboss/lib/bsp/makalu/MakaluBspPlatformMapping.h
@@ -0,0 +1,16 @@
+// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+#pragma once
+
+#include "fboss/lib/bsp/BspPlatformMapping.h"
+
+namespace facebook {
+namespace fboss {
+
+class MakaluBspPlatformMapping : public BspPlatformMapping {
+ public:
+  MakaluBspPlatformMapping();
+};
+
+} // namespace fboss
+} // namespace facebook
