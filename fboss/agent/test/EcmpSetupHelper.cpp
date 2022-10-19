bfe2185942e70be643ccd86640cb2ebf1f831cde
Jasmeet Bagga <jasmeetbagga@meta.com>
2022-10-19 09:04:50 -0700

    Fix oss build
    Summary:
    Assure compiler we are not doing a implicit
    narrowing conversion while calculating sys port id
    
    Reviewed By: shri-khare
    
    Differential Revision:
    D40502327
    
    LaMa Project: L1125642
    
    fbshipit-source-id: 5dde6b1c11667cf32f8d865be07112837a20e560

--- fboss/agent/test/EcmpSetupHelper.cpp
+++ fboss/agent/test/EcmpSetupHelper.cpp
@@ -261,7 +261,10 @@ std::optional<InterfaceID> BaseEcmpSetupHelper<AddrT, NextHopT>::getInterface(
       return std::nullopt;
     }
     // TODO: calculate sysport id using sys port range begin
-    SystemPortID sysPortId{port.intID() + kSystemPortBase};
+    SystemPortID sysPortId{
+        // static_cast to avoid spurious narrowing conversion
+        // compiler warning. PortID is just 16 bits
+        static_cast<int64_t>(port.intID()) + kSystemPortBase};
     if (auto intf = state->getInterfaces()->getInterfaceIf(
             InterfaceID(static_cast<int>(sysPortId)))) {
       return intf->getID();
