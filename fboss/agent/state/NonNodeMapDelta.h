cf7d4db00a2666131ebff27d8db5cadd84cccd8a
Jasmeet Bagga <jasmeetbagga@meta.com>
2022-10-20 11:46:35 -0700

    Build NonNodeMapDelta class + UTs
    Summary:
    Add a class + UTs for computing deltas
    over Non node map classes. It essentially use the
    abstractions built in previous diffs viz.
    DeltaValue and DeltaValueIterator. Where DeltaValue
    stores a pointer to old and new values vs shared_ptr.
    DeltaValueIterator similarly is parameterized with
    the right types.
    
    I will do one more pass to try and abstract out a
    common MapDelta class from NodeMapDelta and NonNodeMapDelta.
    
    Reviewed By: phshaikh
    
    Differential Revision:
    D40500609
    
    LaMa Project: L1125642
    
    fbshipit-source-id: 07a7be10ea1d7ed3be6b97b7431255bc4bb764ad

---
+++ fboss/agent/state/NonNodeMapDelta.h
@@ -0,0 +1,56 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+#pragma once
+
+#include <fboss/agent/state/NodeMapDelta.h>
+
+namespace facebook::fboss {
+
+template <
+    typename MAP,
+    typename VALUE =
+        DeltaValue<typename MAP::mapped_type, const typename MAP::mapped_type*>>
+class NonNodeMapDelta {
+ public:
+  using MapType = MAP;
+  using Node = typename MAP::mapped_type;
+  using NodeWrapper = typename VALUE::NodeWrapper;
+  struct NodeMapExtractor {
+    static const auto& getKey(typename MAP::const_iterator i) {
+      return i->first;
+    }
+    static auto getValue(typename MAP::const_iterator i) {
+      return &i->second;
+    }
+  };
+  using Iterator = DeltaValueIterator<MAP, VALUE, NodeMapExtractor>;
+
+  NonNodeMapDelta(const MAP* oldMap, const MAP* newMap)
+      : old_(oldMap), new_(newMap) {}
+
+  /*
+   * Return an iterator pointing to the first change.
+   */
+  Iterator begin() const {
+    return Iterator(old_, old_->begin(), new_, new_->begin());
+  }
+
+  /*
+   * Return an iterator pointing just past the last change.
+   */
+  Iterator end() const {
+    return Iterator(old_, old_->end(), new_, new_->end());
+  }
+
+ private:
+  const MAP *old_, *new_;
+};
+
+} // namespace facebook::fboss
---
+++ fboss/agent/state/tests/NonNodeMapDeltaTests.cpp
@@ -0,0 +1,89 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#include "fboss/agent/state/DeltaFunctions.h"
+#include "fboss/agent/state/NonNodeMapDelta.h"
+
+#include <gtest/gtest.h>
+
+using namespace facebook::fboss;
+
+class NonNodeMapDeltaTest : public ::testing::Test {
+ public:
+  void SetUp() override {
+    added.clear();
+    removed.clear();
+    changed.clear();
+  }
+  void computeDelta(
+      const std::map<int, std::string>& oldMap,
+      const std::map<int, std::string>& newMap) {
+    DeltaFunctions::forEachChanged(
+        NonNodeMapDelta(&oldMap, &newMap),
+        [&](auto oldStr, auto newStr) { changed.push_back(*newStr); },
+        [&](auto addStr) { added.push_back(*addStr); },
+        [&](auto rmStr) { removed.push_back(*rmStr); });
+  }
+  std::vector<std::string> added, removed, changed;
+};
+
+TEST_F(NonNodeMapDeltaTest, addRemoveChange) {
+  std::map<int, std::string> oldMap{{1, "one"}, {2, "too"}};
+  std::map<int, std::string> newMap{{2, "two"}, {3, "three"}};
+  computeDelta(oldMap, newMap);
+  EXPECT_EQ(added, std::vector<std::string>({newMap[3]}));
+  EXPECT_EQ(removed, std::vector<std::string>({oldMap[1]}));
+  EXPECT_EQ(changed, std::vector<std::string>({newMap[2]}));
+}
+
+TEST_F(NonNodeMapDeltaTest, addRemove) {
+  std::map<int, std::string> oldMap{{1, "one"}};
+  std::map<int, std::string> newMap{{2, "two"}};
+  computeDelta(oldMap, newMap);
+  EXPECT_EQ(added, std::vector<std::string>({newMap[2]}));
+  EXPECT_EQ(removed, std::vector<std::string>({oldMap[1]}));
+  EXPECT_EQ(changed.size(), 0);
+}
+
+TEST_F(NonNodeMapDeltaTest, addOnly) {
+  std::map<int, std::string> oldMap{{1, "one"}};
+  std::map<int, std::string> newMap{{1, "one"}, {2, "two"}};
+  computeDelta(oldMap, newMap);
+  EXPECT_EQ(added, std::vector<std::string>({newMap[2]}));
+  EXPECT_EQ(removed.size(), 0);
+  EXPECT_EQ(changed.size(), 0);
+}
+
+TEST_F(NonNodeMapDeltaTest, removeOnly) {
+  std::map<int, std::string> oldMap{{1, "one"}};
+  std::map<int, std::string> newMap;
+  computeDelta(oldMap, newMap);
+  EXPECT_EQ(added.size(), 0);
+  EXPECT_EQ(removed, std::vector<std::string>({oldMap[1]}));
+  EXPECT_EQ(changed.size(), 0);
+}
+
+TEST_F(NonNodeMapDeltaTest, changeOnly) {
+  std::map<int, std::string> oldMap{{1, "1"}};
+  std::map<int, std::string> newMap{{1, "one"}};
+  computeDelta(oldMap, newMap);
+  EXPECT_EQ(added.size(), 0);
+  EXPECT_EQ(removed.size(), 0);
+  EXPECT_EQ(changed, std::vector<std::string>({newMap[1]}));
+}
+
+TEST_F(NonNodeMapDeltaTest, noChange) {
+  std::map<int, std::string> oldMap{{1, "one"}};
+  std::map<int, std::string> newMap(oldMap);
+  computeDelta(oldMap, newMap);
+  EXPECT_EQ(added.size(), 0);
+  EXPECT_EQ(removed.size(), 0);
+  EXPECT_EQ(changed.size(), 0);
+}
