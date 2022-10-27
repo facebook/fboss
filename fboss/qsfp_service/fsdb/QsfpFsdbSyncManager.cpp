7d108dde5ad576a0e42f9fec8077e92405cecdb4
Clement Cheung <clementcheung@meta.com>
2022-10-27 02:38:06 -0700

    Add QSFP FSDB stat syncer
    Reviewed By: harshitgulati18
    
    Differential Revision: D39903546
    
    fbshipit-source-id: 3b2b8087e1cad23b11de0e4fc005a56f21532ea9

--- fboss/qsfp_service/fsdb/QsfpFsdbSyncManager.cpp
+++ fboss/qsfp_service/fsdb/QsfpFsdbSyncManager.cpp
@@ -12,6 +12,7 @@
 
 #include "fboss/fsdb/common/Flags.h"
 #include "fboss/qsfp_service/if/gen-cpp2/qsfp_state_fatal_types.h"
+#include "fboss/qsfp_service/if/gen-cpp2/qsfp_stats_fatal_types.h"
 
 namespace facebook {
 namespace fboss {
@@ -22,30 +23,56 @@ QsfpFsdbSyncManager::QsfpFsdbSyncManager() {
         std::make_unique<fsdb::FsdbSyncManager<state::QsfpServiceData>>(
             "qsfp_service", getStatePath(), false, true);
   }
+  if (FLAGS_publish_stats_to_fsdb) {
+    statsSyncer_ = std::make_unique<fsdb::FsdbSyncManager<stats::QsfpStats>>(
+        "qsfp_service", getStatsPath(), true, false);
+  }
 }
 
 void QsfpFsdbSyncManager::start() {
   if (FLAGS_publish_state_to_fsdb) {
     stateSyncer_->start();
   }
+  if (FLAGS_publish_stats_to_fsdb) {
+    statsSyncer_->start();
+  }
 }
+
 void QsfpFsdbSyncManager::stop() {
   if (FLAGS_publish_state_to_fsdb) {
     stateSyncer_->stop();
   }
+  if (FLAGS_publish_stats_to_fsdb) {
+    statsSyncer_->stop();
+  }
 }
 
 void QsfpFsdbSyncManager::updateConfig(cfg::QsfpServiceConfig newConfig) {
-  if (FLAGS_publish_state_to_fsdb) {
-    stateSyncer_->updateState([newConfig =
-                                   std::move(newConfig)](const auto& in) {
-      auto out = in->clone();
-      out->template modify<state::qsfp_state_tags::strings::config>();
-      out->template ref<state::qsfp_state_tags::strings::config>()->fromThrift(
-          newConfig);
-      return out;
-    });
+  if (!FLAGS_publish_state_to_fsdb) {
+    return;
   }
+
+  stateSyncer_->updateState([newConfig = std::move(newConfig)](const auto& in) {
+    auto out = in->clone();
+    out->template modify<state::qsfp_state_tags::strings::config>();
+    out->template ref<state::qsfp_state_tags::strings::config>()->fromThrift(
+        newConfig);
+    return out;
+  });
+}
+
+void QsfpFsdbSyncManager::updateTcvrStats(std::map<int, TcvrStats>&& stats) {
+  if (!FLAGS_publish_stats_to_fsdb) {
+    return;
+  }
+
+  statsSyncer_->updateState([stats = std::move(stats)](const auto& in) {
+    auto out = in->clone();
+    out->template modify<stats::qsfp_stats_tags::strings::tcvrStats>();
+    out->template ref<stats::qsfp_stats_tags::strings::tcvrStats>()->fromThrift(
+        stats);
+    return out;
+  });
 }
 
 } // namespace fboss
--- fboss/qsfp_service/fsdb/QsfpFsdbSyncManager.h
+++ fboss/qsfp_service/fsdb/QsfpFsdbSyncManager.h
@@ -13,12 +13,15 @@
 #include "fboss/fsdb/client/FsdbSyncManager.h"
 #include "fboss/qsfp_service/if/gen-cpp2/qsfp_service_config_types.h"
 #include "fboss/qsfp_service/if/gen-cpp2/qsfp_state_types.h"
+#include "fboss/qsfp_service/if/gen-cpp2/qsfp_stats_types.h"
 
 namespace facebook {
 namespace fboss {
 
 class QsfpFsdbSyncManager {
  public:
+  using TcvrStatsMap = std::map<int32_t, TcvrStats>;
+
   QsfpFsdbSyncManager();
 
   static std::vector<std::string> getStatePath();
@@ -29,9 +32,11 @@ class QsfpFsdbSyncManager {
   void stop();
 
   void updateConfig(cfg::QsfpServiceConfig newConfig);
+  void updateTcvrStats(TcvrStatsMap&& stats);
 
  private:
   std::unique_ptr<fsdb::FsdbSyncManager<state::QsfpServiceData>> stateSyncer_;
+  std::unique_ptr<fsdb::FsdbSyncManager<stats::QsfpStats>> statsSyncer_;
 };
 
 } // namespace fboss
