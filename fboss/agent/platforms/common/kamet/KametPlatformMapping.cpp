b0a35a4ed04b957e788e9d2b4d96d6078535b4f8
Jasmeet Bagga <jasmeetbagga@meta.com>
2022-10-28 14:54:44 -0700

    Get rid of programmatic platform mapping gen for Kamet
    Summary:
    As titled. We already have this mapping from
    cfgr
    
    Differential Revision:
    D40790854
    
    LaMa Project: L1125642
    
    fbshipit-source-id: 53607de0b76aeab1f6c4463734b841524e98e5eb

--- fboss/agent/platforms/common/kamet/KametPlatformMapping.cpp
+++ fboss/agent/platforms/common/kamet/KametPlatformMapping.cpp
@@ -7431,73 +7431,7 @@ constexpr auto kJsonPlatformMappingStr = R"(
 }
 )";
 
-/*
-cfg::PlatformMapping buildMapping() {
-  cfg::PlatformMapping platformMapping;
-  for (auto i = 0; i < 192; ++i) {
-    // 4 lanes of each Blackhawk core are routed to one of the
-    // 2 Beas chips on Kamet platform
-    if (i % 4 == 0) {
-      phy::DataPlanePhyChip chip;
-      // TODO make BC core id map front panel mapping. So BC0 becomes
-      // first front panel port
-      chip.name() = folly::to<std::string>("BC", i / 4);
-      chip.type() = phy::DataPlanePhyChipType::IPHY;
-      chip.physicalID() = i / 4;
-      platformMapping.chips()->push_back(chip);
-    }
-    cfg::PlatformPortEntry portEntry;
-    portEntry.mapping()->id() = i;
-    // FIXME - fab port id does not match front panel port
-    portEntry.mapping()->name() = folly::to<std::string>("fab1/", i + 1, "/1");
-    portEntry.mapping()->controllingPort() = i;
-    phy::PinConnection pinConnect;
-    phy::PinID asicSerdesPinId;
-    asicSerdesPinId.chip() = *platformMapping.chips()->back().name();
-    asicSerdesPinId.lane() = i % 4;
-    pinConnect.a() = asicSerdesPinId;
-    // FIXME - get z end and polarity swap info
-    portEntry.mapping()->pins()->push_back(pinConnect);
-    portEntry.mapping()->portType() = cfg::PortType::FABRIC_PORT;
-    cfg::PlatformPortConfig portConfig;
-    portConfig.subsumedPorts() = std::vector<int32_t>({i});
-    phy::PinConfig pinConfig;
-    pinConfig.id() = asicSerdesPinId;
-    phy::PortPinConfig portPinConfig;
-    portPinConfig.iphy()->push_back(pinConfig);
-    portConfig.pins() = portPinConfig;
-    portEntry.supportedProfiles()->insert(
-        {cfg::PortProfileID::PROFILE_53POINT125G_1_PAM4_RS545_COPPER,
-         portConfig});
-    platformMapping.ports()->emplace(i, std::move(portEntry));
-  }
-  // Fill in supported profiles
-  cfg::PlatformPortConfigFactor configFactor;
-  configFactor.profileID() =
-      cfg::PortProfileID::PROFILE_53POINT125G_1_PAM4_RS545_COPPER;
-  phy::ProfileSideConfig profileSideConfig;
-  profileSideConfig.numLanes() = 1;
-  profileSideConfig.modulation() = phy::IpModulation::PAM4;
-  // TODO - set to RS545 fec
-  profileSideConfig.fec() = phy::FecMode::NONE;
-  profileSideConfig.medium() = TransmitterTechnology::COPPER;
-
-  phy::PortProfileConfig profileConfig;
-  profileConfig.speed() = cfg::PortSpeed::FIFTYTHREEPOINTONETWOFIVEG;
-  profileConfig.iphy() = profileSideConfig;
-
-  cfg::PlatformPortProfileConfigEntry portProfileConfigEntry;
-  portProfileConfigEntry.factor() = configFactor;
-  portProfileConfigEntry.profile() = profileConfig;
-  platformMapping.platformSupportedProfiles()->push_back(
-      portProfileConfigEntry);
-
-  return platformMapping;
 }
-*/
-
-} // namespace
-
 /*
  * Use configerator generated kJsonPlatformMappingStr by default.
  * Alternatively, we could programmatically generate the platform mappings
