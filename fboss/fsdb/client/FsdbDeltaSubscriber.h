2c5544e8c5fc577a22785872d8b09384dfa3da6e
Jasmeet Bagga <jasmeetbagga@meta.com>
2022-11-08 21:09:23 -0800

    Templatize FsdbDelta subscriber to support normal + ext subscriptions
    Summary: as titled
    
    Reviewed By: zechengh09
    
    Differential Revision:
    D41063815
    
    LaMa Project: L1125642
    
    fbshipit-source-id: 712030a13b666d4f5c2fb4bcdce817437ee5ef56

--- fboss/fsdb/client/FsdbDeltaSubscriber.h
+++ fboss/fsdb/client/FsdbDeltaSubscriber.h
@@ -6,21 +6,29 @@
 #include "fboss/fsdb/if/gen-cpp2/fsdb_oper_types.h"
 
 namespace facebook::fboss::fsdb {
-class FsdbDeltaSubscriber : public FsdbSubscriber<OperDelta, std::string> {
-  using BaseT = FsdbSubscriber<OperDelta, std::string>;
+
+template <typename SubUnit, typename PathElement>
+class FsdbDeltaSubscriberImpl : public FsdbSubscriber<SubUnit, PathElement> {
+  using BaseT = FsdbSubscriber<SubUnit, PathElement>;
   using SubUnitT = typename BaseT::SubUnitT;
 
  public:
   using FsdbOperDeltaUpdateCb = typename BaseT::FsdbSubUnitUpdateCb;
   using BaseT::BaseT;
-  ~FsdbDeltaSubscriber() override {
-    cancel();
+  ~FsdbDeltaSubscriberImpl() override {
+    BaseT::cancel();
   }
 
  private:
 #if FOLLY_HAS_COROUTINES && !defined(IS_OSS)
+  using StreamT = typename BaseT::StreamT;
+  using SubStreamT = typename BaseT::template SubStreamT<SubUnit>;
   folly::coro::Task<StreamT> setupStream() override;
   folly::coro::Task<void> serveStream(StreamT&& stream) override;
 #endif
 };
+
+using FsdbDeltaSubscriber = FsdbDeltaSubscriberImpl<OperDelta, std::string>;
+using FsdbDeltaExtSubscriber =
+    FsdbDeltaSubscriberImpl<OperSubDeltaUnit, ExtendedOperPath>;
 } // namespace facebook::fboss::fsdb
