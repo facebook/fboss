ce382e569f606bba39eb130620d7777874fba882
Jasmeet Bagga <jasmeetbagga@meta.com>
2022-11-12 23:38:18 -0800

    Add UTs for neighors added to interface
    Summary:
    As titled. Soon we will migrate all nbr tables from
    vlan to rifs, but till then add tests for both
    
    Reviewed By: nivinl
    
    Differential Revision: D41252030
    
    fbshipit-source-id: 8df0f94dd3499b465e4e253ae3118387f8569841

--- fboss/agent/test/EncapIndexAllocatorTest.cpp
+++ fboss/agent/test/EncapIndexAllocatorTest.cpp
@@ -58,6 +58,28 @@ class EncapIndexAllocatorTest : public ::testing::Test {
         NeighborEntryFields<folly::IPAddressV6>::fromThrift(nbr));
     return state;
   }
+  std::shared_ptr<SwitchState> addNeighborToInterface(
+      std::shared_ptr<SwitchState> state,
+      const folly::IPAddressV6& ip,
+      int64_t encapIdx) const {
+    auto firstIntf = state->getInterfaces()->cbegin()->second;
+    state::NeighborEntryFields nbr;
+    nbr.mac() = "02:00:00:00:00:01";
+    nbr.interfaceId() = static_cast<int>(firstIntf->getID());
+    nbr.ipaddress() = ip.str();
+    nbr.portId() =
+        PortDescriptor((*getSw()->getState()->getPorts()->begin())->getID())
+            .toThrift();
+    nbr.state() = state::NeighborState::Reachable;
+    nbr.encapIndex() = encapIdx;
+    auto nbrTable = firstIntf->getNdpTable()->toThrift();
+    nbrTable.insert({*nbr.ipaddress(), nbr});
+    auto interfaceMap = state->getInterfaces()->modify(&state);
+    auto interface = interfaceMap->getInterface(firstIntf->getID())->clone();
+    interface->setNdpTable(nbrTable);
+    interfaceMap->updateNode(interface);
+    return state;
+  }
 
   std::unique_ptr<HwTestHandle> handle;
   EncapIndexAllocator allocator;
@@ -95,6 +117,34 @@ TEST_F(EncapIndexAllocatorTest, vlanNeighbors) {
   // Fill the hole, next one should be after all the contiguously allocated
   // indices
   auto stateV3 = addNeighborToVlan(
+      stateV2,
+      folly::IPAddressV6{"2401:db00:2110:3001::0003"},
+      allocationStart() + 1);
+  EXPECT_EQ(
+      allocator.getNextAvailableEncapIdx(stateV3, getAsic()),
+      allocationStart() + 3);
+}
+
+TEST_F(EncapIndexAllocatorTest, intfNeighbors) {
+  auto stateV1 = addNeighborToInterface(
+      getSw()->getState(),
+      folly::IPAddressV6{"2401:db00:2110:3001::0002"},
+      allocationStart());
+  // Next encap idx allocation
+  EXPECT_EQ(
+      allocator.getNextAvailableEncapIdx(stateV1, getAsic()),
+      allocationStart() + 1);
+  // Add a neighbor while creating a hole
+  auto stateV2 = addNeighborToInterface(
+      stateV1,
+      folly::IPAddressV6{"2401:db00:2110:3001::0004"},
+      allocationStart() + 2);
+  EXPECT_EQ(
+      allocator.getNextAvailableEncapIdx(stateV2, getAsic()),
+      allocationStart() + 1);
+  // Fill the hole, next one should be after all the contiguously allocated
+  // indices
+  auto stateV3 = addNeighborToInterface(
       stateV2,
       folly::IPAddressV6{"2401:db00:2110:3001::0003"},
       allocationStart() + 1);
