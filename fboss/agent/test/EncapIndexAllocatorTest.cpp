1a0be33a79e14831b51e62280ba8439fc9ae6799
Jasmeet Bagga <jasmeetbagga@meta.com>
2022-11-12 23:38:18 -0800

    UTs for determining next available encap idx for vlan neighors
    Summary: As titled
    
    Reviewed By: nivinl
    
    Differential Revision: D41251921
    
    fbshipit-source-id: 32c3578d833013fdf163d97c0cf275a05ce647dc

--- fboss/agent/test/EncapIndexAllocatorTest.cpp
+++ fboss/agent/test/EncapIndexAllocatorTest.cpp
@@ -11,6 +11,7 @@
 #include "fboss/agent/hw/switch_asics/HwAsic.h"
 #include "fboss/agent/hw/switch_asics/MockAsic.h"
 #include "fboss/agent/hw/switch_asics/TomahawkAsic.h"
+#include "fboss/agent/state/NeighborEntry.h"
 #include "fboss/agent/state/SwitchState.h"
 #include "fboss/agent/test/HwTestHandle.h"
 #include "fboss/agent/test/TestUtils.h"
@@ -37,6 +38,27 @@ class EncapIndexAllocatorTest : public ::testing::Test {
         EncapIndexAllocator::kEncapIdxReservedForLoopbacks;
   }
 
+  std::shared_ptr<SwitchState> addNeighborToVlan(
+      std::shared_ptr<SwitchState> state,
+      const folly::IPAddressV6& ip,
+      int64_t encapIdx) const {
+    auto firstVlan = *state->getVlans()->begin();
+    state::NeighborEntryFields nbr;
+    nbr.mac() = "02:00:00:00:00:01";
+    nbr.interfaceId() = static_cast<int>(firstVlan->getInterfaceID());
+    nbr.ipaddress() = ip.str();
+    nbr.portId() =
+        PortDescriptor((*getSw()->getState()->getPorts()->begin())->getID())
+            .toThrift();
+    nbr.state() = state::NeighborState::Reachable;
+    nbr.encapIndex() = encapIdx;
+    auto nbrTable =
+        firstVlan->getNdpTable()->modify(firstVlan->getID(), &state);
+    nbrTable->addEntry(
+        NeighborEntryFields<folly::IPAddressV6>::fromThrift(nbr));
+    return state;
+  }
+
   std::unique_ptr<HwTestHandle> handle;
   EncapIndexAllocator allocator;
 };
@@ -51,4 +73,32 @@ TEST_F(EncapIndexAllocatorTest, firstAllocation) {
   EXPECT_EQ(
       allocator.getNextAvailableEncapIdx(getSw()->getState(), getAsic()),
       allocationStart());
+}
+
+TEST_F(EncapIndexAllocatorTest, vlanNeighbors) {
+  auto stateV1 = addNeighborToVlan(
+      getSw()->getState(),
+      folly::IPAddressV6{"2401:db00:2110:3001::0002"},
+      allocationStart());
+  // Next encap idx allocation
+  EXPECT_EQ(
+      allocator.getNextAvailableEncapIdx(stateV1, getAsic()),
+      allocationStart() + 1);
+  // Add a neighbor while creating a hole
+  auto stateV2 = addNeighborToVlan(
+      stateV1,
+      folly::IPAddressV6{"2401:db00:2110:3001::0004"},
+      allocationStart() + 2);
+  EXPECT_EQ(
+      allocator.getNextAvailableEncapIdx(stateV2, getAsic()),
+      allocationStart() + 1);
+  // Fill the hole, next one should be after all the contiguously allocated
+  // indices
+  auto stateV3 = addNeighborToVlan(
+      stateV2,
+      folly::IPAddressV6{"2401:db00:2110:3001::0003"},
+      allocationStart() + 1);
+  EXPECT_EQ(
+      allocator.getNextAvailableEncapIdx(stateV3, getAsic()),
+      allocationStart() + 3);
 }
