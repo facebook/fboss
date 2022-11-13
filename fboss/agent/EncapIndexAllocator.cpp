b49a8d863555b87013efa53bcb00dea81b2556ef
Jasmeet Bagga <jasmeetbagga@meta.com>
2022-11-12 23:38:18 -0800

    Code to find a free encap index in the permitted ASIC range
    Summary:
    As titled. Function in O(n) complexity where n is
    the number of indices already allocated. Thus its directly
    proportional to neighbor entries on switch, which aren't
    too many.
    
    Reviewed By: nivinl
    
    Differential Revision: D41250913
    
    fbshipit-source-id: b4216167ab4a5de2107c9b4719006b566c0d4a06

--- fboss/agent/EncapIndexAllocator.cpp
+++ fboss/agent/EncapIndexAllocator.cpp
@@ -4,22 +4,52 @@
 
 #include "fboss/agent/FbossError.h"
 #include "fboss/agent/hw/switch_asics/HwAsic.h"
+#include "fboss/agent/state/Interface.h"
+#include "fboss/agent/state/SwitchState.h"
+#include "fboss/agent/state/Vlan.h"
 
 namespace facebook::fboss {
 class HwAsic;
 class SwitchState;
 
 int64_t EncapIndexAllocator::getNextAvailableEncapIdx(
-    const std::shared_ptr<SwitchState>& /*state*/,
+    const std::shared_ptr<SwitchState>& state,
     const HwAsic& asic) {
   if (!asic.isSupported(HwAsic::Feature::RESERVED_ENCAP_INDEX_RANGE)) {
     throw FbossError(
         "Encap index allocation not supported on: ", asic.getAsicTypeStr());
   }
+  std::unordered_set<int64_t> allocatedIndices;
+
+  auto extractIndices = [&allocatedIndices](const auto& nbrTable) {
+    for (auto itr = nbrTable->cbegin(); itr != nbrTable->cend(); ++itr) {
+      if (itr->second->getEncapIndex()) {
+        allocatedIndices.insert(*itr->second->getEncapIndex());
+      }
+    }
+  };
+  std::for_each(
+      state->getVlans()->begin(),
+      state->getVlans()->end(),
+      [&](const auto& vlan) {
+        extractIndices(vlan->getArpTable());
+        extractIndices(vlan->getNdpTable());
+      });
+  std::for_each(
+      state->getInterfaces()->cbegin(),
+      state->getInterfaces()->cend(),
+      [&](const auto& idAndIntf) {
+        extractIndices(idAndIntf.second->getArpTable());
+        extractIndices(idAndIntf.second->getNdpTable());
+      });
   auto start = *asic.getReservedEncapIndexRange().minimum() +
       kEncapIdxReservedForLoopbacks;
-  // TODO - figure out a available encap index based on looking
-  // at neighbor tables in switch state
-  return start;
+  while (start <= *asic.getReservedEncapIndexRange().maximum()) {
+    if (allocatedIndices.find(start) == allocatedIndices.end()) {
+      return start;
+    }
+    start++;
+  }
+  throw FbossError("No more unallocated indices for: ", asic.getAsicTypeStr());
 }
 } // namespace facebook::fboss
