c8f644d032765b2639887b289d12e2155f753e5d
Rohit Puri <rohitpuri@meta.com>
2022-11-23 13:20:45 -0800

    UdfPacketMatcher tests.
    Summary: Enhance the UDF tests to do packet matcher configuration as well.
    
    Reviewed By: msomasundaran
    
    Differential Revision: D41397446
    
    fbshipit-source-id: 1e6cec81401fa244edbf3a3906047475def75e54

--- fboss/agent/state/tests/UdfTests.cpp
+++ fboss/agent/state/tests/UdfTests.cpp
@@ -10,31 +10,53 @@
 
 #include "fboss/agent/state/SwitchState.h"
 #include "fboss/agent/state/UdfConfig.h"
+#include "fboss/agent/state/UdfGroup.h"
 #include "fboss/agent/state/UdfGroupMap.h"
+#include "fboss/agent/state/UdfPacketMatcher.h"
+#include "fboss/agent/state/UdfPacketMatcherMap.h"
 #include "fboss/agent/test/TestUtils.h"
 
 #include <gtest/gtest.h>
 
+namespace {
+constexpr folly::StringPiece kPacketMatcherCfgName = "matchCfg_1";
+constexpr folly::StringPiece kPacketMatcherCfgName2 = "matchCfg_2";
+constexpr folly::StringPiece kUdfGroupCfgName1 = "foo1";
+constexpr folly::StringPiece kUdfGroupCfgName2 = "foo2";
+} // namespace
+
 using namespace facebook::fboss;
+
+using UdfGroupCfgList = std::vector<cfg::UdfGroup>;
+using UdfPacketMatcherCfgList = std::vector<cfg::UdfPacketMatcher>;
 
 std::shared_ptr<UdfGroup> createStateUdfGroup(
     const std::string& name,
     int fieldSize = 0) {
   cfg::UdfGroup udfGroupEntry;
-  cfg::UdfPacketMatcher matchCfg;
-
-  matchCfg.name() = "matchCfg_1";
-  matchCfg.l4PktType() = cfg::UdfMatchL4Type::UDF_L4_PKT_TYPE_UDP;
 
   udfGroupEntry.name() = name;
   udfGroupEntry.header() = cfg::UdfBaseHeaderType::UDF_L4_HEADER;
   udfGroupEntry.startOffsetInBytes() = 5;
   udfGroupEntry.fieldSizeInBytes() = fieldSize;
-  udfGroupEntry.udfPacketMatcherIds() = {"matchCfg_1"};
+  udfGroupEntry.udfPacketMatcherIds() = {kPacketMatcherCfgName.str()};
 
   auto udfEntry = std::make_shared<UdfGroup>(name);
   udfEntry->fromThrift(udfGroupEntry);
   return udfEntry;
+}
+
+cfg::UdfPacketMatcher makeCfgUdfPacketMatcherEntry(
+    std::string name,
+    int dstPort = 0) {
+  cfg::UdfPacketMatcher udfPkt;
+
+  udfPkt.name() = name;
+  udfPkt.l2PktType() = cfg::UdfMatchL2Type::UDF_L2_PKT_TYPE_ETH;
+  udfPkt.l3pktType() = cfg::UdfMatchL3Type::UDF_L3_PKT_TYPE_IPV6;
+
+  udfPkt.UdfL4DstPort() = dstPort;
+  return udfPkt;
 }
 
 cfg::UdfGroup makeCfgUdfGroupEntry(std::string name, int fieldSize = 0) {
@@ -44,50 +66,130 @@ cfg::UdfGroup makeCfgUdfGroupEntry(std::string name, int fieldSize = 0) {
   udfGroupEntry.header() = cfg::UdfBaseHeaderType::UDF_L4_HEADER;
   udfGroupEntry.startOffsetInBytes() = 5;
   udfGroupEntry.fieldSizeInBytes() = fieldSize;
-  udfGroupEntry.udfPacketMatcherIds() = {"matchCfg_1"};
+  udfGroupEntry.udfPacketMatcherIds() = {kPacketMatcherCfgName.str()};
 
   return udfGroupEntry;
 }
 
-cfg::UdfConfig makeUdfCfg(std::vector<cfg::UdfGroup> udfGroupList) {
+cfg::UdfConfig makeUdfCfg(
+    UdfGroupCfgList udfGroupList,
+    UdfPacketMatcherCfgList udfPacketMatcherList) {
   cfg::UdfConfig udf;
   cfg::UdfGroup udfGroupEntry;
   std::map<std::string, cfg::UdfGroup> udfMap;
+  std::map<std::string, cfg::UdfPacketMatcher> udfPacketMatcherMap;
 
-  for (const auto& udfGroupEntry : udfGroupList) {
-    std::string name = *udfGroupEntry.name();
-    udfMap.insert(std::make_pair(name, udfGroupEntry));
+  for (const auto& udfGroup : udfGroupList) {
+    std::string name = *udfGroup.name();
+    udfMap.insert(std::make_pair(name, udfGroup));
+  }
+  for (const auto& udfPacketMatcherEntry : udfPacketMatcherList) {
+    std::string name = *udfPacketMatcherEntry.name();
+    udfPacketMatcherMap.insert(std::make_pair(name, udfPacketMatcherEntry));
   }
   udf.udfGroups() = udfMap;
+  udf.udfPacketMatcher() = udfPacketMatcherMap;
   return udf;
 }
 
-TEST(Udf, addRemove) {
+TEST(Udf, addUpdateRemove) {
   auto state = std::make_shared<SwitchState>();
-  auto udfEntry1 = makeCfgUdfGroupEntry("foo1");
-  auto udfEntry2 = makeCfgUdfGroupEntry("foo2");
+  auto udfEntry1 = makeCfgUdfGroupEntry(kUdfGroupCfgName1.str());
+  auto udfEntry2 = makeCfgUdfGroupEntry(kUdfGroupCfgName2.str());
 
-  cfg::UdfConfig udf = makeUdfCfg({udfEntry1, udfEntry2});
+  auto udfPacketmatcherEntry =
+      makeCfgUdfPacketMatcherEntry(kPacketMatcherCfgName.str());
+  // convert to state object
+  const auto pktMatcherCfgName = kPacketMatcherCfgName.str();
+  auto udfStatePacketmatcherEntry =
+      std::make_shared<UdfPacketMatcher>(pktMatcherCfgName);
+  udfStatePacketmatcherEntry->fromThrift(udfPacketmatcherEntry);
+
+  cfg::UdfConfig udf =
+      makeUdfCfg({udfEntry1, udfEntry2}, {udfPacketmatcherEntry});
   auto udfConfig = std::make_shared<UdfConfig>();
   udfConfig->fromThrift(udf);
+
   // update the state
   state->resetUdfConfig(udfConfig);
 
   // both entries should be present
   EXPECT_EQ(state->getUdfConfig()->getUdfGroupMap()->size(), 2);
+  EXPECT_EQ(state->getUdfConfig()->getUdfPacketMatcherMap()->size(), 1);
 
-  state->getUdfConfig()->getUdfGroupMap()->removeNode("foo1");
-  state->getUdfConfig()->getUdfGroupMap()->removeNode("foo2");
+  EXPECT_EQ(
+      *state->getUdfConfig()->getUdfPacketMatcherMap()->getNodeIf(
+          kPacketMatcherCfgName.str()),
+      *udfStatePacketmatcherEntry);
+
+  // add another entry for pktMatcher
+  const auto pktMatcherCfgName2 = kPacketMatcherCfgName2.str();
+  auto udfPacketmatcherEntry2 =
+      makeCfgUdfPacketMatcherEntry(kPacketMatcherCfgName2.str());
+  auto udfStatePacketmatcherEntry2 =
+      std::make_shared<UdfPacketMatcher>(pktMatcherCfgName2);
+  udfStatePacketmatcherEntry2->fromThrift(udfPacketmatcherEntry2);
+  state->getUdfConfig()->getUdfPacketMatcherMap()->addNode(
+      udfStatePacketmatcherEntry2);
+
+  EXPECT_EQ(state->getUdfConfig()->getUdfPacketMatcherMap()->size(), 2);
+
+  // update the second packet matcher entry
+  auto udfPacketmatcherEntryModified2 =
+      makeCfgUdfPacketMatcherEntry(kPacketMatcherCfgName2.str(), 200);
+  udfStatePacketmatcherEntry2->fromThrift(udfPacketmatcherEntryModified2);
+  state->getUdfConfig()->getUdfPacketMatcherMap()->updateNode(
+      udfStatePacketmatcherEntry2);
+  EXPECT_EQ(state->getUdfConfig()->getUdfPacketMatcherMap()->size(), 2);
+
+  // update first udfGroup entry as well
+  auto udfEntryUpdated1 = createStateUdfGroup(kUdfGroupCfgName1.str(), 4);
+  state->getUdfConfig()->getUdfGroupMap()->updateNode(udfEntryUpdated1);
+  // validate that update takes effect by comparing contents
+  EXPECT_EQ(
+      *udfEntryUpdated1,
+      *state->getUdfConfig()->getUdfGroupMap()->getNode(
+          kUdfGroupCfgName1.str()));
+
+  // remove nodes
+  state->getUdfConfig()->getUdfGroupMap()->removeNode(kUdfGroupCfgName1.str());
+  state->getUdfConfig()->getUdfGroupMap()->removeNode(kUdfGroupCfgName2.str());
+  state->getUdfConfig()->getUdfPacketMatcherMap()->removeNode(
+      kPacketMatcherCfgName2.str());
 
   EXPECT_EQ(state->getUdfConfig()->getUdfGroupMap()->size(), 0);
-  EXPECT_EQ(state->getUdfConfig()->getUdfGroupMap()->getNodeIf("foo"), nullptr);
+  EXPECT_EQ(state->getUdfConfig()->getUdfPacketMatcherMap()->size(), 1);
+  EXPECT_EQ(
+      state->getUdfConfig()->getUdfGroupMap()->getNodeIf(
+          kUdfGroupCfgName1.str()),
+      nullptr);
+  EXPECT_EQ(
+      state->getUdfConfig()->getUdfGroupMap()->getNodeIf(
+          kPacketMatcherCfgName2.str()),
+      nullptr);
 }
 
-TEST(Udf, serDesUdfEntries) {
-  auto udfEntry = createStateUdfGroup("foo1");
+TEST(Udf, serDesUdfGroup) {
+  auto udfEntry = createStateUdfGroup(kUdfGroupCfgName1.str());
   auto serialized = udfEntry->toFollyDynamic();
-  auto deserialized = udfEntry->fromFollyDynamic(serialized);
+  auto deserialized = UdfGroup::fromFollyDynamic(serialized);
   EXPECT_TRUE(*udfEntry == *deserialized);
+}
+
+TEST(Udf, serDesUdfPacketMatcher) {
+  auto udfPacketmatcherEntry =
+      makeCfgUdfPacketMatcherEntry(kPacketMatcherCfgName.str());
+
+  // convert to state object
+  const auto pktMatcherCfgName = kPacketMatcherCfgName.str();
+  auto udfStatePacketmatcherEntry =
+      std::make_shared<UdfPacketMatcher>(pktMatcherCfgName);
+  udfStatePacketmatcherEntry->fromThrift(udfPacketmatcherEntry);
+
+  auto serialized = udfStatePacketmatcherEntry->toFollyDynamic();
+  auto deserialized = UdfPacketMatcher::fromFollyDynamic(serialized);
+
+  EXPECT_TRUE(*udfStatePacketmatcherEntry == *deserialized);
 }
 
 TEST(Udf, addUpdate) {
@@ -103,26 +205,30 @@ TEST(Udf, addUpdate) {
 
   EXPECT_EQ(state->getUdfConfig()->getUdfGroupMap()->size(), 0);
 
-  auto udfEntry = createStateUdfGroup("foo");
+  auto udfEntry = createStateUdfGroup(kUdfGroupCfgName1.str());
   state->getUdfConfig()->getUdfGroupMap()->addNode(udfEntry);
 
   EXPECT_EQ(
-      *udfEntry, *state->getUdfConfig()->getUdfGroupMap()->getNode("foo"));
+      *udfEntry,
+      *state->getUdfConfig()->getUdfGroupMap()->getNode(
+          kUdfGroupCfgName1.str()));
 
   auto udfGroupMap = state->getUdfConfig()->getUdfGroupMap();
   for (const auto& udfGroupEntry : *udfGroupMap) {
     auto udfGroup = udfGroupEntry.second;
     EXPECT_EQ(udfGroup->getUdfPacketMatcherIds().size(), 1);
     for (const auto& matcherId : udfGroup->getUdfPacketMatcherIds()) {
-      EXPECT_EQ(matcherId, "matchCfg_1");
+      EXPECT_EQ(matcherId, kPacketMatcherCfgName.str());
     }
   }
 
-  auto udfEntry_1 = createStateUdfGroup("foo", 4);
+  auto udfEntry_1 = createStateUdfGroup(kUdfGroupCfgName1.str(), 4);
   state->getUdfConfig()->getUdfGroupMap()->updateNode(udfEntry_1);
   // validate that update takes effect by comparing with old version
   EXPECT_NE(
-      *udfEntry, *state->getUdfConfig()->getUdfGroupMap()->getNode("foo"));
+      *udfEntry,
+      *state->getUdfConfig()->getUdfGroupMap()->getNode(
+          kUdfGroupCfgName1.str()));
 }
 
 TEST(Udf, publish) {
@@ -150,11 +256,23 @@ TEST(Udf, applyConfig) {
   // no map has been populated
   EXPECT_EQ(stateV0->getUdfConfig()->getUdfGroupMap()->size(), 0);
 
-  auto udfEntry = makeCfgUdfGroupEntry("foo1");
-  config.udfConfig() = makeUdfCfg({udfEntry});
+  auto udfEntry = makeCfgUdfGroupEntry(kUdfGroupCfgName1.str());
+  auto udfPacketmatcherEntry =
+      makeCfgUdfPacketMatcherEntry(kPacketMatcherCfgName.str());
+
+  // add udfGroup, udfPacketMatcher entries to the udf cfg
+  config.udfConfig() = makeUdfCfg({udfEntry}, {udfPacketmatcherEntry});
   auto stateV2 = publishAndApplyConfig(stateV0, &config, platform.get());
   ASSERT_NE(nullptr, stateV2);
 
   // one entry has been added <foo1>
   EXPECT_EQ(stateV2->getUdfConfig()->getUdfGroupMap()->size(), 1);
+  // one entry has been added <matchCfg_1>
+  EXPECT_EQ(stateV2->getUdfConfig()->getUdfPacketMatcherMap()->size(), 1);
+
+  // undo udf cfg
+  config.udfConfig().reset();
+  auto stateV3 = publishAndApplyConfig(stateV2, &config, platform.get());
+  ASSERT_NE(nullptr, stateV3);
+  EXPECT_EQ(stateV3->getUdfConfig(), nullptr);
 }
