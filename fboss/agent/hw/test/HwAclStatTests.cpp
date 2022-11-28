c73d4d7321cf8f04c7b71914c9c9c92885dd302d
Elangovan Natarajan <elangovan@meta.com>
2022-11-28 15:51:27 -0800

    Unit test to create shared stats post warmboot and to delete shared stats post warmboot
    Summary: Unit test to create shared stats post warmboot and to delete shared stats post warmboot
    
    Reviewed By: zechengh09
    
    Differential Revision: D41388601
    
    fbshipit-source-id: 19c6d8d6a05676e4ce8066ec1c306d44b9472603

--- fboss/agent/hw/test/HwAclStatTests.cpp
+++ fboss/agent/hw/test/HwAclStatTests.cpp
@@ -276,6 +276,64 @@ TEST_F(HwAclStatTest, AclStatCreatePostWarmBoot) {
   verifyAcrossWarmBoots(setup, verify, setupPostWB, verifyPostWB);
 }
 
+TEST_F(HwAclStatTest, AclStatDeleteSharedPostWarmBoot) {
+  auto setup = [=]() {
+    auto newCfg = initialConfig();
+    addDscpAcl(&newCfg, "acl0");
+    addDscpAcl(&newCfg, "acl1");
+    utility::addAclStat(&newCfg, "acl0", "stat");
+    utility::addAclStat(&newCfg, "acl1", "stat");
+    applyNewConfig(newCfg);
+  };
+
+  auto verify = [=]() {
+    utility::checkAclEntryAndStatCount(
+        getHwSwitch(), /* ACLs */ 2, /* Stats */ 1, /*counters*/ 1);
+    utility::checkAclStat(
+        getHwSwitch(), getProgrammedState(), {"acl0", "acl1"}, "stat");
+  };
+
+  auto setupPostWB = [&]() {
+    auto newCfg = initialConfig();
+    applyNewConfig(newCfg);
+  };
+
+  auto verifyPostWB = [=]() {
+    utility::checkAclEntryAndStatCount(
+        getHwSwitch(), /*ACLs*/ 0, /*stats*/ 0, /*counters*/ 0);
+    utility::checkAclStatDeleted(getHwSwitch(), "stat0");
+  };
+
+  verifyAcrossWarmBoots(setup, verify, setupPostWB, verifyPostWB);
+}
+
+TEST_F(HwAclStatTest, AclStatCreateSharedPostWarmBoot) {
+  auto setup = [=]() {
+    auto newCfg = initialConfig();
+    applyNewConfig(newCfg);
+  };
+
+  auto verify = [=]() {};
+
+  auto setupPostWB = [=]() {
+    auto newCfg = initialConfig();
+    addDscpAcl(&newCfg, "acl0");
+    addDscpAcl(&newCfg, "acl1");
+    utility::addAclStat(&newCfg, "acl0", "stat");
+    utility::addAclStat(&newCfg, "acl1", "stat");
+    applyNewConfig(newCfg);
+  };
+
+  auto verifyPostWB = [=]() {
+    utility::checkAclEntryAndStatCount(
+        getHwSwitch(), /*ACLs*/ 2, /*stats*/ 1, /*counters*/ 1);
+    utility::checkAclStat(
+        getHwSwitch(), getProgrammedState(), {"acl0", "acl1"}, "stat");
+  };
+
+  verifyAcrossWarmBoots(setup, verify, setupPostWB, verifyPostWB);
+}
+
 TEST_F(HwAclStatTest, AclStatDeleteShared) {
   auto setup = [=]() {
     auto newCfg = initialConfig();
