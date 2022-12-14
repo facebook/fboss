a0170c79967e272adcbeaeea7b0c09b9da2508c8
Tao Ren <taoren@meta.com>
2022-12-13 17:17:54 -0800

    fboss: fbdevd: add initial config file for Darwin
    Summary: Add initial device topology config file for the Darwin switch.
    
    Reviewed By: jasmeetbagga
    
    Differential Revision:
    D41602626
    
    LaMa Project: L1125642
    
    fbshipit-source-id: 39582093a0fdd2c6019cdf779d6a5f82d269992f

---
+++ fboss/platform/fbdevd/darwin/DarwinDeviceTopology.h
@@ -0,0 +1,11 @@
+// Copyright (c) Meta Platforms, Inc. and affiliates.
+
+#pragma once
+
+#include <string>
+
+namespace facebook::fboss::platform::fbdevd {
+
+std::string getDarwinDeviceTopology();
+
+} // namespace facebook::fboss::platform::fbdevd
