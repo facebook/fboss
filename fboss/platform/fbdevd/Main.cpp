a4dcbca9a1c4abf96bece6e566f94012d2b95f7f
Tao Ren <taoren@meta.com>
2022-12-13 17:17:54 -0800

    fboss: fbdevd: add empty main() function
    Summary: Add empty main() function for the fbdevd service.
    
    Reviewed By: somasun
    
    Differential Revision:
    D41602621
    
    LaMa Project: L1125642
    
    fbshipit-source-id: 2fb590fd8e8dd50765aceeaa92ed340a452e27e1

---
+++ fboss/platform/fbdevd/Main.cpp
@@ -0,0 +1,29 @@
+// Copyright (c) Meta Platforms, Inc. and affiliates.
+
+#include <fb303/FollyLoggingHandler.h>
+#include <folly/experimental/FunctionScheduler.h>
+#include <folly/logging/Init.h>
+#include <folly/logging/xlog.h>
+
+#include "fboss/platform/fbdevd/darwin/DarwinDeviceTopology.h"
+#include "fboss/platform/fbdevd/if/gen-cpp2/FbdevManager.h"
+#include "fboss/platform/helpers/Init.h"
+
+using namespace facebook;
+using namespace facebook::services;
+using namespace facebook::fboss::platform;
+using namespace facebook::fboss::platform::fbdevd;
+
+FOLLY_INIT_LOGGING_CONFIG("fboss=DBG2; default:async=true");
+
+int main(int argc, char** argv) {
+  // Init FB and export build values
+  helpers::init(argc, argv);
+  fb303::registerFollyLoggingOptionHandlers();
+
+  // Set up scheduler.
+  folly::FunctionScheduler scheduler;
+  scheduler.start();
+
+  return 0;
+}
