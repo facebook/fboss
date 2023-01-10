41ed3082c981622d1cdad646fc4dc5080338d3c4
Manikandan Somasundaram <manikandan@meta.com>
2023-01-09 17:58:17 -0800

    Spin up thriftservice only for required test
    Reviewed By: jasmeetbagga
    
    Differential Revision: D42355882
    
    fbshipit-source-id: 96e06c33ebfddac38e1927b522f82986ed260680

--- fboss/platform/data_corral_service/hw_test/DataCorralServiceTest.cpp
+++ fboss/platform/data_corral_service/hw_test/DataCorralServiceTest.cpp
@@ -8,44 +8,37 @@
  *
  */
 
-#include "fboss/platform/data_corral_service/hw_test/DataCorralServiceTest.h"
+#include <gtest/gtest.h>
 
 #include <folly/experimental/coro/BlockingWait.h>
-#include <thrift/lib/cpp2/async/RocketClientChannel.h>
-#include <thrift/lib/cpp2/server/ThriftServer.h>
+#include <thrift/lib/cpp2/util/ScopedServerInterfaceThread.h>
 
-#include "common/services/cpp/ServiceFrameworkLight.h"
 #include "fboss/platform/data_corral_service/DataCorralServiceImpl.h"
 #include "fboss/platform/data_corral_service/DataCorralServiceThriftHandler.h"
 #include "fboss/platform/data_corral_service/Flags.h"
-#include "fboss/platform/data_corral_service/SetupDataCorralServiceThrift.h"
+#include "fboss/platform/data_corral_service/if/gen-cpp2/DataCorralServiceThrift.h"
+#include "fboss/platform/data_corral_service/if/gen-cpp2/data_corral_service_types.h"
 
-namespace facebook::fboss::platform::data_corral_service {
+using namespace facebook::fboss::platform::data_corral_service;
 
-DataCorralServiceTest::~DataCorralServiceTest() {}
+namespace {
+class DataCorralServiceTest : public ::testing::Test {
+ public:
+  void SetUp() override {
+    thriftHandler_ = std::make_shared<DataCorralServiceThriftHandler>(
+        std::make_shared<DataCorralServiceImpl>(""));
+  }
 
-void DataCorralServiceTest::SetUp() {
-  std::tie(thriftServer_, thriftHandler_) = setupThrift();
-  service_ = std::make_unique<services::ServiceFrameworkLight>(
-      "Data corral service test");
-  runServer(
-      *service_, thriftServer_, thriftHandler_.get(), false /*loop forever*/);
-}
-void DataCorralServiceTest::TearDown() {
-  service_->stop();
-  service_->waitForStop();
-  service_.reset();
-  thriftServer_.reset();
-  thriftHandler_.reset();
-}
+  void TearDown() override {}
 
-DataCorralFruidReadResponse DataCorralServiceTest::getFruid(bool uncached) {
-  return *(folly::coro::blockingWait(thriftHandler_->co_getFruid(uncached)));
-}
-
-DataCorralServiceImpl* DataCorralServiceTest::getService() {
-  return thriftHandler_->getServiceImpl();
-}
+ protected:
+  DataCorralFruidReadResponse getFruid(bool uncached) {
+    auto client = apache::thrift::makeTestClient(thriftHandler_);
+    return folly::coro::blockingWait(client->co_getFruid(uncached));
+  }
+  std::shared_ptr<DataCorralServiceThriftHandler> thriftHandler_;
+};
+} // namespace
 
 TEST_F(DataCorralServiceTest, getCachedFruid) {
   EXPECT_GT(getFruid(false).fruidData()->size(), 0);
@@ -57,13 +50,13 @@ TEST_F(DataCorralServiceTest, getUncachedFruid) {
 
 TEST_F(DataCorralServiceTest, testThrift) {
   folly::SocketAddress addr("::1", FLAGS_thrift_port);
-  auto socket = folly::AsyncSocket::newSocket(
-      folly::EventBaseManager::get()->getEventBase(), addr, 5000);
-  auto channel =
-      apache::thrift::RocketClientChannel::newChannel(std::move(socket));
-  auto client = DataCorralServiceThriftAsyncClient(std::move(channel));
+  auto server = std::make_unique<apache::thrift::ScopedServerInterfaceThread>(
+      thriftHandler_, folly::SocketAddress("::1", FLAGS_thrift_port));
+  auto resolverEvb = std::make_unique<folly::EventBase>();
+  auto clientPtr =
+      server->newClient<apache::thrift::Client<DataCorralServiceThrift>>(
+          resolverEvb.get());
   DataCorralFruidReadResponse response;
-  client.sync_getFruid(response, false);
+  clientPtr->sync_getFruid(response, false);
   EXPECT_GT(response.fruidData()->size(), 0);
 }
-} // namespace facebook::fboss::platform::data_corral_service
