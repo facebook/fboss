80abc9df19bb784ebee8272c6380900a6b988757
Manikandan Somasundaram <manikandan@meta.com>
2023-01-17 13:57:18 -0800

    Fix OSS thrift service
    Summary: Previously OSS thrift did not work. Fixing it in this diff
    
    Reviewed By: jasmeetbagga
    
    Differential Revision:
    D42551209
    
    Privacy Context Container: L1125642
    
    fbshipit-source-id: 0a607922582e903964549278d8bd16a3f80f27a5

--- fboss/platform/rackmon/Main.cpp
+++ fboss/platform/rackmon/Main.cpp
@@ -1,14 +1,9 @@
 // Copyright 2004-present Facebook. All Rights Reserved.
 
-#ifndef IS_OSS
-#include "common/services/cpp/ServiceFrameworkLight.h"
-#endif
-#include "fboss/platform/helpers/Init.h"
-
 #include <fb303/FollyLoggingHandler.h>
 #include <folly/logging/Init.h>
-#include <glog/logging.h>
 
+#include "fboss/platform/helpers/Init.h"
 #include "fboss/platform/rackmon/RackmonThriftHandler.h"
 
 using namespace facebook;
@@ -23,18 +18,13 @@ int main(int argc, char** argv) {
   helpers::init(argc, argv);
   fb303::registerFollyLoggingOptionHandlers();
 
-  // Setup thrift handler and server
-  auto serverHandlerPair = helpers::setupThrift(
-      std::make_shared<rackmonsvc::ThriftHandler>(), FLAGS_port);
-  __attribute__((unused)) auto server = serverHandlerPair.first;
-  __attribute__((unused)) auto handler = serverHandlerPair.second;
+  auto server = std::make_shared<apache::thrift::ThriftServer>();
+  auto handler = std::make_shared<rackmonsvc::ThriftHandler>();
 
-#ifndef IS_OSS
-  services::ServiceFrameworkLight service("Rackmon Service");
-  // TODO(T123377436) CodeFrameworks Migration - Binary Contract
-  service.addPrimaryThriftService(server, handler.get());
-  helpers::addCommonModules(service);
-  service.go();
-#endif
+  server->setPort(FLAGS_port);
+  server->setInterface(handler);
+  server->setAllowPlaintextOnLoopback(true);
+  helpers::runThriftService(server, handler, "DataCorralService", FLAGS_port);
+
   return 0;
 }
