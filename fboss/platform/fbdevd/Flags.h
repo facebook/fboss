f2af7ce75f70b08fd68b568ac74aa58290486069
Bin Huang <binhuang@meta.com>
2023-01-25 17:26:05 -0800

    Add config_path flag
    Summary: #astitle
    
    Reviewed By: somasun
    
    Differential Revision:
    D42654626
    
    Privacy Context Container: L1125642
    
    fbshipit-source-id: 41ab66503c27623acb165e903148e938b5d1b562

---
+++ fboss/platform/fbdevd/Flags.h
@@ -0,0 +1,15 @@
+/*
+ *  Copyright (c) 2004-present, Meta Platforms, Inc. and affiliates.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#pragma once
+
+#include <gflags/gflags.h>
+
+DECLARE_string(config_file);
--- fboss/platform/fbdevd/Main.cpp
+++ fboss/platform/fbdevd/Main.cpp
@@ -6,6 +6,7 @@
 #include <folly/logging/xlog.h>
 
 #include "fboss/platform/fbdevd/FbdevdImpl.h"
+#include "fboss/platform/fbdevd/Flags.h"
 #include "fboss/platform/fbdevd/if/gen-cpp2/FbdevManager.h"
 #include "fboss/platform/helpers/Init.h"
 
@@ -16,11 +17,10 @@ using namespace facebook::fboss::platform::fbdevd;
 FOLLY_INIT_LOGGING_CONFIG("fboss=DBG2; default:async=true");
 
 int main(int argc, char** argv) {
-  // Init FB and export build values
-  helpers::init(argc, argv);
   fb303::registerFollyLoggingOptionHandlers();
+  helpers::init(argc, argv);
 
-  auto serviceImpl = std::make_shared<FbdevdImpl>();
+  auto serviceImpl = std::make_shared<FbdevdImpl>(FLAGS_config_file);
 
   // Set up scheduler.
   folly::FunctionScheduler scheduler;
