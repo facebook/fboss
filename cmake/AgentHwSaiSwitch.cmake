f2a9148f1c8df5a6f502c18b0f3b2e088394cd45
Srikrishna Gopu <srikrishnagopu@meta.com>
2023-02-02 15:35:10 -0800

    use the tajo specific debug counter for oss
    Summary:
    As titled, use the tajo specific file in OSS for debug counter
    
    NOTE: Trying to minimize the OSS patches to build tajo in OSS
    
    Reviewed By: nivinl
    
    Differential Revision: D42970632
    
    fbshipit-source-id: 8ef2242046c7ef1096b5f706ff36ceddc97f29b0

--- cmake/AgentHwSaiSwitch.cmake
+++ cmake/AgentHwSaiSwitch.cmake
@@ -39,7 +39,6 @@ set(SAI_SWITCH_SRC
   fboss/agent/hw/sai/switch/SaiVirtualRouterManager.cpp
   fboss/agent/hw/sai/switch/SaiWredManager.cpp
   fboss/agent/hw/sai/switch/oss/SaiBufferManager.cpp
-  fboss/agent/hw/sai/switch/oss/SaiDebugCounterManager.cpp
   fboss/agent/hw/sai/switch/oss/SaiHostifManager.cpp
   fboss/agent/hw/sai/switch/npu/SaiAclTableManager.cpp
   fboss/agent/hw/sai/switch/npu/SaiPortManager.cpp
@@ -53,6 +52,7 @@ if (SAI_TAJO_IMPL)
     fboss/agent/hw/sai/switch/npu/tajo/SaiTamManager.cpp
     fboss/agent/hw/sai/switch/npu/tajo/SaiPortManager.cpp
     fboss/agent/hw/sai/switch/npu/tajo/SaiAclTableManager.cpp
+    fboss/agent/hw/sai/switch/npu/tajo/SaiDebugCounterManager.cpp
   )
 elseif (SAI_BRCM_IMPL)
   list(APPEND SAI_SWITCH_SRC
@@ -60,6 +60,7 @@ elseif (SAI_BRCM_IMPL)
     fboss/agent/hw/sai/switch/npu/bcm/SaiTamManager.cpp
     fboss/agent/hw/sai/switch/npu/bcm/SaiPortManager.cpp
     fboss/agent/hw/sai/switch/oss/SaiAclTableManager.cpp
+    fboss/agent/hw/sai/switch/oss/SaiDebugCounterManager.cpp
   )
 else()
   list(APPEND SAI_SWITCH_SRC
@@ -67,6 +68,7 @@ else()
     fboss/agent/hw/sai/switch/oss/SaiTamManager.cpp
     fboss/agent/hw/sai/switch/oss/SaiPortManager.cpp
     fboss/agent/hw/sai/switch/oss/SaiAclTableManager.cpp
+    fboss/agent/hw/sai/switch/oss/SaiDebugCounterManager.cpp
   )
 endif()
 
