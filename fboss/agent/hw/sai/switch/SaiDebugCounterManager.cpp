5240ce3e029f17ad04b8666d201b54b60841ab00
Srikrishna Gopu <srikrishnagopu@meta.com>
2023-02-02 16:38:43 -0800

    kill tajo/oss SaiDebugCounterManager
    Summary:
    As titled, kill tajo/oss SaiDebugCounterManager since they look alike except the SAI versioning which exists in OSS.
    
    Kill both the files and move the source to main file since they are all open sourced anyways
    
    Reviewed By: jasmeetbagga
    
    Differential Revision: D42973135
    
    fbshipit-source-id: f34b319fbc16d1beda69dad725d406277e070039

--- fboss/agent/hw/sai/switch/SaiDebugCounterManager.cpp
+++ fboss/agent/hw/sai/switch/SaiDebugCounterManager.cpp
@@ -18,6 +18,10 @@
 #include "fboss/agent/hw/switch_asics/HwAsic.h"
 #include "fboss/agent/platforms/sai/SaiPlatform.h"
 
+extern "C" {
+#include <sai.h>
+}
+
 namespace facebook::fboss {
 
 void SaiDebugCounterManager::setupDebugCounters() {
@@ -38,5 +42,25 @@ void SaiDebugCounterManager::setupPortL3BlackHoleCounter() {
       SaiApiTable::getInstance()->debugCounterApi().getAttribute(
           portL3BlackHoleCounter_->adapterKey(),
           SaiDebugCounterTraits::Attributes::Index{});
+}
+
+void SaiDebugCounterManager::setupMPLSLookupFailedCounter() {
+  if (!platform_->getAsic()->isSupported(
+          HwAsic::Feature::SAI_MPLS_LABEL_LOOKUP_FAIL_COUNTER)) {
+    return;
+  }
+#if SAI_API_VERSION >= SAI_VERSION(1, 9, 0)
+  SaiDebugCounterTraits::CreateAttributes attrs{
+      SAI_DEBUG_COUNTER_TYPE_PORT_IN_DROP_REASONS,
+      SAI_DEBUG_COUNTER_BIND_METHOD_AUTOMATIC,
+      SaiDebugCounterTraits::Attributes::InDropReasons{
+          {SAI_IN_DROP_REASON_MPLS_MISS}}};
+  auto& debugCounterStore = saiStore_->get<SaiDebugCounterTraits>();
+  mplsLookupFailCounter_ = debugCounterStore.setObject(attrs, attrs);
+  mplsLookupFailCounterStatId_ = SAI_SWITCH_STAT_IN_DROP_REASON_RANGE_BASE +
+      SaiApiTable::getInstance()->debugCounterApi().getAttribute(
+          mplsLookupFailCounter_->adapterKey(),
+          SaiDebugCounterTraits::Attributes::Index{});
+#endif
 }
 } // namespace facebook::fboss
