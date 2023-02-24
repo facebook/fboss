4df6eb86186791fe5298ffad4f12dae77e4edf6f
Shrikrishna (Shri) Khare <skhare@meta.com>
2023-02-24 14:41:09 -0800

    Program SAMPLEPACKET trap only if ASIC supports that feature
    Summary: As titled.
    
    Reviewed By: jasmeetbagga
    
    Differential Revision:
    D43526437
    
    Privacy Context Container: L1125642
    
    fbshipit-source-id: 17f712d2a068c222f58f26e473ee91c04ea70aba

--- fboss/agent/hw/sai/hw_test/HwTestCoppUtils.cpp
+++ fboss/agent/hw/sai/hw_test/HwTestCoppUtils.cpp
@@ -98,10 +98,14 @@ std::vector<cfg::PacketRxReasonToQueue> getCoppRxReasonToQueues(
         cfg::PacketRxReason::MPLS_TTL_1, kCoppLowPriQueueId));
 #endif
   }
+
+  if (hwAsic->isSupported(HwAsic::Feature::SAI_SAMPLEPACKET_TRAP)) {
 #if !defined(SAI_VERSION_5_1_0_3_ODP)
-  rxReasonToQueues.push_back(ControlPlane::makeRxReasonToQueueEntry(
-      cfg::PacketRxReason::SAMPLEPACKET, kCoppLowPriQueueId));
+    rxReasonToQueues.push_back(ControlPlane::makeRxReasonToQueueEntry(
+        cfg::PacketRxReason::SAMPLEPACKET, kCoppLowPriQueueId));
 #endif
+  }
+
   return rxReasonToQueues;
 }
 
