9a3b3d4347786c551fee20bf391590b12d70ddf5
Jasmeet Bagga <jasmeetbagga@meta.com>
2023-02-25 15:49:27 -0800

    Implement setForceTrafficOverFabric api in sai layer
    Summary: As titled
    
    Reviewed By: srikrishnagopu
    
    Differential Revision:
    D43595648
    
    Privacy Context Container: L1125642
    
    fbshipit-source-id: 27da222255df917ffa33d0042df8eaed4b2349b7

--- fboss/agent/hw/sai/hw_test/HwFabricUtils.cpp
+++ fboss/agent/hw/sai/hw_test/HwFabricUtils.cpp
@@ -2,10 +2,15 @@
 
 #include "fboss/agent/hw/test/HwFabricUtils.h"
 
+#include "fboss/agent/hw/sai/switch/SaiSwitch.h"
+
 namespace facebook::fboss {
 
-void setForceTrafficOverFabric(const HwSwitch* /*hw*/, bool /*force*/) {
-  // TODO
+void setForceTrafficOverFabric(const HwSwitch* hw, bool force) {
+  auto saiSwitch = static_cast<const SaiSwitch*>(hw);
+  auto switchID = saiSwitch->getSaiSwitchId();
+  SaiApiTable::getInstance()->switchApi().setAttribute(
+      switchID, SaiSwitchTraits::Attributes::ForceTrafficOverFabric{force});
 }
 
 } // namespace facebook::fboss
