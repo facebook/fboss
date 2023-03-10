e84a7fd587100dc8fde07f5b50d0ec3a19565dda
Srikrishna Gopu <srikrishnagopu@meta.com>
2023-03-09 16:03:38 -0800

    create agent platform common utils to mimic internal build
    Summary: As titled, create agent platform common utils to mimic internal build. This is becasue
    
    Reviewed By: simuthus-fb
    
    Differential Revision: D43936769
    
    fbshipit-source-id: 426e74996d7b4c54284acf9256ea7192d0c075dc

---
+++ cmake/AgentPlatformsCommonUtils.cmake
@@ -0,0 +1,31 @@
+# CMake to build libraries and binaries in fboss/agent/platforms/common
+
+# In general, libraries and binaries in fboss/foo/bar are built by
+# cmake/FooBar.cmake
+
+add_library(wedge_led_utils
+  fboss/agent/platforms/common/utils/GalaxyLedUtils.cpp
+  fboss/agent/platforms/common/utils/MinipackLedUtils.cpp
+  fboss/agent/platforms/common/utils/Wedge100LedUtils.cpp
+  fboss/agent/platforms/common/utils/Wedge40LedUtils.cpp
+  fboss/agent/platforms/common/utils/Wedge400LedUtils.cpp
+)
+
+target_link_libraries(wedge_led_utils
+  error
+  ctrl_cpp2
+  fboss_types
+  transceiver_cpp2
+  fboss_i2c_lib
+  qsfp_lib
+)
+
+add_library(bcm_yaml_config
+  fboss/agent/platforms/common/utils/BcmYamlConfig.cpp
+)
+
+target_link_libraries(bcm_yaml_config
+  platform_mapping
+  ${YAML-CPP}
+  ${YAML}
+)
