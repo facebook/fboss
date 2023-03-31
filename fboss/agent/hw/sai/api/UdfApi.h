972899305ecb8176f99dda9d4c5cf6d1d666a915
Ron He <zecheng@meta.com>
2023-03-30 23:31:08 -0700

    Introduce UDF group API
    Summary: As titled.
    
    Reviewed By: shri-khare
    
    Differential Revision:
    D44493029
    
    Privacy Context Container: L1125642
    
    fbshipit-source-id: 7078e2026f37be54bfc7e20f017e4895478f6e66

--- fboss/agent/hw/sai/api/UdfApi.h
+++ fboss/agent/hw/sai/api/UdfApi.h
@@ -53,6 +53,25 @@ SAI_ATTRIBUTE_NAME(Udf, UdfGroupId);
 SAI_ATTRIBUTE_NAME(Udf, Base);
 SAI_ATTRIBUTE_NAME(Udf, Offset);
 
+struct SaiUdfGroupTraits {
+  static constexpr sai_object_type_t ObjectType = SAI_OBJECT_TYPE_UDF_GROUP;
+  using SaiApiT = UdfApi;
+  struct Attributes {
+    using EnumType = sai_udf_group_attr_t;
+    using Type = SaiAttribute<EnumType, SAI_UDF_GROUP_ATTR_TYPE, sai_int32_t>;
+    using Length =
+        SaiAttribute<EnumType, SAI_UDF_GROUP_ATTR_LENGTH, sai_uint16_t>;
+  };
+
+  using AdapterKey = UdfGroupSaiId;
+  using CreateAttributes =
+      std::tuple<std::optional<Attributes::Type>, Attributes::Length>;
+  using AdapterHostKey = CreateAttributes;
+};
+
+SAI_ATTRIBUTE_NAME(UdfGroup, Type);
+SAI_ATTRIBUTE_NAME(UdfGroup, Length);
+
 class UdfApi : public SaiApi<UdfApi> {
  public:
   static constexpr sai_api_t ApiType = SAI_API_UDF;
@@ -80,6 +99,27 @@ class UdfApi : public SaiApi<UdfApi> {
 
   sai_status_t _setAttribute(UdfSaiId id, const sai_attribute_t* attr) const {
     return api_->set_udf_attribute(id, attr);
+  }
+
+  sai_status_t _create(
+      UdfGroupSaiId* id,
+      sai_object_id_t switch_id,
+      size_t count,
+      sai_attribute_t* attr_list) const {
+    return api_->create_udf_group(rawSaiId(id), switch_id, count, attr_list);
+  }
+
+  sai_status_t _remove(UdfGroupSaiId id) const {
+    return api_->remove_udf_group(id);
+  }
+
+  sai_status_t _getAttribute(UdfGroupSaiId id, sai_attribute_t* attr) const {
+    return api_->get_udf_group_attribute(id, 1, attr);
+  }
+
+  sai_status_t _setAttribute(UdfGroupSaiId id, const sai_attribute_t* attr)
+      const {
+    return api_->set_udf_group_attribute(id, attr);
   }
 
   sai_udf_api_t* api_;
