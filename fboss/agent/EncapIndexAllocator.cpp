0e84f248d184be00e35987cd46d4b809b04802f1
Jasmeet Bagga <jasmeetbagga@meta.com>
2023-03-30 17:23:23 -0700

    Pull up logic to assign encap indices in tests
    Summary: We will use this same logic in Hw, Agent {test, benchmarks}
    
    Reviewed By: shri-khare
    
    Differential Revision:
    D44529040
    
    Privacy Context Container: L1125642
    
    fbshipit-source-id: e42296725949bd9040bce4db89ec47e3264c8f7d

--- fboss/agent/EncapIndexAllocator.cpp
+++ fboss/agent/EncapIndexAllocator.cpp
@@ -5,6 +5,7 @@
 #include "fboss/agent/FbossError.h"
 #include "fboss/agent/hw/switch_asics/HwAsic.h"
 #include "fboss/agent/state/Interface.h"
+#include "fboss/agent/state/StateDelta.h"
 #include "fboss/agent/state/SwitchState.h"
 #include "fboss/agent/state/Vlan.h"
 
@@ -51,5 +52,92 @@ int64_t EncapIndexAllocator::getNextAvailableEncapIdx(
     start++;
   }
   throw FbossError("No more unallocated indices for: ", asic.getAsicTypeStr());
+}
+
+std::shared_ptr<SwitchState> EncapIndexAllocator::updateEncapIndices(
+    const StateDelta& delta,
+    const HwAsic& asic) {
+  auto newState = delta.newState()->clone();
+  // This function's charter is
+  // - Assign encap index to reachable nbr entries (if one is not already
+  //  assigned)
+  // - Clear encap index from unreachable/pending entries
+  if (asic.isSupported(HwAsic::Feature::RESERVED_ENCAP_INDEX_RANGE)) {
+    auto handleNewNbr = [&newState, &asic](auto newNbr) {
+      if (!newNbr->isPending() && !newNbr->getEncapIndex()) {
+        auto nbr = newNbr->clone();
+        nbr->setEncapIndex(
+            EncapIndexAllocator::getNextAvailableEncapIdx(newState, asic));
+        return nbr;
+      }
+      if (newNbr->isPending() && newNbr->getEncapIndex()) {
+        auto nbr = newNbr->clone();
+        nbr->setEncapIndex(std::nullopt);
+        return nbr;
+      }
+      return std::shared_ptr<std::remove_reference_t<decltype(*newNbr)>>();
+    };
+    for (const auto& intfDelta : delta.getIntfsDelta()) {
+      auto updateIntf =
+          [&](auto newNbr, const auto& nbrTable, InterfaceID intfId) {
+            auto intfMap = newState->getInterfaces()->modify(&newState);
+            auto intf = intfMap->getInterface(intfId);
+            if (intf->getType() != cfg::InterfaceType::SYSTEM_PORT) {
+              return;
+            }
+            auto updatedNbr = handleNewNbr(newNbr);
+            if (updatedNbr) {
+              auto intfNew = intf->clone();
+              auto updatedNbrTable = nbrTable->toThrift();
+              if (!nbrTable->getEntry(newNbr->getIP())) {
+                updatedNbrTable.insert(
+                    {newNbr->getIP().str(), updatedNbr->toThrift()});
+              } else {
+                updatedNbrTable.erase(newNbr->getIP().str());
+                updatedNbrTable.insert(
+                    {newNbr->getIP().str(), updatedNbr->toThrift()});
+              }
+              if (newNbr->getIP().version() == 6) {
+                intfNew->setNdpTable(updatedNbrTable);
+              } else {
+                intfNew->setArpTable(updatedNbrTable);
+              }
+              intfMap->updateNode(intfNew);
+            }
+          };
+      DeltaFunctions::forEachChanged(
+          intfDelta.getArpEntriesDelta(),
+          [&](auto /*oldNbr*/, auto newNbr) {
+            updateIntf(
+                newNbr,
+                intfDelta.getNew()->getArpTable(),
+                intfDelta.getNew()->getID());
+          },
+          [&](auto newNbr) {
+            updateIntf(
+                newNbr,
+                intfDelta.getNew()->getArpTable(),
+                intfDelta.getNew()->getID());
+          },
+          [&](auto /*rmNbr*/) {});
+
+      DeltaFunctions::forEachChanged(
+          intfDelta.getNdpEntriesDelta(),
+          [&](auto /*oldNbr*/, auto newNbr) {
+            updateIntf(
+                newNbr,
+                intfDelta.getNew()->getNdpTable(),
+                intfDelta.getNew()->getID());
+          },
+          [&](auto newNbr) {
+            updateIntf(
+                newNbr,
+                intfDelta.getNew()->getNdpTable(),
+                intfDelta.getNew()->getID());
+          },
+          [&](auto /*rmNbr*/) {});
+    }
+  }
+  return newState;
 }
 } // namespace facebook::fboss
--- fboss/agent/EncapIndexAllocator.h
+++ fboss/agent/EncapIndexAllocator.h
@@ -7,12 +7,16 @@
 namespace facebook::fboss {
 class HwAsic;
 class SwitchState;
+class StateDelta;
 
 class EncapIndexAllocator {
  public:
   static constexpr int kEncapIdxReservedForLoopbacks = 100;
   static int64_t getNextAvailableEncapIdx(
       const std::shared_ptr<SwitchState>& state,
+      const HwAsic& asic);
+  static std::shared_ptr<SwitchState> updateEncapIndices(
+      const StateDelta& delta,
       const HwAsic& asic);
 };
 } // namespace facebook::fboss
