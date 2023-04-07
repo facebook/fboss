bf912914d55ece565a8bf1f1d40ba05f66ddd58e
Parvez Shaikh <pshaikh@meta.com>
2023-04-07 14:01:04 -0700

    fix in hw acl tests to account for oper delta apply
    Summary: as titled.
    
    Reviewed By: jasmeetbagga
    
    Differential Revision:
    D44726189
    
    Privacy Context Container: L1125642
    
    fbshipit-source-id: 774c780483e1b42c2a1ebbf9c026d9a38cd50a03

--- fboss/agent/hw/test/HwAclStatTests.cpp
+++ fboss/agent/hw/test/HwAclStatTests.cpp
@@ -447,7 +447,14 @@ TEST_F(HwAclStatTest, AclStatCreateSameTwice) {
   utility::addAclStat(&newCfg, "acl0", "stat0");
   applyNewConfig(newCfg);
   StateDelta delta(state, getProgrammedState());
-  EXPECT_THROW(getHwSwitch()->stateChanged(delta), FbossError);
+
+  if (FLAGS_enable_state_oper_delta) {
+    // adding same ACL twice with oper delta and state maintained in HW switch
+    // leads to process change. not process added.
+    EXPECT_NO_THROW(getHwSwitch()->stateChanged(delta));
+  } else {
+    EXPECT_THROW(getHwSwitch()->stateChanged(delta), FbossError);
+  }
 }
 
 TEST_F(HwAclStatTest, AclStatDeleteNonExistent) {
