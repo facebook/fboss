9a87a20a3571521bf3984d32bbcc4dbf624ea432
Wei Dai <daiweix@meta.com>
2023-04-10 10:04:27 -0700

    make fsdb thrift server also listen on port 5958
    Summary:
    Existing port 5958 is in conflict with VNC ports. So, also make fsdb listen on port 5958, see https://fb.workplace.com/groups/280091245787228/permalink/1603339303462409/ for context
    
    Note: I followed thrift team suggestion in https://fb.workplace.com/groups/thriftusers/permalink/1190603738315703/ to change ServiceFrameworkLight setup
    
    Reviewed By: peygar
    
    Differential Revision: D44802094
    
    fbshipit-source-id: 8dc3301238b6f637252d2cc36fd465e53219319e

--- fboss/fsdb/common/Flags.cpp
+++ fboss/fsdb/common/Flags.cpp
@@ -7,6 +7,9 @@ DEFINE_int32(
     fsdbPort,
     facebook::fboss::fsdb::fsdb_common_constants::PORT(),
     "FSDB server port");
+// current default 5908 is in conflict with VNC ports, need to
+// eventually migrate to 5958
+DEFINE_int32(migrated_fsdbPort, 5958, "New FSDB thrift server port migrate to");
 DEFINE_bool(publish_stats_to_fsdb, false, "Whether to publish stats to fsdb");
 DEFINE_bool(
     publish_state_to_fsdb,
--- fboss/fsdb/common/Flags.h
+++ fboss/fsdb/common/Flags.h
@@ -5,6 +5,7 @@
 #include <gflags/gflags.h>
 
 DECLARE_int32(fsdbPort);
+DECLARE_int32(migrated_fsdbPort);
 DECLARE_bool(publish_stats_to_fsdb);
 DECLARE_bool(publish_state_to_fsdb);
 DECLARE_bool(subscribe_to_stats_from_fsdb);
