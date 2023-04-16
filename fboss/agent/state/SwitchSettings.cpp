7d6c408c474ee2d701e44ea824af13a643b42365
Jasmeet Bagga <jasmeetbagga@meta.com>
2023-04-15 20:30:12 -0700

    Get rid of unused string constants
    Summary: As titled
    
    Reviewed By: shri-khare, msomasundaran
    
    Differential Revision:
    D45023462
    
    Privacy Context Container: L1125642
    
    fbshipit-source-id: e362fb4bdce37dd4905f6a2633ae531991b31478

--- fboss/agent/state/SwitchSettings.cpp
+++ fboss/agent/state/SwitchSettings.cpp
@@ -17,26 +17,6 @@
 #include "folly/dynamic.h"
 #include "folly/json.h"
 
-namespace {
-constexpr auto kL2LearningMode = "l2LearningMode";
-constexpr auto kQcmEnable = "qcmEnable";
-constexpr auto kPtpTcEnable = "ptpTcEnable";
-constexpr auto kL2AgeTimerSeconds = "l2AgeTimerSeconds";
-constexpr auto kMaxRouteCounterIDs = "maxRouteCounterIDs";
-constexpr auto kBlockNeighbors = "blockNeighbors";
-constexpr auto kBlockNeighborVlanID = "blockNeighborVlanID";
-constexpr auto kBlockNeighborIP = "blockNeighborIP";
-constexpr auto kMacAddrsToBlock = "macAddrsToBlock";
-constexpr auto kMacAddrToBlockVlanID = "macAddrToBlockVlanID";
-constexpr auto kMacAddrToBlockAddr = "macAddrToBlockAddr";
-constexpr auto kSwitchType = "switchType";
-constexpr auto kSwitchId = "switchId";
-constexpr auto kExactMatchTableConfigs = "exactMatchTableConfigs";
-constexpr auto kExactMatchTableName = "name";
-constexpr auto kExactMatchTableDstPrefixLength = "dstPrefixLength";
-
-} // namespace
-
 namespace facebook::fboss {
 
 SwitchSettings* SwitchSettings::modify(std::shared_ptr<SwitchState>* state) {
