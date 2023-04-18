ba20cd7fd5415efd3011b3d55741b05dd87bf53f
Parvez Shaikh <pshaikh@meta.com>
2023-04-18 10:34:18 -0700

    add missing code in migrating remote system ports to m-NPU map
    Summary: remote system ports were missed in SwitchState's in handling migration to m-NPU format
    
    Reviewed By: peygar
    
    Differential Revision: D45075268
    
    fbshipit-source-id: c3bce45a8888605b0416fdc2802dae0b924ce024

--- fboss/agent/state/SwitchState.cpp
+++ fboss/agent/state/SwitchState.cpp
@@ -702,6 +702,10 @@ std::unique_ptr<SwitchState> SwitchState::uniquePtrFromThrift(
   state->fromThrift<
       switch_state_tags::systemPortMaps,
       switch_state_tags::systemPortMap>();
+  state->fromThrift<
+      switch_state_tags::remoteSystemPortMaps,
+      switch_state_tags::remoteSystemPortMap>();
+
   return state;
 }
 
@@ -913,6 +917,9 @@ state::SwitchState SwitchState::toThrift() const {
   }
   if (auto obj = toThrift(cref<switch_state_tags::portMaps>())) {
     data.portMap() = *obj;
+  }
+  if (auto obj = toThrift(cref<switch_state_tags::remoteSystemPortMaps>())) {
+    data.remoteSystemPortMap() = *obj;
   }
   if (auto obj = toThrift(cref<switch_state_tags::interfaceMaps>())) {
     data.interfaceMap() = *obj;
