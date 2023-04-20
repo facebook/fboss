1475ede2d87b8d0b03b79023986bad6ee9b35875
generatedunixname226714639793621 <noreply+226714639793621@fb.com>
2023-04-20 10:29:06 -0700

    fbcode/fboss/fsdb/
    Reviewed By: peygar
    
    Differential Revision: D45145446
    
    fbshipit-source-id: 38fe1f49753e0038ff11a21c98ddc8d077de2ff6

--- fboss/fsdb/if/fsdb_config.thrift
+++ fboss/fsdb/if/fsdb_config.thrift
@@ -5,12 +5,11 @@ namespace py.asyncio neteng.fboss.asyncio.fsdb_config
 
 include "fboss/fsdb/if/fsdb_common.thrift"
 include "fboss/fsdb/if/fsdb_oper.thrift"
+include "thrift/annotation/cpp.thrift"
 
 struct Config {
-  1: map<string, string> (
-    cpp.template = 'std::unordered_map',
-  ) defaultCommandLineArgs;
-  2: map<fsdb_common.PublisherId, list<fsdb_oper.OperPath>> (
-    cpp.template = 'std::unordered_map',
-  ) operPublishers = {};
+  @cpp.Type{template = "std::unordered_map"}
+  1: map<string, string> defaultCommandLineArgs;
+  @cpp.Type{template = "std::unordered_map"}
+  2: map<fsdb_common.PublisherId, list<fsdb_oper.OperPath>> operPublishers = {};
 }
