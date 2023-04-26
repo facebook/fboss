0581f6962ebbe47eca10311fbb72df068e9d7bea
Jasmeet Bagga <jasmeetbagga@meta.com>
2023-04-25 17:16:39 -0700

    Use SwSwitch::scopeResolver in tests
    Summary: As titled
    
    Reviewed By: phshaikh
    
    Differential Revision: D45255989
    
    fbshipit-source-id: 0c7e73459e5998e793d29326b56a8f78f920930b

--- fboss/agent/test/MirrorManagerTest.cpp
+++ fboss/agent/test/MirrorManagerTest.cpp
@@ -125,17 +125,13 @@ class MirrorManagerTest : public ::testing::Test {
   void updateState(folly::StringPiece name, StateUpdateFn func) {
     sw_->updateStateBlocking(name, func);
   }
-  SwitchIdScopeResolver scopeResolver() const {
-    return SwitchIdScopeResolver(
-        sw_->getState()->getSwitchSettings()->getSwitchIdToSwitchInfo());
-  }
 
   std::shared_ptr<SwitchState> addNewMirror(
       const std::shared_ptr<SwitchState>& state,
       std::shared_ptr<Mirror> mirror) {
     auto newState = state->clone();
     auto mirrors = newState->getMirrors()->modify(&newState);
-    mirrors->addNode(mirror, scopeResolver().scope(mirror));
+    mirrors->addNode(mirror, sw_->getScopeResolver()->scope(mirror));
     return newState;
   }
   std::shared_ptr<SwitchState> updateMirror(
@@ -143,7 +139,7 @@ class MirrorManagerTest : public ::testing::Test {
       std::shared_ptr<Mirror> mirror) {
     auto newState = state->clone();
     auto mirrors = newState->getMirrors()->modify(&newState);
-    mirrors->updateNode(mirror, scopeResolver().scope(mirror));
+    mirrors->updateNode(mirror, sw_->getScopeResolver()->scope(mirror));
     return newState;
   }
 
