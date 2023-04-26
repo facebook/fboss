bd8a16234c7adbede6394a5dbdf9740be82f8196
Priyank Warkhede <priyankw@meta.com>
2023-04-26 14:43:10 -0700

    Define fsdb config for DSF trustedSubnets
    Summary:
    This will be used for server side enforcement of transport privileges
    (TC marking, plaintext connections).
    
    Reviewed By: shri-khare
    
    Differential Revision: D44924359
    
    fbshipit-source-id: f2c1a5519cf779da40348db8c3f3f49775405ec1

--- fboss/fsdb/if/fsdb_config.thrift
+++ fboss/fsdb/if/fsdb_config.thrift
@@ -13,4 +13,7 @@ struct Config {
   2: map<fsdb_common.PublisherId, list<fsdb_oper.OperPath>> (
     cpp.template = 'std::unordered_map',
   ) operPublishers = {};
+  // trustedSubnets in CIDRNetwork format. Used for server side enforcement of transport privileges.
+  // only clients from trustedSubnets are allowed to connect with TC marking enabled
+  3: list<string> trustedSubnets = [];
 }
