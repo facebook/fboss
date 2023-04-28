4e113aad0198deb531318103c270b671b6d060ca
Jasmeet Bagga <jasmeetbagga@meta.com>
2023-04-28 09:30:53 -0700

    MultiDsfNodeMap - use base class add,remove, get, update API
    Summary: As titled
    
    Reviewed By: msomasundaran
    
    Differential Revision:
    D45376172
    
    Privacy Context Container: L1125642
    
    fbshipit-source-id: a9262d3f6f63a3f59e8265c9b4b3b47fd9a9a0a8

--- fboss/agent/state/DsfNodeMap.cpp
+++ fboss/agent/state/DsfNodeMap.cpp
@@ -7,18 +7,6 @@
 
 namespace facebook::fboss {
 
-void MultiDsfNodeMap::addNode(
-    std::shared_ptr<DsfNode> node,
-    const HwSwitchMatcher& matcher) {
-  const auto& key = matcher.matcherString();
-  auto mitr = find(key);
-  if (mitr == end()) {
-    mitr = insert(key, std::make_shared<DsfNodeMap>()).first;
-  }
-  auto& map = mitr->second;
-  map->addNode(std::move(node));
-}
-
 size_t MultiDsfNodeMap::numNodes() const {
   size_t cnt = 0;
   for (auto mnitr = cbegin(); mnitr != cend(); ++mnitr) {
@@ -27,35 +15,5 @@ size_t MultiDsfNodeMap::numNodes() const {
   return cnt;
 }
 
-void MultiDsfNodeMap::updateNode(
-    std::shared_ptr<DsfNode> node,
-    const HwSwitchMatcher& matcher) {
-  const auto& key = matcher.matcherString();
-  auto mitr = find(key);
-  if (mitr == end()) {
-    throw FbossError("No nodes found for switchIds: ", key);
-  }
-  auto& nodeMap = mitr->second;
-  nodeMap->updateNode(std::move(node));
-}
-
-void MultiDsfNodeMap::removeNode(SwitchID switchId) {
-  for (auto mitr = begin(); mitr != end(); ++mitr) {
-    if (mitr->second->remove(switchId)) {
-      return;
-    }
-  }
-  throw FbossError("Node not found: ", switchId);
-}
-
-std::shared_ptr<DsfNode> MultiDsfNodeMap::getNodeIf(SwitchID switchId) const {
-  for (auto mnitr = cbegin(); mnitr != cend(); ++mnitr) {
-    auto node = mnitr->second->getNodeIf(switchId);
-    if (node) {
-      return node;
-    }
-  }
-  return nullptr;
-}
 template class ThriftMapNode<DsfNodeMap, DsfNodeMapTraits>;
 } // namespace facebook::fboss
