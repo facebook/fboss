37a058dafcc9b769d1eae34db0190e849abf1c67
Jasmeet Bagga <jasmeetbagga@meta.com>
2023-05-02 15:00:02 -0700

    Start adding UTs for SwitchIdScopeResolver
    Summary: As titled
    
    Reviewed By: msomasundaran
    
    Differential Revision: D45472987
    
    fbshipit-source-id: 64ce8270e66a3609322f435ff9483e2077cf4719

---
+++ fboss/agent/test/SwitchIdScopeResolverTests.cpp
@@ -0,0 +1,41 @@
+// Copyright 2004-present Facebook. All Rights Reserved.
+
+#include "fboss/agent/HwSwitchMatcher.h"
+#include "fboss/agent/SwSwitch.h"
+#include "fboss/agent/SwitchIdScopeResolver.h"
+#include "fboss/agent/test/HwTestHandle.h"
+#include "fboss/agent/test/TestUtils.h"
+
+#include <gtest/gtest.h>
+
+using namespace facebook::fboss;
+
+class SwitchIdScopeResolverTest : public ::testing::Test {
+ public:
+  void SetUp() override {
+    auto config = testConfigA(cfg::SwitchType::VOQ);
+    handle_ = createTestHandle(&config);
+    sw_ = handle_->getSw();
+    sw_->initialConfigApplied(std::chrono::steady_clock::now());
+  }
+  HwSwitchMatcher l3SwitchMatcher() const {
+    return HwSwitchMatcher(std::unordered_set<SwitchID>{SwitchID{1}});
+  }
+  const SwitchIdScopeResolver& scopeResolver() const {
+    return *sw_->getScopeResolver();
+  }
+  SwSwitch* sw_;
+  std::unique_ptr<HwTestHandle> handle_;
+};
+
+TEST_F(SwitchIdScopeResolverTest, mirrorScope) {
+  EXPECT_EQ(l3SwitchMatcher(), scopeResolver().scope(cfg::Mirror{}));
+  EXPECT_EQ(
+      l3SwitchMatcher(), scopeResolver().scope(std::shared_ptr<Mirror>()));
+}
+
+TEST_F(SwitchIdScopeResolverTest, dsfNodeScope) {
+  EXPECT_EQ(l3SwitchMatcher(), scopeResolver().scope(cfg::DsfNode{}));
+  EXPECT_EQ(
+      l3SwitchMatcher(), scopeResolver().scope(std::shared_ptr<DsfNode>()));
+}
