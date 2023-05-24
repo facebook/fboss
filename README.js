4063a9d010723151e283099f6ab25d473b801de0
Questio computing Security work 3World in time zone <107187205+S-pegin@users.noreply.github.com>
2023-05-24 15:27:53 +0700

    Update README.js
    < ความปลอดภัยในระบบ " KEY/SSH/GUL/GUYS ">
     < แสกนการเข้าถึงในระบบพิเศษของ "/API/ROI/AI/PI/EI/BI/XI/CI/CD/">
    < โปรไฟล์ส่วนตัว " https://www.facebook.com/profile.php?id=100092209314694&sk=grid ">
    
      > ความสามารถ " https://developers.facebook.com/docs/ "
      > เอกสาร " https://developers.facebook.com/docs/ "
      > ระบบเอไอ" https://ai.facebook.com/tools/ "
      > แผงวงจรเอไอ " https://ai.facebook.com/tools/system-cards/ "
      > เครื่องมือ " https://developers.facebook.com/products/ "
      > แผนผังเว็บไซต์ตัวช่วย " https://developers.facebook.com/m/sitemap/ "
           > "https://ai.facebook.com/datasets/ " <>
    
    < โปรไฟล์สาธารณะ " https://www.facebook.com/profile.php?id=100092209314694 " >
    ที่ผู้ใช้งานในเฟสบุ๊คสามารถมองเห็นได้.

--- README.js
+++ README.js
@@ -1,2 +1,23 @@
-ลบข้อมูล ใน(https://developers.facebook.com/docs/)
-ยกเลิกการใช้งานของฉันและทั้งหมด
+< ความปลอดภัยในระบบ " KEY/SSH/GUL/GUYS ">
+ < แสกนการเข้าถึงในระบบพิเศษของ "/API/ROI/AI/PI/EI/BI/XI/CI/CD/">
+< โปรไฟล์ส่วนตัว " https://www.facebook.com/profile.php?id=100092209314694&sk=grid ">
+
+  > ความสามารถ " https://developers.facebook.com/docs/ "
+  > เอกสาร " https://developers.facebook.com/docs/ "
+  > ระบบเอไอ" https://ai.facebook.com/tools/ "
+  > แผงวงจรเอไอ " https://ai.facebook.com/tools/system-cards/ "
+  > เครื่องมือ " https://developers.facebook.com/products/ "
+  > แผนผังเว็บไซต์ตัวช่วย " https://developers.facebook.com/m/sitemap/ "
+       > "https://ai.facebook.com/datasets/ " <>
+
+< โปรไฟล์สาธารณะ " https://www.facebook.com/profile.php?id=100092209314694 " >
+ที่ผู้ใช้งานในเฟสบุ๊คสามารถมองเห็นได้.
+
+
+
+
+
+
+
+
+
