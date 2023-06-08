4b63ceb1c04a25db72f76fcf34ea4bdf1bd9ca99
Harshit Gulati <hgulati@meta.com>
2023-06-08 04:06:08 -0700

    Added cmake targets for Fb Fpga libraries
    Summary: as titled. Will use these libraries in next diffs to compile w400c in oss
    
    Reviewed By: srikrishnagopu
    
    Differential Revision: D46178020
    
    fbshipit-source-id: 611cba5347782dd1c1b8d2ca9e9e2540b7a12689

--- cmake/LibFpga.cmake
+++ cmake/LibFpga.cmake
@@ -10,3 +10,24 @@ add_library(fpga_multi_pim_container
 target_link_libraries(fpga_multi_pim_container
   fboss_error
 )
+
+add_library(facebook_fpga
+  fboss/lib/fpga/FbDomFpga.cpp
+  fboss/lib/fpga/FbFpgaRegisters.cpp
+)
+
+target_link_libraries(facebook_fpga
+  fboss_error
+  Folly::folly
+  fpga_device
+)
+
+add_library(fb_fpga_i2c
+  fboss/lib/fpga/FbFpgaI2c.cpp
+)
+
+target_link_libraries(fb_fpga_i2c
+  facebook_fpga
+  utils
+  Folly::folly
+)
