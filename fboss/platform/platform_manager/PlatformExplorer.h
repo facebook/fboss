d85c01d6f03e7dfbcef59af654b0bd2e85de62d2
Manikandan Somasundaram <manikandan@meta.com>
2023-06-09 11:05:51 -0700

    Bootstrapping exploration logic
    Reviewed By: tfangit
    
    Differential Revision:
    D46471206
    
    Privacy Context Container: L1125642
    
    fbshipit-source-id: e3292968c05d8e74e427d17b574828c318a4e35d

--- fboss/platform/platform_manager/PlatformExplorer.h
+++ fboss/platform/platform_manager/PlatformExplorer.h
@@ -6,6 +6,8 @@
 #include <folly/experimental/FunctionScheduler.h>
 
 #include "fboss/platform/config_lib/ConfigLib.h"
+#include "fboss/platform/platform_manager/PlatformI2cExplorer.h"
+#include "fboss/platform/platform_manager/PresenceDetector.h"
 #include "fboss/platform/platform_manager/gen-cpp2/platform_manager_config_types.h"
 
 namespace facebook::fboss::platform::platform_manager {
@@ -16,10 +18,32 @@ class PlatformExplorer {
       const std::string& configFile,
       const ConfigLib& configLib = ConfigLib());
   void explore();
+  void exploreFRU(
+      const std::string& parentFruName,
+      const std::string& parentSlotName,
+      const SlotConfig& parentSlot,
+      const std::string& fruTypeName,
+      const FruTypeConfig& fruTypeConfig);
+  void exploreI2cDevices(
+      const std::string& fruName,
+      const std::vector<I2cDeviceConfig>& i2cDeviceConfigs);
+  std::string getKernelI2cBusName(
+      const std::string& fruName,
+      const std::string& fruScopeBusName);
+  void updateKernelI2cBusNames(
+      const std::string& fruName,
+      const std::string& fruScopeBusName,
+      const std::string& kernelI2cBusName);
 
  private:
   folly::FunctionScheduler scheduler_;
   PlatformConfig platformConfig_{};
+  PlatformI2cExplorer i2cExplorer_{};
+  PresenceDetector presenceDetector_{};
+  // Map from <fruName, fruScopeBusName> to kernel i2c bus name.
+  // Example: <CHASSIS/PIMSlot0:PIM-8DD, INCOMING@3> -> i2c-54
+  std::map<std::pair<std::string, std::string>, std::string>
+      kernelI2cBusNames_{};
 };
 
 } // namespace facebook::fboss::platform::platform_manager
