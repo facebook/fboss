eeb41f104ffaeaaf58ab8cfbe1c48213f8d6de9b
Parvez Shaikh <pshaikh@meta.com>
2023-06-16 13:39:21 -0700

    apply thrift config to not query platform local mac address
    Summary:
    as titled, instead get this information from utils. PlatformInit functions internally invokes the same method to set local MacAddress as platform mac address
    
    this is another step to remove platform use in config apply
    
    Reviewed By: zechengh09
    
    Differential Revision:
    D46297419
    
    Privacy Context Container: L1125642
    
    fbshipit-source-id: 684fc2a780be82939aeb14fefb29755bd577dd77

--- fboss/agent/ApplyThriftConfig.cpp
+++ fboss/agent/ApplyThriftConfig.cpp
@@ -2239,7 +2239,7 @@ ThriftConfigApplier::getSystemLacpConfig() {
     // is not a compile-time constant (it is derived from the CPU mac),
     // the default value is defined here, instead of, say,
     // AggregatePortFields::kDefaultSystemID.
-    systemID = platform_->getLocalMac();
+    systemID = getLocalMacAddress();
     systemPriority = kDefaultSystemPriority;
   }
 
@@ -4156,7 +4156,7 @@ MacAddress ThriftConfigApplier::getInterfaceMac(const cfg::Interface* config) {
   if (auto mac = config->mac()) {
     return MacAddress(*mac);
   }
-  return platform_->getLocalMac();
+  return getLocalMacAddress();
 }
 
 Interface::Addresses ThriftConfigApplier::getInterfaceAddresses(
@@ -4169,7 +4169,7 @@ Interface::Addresses ThriftConfigApplier::getInterfaceAddresses(
   if (auto mac = config->mac()) {
     macAddr = folly::MacAddress(*mac);
   } else {
-    macAddr = platform_->getLocalMac();
+    macAddr = getLocalMacAddress();
   }
   const folly::IPAddressV6 v6llAddr(folly::IPAddressV6::LINK_LOCAL, macAddr);
   addrs.emplace(v6llAddr, kV6LinkLocalAddrMask);
