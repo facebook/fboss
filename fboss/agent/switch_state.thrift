a197c7d3854f767c1ca41880afacd146cd73d635
Shrikrishna (Shri) Khare <skhare@meta.com>
2023-07-05 13:51:01 -0700

    Introduce the notion of desired vs. actual SwitchDrainState
    Summary:
    It is possible to change SwitchDrainState to DRAINED/UNDRAINED via config.
    However, some VOQ switch implementations require the switch to be initialized
    as DRAINED and only UNDRAIN provided some configured number of fabric links are
    up.
    
    Thus, the config DRAIN/UNDRAIN (desired) may not always be the actual
    DRAIN/UNDRAIN state.
    
    This diff is towards introducing notion of desired vs. actual SwitchDrainState
    to accommodate this.
    
    Reviewed By: jasmeetbagga
    
    Differential Revision:
    D47199122
    
    Privacy Context Container: L1125642
    
    fbshipit-source-id: 632ca0c36219476223fc9a9ebd12d07de26831c0

--- fboss/agent/switch_state.thrift
+++ fboss/agent/switch_state.thrift
@@ -329,6 +329,7 @@ struct SwitchSettingsFields {
   29: map<i64, switch_config.SwitchInfo> switchIdToSwitchInfo;
   30: optional i32 minLinksToRemainInVOQDomain;
   31: optional i32 minLinksToJoinVOQDomain;
+  32: switch_config.SwitchDrainState desiredSwitchDrainState = switch_config.SwitchDrainState.UNDRAINED;
 }
 
 struct RoutePrefix {
