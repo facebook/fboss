d6be1944db43b23d1540f5f138274f1ce8c25f17
Priyank Warkhede <priyankw@meta.com>
2023-07-05 12:07:12 -0700

    test OpenR prefix advertisement and route programming
    Summary:
    Add multi-node integration test to advertise prefix via Open-R API
    and verify route programming in Agent.
    
    Reviewed By: msomasundaran
    
    Differential Revision: D47199200
    
    fbshipit-source-id: 1d623f7a3a86aed92ec90cb7e63a879d17412991

--- fboss/agent/test/MultiNodeOpenrTests.cpp
+++ fboss/agent/test/MultiNodeOpenrTests.cpp
@@ -17,6 +17,7 @@
 #include "fboss/agent/test/MultiNodeTest.h"
 #include "fboss/agent/test/TestUtils.h"
 #include "fboss/lib/CommonUtils.h"
+#include "openr/common/NetworkUtil.h"
 
 #include <openr/if/gen-cpp2/OpenrCtrlCpp.h>
 #include <memory>
@@ -28,12 +29,106 @@
 
 using namespace facebook::fboss;
 
+static constexpr int kOpenrThriftPort{2018};
+
+static auto constexpr kConnTimeout = 1000;
+static auto constexpr kRecvTimeout = 45000;
+static auto constexpr kSendTimeout = 5000;
+
+static const std::string& kTestPrefix = "123::/96";
+
+template <typename Client>
+std::unique_ptr<Client> createPlaintextClient(const int port) {
+  auto eb = folly::EventBaseManager::get()->getEventBase();
+  auto addr = folly::SocketAddress("::1", port);
+  auto sock = folly::AsyncSocket::newSocket(eb, addr, kConnTimeout);
+  sock->setSendTimeout(kSendTimeout);
+  auto channel =
+      apache::thrift::HeaderClientChannel::newChannel(std::move(sock));
+  channel->setTimeout(kRecvTimeout);
+  return std::make_unique<Client>(std::move(channel));
+}
+
+std::unique_ptr<apache::thrift::Client<openr::thrift::OpenrCtrlCpp>>
+getOpenrClient() {
+  return createPlaintextClient<
+      apache::thrift::Client<openr::thrift::OpenrCtrlCpp>>(kOpenrThriftPort);
+}
+
+void checkOpenrInitialization() {
+  bool initializationConverged = false;
+  checkWithRetry(
+      [&initializationConverged]() {
+        try {
+          auto client = getOpenrClient();
+          initializationConverged = client->sync_initializationConverged();
+          return true;
+        } catch (const std::exception& e) {
+          return false;
+        }
+      },
+      300, /* retries */
+      1s,
+      "Cannot connect to OpenR service");
+  EXPECT_TRUE(initializationConverged);
+}
+
+int checkOpenrNeighbors() {
+  int nbrCount = 0;
+  checkWithRetry(
+      [&nbrCount]() {
+        try {
+          std::vector<::openr::thrift::SparkNeighbor> nbrs;
+          auto client = getOpenrClient();
+          client->sync_getNeighbors(nbrs);
+          nbrCount = nbrs.size();
+          if (nbrCount >= 1) {
+            return true;
+          }
+        } catch (const std::exception& e) {
+          return false;
+        }
+        return false;
+      },
+      600, /* retries */
+      1s,
+      "Cannot connect to OpenR service");
+
+  return nbrCount;
+}
+
+void advertiseOpenrRoutes(std::vector<std::string> toAdd) {
+  std::vector<openr::thrift::PrefixEntry> prefixes;
+  for (const auto& entry : toAdd) {
+    openr::thrift::PrefixEntry prefixEntry;
+    *prefixEntry.prefix() = openr::toIpPrefix(entry);
+    prefixEntry.type() = openr::thrift::PrefixType::LOOPBACK;
+    prefixes.push_back(prefixEntry);
+  }
+
+  checkWithRetry(
+      [&]() {
+        try {
+          auto client = getOpenrClient();
+          client->sync_advertisePrefixes(std::move(prefixes));
+        } catch (const std::exception& e) {
+          return false;
+        }
+        return true;
+      },
+      600, /* retries */
+      1s,
+      "Cannot connect to OpenR service");
+}
+
 class MultiNodeOpenrTest : public MultiNodeTest {
  public:
   void SetUp() override {
     MultiNodeTest::SetUp();
     if (isDUT()) {
       verifyReachability();
+    } else {
+      advertiseOpenrRoutes({kTestPrefix});
     }
     auto addRoute = [this](auto ip, const auto& nhops) {
       auto updater = sw()->getRouteUpdater();
@@ -124,74 +219,46 @@ class MultiNodeOpenrTest : public MultiNodeTest {
 
     return config;
   }
-};
 
-static constexpr int kOpenrThriftPort{2018};
-
-static auto constexpr kConnTimeout = 1000;
-static auto constexpr kRecvTimeout = 45000;
-static auto constexpr kSendTimeout = 5000;
-
-template <typename Client>
-std::unique_ptr<Client> createPlaintextClient(const int port) {
-  auto eb = folly::EventBaseManager::get()->getEventBase();
-  auto addr = folly::SocketAddress("::1", port);
-  auto sock = folly::AsyncSocket::newSocket(eb, addr, kConnTimeout);
-  sock->setSendTimeout(kSendTimeout);
-  auto channel =
-      apache::thrift::HeaderClientChannel::newChannel(std::move(sock));
-  channel->setTimeout(kRecvTimeout);
-  return std::make_unique<Client>(std::move(channel));
-}
-
-std::unique_ptr<apache::thrift::Client<openr::thrift::OpenrCtrlCpp>>
-getOpenrClient() {
-  return createPlaintextClient<
-      apache::thrift::Client<openr::thrift::OpenrCtrlCpp>>(kOpenrThriftPort);
-}
-
-void checkOpenrInitialization() {
-  bool initializationConverged = false;
-  checkWithRetry(
-      [&initializationConverged]() {
-        try {
-          auto client = getOpenrClient();
-          initializationConverged = client->sync_initializationConverged();
-          return true;
-        } catch (const std::exception& e) {
-          return false;
+ protected:
+  template <typename TIpAddress>
+  void checkRoute(TIpAddress prefix, uint8_t length, bool exists) {
+    WITH_RETRIES({
+      const auto& fibContainer =
+          sw()->getState()->getFibs()->getNode(RouterID(0));
+      auto fib = fibContainer->template getFib<TIpAddress>();
+      auto testRoute = fib->getRouteIf({prefix, length});
+      if (exists) {
+        EXPECT_EVENTUALLY_NE(testRoute, nullptr);
+        if (testRoute) {
+          EXPECT_TRUE(testRoute->getEntryForClient(ClientID::OPENR));
         }
-      },
-      300, /* retries */
-      1s,
-      "Cannot connect to OpenR service");
-  EXPECT_TRUE(initializationConverged);
-}
+      } else {
+        EXPECT_EVENTUALLY_EQ(testRoute, nullptr);
+      }
+    });
+  }
+};
 
 TEST_F(MultiNodeOpenrTest, verifyNeighborDiscovery) {
   auto verify = [&]() {
     checkOpenrInitialization();
-    int nbrCount = 0;
-    checkWithRetry(
-        [&nbrCount]() {
-          try {
-            std::vector<::openr::thrift::SparkNeighbor> nbrs;
-            auto client = getOpenrClient();
-            client->sync_getNeighbors(nbrs);
-            nbrCount = nbrs.size();
-            if (nbrCount >= 1) {
-              return true;
-            }
-          } catch (const std::exception& e) {
-            return false;
-          }
-          return false;
-        },
-        600, /* retries */
-        1s,
-        "Cannot connect to OpenR service");
-
+    auto nbrCount = checkOpenrNeighbors();
     EXPECT_GE(nbrCount, 1);
   };
   verifyAcrossWarmBoots(verify);
+}
+
+TEST_F(MultiNodeOpenrTest, addOpenrRoute) {
+  auto setup = [&]() {
+    checkOpenrInitialization();
+    auto nbrCount = checkOpenrNeighbors();
+    EXPECT_GE(nbrCount, 1);
+  };
+  auto verify = [&]() {
+    auto prefix = folly::IPAddress::createNetwork(kTestPrefix);
+    checkRoute(prefix.first.asV6(), prefix.second, true);
+  };
+
+  verifyAcrossWarmBoots(setup, verify);
 }
