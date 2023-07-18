4ec3ed71e5ac16bab3bf92488bb7908482eeca7d
Rajan Kumar <rajank@meta.com>
2023-07-18 09:42:28 -0700

    IPHY Port toggle workaround for Minipack3
    Summary:
    The Minipack3 current Serdes firmware has issue where sometimes the port does not come up. The IPHY port toggle brings up the port. The diff adds this workaround for MP3 platform.
    
    NOTE: Remove this diff after new SDK 6.5.29 gets integrated as the new Serdes firmware in that SDK is expected to fix this problem inherently.
    
    Reviewed By: daiwei1983
    
    Differential Revision:
    D47529349
    
    Privacy Context Container: L1125642
    
    fbshipit-source-id: d8315501bc796d2e094c28c479a4c8578f8ae91b

--- fboss/agent/hw/sai/switch/npu/SaiPortManager.cpp
+++ fboss/agent/hw/sai/switch/npu/SaiPortManager.cpp
@@ -664,7 +664,12 @@ void SaiPortManager::programSerdes(
   // create if serdes doesn't exist or update existing serdes
   portHandle->serdes = store.setObject(serdesKey, serdesAttributes);
 
-  if (platform_->getAsic()->getAsicType() == cfg::AsicType::ASIC_TYPE_GARONNE) {
+  // TODO(daiweix): Remove the TH5 from the port toggle workaround after SDK
+  // 6.5.29 integration. The new Serdes firmware should take care of Serdes
+  // init issue
+  if (platform_->getAsic()->getAsicType() == cfg::AsicType::ASIC_TYPE_GARONNE ||
+      platform_->getAsic()->getAsicType() ==
+          cfg::AsicType::ASIC_TYPE_TOMAHAWK5) {
     /*
      * SI settings are not programmed to the hardware when the port is
      * created with admin UP. We need to explicitly toggle the admin
