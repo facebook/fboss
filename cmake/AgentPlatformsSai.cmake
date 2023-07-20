515af94e394ec55f1526af43891de14f8102a975
Srikrishna Gopu <srikrishnagopu@meta.com>
2023-07-19 17:44:41 -0700

    add led mapping as deps for sai platform
    Summary: As titled, add led mapping as deps for sai platform
    
    Reviewed By: shri-khare, harshitgulati18
    
    Differential Revision: D47583582
    
    fbshipit-source-id: f92a835f49500aa3fcd4276a819494ffdd7df64c

--- cmake/AgentPlatformsSai.cmake
+++ cmake/AgentPlatformsSai.cmake
@@ -108,6 +108,7 @@ target_link_libraries(sai_platform
   meru800bfa_platform_mapping
   montblanc_platform_mapping
   led_structs_types_cpp2
+  led_mapping_cpp2
 )
 
 set_target_properties(sai_platform PROPERTIES COMPILE_FLAGS
