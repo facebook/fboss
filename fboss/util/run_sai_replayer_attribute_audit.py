3b6d89a2bccd985f873e633c170f5765b5efce4b
Ron He <zecheng@meta.com>
2023-08-04 09:36:52 -0700

    Fix Sai replayer audit to latest form
    Summary:
    As titled. Updating Sai Replayer audit to latest form - we've improved replayer to use macro to define attributes.
    
    Will send out diffs to fix following missing attributes and then enable the check on-diff.
    
    Reviewed By: shri-khare
    
    Differential Revision:
    D48055748
    
    Privacy Context Container: L1125642
    
    fbshipit-source-id: 2283402ade39bf2c12a7f8cbfa908d99a44da054

--- fboss/util/run_sai_replayer_attribute_audit.py
+++ fboss/util/run_sai_replayer_attribute_audit.py
@@ -1,40 +1,59 @@
 #!/usr/bin/env python3
 
-# Run as python3 run_sai_replayer_attribute_audit.py
+# Run as python3 fboss/util/run_sai_replayer_audit.py
 #
 #
-# This script looks for Sai attributes that are not logged by Sai Replayer.
+# This script looks for SAI attributes that are currently not logged by Sai Replayer.
 # It works as follows:
-#   - For each Sai API, go to /agent/hw/sai/api/*Api.h to search for
-#     Sai Attributes.
-#   - Compare the ones appeared in /agent/hw/sai/tracer/*ApiTracer.h.
-#   - Print out those attributes not exist in Sai Replayer.
+#   - find all attributes defined in *Api.h
+#   - match them with attributes logged by Sai Replayer.
+#   - If not, flag and printout the diff.
 
-import re
 import subprocess
+import sys
+
+API_FILES_TO_SEARCH = "fboss/agent/hw/sai/api/*Api.h"
+TRACER_FILES_TO_SEARCH = "fboss/agent/hw/sai/tracer/*ApiTracer.cpp"
 
 
-def get_attr(file_path):
-    grep_command = f"grep -r '_ATTR_' {file_path}"
-    grep_lines = (
-        subprocess.run(grep_command, shell=True, capture_output=True)
+def get_used_attributes():
+    grep_cmd = f"grep SAI_ATTRIBUTE_NAME {API_FILES_TO_SEARCH}"
+    grep_output = (
+        subprocess.run(grep_cmd, shell=True, stdout=subprocess.PIPE)
         .stdout.decode("utf-8")
         .split("\n")[:-1]
     )
+    used_attributes = set()
+    for line in grep_output:
+        used_attributes.add(line.split("(")[1].split(")")[0])
+    return used_attributes
 
-    attributes = set()
 
-    for line in grep_lines:
-        for match in re.findall(r"SAI_\w+_ATTR\w+", line):
-            attributes.add(match)
-
-    return attributes
+def get_logged_attributes():
+    grep_cmd = f"grep -e SAI_ATTR_MAP -e SAI_EXT_ATTR_MAP {TRACER_FILES_TO_SEARCH}"
+    grep_output = (
+        subprocess.run(grep_cmd, shell=True, stdout=subprocess.PIPE)
+        .stdout.decode("utf-8")
+        .split("\n")[:-1]
+    )
+    logged_attributes = set()
+    for line in grep_output:
+        # Due to formatting, there could be multiple SAI_ATTR_MAP on single line
+        attributes = line.split(", SAI_ATTR_MAP")
+        for attr in attributes:
+            logged_attributes.add(attr.split("(")[1].split(")")[0])
+    return logged_attributes
 
 
 if __name__ == "__main__":
-    used_attributes = get_attr("fboss/agent/hw/sai/api/*Api.h")
-    logged_attributes = get_attr("fboss/agent/hw/sai/tracer/*ApiTracer.cpp")
-
+    used_attributes = get_used_attributes()
+    logged_attributes = get_logged_attributes()
     diff = used_attributes - logged_attributes
-    print("Attributes not logged by Sai Replayer:\n")
-    print(*sorted(diff), sep="\n")
+    if len(diff):
+        print("Attributes missing from Sai Replayer:")
+        for attr in sorted(diff):
+            print(f"    {attr}")
+        sys.exit(1)
+    else:
+        print("No missing attribute from Sai Replayer")
+        sys.exit(0)
