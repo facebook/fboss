ba8f5cd2748477de3699ef4c81f8ab45a0245009
Manikandan Somasundaram <manikandan@meta.com>
2023-08-15 17:59:07 -0700

    Use dmidecode to determine platform name
    Summary:
    If a --config_file is provided, that will take priority. If there is error in
    parsing this config_file, then the program exits.
    
    Else, the code will read dmidecode to get the platform name and load the
    corresponding config. If no config can be fetched for the inferred platform
    name, then the program exits.  If there is failure to read dmidecode, then the
    program exits.
    
    Differential Revision: D48073429
    
    fbshipit-source-id: fa41ca04ef0d908a5738b3a3e44eeda2a9b219a2

--- fboss/platform/platform_manager/Utils.cpp
+++ fboss/platform/platform_manager/Utils.cpp
@@ -5,23 +5,44 @@
 #include <folly/FileUtil.h>
 #include <folly/logging/xlog.h>
 #include <thrift/lib/cpp2/protocol/Serializer.h>
+#include <stdexcept>
 
 #include "fboss/platform/config_lib/ConfigLib.h"
+#include "fboss/platform/helpers/PlatformUtils.h"
 #include "fboss/platform/platform_manager/PlatformValidator.h"
+
+using namespace facebook::fboss::platform;
+
+namespace {
+
+std::string getPlatformNameFromBios() {
+  XLOG(INFO) << "Getting platform name from bios using dmedicode ...";
+  auto [exitStatus, standardOut] =
+      PlatformUtils().execCommand("dmidecode -s system-product-name");
+  if (exitStatus != 0) {
+    XLOG(ERR) << "Failed to get platform name from bios: " << stdout;
+    throw std::runtime_error("Failed to get platform name from bios");
+  }
+  standardOut = folly::trimWhitespace(standardOut).str();
+  XLOG(INFO) << "Platform name inferred from bios: " << standardOut;
+  return standardOut;
+}
+} // namespace
 
 namespace facebook::fboss::platform::platform_manager {
 
 PlatformConfig Utils::getConfig(const std::string& configFile) {
   std::string configJson;
   if (configFile.empty()) {
-    XLOG(INFO) << "No config file was provided. Inferring from config_lib";
-    configJson = ConfigLib().getPlatformManagerConfig();
+    XLOG(INFO) << "No config file was provided";
+    configJson =
+        ConfigLib().getPlatformManagerConfig(getPlatformNameFromBios());
   } else {
     XLOG(INFO) << "Using config file: " << configFile;
     if (!folly::readFile(configFile.c_str(), configJson)) {
-      XLOG(ERR) << "Can not find sensor config file: " + configFile;
+      XLOG(ERR) << "Can not find platform_manager config file: " + configFile;
       throw std::runtime_error(
-          "Can not find sensor config file: " + configFile);
+          "Can not find platform_manager config file: " + configFile);
     }
   }
 
