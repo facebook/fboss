b6d8d68a8ff65f70acc848a0682a3f36af3cbe77
Manikandan Somasundaram <manikandan@meta.com>
2023-08-15 17:59:07 -0700

    Include kernel name in bsp kmods rpm
    Differential Revision: D48168166
    
    fbshipit-source-id: 234c80f66c2a769756fe6bf720c14cfbbeed3c85

--- fboss/platform/platform_manager/PkgUtils.cpp
+++ fboss/platform/platform_manager/PkgUtils.cpp
@@ -6,13 +6,20 @@
 
 #include "fboss/platform/helpers/PlatformUtils.h"
 
+namespace {
+constexpr auto kHostKernelVersion = "`uname --kernel-release`";
+}
+
 namespace facebook::fboss::platform::platform_manager {
 
 void PkgUtils::run(const PlatformConfig& config) {
   XLOG(INFO) << "Installing BSP Kmods";
   runImpl(
       fmt::format(
-          "{}-{}", *config.bspKmodsRpmName(), *config.bspKmodsRpmVersion()),
+          "{}-{}-{}",
+          *config.bspKmodsRpmName(),
+          kHostKernelVersion,
+          *config.bspKmodsRpmVersion()),
       3 /* maxAttempts */);
 
   XLOG(INFO) << "Installing udev rules";
@@ -27,15 +34,15 @@ void PkgUtils::runImpl(const std::string& rpmFullName, int maxAttempts) {
   auto cmd = fmt::format("dnf install {} --assumeyes", rpmFullName);
   do {
     XLOG(INFO) << fmt::format(
-        "Running command: `{}`; Attempt: {}", cmd, attempt++);
+        "Running command ({}); Attempt: {}", cmd, attempt++);
     std::tie(exitStatus, standardOut) = PlatformUtils().execCommand(cmd);
     XLOG(INFO) << standardOut;
   } while (attempt <= maxAttempts && exitStatus != 0);
   if (exitStatus != 0) {
     XLOG(ERR) << fmt::format(
-        "Command `{}` failed with exit code {}", cmd, exitStatus);
+        "Command ({}) failed with exit code {}", cmd, exitStatus);
     throw std::runtime_error(fmt::format(
-        "Failed to install rpm `{}` with exit code {}",
+        "Failed to install rpm ({}) with exit code {}",
         rpmFullName,
         exitStatus));
   }
