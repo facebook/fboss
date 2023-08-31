756225edc679322b425740530e50fb6d6e0b8c1a
Priyank Warkhede <priyankw@meta.com>
2023-08-31 10:15:40 -0700

    config option to specify expected publisher paths
    Summary:
    Add config option to specify per-publisherId the list of publisher paths
    that are expected to be connected.
    This config will be used in next diffs to implement health checks on
    publisher streams.
    
    Reviewed By: peygar
    
    Differential Revision:
    D48795244
    
    Privacy Context Container: L1125642
    
    fbshipit-source-id: 7a6b8b1298c791df4da577cad1497ffd316e4281

--- fboss/fsdb/if/fsdb_config.thrift
+++ fboss/fsdb/if/fsdb_config.thrift
@@ -7,12 +7,24 @@ include "fboss/fsdb/if/fsdb_common.thrift"
 include "fboss/fsdb/if/fsdb_oper.thrift"
 include "thrift/annotation/cpp.thrift"
 
+struct PathConfig {
+  1: fsdb_oper.OperPath path;
+  2: bool isExpected; // whether stream is expected to be connected on this path in healthy state
+}
+
+struct PublisherConfig {
+  1: list<PathConfig> paths = [];
+}
+
 struct Config {
   @cpp.Type{template = "std::unordered_map"}
   1: map<string, string> defaultCommandLineArgs;
+  // TODO: deprecate operPublishers config in lieu of publishers{} after 1 push.
   @cpp.Type{template = "std::unordered_map"}
   2: map<fsdb_common.PublisherId, list<fsdb_oper.OperPath>> operPublishers = {};
   // trustedSubnets in CIDRNetwork format. Used for server side enforcement of transport privileges.
   // only clients from trustedSubnets are allowed to connect with TC marking enabled
   3: list<string> trustedSubnets = [];
+  @cpp.Type{template = "std::unordered_map"}
+  4: map<fsdb_common.PublisherId, PublisherConfig> publishers = {};
 }
