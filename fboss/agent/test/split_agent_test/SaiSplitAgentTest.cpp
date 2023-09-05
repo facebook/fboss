e354d6b5cd08bb5fa2212c913d6c3efca8fdf4cb
Ron He <zecheng@meta.com>
2023-09-05 16:58:01 -0700

    Add default logging and gtest init
    Summary: To be on the same level as HW tests.
    
    Reviewed By: jasmeetbagga
    
    Differential Revision: D48765089
    
    fbshipit-source-id: 80a20cc94899ca975541b113e3f669bcbbf74cc3

--- fboss/agent/test/split_agent_test/SaiSplitAgentTest.cpp
+++ fboss/agent/test/split_agent_test/SaiSplitAgentTest.cpp
@@ -3,7 +3,13 @@
 #include "fboss/agent/platforms/sai/SaiPlatformInit.h"
 #include "fboss/agent/test/AgentEnsemble.h"
 
+#include <folly/logging/Init.h>
+#include <gtest/gtest.h>
+
+FOLLY_INIT_LOGGING_CONFIG("fboss=DBG4; default:async=true");
+
 int main(int argc, char* argv[]) {
+  testing::InitGoogleTest(&argc, argv);
   return facebook::fboss::ensembleMain(
       argc, argv, facebook::fboss::initSaiPlatform);
 }
