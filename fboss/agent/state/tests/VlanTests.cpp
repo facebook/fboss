03f2fb2943aa2be3edbf455fd117a7a09e010b69
Shrikrishna (Shri) Khare <skhare@meta.com>
2023-09-05 23:05:36 -0700

    vlan.ApplyConfig: factor out helper lambda
    Summary:
    VOQ switches don't support VLANs.
    Thus, we are in the process of migrating wedge_agent to use neighbor tables
    from Interfaces.
    
    This diff: as titled.
    
    Useful for testing vlan nbr tables and interface nbr tables.
    
    Reviewed By: jasmeetbagga
    
    Differential Revision: D48941905
    
    fbshipit-source-id: ad812dbe32c8f4a84aab521568f41f96ecff698f

--- fboss/agent/state/tests/VlanTests.cpp
+++ fboss/agent/state/tests/VlanTests.cpp
@@ -55,232 +55,239 @@ void validateRespEntry(
 }
 
 TEST(Vlan, applyConfig) {
-  auto platform = createMockPlatform();
-  auto stateV0 = make_shared<SwitchState>();
-  auto vlanV0 = make_shared<Vlan>(VlanID(1234), kVlan1234);
+  auto applyConfigHelper = [](bool use_intf_nbr_tables) {
+    FLAGS_intf_nbr_tables = use_intf_nbr_tables;
 
-  stateV0->getVlans()->addNode(vlanV0, scope());
-  registerPort(stateV0, PortID(1), "port1", scope());
-  registerPort(stateV0, PortID(2), "port2", scope());
+    auto platform = createMockPlatform();
+    auto stateV0 = make_shared<SwitchState>();
+    auto vlanV0 = make_shared<Vlan>(VlanID(1234), kVlan1234);
 
-  NodeID nodeID = vlanV0->getNodeID();
-  EXPECT_EQ(0, vlanV0->getGeneration());
-  EXPECT_FALSE(vlanV0->isPublished());
-  EXPECT_EQ(VlanID(1234), vlanV0->getID());
-  Vlan::MemberPorts emptyPorts;
-  EXPECT_EQ(emptyPorts, vlanV0->getPorts());
+    stateV0->getVlans()->addNode(vlanV0, scope());
+    registerPort(stateV0, PortID(1), "port1", scope());
+    registerPort(stateV0, PortID(2), "port2", scope());
 
-  vlanV0->publish();
-  EXPECT_TRUE(vlanV0->isPublished());
+    NodeID nodeID = vlanV0->getNodeID();
+    EXPECT_EQ(0, vlanV0->getGeneration());
+    EXPECT_FALSE(vlanV0->isPublished());
+    EXPECT_EQ(VlanID(1234), vlanV0->getID());
+    Vlan::MemberPorts emptyPorts;
+    EXPECT_EQ(emptyPorts, vlanV0->getPorts());
 
-  cfg::SwitchConfig config;
-  config.ports()->resize(2);
-  preparedMockPortConfig(config.ports()[0], 1);
-  preparedMockPortConfig(config.ports()[1], 2);
-  config.vlans()->resize(1);
-  config.vlans()[0].id() = 1234;
-  config.vlans()[0].name() = kVlan1234;
-  config.vlans()[0].dhcpRelayAddressV4() = "30.1.1.1";
-  config.vlans()[0].dhcpRelayAddressV6() = "2a03:2880:10:1f07:face:b00c:0:0";
-  config.vlans()[0].dhcpRelayOverridesV4() = {};
-  (*config.vlans()[0].dhcpRelayOverridesV4())["02:00:00:00:00:02"] = "1.2.3.4";
-  config.vlans()[0].dhcpRelayOverridesV6() = {};
-  (*config.vlans()[0].dhcpRelayOverridesV6())["02:00:00:00:00:02"] =
-      "2a03:2880:10:1f07:face:b00c:0:0";
-  config.vlans()[0].intfID() = 1234;
-  config.vlanPorts()->resize(2);
-  config.vlanPorts()[0].logicalPort() = 1;
-  config.vlanPorts()[0].vlanID() = 1234;
-  config.vlanPorts()[0].emitTags() = false;
-  config.vlanPorts()[1].logicalPort() = 2;
-  config.vlanPorts()[1].vlanID() = 1234;
-  config.vlanPorts()[1].emitTags() = true;
+    vlanV0->publish();
+    EXPECT_TRUE(vlanV0->isPublished());
 
-  config.interfaces()->resize(1);
-  config.interfaces()[0].intfID() = 1234;
-  config.interfaces()[0].vlanID() = 1234;
+    cfg::SwitchConfig config;
+    config.ports()->resize(2);
+    preparedMockPortConfig(config.ports()[0], 1);
+    preparedMockPortConfig(config.ports()[1], 2);
+    config.vlans()->resize(1);
+    config.vlans()[0].id() = 1234;
+    config.vlans()[0].name() = kVlan1234;
+    config.vlans()[0].dhcpRelayAddressV4() = "30.1.1.1";
+    config.vlans()[0].dhcpRelayAddressV6() = "2a03:2880:10:1f07:face:b00c:0:0";
+    config.vlans()[0].dhcpRelayOverridesV4() = {};
+    (*config.vlans()[0].dhcpRelayOverridesV4())["02:00:00:00:00:02"] =
+        "1.2.3.4";
+    config.vlans()[0].dhcpRelayOverridesV6() = {};
+    (*config.vlans()[0].dhcpRelayOverridesV6())["02:00:00:00:00:02"] =
+        "2a03:2880:10:1f07:face:b00c:0:0";
+    config.vlans()[0].intfID() = 1234;
+    config.vlanPorts()->resize(2);
+    config.vlanPorts()[0].logicalPort() = 1;
+    config.vlanPorts()[0].vlanID() = 1234;
+    config.vlanPorts()[0].emitTags() = false;
+    config.vlanPorts()[1].logicalPort() = 2;
+    config.vlanPorts()[1].vlanID() = 1234;
+    config.vlanPorts()[1].emitTags() = true;
 
-  Vlan::MemberPorts expectedPorts;
-  expectedPorts.insert(std::make_pair(1, false));
-  expectedPorts.insert(std::make_pair(2, true));
+    config.interfaces()->resize(1);
+    config.interfaces()[0].intfID() = 1234;
+    config.interfaces()[0].vlanID() = 1234;
 
-  auto stateV1 = publishAndApplyConfig(stateV0, &config, platform.get());
-  auto vlanV1 = stateV1->getVlans()->getNode(VlanID(1234));
-  ASSERT_NE(nullptr, vlanV1);
-  auto vlanV1_byName = stateV1->getVlans()->getVlanSlow(kVlan1234);
-  EXPECT_EQ(vlanV1, vlanV1_byName);
-  EXPECT_EQ(nodeID, vlanV1->getNodeID());
-  EXPECT_EQ(1, vlanV1->getGeneration());
-  EXPECT_FALSE(vlanV1->isPublished());
-  EXPECT_EQ(VlanID(1234), vlanV1->getID());
-  EXPECT_EQ(kVlan1234, vlanV1->getName());
-  EXPECT_EQ(expectedPorts, vlanV1->getPorts());
-  EXPECT_EQ(0, vlanV1->getArpResponseTable()->size());
-  EXPECT_EQ(InterfaceID(1234), vlanV1->getInterfaceID());
-  EXPECT_EQ(folly::IPAddressV4("30.1.1.1"), vlanV1->getDhcpV4Relay());
-  EXPECT_EQ(
-      folly::IPAddressV6("2a03:2880:10:1f07:face:b00c:0:0"),
-      vlanV1->getDhcpV6Relay());
+    Vlan::MemberPorts expectedPorts;
+    expectedPorts.insert(std::make_pair(1, false));
+    expectedPorts.insert(std::make_pair(2, true));
 
-  auto map4 = vlanV1->getDhcpV4RelayOverrides();
-  EXPECT_EQ(
-      IPAddressV4("1.2.3.4"),
-      IPAddressV4(map4[MacAddress("02:00:00:00:00:02")]));
-  auto map6 = vlanV1->getDhcpV6RelayOverrides();
-  EXPECT_EQ(
-      IPAddressV6("2a03:2880:10:1f07:face:b00c:0:0"),
-      IPAddressV6(map6[MacAddress("02:00:00:00:00:02")]));
+    auto stateV1 = publishAndApplyConfig(stateV0, &config, platform.get());
+    auto vlanV1 = stateV1->getVlans()->getNode(VlanID(1234));
+    ASSERT_NE(nullptr, vlanV1);
+    auto vlanV1_byName = stateV1->getVlans()->getVlanSlow(kVlan1234);
+    EXPECT_EQ(vlanV1, vlanV1_byName);
+    EXPECT_EQ(nodeID, vlanV1->getNodeID());
+    EXPECT_EQ(1, vlanV1->getGeneration());
+    EXPECT_FALSE(vlanV1->isPublished());
+    EXPECT_EQ(VlanID(1234), vlanV1->getID());
+    EXPECT_EQ(kVlan1234, vlanV1->getName());
+    EXPECT_EQ(expectedPorts, vlanV1->getPorts());
+    EXPECT_EQ(0, vlanV1->getArpResponseTable()->size());
+    EXPECT_EQ(InterfaceID(1234), vlanV1->getInterfaceID());
+    EXPECT_EQ(folly::IPAddressV4("30.1.1.1"), vlanV1->getDhcpV4Relay());
+    EXPECT_EQ(
+        folly::IPAddressV6("2a03:2880:10:1f07:face:b00c:0:0"),
+        vlanV1->getDhcpV6Relay());
 
-  // Applying the same config again should return null
-  EXPECT_EQ(nullptr, publishAndApplyConfig(stateV1, &config, platform.get()));
+    auto map4 = vlanV1->getDhcpV4RelayOverrides();
+    EXPECT_EQ(
+        IPAddressV4("1.2.3.4"),
+        IPAddressV4(map4[MacAddress("02:00:00:00:00:02")]));
+    auto map6 = vlanV1->getDhcpV6RelayOverrides();
+    EXPECT_EQ(
+        IPAddressV6("2a03:2880:10:1f07:face:b00c:0:0"),
+        IPAddressV6(map6[MacAddress("02:00:00:00:00:02")]));
 
-  // Add an interface
-  config.interfaces()->resize(2);
-  *config.interfaces()[1].intfID() = 1;
-  *config.interfaces()[1].routerID() = 0;
-  *config.interfaces()[1].vlanID() = 1;
-  config.interfaces()[1].ipAddresses()->resize(2);
-  config.interfaces()[1].ipAddresses()[0] = "10.1.1.1/24";
-  config.interfaces()[1].ipAddresses()[1] =
-      "2a03:2880:10:1f07:face:b00c:0:0/96";
-  config.vlans()->resize(2);
-  config.vlans()[1].id() = 1;
-  config.vlans()[1].name() = "vlan1";
-  config.vlans()[1].intfID() = 1;
+    // Applying the same config again should return null
+    EXPECT_EQ(nullptr, publishAndApplyConfig(stateV1, &config, platform.get()));
 
-  auto stateV2 = publishAndApplyConfig(stateV1, &config, platform.get());
-  auto vlanV2 = stateV2->getVlans()->getNode(VlanID(1));
-  EXPECT_EQ(0, vlanV2->getGeneration());
-  EXPECT_FALSE(vlanV2->isPublished());
-  EXPECT_EQ(VlanID(1), vlanV2->getID());
-  EXPECT_EQ("vlan1", vlanV2->getName());
-  EXPECT_EQ(0, vlanV2->getPorts().size());
-  EXPECT_EQ(InterfaceID(1), vlanV2->getInterfaceID());
-  // Check the ArpResponseTable
-  auto arpRespTable = vlanV2->getArpResponseTable();
-  EXPECT_EQ(arpRespTable->size(), 1);
-  validateRespEntry(
-      arpRespTable->getEntry(IPAddressV4("10.1.1.1")),
-      MockPlatform::getMockLocalMac(),
-      InterfaceID(1));
-  // Check the NdpResponseTable
-  auto ndpRespTable = vlanV2->getNdpResponseTable();
-  EXPECT_EQ(ndpRespTable->size(), 2);
-  validateRespEntry(
-      ndpRespTable->getEntry(IPAddressV6("2a03:2880:10:1f07:face:b00c:0:0")),
-      MockPlatform::getMockLocalMac(),
-      InterfaceID(1));
+    // Add an interface
+    config.interfaces()->resize(2);
+    *config.interfaces()[1].intfID() = 1;
+    *config.interfaces()[1].routerID() = 0;
+    *config.interfaces()[1].vlanID() = 1;
+    config.interfaces()[1].ipAddresses()->resize(2);
+    config.interfaces()[1].ipAddresses()[0] = "10.1.1.1/24";
+    config.interfaces()[1].ipAddresses()[1] =
+        "2a03:2880:10:1f07:face:b00c:0:0/96";
+    config.vlans()->resize(2);
+    config.vlans()[1].id() = 1;
+    config.vlans()[1].name() = "vlan1";
+    config.vlans()[1].intfID() = 1;
 
-  // The link-local IPv6 address should also have been automatically added
-  // to the NDP response table.
-  validateRespEntry(
-      ndpRespTable->getEntry(MockPlatform::getMockLinkLocalIp6()),
-      MockPlatform::getMockLocalMac(),
-      InterfaceID(1));
+    auto stateV2 = publishAndApplyConfig(stateV1, &config, platform.get());
+    auto vlanV2 = stateV2->getVlans()->getNode(VlanID(1));
+    EXPECT_EQ(0, vlanV2->getGeneration());
+    EXPECT_FALSE(vlanV2->isPublished());
+    EXPECT_EQ(VlanID(1), vlanV2->getID());
+    EXPECT_EQ("vlan1", vlanV2->getName());
+    EXPECT_EQ(0, vlanV2->getPorts().size());
+    EXPECT_EQ(InterfaceID(1), vlanV2->getInterfaceID());
+    // Check the ArpResponseTable
+    auto arpRespTable = vlanV2->getArpResponseTable();
+    EXPECT_EQ(arpRespTable->size(), 1);
+    validateRespEntry(
+        arpRespTable->getEntry(IPAddressV4("10.1.1.1")),
+        MockPlatform::getMockLocalMac(),
+        InterfaceID(1));
+    // Check the NdpResponseTable
+    auto ndpRespTable = vlanV2->getNdpResponseTable();
+    EXPECT_EQ(ndpRespTable->size(), 2);
+    validateRespEntry(
+        ndpRespTable->getEntry(IPAddressV6("2a03:2880:10:1f07:face:b00c:0:0")),
+        MockPlatform::getMockLocalMac(),
+        InterfaceID(1));
 
-  // Add another vlan and interface
-  config.vlans()->resize(3);
-  *config.vlans()[2].id() = 1299;
-  *config.vlans()[2].name() = kVlan1299;
-  config.vlans()[2].intfID() = 1299;
-  config.interfaces()->resize(3);
-  *config.interfaces()[2].intfID() = 1299;
-  *config.interfaces()[2].routerID() = 0;
-  *config.interfaces()[2].vlanID() = 1299;
-  config.interfaces()[2].ipAddresses()->resize(2);
-  config.interfaces()[2].ipAddresses()[0] = "10.1.10.1/24";
-  config.interfaces()[2].ipAddresses()[1] = "192.168.0.1/31";
-  MacAddress intf2Mac("02:01:02:ab:cd:78");
-  config.interfaces()[2].mac() = intf2Mac.toString();
-  auto stateV3 = publishAndApplyConfig(stateV2, &config, platform.get());
-  auto vlanV3 = stateV3->getVlans()->getNode(VlanID(1299));
-  EXPECT_EQ(0, vlanV3->getGeneration());
-  EXPECT_FALSE(vlanV3->isPublished());
-  EXPECT_EQ(VlanID(1299), vlanV3->getID());
-  EXPECT_EQ(kVlan1299, vlanV3->getName());
-  EXPECT_EQ(InterfaceID(1299), vlanV3->getInterfaceID());
+    // The link-local IPv6 address should also have been automatically added
+    // to the NDP response table.
+    validateRespEntry(
+        ndpRespTable->getEntry(MockPlatform::getMockLinkLocalIp6()),
+        MockPlatform::getMockLocalMac(),
+        InterfaceID(1));
 
-  // Check the ArpResponseTable
-  arpRespTable = vlanV3->getArpResponseTable();
-  EXPECT_EQ(arpRespTable->size(), 2);
-  validateRespEntry(
-      arpRespTable->getEntry(IPAddressV4("10.1.10.1")),
-      intf2Mac,
-      InterfaceID(1299));
-  validateRespEntry(
-      arpRespTable->getEntry(IPAddressV4("192.168.0.1")),
-      intf2Mac,
-      InterfaceID(1299));
+    // Add another vlan and interface
+    config.vlans()->resize(3);
+    *config.vlans()[2].id() = 1299;
+    *config.vlans()[2].name() = kVlan1299;
+    config.vlans()[2].intfID() = 1299;
+    config.interfaces()->resize(3);
+    *config.interfaces()[2].intfID() = 1299;
+    *config.interfaces()[2].routerID() = 0;
+    *config.interfaces()[2].vlanID() = 1299;
+    config.interfaces()[2].ipAddresses()->resize(2);
+    config.interfaces()[2].ipAddresses()[0] = "10.1.10.1/24";
+    config.interfaces()[2].ipAddresses()[1] = "192.168.0.1/31";
+    MacAddress intf2Mac("02:01:02:ab:cd:78");
+    config.interfaces()[2].mac() = intf2Mac.toString();
+    auto stateV3 = publishAndApplyConfig(stateV2, &config, platform.get());
+    auto vlanV3 = stateV3->getVlans()->getNode(VlanID(1299));
+    EXPECT_EQ(0, vlanV3->getGeneration());
+    EXPECT_FALSE(vlanV3->isPublished());
+    EXPECT_EQ(VlanID(1299), vlanV3->getID());
+    EXPECT_EQ(kVlan1299, vlanV3->getName());
+    EXPECT_EQ(InterfaceID(1299), vlanV3->getInterfaceID());
 
-  // The new interface has no IPv6 address, but the NDP table should still
-  // be updated with the link-local address.
-  ndpRespTable = vlanV3->getNdpResponseTable();
-  EXPECT_EQ(ndpRespTable->size(), 1);
-  validateRespEntry(
-      ndpRespTable->getEntry(IPAddressV6("fe80::1:02ff:feab:cd78")),
-      intf2Mac,
-      InterfaceID(1299));
+    // Check the ArpResponseTable
+    arpRespTable = vlanV3->getArpResponseTable();
+    EXPECT_EQ(arpRespTable->size(), 2);
+    validateRespEntry(
+        arpRespTable->getEntry(IPAddressV4("10.1.10.1")),
+        intf2Mac,
+        InterfaceID(1299));
+    validateRespEntry(
+        arpRespTable->getEntry(IPAddressV4("192.168.0.1")),
+        intf2Mac,
+        InterfaceID(1299));
 
-  // Add a new VLAN with an ArpResponseTable that needs to be set up
-  // when the VLAN is first created
-  config.vlans()->resize(4);
-  *config.vlans()[3].id() = 99;
-  *config.vlans()[3].name() = kVlan99;
-  config.vlans()[3].intfID() = 99;
-  config.interfaces()->resize(4);
-  *config.interfaces()[3].intfID() = 99;
-  *config.interfaces()[3].routerID() = 1;
-  *config.interfaces()[3].vlanID() = 99;
-  config.interfaces()[3].ipAddresses()->resize(2);
-  config.interfaces()[3].ipAddresses()[0] = "1.2.3.4/24";
-  config.interfaces()[3].ipAddresses()[1] = "10.0.0.1/9";
-  auto stateV4 = publishAndApplyConfig(stateV3, &config, platform.get());
-  ASSERT_NE(nullptr, stateV4);
-  // VLAN 1 should be unchanged
-  EXPECT_EQ(vlanV2, stateV4->getVlans()->getNode(VlanID(1)));
-  auto vlan99 = stateV4->getVlans()->getNode(VlanID(99));
-  auto vlan99_byName = stateV4->getVlans()->getVlanSlow(kVlan99);
-  ASSERT_NE(nullptr, vlan99);
-  EXPECT_EQ(vlan99, vlan99_byName);
-  EXPECT_EQ(0, vlan99->getGeneration());
-  EXPECT_EQ(InterfaceID(99), vlan99->getInterfaceID());
+    // The new interface has no IPv6 address, but the NDP table should still
+    // be updated with the link-local address.
+    ndpRespTable = vlanV3->getNdpResponseTable();
+    EXPECT_EQ(ndpRespTable->size(), 1);
+    validateRespEntry(
+        ndpRespTable->getEntry(IPAddressV6("fe80::1:02ff:feab:cd78")),
+        intf2Mac,
+        InterfaceID(1299));
 
-  EXPECT_EQ(vlan99->getArpResponseTable()->size(), 2);
-  validateRespEntry(
-      vlan99->getArpResponseTable()->getEntry(IPAddressV4("1.2.3.4")),
-      MockPlatform::getMockLocalMac(),
-      InterfaceID(99));
-  validateRespEntry(
-      vlan99->getArpResponseTable()->getEntry(IPAddressV4("10.0.0.1")),
-      MockPlatform::getMockLocalMac(),
-      InterfaceID(99));
+    // Add a new VLAN with an ArpResponseTable that needs to be set up
+    // when the VLAN is first created
+    config.vlans()->resize(4);
+    *config.vlans()[3].id() = 99;
+    *config.vlans()[3].name() = kVlan99;
+    config.vlans()[3].intfID() = 99;
+    config.interfaces()->resize(4);
+    *config.interfaces()[3].intfID() = 99;
+    *config.interfaces()[3].routerID() = 1;
+    *config.interfaces()[3].vlanID() = 99;
+    config.interfaces()[3].ipAddresses()->resize(2);
+    config.interfaces()[3].ipAddresses()[0] = "1.2.3.4/24";
+    config.interfaces()[3].ipAddresses()[1] = "10.0.0.1/9";
+    auto stateV4 = publishAndApplyConfig(stateV3, &config, platform.get());
+    ASSERT_NE(nullptr, stateV4);
+    // VLAN 1 should be unchanged
+    EXPECT_EQ(vlanV2, stateV4->getVlans()->getNode(VlanID(1)));
+    auto vlan99 = stateV4->getVlans()->getNode(VlanID(99));
+    auto vlan99_byName = stateV4->getVlans()->getVlanSlow(kVlan99);
+    ASSERT_NE(nullptr, vlan99);
+    EXPECT_EQ(vlan99, vlan99_byName);
+    EXPECT_EQ(0, vlan99->getGeneration());
+    EXPECT_EQ(InterfaceID(99), vlan99->getInterfaceID());
 
-  // Check vlan congfig with no intfID set
-  config.vlans()->resize(5);
-  *config.vlans()[4].id() = 100;
-  config.vlans()[4].intfID().reset();
-  config.interfaces()->resize(5);
-  *config.interfaces()[4].intfID() = 100;
-  *config.interfaces()[4].routerID() = 0;
-  *config.interfaces()[4].vlanID() = 100;
-  config.interfaces()[4].ipAddresses()->resize(2);
-  config.interfaces()[4].ipAddresses()[0] = "10.50.3.7/24";
-  config.interfaces()[4].ipAddresses()[1] = "10.50.0.3/9";
-  auto stateV5 = publishAndApplyConfig(stateV4, &config, platform.get());
-  ASSERT_NE(nullptr, stateV5);
-  auto vlan100 = stateV5->getVlans()->getNode(VlanID(100));
-  EXPECT_EQ(InterfaceID(100), vlan100->getInterfaceID());
+    EXPECT_EQ(vlan99->getArpResponseTable()->size(), 2);
+    validateRespEntry(
+        vlan99->getArpResponseTable()->getEntry(IPAddressV4("1.2.3.4")),
+        MockPlatform::getMockLocalMac(),
+        InterfaceID(99));
+    validateRespEntry(
+        vlan99->getArpResponseTable()->getEntry(IPAddressV4("10.0.0.1")),
+        MockPlatform::getMockLocalMac(),
+        InterfaceID(99));
 
-  // Change DHCP relay configuration
-  config.vlans()[0].dhcpRelayAddressV4() = "30.1.1.2";
-  config.vlans()[0].dhcpRelayAddressV6() = "2a03:2880:10:1f07:face:b00c:0:2";
-  auto stateV6 = publishAndApplyConfig(stateV5, &config, platform.get());
-  auto vlanV6 = stateV6->getVlans()->getNode(VlanID(1234));
+    // Check vlan congfig with no intfID set
+    config.vlans()->resize(5);
+    *config.vlans()[4].id() = 100;
+    config.vlans()[4].intfID().reset();
+    config.interfaces()->resize(5);
+    *config.interfaces()[4].intfID() = 100;
+    *config.interfaces()[4].routerID() = 0;
+    *config.interfaces()[4].vlanID() = 100;
+    config.interfaces()[4].ipAddresses()->resize(2);
+    config.interfaces()[4].ipAddresses()[0] = "10.50.3.7/24";
+    config.interfaces()[4].ipAddresses()[1] = "10.50.0.3/9";
+    auto stateV5 = publishAndApplyConfig(stateV4, &config, platform.get());
+    ASSERT_NE(nullptr, stateV5);
+    auto vlan100 = stateV5->getVlans()->getNode(VlanID(100));
+    EXPECT_EQ(InterfaceID(100), vlan100->getInterfaceID());
 
-  EXPECT_EQ(folly::IPAddressV4("30.1.1.2"), vlanV6->getDhcpV4Relay());
-  EXPECT_EQ(
-      folly::IPAddressV6("2a03:2880:10:1f07:face:b00c:0:2"),
-      vlanV6->getDhcpV6Relay());
+    // Change DHCP relay configuration
+    config.vlans()[0].dhcpRelayAddressV4() = "30.1.1.2";
+    config.vlans()[0].dhcpRelayAddressV6() = "2a03:2880:10:1f07:face:b00c:0:2";
+    auto stateV6 = publishAndApplyConfig(stateV5, &config, platform.get());
+    auto vlanV6 = stateV6->getVlans()->getNode(VlanID(1234));
+
+    EXPECT_EQ(folly::IPAddressV4("30.1.1.2"), vlanV6->getDhcpV4Relay());
+    EXPECT_EQ(
+        folly::IPAddressV6("2a03:2880:10:1f07:face:b00c:0:2"),
+        vlanV6->getDhcpV6Relay());
+  };
+
+  applyConfigHelper(false /* Use VLAN neighbor tables */);
 }
 
 /*
