f1153ede0f7d62f52c9e02e3b4d58617bfc3aa4d
Srikrishna Gopu <srikrishnagopu@meta.com>
2023-09-21 01:09:52 -0700

    getAsicSdkVersion utility
    Summary:
    As titled, implemented getAsicSdkVersion utility which converts the SDK version into a long which can be compared easily similar to SAI_API_VERSION.
    
    Any SDK >= 1.65.0 will have stream type as UNICAST by default. This will help and prevent us from modifying the SDK version check every time a new SDK is released.
    
    Read the detailed summary in D49358544 for more details.
    
    Reviewed By: msomasundaran
    
    Differential Revision: D49358545
    
    fbshipit-source-id: 06dd6a9c0b7c2b9ba4999e7e90a6a4ee9c6f2309

--- fboss/agent/hw/sai/impl/util.cpp
+++ fboss/agent/hw/sai/impl/util.cpp
@@ -1,6 +1,8 @@
 // Copyright 2004-present Facebook. All Rights Reserved.
 
 #include "fboss/agent/hw/sai/impl/util.h"
+#include <glog/logging.h>
+#include <vector>
 
 namespace facebook::fboss {
 void findMajorMinorVersions(
@@ -25,4 +27,32 @@ void findMajorMinorVersions(
     minor.push_back('0');
   }
 }
+
+uint64_t getAsicSdkVersion(const std::string& sdkVersion) {
+  std::string delim = ".";
+
+  // SDK version is in the format of X.Y.Z
+  // Major
+  auto start = 0U;
+  auto end = sdkVersion.find(delim);
+  std::vector<uint16_t> tokens = {0, 0, 0};
+  for (auto index = 0; end != std::string::npos; index++) {
+    tokens[index] = std::stoi((sdkVersion.substr(start, end - start)));
+    start = end + delim.length();
+    end = sdkVersion.find(delim, start);
+  }
+
+  // Patch number is optional and may be part of the sdk version string.
+  // Ignore the patch number if it exists.
+  end = sdkVersion.find("-", start);
+  end = (end == std::string::npos) ? sdkVersion.length() : end;
+  tokens[2] = std::stoi((sdkVersion.substr(start, end - start)));
+
+  CHECK_EQ(tokens.size(), 3);
+  // Ignore the patch number and convert to uint64
+  // 0 represent major, 1 represents minor, 2 represents micro
+  auto asicSdkVersion = ASIC_SDK_VERSION(tokens[0], tokens[1], tokens[2]);
+  return asicSdkVersion;
+}
+
 } // namespace facebook::fboss
--- fboss/agent/hw/sai/impl/util.h
+++ fboss/agent/hw/sai/impl/util.h
@@ -6,10 +6,17 @@
 
 namespace facebook::fboss {
 
+#if !defined(ASIC_SDK_VERSION)
+#define ASIC_SDK_VERSION(major, minor, micro) \
+  (100000 * (major) + 1000 * (minor) + (micro))
+#endif
+
 void findMajorMinorVersions(
     const std::string& ver,
     std::string& major,
     std::string& minor,
     int majorLength);
+
+uint64_t getAsicSdkVersion(const std::string& sdkVersion);
 
 } // namespace facebook::fboss
