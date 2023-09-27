dce83ba8c5b78b69f3777f7698b88f333bd3d1fa
Peyman Gardideh <pgardideh@meta.com>
2023-09-27 13:31:12 -0700

    Condensed delta structs (patch)
    Summary:
    ## Overview
    
    At a high level, currently we encode as `list<pair<path, val>>`, where `path = list<string>`. This works fine for a single delta, but as you might expect we end up wasting a bunch of space when emitting granular deltas deep in the state tree. As an example, if I have a struct at root/a/b/c and want to change three vals (val1, val2, val3), this will be encoded as:
    ```
    [
      (["a", "b", "c", "val1"], newVal1)
      (["a", "b", "c", "val2"], newVal2)
      (["a", "b", "c", "val3"], newVal3)
    ]
    ```
    There is some clear duplication in the paths here, the goal of this library is to reduce this and get some memory savings. Here I'm adding a recursive tree definition for deltas. With this definition I can get the same delta above as:
    
    ```
    {
      "a": {
        "b": {
          "c": {
            "val1": newVal1,
            "val2": newVal2,
            "val3": newVal3,
          }
        }
      }
    }
    ```
    There by reducing duplication and getting some memory/bandwidth savings. I've also thrown away the concept of old val here since we don't really use it. As such naming the concept as Patch.
    
    ## Implementation Details
    
    I split up patches by type of the parent we're patching for a couple reasons. Firstly, we get the benefit of representing id paths into structs/lists directly as integrals instead of string which should give us some space savings. Second, I think we can get more customization later with how we encode deltas for things like lists (i.e. add/remove operations). Also some extra type class information never hurt anyone
    
    You can't have optionals in unions so I'm encoding deletions using an explicit type, denoted using an empty byte
    
    In coming diffs I will introduce a PatchBuilder to create a Patch out of two cow nodes and a PatchApplier to apply said Patch to a cow node.
    
    With since we have new types to emit, I'll have to create new apis in FSDB to support pub/sub
    
    ## Appendix
    
    Original document: https://docs.google.com/document/d/1RcauAQJYIJJAotIcvCcl9aNw19L-jw9Gls1R_cV9DLw/edit#heading=h.eucaehiqf4j6
    
    I couldn't use thrift.Any since it's still in experimental. It looks like it hasn't been worked on in a while (due to layoffs) so I'm a bit hesitant now to use it. Moreover since Any encodes type info we'll end up using more memory and since we're now seeing mem issues I think we can shelf the idea for now. Since our patches are split by type class, we end up getting some extra type info anyways
    
    Differential Revision:
    D47135229
    
    Privacy Context Container: L1125642
    
    fbshipit-source-id: 4de386b103c95743e9876546cbba78eff1e7a3fa

---
+++ fboss/thrift_cow/patch.thrift
@@ -0,0 +1,44 @@
+include "thrift/annotation/thrift.thrift"
+
+namespace py3 neteng.fboss.thrift_cow
+namespace py neteng.fboss.thrift_cow.patch
+namespace py.asyncio neteng.fboss.asyncio.thrift_cow.patch
+namespace cpp2 facebook.fboss.thrift_cow
+namespace go facebook.fboss.thrift_cow
+
+typedef binary (cpp.type = "folly::IOBuf") ByteBuffer
+typedef byte Empty
+
+struct StructPatch {
+  1: map<i16, PatchNode> children;
+}
+
+struct MapPatch {
+  1: map<string, PatchNode> children;
+}
+
+struct ListPatch {
+  1: map<i32, PatchNode> children;
+}
+
+// keys for set children are the actual value. PatchNode will be either a val or del
+// TODO: split this to an added + removed sets
+struct SetPatch {
+  1: map<string, PatchNode> children;
+}
+
+struct VariantPatch {
+  1: i16 id;
+  @thrift.Box // avoid circular dep
+  2: optional PatchNode child;
+}
+
+union PatchNode {
+  1: Empty del;
+  2: ByteBuffer val;
+  3: StructPatch struct_node;
+  4: MapPatch map_node;
+  5: ListPatch list_node;
+  6: SetPatch set_node;
+  7: VariantPatch variant_node;
+}
