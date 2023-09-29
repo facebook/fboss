3f00da1ca6b4a567413620464fafab151b455bd8
Justin Kim <jkimdo@meta.com>
2023-09-29 10:47:39 -0700

    Define a generalized thrift struct for presence LED support
    Summary:
    Looking into provide a generic presence LED programming for Darwin and DSF.
    
    I'm starting with defining a thrift struct that will replace current [Darwin](https://fburl.com/code/j2hd3rm5)/[DSF](https://www.internalfb.com/intern/diff/view-changeset/?destination_changeset_fbid=605138385139565) platform config.
    
    Overall idea still remains the same:
    1. FRU modules field to iterate over to check their presence
    2. LED configuration for each slot.
    
    But modification in details to make codes simpler.
    
    [Doc](https://docs.google.com/document/d/1JyPVxMuXJ-zh1DqdCJ6U1Zh161oyB-sixRj2T863_Qg/edit?usp=sharing) I've been dumping stuff as a reference.
    
    Reviewed By: somasun
    
    Differential Revision: D48981085
    
    fbshipit-source-id: c5177c68b996c7bcb96866b295f1230d2716db42

---
+++ fboss/platform/data_corral_service/if/led_manager_config.thrift
@@ -0,0 +1,52 @@
+namespace cpp2 facebook.fboss.platform.data_corral_service
+namespace py3 neteng.fboss.platform.data_corral_service
+
+enum LedColor {
+  OFF = 0,
+  GREEN = 1,
+  RED = 2,
+  BLUE = 3,
+  AMBER = 4,
+}
+
+// LedConfig holds a mechanism to program LED in the case of presence and absence.
+//
+// `presentLedColor`: Led color for presence
+//
+// `presentLedSysfsPath`: Sysfs path for presence
+//
+// `absentLedColor`: Led color for absence
+//
+// `absentLedSysfsPath`: Sysfs path for absence.
+struct LedConfig {
+  1: LedColor presentLedColor;
+  2: string presentLedSysfsPath;
+  3: LedColor absentLedColor;
+  4: string absentLedSysfsPath;
+}
+
+// FruConfig contains information to detect FRU presence.
+//
+// `fruName`: Name of FRU. E.g FAN1, PEM1
+//
+// `fruType`: Type of FRU. E.g FAN, PEM, PSU
+//
+// `presenceSysfsPath`: Sysfs  path to detect presence of this FRU.
+struct FruConfig {
+  1: string fruName;
+  2: string fruType;
+  3: string presenceSysfsPath;
+}
+
+// Main struct to support generic LED programming for presence.
+//
+// `systemLedConfig`: Led config at system level. System is present if and only if all frus are present.
+//
+// `fruTypeLedConfigs`: Presence LED programming mechanism for every fru type.
+//
+// `fruConfigs`: FRU presence detection mechanism in the platform.
+struct LedManagerConfig {
+  1: LedConfig systemLedConfig;
+  2: map<string/*fruType*/ , LedConfig> fruTypeLedConfigs;
+  3: list<FruConfig> fruConfigs;
+}
