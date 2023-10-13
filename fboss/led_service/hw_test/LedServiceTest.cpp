a96f4c27bd9d1b897f80f84cfbdbc54138e3de63
Rajan Kumar <rajank@meta.com>
2023-10-12 23:32:32 -0700

    Added LED color change test
    Summary: Added a test to check the LED color change as per setting of port external state forcefully
    
    Reviewed By: birdsoup
    
    Differential Revision: D49847964
    
    fbshipit-source-id: 436b33e6371e724fef7582fa87780ba171bebd72

--- fboss/led_service/hw_test/LedServiceTest.cpp
+++ fboss/led_service/hw_test/LedServiceTest.cpp
@@ -13,6 +13,7 @@
 #include <thrift/lib/cpp2/async/RocketClientChannel.h>
 #include <thrift/lib/cpp2/protocol/Serializer.h>
 #include "common/init/Init.h"
+#include "fboss/agent/EnumUtils.h"
 #include "fboss/led_service/LedManager.h"
 #include "fboss/led_service/hw_test/LedServiceTest.h"
 
@@ -35,6 +36,62 @@ void LedServiceTest::TearDown() {
   ensemble_.reset();
 }
 
+/*
+ * Set the LED state by updating the LED manager and check if the LED color
+ * changes accordingly
+ */
+TEST_F(LedServiceTest, checkLedColorChange) {
+  auto ledManager = getLedEnsemble()->getLedManager();
+
+  // Test on the first module port
+  // Use the ports max speed and profile
+  auto platformMap = ledManager->getPlatformMapping();
+  auto swPorts = platformMap->getSwPortListFromTransceiverId(0);
+  CHECK_GT(swPorts.size(), 0);
+  auto swPort = swPorts[0];
+
+  // The setExternalLedState will throw because first update from FSDB has not
+  // happened to the LedManager
+  EXPECT_THROW(
+      ledManager->setExternalLedState(
+          swPort, PortLedExternalState::EXTERNAL_FORCE_OFF),
+      FbossError);
+
+  // Do the first update from FSDB to LedService
+  auto maxSpeed = platformMap->getPortMaxSpeed(swPort);
+  auto profile = platformMap->getProfileIDBySpeed(swPort, maxSpeed);
+  LedManager::LedSwitchStateUpdate ledUpdate = {
+      static_cast<short>(swPort),
+      "",
+      enumToName<cfg::PortProfileID>(profile),
+      false};
+
+  std::map<short, LedManager::LedSwitchStateUpdate> switchUpdate_0;
+  switchUpdate_0[swPort] = ledUpdate;
+  ledManager->updateLedStatus(switchUpdate_0);
+
+  // Now the setting of external LED state should be successful
+  EXPECT_NO_THROW(ledManager->setExternalLedState(
+      swPort, PortLedExternalState::EXTERNAL_FORCE_OFF));
+
+  // Verify link Down, the expected LED color is OFF
+  auto offLedColor = ledManager->getCurrentLedColor(swPort);
+  EXPECT_EQ(offLedColor, led::LedColor::OFF);
+
+  // Verify link Up, the expected LED color is either Blue or Green
+  ledManager->setExternalLedState(
+      swPort, PortLedExternalState::EXTERNAL_FORCE_ON);
+  auto onLedColorCurrent = ledManager->getCurrentLedColor(swPort);
+  auto onLedColorExpected = ledManager->onColor();
+  EXPECT_EQ(onLedColorCurrent, onLedColorExpected);
+
+  // Put it back to Off state and check again
+  ledManager->setExternalLedState(
+      swPort, PortLedExternalState::EXTERNAL_FORCE_OFF);
+  offLedColor = ledManager->getCurrentLedColor(swPort);
+  EXPECT_EQ(offLedColor, led::LedColor::OFF);
+}
+
 } // namespace facebook::fboss
 
 int main(int argc, char* argv[]) {
@@ -43,5 +100,5 @@ int main(int argc, char* argv[]) {
 
   facebook::initFacebook(&argc, &argv);
 
-  return 0;
+  return RUN_ALL_TESTS();
 }
