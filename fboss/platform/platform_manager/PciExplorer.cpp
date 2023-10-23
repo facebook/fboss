ce18b01f10517c9c1c3962732af6247c1ecd99a4
Manikandan Somasundaram <manikandan@meta.com>
2023-10-23 14:10:30 -0700

    PCI char device path is made of vendor/device id
    Summary:
    PCI char device was earlier based on domain, bus, slot, function. But
    now it has changed to vendor/device_id. Will incorporate subsystem ids later
    
    Differential Revision: D50561689
    
    fbshipit-source-id: fc1ed3927985959be43504697c2e6fc0c9d75735

--- fboss/platform/platform_manager/PciExplorer.cpp
+++ fboss/platform/platform_manager/PciExplorer.cpp
@@ -1,22 +1,11 @@
 // (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
-
-#include <filesystem>
+#include "fboss/platform/platform_manager/PciExplorer.h"
 
 #include <folly/FileUtil.h>
 #include <folly/String.h>
 #include <folly/logging/xlog.h>
 
-#include "fboss/platform/platform_manager/PciExplorer.h"
-
-namespace fs = std::filesystem;
 using namespace facebook::fboss::platform::platform_manager;
-
-namespace {
-bool isSamePciId(const std::string& id1, const std::string& id2) {
-  return RE2::FullMatch(id1, PciExplorer().kPciIdRegex) &&
-      RE2::FullMatch(id2, PciExplorer().kPciIdRegex) && id1 == id2;
-}
-} // namespace
 
 namespace facebook::fboss::platform::platform_manager {
 
@@ -25,36 +14,10 @@ std::optional<std::string> PciExplorer::getDevicePath(
     const std::string& deviceId,
     const std::string& subSystemVendorId,
     const std::string& subSystemDeviceId) {
-  for (const auto& dirEntry : fs::directory_iterator("/sys/bus/pci/devices")) {
-    std::string vendor, device, subSystemVendor, subSystemDevice;
-    auto deviceFilePath = dirEntry.path() / "device";
-    auto vendorFilePath = dirEntry.path() / "vendor";
-    auto subSystemVendorFilePath = dirEntry.path() / "subsystem_vendor";
-    auto subSystemDeviceFilePath = dirEntry.path() / "subsystem_device";
-    if (!folly::readFile(vendorFilePath.c_str(), vendor)) {
-      XLOG(ERR) << "Failed to read vendor file from " << dirEntry.path();
-    }
-    if (!folly::readFile(deviceFilePath.c_str(), device)) {
-      XLOG(ERR) << "Failed to read device file from " << dirEntry.path();
-    }
-    if (!folly::readFile(subSystemVendorFilePath.c_str(), subSystemVendor)) {
-      XLOG(ERR) << "Failed to read subsystem_vendor file from "
-                << dirEntry.path();
-    }
-    if (!folly::readFile(subSystemDeviceFilePath.c_str(), subSystemDevice)) {
-      XLOG(ERR) << "Failed to read subsystem_device file from "
-                << dirEntry.path();
-    }
-    if (isSamePciId(folly::trimWhitespace(vendor).str(), vendorId) &&
-        isSamePciId(folly::trimWhitespace(device).str(), deviceId) &&
-        isSamePciId(
-            folly::trimWhitespace(subSystemVendor).str(), subSystemVendorId) &&
-        isSamePciId(
-            folly::trimWhitespace(subSystemDevice).str(), subSystemDeviceId)) {
-      return fmt::format("/dev/fbiob_{}", dirEntry.path().filename().string());
-    }
-  }
-  return std::nullopt;
+  return fmt::format(
+      "/dev/fbiob_{}_{}",
+      std::string(vendorId, 2, 4),
+      std::string(deviceId, 2, 4));
 }
 
 std::vector<uint16_t> PciExplorer::createI2cAdapter(
