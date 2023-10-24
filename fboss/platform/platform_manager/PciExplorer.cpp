273639dca71c0789c1c6d20509fa78397537dc5a
Manikandan Somasundaram <manikandan@meta.com>
2023-10-24 15:15:39 -0700

    Basic logic to setup i2c-adapters behind fpga
    Summary:
    This code creates the i2c-adapters behind fpga. A couple of things are
    TODO
    - Support for multiple buses in adapter (BSP needs to take in this arg first).
    - Inferring the kernel assigned bus numbers for the created buses.
    - Error handling.
    - And handling of additional device specific data (like hz for i2c adapter)
    
    Differential Revision: D50561690
    
    fbshipit-source-id: ad3b9e0c79e797ca15da10635943dab5dc6cbeac

--- fboss/platform/platform_manager/PciExplorer.cpp
+++ fboss/platform/platform_manager/PciExplorer.cpp
@@ -1,9 +1,13 @@
 // (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
 #include "fboss/platform/platform_manager/PciExplorer.h"
 
+#include <sys/ioctl.h>
+
 #include <folly/FileUtil.h>
 #include <folly/String.h>
 #include <folly/logging/xlog.h>
+
+#include "fboss/platform/platform_manager/uapi/fbiob-ioctl.h"
 
 using namespace facebook::fboss::platform::platform_manager;
 
@@ -21,38 +25,76 @@ std::optional<std::string> PciExplorer::getDevicePath(
 }
 
 std::vector<uint16_t> PciExplorer::createI2cAdapter(
-    const std::string& /* pciDevPath */,
-    const I2cAdapterConfig& /* i2cAdapterConfig */,
-    uint32_t /* instanceId */) {
-  throw std::runtime_error("Not implemented");
+    const std::string& pciDevPath,
+    const I2cAdapterConfig& i2cAdapterConfig,
+    uint32_t instanceId) {
+  create(pciDevPath, *i2cAdapterConfig.fpgaIpBlockConfig(), instanceId);
+  return {};
 }
 
 void PciExplorer::createSpiMaster(
-    const std::string& /* pciDevPath */,
-    const SpiMasterConfig& /* spiMasterConfig */,
-    uint32_t /* instanceId */) {
-  throw std::runtime_error("Not implemented");
+    const std::string& pciDevPath,
+    const SpiMasterConfig& spiMasterConfig,
+    uint32_t instanceId) {
+  create(pciDevPath, *spiMasterConfig.fpgaIpBlockConfig(), instanceId);
 }
 
 void PciExplorer::createLedCtrl(
-    const std::string& /* pciDevPath */,
-    const LedCtrlConfig& /* ledCtrlConfig */,
-    uint32_t /* instanceId */) {
-  throw std::runtime_error("Not implemented");
+    const std::string& pciDevPath,
+    const LedCtrlConfig& ledCtrlConfig,
+    uint32_t instanceId) {
+  create(pciDevPath, *ledCtrlConfig.fpgaIpBlockConfig(), instanceId);
 }
 
 void PciExplorer::createXcvrCtrl(
-    const std::string& /* pciDevPath */,
-    const XcvrCtrlConfig& /* xcvrCtrlConfig */,
-    uint32_t /* instanceId */) {
-  throw std::runtime_error("Not implemented");
+    const std::string& pciDevPath,
+    const XcvrCtrlConfig& xcvrCtrlConfig,
+    uint32_t instanceId) {
+  create(pciDevPath, *xcvrCtrlConfig.fpgaIpBlockConfig(), instanceId);
 }
 
 void PciExplorer::create(
-    const std::string& /* pciDevPath */,
-    const FpgaIpBlockConfig& /* fpgaIpBlock */,
-    uint32_t /* instanceId */) {
-  throw std::runtime_error("Not implemented");
+    const std::string& pciDevPath,
+    const FpgaIpBlockConfig& fpgaIpBlockConfig,
+    uint32_t instanceId) {
+  XLOG(INFO) << fmt::format(
+      "Creating a new device using: {}, deviceName: {} instanceId: {}, "
+      "csrOffset: {}, iobufOffset: {}",
+      pciDevPath,
+      *fpgaIpBlockConfig.deviceName(),
+      instanceId,
+      *fpgaIpBlockConfig.csrOffset(),
+      *fpgaIpBlockConfig.iobufOffset());
+  struct fbiob_aux_data aux_data {};
+  strcpy(aux_data.id.name, fpgaIpBlockConfig.deviceName()->c_str());
+  aux_data.id.id = instanceId;
+  if (!fpgaIpBlockConfig.csrOffset()->empty()) {
+    aux_data.csr_offset =
+        std::stoi(*fpgaIpBlockConfig.csrOffset(), nullptr, 16);
+  }
+  if (!fpgaIpBlockConfig.iobufOffset()->empty()) {
+    aux_data.iobuf_offset =
+        std::stoi(*fpgaIpBlockConfig.iobufOffset(), nullptr, 16);
+  }
+
+  auto fd = open(pciDevPath.c_str(), O_RDWR);
+  if (fd < 0) {
+    XLOG(ERR) << fmt::format(
+        "Failed to open {}: {}", pciDevPath, folly::errnoStr(errno));
+    return;
+  }
+  auto ret = ioctl(fd, FBIOB_IOC_NEW_DEVICE, &aux_data);
+  if (ret < 0) {
+    XLOG(ERR) << fmt::format(
+        "Failed to create new device using: {}, instanceId: {}, "
+        "csrOffset: {:#04x}, iobufOffset: {:#04x}, error: {} ",
+        pciDevPath,
+        aux_data.id.id,
+        aux_data.csr_offset,
+        aux_data.iobuf_offset,
+        folly::errnoStr(errno));
+  }
+  close(fd);
 }
 
 } // namespace facebook::fboss::platform::platform_manager
