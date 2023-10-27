3360aca8fbb55470d71249f64946d9dd1d37db6d
Mike Choi <mikechoi@meta.com>
2023-10-27 14:16:01 -0700

    weutil : Define the new FbossEepromParser class (header file)
    Summary: This FbossEepromParser class is the class that will be shared with OpenBMC Code base. This class has all the logic for reading and parsing EEPROM, but does not depend on folly or thrift so that it will also compile in BMC;  another class, FbossEepromLib will have all the fbcode specific folly and thrift related logic. This class will be defined in the later diff of this stack.
    
    Reviewed By: somasun
    
    Differential Revision: D49491158
    
    fbshipit-source-id: 165448442c249f0b8f192358d9b25a53a975b3e1

---
+++ fboss/platform/weutil/FbossEepromParser.h
@@ -0,0 +1,45 @@
+// (c) Facebook, Inc. and its affiliates. Confidential and proprietary.
+
+#pragma once
+#include <optional>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+namespace facebook::fboss::platform {
+
+typedef std::vector<std::pair<std::string, std::string>> parsedEepromEntry;
+
+class FbossEepromParser {
+ private:
+  std::string eepromPath_;
+  int loadEeprom(
+      const std::string& eeprom,
+      unsigned char* output,
+      int offset,
+      int max);
+  std::unordered_map<int, std::string> parseEepromBlobV3(
+      const unsigned char* buffer);
+  std::unordered_map<int, std::string> parseEepromBlobV4(
+      const unsigned char* buffer,
+      const int readCount);
+  parsedEepromEntry getAllInfo(const std::string& eeprom, const int offset);
+  // This method is a helper function to translate <field_id, value> pair
+  // into <field_name, field_value> pair, so that the user of this
+  // methon can parse the data into human readable screen output or
+  // JSON
+  std::vector<std::pair<std::string, std::string>> prepareEepromFieldMap(
+      std::unordered_map<int, std::string> parsedValue,
+      int eepromVer);
+  std::string parseUint(int len, unsigned char* ptr);
+  std::string parseHex(int len, unsigned char* ptr);
+  std::string parseString(int len, unsigned char* ptr);
+  std::string parseMac(int len, unsigned char* ptr);
+  std::string parseLegacyMac(int len, unsigned char* ptr);
+  std::string parseDate(int len, unsigned char* ptr);
+
+ public:
+  parsedEepromEntry getEeprom(const std::string& eepromPath, const int offset);
+};
+
+} // namespace facebook::fboss::platform
