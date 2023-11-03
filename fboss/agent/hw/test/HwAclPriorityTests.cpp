36b2701dc120065827f51e189e94ecec391e1d05
Elangovan Natarajan <elangovan@meta.com>
2023-11-03 07:38:48 -0700

    Include multi acl test for HwAclPriorityTests.cpp
    Summary: Include multi acl test for HwAclPriorityTests.cpp
    
    Reviewed By: shri-khare
    
    Differential Revision: D50711825
    
    fbshipit-source-id: 48b1ee33b379b6456296055406f5ffe5347f4031

--- fboss/agent/hw/test/HwAclPriorityTests.cpp
+++ fboss/agent/hw/test/HwAclPriorityTests.cpp
@@ -14,6 +14,7 @@
 #include "fboss/agent/gen-cpp2/switch_config_types.h"
 #include "fboss/agent/hw/test/HwTestAclUtils.h"
 #include "fboss/agent/hw/test/HwTestCoppUtils.h"
+#include "fboss/agent/hw/test/dataplane_tests/HwTestQueuePerHostUtils.h"
 #include "fboss/agent/state/StateDelta.h"
 #include "fboss/agent/state/SwitchState.h"
 
@@ -26,12 +27,24 @@ namespace {
 using namespace facebook::fboss;
 using namespace facebook::fboss::utility;
 
+void addAclEntry(cfg::SwitchConfig& cfg, cfg::AclEntry* acl) {
+  if (FLAGS_enable_acl_table_group) {
+    int tableNumber = getAclTableIndex(&cfg, utility::kDefaultAclTable());
+    if (cfg.aclTableGroup()) {
+      cfg.aclTableGroup()->aclTables()[tableNumber].aclEntries()->push_back(
+          *acl);
+    }
+  } else {
+    cfg.acls()->push_back(*acl);
+  }
+}
+
 void addDenyPortAcl(cfg::SwitchConfig& cfg, const std::string& aclName) {
   auto acl = cfg::AclEntry();
   *acl.name() = aclName;
   *acl.actionType() = cfg::AclActionType::DENY;
   acl.dscp() = 0x24;
-  cfg.acls()->push_back(acl);
+  addAclEntry(cfg, &acl);
 }
 
 void addPermitIpAcl(
@@ -43,116 +56,166 @@ void addPermitIpAcl(
   acl.actionType() = cfg::AclActionType::PERMIT;
   acl.dstIp() = ip.str();
   acl.dscp() = 0x24;
-  cfg.acls()->push_back(acl);
+  addAclEntry(cfg, &acl);
 }
 
 } // unnamed namespace
 
 namespace facebook::fboss {
 
+template <bool enableMultiAclTable>
+struct EnableMultiAclTableT {
+  static constexpr auto multiAclTableEnabled = enableMultiAclTable;
+};
+
+using TestTypes =
+    ::testing::Types<EnableMultiAclTableT<false>, EnableMultiAclTableT<true>>;
+
+template <typename EnableMultiAclTableT>
 class HwAclPriorityTest : public HwTest {
+  static auto constexpr isMultiAclEnabled =
+      EnableMultiAclTableT::multiAclTableEnabled;
+
  protected:
+  void SetUp() override {
+    FLAGS_enable_acl_table_group = isMultiAclEnabled;
+    HwTest::SetUp();
+    /*
+     * Native SDK does not support multi acl feature.
+     * So skip multi acl tests for fake bcm sdk
+     */
+    if ((this->getPlatform()->getAsic()->getAsicType() ==
+         cfg::AsicType::ASIC_TYPE_FAKE) &&
+        (isMultiAclEnabled)) {
+      GTEST_SKIP();
+    }
+  }
+
   cfg::SwitchConfig initialConfig() const {
-    return utility::onePortPerInterfaceConfig(
+    auto cfg = utility::onePortPerInterfaceConfig(
         getHwSwitch(),
         masterLogicalPortIds(),
         getAsic()->desiredLoopbackModes());
+    if (isMultiAclEnabled) {
+      utility::addAclTableGroup(
+          &cfg, cfg::AclStage::INGRESS, utility::getAclTableGroupName());
+      utility::addDefaultAclTable(cfg);
+    }
+    return cfg;
   }
 };
 
-TEST_F(HwAclPriorityTest, CheckAclPriorityOrder) {
+TYPED_TEST_SUITE(HwAclPriorityTest, TestTypes);
+
+TYPED_TEST(HwAclPriorityTest, CheckAclPriorityOrder) {
   auto setup = [this]() {
-    auto newCfg = initialConfig();
+    auto newCfg = this->initialConfig();
     addDenyPortAcl(newCfg, "A");
     addDenyPortAcl(newCfg, "B");
-    applyNewConfig(newCfg);
+    this->applyNewConfig(newCfg);
   };
 
   auto verify = [=, this]() {
     for (auto acl : {"A", "B"}) {
-      checkSwHwAclMatch(getHwSwitch(), getProgrammedState(), acl);
+      checkSwHwAclMatch(this->getHwSwitch(), this->getProgrammedState(), acl);
     }
-    int aPrio = getProgrammedState()->getAcls()->getNodeIf("A")->getPriority();
-    int bPrio = getProgrammedState()->getAcls()->getNodeIf("B")->getPriority();
+    auto aAcl = utility::getAclEntryByName(this->getProgrammedState(), "A");
+    int aPrio = aAcl->getPriority();
+    auto bAcl = utility::getAclEntryByName(this->getProgrammedState(), "B");
+    int bPrio = bAcl->getPriority();
     EXPECT_EQ(aPrio + 1, bPrio);
   };
-  verifyAcrossWarmBoots(setup, verify);
+  this->verifyAcrossWarmBoots(setup, verify);
 }
 
-TEST_F(HwAclPriorityTest, CheckAclPriortyOrderInsertMiddle) {
+TYPED_TEST(HwAclPriorityTest, CheckAclPriortyOrderInsertMiddle) {
   auto setup = [this]() {
-    auto newCfg = initialConfig();
+    auto newCfg = this->initialConfig();
     addDenyPortAcl(newCfg, "A");
     addDenyPortAcl(newCfg, "B");
-    applyNewConfig(newCfg);
-    newCfg.acls()->pop_back();
+    this->applyNewConfig(newCfg);
+    utility::delLastAddedAcl(&newCfg);
     addDenyPortAcl(newCfg, "C");
     addDenyPortAcl(newCfg, "B");
-    applyNewConfig(newCfg);
+    this->applyNewConfig(newCfg);
   };
 
   auto verify = [=, this]() {
     for (auto acl : {"A", "B", "C"}) {
-      checkSwHwAclMatch(getHwSwitch(), getProgrammedState(), acl);
+      checkSwHwAclMatch(this->getHwSwitch(), this->getProgrammedState(), acl);
     }
-    int aPrio = getProgrammedState()->getAcls()->getNodeIf("A")->getPriority();
-    int bPrio = getProgrammedState()->getAcls()->getNodeIf("B")->getPriority();
-    int cPrio = getProgrammedState()->getAcls()->getNodeIf("C")->getPriority();
+    auto aAcl = utility::getAclEntryByName(this->getProgrammedState(), "A");
+    auto bAcl = utility::getAclEntryByName(this->getProgrammedState(), "B");
+    auto cAcl = utility::getAclEntryByName(this->getProgrammedState(), "C");
+    int aPrio = aAcl->getPriority();
+    int bPrio = bAcl->getPriority();
+    int cPrio = cAcl->getPriority();
     // Order should be A, C, B now
     EXPECT_EQ(aPrio + 1, cPrio);
     EXPECT_EQ(aPrio + 2, bPrio);
   };
-  verifyAcrossWarmBoots(setup, verify);
+  this->verifyAcrossWarmBoots(setup, verify);
 }
 
 /**
  * This unit test case is to test we won't crash cause we're using aclName as
  * key of the aclMap in S/W while using priority as key of aclTable in H/W
  */
-TEST_F(HwAclPriorityTest, AclNameChange) {
+TYPED_TEST(HwAclPriorityTest, AclNameChange) {
   auto setup = [this]() {
-    auto newCfg = initialConfig();
+    auto newCfg = this->initialConfig();
     addDenyPortAcl(newCfg, "A");
-    applyNewConfig(newCfg);
-    *newCfg.acls()->back().name() = "AA";
-    applyNewConfig(newCfg);
+    this->applyNewConfig(newCfg);
+    if (FLAGS_enable_acl_table_group) {
+      *newCfg.aclTableGroup()
+           ->aclTables()[utility::getAclTableIndex(
+               &newCfg, utility::kDefaultAclTable())]
+           .aclEntries()
+           ->back()
+           .name() = "AA";
+    } else {
+      *newCfg.acls()->back().name() = "AA";
+    }
+    this->applyNewConfig(newCfg);
   };
 
   auto verify = [&]() {
     // check s/w acl matches h/w
-    ASSERT_EQ(nullptr, getProgrammedState()->getAcls()->getNodeIf("A"));
-    ASSERT_NE(nullptr, getProgrammedState()->getAcls()->getNodeIf("AA"));
-    checkSwHwAclMatch(getHwSwitch(), getProgrammedState(), "AA");
+    ASSERT_EQ(
+        nullptr, utility::getAclEntryByName(this->getProgrammedState(), "A"));
+    ASSERT_NE(
+        nullptr, utility::getAclEntryByName(this->getProgrammedState(), "AA"));
+    checkSwHwAclMatch(this->getHwSwitch(), this->getProgrammedState(), "AA");
   };
-  verifyAcrossWarmBoots(setup, verify);
+  this->verifyAcrossWarmBoots(setup, verify);
 }
 
-TEST_F(HwAclPriorityTest, AclsChanged) {
+TYPED_TEST(HwAclPriorityTest, AclsChanged) {
   const folly::IPAddress kIp("2400::1");
   auto setup = [this, kIp]() {
-    auto config = initialConfig();
+    auto config = this->initialConfig();
     addDenyPortAcl(config, "acl0");
     // Get Acls from COPP policy
-    setDefaultCpuTrafficPolicyConfig(config, getPlatform()->getAsic());
+    setDefaultCpuTrafficPolicyConfig(config, this->getPlatform()->getAsic());
     addPermitIpAcl(config, "acl1", kIp);
-    applyNewConfig(config);
+    this->applyNewConfig(config);
   };
 
   auto setupPostWb = [this, kIp]() {
-    auto config = initialConfig();
+    auto config = this->initialConfig();
     // Drop COPP acls
     addDenyPortAcl(config, "acl0");
     addPermitIpAcl(config, "acl1", kIp);
-    applyNewConfig(config);
+    this->applyNewConfig(config);
   };
 
-  verifyAcrossWarmBoots(
+  this->verifyAcrossWarmBoots(
       setup, []() {}, setupPostWb, []() {});
 }
 
-TEST_F(HwAclPriorityTest, Reprioritize) {
+TYPED_TEST(HwAclPriorityTest, Reprioritize) {
   auto setup = [=, this]() {
-    auto config = initialConfig();
+    auto config = this->initialConfig();
     addPermitIpAcl(config, "B", folly::IPAddress("1::2"));
     addPermitIpAcl(config, "A", folly::IPAddress("1::3"));
 
@@ -161,16 +224,17 @@ TEST_F(HwAclPriorityTest, Reprioritize) {
     trafficConfig.matchToAction()->resize(2);
     cfg::MatchAction matchAction = getToQueueAction(1, cfg::ToCpuAction::TRAP);
     for (int i = 0; i < 2; i++) {
-      trafficConfig.matchToAction()[i].matcher() = *config.acls()[i].name();
+      auto& acls = utility::getAcls(&config, std::nullopt);
+      trafficConfig.matchToAction()[i].matcher() = *acls[i].name();
       trafficConfig.matchToAction()[i].action() = matchAction;
     }
     cpuConfig.trafficPolicy() = trafficConfig;
     config.cpuTrafficPolicy() = cpuConfig;
-    applyNewConfig(config);
+    this->applyNewConfig(config);
   };
 
   auto setupPostWb = [=, this]() {
-    auto config = initialConfig();
+    auto config = this->initialConfig();
     addPermitIpAcl(config, "A", folly::IPAddress("1::3"));
     addPermitIpAcl(config, "B", folly::IPAddress("1::2"));
 
@@ -179,15 +243,16 @@ TEST_F(HwAclPriorityTest, Reprioritize) {
     trafficConfig.matchToAction()->resize(2);
     cfg::MatchAction matchAction = getToQueueAction(1, cfg::ToCpuAction::TRAP);
     for (int i = 0; i < 2; i++) {
-      trafficConfig.matchToAction()[i].matcher() = *config.acls()[i].name();
+      auto& acls = utility::getAcls(&config, std::nullopt);
+      trafficConfig.matchToAction()[i].matcher() = *acls[i].name();
       trafficConfig.matchToAction()[i].action() = matchAction;
     }
     cpuConfig.trafficPolicy() = trafficConfig;
     config.cpuTrafficPolicy() = cpuConfig;
-    applyNewConfig(config);
+    this->applyNewConfig(config);
   };
 
-  verifyAcrossWarmBoots(
+  this->verifyAcrossWarmBoots(
       setup, []() {}, setupPostWb, []() {});
 }
 } // namespace facebook::fboss
