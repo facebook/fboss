b89b3a36c79e96b3e1a2291c3479341b4c0be3b0
Yedidya Feldblum <yfeldblum@meta.com>
2023-12-02 16:23:41 -0800

    migrate from nested-name ReadHolder ctors passing an mutex address
    Summary: Migrate them to equivalent nested-name `ReadHolder` ctors passing the mutex by reference.
    
    Reviewed By: bcardosolopes
    
    Differential Revision: D51727029
    
    fbshipit-source-id: 7568c830147ea7e42fddad0877f297bcf3e20ee8

--- fboss/agent/LinkAggregationManager.cpp
+++ fboss/agent/LinkAggregationManager.cpp
@@ -109,7 +109,7 @@ void LinkAggregationManager::handlePacket(
     std::unique_ptr<RxPacket> pkt,
     folly::io::Cursor c) {
   // TODO(samank): check this is running in RX thread?
-  folly::SharedMutexWritePriority::ReadHolder g(&controllersLock_);
+  folly::SharedMutexWritePriority::ReadHolder g(controllersLock_);
   auto ingressPort = pkt->getSrcPort();
 
   auto it = portToController_.find(ingressPort);
@@ -281,7 +281,7 @@ void LinkAggregationManager::populatePartnerPair(
 
   std::shared_ptr<LacpController> controller;
   {
-    folly::SharedMutexWritePriority::ReadHolder g(&controllersLock_);
+    folly::SharedMutexWritePriority::ReadHolder g(controllersLock_);
 
     auto it = portToController_.find(portID);
     if (it == portToController_.end()) {
@@ -301,7 +301,7 @@ void LinkAggregationManager::populatePartnerPairs(
 
   partnerPairs.clear();
 
-  folly::SharedMutexWritePriority::ReadHolder g(&controllersLock_);
+  folly::SharedMutexWritePriority::ReadHolder g(controllersLock_);
 
   partnerPairs.reserve(
       std::distance(portToController_.begin(), portToController_.end()));
@@ -396,7 +396,7 @@ LinkAggregationManager::getControllersFor(
   std::vector<std::shared_ptr<LacpController>> controllers(
       std::distance(ports.begin(), ports.end()));
 
-  folly::SharedMutexWritePriority::ReadHolder g(&controllersLock_);
+  folly::SharedMutexWritePriority::ReadHolder g(controllersLock_);
 
   // TODO(samank): Rerwite as an O(N + M) algorithm
   for (auto i = 0; i < controllers.size(); ++i) {
