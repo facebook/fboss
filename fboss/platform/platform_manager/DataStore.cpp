0b1fd127d58dcb01c1e6480749611cdb8d2c9c50
Justin Kim <jkimdo@meta.com>
2023-12-06 09:08:09 -0800

    A class to store shared data across different classes
    Summary:
    Data like i2c number, gpio chip number and such would be used for different features.
    To make to shareable, creating a new class managing these data.
    
    Reviewed By: somasun
    
    Differential Revision: D51411441
    
    fbshipit-source-id: 804177e7840bf828fcc3701368045a9d426ab6e3

---
+++ fboss/platform/platform_manager/DataStore.cpp
@@ -0,0 +1,80 @@
+// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+#include "fboss/platform/platform_manager/DataStore.h"
+
+#include <fmt/format.h>
+#include <folly/logging/xlog.h>
+
+namespace facebook::fboss::platform::platform_manager {
+uint16_t DataStore::getI2cBusNum(
+    const std::optional<std::string>& slotPath,
+    const std::string& pmUnitScopeBusName) const {
+  auto it = i2cBusNums_.find(std::make_pair(std::nullopt, pmUnitScopeBusName));
+  if (it != i2cBusNums_.end()) {
+    return it->second;
+  }
+  it = i2cBusNums_.find(std::make_pair(slotPath, pmUnitScopeBusName));
+  if (it != i2cBusNums_.end()) {
+    return it->second;
+  }
+  throw std::runtime_error(fmt::format(
+      "Could not find bus number for {} at {}",
+      pmUnitScopeBusName,
+      slotPath ? *slotPath : "CPU"));
+}
+
+void DataStore::updateI2cBusNum(
+    const std::optional<std::string>& slotPath,
+    const std::string& pmUnitScopeBusName,
+    uint16_t busNum) {
+  XLOG(INFO) << fmt::format(
+      "Updating bus {} in {} to bus number {} (i2c-{})",
+      pmUnitScopeBusName,
+      slotPath ? fmt::format("SlotPath {}", *slotPath) : "Global Scope",
+      busNum,
+      busNum);
+  i2cBusNums_[std::make_pair(slotPath, pmUnitScopeBusName)] = busNum;
+}
+
+uint16_t DataStore::getGpioChipNum(
+    const std::string& slotPath,
+    const std::string& gpioChipDeviceName) const {
+  auto it = gpioChipNums_.find(std::make_pair(slotPath, gpioChipDeviceName));
+  if (it != gpioChipNums_.end()) {
+    return it->second;
+  }
+  throw std::runtime_error(fmt::format(
+      "Could not find gpio chip number for {} at {}",
+      gpioChipDeviceName,
+      slotPath));
+}
+
+void DataStore::updateGpioChipNum(
+    const std::string& slotPath,
+    const std::string& gpioChipDeviceName,
+    uint16_t gpioChipNum) {
+  XLOG(INFO) << fmt::format(
+      "Updating gpio chip {} in {} to gpio chip number {} (gpiochip{})",
+      gpioChipDeviceName,
+      slotPath,
+      gpioChipNum,
+      gpioChipNum);
+  gpioChipNums_[std::make_pair(slotPath, gpioChipDeviceName)] = gpioChipNum;
+}
+
+std::string DataStore::getPmUnitName(const std::string& slotPath) const {
+  if (slotPathToPmUnitName_.find(slotPath) != slotPathToPmUnitName_.end()) {
+    return slotPathToPmUnitName_.at(slotPath);
+  }
+  throw std::runtime_error(
+      fmt::format("Could not find PmUnit at {}", slotPath));
+}
+
+void DataStore::updatePmUnitName(
+    const std::string& slotPath,
+    const std::string& pmUnitName) {
+  XLOG(INFO) << fmt::format("Updating PmUnit {} at {}", pmUnitName, slotPath);
+  slotPathToPmUnitName_[slotPath] = pmUnitName;
+}
+
+} // namespace facebook::fboss::platform::platform_manager
---
+++ fboss/platform/platform_manager/DataStore.h
@@ -0,0 +1,66 @@
+// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+#pragma once
+
+#include <map>
+#include <optional>
+#include <string>
+
+namespace facebook::fboss::platform::platform_manager {
+class DataStore {
+ public:
+  DataStore() = default;
+  virtual ~DataStore() = default;
+
+  // Get the kernel assigned I2C bus number for the given busName.
+  // If the busName could be found in global scope (e.g., bus from CPU),
+  // then return it directly. Otherwise, check if there is a mapping in the
+  // provided slotPath.
+  uint16_t getI2cBusNum(
+      const std::optional<std::string>& slotPath,
+      const std::string& pmUnitScopeBusName) const;
+
+  // Update the kernel assigned I2C bus number for the given busName. If the
+  // bus name is of global scope, then SlotPath is empty.
+  void updateI2cBusNum(
+      const std::optional<std::string>& slotPath,
+      const std::string& pmUnitScopeBusName,
+      uint16_t busNum);
+
+  // Get Gpio chip number of the given chip name at the given SlotPath
+  uint16_t getGpioChipNum(
+      const std::string& slotPath,
+      const std::string& gpioChipDeviceName) const;
+
+  // Update Gpio chip number of the given chip name at the given SlotPath
+  void updateGpioChipNum(
+      const std::string& slotPath,
+      const std::string& gpioChipDeviceName,
+      uint16_t gpioChipNum);
+
+  // Get PmUnitName at the given SlotPath
+  std::string getPmUnitName(const std::string& slotPath) const;
+
+  // Update PmUnitName at the given SlotPath
+  void updatePmUnitName(
+      const std::string& slotPath,
+      const std::string& pmUnitName);
+
+ private:
+  // Map from <pmUnitPath, pmUnitScopeBusName> to kernel i2c bus name.
+  // - The pmUnitPath to the rootPmUnit is /. So a bus at root PmUnit will
+  // have the entry <"/", "MuxA@1"> -> i2c-54.
+  // - The CPU buses are not pinned to any PmUnit, so they are stored as
+  // entry <std::nullopt, "SMBus Adapter 1654"> -> i2c-7.
+  // - An INCOMING @1 bus at pmUnitPath /MCB_SLOT@0/PIM_SLOT@1 will have the
+  // entry <"/MCB_SLOT@0/PIM_SLOT@1", "INCOMING@1"> -> i2c-52
+  std::map<std::pair<std::optional<std::string>, std::string>, uint16_t>
+      i2cBusNums_{};
+
+  // Map from <SlotPath, GpioChipDeviceName> to gpio chip number.
+  std::map<std::pair<std::string, std::string>, uint16_t> gpioChipNums_{};
+
+  // Stores the PmUnitName which has been discovered at each SlotPath.
+  std::map<std::string, std::string> slotPathToPmUnitName_{};
+};
+} // namespace facebook::fboss::platform::platform_manager
