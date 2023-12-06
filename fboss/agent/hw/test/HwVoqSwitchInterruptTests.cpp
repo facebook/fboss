04ae5b70feacb67bc12968bd12939d426e5c5e3f
Jasmeet Bagga <jasmeetbagga@meta.com>
2023-12-05 16:14:52 -0800

    Test for inducing EPNI errors + counters
    Summary:
    As titled. Induce epni errors, which leads
    to a interrupt. Assert for error counter increment
    
    Reviewed By: shri-khare
    
    Differential Revision: D51788037
    
    fbshipit-source-id: 5b789b4e9446a9d3cfc69084b95661bff2a90dc7

--- fboss/agent/hw/test/HwVoqSwitchInterruptTests.cpp
+++ fboss/agent/hw/test/HwVoqSwitchInterruptTests.cpp
@@ -87,4 +87,41 @@ TEST_F(HwVoqSwitchInterruptTest, itppError) {
   };
   verifyAcrossWarmBoots([]() {}, verify);
 }
+
+TEST_F(HwVoqSwitchInterruptTest, epniError) {
+  auto verify = [=, this]() {
+    constexpr auto kEpniErrorIncjectorCintStr = R"(
+  cint_reset();
+  bcm_switch_event_control_t event_ctrl;
+  event_ctrl.event_id = 717;  // JR3_INT_EPNI_FIFO_OVERFLOW_INT
+  event_ctrl.index = 0; /* core ID */
+  event_ctrl.action = bcmSwitchEventForce;
+  print bcm_switch_event_control_set(0, BCM_SWITCH_EVENT_DEVICE_INTERRUPT, event_ctrl, 1);
+  )";
+    folly::test::TemporaryFile file;
+    XLOG(INFO) << " Cint file " << file.path().c_str();
+    folly::writeFull(
+        file.fd(),
+        kEpniErrorIncjectorCintStr,
+        std::strlen(kEpniErrorIncjectorCintStr));
+    std::string out;
+    getHwSwitchEnsemble()->runDiagCommand(
+        folly::sformat("cint {}\n", file.path().c_str()), out);
+    getHwSwitchEnsemble()->runDiagCommand("quit\n", out);
+    WITH_RETRIES({
+      getHwSwitch()->updateStats();
+      fb303::ThreadCachedServiceData::get()->publishStats();
+      auto epniErrors = getHwSwitch()
+                            ->getSwitchStats()
+                            ->getHwAsicErrors()
+                            .egressPacketNetworkInterfaceErrors()
+                            .value_or(0);
+      XLOG(INFO) << " EPNI Errors: " << epniErrors;
+      EXPECT_EVENTUALLY_GT(epniErrors, 0);
+      EXPECT_EVENTUALLY_GT(getHwSwitch()->getSwitchStats()->getEpniErrors(), 0);
+    });
+  };
+  verifyAcrossWarmBoots([]() {}, verify);
+}
+
 } // namespace facebook::fboss
