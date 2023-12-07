4496b5d3a41b294038110b7c8056a020d7d25d3b
Srikrishna Gopu <srikrishnagopu@meta.com>
2023-12-07 13:19:06 -0800

    create client support in fsdb stream client in OSS
    Summary:
    As titled, create client support in fsdb stream client in OSS.
    
    I have replicated facebook/Client.cpp. Once fsdb client is validated in OSS, we can unify the Client.cpp
    
    Reviewed By: peygar
    
    Differential Revision: D51930202
    
    fbshipit-source-id: b070c7988ee50ef4a70426a2b4d6d9b19bd3c624

--- fboss/fsdb/client/oss/FsdbStreamClient.cpp
+++ fboss/fsdb/client/oss/FsdbStreamClient.cpp
@@ -2,10 +2,64 @@
 
 #include "fboss/fsdb/client/FsdbStreamClient.h"
 
+#ifdef IS_OSS_FBOSS_CENTOS9
+#include <cstdint>
+#include <optional>
+#include "fboss/fsdb/client/Client.h"
+#endif
+
 namespace facebook::fboss::fsdb {
+#ifdef IS_OSS_FBOSS_CENTOS9
+/**
+ * Default value sourced from Cfgr "neteng/qosdb/cos_utility_maps"
+ * dscpToClassOfServiceMap.ClassOfService.NC : 48
+ */
+const uint8_t kDscpForClassOfServiceNC = 48;
+
+void FsdbStreamClient::resetClient() {
+  CHECK(streamEvb_->getEventBase()->isInEventBaseThread());
+  client_.reset();
+}
+
+std::optional<uint8_t> getTosForClientPriority(
+    const std::optional<FsdbStreamClient::Priority> priority) {
+  if (priority.has_value()) {
+    switch (*priority) {
+      case FsdbStreamClient::Priority::CRITICAL:
+        return kDscpForClassOfServiceNC;
+      case FsdbStreamClient::Priority::NORMAL:
+        // no TC marking by default
+        return std::nullopt;
+    }
+  }
+  return std::nullopt;
+}
+
+bool shouldUseEncryptedClient(const FsdbStreamClient::ServerOptions& options) {
+  // use encrypted connection for all clients except CRITICAL ones.
+  return (options.priority != FsdbStreamClient::Priority::CRITICAL);
+}
+
+void FsdbStreamClient::createClient(const ServerOptions& options) {
+  CHECK(streamEvb_->getEventBase()->isInEventBaseThread());
+  resetClient();
+
+  auto tos = getTosForClientPriority(options.priority);
+  bool encryptedClient = shouldUseEncryptedClient(options);
+
+  client_ = Client::getClient(
+      options.dstAddr /* dstAddr */,
+      options.srcAddr /* srcAddr */,
+      tos,
+      !encryptedClient,
+      streamEvb_->getEventBase());
+}
+
+#else
 
 void FsdbStreamClient::resetClient() {}
 
 void FsdbStreamClient::createClient(const ServerOptions& /* options */) {}
 
+#endif
 } // namespace facebook::fboss::fsdb
