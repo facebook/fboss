5e15b3b7b4730be7fa353c928449955d607a9b8f
Shrikrishna (Shri) Khare <skhare@meta.com>
2023-12-08 10:03:09 -0800

    Remove FabricReachability implementation during refactor
    Summary:
    This diff stack will enhance the fabric reachability agent code.
    
    Start from scratch, keep APIs exposed to SwSwitch etc. to avoid breaking
    compilation.
    
    Reviewed By: jasmeetbagga
    
    Differential Revision:
    D51243994
    
    Privacy Context Container: L1125642
    
    fbshipit-source-id: 48d53aa69e996dbeacb01323c73caba646b4aed2

--- fboss/agent/FabricReachabilityManager.h
+++ fboss/agent/FabricReachabilityManager.h
@@ -13,44 +13,14 @@ class SwitchState;
 class PlatformMapping;
 
 class FabricReachabilityManager {
-  using NeighborReachabilityList = std::vector<cfg::PortNeighbor>;
-
  public:
   explicit FabricReachabilityManager() {}
   void stateUpdated(const StateDelta& stateDelta);
   std::map<PortID, FabricEndpoint> getReachabilityInfo();
-  std::map<PortID, FabricEndpoint> processReachabilityInfo(
-      const std::map<PortID, FabricEndpoint>& hwReachability);
   FabricEndpoint processReachabilityInfoForPort(
       const PortID& portId,
       const FabricEndpoint& hwReachability);
   bool isReachabilityInfoMissing(const PortID& portId);
   bool isReachabilityInfoMismatch(const PortID& portId);
-
- private:
-  void updatePorts(const StateDelta& delta);
-  void updateDsfNodes(const StateDelta& delta);
-  void addPort(const std::shared_ptr<Port>& swPort);
-  void removePort(const std::shared_ptr<Port>& swPort);
-  void addDsfNode(const std::shared_ptr<DsfNode>& dsfNode);
-  void removeDsfNode(const std::shared_ptr<DsfNode>& dsfNode);
-
-  void updateExpectedPortId(
-      FabricEndpoint& endpoint,
-      const cfg::AsicType asicType,
-      const PlatformType platformType,
-      const cfg::DsfNodeType dsfNodeType);
-  void updatePortName(
-      FabricEndpoint& endpoint,
-      const cfg::AsicType asicType,
-      PlatformType platformType);
-  void updateHwFabricEndpointInfo(FabricEndpoint& resEndpoint);
-  void updateExpectedFabricEndpointInfo(
-      FabricEndpoint& resEndpoint,
-      const FabricEndpoint& expectedEndpoint);
-
-  std::unordered_map<std::string, std::shared_ptr<DsfNode>> dsfNameToNodesMap_;
-  std::unordered_map<uint64_t, std::shared_ptr<DsfNode>> dsfSwitchIdToNodesMap_;
-  std::map<PortID, FabricEndpoint> actualNeighborReachability_;
 };
 } // namespace facebook::fboss
