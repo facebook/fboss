919e35a978e069dcabdeb5e90b921a3de07f91f9
Jasmeet Bagga <jasmeetbagga@meta.com>
2023-12-16 13:47:52 -0800

    When parsing TCP pkt, use payloadSize from ip header
    Summary:
    As titled, we were using cursor.totalLenght which
    can include the FCS bytes at the end of the ethernet frame.
    
    Reviewed By: zechengh09
    
    Differential Revision:
    D52227575
    
    Privacy Context Container: L1125642
    
    fbshipit-source-id: dcc06eff51c0b9878575ba8af738f836de30fa64

--- fboss/agent/packet/PktFactory.cpp
+++ fboss/agent/packet/PktFactory.cpp
@@ -33,7 +33,7 @@ IPPacket<AddrT>::IPPacket(folly::io::Cursor& cursor) {
     // if proto is udp, encapsulate udp
     udpPayLoad_ = UDPDatagram(cursor);
   } else if (nextHeader(hdr_) == static_cast<uint8_t>(IP_PROTO::IP_PROTO_TCP)) {
-    tcpPayLoad_ = TCPPacket(cursor, cursor.totalLength());
+    tcpPayLoad_ = TCPPacket(cursor, hdr_.payloadSize());
   }
 }
 
