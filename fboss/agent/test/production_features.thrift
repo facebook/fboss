0929775677dfc5db96f41e85ea1909581cfbd0b1
Ron He <zecheng@meta.com>
2023-12-20 12:27:11 -0800

    Define initial asic_features thrift file
    Summary:
    Define the asic_features file that would be used towards filtering agent hw tests based on feature supported on the ASIC.
    
    The idea of keeping it in fbcode, was mainly for OSS purpose. This file can directly consumed by vendors once agent hw tests are built in OSS.
    
    Reviewed By: jasmeetbagga
    
    Differential Revision: D52189897
    
    fbshipit-source-id: 3ac9f047531586063c0df8f52cfea71152b095ac

---
+++ fboss/agent/test/production_features.thrift
@@ -0,0 +1,15 @@
+#
+# Copyright 2004-present Facebook. All Rights Reserved.
+#
+
+namespace py neteng.fboss.production_features
+namespace py3 neteng.fboss
+namespace py.asyncio neteng.fboss.asyncio.production_features
+namespace cpp2 facebook.fboss.production_features
+namespace go neteng.fboss.production_features
+namespace php fboss_production_features
+
+enum ProductionFeature {
+  VOQ = 0,
+  FABRIC = 1,
+}
