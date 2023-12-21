ad847dd92db6ac8c19e9f9638114c7b58788631e
Jasmeet Bagga <jasmeetbagga@meta.com>
2023-12-20 16:59:08 -0800

    Get FDR cell drop counter test working on j3-ai
    Summary:
    For FDR block to come into play, we need to
    force traffic over the fabric.
    
    Reviewed By: shri-khare, nivinl
    
    Differential Revision: D52343507
    
    fbshipit-source-id: b43c966e17de7e0c52d2cbe4baf2d13fb80f1a47

--- fboss/agent/hw/test/HwVoqSwitchTests.cpp
+++ fboss/agent/hw/test/HwVoqSwitchTests.cpp
@@ -529,6 +529,39 @@ TEST_F(HwVoqSwitchWithFabricPortsTest, verifyNifMulticastTrafficDropped) {
   verifyAcrossWarmBoots(setup, verify);
 }
 
+TEST_F(HwVoqSwitchWithFabricPortsTest, fdrCellDrops) {
+  utility::EcmpSetupAnyNPorts6 ecmpHelper(getProgrammedState());
+  const auto kPort = ecmpHelper.ecmpPortDescriptorAt(0);
+  auto setup = [this, kPort]() {
+    addRemoveNeighbor(kPort, true /* add neighbor*/);
+    setForceTrafficOverFabric(getHwSwitch(), true);
+    std::string out;
+    getHwSwitchEnsemble()->runDiagCommand(
+        "setreg FDA_OFM_CLASS_DROP_TH_CORE 0x001001001001001001001001\n", out);
+    getHwSwitchEnsemble()->runDiagCommand("quit\n", out);
+  };
+
+  auto verify = [this, kPort, &ecmpHelper]() {
+    auto sendPkts = [this, kPort, &ecmpHelper]() {
+      for (auto i = 0; i < 1000; ++i) {
+        sendPacket(
+            ecmpHelper.ip(kPort),
+            std::nullopt,
+            std::vector<uint8_t>(1024, 0xff));
+      }
+    };
+    int64_t fdrCellDrops = 0;
+    WITH_RETRIES({
+      sendPkts();
+      getHwSwitch()->updateStats();
+      fb303::ThreadCachedServiceData::get()->publishStats();
+      fdrCellDrops = getHwSwitch()->getSwitchStats()->getFdrCellDrops();
+      XLOG(DBG2) << "FDR Cell drops : " << fdrCellDrops;
+      EXPECT_EVENTUALLY_GT(fdrCellDrops, 0);
+    });
+  };
+  verifyAcrossWarmBoots(setup, verify);
+}
 TEST_F(HwVoqSwitchTest, addRemoveNeighbor) {
   auto setup = [this]() {
     const PortDescriptor kPort(
@@ -953,39 +986,6 @@ TEST_F(HwVoqSwitchTest, dramEnqueueDequeueBytes) {
           getHwSwitch()->getSwitchStats()->getDramDequeuedBytes();
       XLOG(DBG2) << "Dram dequeued bytes : " << dramDequeuedBytes;
       EXPECT_EVENTUALLY_GT(dramDequeuedBytes, 0);
-    });
-  };
-  verifyAcrossWarmBoots(setup, verify);
-}
-
-TEST_F(HwVoqSwitchTest, fdrCellDrops) {
-  utility::EcmpSetupAnyNPorts6 ecmpHelper(getProgrammedState());
-  const auto kPort = ecmpHelper.ecmpPortDescriptorAt(0);
-  auto setup = [this, kPort]() {
-    addRemoveNeighbor(kPort, true /* add neighbor*/);
-    std::string out;
-    getHwSwitchEnsemble()->runDiagCommand(
-        "setreg FDA_OFM_CLASS_DROP_TH_CORE 0x001001001001001001001001\n", out);
-    getHwSwitchEnsemble()->runDiagCommand("quit\n", out);
-  };
-
-  auto verify = [this, kPort, &ecmpHelper]() {
-    auto sendPkts = [this, kPort, &ecmpHelper]() {
-      for (auto i = 0; i < 1000; ++i) {
-        sendPacket(
-            ecmpHelper.ip(kPort),
-            std::nullopt,
-            std::vector<uint8_t>(1024, 0xff));
-      }
-    };
-    int64_t fdrCellDrops = 0;
-    WITH_RETRIES({
-      sendPkts();
-      getHwSwitch()->updateStats();
-      fb303::ThreadCachedServiceData::get()->publishStats();
-      fdrCellDrops = getHwSwitch()->getSwitchStats()->getFdrCellDrops();
-      XLOG(DBG2) << "FDR Cell drops : " << fdrCellDrops;
-      EXPECT_EVENTUALLY_GT(fdrCellDrops, 0);
     });
   };
   verifyAcrossWarmBoots(setup, verify);
