e7a8b2be2950ac59812bb0a5dc8f52feb72cd018
Srikrishna Gopu <srikrishnagopu@meta.com>
2024-01-09 21:00:44 -0800

    remove sai version suffix for hw test/link test oss binaries
    Summary:
    As titled, remove sai version suffix for hw test and link test oss binaries.
    
    sai version to the binary names does not convey the proper meaning for the binary. Goal is to keep the same name as the internal build.
    
    Reviewed By: shri-khare
    
    Differential Revision: D52608698
    
    fbshipit-source-id: 7389012e49ce13103d293d3765b506758df4bdd5

--- cmake/AgentHwSaiHwTest.cmake
+++ cmake/AgentHwSaiHwTest.cmake
@@ -174,7 +174,7 @@ function(BUILD_SAI_TEST SAI_IMPL_NAME SAI_IMPL_ARG)
 
   message(STATUS "Building SAI_IMPL_NAME: ${SAI_IMPL_NAME} SAI_IMPL_ARG: ${SAI_IMPL_ARG}")
 
-  add_executable(sai_test-${SAI_IMPL_NAME}-${SAI_VER_SUFFIX}
+  add_executable(sai_test-${SAI_IMPL_NAME}
     fboss/agent/hw/sai/hw_test/dataplane_tests/SaiAclTableGroupTrafficTests.cpp
     fboss/agent/hw/sai/hw_test/HwTestTamUtils.cpp
     fboss/agent/hw/sai/hw_test/HwTestAclUtils.cpp
@@ -206,7 +206,7 @@ function(BUILD_SAI_TEST SAI_IMPL_NAME SAI_IMPL_ARG)
     fboss/agent/hw/sai/hw_test/SaiQPHRollbackTests.cpp
   )
 
-  target_link_libraries(sai_test-${SAI_IMPL_NAME}-${SAI_VER_SUFFIX}
+  target_link_libraries(sai_test-${SAI_IMPL_NAME}
     # --whole-archive is needed for gtest to find these tests
     -Wl,--whole-archive
     ${SAI_IMPL_ARG}
@@ -221,12 +221,12 @@ function(BUILD_SAI_TEST SAI_IMPL_NAME SAI_IMPL_ARG)
   )
 
   if (SAI_BRCM_IMPL)
-    target_link_libraries(sai_test-${SAI_IMPL_NAME}-${SAI_VER_SUFFIX}
+    target_link_libraries(sai_test-${SAI_IMPL_NAME}
       ${YAML}
     )
   endif()
 
-  set_target_properties(sai_test-${SAI_IMPL_NAME}-${SAI_VER_SUFFIX}
+  set_target_properties(sai_test-${SAI_IMPL_NAME}
       PROPERTIES COMPILE_FLAGS
       "-DSAI_VER_MAJOR=${SAI_VER_MAJOR} \
       -DSAI_VER_MINOR=${SAI_VER_MINOR}  \
@@ -239,7 +239,7 @@ if(BUILD_SAI_FAKE)
 BUILD_SAI_TEST("fake" fake_sai)
 install(
   TARGETS
-  sai_test-fake-${SAI_VER_SUFFIX})
+  sai_test-fake)
 endif()
 
 # If libsai_impl is provided, build sai tests linking with it
@@ -250,5 +250,5 @@ if(SAI_IMPL)
   BUILD_SAI_TEST("sai_impl" ${SAI_IMPL})
   install(
     TARGETS
-    sai_test-sai_impl-${SAI_VER_SUFFIX})
+    sai_test-sai_impl)
 endif()
