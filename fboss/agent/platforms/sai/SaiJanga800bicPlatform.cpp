12ca32237668a383873da1919ad1bcf0bffd7452
Srikrishna Gopu <srikrishnagopu@meta.com>
2024-01-23 12:49:42 -0800

    sai janga800bic platform support
    Summary: As titled, sai janga800bic platform support
    
    Reviewed By: daiwei1983
    
    Differential Revision: D52967981
    
    fbshipit-source-id: 354e817a96d32f739053b8bcfc446a9bb4fd5175

---
+++ fboss/agent/platforms/sai/SaiJanga800bicPlatform.cpp
@@ -0,0 +1,54 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#include "fboss/agent/platforms/sai/SaiJanga800bicPlatform.h"
+
+#include "fboss/agent/hw/switch_asics/Jericho3Asic.h"
+#include "fboss/agent/platforms/common/janga800bic/Janga800bicPlatformMapping.h"
+
+#include <cstdio>
+#include <cstring>
+namespace facebook::fboss {
+
+SaiJanga800bicPlatform::SaiJanga800bicPlatform(
+    std::unique_ptr<PlatformProductInfo> productInfo,
+    folly::MacAddress localMac,
+    const std::string& platformMappingStr)
+    : SaiBcmPlatform(
+          std::move(productInfo),
+          platformMappingStr.empty()
+              ? std::make_unique<Janga800bicPlatformMapping>()
+              : std::make_unique<Janga800bicPlatformMapping>(
+                    platformMappingStr),
+          localMac) {}
+
+void SaiJanga800bicPlatform::setupAsic(
+    cfg::SwitchType switchType,
+    std::optional<int64_t> switchId,
+    int16_t switchIndex,
+    std::optional<cfg::Range64> systemPortRange,
+    folly::MacAddress& mac) {
+  asic_ = std::make_unique<Jericho3Asic>(
+      switchType, switchId, switchIndex, systemPortRange, mac);
+}
+
+HwAsic* SaiJanga800bicPlatform::getAsic() const {
+  return asic_.get();
+}
+
+std::vector<sai_system_port_config_t>
+SaiJanga800bicPlatform::getInternalSystemPortConfig() const {
+  CHECK(asic_) << " Asic must be set before getting sys port info";
+  CHECK(asic_->getSwitchId()) << " Switch Id must be set before sys port info";
+  return {{0, static_cast<uint32_t>(*asic_->getSwitchId()), 0, 0, 10000, 8}};
+}
+SaiJanga800bicPlatform::~SaiJanga800bicPlatform() = default;
+
+} // namespace facebook::fboss
---
+++ fboss/agent/platforms/sai/SaiJanga800bicPlatform.h
@@ -0,0 +1,71 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+#pragma once
+
+#include "fboss/agent/platforms/sai/SaiBcmPlatform.h"
+
+namespace facebook::fboss {
+
+class Jericho3Asic;
+
+class SaiJanga800bicPlatform : public SaiBcmPlatform {
+ public:
+  SaiJanga800bicPlatform(
+      std::unique_ptr<PlatformProductInfo> productInfo,
+      folly::MacAddress localMac,
+      const std::string& platformMappingStr);
+  ~SaiJanga800bicPlatform() override;
+  HwAsic* getAsic() const override;
+
+  uint32_t numLanesPerCore() const override {
+    return 8;
+  }
+
+  uint32_t numCellsAvailable() const override {
+    return 130665;
+  }
+
+  bool isSerdesApiSupported() const override {
+    return true;
+  }
+
+  void initLEDs() override {}
+
+  std::vector<PortID> getAllPortsInGroup(PortID /*portID*/) const override {
+    return {};
+  }
+
+  std::vector<FlexPortMode> getSupportedFlexPortModes() const override {
+    return {};
+  }
+
+  bool supportInterfaceType() const override {
+    return true;
+  }
+
+  std::optional<sai_port_interface_type_t> getInterfaceType(
+      TransmitterTechnology /*transmitterTech*/,
+      cfg::PortSpeed /*speed*/) const override {
+    return std::nullopt;
+  }
+  std::vector<sai_system_port_config_t> getInternalSystemPortConfig()
+      const override;
+
+ private:
+  void setupAsic(
+      cfg::SwitchType switchType,
+      std::optional<int64_t> switchId,
+      int16_t switchIndex,
+      std::optional<cfg::Range64> systemPortRange,
+      folly::MacAddress& mac) override;
+  std::unique_ptr<Jericho3Asic> asic_;
+};
+
+} // namespace facebook::fboss
