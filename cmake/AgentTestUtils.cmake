08dbfe09b71121392d7cc1898c3bc14ddb028276
Midhun Somasundaran <msomasundaran@meta.com>
2024-02-13 09:31:11 -0800

    Create a copp test utils file
    Summary: creating a util file for coop. Next diff in stack moves non hwswitch/platform contents to this file
    
    Reviewed By: shri-khare
    
    Differential Revision: D53635111
    
    fbshipit-source-id: bbb8bdd157c84743a961213dbafe4492f65f2205

--- cmake/AgentTestUtils.cmake
+++ cmake/AgentTestUtils.cmake
@@ -12,3 +12,10 @@ target_link_libraries(agent_test_acl_utils
   switch_config_cpp2
   switch_state_cpp2
 )
+
+add_library(agent_test_copp_utils
+  fboss/agent/test/utils/AgentTestCoppUtils.cpp
+)
+
+target_link_libraries(agent_test_copp_utils
+)
