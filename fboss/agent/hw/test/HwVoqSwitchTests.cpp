e8026f2ebf755d0125c0da9920fe0b17c21b9f46
Jasmeet Bagga <jasmeetbagga@meta.com>
2024-02-26 10:43:46 -0800

    For fdr drops test allow stats to settle before asserting for no change
    Summary:
    Since to induce FDR drops we may endup sending several thousand pkts,
    not all stats may have accounted for these when we get fdr drop count
    increment. So set checkNoStatChange with retries. With checkNoStatChange
    we are basically looking for perpetual increments, check for which still
    stands
    
    Reviewed By: shri-khare, nivinl
    
    Differential Revision: D54180209
    
    fbshipit-source-id: 0557f6a8c2e25dc271a9efd95e3996c677a04a7c

--- fboss/agent/hw/test/HwVoqSwitchTests.cpp
+++ fboss/agent/hw/test/HwVoqSwitchTests.cpp
@@ -601,7 +601,7 @@ TEST_F(HwVoqSwitchWithFabricPortsTest, fdrCellDrops) {
     // Assert that we don't spuriously increment fdrCellDrops on every drop
     // stats. This would happen if we treated a stat as clear on read, while
     // in HW it was cumulative
-    checkNoStatsChange();
+    checkNoStatsChange(30 /*retries*/);
   };
   verifyAcrossWarmBoots(setup, verify);
 }
