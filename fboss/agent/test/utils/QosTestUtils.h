eb309bd67ec8286400f378942df0546c25a8bcad
Parvez Shaikh <pshaikh@meta.com>
2024-02-28 22:31:34 -0800

    disable ttl decrements api on sw switch
    Summary: as titled
    
    Reviewed By: jasmeetbagga
    
    Differential Revision: D54159817
    
    fbshipit-source-id: c61d54ebc8a4789e5e0d3f920d3d7b4b1c0bf918

--- fboss/agent/test/utils/QosTestUtils.h
+++ fboss/agent/test/utils/QosTestUtils.h
@@ -6,6 +6,7 @@
 #include "fboss/agent/test/EcmpSetupHelper.h"
 
 #include "fboss/agent/HwAsicTable.h"
+#include "fboss/agent/SwSwitch.h"
 #include "fboss/agent/state/SwitchState.h"
 #include "fboss/agent/types.h"
 
@@ -45,5 +46,19 @@ std::shared_ptr<SwitchState> disableTTLDecrements(
   } else {
     throw FbossError("Disable decrement not supported");
   }
+}
+
+template <typename EcmpNhopT>
+void disableTTLDecrements(
+    SwSwitch* sw,
+    RouterID routerId,
+    const EcmpNhopT& nhop) {
+  sw->updateStateBlocking(
+      "disable TTL decrement",
+      [sw, &routerId, &nhop](const std::shared_ptr<SwitchState>& state) {
+        auto newState = utility::disableTTLDecrements(
+            sw->getHwAsicTable(), state, routerId, nhop);
+        return newState;
+      });
 }
 } // namespace facebook::fboss::utility
