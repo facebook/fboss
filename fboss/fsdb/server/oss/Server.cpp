adadcb9693677536dac8a38cc7d4ae82f3b76d09
Peyman Gardideh <pgardideh@meta.com>
2024-03-12 10:11:36 -0700

    Implement oss thrift server
    Summary: We don't have service framework in oss, using a raw thrift server
    
    Differential Revision:
    D54593208
    
    Privacy Context Container: L1158612
    
    fbshipit-source-id: a3dc3e71b0dc1c2e62f03c48bf67d2ba20da394d

--- fboss/fsdb/server/oss/Server.cpp
+++ fboss/fsdb/server/oss/Server.cpp
@@ -1,4 +1,5 @@
 #include "fboss/fsdb/server/Server.h"
+#include <thrift/lib/cpp2/server/ThriftServer.h>
 
 namespace facebook::fboss::fsdb {
 
@@ -7,7 +8,11 @@ void setVersionString() {}
 void startThriftServer(
     std::shared_ptr<apache::thrift::ThriftServer> server,
     std::shared_ptr<ServiceHandler> handler) {
-  // TODO: run a regular thrift server
+  auto evbThread =
+      std::make_shared<folly::ScopedEventBaseThread>("fsdbSigHandlerThread");
+  SignalHandler signalHandler(
+      evbThread->getEventBase(), [server]() { server->stop(); });
+  server->serve();
 }
 
 } // namespace facebook::fboss::fsdb
