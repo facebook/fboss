4c2f22443c30f8755398e1e4dacdb39b5fdb1cfa
Jasmeet Bagga <jasmeetbagga@meta.com>
2024-03-24 22:42:46 -0700

    Add fabric topology CLI moel
    Summary: As titled
    
    Reviewed By: zechengh09
    
    Differential Revision: D55298938
    
    fbshipit-source-id: d1348099a37aa7aad8411277a31935365d0b967b

---
+++ fboss/cli/fboss2/commands/show/fabric/topology/model.thrift
@@ -0,0 +1,13 @@
+namespace cpp2 facebook.fboss.cli
+
+struct ShowFabricTopologyModel {
+  1: list<FabricVirtualDeviceTopology> virtualDeviceTopology;
+}
+
+struct FabricVirtualDeviceTopology {
+  1: i64 virtualDeviceId;
+  2: i32 numConnections;
+  3: i64 remoteSwitchId;
+  4: string remoteSwitchName;
+  5: list<string> connectingPorts;
+}
