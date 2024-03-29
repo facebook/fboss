2a8785077f909fed21fc51f44a3a41689ee10e4d
Jeff Kim <jeffkim8482@meta.com>
2024-03-28 18:43:59 -0700

    Create Separate PRBS Hardware Testing Class for Fabric Switches
    Summary:
    To handle fabric devices properly, I created a separate PRBS hardware testing class AgentFabricPrbsTest, which enables fabric ports throughout the tests.
    
       OK : 2
       FAILED : 0
       SKIPPED : 0
       TIMEOUT : 0
    
    Test output stored at: hwtest_results_2024_Mar_28-02_35_58_PM.csv
    ```
    
    Reviewed By: zechengh09
    
    Differential Revision: D55447595
    
    fbshipit-source-id: 484ba10ceced7a7f27c25b4d00e3306e18d4e63a

--- fboss/agent/test/agent_hw_tests/AgentPrbsTests.cpp
+++ fboss/agent/test/agent_hw_tests/AgentPrbsTests.cpp
@@ -1,6 +1,8 @@
 // (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
 
+#include "fboss/agent/hw/test/ConfigFactory.h"
 #include "fboss/agent/test/AgentHwTest.h"
+#include "fboss/agent/test/utils/FabricTestUtils.h"
 #include "fboss/lib/CommonUtils.h"
 
 #include "fboss/agent/test/gen-cpp2/production_features_types.h"
@@ -17,75 +19,122 @@ class AgentPrbsTest : public AgentHwTest {
   getProductionFeaturesVerified() const override {
     return {production_features::ProductionFeature::PRBS};
   }
+
   std::vector<PortID> getTestPortIds() const {
     return masterLogicalPortIds(
         {cfg::PortType::INTERFACE_PORT,
          cfg::PortType::FABRIC_PORT,
          cfg::PortType::MANAGEMENT_PORT});
   }
+
+ protected:
+  void testEnablePortPrbs() {
+    auto setup = [=, this]() {
+      phy::PortPrbsState prbsState;
+      prbsState.enabled() = true;
+      prbsState.polynominal() = kPrbsPolynomial;
+      applyNewState([&](const std::shared_ptr<SwitchState>& in) {
+        auto out = in->clone();
+        for (const auto& portId : getTestPortIds()) {
+          auto port = out->getPorts()->getNodeIf(portId);
+          auto newPort = port->modify(&out);
+          newPort->setAsicPrbs(prbsState);
+        }
+        return out;
+      });
+    };
+    auto verify = [=, this]() {
+      for (const auto& portId : getTestPortIds()) {
+        auto port = getProgrammedState()->getPorts()->getNodeIf(portId);
+        auto asicPrbs = port->getAsicPrbs();
+        EXPECT_TRUE(*asicPrbs.enabled());
+        EXPECT_EQ(*asicPrbs.polynominal(), kPrbsPolynomial);
+      }
+    };
+    verifyAcrossWarmBoots(setup, verify);
+  }
+
+  void testDisablePortPrbs() {
+    auto setup = [=, this]() {
+      phy::PortPrbsState initialPrbsState;
+      initialPrbsState.enabled() = true;
+      initialPrbsState.polynominal() = kPrbsPolynomial;
+      applyNewState([&](const std::shared_ptr<SwitchState>& in) {
+        auto out = in->clone();
+        for (const auto& portId : getTestPortIds()) {
+          auto port = out->getPorts()->getNodeIf(portId);
+          auto newPort = port->modify(&out);
+          newPort->setAsicPrbs(initialPrbsState);
+        }
+        return out;
+      });
+      phy::PortPrbsState finalPrbsState;
+      finalPrbsState.enabled() = false;
+      applyNewState([&](const std::shared_ptr<SwitchState>& in) {
+        auto out = in->clone();
+        for (const auto& portId : getTestPortIds()) {
+          auto port = out->getPorts()->getNodeIf(portId);
+          auto newPort = port->modify(&out);
+          newPort->setAsicPrbs(finalPrbsState);
+        }
+        return out;
+      });
+    };
+    auto verify = [=, this]() {
+      for (const auto& portId : getTestPortIds()) {
+        auto port = getProgrammedState()->getPorts()->getNodeIf(portId);
+        auto asicPrbs = port->getAsicPrbs();
+        EXPECT_FALSE(*asicPrbs.enabled());
+        EXPECT_EQ(*asicPrbs.polynominal(), 0);
+      }
+    };
+    verifyAcrossWarmBoots(setup, verify);
+  }
+
+ private:
+  bool hideFabricPorts() const override {
+    return false;
+  }
+};
+
+class AgentFabricPrbsTest : public AgentPrbsTest {
+ public:
+  std::vector<production_features::ProductionFeature>
+  getProductionFeaturesVerified() const override {
+    return {
+        production_features::ProductionFeature::FABRIC,
+        production_features::ProductionFeature::PRBS};
+  }
+
+  cfg::SwitchConfig initialConfig(
+      const AgentEnsemble& ensemble) const override {
+    auto config = utility::onePortPerInterfaceConfig(
+        ensemble.getSw(),
+        ensemble.masterLogicalPortIds(),
+        false /*interfaceHasSubnet*/,
+        false /*setInterfaceMac*/,
+        utility::kBaseVlanId,
+        true /*enable fabric ports*/);
+    utility::populatePortExpectedNeighbors(
+        ensemble.masterLogicalPortIds(), config);
+    return config;
+  }
 };
 
 TEST_F(AgentPrbsTest, enablePortPrbs) {
-  auto setup = [=, this]() {
-    phy::PortPrbsState prbsState;
-    prbsState.enabled() = true;
-    prbsState.polynominal() = kPrbsPolynomial;
-    applyNewState([&](const std::shared_ptr<SwitchState>& in) {
-      auto out = in->clone();
-      for (const auto& portId : getTestPortIds()) {
-        auto port = out->getPorts()->getNodeIf(portId);
-        auto newPort = port->modify(&out);
-        newPort->setAsicPrbs(prbsState);
-      }
-      return out;
-    });
-  };
-  auto verify = [=, this]() {
-    for (const auto& portId : getTestPortIds()) {
-      auto port = getProgrammedState()->getPorts()->getNodeIf(portId);
-      auto asicPrbs = port->getAsicPrbs();
-      EXPECT_TRUE(*asicPrbs.enabled());
-      EXPECT_EQ(*asicPrbs.polynominal(), kPrbsPolynomial);
-    }
-  };
-  verifyAcrossWarmBoots(setup, verify);
+  testEnablePortPrbs();
 }
 
 TEST_F(AgentPrbsTest, disablePortPrbs) {
-  auto setup = [=, this]() {
-    phy::PortPrbsState initialPrbsState;
-    initialPrbsState.enabled() = true;
-    initialPrbsState.polynominal() = kPrbsPolynomial;
-    applyNewState([&](const std::shared_ptr<SwitchState>& in) {
-      auto out = in->clone();
-      for (const auto& portId : getTestPortIds()) {
-        auto port = out->getPorts()->getNodeIf(portId);
-        auto newPort = port->modify(&out);
-        newPort->setAsicPrbs(initialPrbsState);
-      }
-      return out;
-    });
-    phy::PortPrbsState finalPrbsState;
-    finalPrbsState.enabled() = false;
-    applyNewState([&](const std::shared_ptr<SwitchState>& in) {
-      auto out = in->clone();
-      for (const auto& portId : getTestPortIds()) {
-        auto port = out->getPorts()->getNodeIf(portId);
-        auto newPort = port->modify(&out);
-        newPort->setAsicPrbs(finalPrbsState);
-      }
-      return out;
-    });
-  };
-  auto verify = [=, this]() {
-    for (const auto& portId : getTestPortIds()) {
-      auto port = getProgrammedState()->getPorts()->getNodeIf(portId);
-      auto asicPrbs = port->getAsicPrbs();
-      EXPECT_FALSE(*asicPrbs.enabled());
-      EXPECT_EQ(*asicPrbs.polynominal(), 0);
-    }
-  };
-  verifyAcrossWarmBoots(setup, verify);
+  testDisablePortPrbs();
+}
+
+TEST_F(AgentFabricPrbsTest, enablePortPrbs) {
+  testEnablePortPrbs();
+}
+
+TEST_F(AgentFabricPrbsTest, disablePortPrbs) {
+  testDisablePortPrbs();
 }
 
 } // namespace facebook::fboss
