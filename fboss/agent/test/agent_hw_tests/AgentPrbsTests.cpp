6e0dd5549b05a78c7e297ba8780c8b103de37b25
Jeff Kim <jeffkim8482@meta.com>
2024-03-28 18:43:59 -0700

    Extend PRBS Hardware Testing to All Interface, Fabric, and Management Ports
    Summary: I extended both agent hardware PRBS test cases to verify SAI attributes for all interface, fabric, and management ports when enabling and disabling PRBS.
    
    Reviewed By: zechengh09
    
    Differential Revision: D55345620
    
    fbshipit-source-id: d6982188efce71b9ba03baaa3e9422e4004d5379

--- fboss/agent/test/agent_hw_tests/AgentPrbsTests.cpp
+++ fboss/agent/test/agent_hw_tests/AgentPrbsTests.cpp
@@ -17,69 +17,73 @@ class AgentPrbsTest : public AgentHwTest {
   getProductionFeaturesVerified() const override {
     return {production_features::ProductionFeature::PRBS};
   }
+  std::vector<PortID> getTestPortIds() const {
+    return masterLogicalPortIds(
+        {cfg::PortType::INTERFACE_PORT,
+         cfg::PortType::FABRIC_PORT,
+         cfg::PortType::MANAGEMENT_PORT});
+  }
 };
 
 TEST_F(AgentPrbsTest, enablePortPrbs) {
   auto setup = [=, this]() {
-    EXPECT_GT(masterLogicalPortIds({cfg::PortType::INTERFACE_PORT}).size(), 0);
-    auto interfacePortId =
-        PortID(masterLogicalPortIds({cfg::PortType::INTERFACE_PORT})[0]);
     phy::PortPrbsState prbsState;
     prbsState.enabled() = true;
     prbsState.polynominal() = kPrbsPolynomial;
     applyNewState([&](const std::shared_ptr<SwitchState>& in) {
       auto out = in->clone();
-      auto port = out->getPorts()->getNodeIf(interfacePortId);
-      auto newPort = port->modify(&out);
-      newPort->setAsicPrbs(prbsState);
+      for (const auto& portId : getTestPortIds()) {
+        auto port = out->getPorts()->getNodeIf(portId);
+        auto newPort = port->modify(&out);
+        newPort->setAsicPrbs(prbsState);
+      }
       return out;
     });
   };
   auto verify = [=, this]() {
-    EXPECT_GT(masterLogicalPortIds({cfg::PortType::INTERFACE_PORT}).size(), 0);
-    auto interfacePortId =
-        PortID(masterLogicalPortIds({cfg::PortType::INTERFACE_PORT})[0]);
-    auto port = getProgrammedState()->getPorts()->getNodeIf(interfacePortId);
-    auto asicPrbs = port->getAsicPrbs();
-    EXPECT_TRUE(*asicPrbs.enabled());
-    EXPECT_EQ(*asicPrbs.polynominal(), kPrbsPolynomial);
+    for (const auto& portId : getTestPortIds()) {
+      auto port = getProgrammedState()->getPorts()->getNodeIf(portId);
+      auto asicPrbs = port->getAsicPrbs();
+      EXPECT_TRUE(*asicPrbs.enabled());
+      EXPECT_EQ(*asicPrbs.polynominal(), kPrbsPolynomial);
+    }
   };
   verifyAcrossWarmBoots(setup, verify);
 }
 
 TEST_F(AgentPrbsTest, disablePortPrbs) {
   auto setup = [=, this]() {
-    EXPECT_GT(masterLogicalPortIds({cfg::PortType::INTERFACE_PORT}).size(), 0);
-    auto interfacePortId =
-        PortID(masterLogicalPortIds({cfg::PortType::INTERFACE_PORT})[0]);
     phy::PortPrbsState initialPrbsState;
     initialPrbsState.enabled() = true;
     initialPrbsState.polynominal() = kPrbsPolynomial;
     applyNewState([&](const std::shared_ptr<SwitchState>& in) {
       auto out = in->clone();
-      auto port = out->getPorts()->getNodeIf(interfacePortId);
-      auto newPort = port->modify(&out);
-      newPort->setAsicPrbs(initialPrbsState);
+      for (const auto& portId : getTestPortIds()) {
+        auto port = out->getPorts()->getNodeIf(portId);
+        auto newPort = port->modify(&out);
+        newPort->setAsicPrbs(initialPrbsState);
+      }
       return out;
     });
     phy::PortPrbsState finalPrbsState;
     finalPrbsState.enabled() = false;
     applyNewState([&](const std::shared_ptr<SwitchState>& in) {
       auto out = in->clone();
-      auto port = out->getPorts()->getNodeIf(interfacePortId);
-      auto newPort = port->modify(&out);
-      newPort->setAsicPrbs(finalPrbsState);
+      for (const auto& portId : getTestPortIds()) {
+        auto port = out->getPorts()->getNodeIf(portId);
+        auto newPort = port->modify(&out);
+        newPort->setAsicPrbs(finalPrbsState);
+      }
       return out;
     });
   };
   auto verify = [=, this]() {
-    EXPECT_GT(masterLogicalPortIds({cfg::PortType::INTERFACE_PORT}).size(), 0);
-    auto interfacePortId =
-        PortID(masterLogicalPortIds({cfg::PortType::INTERFACE_PORT})[0]);
-    auto port = getProgrammedState()->getPorts()->getNodeIf(interfacePortId);
-    auto asicPrbs = port->getAsicPrbs();
-    EXPECT_FALSE(*asicPrbs.enabled());
-    EXPECT_EQ(*asicPrbs.polynominal(), 0);
+    for (const auto& portId : getTestPortIds()) {
+      auto port = getProgrammedState()->getPorts()->getNodeIf(portId);
+      auto asicPrbs = port->getAsicPrbs();
+      EXPECT_FALSE(*asicPrbs.enabled());
+      EXPECT_EQ(*asicPrbs.polynominal(), 0);
+    }
   };
   verifyAcrossWarmBoots(setup, verify);
 }
