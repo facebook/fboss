fc81b252bc6340740c21bd73c11d623c3852c74d
Jasmeet Bagga <jasmeetbagga@meta.com>
2024-03-31 17:44:07 -0700

    Add Link change event syncer
    Summary:
    Will be used to sync
    - link up/down event
    - link inactive/active events
    - link connectivity change events
    
    Reviewed By: shri-khare
    
    Differential Revision: D55574961
    
    fbshipit-source-id: faf596d769ccbeba92a64a387069b2a8eecebe3a

---
+++ fboss/agent/mnpu/LinkChangeEventSyncer.cpp
@@ -0,0 +1,47 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+#include "fboss/agent/mnpu/LinkChangeEventSyncer.h"
+#include "fboss/agent/HwSwitch.h"
+#if FOLLY_HAS_COROUTINES
+#include <folly/experimental/coro/BlockingWait.h>
+#endif
+
+namespace facebook::fboss {
+
+LinkChangeEventSyncer::LinkChangeEventSyncer(
+    uint16_t serverPort,
+    SwitchID switchId,
+    folly::EventBase* connRetryEvb,
+    HwSwitch* hw)
+    : ThriftSinkClient<multiswitch::LinkChangeEvent>::ThriftSinkClient(
+          "LinkChangeEventThriftSyncer",
+          serverPort,
+          switchId,
+          LinkChangeEventSyncer::initLinkChangeEventSink,
+          std::make_shared<folly::ScopedEventBaseThread>(
+              "LinkChangeEventSyncerThread"),
+          connRetryEvb),
+      hw_(hw) {}
+
+LinkChangeEventSyncer::EventSink LinkChangeEventSyncer::initLinkChangeEventSink(
+    SwitchID switchId,
+    apache::thrift::Client<multiswitch::MultiSwitchCtrl>* client) {
+#if FOLLY_HAS_COROUTINES
+  return folly::coro::blockingWait(client->co_notifyLinkChangeEvent(switchId));
+#else
+  return apache::thrift::ClientSink<multiswitch::LinkChangeEvent, bool>();
+#endif
+}
+
+void LinkChangeEventSyncer::connected() {
+  // TODO
+}
+
+} // namespace facebook::fboss
