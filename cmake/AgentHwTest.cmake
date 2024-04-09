4569f80e6ca1a4b968e323f4d67ccd7c5a97d25c
Jasmeet Bagga <jasmeetbagga@meta.com>
2024-04-08 23:39:21 -0700

    Kill already ported deep packet inspection tests from hw test layer
    Summary: As titled
    
    Reviewed By: msomasundaran
    
    Differential Revision: D55906043
    
    fbshipit-source-id: f0f155e8220707f259fa2929883cf5cd8ede725a

--- cmake/AgentHwTest.cmake
+++ cmake/AgentHwTest.cmake
@@ -237,7 +237,6 @@ set(hw_switch_test_srcs
   fboss/agent/hw/test/dataplane_tests/HwConfigSetupTest.cpp
   fboss/agent/hw/test/dataplane_tests/HwConfigVerifyQosTests.cpp
   fboss/agent/hw/test/dataplane_tests/HwCoppTests.cpp
-  fboss/agent/hw/test/dataplane_tests/HwDeepPacketInspectionTests.cpp
   fboss/agent/hw/test/dataplane_tests/HwAqmTests.cpp
   fboss/agent/hw/test/dataplane_tests/HwInPauseDiscardsTests.cpp
   fboss/agent/hw/test/dataplane_tests/HwIpInIpTunnelTests.cpp
