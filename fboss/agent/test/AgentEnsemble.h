cc317507ae044565a4bbe5b14c505cd4dfdd6835
Ron He <zecheng@meta.com>
2024-04-09 09:31:24 -0700

    Back out "Back out "[fboss] agent ensemble to apply initial config only on cold boot""
    Summary:
    Original commit changeset: db599a55a18c
    
    Original Phabricator Diff: D55352509
    
    Reapply D55213838 after all the issues being fixed.
    
    Original Summary -
    
    during warm boot read config created during cold boot run
    agent ensemble copies input config file at /var/facebook/fboss/agent_ensemble/agent.conf and all updates to config are applied here.
    next warm boot uses this file for starting or creating switch
    
    Reviewed By: phshaikh
    
    Differential Revision:
    D55805850
    
    Privacy Context Container: L1125642
    
    fbshipit-source-id: 2f20226f580ae1f179cd0e22acc3365efd97a253

--- fboss/agent/test/AgentEnsemble.h
+++ fboss/agent/test/AgentEnsemble.h
@@ -33,7 +33,7 @@ using AgentEnsemblePlatformConfigFn = std::function<void(cfg::PlatformConfig&)>;
 
 class AgentEnsemble : public TestEnsembleIf {
  public:
-  AgentEnsemble() {}
+  AgentEnsemble() : AgentEnsemble("agent.conf") {}
   explicit AgentEnsemble(const std::string& configFileName);
   virtual ~AgentEnsemble() override;
   using TestEnsembleIf::masterLogicalPortIds;
@@ -119,7 +119,7 @@ class AgentEnsemble : public TestEnsembleIf {
 
   static void enableExactMatch(bcm::BcmConfig& config);
 
-  static std::string getInputConfigFile();
+  std::string getInputConfigFile();
 
   void packetReceived(std::unique_ptr<RxPacket> pkt) noexcept override {
     getSw()->packetReceived(std::move(pkt));
@@ -259,6 +259,10 @@ class AgentEnsemble : public TestEnsembleIf {
   }
 
  private:
+  void setConfigFiles(const std::string& fileName);
+  void setBootType();
+  void overrideConfigFlag(const std::string& fileName);
+
   void writeConfig(const cfg::SwitchConfig& config);
   void writeConfig(const cfg::AgentConfig& config);
   void writeConfig(const cfg::AgentConfig& config, const std::string& file);
@@ -270,9 +274,10 @@ class AgentEnsemble : public TestEnsembleIf {
   cfg::SwitchConfig initialConfig_;
   std::unique_ptr<std::thread> asyncInitThread_{nullptr};
   std::vector<PortID> masterLogicalPortIds_;
-  std::string configFile_{"agent.conf"};
+  std::string configFile_{};
   std::unique_ptr<LinkStateToggler> linkToggler_;
   cfg::PortLoopbackMode mode_{cfg::PortLoopbackMode::MAC};
+  BootType bootType_{BootType::COLD_BOOT};
 };
 
 void initEnsemble(
