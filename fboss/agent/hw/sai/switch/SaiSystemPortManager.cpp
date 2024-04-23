0c1ed659972cdcd813f3e19259975dd72f5f3e9f
Wei Dai <daiweix@meta.com>
2024-04-22 18:26:10 -0700

    Back out agent changes in D56173465
    Summary:
    Currently, it looks like SAI_SYSTEM_PORT_ATTR_QOS_TC_TO_QUEUE_MAP is not fully supported during system port creation, see supported attributes at https://fburl.com/code/mn4dmjig
    
    Before we ask broadcom sai to support it, remove this SAI_SYSTEM_PORT_ATTR_QOS_TC_TO_QUEUE_MAP attribute setting from system port creation.
    
    Reviewed By: shri-khare
    
    Differential Revision:
    D56429143
    
    Privacy Context Container: L1125642
    
    fbshipit-source-id: eb8abf43df81ec9eed3c1436b7e8df09cfe571ff

--- fboss/agent/hw/sai/switch/SaiSystemPortManager.cpp
+++ fboss/agent/hw/sai/switch/SaiSystemPortManager.cpp
@@ -72,18 +72,8 @@ SaiSystemPortManager::attributesFromSwSystemPort(
       .speed = static_cast<uint32_t>(swSystemPort->getSpeedMbps()),
       .num_voq = static_cast<uint32_t>(swSystemPort->getNumVoqs()),
   };
-  std::optional<SaiSystemPortTraits::Attributes::QosTcToQueueMap>
-      qosTcToQueueMap = std::nullopt;
-  auto qosMapHandle =
-      managerTable_->qosMapManager().getQosMap(swSystemPort->getQosPolicy());
-  if (qosMapHandle && qosMapHandle->tcToVoqMap) {
-    auto qosMap = qosMapHandle->tcToVoqMap;
-    auto qosMapId = qosMap->adapterKey();
-    qosTcToQueueMap =
-        SaiSystemPortTraits::Attributes::QosTcToQueueMap{qosMapId};
-  }
   return SaiSystemPortTraits::CreateAttributes{
-      config, true /*enabled*/, qosTcToQueueMap};
+      config, true /*enabled*/, std::nullopt};
 }
 
 SystemPortSaiId SaiSystemPortManager::addSystemPort(
@@ -120,6 +110,7 @@ SystemPortSaiId SaiSystemPortManager::addSystemPort(
   concurrentIndices_->sysPortSaiIds.insert(
       {swSystemPort->getID(), saiSystemPort->adapterKey()});
   configureQueues(swSystemPort, swSystemPort->getPortQueues()->impl());
+  setQosPolicy(swSystemPort->getID(), swSystemPort->getQosPolicy());
   return saiSystemPort->adapterKey();
 }
 
