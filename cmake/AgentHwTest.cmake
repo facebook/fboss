5d56eba8aaacca9f678a5a21bb15c9fff25dc5ee
Ron He <zecheng@meta.com>
2024-05-01 19:23:49 -0700

    Remove hw route overflow tests
    Summary: As titled. Migrated to agent hw tests.
    
    Reviewed By: jasmeetbagga
    
    Differential Revision: D56635742
    
    fbshipit-source-id: 971886f57dec2da98b3f553510bebe380def89a2

--- cmake/AgentHwTest.cmake
+++ cmake/AgentHwTest.cmake
@@ -251,7 +251,6 @@ set(hw_switch_test_srcs
   fboss/agent/hw/test/dataplane_tests/HwQueuePerHostL2Tests.cpp
   fboss/agent/hw/test/dataplane_tests/HwQueuePerHostTests.cpp
   fboss/agent/hw/test/dataplane_tests/HwQueuePerHostRouteTests.cpp
-  fboss/agent/hw/test/dataplane_tests/HwRouteOverflowTest.cpp
   fboss/agent/hw/test/dataplane_tests/HwSflowTests.cpp
   fboss/agent/hw/test/dataplane_tests/HwSflowMirrorTest.cpp
   fboss/agent/hw/test/dataplane_tests/HwSwitchStatsTxCounterTests.cpp
