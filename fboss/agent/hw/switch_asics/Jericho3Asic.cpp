1393c5522359771d1e169b6f8aec4a3a267a26c3
Srikrishna Gopu <srikrishnagopu@meta.com>
2024-05-07 01:16:23 -0700

    set the loopback for eventor port
    Summary: As titled, set the loopback mode for eventor port
    
    Reviewed By: shri-khare
    
    Differential Revision: D57011362
    
    fbshipit-source-id: 828fb34df9fd1860fefea8e18b7889ff611ef1d7

--- fboss/agent/hw/switch_asics/Jericho3Asic.cpp
+++ fboss/agent/hw/switch_asics/Jericho3Asic.cpp
@@ -273,7 +273,8 @@ Jericho3Asic::desiredLoopbackModes() const {
       {cfg::PortType::INTERFACE_PORT, cfg::PortLoopbackMode::PHY},
       {cfg::PortType::MANAGEMENT_PORT, cfg::PortLoopbackMode::PHY},
       {cfg::PortType::FABRIC_PORT, cfg::PortLoopbackMode::MAC},
-      {cfg::PortType::RECYCLE_PORT, cfg::PortLoopbackMode::NONE}};
+      {cfg::PortType::RECYCLE_PORT, cfg::PortLoopbackMode::NONE},
+      {cfg::PortType::EVENTOR_PORT, cfg::PortLoopbackMode::NONE}};
   return kLoopbackMode;
 }
 
