bb260ea022af803d00741372f546a38cf5e6f60d
Richard Barnes <rbarnes@meta.com>
2024-05-07 09:46:58 -0700

    result_of -> invoke_result in fboss/cli/fboss2/CLI11/App.hpp
    Summary: `result_of` is eliminated in C++20, so we use `invoke_result` instead
    
    Reviewed By: palmje
    
    Differential Revision: D57016785
    
    fbshipit-source-id: 7faba6a37a51237c04017b192c840645d3f42b05

--- fboss/cli/fboss2/CLI11/App.hpp
+++ fboss/cli/fboss2/CLI11/App.hpp
@@ -3068,32 +3068,20 @@ inline std::string help(const App *app, const Error &e) {
 namespace detail {
 /// This class is simply to allow tests access to App's protected functions
 struct AppFriend {
-#ifdef CLI11_CPP14
-
-    /// Wrap _parse_short, perfectly forward arguments and return
-    template <typename... Args> static decltype(auto) parse_arg(App *app, Args &&... args) {
-        return app->_parse_arg(std::forward<Args>(args)...);
-    }
-
-    /// Wrap _parse_subcommand, perfectly forward arguments and return
-    template <typename... Args> static decltype(auto) parse_subcommand(App *app, Args &&... args) {
-        return app->_parse_subcommand(std::forward<Args>(args)...);
-    }
-#else
     /// Wrap _parse_short, perfectly forward arguments and return
     template <typename... Args>
     static auto parse_arg(App *app, Args &&... args) ->
-        typename std::result_of<decltype (&App::_parse_arg)(App, Args...)>::type {
+        typename std::invoke_result_t<decltype (&App::_parse_arg)(App, Args...)> {
         return app->_parse_arg(std::forward<Args>(args)...);
     }
 
     /// Wrap _parse_subcommand, perfectly forward arguments and return
     template <typename... Args>
     static auto parse_subcommand(App *app, Args &&... args) ->
-        typename std::result_of<decltype (&App::_parse_subcommand)(App, Args...)>::type {
+        typename std::invoke_result_t<decltype (&App::_parse_subcommand)(App, Args...)> {
         return app->_parse_subcommand(std::forward<Args>(args)...);
     }
-#endif
+
     /// Wrap the fallthrough parent function to make sure that is working correctly
     static App *get_fallthrough_parent(App *app) { return app->_get_fallthrough_parent(); }
 };
