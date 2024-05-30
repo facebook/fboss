a24f2ef04246866decc6cf9416c4665d8c7938e7
Jeff Kim <jeffkim8482@meta.com>
2024-05-29 20:51:35 -0700

    Set Default Logging Level to DBG2 for SAI Link Testing
    Summary: I set the default logging level to DBG2 when running SAI link tests.
    
    Reviewed By: harshitgulati18
    
    Differential Revision:
    D57935031
    
    Privacy Context Container: L1125642
    
    fbshipit-source-id: 9e817dcbc3213e55ecfbb9105fac328cb85cdc9d

--- fboss/agent/test/link_tests/SaiLinkTest.cpp
+++ fboss/agent/test/link_tests/SaiLinkTest.cpp
@@ -1,7 +1,14 @@
 // (c) Facebook, Inc. and its affiliates. Confidential and proprietary.
 
+#include <folly/logging/Init.h>
 #include "fboss/agent/platforms/sai/SaiPlatformInit.h"
 #include "fboss/agent/test/link_tests/LinkTest.h"
+
+#ifdef IS_OSS
+FOLLY_INIT_LOGGING_CONFIG("DBG2; default:async=true");
+#else
+FOLLY_INIT_LOGGING_CONFIG("fboss=DBG2; default:async=true");
+#endif
 
 int main(int argc, char* argv[]) {
   std::optional<facebook::fboss::cfg::StreamType> streamType = std::nullopt;
