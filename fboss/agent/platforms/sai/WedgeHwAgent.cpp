bdfe29ef0e366633e3cac70615f72f30d45dcb8d
Wei Dai <daiweix@meta.com>
2024-06-24 17:21:57 -0700

    temporarily set rocket_frame_parser=legacy in fboss
    Summary:
    fboss multi_switch test is failing after changing thrift setting rocket_frame_parser from "legacy" to "strategy" in D56196624. Below comments are from Yuhan/thrift team. It looks like we may need to change our use of IOBuf by following example D58225508. It may take a while to make the change and fully verify. So better to still use old legacy mode temporarily in the meanwhile by diff.
    
    """
    https://www.internalfb.com/code/fbsource/[07f940666bc86ddd2f25b1123bfb38ceef887099]/fbcode/fboss/agent/mnpu/TxPktEventSyncer.cpp?lines=70 I think there may be some issue with this line.
    txPkt is https://www.internalfb.com/code/fbsource/[07f940666bc86ddd2f25b1123bfb38ceef887099]/fbcode/fboss/agent/if/multiswitch_ctrl.thrift?lines=16
    and thrift do not give any guarantee that IOBuf is unchained
    meaning there may be multiple underlying buffers under IOBuf
    In previous implementation, if binary is small enough, returned IOBuf will be unchained, but newer implementation do not give such guarantee
    """
    
    Reviewed By: peygar, zechengh09
    
    Differential Revision: D58957983
    
    fbshipit-source-id: 253d0a284230aaeb01a9c437d57696cc2f5f3689

--- fboss/agent/platforms/sai/WedgeHwAgent.cpp
+++ fboss/agent/platforms/sai/WedgeHwAgent.cpp
@@ -18,6 +18,7 @@ using namespace facebook::fboss;
 
 int main(int argc, char* argv[]) {
   setSDKVersionInfo();
+  THRIFT_FLAG_SET_MOCK(rocket_frame_parser, "legacy");
   return facebook::fboss::hwAgentMain(
       argc,
       argv,
