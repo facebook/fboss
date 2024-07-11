dd35eca4649322e3b5889a445c24bcf9c12bcb36
Shiva Ram Menta <smenta@meta.com>
2024-07-10 21:08:52 -0700

    Optics Config Validation: Update TransceiverManagerTestHelper to Create Mock Validation Config
    Summary:
    Updating `TransceiverManagerTestHelper`'s mock qsfp_config file to include fake vendors and fake transceiver configurations to use for unit tests.
    
    Will add more variation in firmware versions for mock transceiver configs once we start failing validation on firmware.
    
    Reviewed By: srikrishnagopu
    
    Differential Revision: D58946473
    
    fbshipit-source-id: e50d96420acf61d4b38f379a490db0ec77b41be3

--- fboss/qsfp_service/test/TransceiverManagerTestHelper.cpp
+++ fboss/qsfp_service/test/TransceiverManagerTestHelper.cpp
@@ -8,10 +8,12 @@
  *
  */
 #include "fboss/qsfp_service/test/TransceiverManagerTestHelper.h"
+#include "fboss/qsfp_service/if/gen-cpp2/transceiver_validation_types.h"
 
 #include "fboss/qsfp_service/test/FakeConfigsHelper.h"
 
 namespace facebook::fboss {
+
 void TransceiverManagerTestHelper::SetUp() {
   setupFakeAgentConfig(agentCfgPath);
   cfg::QsfpServiceConfig qsfpCfg;
@@ -25,6 +27,42 @@ void TransceiverManagerTestHelper::SetUp() {
   fw.versions() = {fwVersionApp, fwVersionDsp};
   fwVersions.versionsMap()[getFakePartNumber()] = fw;
   qsfpCfg.transceiverFirmwareVersions() = fwVersions;
+
+  auto createTransceiverAttributes =
+      [](std::vector<cfg::PortProfileID> supportedPortProfiles) {
+        TransceiverAttributes config;
+        FirmwarePair pair1, pair2;
+        pair1.applicationFirmwareVersion() = "1";
+        pair1.dspFirmwareVersion() = "1";
+        pair2.applicationFirmwareVersion() = "2";
+        pair2.dspFirmwareVersion() = "2";
+
+        config.mediaInterfaceCode() = MediaInterfaceCode::CR4_100G;
+        config.supportedFirmwareVersions() = {pair1, pair2};
+        config.supportedPortProfiles() = supportedPortProfiles;
+
+        return config;
+      };
+
+  VendorConfig fbossVendorOne, fbossVendorTwo;
+  fbossVendorOne.vendorName() = "FBOSSONE";
+  fbossVendorTwo.vendorName() = "FBOSSTWO";
+  TransceiverAttributes partConfigOne = createTransceiverAttributes({
+      cfg::PortProfileID::PROFILE_100G_4_NRZ_NOFEC,
+      cfg::PortProfileID::PROFILE_100G_4_NRZ_CL91_OPTICAL,
+  });
+  TransceiverAttributes partConfigTwo = createTransceiverAttributes({
+      cfg::PortProfileID::PROFILE_100G_4_NRZ_NOFEC,
+  });
+  TransceiverAttributes partConfigThree = createTransceiverAttributes({
+      cfg::PortProfileID::PROFILE_100G_4_NRZ_NOFEC,
+  });
+  fbossVendorOne.partNumberToTransceiverAttributes() = {
+      {"SPTSMP3CLCK10", partConfigOne}, {"LUX426C4AD", partConfigTwo}};
+  fbossVendorTwo.partNumberToTransceiverAttributes() = {
+      {"TR-FC13H-HFZ", partConfigThree}};
+  qsfpCfg.transceiverValidationConfig() = {fbossVendorOne, fbossVendorTwo};
+
   setupFakeQsfpConfig(qsfpCfgPath, qsfpCfg);
 
   // Set up fake qsfp_service volatile directory
@@ -35,6 +73,9 @@ void TransceiverManagerTestHelper::SetUp() {
 
   gflags::SetCommandLineOptionWithMode(
       "qsfp_data_refresh_interval", "0", gflags::SET_FLAGS_DEFAULT);
+
+  gflags::SetCommandLineOptionWithMode(
+      "enable_tcvr_validation", "t", gflags::SET_FLAGS_DEFAULT);
 
   // Create a wedge manager
   transceiverManager_ =
