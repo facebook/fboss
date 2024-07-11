e546b12b905541e32820031550c1a90716c2938f
Ron He <zecheng@meta.com>
2024-07-11 13:31:53 -0700

    Remove migrated HwOverflow tests
    Summary: As titled. No longer needed
    
    Reviewed By: srikrishnagopu
    
    Differential Revision: D59645804
    
    fbshipit-source-id: d741a56445ad6c4d91297cdea2669ec94e71661e

--- cmake/AgentHwTest.cmake
+++ cmake/AgentHwTest.cmake
@@ -236,7 +236,6 @@ set(hw_switch_test_srcs
   fboss/agent/hw/test/dataplane_tests/HwMPLSTests.cpp
   fboss/agent/hw/test/dataplane_tests/HwLoadBalancerTests.cpp
   fboss/agent/hw/test/dataplane_tests/HwMirroringTests.cpp
-  fboss/agent/hw/test/dataplane_tests/HwOverflowTest.cpp
   fboss/agent/hw/test/dataplane_tests/HwTeFlowTrafficTests.cpp
   fboss/agent/hw/test/dataplane_tests/HwTrafficPfcTests.cpp
   fboss/agent/hw/test/dataplane_tests/HwProdInvariantHelper.cpp
