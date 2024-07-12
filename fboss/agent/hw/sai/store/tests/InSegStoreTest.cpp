b8bf2a05a71ee4fd3fc9186134ac28b6bfc2e363
Richard Barnes <rbarnes@meta.com>
2024-07-12 12:36:29 -0700

    Qualify namespace in fboss/agent/hw/sai/store/tests/InSegStoreTest.cpp +1
    Summary:
    Putting `using namespace` in a header file is a [_bad_ anti-pattern](https://stackoverflow.com/questions/1452721/whats-the-problem-with-using-namespace-std) and we have multiple linters and compiler errors to prevent it. But to get global coverage of `-Wheader-hygiene`, we need to draw down existing instances.
    
    This diff removes qualifies the namespaces of entities in one more files. This will later allow us to remove a `using namespace` from a header file. Making these changes at scale is challenging. If you'd prefer to make the change in a different way, please commandeer, edit, and land.
    
    Reviewed By: palmje
    
    Differential Revision: D59682994
    
    fbshipit-source-id: 29ce5d518e48e70a3f0d3d8e8904686d9b0ec229

--- fboss/agent/hw/sai/store/tests/InSegStoreTest.cpp
+++ fboss/agent/hw/sai/store/tests/InSegStoreTest.cpp
@@ -12,15 +12,16 @@ using namespace ::testing;
 
 TEST_F(SaiStoreTest, createInSegEntry) {
   auto& mplsApi = saiApiTable->mplsApi();
-  typename SaiInSegTraits::InSegEntry entry{0, 100};
-  typename SaiInSegTraits::Attributes::NextHopId nextHopIdAttribute(1010);
-  typename SaiInSegTraits::CreateAttributes attributes{
+  typename facebook::fboss::SaiInSegTraits::InSegEntry entry{0, 100};
+  typename facebook::fboss::SaiInSegTraits::Attributes::NextHopId
+      nextHopIdAttribute(1010);
+  typename facebook::fboss::SaiInSegTraits::CreateAttributes attributes{
       SAI_PACKET_ACTION_FORWARD, 1, nextHopIdAttribute};
-  mplsApi.create<SaiInSegTraits>(entry, attributes);
+  mplsApi.create<facebook::fboss::SaiInSegTraits>(entry, attributes);
 
   saiStore->setSwitchId(0);
   saiStore->reload();
-  auto& store = saiStore->get<SaiInSegTraits>();
+  auto& store = saiStore->get<facebook::fboss::SaiInSegTraits>();
 
   auto got = store.get(entry);
   EXPECT_EQ(got->adapterKey(), entry);
@@ -33,8 +34,9 @@ TEST_F(SaiStoreTest, createInSegEntry) {
 
 TEST_F(SaiStoreTest, modifyInSegEntry) {
   auto& mplsApi = saiApiTable->mplsApi();
-  typename SaiInSegTraits::InSegEntry entry{0, 100};
-  typename SaiInSegTraits::Attributes::NextHopId nextHopIdAttribute(1010);
+  typename facebook::fboss::SaiInSegTraits::InSegEntry entry{0, 100};
+  typename facebook::fboss::SaiInSegTraits::Attributes::NextHopId
+      nextHopIdAttribute(1010);
   typename SaiInSegTraits::CreateAttributes attributes{
       SAI_PACKET_ACTION_FORWARD, 1, nextHopIdAttribute};
   mplsApi.create<SaiInSegTraits>(entry, attributes);
