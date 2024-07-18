b1bf8b99cbde14e3b71beb98047ae7da014eb8e6
Justin Kim <jkimdo@meta.com>
2024-07-17 21:25:20 -0700

    Get product version in cached eeprom parser
    Summary: Add a function to get productVersion field from the eeprom contents
    
    Reviewed By: xanabani
    
    Differential Revision: D59569830
    
    fbshipit-source-id: 90f730758925ba5451951d5407435783dbbdaec8

--- fboss/platform/weutil/CachedFbossEepromParser.h
+++ fboss/platform/weutil/CachedFbossEepromParser.h
@@ -34,6 +34,17 @@ class CachedFbossEepromParser {
     return std::nullopt;
   }
 
+  std::optional<int> getProductVersion(
+      const std::string& eepromFilePath,
+      uint16_t offset = 0) {
+    for (const auto& [key, value] : getContents(eepromFilePath, offset)) {
+      if (key == "Product Version") {
+        return std::stoi(value);
+      }
+    }
+    return std::nullopt;
+  }
+
  private:
   std::map<
       std::pair<std::string /* eepromFilePath */, uint16_t /* offset */>,
