e81967a62d7c6a9e765b28e08fcd9d1fffa4989d
Parvez Shaikh <pshaikh@meta.com>
2024-07-19 17:48:09 -0700

    trunk load balancer agent tests
    Summary: sketching a skeleton of these tests based on trunk load balancer hw tests
    
    Reviewed By: msomasundaran
    
    Differential Revision: D59788484
    
    fbshipit-source-id: 37c421eeb7bb1d0a84aa41ffc8ed35b7368cc0b0

--- cmake/AgentTestAgentHwTests.cmake
+++ cmake/AgentTestAgentHwTests.cmake
@@ -43,6 +43,7 @@ add_library(agent_hw_test_src
   fboss/agent/test/agent_hw_tests/AgentMmuTuningTests.cpp
   fboss/agent/test/agent_hw_tests/AgentSflowMirrorTest.cpp
   fboss/agent/test/agent_hw_tests/AgentAclPriorityTests.cpp
+  fboss/agent/test/agent_hw_tests/AgentTrunkLoadBalancerTests.cpp
 )
 
 target_link_libraries(agent_hw_test_src
---
+++ fboss/agent/test/agent_hw_tests/AgentTrunkLoadBalancerTests.cpp
@@ -0,0 +1,462 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#include "fboss/agent/test/AgentHwTest.h"
+
+#include <fboss/agent/RouteUpdateWrapper.h>
+
+#include "fboss/agent/FbossError.h"
+#include "fboss/agent/gen-cpp2/switch_config_types.h"
+#include "fboss/agent/hw/test/ConfigFactory.h"
+#include "fboss/agent/state/StateUtils.h"
+
+#include "fboss/agent/state/LabelForwardingAction.h"
+#include "fboss/agent/test/EcmpSetupHelper.h"
+#include "fboss/agent/test/TrunkUtils.h"
+#include "fboss/agent/test/utils/LoadBalancerTestUtils.h"
+#include "folly/IPAddressV4.h"
+
+#include "fboss/agent/state/SwitchState.h"
+
+#include <boost/container/flat_set.hpp>
+
+#include <vector>
+
+using boost::container::flat_set;
+using std::vector;
+
+using facebook::fboss::utility::getEcmpFullTrunkHalfHashConfig;
+using facebook::fboss::utility::getEcmpHalfTrunkFullHashConfig;
+
+namespace {
+struct AggPortInfo {
+  uint numPhysicalPorts() const {
+    return numAggPorts * aggPortWidth;
+  }
+  uint numAggPorts{4};
+  uint aggPortWidth{3};
+};
+
+enum TrafficType {
+  IPv4,
+  IPv6,
+  IPv4MPLS,
+  IPv6MPLS,
+  v4MPLS4Swap,
+  v4MPLS4Php,
+  v6MPLS4Swap,
+  v6MPLS4Php
+};
+
+static constexpr uint32_t kV4TopSwap = 10010;
+static constexpr uint32_t kV6TopSwap = 10011;
+static constexpr uint32_t kV4TopPhp = 20010;
+static constexpr uint32_t kV6TopPhp = 20011;
+
+} // namespace
+
+namespace facebook::fboss {
+class AgentTrunkLoadBalancerTest : public AgentHwTest {
+ private:
+  std::vector<production_features::ProductionFeature>
+  getProductionFeaturesVerified() const override {
+    return {
+        production_features::ProductionFeature::LAG,
+        production_features::ProductionFeature::LAG_LOAD_BALANCER};
+  }
+
+  void addAggregatePorts(cfg::SwitchConfig* config, AggPortInfo aggInfo) const {
+    AggregatePortID curAggId{1};
+    for (auto i = 0; i < aggInfo.numAggPorts; ++i) {
+      std::vector<int32_t> members(aggInfo.aggPortWidth);
+      for (auto j = 0; j < aggInfo.aggPortWidth; ++j) {
+        members[j] = masterLogicalPortIds()[i * aggInfo.aggPortWidth + j];
+      }
+      utility::addAggPort(curAggId++, members, config);
+    }
+  }
+  std::vector<PortDescriptor> getPhysicalPorts(AggPortInfo aggInfo) const {
+    std::vector<PortDescriptor> physicalPorts;
+    for (auto i = 0; i < aggInfo.numPhysicalPorts(); ++i) {
+      physicalPorts.push_back(
+          PortDescriptor(PortID(masterLogicalPortIds()[i])));
+    }
+    return physicalPorts;
+  }
+  flat_set<PortDescriptor> getAggregatePorts(AggPortInfo aggInfo) const {
+    flat_set<PortDescriptor> aggregatePorts;
+    for (auto i = 0; i < aggInfo.numAggPorts; ++i) {
+      aggregatePorts.insert(PortDescriptor(AggregatePortID(i + 1)));
+    }
+    return aggregatePorts;
+  }
+
+  void applyConfigAndEnableTrunks(const cfg::SwitchConfig& config) {
+    applyNewConfig(config);
+    applyNewState(
+        [](const std::shared_ptr<SwitchState> state) {
+          return utility::enableTrunkPorts(state);
+        },
+        "enable trunk ports");
+  }
+
+  template <typename ECMP_HELPER>
+  void programRoutes(const ECMP_HELPER& ecmpHelper, const AggPortInfo aggInfo) {
+    applyNewState([&](const std::shared_ptr<SwitchState>& in) {
+      auto out = in->clone();
+      return ecmpHelper.resolveNextHops(out, getAggregatePorts(aggInfo));
+    });
+    ecmpHelper.programRoutes(
+        std::make_unique<SwSwitchRouteUpdateWrapper>(
+            getSw(), getSw()->getRib()),
+        getAggregatePorts(aggInfo));
+  }
+
+  template <typename ECMP_HELPER>
+  void programIp2MplsRoutes(
+      const ECMP_HELPER& ecmpHelper,
+      const AggPortInfo aggInfo) {
+    std::map<PortDescriptor, LabelForwardingAction::LabelStack> stacks{};
+
+    for (auto i = 0; i < aggInfo.numAggPorts; ++i) {
+      stacks.emplace(
+          PortDescriptor(AggregatePortID(i + 1)),
+          LabelForwardingAction::LabelStack{10010 + i + 1});
+    }
+
+    applyNewState([&](const std::shared_ptr<SwitchState>& in) {
+      auto out = in->clone();
+      return ecmpHelper.resolveNextHops(out, getAggregatePorts(aggInfo));
+    });
+    ecmpHelper.programIp2MplsRoutes(
+        std::make_unique<SwSwitchRouteUpdateWrapper>(
+            getSw(), getSw()->getRib()),
+        getAggregatePorts(aggInfo),
+        stacks);
+  }
+
+  template <typename ECMP_HELPER>
+  void programMplsRoutes(
+      const ECMP_HELPER& ecmpHelper,
+      const AggPortInfo aggInfo) {
+    applyNewState([&](const std::shared_ptr<SwitchState>& in) {
+      auto out = in->clone();
+      return ecmpHelper.resolveNextHops(out, getAggregatePorts(aggInfo));
+    });
+    auto ports = getAggregatePorts(aggInfo);
+    ecmpHelper.setupECMPForwarding(
+        getProgrammedState(),
+        std::make_unique<SwSwitchRouteUpdateWrapper>(
+            getSw(), getSw()->getRib()),
+        ports);
+  }
+
+ protected:
+  void
+  pumpIPTraffic(bool isV6, bool loopThroughFrontPanel, AggPortInfo aggInfo) {
+    std::optional<PortID> frontPanelPortToLoopTraffic;
+    if (loopThroughFrontPanel) {
+      // Next port to loop back traffic through
+      frontPanelPortToLoopTraffic =
+          PortID(masterLogicalPortIds()[aggInfo.numPhysicalPorts()]);
+    }
+    auto firstVlanID = utility::firstVlanID(getProgrammedState());
+    auto mac = utility::getFirstInterfaceMac(getProgrammedState());
+
+    utility::pumpTraffic(
+        isV6,
+        utility::getAllocatePktFn(getAgentEnsemble()),
+        utility::getSendPktFunc(getAgentEnsemble()),
+        mac,
+        firstVlanID,
+        frontPanelPortToLoopTraffic);
+  }
+
+  void pumpMPLSTraffic(
+      bool isV6,
+      uint32_t label,
+      bool loopThroughFrontPanel,
+      AggPortInfo aggInfo) {
+    std::optional<PortID> frontPanelPortToLoopTraffic;
+    if (loopThroughFrontPanel) {
+      // Next port to loop back traffic through
+      frontPanelPortToLoopTraffic =
+          PortID(masterLogicalPortIds()[aggInfo.numPhysicalPorts()]);
+    }
+    auto firstVlanID = utility::firstVlanID(getProgrammedState());
+    auto mac = utility::getFirstInterfaceMac(getProgrammedState());
+    utility::pumpMplsTraffic(
+        isV6,
+        utility::getAllocatePktFn(getAgentEnsemble()),
+        utility::getSendPktFunc(getAgentEnsemble()),
+        label,
+        mac,
+        firstVlanID.value(),
+        frontPanelPortToLoopTraffic);
+  }
+
+  cfg::SwitchConfig configureAggregatePorts(AggPortInfo aggInfo) {
+    auto config = initialConfig(*getAgentEnsemble());
+    addAggregatePorts(&config, aggInfo);
+    return config;
+  }
+
+  void setupIPECMP(AggPortInfo aggInfo) {
+    utility::EcmpSetupTargetedPorts6 ecmpHelper6{getProgrammedState()};
+    utility::EcmpSetupTargetedPorts4 ecmpHelper4{getProgrammedState()};
+    programRoutes(ecmpHelper6, aggInfo);
+    programRoutes(ecmpHelper4, aggInfo);
+  }
+
+  void setupIP2MPLSECMP(AggPortInfo aggInfo) {
+    utility::EcmpSetupTargetedPorts6 ecmpHelper6{getProgrammedState()};
+    utility::EcmpSetupTargetedPorts4 ecmpHelper4{getProgrammedState()};
+    programIp2MplsRoutes(ecmpHelper6, aggInfo);
+    programIp2MplsRoutes(ecmpHelper4, aggInfo);
+  }
+
+  void setupMPLSECMP(AggPortInfo aggInfo) {
+    utility::MplsEcmpSetupTargetedPorts<folly::IPAddressV4> v4SwapHelper{
+        getProgrammedState(),
+        kV4TopSwap,
+        LabelForwardingAction::LabelForwardingType::SWAP};
+    utility::MplsEcmpSetupTargetedPorts<folly::IPAddressV6> v6SwapHelper{
+        getProgrammedState(),
+        kV6TopSwap,
+        LabelForwardingAction::LabelForwardingType::SWAP};
+    utility::MplsEcmpSetupTargetedPorts<folly::IPAddressV4> v4PhpHelper{
+        getProgrammedState(),
+        kV4TopPhp,
+        LabelForwardingAction::LabelForwardingType::PHP};
+    utility::MplsEcmpSetupTargetedPorts<folly::IPAddressV4> v6PhpHelper{
+        getProgrammedState(),
+        kV6TopPhp,
+        LabelForwardingAction::LabelForwardingType::PHP};
+    programMplsRoutes(v4SwapHelper, aggInfo);
+    programMplsRoutes(v6SwapHelper, aggInfo);
+    programMplsRoutes(v4PhpHelper, aggInfo);
+    programMplsRoutes(v6PhpHelper, aggInfo);
+  }
+
+  void clearPortStats(AggPortInfo aggInfo) {
+    auto portDescs = getPhysicalPorts(aggInfo);
+    auto ports = std::make_unique<std::vector<int32_t>>();
+    for (auto portDesc : portDescs) {
+      ports->push_back(static_cast<int32_t>(portDesc.phyPortID()));
+    }
+    getSw()->clearPortStats(ports);
+  }
+
+  void pumpIPTrafficAndVerifyLoadBalanced(
+      bool isV6,
+      bool loopThroughFrontPanel,
+      AggPortInfo aggInfo,
+      int deviation) {
+    utility::pumpTrafficAndVerifyLoadBalanced(
+        [=, this]() { pumpIPTraffic(isV6, loopThroughFrontPanel, aggInfo); },
+        [=, this]() { clearPortStats(aggInfo); },
+        [=, this]() {
+          return utility::isLoadBalanced<PortID, HwPortStats>(
+              getPhysicalPorts(aggInfo),
+              std::vector<NextHopWeight>(),
+              [=, this](const std::vector<PortID>& portIds) {
+                return this->getLatestPortStats(portIds);
+              },
+              deviation);
+        });
+  }
+
+  void pumpMPLSTrafficAndVerifyLoadBalanced(
+      bool isV6,
+      uint32_t labelV4,
+      uint32_t labelV6,
+      bool loopThroughFrontPanel,
+      AggPortInfo aggInfo,
+      int deviation) {
+    utility::pumpTrafficAndVerifyLoadBalanced(
+        [=, this]() {
+          if (isV6) {
+            pumpMPLSTraffic(isV6, labelV6, loopThroughFrontPanel, aggInfo);
+          } else {
+            pumpMPLSTraffic(isV6, labelV4, loopThroughFrontPanel, aggInfo);
+          }
+        },
+        [=, this]() { clearPortStats(aggInfo); },
+        [=, this]() {
+          return utility::isLoadBalanced<PortID, HwPortStats>(
+              getPhysicalPorts(aggInfo),
+              std::vector<NextHopWeight>(),
+              [=, this](const std::vector<PortID>& portIds) {
+                return this->getLatestPortStats(portIds);
+              },
+              deviation);
+        });
+  }
+
+  void runIPLoadBalanceTest(
+      bool isV6,
+      const std::vector<cfg::LoadBalancer>& loadBalancers,
+      AggPortInfo aggInfo,
+      bool loopThroughFrontPanel,
+      int deviation) {
+    auto setup = [=, this]() {
+      auto config = configureAggregatePorts(aggInfo);
+      applyConfigAndEnableTrunks(config);
+      setupIPECMP(aggInfo);
+
+      applyNewState([&](const std::shared_ptr<SwitchState>& inputState) {
+        return utility::addLoadBalancers(
+            getAgentEnsemble(), inputState, loadBalancers, scopeResolver());
+      });
+    };
+    auto verify = [=, this]() {
+      pumpIPTrafficAndVerifyLoadBalanced(
+          isV6, loopThroughFrontPanel, aggInfo, deviation);
+    };
+    verifyAcrossWarmBoots(setup, verify);
+  }
+
+  void runIP2MplsLoadBalanceTest(
+      bool isV6,
+      const std::vector<cfg::LoadBalancer>& loadBalancers,
+      AggPortInfo aggInfo,
+      bool loopThroughFrontPanel,
+      int deviation) {
+    auto setup = [=, this]() {
+      auto config = configureAggregatePorts(aggInfo);
+      applyConfigAndEnableTrunks(config);
+      setupIP2MPLSECMP(aggInfo);
+
+      applyNewState([&](const std::shared_ptr<SwitchState>& inputState) {
+        return utility::addLoadBalancers(
+            getAgentEnsemble(), inputState, loadBalancers, scopeResolver());
+      });
+    };
+    auto verify = [=, this]() {
+      pumpIPTrafficAndVerifyLoadBalanced(
+          isV6, loopThroughFrontPanel, aggInfo, deviation);
+    };
+    verifyAcrossWarmBoots(setup, verify);
+  }
+
+  void runMpls2MplsLoadBalanceTest(
+      bool isV6,
+      const std::vector<cfg::LoadBalancer>& loadBalancers,
+      LabelForwardingAction::LabelForwardingType type,
+      AggPortInfo aggInfo,
+      bool loopThroughFrontPanel,
+      int deviation) {
+    uint32_t labelV4 =
+        (type == LabelForwardingAction::LabelForwardingType::SWAP ? kV4TopSwap
+                                                                  : kV4TopPhp);
+    uint32_t labelV6 =
+        (type == LabelForwardingAction::LabelForwardingType::SWAP ? kV6TopSwap
+                                                                  : kV6TopPhp);
+    auto setup = [=, this]() {
+      auto config = configureAggregatePorts(aggInfo);
+      applyConfigAndEnableTrunks(config);
+      setupMPLSECMP(aggInfo);
+
+      applyNewState([&](const std::shared_ptr<SwitchState>& inputState) {
+        return utility::addLoadBalancers(
+            getAgentEnsemble(), inputState, loadBalancers, scopeResolver());
+      });
+    };
+    auto verify = [=, this]() {
+      pumpMPLSTrafficAndVerifyLoadBalanced(
+          isV6, labelV4, labelV6, loopThroughFrontPanel, aggInfo, 25);
+    };
+    verifyAcrossWarmBoots(setup, verify);
+  }
+
+  void runMpls2MplsLoadBalanceTest(
+      bool isV6,
+      const std::vector<cfg::LoadBalancer>& loadBalancers,
+      LabelForwardingAction::LabelForwardingType type,
+      AggPortInfo aggInfo,
+      bool loopThroughFrontPanel) {
+    uint32_t labelV4 =
+        (type == LabelForwardingAction::LabelForwardingType::SWAP ? kV4TopSwap
+                                                                  : kV4TopPhp);
+    uint32_t labelV6 =
+        (type == LabelForwardingAction::LabelForwardingType::SWAP ? kV6TopSwap
+                                                                  : kV6TopPhp);
+    auto setup = [=, this]() {
+      auto config = configureAggregatePorts(aggInfo);
+      applyNewConfig(config);
+      setupMPLSECMP(aggInfo);
+      applyNewState([&](const std::shared_ptr<SwitchState>& inputState) {
+        return utility::addLoadBalancers(
+            getAgentEnsemble(), inputState, loadBalancers, scopeResolver());
+      });
+    };
+    auto verify = [=, this]() {
+      pumpMPLSTrafficAndVerifyLoadBalanced(
+          isV6, labelV4, labelV6, loopThroughFrontPanel, aggInfo, 25);
+    };
+    verifyAcrossWarmBoots(setup, verify);
+  }
+
+  // @lint-ignore-every CLANGTIDY
+  void runLoadBalanceTest(
+      TrafficType traffic,
+      const std::vector<cfg::LoadBalancer>& loadBalancers,
+      AggPortInfo aggInfo,
+      bool loopThroughFrontPanel = false,
+      int deviation = 25) {
+    switch (traffic) {
+      case TrafficType::IPv4:
+        return runIPLoadBalanceTest(
+            false, loadBalancers, aggInfo, loopThroughFrontPanel, deviation);
+      case TrafficType::IPv6:
+        return runIPLoadBalanceTest(
+            true, loadBalancers, aggInfo, loopThroughFrontPanel, deviation);
+      case TrafficType::IPv4MPLS:
+        return runIP2MplsLoadBalanceTest(
+            false, loadBalancers, aggInfo, loopThroughFrontPanel, deviation);
+      case TrafficType::IPv6MPLS:
+        return runIP2MplsLoadBalanceTest(
+            true, loadBalancers, aggInfo, loopThroughFrontPanel, deviation);
+      case TrafficType::v4MPLS4Swap:
+        return runMpls2MplsLoadBalanceTest(
+            false,
+            loadBalancers,
+            LabelForwardingAction::LabelForwardingType::SWAP,
+            aggInfo,
+            loopThroughFrontPanel,
+            deviation);
+      case TrafficType::v6MPLS4Swap:
+        return runMpls2MplsLoadBalanceTest(
+            true,
+            loadBalancers,
+            LabelForwardingAction::LabelForwardingType::SWAP,
+            aggInfo,
+            loopThroughFrontPanel,
+            deviation);
+      case TrafficType::v4MPLS4Php:
+        return runMpls2MplsLoadBalanceTest(
+            false,
+            loadBalancers,
+            LabelForwardingAction::LabelForwardingType::PHP,
+            aggInfo,
+            loopThroughFrontPanel,
+            deviation);
+      case TrafficType::v6MPLS4Php:
+        return runMpls2MplsLoadBalanceTest(
+            true,
+            loadBalancers,
+            LabelForwardingAction::LabelForwardingType::PHP,
+            aggInfo,
+            loopThroughFrontPanel,
+            deviation);
+    }
+  }
+};
+} // namespace facebook::fboss
