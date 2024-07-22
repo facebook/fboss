293daab39f12d05614d3e930f3d5e32a146af474
Srikrishna Gopu <srikrishnagopu@meta.com>
2024-07-22 16:35:30 -0700

    kill hw mirroring tests
    Summary: As titled, kill hw mirroring tests
    
    Reviewed By: nivinl
    
    Differential Revision: D60074530
    
    fbshipit-source-id: de8344f7c8e7d2c0cc3b6b4eb6b900ceb4af8436

--- cmake/AgentHwTest.cmake
+++ cmake/AgentHwTest.cmake
@@ -233,7 +233,6 @@ set(hw_switch_test_srcs
   fboss/agent/hw/test/dataplane_tests/HwIpInIpTunnelTests.cpp
   fboss/agent/hw/test/dataplane_tests/HwMPLSTests.cpp
   fboss/agent/hw/test/dataplane_tests/HwLoadBalancerTests.cpp
-  fboss/agent/hw/test/dataplane_tests/HwMirroringTests.cpp
   fboss/agent/hw/test/dataplane_tests/HwTeFlowTrafficTests.cpp
   fboss/agent/hw/test/dataplane_tests/HwTrafficPfcTests.cpp
   fboss/agent/hw/test/dataplane_tests/HwProdInvariantHelper.cpp
