7e92229d3a39eea76b1adc23f5a8fbc83676c32f
Jeff Kim <jeffkim8482@meta.com>
2024-07-23 20:09:47 -0700

    Keep NDPTest.PendingNdp Disabled for Intf Nbr Tables
    Summary: Leaving NDPTest.PendingNdp disabled for intf nbr tables because Agent currently does not store pending nbr entries in interface objects of switch state.
    
    Reviewed By: jasmeetbagga
    
    Differential Revision: D60084220
    
    fbshipit-source-id: baa5774f06401b20ed61dd14205235e546d21bf0

--- fboss/agent/test/NDPTest.cpp
+++ fboss/agent/test/NDPTest.cpp
@@ -1165,9 +1165,9 @@ TYPED_TEST(NdpTest, FlushOnAggPortTransition) {
 }
 
 TYPED_TEST(NdpTest, PendingNdp) {
-  /*
-   * TODO(skhare) Fix this test for Interface neighbor tables, and then enable.
-   */
+  // Keep test disabled for intf nbr tables because pending neighbor entries are
+  // currently not stored in intfs.
+  // TODO(jeffkim8482) Remove test once intf nbr migration is complete
   if (this->isIntfNbrTable()) {
 #if defined(GTEST_SKIP)
     GTEST_SKIP();
