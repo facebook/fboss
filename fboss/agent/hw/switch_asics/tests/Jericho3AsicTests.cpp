e63d6de0d9559404573e5c82c13d050ac0b4724a
Jasmeet Bagga <jasmeetbagga@meta.com>
2024-07-25 00:54:27 -0700

    Start adding switch_asic UTs
    Summary: Starting with J3 port group tests
    
    Reviewed By: nivinl
    
    Differential Revision: D60210667
    
    fbshipit-source-id: a15f8fdc851713f66ca86e5a139caccb4345c7fe

---
+++ fboss/agent/hw/switch_asics/tests/Jericho3AsicTests.cpp
@@ -0,0 +1,38 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#include "fboss/agent/gen-cpp2/switch_config_types.h"
+#include "fboss/agent/hw/switch_asics/Jericho3Asic.h"
+
+#include <gtest/gtest.h>
+#include <memory>
+
+using namespace facebook::fboss;
+
+class Jericho3AsicTest : public ::testing::Test {
+ public:
+  void SetUp() override {
+    cfg::Range64 sysPortRange;
+    sysPortRange.minimum() = 100;
+    sysPortRange.maximum() = 144;
+    asic_ = std::make_unique<Jericho3Asic>(
+        cfg::SwitchType::VOQ,
+        0,
+        0,
+        sysPortRange,
+        folly::MacAddress("02:00:00:00:0F:0B"));
+  }
+  std::unique_ptr<Jericho3Asic> asic_;
+};
+
+TEST_F(Jericho3AsicTest, checkPortGroups) {
+  auto portGroups = asic_->getPortGroups();
+  EXPECT_EQ(portGroups.size(), 4);
+}
