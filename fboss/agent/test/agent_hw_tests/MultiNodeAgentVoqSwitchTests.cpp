2bf554e27579dcd6a8a9f1415169e3b3d0837945
Shrikrishna (Shri) Khare <skhare@meta.com>
2024-07-31 17:02:13 -0700

    DSF MultiNode Test: get desired loopbackmode for asic used
    Summary: As titled.
    
    Reviewed By: jasmeetbagga
    
    Differential Revision: D59834081
    
    fbshipit-source-id: 06f9f52b6abc3a3f81834b93da06f10dae5cc91d

--- fboss/agent/test/agent_hw_tests/MultiNodeAgentVoqSwitchTests.cpp
+++ fboss/agent/test/agent_hw_tests/MultiNodeAgentVoqSwitchTests.cpp
@@ -1,6 +1,7 @@
 // (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
 
 #include "fboss/agent/test/AgentHwTest.h"
+#include "fboss/agent/test/utils/AsicUtils.h"
 
 DECLARE_bool(disable_neighbor_updates);
 DECLARE_bool(disable_looped_fabric_ports);
@@ -14,12 +15,19 @@ class MultiNodeAgentVoqSwitchTest : public AgentHwTest {
       const AgentEnsemble& ensemble) const override {
     XLOG(DBG0) << "initialConfig() loaded config from file " << FLAGS_config;
 
+    auto hwAsics = ensemble.getSw()->getHwAsicTable()->getL3Asics();
+    auto asic = utility::checkSameAndGetAsic(hwAsics);
+
+    auto it = asic->desiredLoopbackModes().find(cfg::PortType::INTERFACE_PORT);
+    CHECK(it != asic->desiredLoopbackModes().end());
+    auto desiredLoopbackModeForInterfacePort = it->second;
+
     auto agentConfig = AgentConfig::fromFile(FLAGS_config);
     auto config = *agentConfig->thrift.sw();
     for (auto& port : *config.ports()) {
       if (port.portType() == cfg::PortType::INTERFACE_PORT) {
         // TODO determine loopback mode based on ASIC type
-        port.loopbackMode() = cfg::PortLoopbackMode::PHY;
+        port.loopbackMode() = desiredLoopbackModeForInterfacePort;
       }
     }
 
