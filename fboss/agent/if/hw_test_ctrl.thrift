49b05665c46a0d700410f839adcd1f58f899daa2
Parvez Shaikh <pshaikh@meta.com>
2024-08-06 13:56:35 -0700

    hw_test_ctrl.thrift
    Summary: as titled
    
    Reviewed By: msomasundaran
    
    Differential Revision:
    D60611624
    
    Privacy Context Container: L1125642
    
    fbshipit-source-id: c7bc248ed3571cd1f0a99716839c733ce526240d

---
+++ fboss/agent/if/hw_test_ctrl.thrift
@@ -0,0 +1,37 @@
+namespace cpp2 facebook.fboss.utility
+namespace go neteng.fboss.hw_test_ctrl
+namespace php fboss
+namespace py neteng.fboss.hw_test_ctrl
+namespace py3 neteng.fboss
+namespace py.asyncio neteng.fboss.asyncio.hw_test_ctrl
+
+include "fboss/agent/switch_state.thrift"
+include "fboss/agent/switch_config.thrift"
+
+service FbossHwTestCtrl {
+  // acl utils begin
+  i32 getDefaultAclTableNumAclEntries();
+
+  i32 getAclTableNumAclEntries(1: string name);
+
+  bool isDefaultAclTableEnabled();
+
+  bool isAclTableEnabled(1: string name);
+
+  bool isAclEntrySame(1: switch_state.AclEntryFields aclEntry);
+
+  bool areAllAclEntriesEnabled();
+
+  bool isStatProgrammedInDefaultAclTable(
+    1: list<string> aclEntryNames,
+    2: string counterName,
+    3: list<switch_config.CounterType> types,
+  );
+
+  bool isStatProgrammedInAclTable(
+    1: list<string> aclEntryNames,
+    2: string counterName,
+    3: list<switch_config.CounterType> types,
+    4: string tableName,
+  );
+}
