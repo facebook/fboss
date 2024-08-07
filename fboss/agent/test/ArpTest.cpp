4e64bbd113a3048b0c7663426ad106b8649f8a21
Jeff Kim <jeffkim8482@meta.com>
2024-08-07 10:30:16 -0700

    Keep ArpTest.receivedPacketWithDirectlyConnectedDestination Disabled for Intf Nbr Tables
    Summary: Leaving rpTest.receivedPacketWithDirectlyConnectedDestination disabled for intf nbr tables because Agent currently does not store pending nbr entries in interface objects of switch state.
    
    Differential Revision: D60850289
    
    fbshipit-source-id: 95a0ce0a4e657af1188759b1cddec3b170739ffa

--- fboss/agent/test/ArpTest.cpp
+++ fboss/agent/test/ArpTest.cpp
@@ -1346,9 +1346,9 @@ TYPED_TEST(ArpTest, PortFlapRecover) {
 }
 
 TYPED_TEST(ArpTest, receivedPacketWithDirectlyConnectedDestination) {
-  /*
-   * TODO(skhare) Fix this test for Interface neighbor tables, and then enable.
-   */
+  // Keep test disabled for intf nbr tables because pending neighbor entries are
+  // currently not stored in intfs.
+  // TODO(jeffkim8482) Remove test once intf nbr migration is complete
   if (this->isIntfNbrTable()) {
 #if defined(GTEST_SKIP)
     GTEST_SKIP();
