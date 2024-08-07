55955c4bde5b5ea516eba0aa12f61fc0b260188e
Jeff Kim <jeffkim8482@meta.com>
2024-08-07 10:30:16 -0700

    Keep ArpTest.BasicSendRequest Disabled for Intf Nbr Tables
    Summary: Leaving ArpTest.BasicSendRequest disabled for intf nbr tables because Agent currently does not store pending nbr entries in interface objects of switch state.
    
    Differential Revision: D60850222
    
    fbshipit-source-id: 5bda87e8aad16873a639d6aad85d416475709bc6

--- fboss/agent/test/ArpTest.cpp
+++ fboss/agent/test/ArpTest.cpp
@@ -318,9 +318,9 @@ class ArpTest : public ::testing::Test {
 TYPED_TEST_SUITE(ArpTest, NbrTableTypes);
 
 TYPED_TEST(ArpTest, BasicSendRequest) {
-  /*
-   * TODO(skhare) Fix this test for Interface neighbor tables, and then enable.
-   */
+  // Keep test disabled for intf nbr tables because pending neighbor entries are
+  // currently not stored in intfs.
+  // TODO(jeffkim8482) Remove test once intf nbr migration is complete
   if (this->isIntfNbrTable()) {
 #if defined(GTEST_SKIP)
     GTEST_SKIP();
