e64d219b861725fc477c8e27f88661e0d3036f9d
Jeff Kim <jeffkim8482@meta.com>
2024-08-07 10:30:16 -0700

    Keep ArpTest.ArpTableSerialization Disabled for Intf Nbr Tables
    Summary: Leaving ArpTest.ArpTableSerialization disabled for intf nbr tables because Agent currently does not store pending nbr entries in interface objects of switch state.
    
    Differential Revision: D60850252
    
    fbshipit-source-id: 43a56949d5cd0aae4c09139f4903bba259089ccf

--- fboss/agent/test/ArpTest.cpp
+++ fboss/agent/test/ArpTest.cpp
@@ -1073,9 +1073,9 @@ TYPED_TEST(ArpTest, PendingArpCleanup) {
 }
 
 TYPED_TEST(ArpTest, ArpTableSerialization) {
-  /*
-   * TODO(skhare) Fix this test for Interface neighbor tables, and then enable.
-   */
+  // Keep test disabled for intf nbr tables because pending neighbor entries are
+  // currently not stored in intfs.
+  // TODO(jeffkim8482) Remove test once intf nbr migration is complete
   if (this->isIntfNbrTable()) {
 #if defined(GTEST_SKIP)
     GTEST_SKIP();
