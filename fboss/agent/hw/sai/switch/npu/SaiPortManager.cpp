7bb5e9f90408bfac2e048e62f3861135804d2c37
Wei Dai <daiweix@meta.com>
2024-08-23 11:19:01 -0700

    toggle port admin state only when port is enabled
    Summary: SaiPortAdminStateTest.assertAdminState is now failing on TH5 because port is always up by the logics to toggle admin state during coldboot init. This diff resolves this issue by trigger port admin state toggle only when port is enabled
    
    Reviewed By: nivinl
    
    Differential Revision:
    D61718093
    
    Privacy Context Container: L1125642
    
    fbshipit-source-id: 421ad7632d919972afb58a9686b78f6300612ea9

--- fboss/agent/hw/sai/switch/npu/SaiPortManager.cpp
+++ fboss/agent/hw/sai/switch/npu/SaiPortManager.cpp
@@ -927,9 +927,11 @@ void SaiPortManager::programSerdes(
   // create if serdes doesn't exist or update existing serdes
   portHandle->serdes = store.setObject(serdesKey, serdesAttributes);
 
-  if (platform_->getAsic()->getAsicType() == cfg::AsicType::ASIC_TYPE_GARONNE ||
-      platform_->getAsic()->getAsicType() ==
-          cfg::AsicType::ASIC_TYPE_TOMAHAWK5) {
+  if ((platform_->getAsic()->getAsicType() ==
+           cfg::AsicType::ASIC_TYPE_GARONNE ||
+       platform_->getAsic()->getAsicType() ==
+           cfg::AsicType::ASIC_TYPE_TOMAHAWK5) &&
+      swPort->getAdminState() == cfg::PortState::ENABLED) {
     /*
      * SI settings are not programmed to the hardware when the port is
      * created with admin UP. We need to explicitly toggle the admin
