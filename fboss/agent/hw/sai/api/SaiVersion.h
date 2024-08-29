6556988b8bed84997746245524f10e6ee24cc5d2
Nadiia Stetskovych <nstetsko@cisco.com>
2024-08-29 19:57:16 +0300

    P4WB only for Tajo Ebro
    Signed-off-by: Nadiia Stetskovych <nstetsko@cisco.com>

--- fboss/agent/hw/sai/api/SaiVersion.h
+++ fboss/agent/hw/sai/api/SaiVersion.h
@@ -110,15 +110,15 @@
  *   - TAJO_SDK_GTE_24_4_90: Flags for SDK >= 24.4.90
  *   - TAJO_SDK: Flags for all TAJO SDK
  */
-#if defined(TAJO_SDK_VERSION_24_4_90) || defined(TAJO_SDK_VERSION_24_6_1) || \
-    defined(TAJO_SDK_VERSION_24_7_0)
-#define TAJO_P4_WB_SDK
-#endif
-
 #if defined(TAJO_SDK_VERSION_1_42_8) || defined(TAJO_SDK_VERSION_24_4_90) || \
     defined(TAJO_SDK_VERSION_24_6_1) || defined(TAJO_SDK_VERSION_24_7_0)
 #define TAJO_SDK_EBRO
 #endif
+
+#if defined(TAJO_SDK_EBRO)
+#define TAJO_P4_WB_SDK
+#endif
+
 
 #if defined(TAJO_SDK_VERSION_24_4_90) || defined(TAJO_SDK_VERSION_24_6_1) || \
     defined(TAJO_SDK_VERSION_24_7_0)
