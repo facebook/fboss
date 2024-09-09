4e9155bbc2172a34b957d9f468c37cd55830dfe1
Mohammed Al-Sanabani <sanabani@meta.com>
2024-09-09 14:56:06 -0700

    Fix bug in json generation
    Summary: Applicable to BMC Lite Units.
    
    Reviewed By: alandau
    
    Differential Revision: D60961575
    
    fbshipit-source-id: 5a8ea6c72967e3c4dd1bfc1697240c0914098cb5

--- fboss/platform/weutil/WeutilImpl.cpp
+++ fboss/platform/weutil/WeutilImpl.cpp
@@ -29,33 +29,19 @@ void WeutilImpl::printInfo() {
 
 void WeutilImpl::printInfoJson() {
   auto info = getContents();
-
-  int cursor = 0;
-  std::vector<std::string> items;
-  for (auto [key, value] : info) {
-    // CRC16 is not needed in JSON output
+  folly::dynamic eepromObject = folly::dynamic::object;
+  for (const auto& [key, value] : parser_.getContents()) {
     if (key == "CRC16") {
       continue;
     }
-    items.push_back(fmt::format("\"{}\": \"{}\"", key, value));
-  }
-  int vectorSize = items.size();
-  // Manually create JSON object without using folly, so that this code
-  // will be ported to BMC later
-  // Print the first part of the JSON - fixed entry to make a JSON
-  std::cout << "{";
-  std::cout << "\"Information\": {";
-  // Print the second part of the JSON, the dynamic entry
-  for (auto& item : items) {
-    std::cout << item;
-    if (cursor++ != vectorSize - 1) {
-      std::cout << ", ";
-    }
+    eepromObject[key] = value;
   }
 
-  // Finally, print the third part of the JSON - fixed entry
-  std::cout << "}, \"Actions\": [], \"Resources\": []";
-  std::cout << "}" << std::endl;
+  folly::dynamic json = folly::dynamic::object;
+  json["Information"] = eepromObject;
+  json["Actions"] = folly::dynamic::array();
+  json["Resources"] = folly::dynamic::array();
+  std::cout << folly::toPrettyJson(json) << std::endl;
 }
 
 } // namespace facebook::fboss::platform
