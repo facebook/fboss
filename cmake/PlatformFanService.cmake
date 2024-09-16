e499bbceb08550c4e86f1b9299a922ef425d5cd0
Jimmy Ye <jimmyy@meta.com>
2024-09-16 10:07:10 -0700

    Use PlatformNameLib in fan_service
    Reviewed By: Scott8440
    
    Differential Revision: D62729538
    
    fbshipit-source-id: d568ce19eb7ee62de38aeb67446ced66edd04a53

--- cmake/PlatformFanService.cmake
+++ cmake/PlatformFanService.cmake
@@ -38,6 +38,7 @@ target_link_libraries(fan_service_lib
   product_info
   common_file_utils
   platform_config_lib
+  platform_name_lib
   platform_utils
   fan_service_config_types_cpp2
   sensor_service_cpp2
