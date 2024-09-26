99a02e54d35110d957f6b53452edf3a42fa743fd
Priyank Warkhede <priyankw@meta.com>
2024-09-25 18:02:51 -0700

    Introduce ThriftHybridNode to wrap raw thrift
    Summary: Introduce ThriftHybridNode to wrap raw thrift object. In next diffs we will use this to construct Thrift COW trees that embed raw thrift objects based on Thrift annotations.
    
    Reviewed By: wilsonwinhi
    
    Differential Revision:
    D62742461
    
    Privacy Context Container: L1125642
    
    fbshipit-source-id: 95fb625bcb3aa22d84a1d83468bf76fc14cc5ab7

--- fboss/thrift_cow/nodes/Types.h
+++ fboss/thrift_cow/nodes/Types.h
@@ -97,6 +97,9 @@ struct FieldsType {};
 template <typename TType, typename Derived>
 struct ThriftStructFields;
 
+template <typename TypeClass, typename TType>
+struct ThriftHybridNode;
+
 template <typename TType, typename>
 class ThriftStructNode;
 
@@ -132,6 +135,7 @@ class ThriftPrimitiveNode;
 // clang-format off
 #include "fboss/thrift_cow/nodes/ThriftPrimitiveNode-inl.h"
 #include "fboss/thrift_cow/nodes/Traits.h"
+#include "fboss/thrift_cow/nodes/ThriftHybridNode-inl.h"
 #include "fboss/thrift_cow/nodes/ThriftStructNode-inl.h"
 #include "fboss/thrift_cow/nodes/ThriftListNode-inl.h"
 #include "fboss/thrift_cow/nodes/ThriftMapNode-inl.h"
