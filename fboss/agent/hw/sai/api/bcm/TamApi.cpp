1323a5309f9ffbca23f8525d658976f09416e3ae
Max Ng <maxgg@meta.com>
2024-10-09 12:57:33 -0700

    Remove obsolete ifdefs from bcm TamApi
    Summary: Remove obsolete ifdefs from bcm TamApi
    
    Reviewed By: zechengh09
    
    Differential Revision: D64091866
    
    fbshipit-source-id: e38ba8c7f8a6339421a25b7debc601d975370ed4

--- fboss/agent/hw/sai/api/bcm/TamApi.cpp
+++ fboss/agent/hw/sai/api/bcm/TamApi.cpp
@@ -2,7 +2,6 @@
 
 #include "fboss/agent/hw/sai/api/TamApi.h"
 
-#if !defined(BRCM_SAI_SDK_XGS_AND_DNX)
 extern "C" {
 #ifndef IS_OSS_BRCM_SAI
 #include <experimental/saitamextensions.h>
@@ -10,26 +9,17 @@ extern "C" {
 #include <saitamextensions.h>
 #endif
 }
-#endif
 
 namespace facebook::fboss {
 
 std::optional<sai_attr_id_t>
 SaiTamEventTraits::Attributes::AttributeSwitchEventType::operator()() {
-#if !defined(BRCM_SAI_SDK_XGS_AND_DNX)
-  return SAI_TAM_EVENT_ATTR_SWITCH_EVENT_TYPE;
-#else
   return std::nullopt;
-#endif
 }
 
 std::optional<sai_attr_id_t>
 SaiTamEventTraits::Attributes::AttributeEventId::operator()() {
-#if !defined(BRCM_SAI_SDK_XGS_AND_DNX)
-  return SAI_TAM_EVENT_ATTR_EVENT_ID;
-#else
   return std::nullopt;
-#endif
 }
 
 } // namespace facebook::fboss
