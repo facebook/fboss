ee600b28aaf2bf9f2c239ef12b309c27ab7dc9c2
Justin Kim <jkimdo@meta.com>
2024-10-10 14:59:19 -0700

    Drop sensorMapList usage in UnitTests
    Summary: Unit test was using legacy structs. Changed it to use new structs
    
    Reviewed By: alandau
    
    Differential Revision: D64125102
    
    fbshipit-source-id: af8eda890025f896a5c1c30302712243454a92b9

--- fboss/platform/sensor_service/test/TestUtils.cpp
+++ fboss/platform/sensor_service/test/TestUtils.cpp
@@ -13,33 +13,34 @@ namespace {
 
 std::string mockSensorConfig(const std::string& tmpPath) {
   SensorConfig config;
+  PmUnitSensors pmUnitSensors;
+  pmUnitSensors.slotPath() = "/";
+  pmUnitSensors.pmUnitName() = "MOCK";
 
-  Sensor mock_fru_1_sensor_1, mock_fru_1_sensor_2, mock_fru_2_sensor_1;
-  mock_fru_1_sensor_1.path_ref() = tmpPath + "/mock_fru_1_sensor_1_path:temp1";
-  mock_fru_1_sensor_2.path_ref() = tmpPath + "/mock_fru_1_sensor_2_path:fan1";
-  mock_fru_2_sensor_1.path_ref() = tmpPath + "/mock_fru_2_sensor_1_path:vin";
-  std::string value = "25";
-  folly::writeFile(value, (*mock_fru_1_sensor_1.path()).c_str());
-  value = "11152";
-  folly::writeFile(value, (*mock_fru_1_sensor_2.path()).c_str());
-  value = "11.875";
-  folly::writeFile(value, (*mock_fru_2_sensor_1.path()).c_str());
+  PmSensor mockFruSensor1, mockFruSensor2, mockFruSensor3;
+  mockFruSensor1.name() = "MOCK_FRU_SENSOR1";
+  mockFruSensor1.compute() = "@/1000";
+  mockFruSensor1.type() = SensorType::TEMPERTURE;
+  mockFruSensor1.sysfsPath() = tmpPath + "/mock_fru_sensor_1_path:temp1";
+  folly::writeFile(std::string{"25"}, mockFruSensor1.sysfsPath()->c_str());
+  mockFruSensor2.name() = "MOCK_FRU_SENSOR2";
+  mockFruSensor2.type() = SensorType::FAN;
+  mockFruSensor2.sysfsPath() = tmpPath + "/mock_fru_sensor_2_path:fan1";
+  folly::writeFile(std::string{"11152"}, mockFruSensor2.sysfsPath()->c_str());
+  mockFruSensor3.name() = "MOCK_FRU_SENSOR3";
+  mockFruSensor3.compute() = "@ + 5";
+  mockFruSensor3.type() = SensorType::VOLTAGE;
+  mockFruSensor3.sysfsPath() = tmpPath + "/mock_fru_sensor_3_path:vin";
+  folly::writeFile(std::string{"11.875"}, mockFruSensor3.sysfsPath()->c_str());
 
-  mock_fru_1_sensor_1.compute() = "@/1000";
-  mock_fru_2_sensor_1.compute() = "@ + 5";
+  pmUnitSensors.sensors() = {
+      std::move(mockFruSensor1),
+      std::move(mockFruSensor2),
+      std::move(mockFruSensor3),
+  };
+  config.pmUnitSensorsList() = {std::move(pmUnitSensors)};
 
-  mock_fru_1_sensor_1.type_ref() = SensorType::TEMPERTURE;
-  mock_fru_1_sensor_2.type_ref() = SensorType::FAN;
-  mock_fru_2_sensor_1.type_ref() = SensorType::VOLTAGE;
-
-  sensorMap sMapFru1, sMapFru2;
-  sMapFru1["MOCK_FRU_1_SENSOR_1"] = mock_fru_1_sensor_1;
-  sMapFru1["MOCK_FRU_1_SENSOR_2"] = mock_fru_1_sensor_2;
-  sMapFru2["MOCK_FRU_2_SENSOR_1"] = mock_fru_2_sensor_1;
-  config.sensorMapList_ref() = {
-      {"MOCK_FRU1", sMapFru1}, {"MOCK_FRU2", sMapFru2}};
-
-  std::string fileName = tmpPath + "/mock_sensor_config";
+  std::string fileName = tmpPath + "/sensor_service.json";
   folly::writeFile(
       apache::thrift::SimpleJSONSerializer::serialize<std::string>(config),
       fileName.c_str());
@@ -57,8 +58,8 @@ std::shared_ptr<SensorServiceImpl> createSensorServiceImplForTest(
 
 std::map<std::string, float> getDefaultMockSensorData() {
   return std::map<std::string, float>{
-      {"MOCK_FRU_1_SENSOR_1", 0.025 /* 25/10000 */},
-      {"MOCK_FRU_1_SENSOR_2", 11152},
-      {"MOCK_FRU_2_SENSOR_1", 16.875 /* 11.875 + 5*/},
+      {"MOCK_FRU_SENSOR1", 0.025 /* 25/10000 */},
+      {"MOCK_FRU_SENSOR2", 11152},
+      {"MOCK_FRU_SENSOR3", 16.875 /* 11.875 + 5*/},
   };
 }
