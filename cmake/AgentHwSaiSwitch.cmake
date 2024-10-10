f55d78d7f0a0f4ee924291e0460546ac6d8bc0a1
Paul Cruz <paulcruz74@meta.com>
2024-10-09 17:41:19 -0700

    Import vendor reported patch for OSS fix
    Summary: Vendor reported fix from Satish
    
    Reviewed By: srikrishnagopu
    
    Differential Revision: D64145549
    
    fbshipit-source-id: 8dbd85862be7a42e0b52a3f6671556dc5c986a70

--- cmake/AgentHwSaiSwitch.cmake
+++ cmake/AgentHwSaiSwitch.cmake
@@ -64,7 +64,7 @@ elseif (SAI_BRCM_IMPL)
     fboss/agent/hw/sai/switch/npu/bcm/SaiTamManager.cpp
     fboss/agent/hw/sai/switch/npu/bcm/SaiPortManager.cpp
     fboss/agent/hw/sai/switch/npu/bcm/SaiQueueManager.cpp
-    fboss/agent/hw/sai/switch/oss/SaiSwitchManager.cpp
+    fboss/agent/hw/sai/switch/npu/bcm/SaiSwitchManager.cpp
     fboss/agent/hw/sai/switch/oss/SaiAclTableManager.cpp
   )
 else()
