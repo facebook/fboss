11d1388a7f5ba1abaab391366907946a02cb5159
Ron He <zecheng@meta.com>
2024-10-24 21:39:34 -0700

    Fix setting maintap in zero preemphasis setting - third try
    Summary:
    Original diff D58880165 - we had issue on TH3 roundtrip tests, which got clarified during discussion with Brcm. Issue will be addressed in D64218169.
    
    As titled. The goal is to set preemphasis + all six-tap values to zero (except main), and then set main to zero.
    This is due to the check on yamp platform -
      ERROR: in BLACKHAWK, main TAP is expected to be greater than all other TAPs.ERROR:chip/blackhawk/tier1/blackhawk_tsc_config.c->blackhawk_tsc_validate_txfir_cfg() SerDes err_code = ERR_CODE_BAD_PTR_OR_INVALID_INPUT : 26
    Fix this by first setting sixtap but keeping main the same, and then set main to zero.
    
    Differential Revision: D64928565
    
    fbshipit-source-id: 1abedbc93ec9b1f27a99afdf0641d56898f1608a

--- fboss/agent/hw/sai/switch/SaiPortManager.cpp
+++ fboss/agent/hw/sai/switch/SaiPortManager.cpp
@@ -2928,25 +2928,18 @@ void SaiPortManager::changeZeroPreemphasis(
 #else
       // Brcm enforces main tap to be greater than all attributes.
       // Hence set other attributes first, and then set main to zero.
-      auto setTxRxAttr = [](auto& attrs, auto type, const auto& val) {
-        auto& attr =
-            std::get<std::optional<std::decay_t<decltype(type)>>>(attrs);
-        if (!val.empty()) {
-          attr = val;
-        }
-      };
-
       auto nonZeroMainAttribute = serDesAttributes;
       auto txMain =
           std::get<std::optional<SaiPortSerdesTraits::Attributes::TxFirMain>>(
-              serDesAttributes);
+              portHandle->serdes->attributes());
       if (txMain.has_value()) {
-        setTxRxAttr(
-            nonZeroMainAttribute,
-            SaiPortSerdesTraits::Attributes::TxFirMain{},
-            txMain.value().value());
-        portHandle->serdes->setAttributes(nonZeroMainAttribute);
+        std::get<std::optional<SaiPortSerdesTraits::Attributes::TxFirMain>>(
+            nonZeroMainAttribute) = txMain.value().value();
+      } else {
+        std::get<std::optional<SaiPortSerdesTraits::Attributes::TxFirMain>>(
+            nonZeroMainAttribute) = std::nullopt;
       }
+      portHandle->serdes->setAttributes(nonZeroMainAttribute);
       portHandle->serdes->setAttributes(serDesAttributes);
 #endif
     }
