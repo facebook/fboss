21dec0e52ceb5a0fd1e05ea0cbafe0dbc8309f2e
Midhun Somasundaran <msomasundaran@meta.com>
2024-10-29 12:03:01 -0700

    Migrate HwPortTest to AgentTest
    Summary: Migarate port test to agent hw test
    
    Reviewed By: maxwindiff
    
    Differential Revision: D63298845
    
    fbshipit-source-id: f53e4e726a22e5ce75a21d95b8b6ba12f64ad594

--- fboss/agent/hw/bcm/tests/HwTestPortUtilsThriftHandler.cpp
+++ fboss/agent/hw/bcm/tests/HwTestPortUtilsThriftHandler.cpp
@@ -8,6 +8,26 @@ extern "C" {
 #include <bcm/port.h>
 }
 
+using facebook::fboss::cfg::PortLoopbackMode;
+
+namespace {
+facebook::fboss::cfg::PortLoopbackMode bcmToFbLoopbackMode(
+    bcm_port_loopback_t inMode) {
+  switch (inMode) {
+    case BCM_PORT_LOOPBACK_NONE:
+      return facebook::fboss::cfg::PortLoopbackMode::NONE;
+    case BCM_PORT_LOOPBACK_PHY:
+      return facebook::fboss::cfg::PortLoopbackMode::PHY;
+    case BCM_PORT_LOOPBACK_MAC:
+      return facebook::fboss::cfg::PortLoopbackMode::MAC;
+    default:
+      CHECK(0) << "Should never reach here";
+      break;
+  }
+  return facebook::fboss::cfg::PortLoopbackMode::NONE;
+}
+} // namespace
+
 namespace facebook {
 namespace fboss {
 namespace utility {
@@ -28,7 +48,8 @@ void HwTestThriftHandler::getPortInfo(
     auto rv = bcm_port_loopback_get(
         0, static_cast<bcm_port_t>(portId), &loopbackMode);
     bcmCheckError(rv, "Failed to get loopback mode for port:", portId);
-    portInfo.loopbackMode() = loopbackMode;
+    portInfo.loopbackMode() = static_cast<int>(
+        bcmToFbLoopbackMode(static_cast<bcm_port_loopback_t>(loopbackMode)));
     portInfos.push_back(portInfo);
   }
   return;
