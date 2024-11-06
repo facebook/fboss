2442de72bff22378f66e4150f1fc32f1ccb69012
Ron He <zecheng@meta.com>
2024-11-06 09:29:39 -0800

    Agent tests to verify conditional entropy load balancing
    Summary:
    As titled. Add Agent test to verify load balancing of conditional entropy.
    
    Test is not passing as of now, debugging with Broadcom on FW seed update. Will land only after test is passing
    
    Reviewed By: jasmeetbagga
    
    Differential Revision: D65310330
    
    fbshipit-source-id: a81ade25f8fdf8f7acd48f8289d5420e83ce3e81

--- fboss/agent/test/agent_hw_tests/AgentVoqSwitchTests.cpp
+++ fboss/agent/test/agent_hw_tests/AgentVoqSwitchTests.cpp
@@ -2536,6 +2536,7 @@ class AgentVoqSwitchConditionalEntropyTest : public AgentVoqSwitchTest {
         port.conditionalEntropyRehash() = true;
       }
     }
+    cfg.switchSettings()->conditionalEntropyRehashPeriodUS() = 100;
     return cfg;
   }
 };
@@ -2553,6 +2554,83 @@ TEST_F(AgentVoqSwitchConditionalEntropyTest, init) {
       }
     }
     // TODO: Program ECMP route, insert traffic and verify change in next hop.
+  };
+  verifyAcrossWarmBoots(setup, verify);
+}
+
+TEST_F(AgentVoqSwitchConditionalEntropyTest, verifyLoadBalancing) {
+  const auto kEcmpWidth = 4;
+  const auto kMaxDeviation = 25;
+
+  auto getLocalSysPortDesc = [this](auto ecmpHelper) {
+    std::vector<PortDescriptor> sysPortDescs;
+    auto localSysPortDescs = resolveLocalNhops(ecmpHelper);
+    sysPortDescs.insert(
+        sysPortDescs.end(),
+        localSysPortDescs.begin(),
+        localSysPortDescs.begin() + kEcmpWidth);
+    return sysPortDescs;
+  };
+  auto setup = [this, kEcmpWidth, &getLocalSysPortDesc]() {
+    utility::EcmpSetupTargetedPorts6 ecmpHelper(getProgrammedState());
+
+    std::vector<PortDescriptor> sysPortDescs = getLocalSysPortDesc(ecmpHelper);
+
+    auto prefix = RoutePrefixV6{folly::IPAddressV6("0::0"), 0};
+    auto routeUpdater = getSw()->getRouteUpdater();
+    ecmpHelper.programRoutes(
+        &routeUpdater,
+        flat_set<PortDescriptor>(
+            std::make_move_iterator(sysPortDescs.begin()),
+            std::make_move_iterator(sysPortDescs.end())),
+        {prefix});
+  };
+
+  auto verify = [this, &getLocalSysPortDesc]() {
+    // Send traffic through the 5th interface port and verify load balancing
+    const auto kIngressPort = 5;
+    CHECK(masterLogicalInterfacePortIds().size() > kIngressPort + 1);
+
+    utility::EcmpSetupTargetedPorts6 ecmpHelper(getProgrammedState());
+    std::vector<PortDescriptor> sysPortDescs = getLocalSysPortDesc(ecmpHelper);
+
+    std::function<std::map<SystemPortID, HwSysPortStats>(
+        const std::vector<SystemPortID>&)>
+        getSysPortStatsFn = [&](const std::vector<SystemPortID>& portIds) {
+          getSw()->updateStats();
+          return getLatestSysPortStats(portIds);
+        };
+
+    utility::pumpTrafficAndVerifyLoadBalanced(
+        [&]() {
+          utility::pumpRoCETraffic(
+              true /* isV6 */,
+              utility::getAllocatePktFn(getAgentEnsemble()),
+              utility::getSendPktFunc(getAgentEnsemble()),
+              utility::getFirstInterfaceMac(getProgrammedState()),
+              std::nullopt /* vlan */,
+              masterLogicalInterfacePortIds()[kIngressPort],
+              utility::kUdfL4DstPort,
+              255 /* hopLimit */,
+              std::nullopt /* srcMacAddr */,
+              1000000, /* packetCount */
+              utility::kUdfRoceOpcodeAck,
+              0x40, /* reserved */
+              std::nullopt, /* nextHdr */
+              true /* sameDstQueue */);
+        },
+        [&]() {
+          auto ports = std::make_unique<std::vector<int32_t>>();
+          for (auto sysPortDecs : sysPortDescs) {
+            ports->push_back(static_cast<int32_t>(sysPortDecs.sysPortID()));
+          }
+          getSw()->clearPortStats(ports);
+        },
+        [&]() {
+          WITH_RETRIES(EXPECT_EVENTUALLY_TRUE(utility::isLoadBalanced(
+              sysPortDescs, {}, getSysPortStatsFn, kMaxDeviation, false)));
+          return true;
+        });
   };
   verifyAcrossWarmBoots(setup, verify);
 }
