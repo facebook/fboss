e90d834667c4a0c2c1a814155f477be3287b9eab
Parvez Shaikh <pshaikh@meta.com>
2024-11-11 09:04:40 -0800

    remove unnecessary call to add acl table group and acl table
    Summary:
    this is not needed and is already being done in ConfigUtils.cpp:L732
    
    further this is already handled in test config -  D61352107
    
    Reviewed By: shri-khare
    
    Differential Revision: D65635308
    
    fbshipit-source-id: 60559df67dadd74597efb1ac2c7b7ec09bde630a

--- fboss/agent/test/agent_hw_tests/AgentAclCounterTests.cpp
+++ fboss/agent/test/agent_hw_tests/AgentAclCounterTests.cpp
@@ -68,16 +68,10 @@ class AgentAclCounterTest : public AgentHwTest {
   }
   cfg::SwitchConfig initialConfig(
       const AgentEnsemble& ensemble) const override {
-    auto cfg = utility::onePortPerInterfaceConfig(
+    return utility::onePortPerInterfaceConfig(
         ensemble.getSw(),
         ensemble.masterLogicalPortIds(),
         true /*interfaceHasSubnet*/);
-    if (FLAGS_enable_acl_table_group) {
-      utility::addAclTableGroup(
-          &cfg, cfg::AclStage::INGRESS, utility::getAclTableGroupName());
-      utility::addDefaultAclTable(cfg);
-    }
-    return cfg;
   }
 
   std::string getAclName(AclType aclType) const {
