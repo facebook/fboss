f146353d32f8a2fec57bc5fdbb3c7c85dba31e15
Jasmeet Bagga <jasmeetbagga@meta.com>
2024-11-14 21:37:28 -0800

    Add VOQ utils lib. With API to get num voqs for a port
    Summary:
    For 2-stage 3Q/2Q deployment, sys ports need to be
    created with a very specific VOQ allocation. Based on the
    port type, scope and role we can figure this out. Add a
    utility API to determine this infomration.
    
    Note that sys ports get created during cold boot itself,
    before we even have the config. Hence this information needs
    to be available before config. Further, even if we plumbed
    AgentConfig through to switch init, there are tons of tests
    which do not even specify QOS config, yet they need to create
    sys ports and hence need num voq information
    
    Reviewed By: nivinl
    
    Differential Revision: D65969461
    
    fbshipit-source-id: bb3f725c6008a4b2e965db5398fe3836e639c569

---
+++ fboss/agent/VoqUtils.cpp
@@ -0,0 +1,32 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#include "fboss/agent/VoqUtils.h"
+
+#include "fboss/agent/AgentFeatures.h"
+
+namespace facebook::fboss {
+
+int getNumVoqs(cfg::PortType portType, cfg::Scope portScope) {
+  if (!isDualStage3Q2QMode()) {
+    return 8;
+  }
+  if (FLAGS_dual_stage_edsw_3q_2q) {
+    return 3;
+  }
+  CHECK(FLAGS_dual_stage_rdsw_3q_2q);
+  if (portType == cfg::PortType::MANAGEMENT_PORT ||
+      (portType == cfg::PortType::RECYCLE_PORT &&
+       portScope == cfg::Scope::GLOBAL)) {
+    return 2;
+  }
+  return 3;
+}
+} // namespace facebook::fboss
