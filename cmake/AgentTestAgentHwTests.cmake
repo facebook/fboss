3168a016b5c145b766111becfd353f87f96b2705
Midhun Somasundaran <msomasundaran@meta.com>
2024-11-16 10:27:45 -0800

    Port IngressBufferTests to agent hw test
    Summary: porting tests to agent hw tests
    
    Reviewed By: jasmeetbagga
    
    Differential Revision: D65696704
    
    fbshipit-source-id: 2313128d4cc3f322848af30cfc2eb5ccbf1268d1

--- cmake/AgentTestAgentHwTests.cmake
+++ cmake/AgentTestAgentHwTests.cmake
@@ -16,6 +16,7 @@ add_library(agent_hw_test_src
   fboss/agent/test/agent_hw_tests/AgentRouteOverDifferentAddressFamilyNhopTests.cpp
   fboss/agent/test/agent_hw_tests/AgentAclInDiscardCounterTests.cpp
   fboss/agent/test/agent_hw_tests/AgentJumboFramesTests.cpp
+  fboss/agent/test/agent_hw_tests/AgentIngressBufferTests.cpp
   fboss/agent/test/agent_hw_tests/AgentInNullRouteDiscardsTest.cpp
   fboss/agent/test/agent_hw_tests/AgentInTrapDiscardsTest.cpp
   fboss/agent/test/agent_hw_tests/AgentPacketSendTests.cpp
