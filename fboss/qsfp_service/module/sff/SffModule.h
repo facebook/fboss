8c56c47114e26b735003c9cb9f4a66a1bbc6a457
Mohammed Al-Sanabani <sanabani@meta.com>
2024-11-16 00:47:56 -0800

    unifying SFF_FR1 and SFF fields
    Summary: title
    
    Differential Revision: D64645481
    
    fbshipit-source-id: 399cd7424a47dfe867d7954bc8f3a8a31d113831

--- fboss/qsfp_service/module/sff/SffModule.h
+++ fboss/qsfp_service/module/sff/SffModule.h
@@ -19,7 +19,6 @@ enum class SffPages : int {
 };
 
 enum class SffField;
-enum class SffFr1Field;
 
 class SffModule : public QsfpModule {
  public:
@@ -308,14 +307,10 @@ class SffModule : public QsfpModule {
   void readSffField(SffField field, uint8_t* data, bool skipPageChange = false);
   void
   writeSffField(SffField field, uint8_t* data, bool skipPageChange = false);
-  void readSffFr1Field(
-      SffFr1Field field,
-      uint8_t* data,
-      bool skipPageChange = false);
-  void writeSffFr1Field(
-      SffFr1Field field,
-      uint8_t* data,
-      bool skipPageChange = false);
+  void
+  readSffFr1Field(SffField field, uint8_t* data, bool skipPageChange = false);
+  void
+  writeSffFr1Field(SffField field, uint8_t* data, bool skipPageChange = false);
 
   /* readField and writeField are not intended to be used directly in the
    * application code. These just help the readSffField/writeSffField to make
