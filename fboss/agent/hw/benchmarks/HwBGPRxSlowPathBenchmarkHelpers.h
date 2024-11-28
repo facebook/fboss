a8033454004e4016fdc1fd188a4b7b1c0dd866c6
Ruinan Hu <ruinanhu@meta.com>
2024-11-27 23:13:32 -0800

    add BGP traffic generator
    Summary: add bgp traffic generating function
    
    Reviewed By: zechengh09
    
    Differential Revision: D66386463
    
    fbshipit-source-id: c96b18ed3f355e601bec8e79954f013be8791b2d

---
+++ fboss/agent/hw/benchmarks/HwBGPRxSlowPathBenchmarkHelpers.h
@@ -0,0 +1,100 @@
+#include "fboss/agent/TxPacket.h"
+#include "fboss/agent/benchmarks/AgentBenchmarks.h"
+#include "fboss/agent/packet/PktFactory.h"
+#include "fboss/agent/test/AgentEnsemble.h"
+#include "fboss/agent/test/EcmpSetupHelper.h"
+#include "fboss/agent/test/RouteScaleGenerators.h"
+#include "fboss/agent/test/utils/CoppTestUtils.h"
+#include "fboss/agent/test/utils/QosTestUtils.h"
+
+namespace {
+const std::string kDstIp = "2620:0:1cfe:face:b00c::4";
+}; // namespace
+
+namespace facebook::fboss::utility {
+enum BgpRxMode { routeProgramming, routeProgrammingWithPortFlap };
+
+void rxSlowPathBGPRouteChangeBenchmark(BgpRxMode mode) {
+  auto ensemble = createAgentEnsemble(
+      bgpRxBenchmarkConfig, false /*disableLinkStateToggler*/);
+
+  // capture packet exiting port 0 (entering due to loopback)
+  auto dstMac = utility::getFirstInterfaceMac(ensemble->getProgrammedState());
+  auto ecmpHelper =
+      utility::EcmpSetupAnyNPorts6(ensemble->getProgrammedState(), dstMac);
+  flat_set<PortDescriptor> IntfPorts;
+  IntfPorts.insert(
+      PortDescriptor(ensemble->masterLogicalInterfacePortIds()[0]));
+
+  ensemble->applyNewState([&](const std::shared_ptr<SwitchState>& in) {
+    return ecmpHelper.resolveNextHops(in, IntfPorts);
+  });
+  ecmpHelper.programRoutes(
+      std::make_unique<SwSwitchRouteUpdateWrapper>(
+          ensemble->getSw(), ensemble->getSw()->getRib()),
+      IntfPorts,
+      {RoutePrefixV6{folly::IPAddressV6(), 0}});
+  // Disable TTL decrements
+  utility::ttlDecrementHandlingForLoopbackTraffic(
+      ensemble.get(), ecmpHelper.getRouterId(), ecmpHelper.getNextHops()[0]);
+
+  const auto kSrcMac = folly::MacAddress{"fa:ce:b0:00:00:0c"};
+  // Send packet
+  auto vlanId = utility::firstVlanID(ensemble->getProgrammedState());
+  auto constexpr kPacketToSend = 10;
+  for (int i = 0; i < kPacketToSend; i++) {
+    auto txPacket = utility::makeTCPTxPacket(
+        ensemble->getSw(),
+        vlanId,
+        kSrcMac,
+        dstMac,
+        folly::IPAddressV6("2620:0:1cfe:face:b00c::3"),
+        folly::IPAddressV6(kDstIp),
+        47231,
+        kBgpPort);
+    ensemble->getSw()->sendPacketSwitchedAsync(std::move(txPacket));
+  }
+
+  constexpr auto kBurnIntevalInSeconds = 10;
+  // Let the packet flood warm up
+  std::this_thread::sleep_for(std::chrono::seconds(kBurnIntevalInSeconds));
+  // start baseline BGP benchmark test
+  constexpr uint8_t kCpuQueue = 0;
+  std::map<int, CpuPortStats> cpuStatsBefore;
+  ensemble->getSw()->getAllCpuPortStats(cpuStatsBefore);
+  auto statsBefore = cpuStatsBefore[0];
+  auto [pktsBefore, bytesBefore] = utility::getCpuQueueOutPacketsAndBytes(
+      *statsBefore.portStats_(), kCpuQueue);
+  auto timeBefore = std::chrono::steady_clock::now();
+  CHECK_NE(pktsBefore, 0);
+  std::this_thread::sleep_for(std::chrono::seconds(kBurnIntevalInSeconds));
+  std::map<int, CpuPortStats> cpuStatsAfter;
+  ensemble->getSw()->getAllCpuPortStats(cpuStatsAfter);
+  auto statsAfter = cpuStatsAfter[0];
+  auto [pktsAfter, bytesAfter] = utility::getCpuQueueOutPacketsAndBytes(
+      *statsAfter.portStats_(), kCpuQueue);
+  auto timeAfter = std::chrono::steady_clock::now();
+  std::chrono::duration<double, std::milli> durationMillseconds =
+      timeAfter - timeBefore;
+  uint32_t bgpRouteAddPPS = (static_cast<double>(pktsAfter - pktsBefore) /
+                             durationMillseconds.count()) *
+      1000;
+  uint32_t bgpRouteAddBytesPerSec =
+      (static_cast<double>(bytesAfter - bytesBefore) /
+       durationMillseconds.count()) *
+      1000;
+
+  if (FLAGS_json) {
+    folly::dynamic cpuRxRateJson = folly::dynamic::object;
+    cpuRxRateJson["cpu_rx_pps"] = bgpRouteAddPPS;
+    cpuRxRateJson["cpu_rx_bytes_per_sec"] = bgpRouteAddBytesPerSec;
+    std::cout << toPrettyJson(cpuRxRateJson) << std::endl;
+  } else {
+    XLOG(DBG2) << " Pkts before: " << pktsBefore << " Pkts after: " << pktsAfter
+               << " interval ms: " << durationMillseconds.count()
+               << " pps: " << bgpRouteAddPPS
+               << " bytes per sec: " << bgpRouteAddBytesPerSec;
+  }
+}
+
+} // namespace facebook::fboss::utility
