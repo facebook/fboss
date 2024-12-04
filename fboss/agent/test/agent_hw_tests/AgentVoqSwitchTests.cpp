40c1310fdabfd058c98b2f0391ebc5a36f5d6051
Ron He <zecheng@meta.com>
2024-12-03 20:06:32 -0800

    AgentVoqSwitch Shel init test
    Summary: As titled. Configure corresponding SAI switch and port configuration.
    
    Reviewed By: jasmeetbagga
    
    Differential Revision: D66533116
    
    fbshipit-source-id: dbf4fbf320174868988132a6407241ef79a672b8

--- fboss/agent/test/agent_hw_tests/AgentVoqSwitchTests.cpp
+++ fboss/agent/test/agent_hw_tests/AgentVoqSwitchTests.cpp
@@ -2665,4 +2665,45 @@ TEST_F(AgentVoqSwitchConditionalEntropyTest, verifyLoadBalancing) {
   };
   verifyAcrossWarmBoots(setup, verify);
 }
+
+class AgentVoqShelSwitchTest : public AgentVoqSwitchWithMultipleDsfNodesTest {
+ public:
+  cfg::SwitchConfig initialConfig(
+      const AgentEnsemble& ensemble) const override {
+    auto config =
+        AgentVoqSwitchWithMultipleDsfNodesTest::initialConfig(ensemble);
+    cfg::SelfHealingEcmpLagConfig shelConfig;
+    shelConfig.shelSrcIp() = "2222::1";
+    shelConfig.shelDstIp() = "2222::2";
+    shelConfig.shelPeriodicIntervalMS() = 5000;
+    config.switchSettings()->selfHealingEcmpLagConfig() = shelConfig;
+    return config;
+  }
+};
+
+TEST_F(AgentVoqShelSwitchTest, init) {
+  auto setup = [this]() {
+    auto config = initialConfig(*getAgentEnsemble());
+    // Enable Conditional Entropy on Interface Ports
+    for (auto& port : *config.ports()) {
+      if (port.portType() == cfg::PortType::INTERFACE_PORT) {
+        port.selfHealingECMPLagEnable() = true;
+      }
+    }
+    applyNewConfig(config);
+  };
+
+  auto verify = [this]() {
+    auto state = getProgrammedState();
+    for (const auto& portMap : std::as_const(*state->getPorts())) {
+      for (const auto& port : std::as_const(*portMap.second)) {
+        if (port.second->getPortType() == cfg::PortType::INTERFACE_PORT) {
+          EXPECT_TRUE(port.second->getSelfHealingECMPLagEnable());
+        }
+      }
+    }
+  };
+  verifyAcrossWarmBoots(setup, verify);
+}
+
 } // namespace facebook::fboss
