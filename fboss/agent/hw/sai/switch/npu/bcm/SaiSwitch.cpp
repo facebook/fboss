4ca084e816538d0ec22374fb302209f307472f2d
Nivin Lawrence <nivinl@meta.com>
2024-12-10 22:31:05 -0800

    Use the new ASIC error interrupts to report ECC and OVERFLOW events
    Summary:
    1. As titled, add support for the new interrupts
    2. Some errors could increment multiple counters, hence avoid the else if.
    
    Reviewed By: jasmeetbagga
    
    Differential Revision: D66566142
    
    fbshipit-source-id: e0ffd45ea4d6ae6f7b7349127f449646fdeaa6d4

--- fboss/agent/hw/sai/switch/npu/bcm/SaiSwitch.cpp
+++ fboss/agent/hw/sai/switch/npu/bcm/SaiSwitch.cpp
@@ -485,6 +485,75 @@ bool allReassemblyContextsTakenError(sai_switch_error_type_t type) {
   return false;
 }
 
+bool isEccError(sai_switch_error_type_t type) {
+  switch (type) {
+    case SAI_SWITCH_ERROR_TYPE_ALIGNER_ECC_ECC_2B_ERR_INT:
+    case SAI_SWITCH_ERROR_TYPE_EPNI_ECC_ECC_2B_ERR_INT:
+    case SAI_SWITCH_ERROR_TYPE_FQP_ECC_ECC_2B_ERR_INT:
+#if defined(SAI_VERSION_11_7_0_0_DNX_ODP)
+    // TODO: Fix the 11.7 specific check added. Currently support is not
+    // available in 12.0 or 11.3, hence using the 11.7 specific check.
+    case SAI_SWITCH_ERROR_TYPE_FDA_ECC_ECC_2B_ERR_INT:
+    case SAI_SWITCH_ERROR_TYPE_FDR_ECC_ECC_2B_ERR_INT:
+    case SAI_SWITCH_ERROR_TYPE_FMAC_ECC_ECC_2B_ERR_INT:
+    case SAI_SWITCH_ERROR_TYPE_FCR_ECC_ECC_2B_ERR_INT:
+    case SAI_SWITCH_ERROR_TYPE_FCT_ECC_ECC_2B_ERR_INT:
+    case SAI_SWITCH_ERROR_TYPE_FDT_ECC_ECC_2B_ERR_INT:
+#endif
+      return true;
+    default:
+      break;
+  }
+  return false;
+}
+
+bool isFdaFifoOverflowError(sai_switch_error_type_t type) {
+#if defined(SAI_VERSION_11_7_0_0_DNX_ODP)
+  // TODO: Fix the 11.7 specific check added. Currently support is not
+  // available in 12.0 or 11.3, hence using the 11.7 specific check.
+  switch (type) {
+    case SAI_SWITCH_ERROR_TYPE_FDA_P_0_OFM_FIFO_OVFLW_DROP_INT:
+    case SAI_SWITCH_ERROR_TYPE_FDA_P_1_OFM_FIFO_OVFLW_DROP_INT:
+    case SAI_SWITCH_ERROR_TYPE_FDA_FAB_FIF_P_1_RXI_0_OVERFLOW_INT:
+    case SAI_SWITCH_ERROR_TYPE_FDA_FDA_IPTCTRL_0_LUC_OVERFLOW_INT:
+    case SAI_SWITCH_ERROR_TYPE_FDA_FDA_IPTCTRL_1_LUC_OVERFLOW_INT:
+    case SAI_SWITCH_ERROR_TYPE_FDA_FDA_IPTCTRL_2_LUC_OVERFLOW_INT:
+    case SAI_SWITCH_ERROR_TYPE_FDA_FDA_IPTCTRL_3_LUC_OVERFLOW_INT:
+    case SAI_SWITCH_ERROR_TYPE_FDA_FDA_IPTCTRL_0_MMC_OVERFLOW_INT:
+    case SAI_SWITCH_ERROR_TYPE_FDA_FDA_IPTCTRL_1_MMC_OVERFLOW_INT:
+    case SAI_SWITCH_ERROR_TYPE_FDA_FDA_IPTCTRL_2_MMC_OVERFLOW_INT:
+    case SAI_SWITCH_ERROR_TYPE_FDA_FDA_IPTCTRL_3_MMC_OVERFLOW_INT:
+      return true;
+    default:
+      break;
+  }
+#endif
+  return false;
+}
+
+bool isFdrFifoOverflowError(sai_switch_error_type_t type) {
+#if defined(SAI_VERSION_11_7_0_0_DNX_ODP)
+  // TODO: Fix the 11.7 specific check added. Currently support is not
+  // available in 12.0 or 11.3, hence using the 11.7 specific check.
+  switch (type) {
+    case SAI_SWITCH_ERROR_TYPE_FDR_FDR_P_1_MAC_0_P_1_IFM_OVERFLOW_BY_MFIFO_MAC_0:
+    case SAI_SWITCH_ERROR_TYPE_FDR_FDR_P_2_MAC_0_P_2_IFM_OVERFLOW_BY_MFIFO_MAC_0:
+    case SAI_SWITCH_ERROR_TYPE_FDR_FDR_P_1_MAC_1_P_1_IFM_OVERFLOW_BY_MFIFO_MAC_1:
+    case SAI_SWITCH_ERROR_TYPE_FDR_FDR_P_2_MAC_1_P_2_IFM_OVERFLOW_BY_MFIFO_MAC_1:
+    case SAI_SWITCH_ERROR_TYPE_FDR_FDR_P_1_MAC_2_P_1_IFM_OVERFLOW_BY_MFIFO_MAC_2:
+    case SAI_SWITCH_ERROR_TYPE_FDR_FDR_P_2_MAC_2_P_2_IFM_OVERFLOW_BY_MFIFO_MAC_2:
+    case SAI_SWITCH_ERROR_TYPE_FDR_FDR_P_1_MAC_3_P_1_IFM_OVERFLOW_BY_MFIFO_MAC_3:
+    case SAI_SWITCH_ERROR_TYPE_FDR_FDR_P_2_MAC_3_P_2_IFM_OVERFLOW_BY_MFIFO_MAC_3:
+    case SAI_SWITCH_ERROR_TYPE_FDR_FDR_P_1_MAC_4_P_1_IFM_OVERFLOW_BY_MFIFO_MAC_4:
+    case SAI_SWITCH_ERROR_TYPE_FDR_FDR_P_2_MAC_4_P_2_IFM_OVERFLOW_BY_MFIFO_MAC_4:
+      return true;
+    default:
+      break;
+  }
+#endif
+  return false;
+}
+
 bool rtpTableChangedEvent(sai_switch_error_type_t type) {
   if (type == SAI_SWITCH_ERROR_TYPE_RTP_TABLE_CHANGE) {
     return true;
@@ -543,29 +612,52 @@ void SaiSwitch::switchEventCallback(
       auto reassemblyError = isReassemblyError(eventInfo->error_type);
       auto allReassemblyContextsTaken =
           allReassemblyContextsTakenError(eventInfo->error_type);
+      auto eccError = isEccError(eventInfo->error_type);
+      auto fdrFifoOverflowError = isFdrFifoOverflowError(eventInfo->error_type);
+      auto fdaFifoOverflowError = isFdaFifoOverflowError(eventInfo->error_type);
       auto rtpTableChanged = rtpTableChangedEvent(eventInfo->error_type);
       XLOG(ERR) << " Got interrupt event, is IRE: " << ireError
                 << " is ITPP: " << itppError << " is EPNI: " << epniError
                 << " is Aligner: " << alignerError << " is FQP: " << fqpError
                 << " is Reassembly: " << reassemblyError
                 << " all reassembly context taken: "
-                << allReassemblyContextsTaken
+                << allReassemblyContextsTaken << " is ECC: " << eccError
+                << " is FDR FIFO overflow: " << fdrFifoOverflowError
+                << " is FDA FIFO overflow: " << fdaFifoOverflowError
                 << " rtp table changed: " << rtpTableChanged;
       if (ireError) {
         getSwitchStats()->ireError();
-      } else if (itppError) {
+      }
+      if (itppError) {
         getSwitchStats()->itppError();
-      } else if (epniError) {
+      }
+      if (epniError) {
         getSwitchStats()->epniError();
-      } else if (alignerError) {
+      }
+      if (alignerError) {
         getSwitchStats()->alignerError();
-      } else if (fqpError) {
+      }
+      if (fqpError) {
         getSwitchStats()->forwardingQueueProcessorError();
-      } else if (reassemblyError) {
+      }
+      if (reassemblyError) {
         getSwitchStats()->reassemblyError();
-      } else if (allReassemblyContextsTaken) {
+      }
+      if (allReassemblyContextsTaken) {
         getSwitchStats()->allReassemblyContextsTaken();
-      } else if (rtpTableChanged) {
+      }
+      if (eccError) {
+        // ECC error indicates an error in the ASIC block and only 2bit
+        // errors are tracked, which translates to uncorrectable errors.
+        getSwitchStats()->uncorrParityError();
+      }
+      if (fdrFifoOverflowError) {
+        getSwitchStats()->fdrFifoOverflowError();
+      }
+      if (fdaFifoOverflowError) {
+        getSwitchStats()->fdaFifoOverflowError();
+      }
+      if (rtpTableChanged) {
         // RTP table change notification is vendor specific, this
         // means a change in switch reachability over fabric!
         // Initiate processing for RTP table change, ie. invoke
