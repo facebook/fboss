e3de4e55dbe0bc7c97f2d8c94ed71ea77cd69d25
Prasoon Patel <prasoon@meta.com>
2024-12-20 15:03:48 -0800

    Retire HwResourceStatsTests
    Summary: Retiring HwResourceStatsTests as AgentHwResourceStatsTests are added with new framework.
    
    Reviewed By: msomasundaran
    
    Differential Revision: D67049264
    
    fbshipit-source-id: b695085699cd1fe6d8ed0525132141816d2f2ea6

--- cmake/AgentHwTest.cmake
+++ cmake/AgentHwTest.cmake
@@ -212,7 +212,6 @@ set(hw_switch_test_srcs
   fboss/agent/hw/test/HwAclStatTests.cpp
   fboss/agent/hw/test/HwPortProfileTests.cpp
   fboss/agent/hw/test/HwPortStressTests.cpp
-  fboss/agent/hw/test/HwResourceStatsTests.cpp
   fboss/agent/hw/test/HwSwitchStateReplayTest.cpp
   fboss/agent/hw/test/HwParityErrorTest.cpp
   fboss/agent/hw/test/HwPtpTcTests.cpp
