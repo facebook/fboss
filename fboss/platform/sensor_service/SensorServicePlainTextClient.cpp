29d1ff65e3de2e3b0ca090190a628c44f6284f36
Manikandan Somasundaram <manikandan@meta.com>
2025-01-10 10:22:29 -0800

    Simplify the output of sensor_service_client
    Summary:
    The full file path of the logging location is not necessary for this
    simple utility. folly::logging does not seem to provide a straightforward way
    to customize it. So just use plain std::cout logging
    
    Reviewed By: kimdo8736
    
    Differential Revision: D68000250
    
    fbshipit-source-id: 2a958371633010e5acab8e824de55922875379dd

--- fboss/platform/sensor_service/SensorServicePlainTextClient.cpp
+++ fboss/platform/sensor_service/SensorServicePlainTextClient.cpp
@@ -1,8 +1,7 @@
 // (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
 #include <time.h>
-#include <chrono>
+#include <iostream>
 
-#include <folly/logging/xlog.h>
 #include <thrift/lib/cpp2/async/RocketClientChannel.h>
 
 #include "fboss/platform/helpers/InitCli.h"
@@ -35,22 +34,25 @@ int main(int argc, char** argv) {
       std::move(channel));
   auto req = std::vector<std::string>{};
   facebook::fboss::platform::sensor_service::SensorReadResponse res;
-  XLOG(INFO) << "Querying ...";
+  std::cout << "Querying ..." << std::endl;
   try {
     client->sync_getSensorValuesByNames(res, req);
   } catch (std::exception& ex) {
-    XLOG(INFO) << ex.what();
+    std::cout << ex.what() << std::endl;
   }
-  XLOG(INFO) << fmt::format("Collected data at {}", tstoStr(*res.timeStamp()));
+  std::cout << fmt::format("Collected data at {}", tstoStr(*res.timeStamp()))
+            << std::endl;
   for (auto v : *res.sensorData()) {
     if (v.value()) {
-      XLOG(INFO) << fmt::format(
-          "{} -> {}. Last logged at {}",
-          *v.name(),
-          *v.value(),
-          v.timeStamp() ? tstoStr(*v.timeStamp()) : "N/A");
+      std::cout << fmt::format(
+                       "{} -> {}. Last logged at {}",
+                       *v.name(),
+                       *v.value(),
+                       v.timeStamp() ? tstoStr(*v.timeStamp()) : "N/A")
+                << std::endl;
     } else {
-      XLOG(ERR) << fmt::format("{} -> NOT_SET. Error Reading", *v.name());
+      std::cout << fmt::format("{} -> NOT_SET. Error Reading", *v.name())
+                << std::endl;
     }
   }
   return 0;
