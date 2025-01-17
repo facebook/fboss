fb467b623ec4dd3cd620c86bac0500ab1794a5e1
Parvez Shaikh <pshaikh@meta.com>
2025-01-17 11:57:31 -0800

    build traffic pfc agent hardware test in oss
    Summary: as titled
    
    Reviewed By: jasmeetbagga
    
    Differential Revision: D68324519
    
    fbshipit-source-id: b28ac65e8388aabe4a3dd27f5263e03522cb7abd

--- cmake/AgentTestAgentHwTests.cmake
+++ cmake/AgentTestAgentHwTests.cmake
@@ -64,6 +64,7 @@ add_library(agent_hw_test_src
   fboss/agent/test/agent_hw_tests/AgentHwAclStatTests.cpp
   fboss/agent/test/agent_hw_tests/AgentHwResourceStatsTests.cpp
   fboss/agent/test/agent_hw_tests/AgentHwParityErrorTests.cpp
+  fboss/agent/test/agent_hw_tests/AgentTrafficPfcTests.cpp
 )
 
 target_link_libraries(agent_hw_test_src
