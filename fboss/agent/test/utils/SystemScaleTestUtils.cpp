7c4930de06ec1cae1fc96a5abbf9f89f718b3d1e
Ruinan Hu <ruinanhu@meta.com>
2025-02-21 12:17:50 -0800

    add route entries
    Reviewed By: zechengh09
    
    Differential Revision: D69672956
    
    fbshipit-source-id: 08dbd8c799a1902883e637cdba3f69b524786a91

--- fboss/agent/test/utils/SystemScaleTestUtils.cpp
+++ fboss/agent/test/utils/SystemScaleTestUtils.cpp
@@ -2,6 +2,7 @@
 #include "fboss/agent/TxPacket.h"
 #include "fboss/agent/hw/test/ConfigFactory.h"
 
+#include "fboss/agent/AgentFeatures.h"
 #include "fboss/agent/packet/PktFactory.h"
 #include "fboss/agent/test/EcmpSetupHelper.h"
 #include "fboss/agent/test/RouteScaleGenerators.h"
@@ -12,6 +13,110 @@
 #include "fboss/agent/test/utils/ScaleTestUtils.h"
 
 namespace facebook::fboss::utility {
+
+void configureMaxRouteEntries(AgentEnsemble* ensemble) {
+  auto switchIds = ensemble->getHwAsicTable()->getSwitchIDs();
+  CHECK_EQ(switchIds.size(), 1);
+  auto asic =
+      utility::checkSameAndGetAsic(ensemble->getHwAsicTable()->getL3Asics());
+  auto* sw = ensemble->getSw();
+  // reserve 10 ecmp for scaling routes
+  auto const kMaxEcmpGropus =
+      utility::getMaxEcmpGroups(ensemble->getL3Asics()) *
+          FLAGS_ecmp_resource_percentage / 100 -
+      10;
+
+  auto maxNumRoute =
+      asic->getMaxRoutes().has_value() ? asic->getMaxRoutes().value() : 0;
+  CHECK_GT(maxNumRoute, 0);
+
+  auto routeGenerator = ScaleTestRouteScaleGenerator(sw->getState());
+  auto allThriftRoutes = routeGenerator.allThriftRoutes();
+
+  std::vector<RoutePrefixV6> ecmpPrefixes;
+  utility::EcmpSetupTargetedPorts6 ecmpHelper(ensemble->getProgrammedState());
+  std::vector<PortID> portIds = ensemble->masterLogicalInterfacePortIds();
+  std::vector<PortDescriptor> portDescriptorIds;
+
+  for (const auto& portId : portIds) {
+    portDescriptorIds.push_back(PortDescriptor(portId));
+  }
+
+  std::vector<std::vector<PortDescriptor>> allCombinations =
+      utility::generateEcmpGroupScale(portDescriptorIds, kMaxEcmpGropus);
+  std::vector<flat_set<PortDescriptor>> nhopSets;
+  for (const auto& combination : allCombinations) {
+    nhopSets.emplace_back(combination.begin(), combination.end());
+  }
+
+  XLOG(DBG2) << "Max Ecmp Groups: " << kMaxEcmpGropus
+             << " Default Ecmp Width: " << kDefaulEcmpWidth
+             << " combination size: " << allCombinations.size()
+             << " generated routes: " << allThriftRoutes.size()
+             << " routes assigned to ecmp group: " << allCombinations.size();
+
+  // ECMP routes + generated routes should be less than max routes
+  CHECK_LE(allThriftRoutes.size() + allCombinations.size(), maxNumRoute);
+
+  ensemble->applyNewState(
+      [&portDescriptorIds,
+       &ecmpHelper](const std::shared_ptr<SwitchState>& in) {
+        return ecmpHelper.resolveNextHops(
+            in,
+            flat_set<PortDescriptor>(
+                std::make_move_iterator(portDescriptorIds.begin()),
+                std::make_move_iterator(portDescriptorIds.end())));
+      },
+      "program ecmp",
+      false);
+
+  std::generate_n(
+      std::back_inserter(ecmpPrefixes), kMaxEcmpGropus, [i = 0]() mutable {
+        return RoutePrefixV6{
+            folly::IPAddressV6(folly::to<std::string>(2401, "::", i++)), 128};
+      });
+
+  auto wrapper = sw->getRouteUpdater();
+  ecmpHelper.programRoutes(&wrapper, nhopSets, ecmpPrefixes);
+
+  auto platformType = sw->getPlatformType();
+  if (!routeGenerator.isSupported(platformType)) {
+    throw FbossError(
+        "Route scale generator not supported for platform: ", platformType);
+  }
+
+  auto updater = ensemble->getSw()->getRouteUpdater();
+  const RouterID kRid(0);
+  auto syncFib =
+      [&updater, kRid](
+          const utility::RouteDistributionGenerator::ThriftRouteChunk& routes) {
+        std::for_each(
+            routes.begin(), routes.end(), [&updater, kRid](const auto& route) {
+              updater.addRoute(kRid, ClientID::BGPD, route);
+            });
+        updater.program(
+            {{{kRid, ClientID::BGPD}},
+             RouteUpdateWrapper::SyncFibInfo::SyncFibType::IP_ONLY});
+      };
+  // Sync fib with same set of routes
+  // TH doesn't support 75K route scale, the following is a work around
+  if (asic->getAsicType() == cfg::AsicType::ASIC_TYPE_TOMAHAWK) {
+    std::vector<UnicastRoute> quarterThriftRoutes = std::vector<UnicastRoute>(
+        allThriftRoutes.begin(),
+        allThriftRoutes.begin() + allThriftRoutes.size() / 4);
+    syncFib(quarterThriftRoutes);
+  } else {
+    syncFib(allThriftRoutes);
+  }
+  int route_count = 0;
+  auto countRoutes = [&route_count](RouterID, auto&) { ++route_count; };
+  forAllRoutes(sw->getState(), countRoutes);
+  if (asic->getAsicType() == cfg::AsicType::ASIC_TYPE_TOMAHAWK) {
+    CHECK_GE(route_count, allThriftRoutes.size() / 4);
+  } else {
+    CHECK_GE(route_count, allThriftRoutes.size());
+  }
+}
 
 void configureMaxAclEntries(AgentEnsemble* ensemble) {
   auto cfg = ensemble->getCurrentConfig();
@@ -54,5 +159,6 @@ cfg::SwitchConfig getSystemScaleTestSwitchConfiguration(
 
 void initSystemScaleTest(AgentEnsemble* ensemble) {
   configureMaxAclEntries(ensemble);
+  configureMaxRouteEntries(ensemble);
 }
 } // namespace facebook::fboss::utility
