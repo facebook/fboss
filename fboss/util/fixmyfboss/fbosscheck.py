689101d8c63d2766ee2eb7227a438febd75e41c8
Scott Smith <smithscott@meta.com>
2025-03-11 12:17:33 -0700

    Move to fboss/util directory
    Summary: To avoid having to rewrite a lot of common code in FixMyFboss|BMC we can publish fixmyfboss to a public part of the fboss repo. In openbmc we can use SRC_URI to import the code (excluding the modules/ directory which contains fboss-specific checks). Then we will probably need to sed the files to fix the import statements but once that is done we will have free syncing between fixmyfboss and fixmybmc. Modules in openbmc will be unchanged
    
    Reviewed By: kimdo8736
    
    Differential Revision: D70575272
    
    fbshipit-source-id: 6f72e8c4923f850cf514af0998a7adedbfdd1326

---
+++ fboss/util/fixmyfboss/fbosscheck.py
@@ -0,0 +1,63 @@
+# pyre-unsafe
+
+import traceback
+from functools import wraps
+from typing import Callable, Dict
+
+from fboss.util.fixmyfboss import status
+
+_checks: Dict[str, "FbossCheck"] = {}
+
+
+class FbossCheck:
+    def __init__(
+        self,
+        name: str,
+        func: Callable,
+    ) -> None:
+        self.name = name
+        self.func = func
+        self.result = None
+
+    def run(self) -> None:
+        try:
+            result = self.func()
+        except Exception as e:
+            traceback.print_exc()
+            result = status.Error(exception=e)
+
+        if not isinstance(result, status.Status):
+            te = TypeError(f"check function returned non-Status: {result!r}")
+            result = status.Error(exception=te)
+
+        self.result = result
+
+    def __repr__(self) -> str:
+        return f"<Check {self.name}>"
+
+
+def fbossCheck(
+    func=None,
+):
+    def check_decorator(func):
+        name = func.__name__
+
+        @wraps(func)
+        def returnStatusOkWrapper(*args, **kwargs):
+            result = func(*args, **kwargs)
+            if result is None:
+                return status.Ok()
+            return result
+
+        check = FbossCheck(
+            name,
+            func=returnStatusOkWrapper,
+        )
+        _checks[name] = check
+        return check
+
+    return check_decorator(func)
+
+
+def load_checks() -> None:
+    import fboss.util.fixmyfboss.modules  # noqa: F401
