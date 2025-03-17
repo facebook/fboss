996d43618bdd37158c86006cb3b3c1a93e0ca1d6
Jeffrey Leung <jycleung@meta.com>
2025-03-17 13:17:30 -0700

    Add logs for no DSF node found for peer switch
    Summary:
    Add extra logs including expected switch/port name when a switch name
    to ID mapping is found but not a switch ID to DSF node mapping.
    
    Reviewed By: daiwei1983
    
    Differential Revision:
    D71320581
    
    Privacy Context Container: L1125642
    
    fbshipit-source-id: d840aa957f7f93fdaee6febb724d21eb7934dc12

--- fboss/agent/FabricConnectivityManager.cpp
+++ fboss/agent/FabricConnectivityManager.cpp
@@ -147,7 +147,9 @@ void FabricConnectivityManager::updateExpectedSwitchIdAndPortIdForPort(
 
   auto baseSwitchId = *it->second.begin();
   if (switchIdToDsfNode_.find(baseSwitchId) == switchIdToDsfNode_.end()) {
-    XLOG(WARN) << "no dsf node for switch id " << baseSwitchId;
+    XLOG(WARN) << "No DSF node for switch ID " << baseSwitchId
+               << ". Unable to assign expected switch and port IDs for peer "
+               << expectedSwitchName << " on port " << expectedPortName << ".";
     return;
   }
   const auto platformMapping = getPlatformMappingForPlatformType(
@@ -193,7 +195,7 @@ void FabricConnectivityManager::updateExpectedSwitchIdAndPortIdForPort(
 
 void FabricConnectivityManager::addOrUpdatePort(
     const std::shared_ptr<Port>& swPort) {
-  // Non-Faric port connectivity is handled by LLDP
+  // Non-Fabric port connectivity is handled by LLDP
   if (swPort->getPortType() != cfg::PortType::FABRIC_PORT) {
     return;
   }
