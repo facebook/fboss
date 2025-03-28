f587dfc335641e630c50529df5aacace50ca5fa0
Shrikrishna (Shri) Khare <skhare@meta.com>
2025-03-28 16:14:25 -0700

    Firmware API: get OpStatus
    Summary: As titled.
    
    Reviewed By: jasmeetbagga
    
    Differential Revision: D71700872
    
    fbshipit-source-id: e7d41bd2c476099e2f1a20d0bf8df5ed48465354

--- fboss/agent/hw/sai/api/FirmwareApi.h
+++ fboss/agent/hw/sai/api/FirmwareApi.h
@@ -48,14 +48,21 @@ struct SaiFirmwareTraits {
         SAI_FIRMWARE_ATTR_VERSION,
         std::vector<int8_t>,
         SaiS8ListDefault>;
+    using OpStatus = SaiAttribute<
+        EnumType,
+        SAI_FIRMWARE_ATTR_OP_STATUS,
+        sai_int32_t,
+        SaiIntDefault<int32_t>>;
   };
 
   using AdapterKey = FirmwareSaiId;
   using AdapterHostKey = std::monostate;
-  using CreateAttributes = std::tuple<Attributes::Version>;
+  using CreateAttributes =
+      std::tuple<Attributes::Version, Attributes::OpStatus>;
 };
 
 SAI_ATTRIBUTE_NAME(Firmware, Version)
+SAI_ATTRIBUTE_NAME(Firmware, OpStatus)
 
 class FirmwareApi : public SaiApi<FirmwareApi> {
  public:
