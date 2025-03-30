927f674b39b34b566d996be9f141f290e44fa056
Shiva Menta <smenta@meta.com>
2025-03-29 17:30:29 -0700

    OSS Platform Mapping: OSS Target – Add Command Line Args
    Summary: Adds helper function to parse all args needed for OSS platform mapping gen CLI tool.
    
    Reviewed By: paulcruz74
    
    Differential Revision: D71268963
    
    fbshipit-source-id: da0cacf86b46f688540aa5f2c7693bca6680b3f7

--- fboss/lib/platform_mapping_v2/gen.py
+++ fboss/lib/platform_mapping_v2/gen.py
@@ -1,6 +1,38 @@
 # pyre-strict
+import argparse
 import os
-from typing import Dict
+from typing import Dict, Tuple
+
+
+def get_command_line_args() -> Tuple[str, str, str, bool]:
+    parser = argparse.ArgumentParser(description="OSS platform mapping generation.")
+    parser.add_argument(
+        "--input-dir",
+        type=str,
+        required=True,
+        help="Path to input directory holding CSVs",
+    )
+    parser.add_argument(
+        "--output-dir",
+        type=str,
+        default="/tmp/generated_platform_mappings",
+        required=False,
+        help="Path to output directory for JSON (default: /tmp/generated_platform_mappings)",
+    )
+    parser.add_argument(
+        "--platform-name",
+        type=str,
+        required=True,
+        help="Name of platform that each CSV file has as prefix",
+    )
+    parser.add_argument(
+        "--multi-npu",
+        action="store_true",
+        help="Generate multi-NPU platform mapping (default: False)",
+    )
+
+    args = parser.parse_args()
+    return args.input_dir, args.output_dir, args.platform_name, args.multi_npu
 
 
 def read_vendor_data(input_file_path: str) -> Dict[str, str]:
