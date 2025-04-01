d1fe3fd37d3f22575d2ab7539ff9211500afb109
Priyank Warkhede <priyankw@meta.com>
2025-03-31 17:06:33 -0700

    churn-tests: use AgentFsdbIntegrationBenchmarkHelper
    Summary: In SystemScaleChurnMemoryBenchmark, use AgentFsdbIntegrationBenchmarkHelper to allow measuring agent-fsdb integration metrics when run with gflag fsdb_publish_test
    
    Differential Revision: D72147277
    
    fbshipit-source-id: 616e2bf803aa8f2e9f1bfdcee94e30eb622be2f6

--- fboss/agent/hw/benchmarks/HwSystemScaleChurnMemoryBenchmark.cpp
+++ fboss/agent/hw/benchmarks/HwSystemScaleChurnMemoryBenchmark.cpp
@@ -1,4 +1,5 @@
 #include "fboss/agent/test/AgentEnsemble.h"
+#include "fboss/agent/test/utils/AgentFsdbIntegrationBenchmarkHelper.h"
 #include "fboss/agent/test/utils/SystemScaleTestUtils.h"
 
 #include <folly/Benchmark.h>
@@ -6,9 +7,15 @@
 namespace facebook::fboss {
 
 BENCHMARK(SystemScaleChurnMemoryBenchmark) {
+  auto helper = AgentFsdbIntegrationBenchmarkHelper();
+
   auto ensemble = createAgentEnsemble(
       utility::getSystemScaleTestSwitchConfiguration,
       false /*disableLinkStateToggler*/);
+
+  // benchmark test
   utility::initSystemScaleChurnTest(ensemble.get());
+
+  helper.awaitCompletion(ensemble.get());
 }
 } // namespace facebook::fboss
