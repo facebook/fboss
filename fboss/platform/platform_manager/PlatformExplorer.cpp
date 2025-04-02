42555181983d65293c827b35b52c173b610ea19d
Justin Kim <jkimdo@meta.com>
2025-04-02 11:52:47 -0700

    No hard failure during readable eeprom content generation.
    Summary:
    This will be an issue with Darwin and mask other true failures in other platforms.
    
    1. Only generate readable contents for IDPROM
    2. Don't throw exception, just log.
    
    Reviewed By: somasun
    
    Differential Revision: D72318882
    
    fbshipit-source-id: ee74769dad2020ea660257de7820e7c8f3e45ea2

--- fboss/platform/platform_manager/PlatformExplorer.cpp
+++ fboss/platform/platform_manager/PlatformExplorer.cpp
@@ -818,13 +818,13 @@ void PlatformExplorer::createPciSubDevices(
 void PlatformExplorer::genHumanReadableEeproms() {
   const auto writeEepromContent = [&](const auto& devicePath,
                                       const auto& eepromRuntimePath) {
-    // Parse the eeproms which are defined as just I2cDeviceConfig (and not
-    // IdpromConfig). All eeproms in IdpromConfigs are parsed during ...
+    // Ignore if eeprom content wasn't populated, something went wrong in
+    // exploration.
     if (!dataStore_.hasEepromContents(devicePath)) {
-      // Eeproms defined in I2cDeviceConfig doesn't have offset defined.
-      dataStore_.updateEepromContents(
-          devicePath,
-          eepromParser_.getContents(eepromRuntimePath, /*offset*/ 0));
+      XLOG(ERR) << fmt::format(
+          "{} has empty eeprom contents. Skip generating eeprom content",
+          devicePath);
+      return;
     }
     auto contents = dataStore_.getEepromContents(devicePath);
     std::ostringstream os;
@@ -838,6 +838,12 @@ void PlatformExplorer::genHumanReadableEeproms() {
   for (const auto& [linkPath, devicePath] :
        *platformConfig_.symbolicLinkToDevicePath()) {
     if (!linkPath.starts_with("/run/devmap/eeproms")) {
+      continue;
+    }
+    const auto [slotPath, deviceName] = Utils().parseDevicePath(devicePath);
+    if (deviceName != "IDPROM") {
+      XLOG(WARNING) << fmt::format(
+          "{} is not IDPROM. Skip generating eeprom content.", linkPath);
       continue;
     }
     writeEepromContent(devicePath, linkPath);
