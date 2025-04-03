e2b70a5400afe228e37f8dd9f0af1c8285ffc794
Wei Dai <daiweix@meta.com>
2025-04-03 14:29:18 -0700

    set TcRateLimitExclude to cpu system port only when changed
    Summary: as titled
    
    Differential Revision:
    D72391320
    
    Privacy Context Container: L1125642
    
    fbshipit-source-id: cab96f66de2cc246761394f317ccad38a17cd043

--- fboss/agent/hw/sai/switch/SaiHostifManager.cpp
+++ fboss/agent/hw/sai/switch/SaiHostifManager.cpp
@@ -685,20 +685,26 @@ void SaiHostifManager::loadCpuSystemPortVoqs() {
 void SaiHostifManager::loadCpuPort() {
   cpuPortHandle_ = std::make_unique<SaiCpuPortHandle>();
   cpuPortHandle_->cpuPortId = managerTable_->switchManager().getCpuPort();
-  XLOG(DBG5) << "Got cpu sai port ID " << cpuPortHandle_->cpuPortId;
+  XLOG(DBG2) << "Got cpu sai port ID " << cpuPortHandle_->cpuPortId;
   const auto& portApi = SaiApiTable::getInstance()->portApi();
   if (platform_->getAsic()->isSupported(HwAsic::Feature::VOQ)) {
     auto attr = SaiPortTraits::Attributes::SystemPort{};
     cpuPortHandle_->cpuSystemPortId =
         portApi.getAttribute(cpuPortHandle_->cpuPortId, attr);
-    XLOG(DBG5) << "Got cpu sai system port ID "
+    XLOG(DBG2) << "Got cpu sai system port ID "
                << cpuPortHandle_->cpuSystemPortId.value();
 #if defined(BRCM_SAI_SDK_DNX_GTE_12_0)
     auto& systemPortApi = SaiApiTable::getInstance()->systemPortApi();
-    systemPortApi.setAttribute(
+    auto oldTcRateLimitExclude = systemPortApi.getAttribute(
         cpuPortHandle_->cpuSystemPortId.value(),
-        SaiSystemPortTraits::Attributes::TcRateLimitExclude{true});
-    XLOG(DBG5) << "Excluded cpu system port from global tc rate limit";
+        SaiSystemPortTraits::Attributes::TcRateLimitExclude{});
+    // always exclude cpu system port from global tc based rate limit
+    if (!oldTcRateLimitExclude) {
+      systemPortApi.setAttribute(
+          cpuPortHandle_->cpuSystemPortId.value(),
+          SaiSystemPortTraits::Attributes::TcRateLimitExclude{true});
+      XLOG(DBG2) << "Excluded cpu system port from global tc rate limit";
+    }
 #endif
   }
   loadCpuPortQueues();
