4ece76fe4056462cd0bd53ca635106f0d68d911d
Max Ng <maxgg@meta.com>
2025-04-04 11:15:28 -0700

    Use facebook-internal manager implementations for CHENAB
    Summary: Use facebook-internal manager implementations for CHENAB so that fb303 stats are published.
    
    Reviewed By: nivinl
    
    Differential Revision: D72341325
    
    fbshipit-source-id: 5bb5c34d175020c661b18e0142af146f2e0203d1

---
+++ fboss/agent/hw/sai/switch/npu/chenab/SaiSwitchManager.cpp
@@ -0,0 +1,34 @@
+// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+#include "fboss/agent/hw/sai/switch/SaiSwitchManager.h"
+
+#include "fboss/agent/hw/HwSwitchFb303Stats.h"
+
+namespace facebook::fboss {
+
+void fillHwSwitchDramStats(
+    const folly::F14FastMap<sai_stat_id_t, uint64_t>& counterId2Value,
+    HwSwitchDramStats& /*hwSwitchDramStats*/) {
+  CHECK_EQ(counterId2Value.size(), 0);
+}
+
+void fillHwSwitchWatermarkStats(
+    const folly::F14FastMap<sai_stat_id_t, uint64_t>& counterId2Value,
+    HwSwitchWatermarkStats& /*hwSwitchWatermarkStats*/) {
+  CHECK_EQ(counterId2Value.size(), 0);
+}
+
+void fillHwSwitchCreditStats(
+    const folly::F14FastMap<sai_stat_id_t, uint64_t>& counterId2Value,
+    HwSwitchCreditStats& /* hwSwitchCreditStats */) {
+  CHECK_EQ(counterId2Value.size(), 0);
+}
+
+void fillHwSwitchErrorStats(
+    const folly::F14FastMap<sai_stat_id_t, uint64_t>& counterId2Value,
+    HwSwitchDropStats& /*dropStats*/) {
+  CHECK_EQ(counterId2Value.size(), 0);
+}
+void switchPreInitSequence(HwAsic* /*asic*/) {}
+
+} // namespace facebook::fboss
