4d959f040dc2ce6eb5b408bee26cab2fc55e9517
Santhosh Nagaraj <santhoshn@meta.com>
2025-04-09 23:31:41 -0700

    Add a new benchmark for FSDB churn test
    Summary: As titled
    
    Differential Revision: D72012383
    
    fbshipit-source-id: d5257ebc741acfe3a00dfb6c4c0b801d607ed801

---
+++ fboss/fsdb/benchmarks/FsdbSystemScaleChurn.cpp
@@ -0,0 +1,57 @@
+// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+#include <folly/Benchmark.h>
+
+#include "fboss/fsdb/common/Flags.h"
+#include "fboss/fsdb/tests/utils/FsdbTestServer.h"
+#include "fboss/lib/CommonFileUtils.h"
+
+DEFINE_string(
+    write_agent_config_marker_for_fsdb,
+    "",
+    "Write marker file for FSDB");
+
+using facebook::fboss::fsdb::test::FsdbTestServer;
+
+namespace {
+
+const thriftpath::RootThriftPath<facebook::fboss::fsdb::FsdbOperStateRoot>
+    stateRoot;
+const std::vector<std::string> kPublishRoot{"agent"};
+
+} // anonymous namespace
+
+namespace facebook::fboss::fsdb::test {
+
+BENCHMARK(FsdbSystemScaleChurn) {
+  auto filePath =
+      folly::to<std::string>(FLAGS_write_agent_config_marker_for_fsdb);
+  auto port = FLAGS_fsdbPort;
+  std::unique_ptr<fsdb::test::FsdbTestServer> fsdbTestServer_ =
+      std::make_unique<FsdbTestServer>(port);
+
+  // Wait for publisher root agent to be available
+  while (true) {
+    auto md =
+        fsdbTestServer_->getPublisherRootMetadata(*kPublishRoot.begin(), false);
+    if (md.has_value()) {
+      break;
+    } else {
+      std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    }
+  }
+
+  // Once publisher root is available, wait until churn is complete
+  while (true) {
+    if (checkFileExists(filePath)) {
+      break;
+    } else {
+      std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    }
+  }
+
+  // Once churn is complete, remove the marker file and exit
+  fsdbTestServer_.reset();
+  removeFile(filePath, true);
+}
+} // namespace facebook::fboss::fsdb::test
