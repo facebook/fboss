253ba10479ecf0b937a0fed7234b6fcc32a9a8b7
Shrikrishna (Shri) Khare <skhare@meta.com>
2025-04-13 08:20:14 -0700

    Firmware obj is owned by adapter
    Summary:
    As titled.
    This is implicitly created by the adapter.
    
    Reviewed By: jasmeetbagga
    
    Differential Revision:
    D72919016
    
    Privacy Context Container: L1125642
    
    fbshipit-source-id: bdb86820735ab83e98208b840a683f12b3e6222e

--- fboss/agent/hw/sai/switch/npu/bcm/SaiFirmwareManager.cpp
+++ fboss/agent/hw/sai/switch/npu/bcm/SaiFirmwareManager.cpp
@@ -2,12 +2,33 @@
 
 #include "fboss/agent/hw/sai/switch/SaiFirmwareManager.h"
 
+#include "fboss/agent/hw/sai/store/SaiStore.h"
+#include "fboss/agent/hw/sai/switch/SaiManagerTable.h"
+#include "fboss/agent/hw/sai/switch/SaiSwitchManager.h"
+
 namespace facebook::fboss {
 
 SaiFirmwareManager::SaiFirmwareManager(
     SaiStore* saiStore,
     SaiManagerTable* managerTable,
     SaiPlatform* platform)
-    : saiStore_(saiStore), managerTable_(managerTable), platform_(platform) {}
+    : saiStore_(saiStore), managerTable_(managerTable), platform_(platform) {
+#if defined(BRCM_SAI_SDK_DNX_GTE_12_0)
+  SwitchSaiId switchId = managerTable_->switchManager().getSwitchSaiId();
+
+  auto firmwareObjectList =
+      SaiApiTable::getInstance()->switchApi().getAttribute(
+          switchId, SaiSwitchTraits::Attributes::FirmwareObjectList{});
+
+  CHECK_LE(firmwareObjectList.size(), 1);
+  if (firmwareObjectList.size() == 1) {
+    auto firmwareSaiId = *firmwareObjectList.begin();
+    XLOG(DBG2) << "Firmware OID: " << firmwareSaiId;
+    auto& store = saiStore_->get<SaiFirmwareTraits>();
+    store.loadObjectOwnedByAdapter(
+        SaiFirmwareTraits::AdapterKey{firmwareSaiId});
+  }
+#endif
+}
 
 } // namespace facebook::fboss
