d1961244989b51a671514748cc015f990615dff5
Santhosh Nagaraj <santhoshn@meta.com>
2025-04-24 10:10:44 -0700

    Refactor and remove agent marker file writing
    Summary: Removed the write marker file function. It has been refactor into the helper class in D72996699
    
    Differential Revision: D73374472
    
    fbshipit-source-id: a48e1ef18ec1f068b3c2653c3637e57bbf945ff0

--- fboss/agent/test/utils/SystemScaleTestUtils.cpp
+++ fboss/agent/test/utils/SystemScaleTestUtils.cpp
@@ -15,7 +15,6 @@
 #include "fboss/agent/test/utils/PortFlapHelper.h"
 #include "fboss/agent/test/utils/ScaleTestUtils.h"
 #include "fboss/agent/test/utils/TrapPacketUtils.h"
-#include "fboss/lib/CommonFileUtils.h"
 #include "folly/Benchmark.h"
 
 DECLARE_bool(intf_nbr_tables);
@@ -23,11 +22,6 @@ DECLARE_bool(json);
 DECLARE_int32(max_l2_entries);
 DECLARE_int32(max_ndp_entries);
 DECLARE_int32(max_arp_entries);
-
-DEFINE_string(
-    write_agent_config_marker_for_fsdb,
-    "",
-    "Write marker file for FSDB");
 
 namespace {
 constexpr uint64_t kBaseMac = 0xFEEEC2000010;
@@ -546,33 +540,11 @@ cfg::SwitchConfig getSystemScaleTestSwitchConfiguration(
   return config;
 };
 
-void writeAgentConfigMarkerForFsdb() {
-  auto filePath =
-      folly::to<std::string>(FLAGS_write_agent_config_marker_for_fsdb);
-
-  if (createFile(filePath) < 0) {
-    XLOG(DBG2) << "Failed to create file: " << filePath;
-    return;
-  }
-
-  while (true) {
-    if (!checkFileExists(filePath)) {
-      XLOG(DBG2) << "FSDB done with benchmarking and has deleted marker file";
-      break;
-    } else {
-      std::this_thread::sleep_for(std::chrono::seconds(1));
-    }
-  }
-}
-
 void initSystemScaleTest(AgentEnsemble* ensemble) {
   configureMaxAclEntries(ensemble);
   configureMaxRouteEntries(ensemble);
   configureMaxMacEntries(ensemble);
   configureMaxNeighborEntries(ensemble);
-  if (FLAGS_write_agent_config_marker_for_fsdb != "") {
-    writeAgentConfigMarkerForFsdb();
-  }
 }
 
 void initSystemScaleChurnTest(AgentEnsemble* ensemble) {
