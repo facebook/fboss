4219af568f34bc0bc28a0b9c8ad2dd7a87ca4940
Prasoon Patel <prasoon@meta.com>
2025-04-27 18:32:03 -0700

    define thrift call for HwUdfTests for agent hw test
    Summary: define and add thrift call for  HwUdfTests for agent hw test
    
    Reviewed By: msomasundaran
    
    Differential Revision: D70741550
    
    fbshipit-source-id: 2e74a0b948cb27e59e0b0f2c3c734a49cdec1678

--- fboss/agent/if/agent_hw_test_ctrl.thrift
+++ fboss/agent/if/agent_hw_test_ctrl.thrift
@@ -127,4 +127,16 @@ service AgentHwTestCtrl {
   bool getPtpTcEnabled();
 
   void clearInterfacePhyCounters(1: list<i32> portIds);
+
+  bool validateUdfConfig(1: string udfGroupName, 2: string udfPackeMatchName);
+  bool validateRemoveUdfGroup(1: string udfGroupName, 2: i32 udfGroupId);
+  bool validateRemoveUdfPacketMatcher(
+    1: string udfPackeMatchName,
+    2: i32 udfPacketMatcherId,
+  );
+  i32 getHwUdfGroupId(1: string udfGroupName);
+
+  i32 getHwUdfPacketMatcherId(1: string udfPacketMatchName);
+  bool validateUdfAclRoceOpcodeConfig(1: switch_state.SwitchState curState);
+  bool validateUdfIdsInQset(1: i32 aclGroupId, 2: bool isSet);
 }
