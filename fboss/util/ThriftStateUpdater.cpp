319d7604c5f723865646ebf40589ec9f2a832d8a
Paul Cruz <paulcruz74@meta.com>
2025-05-01 14:51:58 -0700

    Replace all instances of HeaderClientChannel::newChannel with (#418)
    Summary:
    
    This is for the purpose of testing a hypothesis that some internal difference
    between HeaderClientChannel and RocketClientChannel is causing high
    latency/hangs during thrift requests to FBOSS services in OSS.
    
    Differential Revision: D73672539

--- fboss/util/ThriftStateUpdater.cpp
+++ fboss/util/ThriftStateUpdater.cpp
@@ -52,7 +52,7 @@ std::unique_ptr<facebook::fboss::FbossCtrlAsyncClient> createAgentClient(
   auto sock = folly::AsyncSocket::newSocket(eb, addr, kConnTimeout);
   sock->setSendTimeout(kSendTimeout);
   auto channel =
-      apache::thrift::HeaderClientChannel::newChannel(std::move(sock));
+      apache::thrift::RocketClientChannel::newChannel(std::move(sock));
   channel->setTimeout(kRecvTimeout);
 
   return std::make_unique<facebook::fboss::FbossCtrlAsyncClient>(
