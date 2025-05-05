5b8bbe63301e1aca662efe35c74dbb406f1dec21
Parvez Shaikh <pshaikh@meta.com>
2025-05-04 18:27:59 -0700

    setup nullopt default for certain acl entry fields
    Summary:
    chenab doesn't support
    - outport in ingress acl table, let  nullopt default handle sdk not-supported error gracefully for outport
    - not all acl tables will have source or dest ip (due to limited key width in chenab's acl engine), let  nullopt default handles sdk not-supported error gracefully for outport
    
    Differential Revision:
    D74107452
    
    Privacy Context Container: L1125642
    
    fbshipit-source-id: a8577fe7cf3271103528b901078a254c3e6a4b2d

--- fboss/agent/hw/sai/api/AclApi.h
+++ fboss/agent/hw/sai/api/AclApi.h
@@ -351,19 +351,23 @@ struct SaiAclEntryTraits {
     using FieldSrcIpV6 = SaiAttribute<
         EnumType,
         SAI_ACL_ENTRY_ATTR_FIELD_SRC_IPV6,
-        AclEntryFieldIpV6>;
+        AclEntryFieldIpV6,
+        StdNullOptDefault<AclEntryFieldIpV6>>;
     using FieldDstIpV6 = SaiAttribute<
         EnumType,
         SAI_ACL_ENTRY_ATTR_FIELD_DST_IPV6,
-        AclEntryFieldIpV6>;
+        AclEntryFieldIpV6,
+        StdNullOptDefault<AclEntryFieldIpV6>>;
     using FieldSrcIpV4 = SaiAttribute<
         EnumType,
         SAI_ACL_ENTRY_ATTR_FIELD_SRC_IP,
-        AclEntryFieldIpV4>;
+        AclEntryFieldIpV4,
+        StdNullOptDefault<AclEntryFieldIpV4>>;
     using FieldDstIpV4 = SaiAttribute<
         EnumType,
         SAI_ACL_ENTRY_ATTR_FIELD_DST_IP,
-        AclEntryFieldIpV4>;
+        AclEntryFieldIpV4,
+        StdNullOptDefault<AclEntryFieldIpV4>>;
 #if defined(TAJO_SDK) || defined(CHENAB_SAI_SDK)
     using FieldSrcPort = SaiAttribute<
         EnumType,
@@ -378,7 +382,8 @@ struct SaiAclEntryTraits {
     using FieldOutPort = SaiAttribute<
         EnumType,
         SAI_ACL_ENTRY_ATTR_FIELD_OUT_PORT,
-        AclEntryFieldSaiObjectIdT>;
+        AclEntryFieldSaiObjectIdT,
+        StdNullOptDefault<AclEntryFieldSaiObjectIdT>>;
     using FieldL4SrcPort = SaiAttribute<
         EnumType,
         SAI_ACL_ENTRY_ATTR_FIELD_L4_SRC_PORT,
