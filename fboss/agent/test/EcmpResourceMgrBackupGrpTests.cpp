afd6b2e75d5c0f56ac94ca4688e7ccaaad887b39
Jasmeet Bagga <jasmeetbagga@meta.com>
2025-05-19 16:02:00 -0700

    Change backup switching mode and add prefixes that overflow ecmp limt
    Summary: As titled
    
    Reviewed By: msomasundaran
    
    Differential Revision: D74951153
    
    fbshipit-source-id: 5cb6010c2af8ff2e9f8d5d9199e4385aac22bc08

--- fboss/agent/test/EcmpResourceMgrBackupGrpTests.cpp
+++ fboss/agent/test/EcmpResourceMgrBackupGrpTests.cpp
@@ -683,4 +683,28 @@ TEST_F(EcmpBackupGroupTypeTest, changeSwitchingModeNoBackupEcmpRoutes) {
       cfg::SwitchingMode::FIXED_ASSIGNMENT);
 }
 
+TEST_F(EcmpBackupGroupTypeTest, overflowAndSwitchingModeChange) {
+  // Add new routes pointing to new nhops. ECMP limit is breached.
+  auto nhopSets = nextNhopSets();
+  auto oldState = state_;
+  auto newState = oldState->clone();
+  auto fib6 = fib(newState);
+  auto routesBefore = fib6->size();
+  std::set<RouteNextHopSet> nhops;
+  std::set<RouteV6::Prefix> overflowPrefixes;
+  for (auto i = 0; i < numStartRoutes(); ++i) {
+    auto route = makeRoute(makePrefix(routesBefore + i), nhopSets[i]);
+    overflowPrefixes.insert(route->prefix());
+    nhops.insert(nhopSets[i]);
+    fib6->addNode(route);
+  }
+  // Change backup ecmp switching mode
+  auto newFlowletSwitchingConfig =
+      newState->getFlowletSwitchingConfig()->modify(&newState);
+  newFlowletSwitchingConfig->setBackupSwitchingMode(
+      cfg::SwitchingMode::FIXED_ASSIGNMENT);
+  auto deltas = consolidate(newState);
+  EXPECT_EQ(deltas.size(), overflowPrefixes.size() + 1);
+  assertEndState(newState, overflowPrefixes);
+}
 } // namespace facebook::fboss
