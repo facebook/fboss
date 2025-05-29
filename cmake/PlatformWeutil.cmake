e6dec0ccb5bea68c2e37d18fea45c2f4d8bc2a55
Roman Choporov <romanc@meta.com>
2025-05-28 20:20:02 -0700

    Implement structurized V5 EEPROMs
    Summary: Introduce V5 EEPROM structure.
    
    Reviewed By: somasun
    
    Differential Revision: D73218509
    
    fbshipit-source-id: 58c69724d0f68b11bffe49eff9b9e13341d3877f

--- cmake/PlatformWeutil.cmake
+++ cmake/PlatformWeutil.cmake
@@ -18,6 +18,7 @@ add_library(weutil_crc16_ccitt_aug
 add_library(weutil_fboss_eeprom_parser
   fboss/platform/weutil/FbossEepromParserUtils.cpp
   fboss/platform/weutil/FbossEepromParser.cpp
+  fboss/platform/weutil/FbossEepromV5.cpp
 )
 
 target_link_libraries(weutil_fboss_eeprom_parser
