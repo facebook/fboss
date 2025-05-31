25a35be1a98604d41958e5f39a5b2f5728a8f86a
Shrikrishna (Shri) Khare <skhare@meta.com>
2025-05-30 17:27:45 -0700

    Refactor show fabric impl to .cpp
    Summary: As titled.
    
    Reviewed By: jasmeetbagga
    
    Differential Revision: D75414375
    
    fbshipit-source-id: 44d4b83304e39bd5e9afae770923e9db286d6c9e

--- cmake/CliFboss2.cmake
+++ cmake/CliFboss2.cmake
@@ -391,6 +391,7 @@ add_executable(fboss2
   fboss/cli/fboss2/commands/show/example/CmdShowExample.h
   fboss/cli/fboss2/commands/show/example/CmdShowExample.cpp
   fboss/cli/fboss2/commands/show/fabric/CmdShowFabric.h
+  fboss/cli/fboss2/commands/show/fabric/CmdShowFabric.cpp
   fboss/cli/fboss2/commands/show/fabric/reachability/CmdShowFabricReachability.h
   fboss/cli/fboss2/commands/show/fabric/reachability/uncached/CmdShowFabricReachabilityUncached.h
   fboss/cli/fboss2/commands/show/fabric/inputbalance/CmdShowFabricInputBalance.h
