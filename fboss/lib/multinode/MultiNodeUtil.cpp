d5036807d52829e7751170e27578c4d79f14c2ce
Shrikrishna (Shri) Khare <skhare@meta.com>
2025-06-02 12:23:14 -0700

    Start Creating Multi Node Util library
    Summary:
    Avoids code dup.
    Multi Node Tests as well as fboss2, and possibly other monitoring consumers can
    leverage this.
    
    Reviewed By: jasmeetbagga
    
    Differential Revision: D75740498
    
    fbshipit-source-id: 8531775e38438b8a7f146ade4bc5071e78ccb115

---
+++ fboss/lib/multinode/MultiNodeUtil.cpp
@@ -0,0 +1,31 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#include "fboss/lib/multinode/MultiNodeUtil.h"
+
+#include <thrift/lib/cpp2/async/RocketClientChannel.h>
+#include "common/network/NetworkUtil.h"
+
+namespace facebook::fboss::utility {
+
+std::unique_ptr<apache::thrift::Client<FbossCtrl>> getSwAgentThriftClient(
+    const std::string& switchName) {
+  folly::EventBase* eb = folly::EventBaseManager::get()->getEventBase();
+  auto remoteSwitchIp =
+      facebook::network::NetworkUtil::getHostByName(switchName);
+  folly::SocketAddress agent(remoteSwitchIp, 5909);
+  auto socket = folly::AsyncSocket::newSocket(eb, agent);
+  auto channel =
+      apache::thrift::RocketClientChannel::newChannel(std::move(socket));
+  return std::make_unique<apache::thrift::Client<FbossCtrl>>(
+      std::move(channel));
+}
+
+} // namespace facebook::fboss::utility
---
+++ fboss/lib/multinode/MultiNodeUtil.h
@@ -0,0 +1,20 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#pragma once
+
+#include <string>
+#include "fboss/agent/if/gen-cpp2/FbossCtrlAsyncClient.h"
+
+namespace facebook::fboss::utility {
+std::unique_ptr<apache::thrift::Client<FbossCtrl>> getSwAgentThriftClient(
+    const std::string& switchName);
+
+} // namespace facebook::fboss::utility
