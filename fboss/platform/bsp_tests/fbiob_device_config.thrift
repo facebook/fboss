15140ef33f27a2d2b3b889fd754fa8d8a6c47668
Scott Smith <smithscott@meta.com>
2025-06-09 16:15:29 -0700

    initial thrift config
    Summary:
    For use in C++ BSP Tests
    
    * BspTestsConfig is the config that vendors will provide which includes test data (e.g. expected values from reading certain i2c registers)
    * This is pretty much the same thing as the BspTestConfig from the python bsp tests, but I made a few changes to make it simpler to use/write.
    * Also provides `RuntimeConfig` which is the final config that the tests will use and takes from the platform_manager config
    * Adds `fbiob_device_config.thrift` which creates thrift structs for the IOCTL device specifications in `platform_manager/upai/fbiob-ioctl.h`
    
    Reviewed By: kimdo8736
    
    Differential Revision: D75629915
    
    fbshipit-source-id: 1a69b15d45ec136aca774755865ede530f7435de

---
+++ fboss/platform/bsp_tests/fbiob_device_config.thrift
@@ -0,0 +1,62 @@
+// Copyright (c) Meta Platforms, Inc. and affiliates.
+
+namespace cpp2 facebook.fboss.platform.bsp_tests.fbiob
+
+/*
+ * Corresponds to platform_manager/uapi/fbiob-ioctl.h
+ */
+
+struct AuxData {
+  1: string name;
+  2: AuxDeviceType type;
+  3: AuxID id;
+  4: optional string csrOffset;
+  5: optional string iobufOffset;
+  6: optional FanData fanData;
+  7: optional I2cData i2cData;
+  8: optional SpiData spiData;
+  9: optional LedData ledData;
+  10: optional XcvrData xcvrData;
+}
+
+struct AuxID {
+  1: string deviceName;
+  2: i32 id;
+}
+
+enum AuxDeviceType {
+  FAN = 0,
+  I2C = 1,
+  SPI = 2,
+  LED = 3,
+  XCVR = 4,
+}
+
+struct I2cData {
+  1: optional i32 busFreqHz;
+  2: optional i32 numChannels;
+}
+
+struct SpiDevInfo {
+  1: string modalias;
+  2: i32 chipSelect;
+  3: i32 maxSpeedHz;
+}
+
+struct SpiData {
+  1: i32 numSpidevs;
+  2: list<SpiDevInfo> spiDevices;
+}
+
+struct LedData {
+  1: i32 portNumber;
+  2: i32 ledId;
+}
+
+struct XcvrData {
+  1: i32 portNumber;
+}
+
+struct FanData {
+  1: i32 numFans;
+}
