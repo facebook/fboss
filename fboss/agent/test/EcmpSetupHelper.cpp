9901e572a67c762b341b3deeae4959e2ee7e1e96
Ron He <zecheng@meta.com>
2025-06-18 11:09:06 -0700

    Back out "Back out "[fboss] Fix unresolve nextHop for EcmpSetupHelper""
    Summary:
    Original commit changeset: 64f9c4ccc591
    
    Original Phabricator Diff: D76667952
    
    Second attempt to land the diff - In getRemoteSysPortDesc we should also skip static scope (global RCY port).
    
    Reviewed By: ashutoshgrewal
    
    Differential Revision: D76876330
    
    fbshipit-source-id: 9c84f8bbd5a9eaee9b9fbd0a40696035d0bd871a

--- fboss/agent/test/EcmpSetupHelper.cpp
+++ fboss/agent/test/EcmpSetupHelper.cpp
@@ -385,7 +385,10 @@ BaseEcmpSetupHelper<AddrT, NextHopT>::unresolveNextHop(
     const NextHopT& nhop,
     bool useLinkLocal) const {
   auto intfID = portDesc2Interface_.find(nhop.portDesc)->second;
-  auto intf = inputState->getInterfaces()->getNode(intfID);
+  auto intf = inputState->getInterfaces()->getNodeIf(intfID);
+  if (intf == nullptr) {
+    intf = inputState->getRemoteInterfaces()->getNodeIf(intfID);
+  }
   switch (intf->getType()) {
     case cfg::InterfaceType::VLAN:
       return unresolveVlanRifNextHop(inputState, nhop, intf, useLinkLocal);
