a67770cb109ad09e50ea52a816afb1ec45dab13e
Uchenna Mark Ezeobi <uezeobi@meta.com>
2025-06-18 10:58:36 -0700

    fw_util: Migrate fw_util CLI parsing from gflags to CLI11
    Summary: This diff migrates the fw_util CLI parsing from gflags to CLI11.
    
    Reviewed By: Scott8440
    
    Differential Revision: D76835222
    
    fbshipit-source-id: f537c6d6607a6df641687e766aa4428c4823a35b

--- fboss/platform/fw_util/fw_util.cpp
+++ fboss/platform/fw_util/fw_util.cpp
@@ -3,11 +3,11 @@
 #include <folly/logging/Init.h>
 #include <folly/logging/xlog.h>
 
+#include <CLI/CLI.hpp>
 #include <filesystem>
-#include "fboss/platform/fw_util/Flags.h"
+#include "common/base/BuildInfo.h"
 #include "fboss/platform/fw_util/FwUtilImpl.h"
 #include "fboss/platform/fw_util/fw_util_helpers.h"
-#include "fboss/platform/helpers/InitCli.h"
 
 using namespace facebook::fboss::platform::fw_util;
 using namespace facebook::fboss::platform;
@@ -20,61 +20,84 @@ using namespace facebook::fboss::platform;
 int main(int argc, char* argv[]) {
   // TODO: Add file lock to prevent multiple instance of fw_util from running
   // simultaneously.
+  folly::initLoggingOrDie();
+  CLI::App app{"Firmware utility for FBOSS platforms"};
+  std::string fw_target_name;
+  std::string fw_action;
+  std::string fw_binary_file;
+  bool verify_sha1sum = false;
+  bool dry_run = false;
 
-  helpers::initCli(&argc, &argv, "fw_util");
+  app.set_version_flag("--version", facebook::BuildInfo::toDebugString());
+  app.add_option(
+      "--fw_target_name",
+      fw_target_name,
+      "The fpd name that needs to be programmed");
+  app.add_option(
+      "--fw_action",
+      fw_action,
+      "The firmware action (program, verify, read, version, list, audit)");
+  app.add_option(
+      "--fw_binary_file",
+      fw_binary_file,
+      "The binary file to program (only required for 'program')");
+
+  app.add_flag(
+      "--verify_sha1sum", verify_sha1sum, "Verify SHA1 sum of the firmware");
+  app.add_flag(
+      "--dry_run", dry_run, "Dry run without actually programming firmware");
+
+  CLI11_PARSE(app, argc, argv);
 
   // TODO: To be removed once XFN change the commands in their codes
-  if (FLAGS_fw_action.empty()) {
+  if (fw_action.empty()) {
     if (argc > 1) {
-      FLAGS_fw_target_name = argv[1];
+      fw_target_name = argv[1];
     }
     if (argc > 2) {
-      FLAGS_fw_action = argv[2];
+      fw_action = argv[2];
     }
     if (argc > 3) {
-      FLAGS_fw_binary_file = argv[3];
+      fw_binary_file = argv[3];
     }
     XLOG(WARNING)
         << "Deprecation Warning: This current command line format will soon be deprecated.Please consider using fw_util --fw_target_name='binaryName' --fw_action='action' --fw_binary_file='path_to_file_to_be_upraded'";
   }
 
-  if (FLAGS_fw_action == "version" && !FLAGS_fw_binary_file.empty()) {
+  if (fw_action == "version" && !fw_binary_file.empty()) {
     XLOG(ERR) << "--fw_binary_file cannot be part of version command";
     exit(1);
   }
 
-  if (FLAGS_fw_action == "program" &&
-      (FLAGS_fw_target_name.empty() || FLAGS_fw_binary_file.empty())) {
+  if (fw_action == "program" &&
+      (fw_target_name.empty() || fw_binary_file.empty())) {
     XLOG(ERR)
         << "--fw_binary_file and --fw_target_name need to be specified for firmware programming";
     exit(1);
   }
 
   // Check for file existence only for actions that require a binary file
-  if (!FLAGS_fw_binary_file.empty() && FLAGS_fw_action != "read" &&
-      !std::filesystem::exists(FLAGS_fw_binary_file)) {
+  if (!fw_binary_file.empty() && fw_action != "read" &&
+      !std::filesystem::exists(fw_binary_file)) {
     XLOG(ERR) << "--fw_binary_file cannot be found in the specified path";
     exit(1);
   }
 
-  FwUtilImpl fwUtilImpl(
-      FLAGS_fw_binary_file, FLAGS_verify_sha1sum, FLAGS_dry_run);
+  FwUtilImpl fwUtilImpl(fw_binary_file, verify_sha1sum, dry_run);
 
-  if (FLAGS_fw_action == "version" && !FLAGS_fw_target_name.empty()) {
-    fwUtilImpl.printVersion(toLower(FLAGS_fw_target_name));
+  if (fw_action == "version" && !fw_target_name.empty()) {
+    fwUtilImpl.printVersion(toLower(fw_target_name));
   } else if (
-      FLAGS_fw_action == "program" || FLAGS_fw_action == "verify" ||
-      FLAGS_fw_action == "read") {
+      fw_action == "program" || fw_action == "verify" || fw_action == "read") {
     // For actions which involve more than just reading versions/config, we want
     // to always log all the commands that are run.
     folly::LoggerDB::get()
         .getCategory("fboss.platform.helpers.PlatformUtils")
         ->setLevel(folly::LogLevel::DBG2);
-    fwUtilImpl.doFirmwareAction(
-        toLower(FLAGS_fw_target_name), toLower(FLAGS_fw_action));
-  } else if (FLAGS_fw_action == "list") {
+    fwUtilImpl.doFirmwareAction(toLower(fw_target_name), toLower(fw_action));
+  } else if (fw_action == "list") {
     XLOG(INFO) << "supported Binary names are: " << fwUtilImpl.printFpdList();
-  } else if (FLAGS_fw_action == "audit") {
+  } else if (fw_action == "audit") {
     fwUtilImpl.doVersionAudit();
   } else {
     XLOG(ERR)
