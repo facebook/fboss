0a82a9c2be2e93bc52abbc0522e903e841ac003c
Shiva Menta <smenta@meta.com>
2025-06-25 07:38:54 -0700

    Change WedgeManagerInit to Use Switch Statement
    Summary: Simple refactor to switch statement for cleaner code. Split up from previous diff at request of reviewer.
    
    Reviewed By: xanabani
    
    Differential Revision: D76821629
    
    fbshipit-source-id: 969992e8434a8cc862f41549752cf4727f8e30be

--- fboss/qsfp_service/platforms/wedge/WedgeManagerInit.cpp
+++ fboss/qsfp_service/platforms/wedge/WedgeManagerInit.cpp
@@ -60,74 +60,71 @@ std::unique_ptr<WedgeManager> createWedgeManager() {
   }
 
   createDir(FLAGS_qsfp_service_volatile_dir);
-  if (mode == PlatformType::PLATFORM_WEDGE100) {
-    return std::make_unique<Wedge100Manager>(platformMapping, threads);
-  } else if (
-      mode == PlatformType::PLATFORM_GALAXY_LC ||
-      mode == PlatformType::PLATFORM_GALAXY_FC) {
-    return std::make_unique<GalaxyManager>(mode, platformMapping, threads);
-  } else if (mode == PlatformType::PLATFORM_YAMP) {
-    return createYampWedgeManager(platformMapping, threads);
-  } else if (
-      mode == PlatformType::PLATFORM_DARWIN ||
-      mode == PlatformType::PLATFORM_DARWIN48V) {
-    return createDarwinWedgeManager(platformMapping, threads);
-  } else if (mode == PlatformType::PLATFORM_ELBERT) {
-    return createElbertWedgeManager(platformMapping, threads);
-  } else if (mode == PlatformType::PLATFORM_MERU400BFU) {
-    return createBspWedgeManager<
-        Meru400bfuBspPlatformMapping,
-        PlatformType::PLATFORM_MERU400BFU>(platformMapping, threads);
-  } else if (mode == PlatformType::PLATFORM_MERU400BIA) {
-    return createBspWedgeManager<
-        Meru400biaBspPlatformMapping,
-        PlatformType::PLATFORM_MERU400BIA>(platformMapping, threads);
-  } else if (mode == PlatformType::PLATFORM_MERU400BIU) {
-    return createBspWedgeManager<
-        Meru400biuBspPlatformMapping,
-        PlatformType::PLATFORM_MERU400BIU>(platformMapping, threads);
-  } else if (
-      mode == PlatformType::PLATFORM_MERU800BIA ||
-      mode == PlatformType::PLATFORM_MERU800BIAB) {
-    return createBspWedgeManager<
-        Meru800biaBspPlatformMapping,
-        PlatformType::PLATFORM_MERU800BIA>(platformMapping, threads);
-  } else if (
-      mode == PlatformType::PLATFORM_MERU800BFA ||
-      mode == PlatformType::PLATFORM_MERU800BFA_P1) {
-    return createBspWedgeManager<
-        Meru800bfaBspPlatformMapping,
-        PlatformType::PLATFORM_MERU800BFA>(platformMapping, threads);
-  } else if (mode == PlatformType::PLATFORM_MONTBLANC) {
-    return createBspWedgeManager<
-        MontblancBspPlatformMapping,
-        PlatformType::PLATFORM_MONTBLANC>(platformMapping, threads);
-  } else if (mode == PlatformType::PLATFORM_MINIPACK3N) {
-    return createBspWedgeManager<
-        Minipack3NBspPlatformMapping,
-        PlatformType::PLATFORM_MINIPACK3N>(platformMapping, threads);
-  } else if (mode == PlatformType::PLATFORM_MORGAN800CC) {
-    return createBspWedgeManager<
-        Morgan800ccBspPlatformMapping,
-        PlatformType::PLATFORM_MORGAN800CC>(platformMapping, threads);
-  } else if (mode == PlatformType::PLATFORM_WEDGE400C) {
-    return std::make_unique<Wedge400CManager>(platformMapping, threads);
-  } else if (mode == PlatformType::PLATFORM_JANGA800BIC) {
-    return createBspWedgeManager<
-        Janga800bicBspPlatformMapping,
-        PlatformType::PLATFORM_JANGA800BIC>(platformMapping, threads);
-  } else if (mode == PlatformType::PLATFORM_TAHAN800BC) {
-    return createBspWedgeManager<
-        Tahan800bcBspPlatformMapping,
-        PlatformType::PLATFORM_TAHAN800BC>(platformMapping, threads);
-  } else if (
-      mode == PlatformType::PLATFORM_FUJI ||
-      mode == PlatformType::PLATFORM_MINIPACK ||
-      mode == PlatformType::PLATFORM_WEDGE400) {
-    return createFBWedgeManager(
-        std::move(productInfo), platformMapping, threads);
+  switch (mode) {
+    case PlatformType::PLATFORM_WEDGE100:
+      return std::make_unique<Wedge100Manager>(platformMapping, threads);
+    case PlatformType::PLATFORM_GALAXY_LC:
+    case PlatformType::PLATFORM_GALAXY_FC:
+      return std::make_unique<GalaxyManager>(mode, platformMapping, threads);
+    case PlatformType::PLATFORM_YAMP:
+      return createYampWedgeManager(platformMapping, threads);
+    case PlatformType::PLATFORM_DARWIN:
+    case PlatformType::PLATFORM_DARWIN48V:
+      return createDarwinWedgeManager(platformMapping, threads);
+    case PlatformType::PLATFORM_ELBERT:
+      return createElbertWedgeManager(platformMapping, threads);
+    case PlatformType::PLATFORM_MERU400BFU:
+      return createBspWedgeManager<
+          Meru400bfuBspPlatformMapping,
+          PlatformType::PLATFORM_MERU400BFU>(platformMapping, threads);
+    case PlatformType::PLATFORM_MERU400BIA:
+      return createBspWedgeManager<
+          Meru400biaBspPlatformMapping,
+          PlatformType::PLATFORM_MERU400BIA>(platformMapping, threads);
+    case PlatformType::PLATFORM_MERU400BIU:
+      return createBspWedgeManager<
+          Meru400biuBspPlatformMapping,
+          PlatformType::PLATFORM_MERU400BIU>(platformMapping, threads);
+    case PlatformType::PLATFORM_MERU800BIA:
+    case PlatformType::PLATFORM_MERU800BIAB:
+      return createBspWedgeManager<
+          Meru800biaBspPlatformMapping,
+          PlatformType::PLATFORM_MERU800BIA>(platformMapping, threads);
+    case PlatformType::PLATFORM_MERU800BFA:
+    case PlatformType::PLATFORM_MERU800BFA_P1:
+      return createBspWedgeManager<
+          Meru800bfaBspPlatformMapping,
+          PlatformType::PLATFORM_MERU800BFA>(platformMapping, threads);
+    case PlatformType::PLATFORM_MONTBLANC:
+      return createBspWedgeManager<
+          MontblancBspPlatformMapping,
+          PlatformType::PLATFORM_MONTBLANC>(platformMapping, threads);
+    case PlatformType::PLATFORM_MINIPACK3N:
+      return createBspWedgeManager<
+          Minipack3NBspPlatformMapping,
+          PlatformType::PLATFORM_MINIPACK3N>(platformMapping, threads);
+    case PlatformType::PLATFORM_MORGAN800CC:
+      return createBspWedgeManager<
+          Morgan800ccBspPlatformMapping,
+          PlatformType::PLATFORM_MORGAN800CC>(platformMapping, threads);
+    case PlatformType::PLATFORM_WEDGE400C:
+      return std::make_unique<Wedge400CManager>(platformMapping, threads);
+    case PlatformType::PLATFORM_JANGA800BIC:
+      return createBspWedgeManager<
+          Janga800bicBspPlatformMapping,
+          PlatformType::PLATFORM_JANGA800BIC>(platformMapping, threads);
+    case PlatformType::PLATFORM_TAHAN800BC:
+      return createBspWedgeManager<
+          Tahan800bcBspPlatformMapping,
+          PlatformType::PLATFORM_TAHAN800BC>(platformMapping, threads);
+    case PlatformType::PLATFORM_FUJI:
+    case PlatformType::PLATFORM_MINIPACK:
+    case PlatformType::PLATFORM_WEDGE400:
+      return createFBWedgeManager(
+          std::move(productInfo), platformMapping, threads);
+    default:
+      return std::make_unique<Wedge40Manager>(platformMapping, threads);
   }
-  return std::make_unique<Wedge40Manager>(platformMapping, threads);
 }
 
 template <typename BspPlatformMapping, PlatformType platformType>
