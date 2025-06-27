2e52e6c0c58b78af4f22004ddc7e6a6f7006d1bb
Midhun Somasundaran <msomasundaran@meta.com>
2025-06-27 10:50:56 -0700

    Use normalized nexthops as key in resource accountant
    Summary: Normalized nexthops is what we use for key in hwswitch ecmp manager. Resource accountant should also use normalized nexthops as key since it is tracking hw resource usage. This becomes important in NSF because we have topology information in regular nexthop which is not used in hw key and resource accountant will compute may end up not sharing ecmps when topology information is present.
    
    Reviewed By: zechengh09
    
    Differential Revision: D77396971
    
    fbshipit-source-id: ec307c5811b533ca4903b8b23a31131f0bd8da4d

--- fboss/agent/ResourceAccountant.cpp
+++ fboss/agent/ResourceAccountant.cpp
@@ -145,7 +145,7 @@ bool ResourceAccountant::checkAndUpdateGenericEcmpResource(
   // Forwarding to nextHops and more than one nextHop - use ECMP
   if (fwd.getAction() == RouteForwardAction::NEXTHOPS &&
       fwd.getNextHopSet().size() > 1) {
-    const auto& nhSet = fwd.getNextHopSet();
+    const auto& nhSet = fwd.normalizedNextHops();
     if (auto it = ecmpGroupRefMap_.find(nhSet); it != ecmpGroupRefMap_.end()) {
       it->second = it->second + (add ? 1 : -1);
       CHECK(it->second >= 0);
