cbd7ab5e615df2eb28e717f0f411a21cb393c2d5
Ron He <zecheng@meta.com>
2025-07-07 17:42:45 -0700

    Define the interface for PreUpdateStateModifier
    Summary:
    As titled. This interface will be used for EcmpResourceManager, as well as PortShelManager (which will enable self-healing ECMP if there is ECMP next hop towards intf port).
    
    Given a list of state delta, the PreUpdateStateModifier could modify state deltas or split out multiple state delta from one.
    
    Differential Revision: D76093658
    
    fbshipit-source-id: b60aaad4adf76ca9d067901b51ae362b3d1ec355

---
+++ fboss/agent/PreUpdateStateModifier.h
@@ -0,0 +1,25 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+#pragma once
+
+#include <boost/core/noncopyable.hpp>
+
+#include "fboss/agent/state/StateDelta.h"
+
+namespace facebook::fboss {
+
+class PreUpdateStateModifier : public boost::noncopyable {
+ public:
+  virtual ~PreUpdateStateModifier() {}
+  virtual std::vector<StateDelta> modifyState(
+      const std::vector<StateDelta>& deltas) = 0;
+};
+
+} // namespace facebook::fboss
