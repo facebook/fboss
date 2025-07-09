9a4301827f74fc5c38de457ff427c2961db250b9
Mohammed Al-Sanabani <sanabani@meta.com>
2025-07-09 11:31:19 -0700

    Adding LPO Transceiver Tests
    Summary:
    Adding Gtests (with EEPROM dumped from LPO Module).
    Note that This will be updated once we have modules that support 1x800G mode.
    
    Reviewed By: harshitgulati18
    
    Differential Revision: D77925223
    
    fbshipit-source-id: 2c73eec1ff8f9fcf66a8d24fa3d671d457663831

--- fboss/qsfp_service/module/oss/CmisModule.cpp
+++ fboss/qsfp_service/module/oss/CmisModule.cpp
@@ -1,5 +1,6 @@
 #include "fboss/qsfp_service/module/cmis/CmisModule.h"
 
+// TODO: delete this file once T230016502 is resolved.
 namespace facebook {
 namespace fboss {
 bool CmisModule::isLpoModule() const {
