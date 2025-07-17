3b436a9290504cf723c2aafa124090ba3d66483a
Roman Choporov <romanc@meta.com>
2025-07-16 17:36:29 -0700

    Update weutil with valid FRU EEPROM format versions
    Summary: Invalidate V4 EEPROM version since it's not relevant anymore.
    
    Reviewed By: somasun
    
    Differential Revision: D77262404
    
    fbshipit-source-id: dc47daf4101325ab96682e80627069181da71e35

--- fboss/platform/weutil/if/weutil_config.thrift
+++ fboss/platform/weutil/if/weutil_config.thrift
@@ -4,25 +4,12 @@ typedef string FruEepromPath
 typedef string FruEepromName
 typedef i32 FruIdOffset
 
-enum IdEepromFormatVersion {
-  // Reserved for prefdl, backwards compatibility
-  V0 = 0,
-  // Meta ID Eeprom Format Version 3
-  V3 = 3,
-  // Meta ID Eeprom Format Version 4
-  V4 = 4,
-  // Meta ID Eeprom Format Version 5
-  V5 = 5,
-}
-
 struct FruEepromConfig {
   // the unique path of the FRU ID EEPROM
   // e.g. "/run/devmap/eeproms/FANSPINNER_EEPROM"
   1: FruEepromPath path;
   // FRU ID section offset in the EEPROM, default is 0
   2: FruIdOffset offset = 0;
-  // ID EEPROM format version
-  3: IdEepromFormatVersion idEepromFormatVer = V4;
 }
 
 struct WeutilConfig {
