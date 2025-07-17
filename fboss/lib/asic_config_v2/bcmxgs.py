9ff39b9cf307d4675e7d99361719a65e40c70d03
Shiva Menta <smenta@meta.com>
2025-07-17 09:36:31 -0700

    Copy Over All TH6 from Cfgr and Fix Dependencies
    Summary:
    Pure file copy from `configerator/source/neteng/fboss/asic_config_v2` + rename from cinc/cconf to py. I only copied files needed to enable TH6 asic config generation, we can copy over other asic support as needed once we figure out if we want to fully migrate asic config gen to fbcode.
    
    Diffing Views (fbcode vs. cfgr):
    ‎fbcode‎/fboss‎/lib‎/asic_config_v2‎/asic_config.py‎: https://www.internalfb.com/intern/diffing/?paste_number=1871921665
    ‎fbcode‎/fboss‎/lib‎/asic_config_v2‎/bcm.py‎: https://www.internalfb.com/intern/diffing/?paste_number=1871922142
    ‎fbcode‎/fboss‎/lib‎/asic_config_v2‎/bcmxgs.py‎: https://www.internalfb.com/intern/diffing/?paste_number=1871927884
    ‎fbcode‎/fboss‎/lib‎/asic_config_v2‎/icecube800bc.py‎: https://www.internalfb.com/intern/diffing/?paste_number=1871927156
    ‎fbcode‎/fboss‎/lib‎/asic_config_v2‎/icecube800bc.yml‎: https://www.internalfb.com/intern/diffing/?paste_number=1871926149
    ‎fbcode‎/fboss‎/lib‎/asic_config_v2‎/tomahawk6.py‎: https://www.internalfb.com/intern/diffing/?paste_number=1871925079
    
    Reviewed By: harshitgulati18
    
    Differential Revision: D77596582
    
    fbshipit-source-id: 624f1ac2f1a25e63b384589756860b847c116fb5

---
+++ fboss/lib/asic_config_v2/bcmxgs.py
@@ -0,0 +1,81 @@
+# pyre-strict
+from typing import List, Optional
+
+import neteng.fboss.asic_config_v2.thrift_types as asic_config_thrift
+import neteng.fboss.platform_mapping_config.ttypes as pm_types
+
+from fboss.lib.asic_config_v2.bcm import BcmAsicConfig
+from fboss.lib.platform_mapping_v2.asic_vendor_config import AsicVendorConfig
+
+
+# Common FBOSS bcm configs to all platforms
+BCM_CFG_COMMON = {
+    # Fix the ATOM CPU hung upon PCIe timeout bug. context in D25261964
+    "pcie_host_intf_timeout_purge_enable": "0",
+    # Fix BcmQosMapTest.BcmAllQosMaps failure. context in D26037426
+    "qos_map_multi_get_mode": "1",
+    # Fix HwHashTrunk8x3PolarizationTests.FullHalfxFullHalf failure.
+    # Context in D40313640.
+    "macro_flow_hash_shuffle_random_seed": "34345645",
+    "bcm_linkscan_interval": "25000",
+}
+
+# Common SAI configs added to all platforms
+SAI_CFG_COMMON = {
+    "sai_common_hash_crc": "0x1",
+    "sai_disable_srcmacqedstmac_ctrl": "0x1",
+    "sai_acl_qset_optimization": "0x1",
+    "sai_optimized_mmu": "0x1",
+    "sai_pkt_rx_custom_cfg": "1",
+    "sai_pkt_rx_pkt_size": "16512",
+    "sai_pkt_rx_cfg_ppc": "16",
+    "sai_async_fdb_nbr_enable": "0x1",
+    "sai_pfc_defaults_disable": "0x1",
+    "sai_ifp_enable_on_cpu_tx": "0x1",
+    "sai_vfp_smac_drop_filter_disable": "1",
+    "sai_macro_flow_based_hash": "1",
+    "sai_mmu_qgroups_default": "1",
+    "sai_dis_ctr_incr_on_port_ln_dn": "0",  # CS00012279422
+    "custom_feature_mesh_topology_sync_mode": "1",
+    "sai_ecmp_group_members_increment": "1",  # CS00012255302
+}
+
+
+class BcmXgsAsicConfig(BcmAsicConfig):
+    def __init__(
+        self, asic_config_params: asic_config_thrift.AsicConfigParameters
+    ) -> None:
+        super(BcmXgsAsicConfig, self).__init__(asic_config_params)
+
+    def get_asic_vendor_config(self) -> AsicVendorConfig:
+        raise NotImplementedError("Asic Vendor Config not provided.")
+
+    def get_static_mapping(self) -> pm_types.StaticMapping:
+        raise NotImplementedError("Platform Mapping not provided.")
+
+    def generate_yaml_string(self, preamble: Optional[str] = None) -> str:
+        raise NotImplementedError("generate_yaml_string() not defined")
+
+    def get_lane_map_str(self, lane_map: List[int]) -> str:
+        lane_map_prefix = "0x"
+        lane_map_str = ""
+        for lane in lane_map:
+            lane_map_str += str(lane)
+        return lane_map_prefix + lane_map_str[::-1]
+
+    def get_polarity_map_str(self, pn_map: List[int]) -> str:
+        pn_map_str = ""
+        for pn in pn_map:
+            pn_map_str += str(pn)
+        pn_map_str = pn_map_str[::-1]
+        # polarity maps are binary strings which need to be converted
+        # to hex values. For eg, a binary string "01011100" will be
+        # converted to 0x5C.
+        pn_map_hex = "0x" + "{:02x}".format(int(pn_map_str, 2)).upper()
+        return pn_map_hex
+
+    def export(self, preamble: Optional[str] = None) -> asic_config_thrift.AsicConfig:
+        configEntry = asic_config_thrift.AsicConfigEntry(
+            yamlConfig=self.generate_yaml_string(preamble=preamble)
+        )
+        return asic_config_thrift.AsicConfig(common=configEntry)
---
+++ fboss/lib/asic_config_v2/icecube800bc.py
@@ -0,0 +1,94 @@
+# pyre-strict
+import neteng.fboss.asic_config_v2.thrift_types as asic_config_thrift
+import neteng.fboss.platform_mapping_config.ttypes as pm_types
+from fboss.lib.asic_config_v2.tomahawk6 import Tomahawk6AsicConfig
+from fboss.lib.platform_mapping_v2.asic_vendor_config import AsicVendorConfig
+from fboss.lib.platform_mapping_v2.gen import read_all_vendor_data
+from fboss.lib.platform_mapping_v2.platform_mapping_v2 import PlatformMappingParser
+
+
+class Icecube800bcAsicConfig(Tomahawk6AsicConfig):
+    def __init__(
+        self, asic_config_params: asic_config_thrift.AsicConfigParameters
+    ) -> None:
+        super(Icecube800bcAsicConfig, self).__init__(asic_config_params)
+        self.parser = PlatformMappingParser(read_all_vendor_data(), "icecube800bc")
+        self.num_ports_per_core = 2
+
+    def get_asic_vendor_config(self) -> AsicVendorConfig:
+        asic_vendor_config = self.parser.get_asic_vendor_config()
+        if not asic_vendor_config:
+            raise Exception("No asic vendor config found.")
+        return asic_vendor_config
+
+    def get_static_mapping(self) -> pm_types.StaticMapping:
+        return self.parser.get_static_mapping().get_static_mapping()
+
+    def generate_port_config(self, mgmt_port: bool = False) -> None:
+        # TODO: update these default settings if necessary, currently taken from montblanc
+        # We also need to enable CPU port
+        self.values["PC_PORT"][("PORT_ID: {}".format(0),)] = (
+            "ENABLE: 1",
+            "SPEED: 10000",
+            "NUM_LANES: 1",
+        )
+        speed_to_fec = {
+            100000: "PC_FEC_RS544",
+            200000: "PC_FEC_RS544_2XN",
+            400000: "PC_FEC_RS544_2XN",
+            800000: "PC_FEC_RS544_2XN",
+        }
+        speed = 800000
+        port_ranges = []
+        for lp_map in self.get_logical_port_to_physical_port_mapping():
+            port_ranges.append("[{}, {}]".format(lp_map[0], lp_map[0]))
+        pc_key = ("PORT_ID: [{}]".format(", ".join(port_ranges)),)
+        pc_value = (
+            "ENABLE: 0",
+            "SPEED: {}".format(speed),
+            "NUM_LANES: {}".format(4),
+            "FEC_MODE: {}".format(speed_to_fec[speed]),
+            "MAX_FRAME_SIZE: {}".format(self.MMU_SIZE),
+        )
+        self.values["PC_PORT"][pc_key] = pc_value
+        if mgmt_port:
+            mgmt_port_key = ("PORT_ID: {}".format(self.MGMT_PORT_LOGICAL_ID),)
+            mgmt_port_value = (
+                "ENABLE: 0",
+                "SPEED: {}".format(self.MGMT_PORT_SPEED),
+                "NUM_LANES: {}".format(self.MGMT_PORT_NUM_LANES),
+                "FEC_MODE: {}".format(self.MGMT_PORT_FEC),
+                "MAX_FRAME_SIZE: {}".format(self.MMU_SIZE),
+            )
+            self.values["PC_PORT"][mgmt_port_key] = mgmt_port_value
+
+        # add MTU check config so that such ports can drop packets larger
+        # than MTU size as previous platforms.
+        # The default Egress MTU on TH4 is 16K.
+        self.values["PORT"][pc_key] = (
+            "MTU: {}".format(self.MMU_SIZE),
+            "MTU_CHECK: 1",
+        )
+
+    def generate_asic_config(self) -> None:
+        # TODO: update if necessary, same as montblanc for now
+        self.generate_global_settings()
+        self.generate_logical_port_to_physical_port_mapping(mgmt_port=True)
+        self.generate_lane_map()
+        self.generate_polarity_map()
+        self.generate_port_config(mgmt_port=True)
+        self.generate_autoload_board_settings()
+        self.generate_flex_counter_settings()
+        self.generate_asic_vendor_config()
+
+
+def gen_icecube800bc_asic_config() -> Icecube800bcAsicConfig:
+    asic_config_params = asic_config_thrift.AsicConfigParameters(
+        configType=asic_config_thrift.AsicConfigType.YAML_CONFIG,
+        exactMatch=False,
+        mmuLossless=False,
+        configGenType=asic_config_thrift.AsicConfigGenType.DEFAULT,
+    )
+    cfg = Icecube800bcAsicConfig(asic_config_params)
+    cfg.generate_asic_config()
+    return cfg
