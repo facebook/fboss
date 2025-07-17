74b220805b7e6c7df0a905bf2d014a329c28e861
Manikandan Somasundaram <manikandan@meta.com>
2025-07-17 14:31:24 -0700

    Add cmake rules for showtech binary
    Reviewed By: RomanChoporov-Meta
    
    Differential Revision: D78508370
    
    fbshipit-source-id: b1480cb4ca4be5941ef7317fb3f5ca1ad8a45194

--- cmake/PlatformShowtech.cmake
+++ cmake/PlatformShowtech.cmake
@@ -1,3 +1,8 @@
+# Make to build libraries and binaries in fboss/platform/showtech
+
+# In general, libraries and binaries in fboss/foo/bar are built by
+# cmake/FooBar.cmake
+
 add_fbthrift_cpp_library(
   showtech_config_cpp2
   fboss/platform/showtech/showtech_config.thrift
@@ -5,3 +10,17 @@ add_fbthrift_cpp_library(
     json
     reflection
 )
+
+add_executable(showtech
+  fboss/platform/showtech/Main.cpp
+  fboss/platform/showtech/Utils.cpp
+)
+
+target_link_libraries(showtech
+  fmt::fmt
+  fb303::fb303
+  platform_utils
+  showtech_config_cpp2
+)
+
+install(TARGETS showtech)
