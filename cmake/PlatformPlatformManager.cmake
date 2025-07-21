998d017aa8c4dbb3679612ea9d9f5f448dcbf1e4
Manikandan Somasundaram <manikandan@meta.com>
2025-07-21 10:58:51 -0700

    Simplify platform_manager build rules
    Summary:
    These cpp files get compiled in other libraries and are not needed
    here
    
    Reviewed By: Scott8440
    
    Differential Revision: D78586611
    
    fbshipit-source-id: 589af41a83313f027a61bd4b817bd5f88acfef2d

--- cmake/PlatformPlatformManager.cmake
+++ cmake/PlatformPlatformManager.cmake
@@ -186,14 +186,7 @@ target_link_libraries(platform_manager_handler
 )
 
 add_executable(platform_manager
-  fboss/platform/platform_manager/DataStore.cpp
-  fboss/platform/platform_manager/I2cExplorer.cpp
   fboss/platform/platform_manager/Main.cpp
-  fboss/platform/platform_manager/PciExplorer.cpp
-  fboss/platform/platform_manager/PlatformExplorer.cpp
-  fboss/platform/platform_manager/DevicePathResolver.cpp
-  fboss/platform/platform_manager/PresenceChecker.cpp
-  fboss/platform/platform_manager/ExplorationSummary.cpp
 )
 
 target_link_libraries(platform_manager
@@ -207,6 +200,7 @@ target_link_libraries(platform_manager
   platform_manager_config_validator
   platform_manager_handler
   platform_manager_pkg_manager
+  platform_manager_platform_explorer
   platform_manager_presence_cpp2
   platform_manager_service_cpp2
   platform_manager_snapshot_cpp2
