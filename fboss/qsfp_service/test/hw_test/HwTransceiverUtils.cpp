0377edc11d022c24015021b55094365adfdfb4fa
generatedunixname1030151775592158 <noreply+1030151775592158@fb.com>
2025-07-23 16:16:36 -0700

    ThriftRemoveRefSuffix_fboss_qsfp_service_test
    Reviewed By: dtolnay
    
    Differential Revision: D78801283
    
    fbshipit-source-id: cb04a1e72bf9260a876c259b2e1f80f37de2eb98

--- fboss/qsfp_service/test/hw_test/HwTransceiverUtils.cpp
+++ fboss/qsfp_service/test/hw_test/HwTransceiverUtils.cpp
@@ -388,9 +388,9 @@ void HwTransceiverUtils::verify10gProfile(
 
   for (const auto& mediaId : mediaInterfaces) {
     EXPECT_TRUE(
-        *mediaId.media()->ethernet10GComplianceCode_ref() ==
+        *mediaId.media()->ethernet10GComplianceCode() ==
             Ethernet10GComplianceCode::LR_10G ||
-        *mediaId.media()->ethernet10GComplianceCode_ref() ==
+        *mediaId.media()->ethernet10GComplianceCode() ==
             Ethernet10GComplianceCode::CR_10G);
 
     EXPECT_TRUE(
@@ -405,7 +405,7 @@ void HwTransceiverUtils::verify25gProfile(
   for (const auto& mediaId : mediaInterfaces) {
     EXPECT_EQ(mgmtInterface, TransceiverManagementInterface::SFF);
     auto specComplianceCode =
-        *mediaId.media()->extendedSpecificationComplianceCode_ref();
+        *mediaId.media()->extendedSpecificationComplianceCode();
     EXPECT_EQ(specComplianceCode, ExtendedSpecComplianceCode::CWDM4_100G);
   }
 }
@@ -416,7 +416,7 @@ void HwTransceiverUtils::verify50gProfile(
   for (const auto& mediaId : mediaInterfaces) {
     EXPECT_EQ(mgmtInterface, TransceiverManagementInterface::SFF);
     auto specComplianceCode =
-        *mediaId.media()->extendedSpecificationComplianceCode_ref();
+        *mediaId.media()->extendedSpecificationComplianceCode();
     EXPECT_EQ(specComplianceCode, ExtendedSpecComplianceCode::CWDM4_100G);
   }
 }
@@ -427,7 +427,7 @@ void HwTransceiverUtils::verify100gProfile(
   for (const auto& mediaId : mediaInterfaces) {
     if (mgmtInterface == TransceiverManagementInterface::SFF) {
       auto specComplianceCode =
-          *mediaId.media()->extendedSpecificationComplianceCode_ref();
+          *mediaId.media()->extendedSpecificationComplianceCode();
       EXPECT_TRUE(
           specComplianceCode == ExtendedSpecComplianceCode::CWDM4_100G ||
           specComplianceCode == ExtendedSpecComplianceCode::FR1_100G ||
@@ -438,9 +438,8 @@ void HwTransceiverUtils::verify100gProfile(
           mediaId.code() == MediaInterfaceCode::CR4_100G);
     } else if (mgmtInterface == TransceiverManagementInterface::CMIS) {
       EXPECT_TRUE(
-          *mediaId.media()->smfCode_ref() ==
-              SMFMediaInterfaceCode::CWDM4_100G ||
-          *mediaId.media()->smfCode_ref() == SMFMediaInterfaceCode::FR1_100G);
+          *mediaId.media()->smfCode() == SMFMediaInterfaceCode::CWDM4_100G ||
+          *mediaId.media()->smfCode() == SMFMediaInterfaceCode::FR1_100G);
       EXPECT_TRUE(
           *mediaId.code() == MediaInterfaceCode::CWDM4_100G ||
           *mediaId.code() == MediaInterfaceCode::FR1_100G);
@@ -455,9 +454,9 @@ void HwTransceiverUtils::verify200gProfile(
 
   for (const auto& mediaId : mediaInterfaces) {
     EXPECT_TRUE(
-        *mediaId.media()->smfCode_ref() == SMFMediaInterfaceCode::FR4_200G ||
-        *mediaId.media()->smfCode_ref() == SMFMediaInterfaceCode::LR4_200G ||
-        *mediaId.media()->smfCode_ref() == SMFMediaInterfaceCode::DR1_200G);
+        *mediaId.media()->smfCode() == SMFMediaInterfaceCode::FR4_200G ||
+        *mediaId.media()->smfCode() == SMFMediaInterfaceCode::LR4_200G ||
+        *mediaId.media()->smfCode() == SMFMediaInterfaceCode::DR1_200G);
     EXPECT_TRUE(
         *mediaId.code() == MediaInterfaceCode::FR4_200G ||
         *mediaId.code() == MediaInterfaceCode::LR4_200G ||
@@ -472,9 +471,9 @@ void HwTransceiverUtils::verify400gProfile(
 
   for (const auto& mediaId : mediaInterfaces) {
     EXPECT_TRUE(
-        *mediaId.media()->smfCode_ref() == SMFMediaInterfaceCode::FR4_400G ||
-        *mediaId.media()->smfCode_ref() == SMFMediaInterfaceCode::LR4_10_400G ||
-        *mediaId.media()->smfCode_ref() == SMFMediaInterfaceCode::DR4_400G);
+        *mediaId.media()->smfCode() == SMFMediaInterfaceCode::FR4_400G ||
+        *mediaId.media()->smfCode() == SMFMediaInterfaceCode::LR4_10_400G ||
+        *mediaId.media()->smfCode() == SMFMediaInterfaceCode::DR4_400G);
     EXPECT_TRUE(
         *mediaId.code() == MediaInterfaceCode::FR4_400G ||
         *mediaId.code() == MediaInterfaceCode::LR4_400G_10KM ||
@@ -541,8 +540,8 @@ void HwTransceiverUtils::verifyOptical800gProfile(
   EXPECT_EQ(mgmtInterface, TransceiverManagementInterface::CMIS);
   for (const auto& mediaId : mediaInterfaces) {
     EXPECT_TRUE(
-        *mediaId.media()->smfCode_ref() == SMFMediaInterfaceCode::FR8_800G ||
-        *mediaId.media()->smfCode_ref() == SMFMediaInterfaceCode::DR4_800G);
+        *mediaId.media()->smfCode() == SMFMediaInterfaceCode::FR8_800G ||
+        *mediaId.media()->smfCode() == SMFMediaInterfaceCode::DR4_800G);
     EXPECT_TRUE(
         *mediaId.code() == MediaInterfaceCode::FR8_800G ||
         *mediaId.code() == MediaInterfaceCode::DR4_800G);
@@ -555,7 +554,7 @@ void HwTransceiverUtils::verifyActiveCopper800gProfile(
   EXPECT_EQ(mgmtInterface, TransceiverManagementInterface::CMIS);
   for (const auto& mediaId : mediaInterfaces) {
     EXPECT_TRUE(
-        *mediaId.media()->activeCuCode_ref() ==
+        *mediaId.media()->activeCuCode() ==
         ActiveCuHostInterfaceCode::AUI_PAM4_8S_800G);
     EXPECT_TRUE(*mediaId.code() == MediaInterfaceCode::CR8_800G);
   }
