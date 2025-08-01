6867317afea6500a52fe9b1e842520f69d4a6982
Parvez Shaikh <pshaikh@meta.com>
2025-08-01 09:17:08 -0700

    skip setting unsupported serdes attributes
    Summary:
    as titled
    
    post2 and post3 are unimplemented
    
    Differential Revision: D79409458
    
    fbshipit-source-id: d1aaba8a25979cc9a9393ad6b305365f0d13701e

--- fboss/agent/hw/sai/switch/npu/SaiPortManager.cpp
+++ fboss/agent/hw/sai/switch/npu/SaiPortManager.cpp
@@ -1276,8 +1276,16 @@ SaiPortManager::serdesAttributesFromSwPinConfigs(
       platform_->getAsic()->isSupported(
           HwAsic::Feature::SAI_CONFIGURE_SIX_TAP)) {
     setTxRxAttr(attrs, SaiPortSerdesTraits::Attributes::TxFirPre2{}, txPre2);
-    setTxRxAttr(attrs, SaiPortSerdesTraits::Attributes::TxFirPost2{}, txPost2);
-    setTxRxAttr(attrs, SaiPortSerdesTraits::Attributes::TxFirPost3{}, txPost3);
+    if (platform_->getAsic()->getAsicType() !=
+        cfg::AsicType::ASIC_TYPE_CHENAB) {
+      // post2 and post3 are unsupported by chenab but fboss thrift model
+      // (phy.thrift) has them non-optional instead of passing them as zero,
+      // ignore them.
+      setTxRxAttr(
+          attrs, SaiPortSerdesTraits::Attributes::TxFirPost2{}, txPost2);
+      setTxRxAttr(
+          attrs, SaiPortSerdesTraits::Attributes::TxFirPost3{}, txPost3);
+    }
     if (platform_->getAsic()->getAsicVendor() ==
         HwAsic::AsicVendor::ASIC_VENDOR_TAJO) {
       setTxRxAttr(
