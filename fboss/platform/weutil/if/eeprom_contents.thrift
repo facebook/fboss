5891a837ebca99bf9fd803e4d866e0a4897d03ae
Dustin Haggard <haggarddustin@meta.com>
2025-08-06 13:04:03 -0700

    Add hack attribute to export thrift enum
    Summary: - Adds hack attribute to sync enum to www
    
    Differential Revision: D79738364
    
    fbshipit-source-id: 18c26f82df66f70a10a7c80a611ec8d6e6db9170

--- fboss/platform/weutil/if/eeprom_contents.thrift
+++ fboss/platform/weutil/if/eeprom_contents.thrift
@@ -1,5 +1,8 @@
 namespace cpp2 facebook.fboss.platform
+namespace php NetengFbossPlatform
 namespace py3 facebook.fboss.platform
+
+include "thrift/annotation/hack.thrift"
 
 // This is a common thrift struct for both V5 and V6 EEPROMs
 // See https://facebook.github.io/fboss/docs/platform/meta_eeprom_format_v6/ for details.
@@ -32,6 +35,15 @@ struct EepromContents {
   26: string crc16;
 }
 
+@hack.Attributes{
+  attributes = [
+    "Oncalls('fboss_platform')",
+    "JSEnum",
+    "GraphQLEnum('ProductionState')",
+    "SelfDescriptive",
+    "RelayFlowEnum",
+  ],
+}
 enum ProductionState {
   EVT = 1,
   DVT = 2,
