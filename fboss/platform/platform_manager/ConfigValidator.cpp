11f95513539b26c54dba538a69bf93cc418c632f
chee-zhang <chee.zhang@celestica.com>
2025-08-19 09:54:19 +0800

    Add mdio configuration function

--- fboss/platform/platform_manager/ConfigValidator.cpp
+++ fboss/platform/platform_manager/ConfigValidator.cpp
@@ -36,7 +36,8 @@ constexpr auto kSymlinkDirs = {
     "gpiochips",
     "xcvrs",
     "flashes",
-    "watchdogs"};
+    "watchdogs",
+    "mdio-busses"};
 // Supported modalias - spidev +
 // https://github.com/torvalds/linux/blob/master/drivers/spi/spidev.c#L702
 constexpr auto kSpiDevModaliases = {
@@ -287,6 +288,9 @@ bool ConfigValidator::isValidPciDeviceConfig(
   for (const auto& config : *pciDeviceConfig.miscCtrlConfigs()) {
     fpgaIpBlockConfigs.push_back(config);
   }
+  for (const auto& config : *pciDeviceConfig.mdioBusConfigs()) {
+    fpgaIpBlockConfigs.push_back(config);
+  }
 
   std::set<std::string> uniqueNames{};
   for (const auto& config : fpgaIpBlockConfigs) {
@@ -455,7 +459,8 @@ bool ConfigValidator::isValidDeviceName(
               *pciDeviceConfig.gpioChipConfigs(),
               *pciDeviceConfig.watchdogConfigs(),
               *pciDeviceConfig.infoRomConfigs(),
-              *pciDeviceConfig.miscCtrlConfigs())) {
+              *pciDeviceConfig.miscCtrlConfigs(),
+              *pciDeviceConfig.mdioBusConfigs())) {
         return true;
       }
     }
