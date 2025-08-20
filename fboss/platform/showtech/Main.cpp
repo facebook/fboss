05e6aebecdefa10c164e3fda96ac812099f60fb0
Huy Chu <huyc@arista.com>
2025-08-19 18:46:23 -0700

    Ignore scanning certain i2c buses for meru800bia and meru800bfa (#508)
    Summary:
    ## Background
    Currently showtech service scans every available i2c bus, this can take very
    long on some unused buses. Adding a way to skip specific based on
    showtech_configs. This PR also remove showtech verbose mode.
    
    ## Changes
    - removed verbose mode
    - added showtech I2cHelper class
    - added showtech_configs for Meru800bia, Meru800bfa, Darwin and Darwin48v
    - added `i2cBusIgnore` to showtech_configs, skip scanning these bus based on
      configs
    
    ## Testing
    - Manually ran showtech and checked output on Meru800bia, Meru800bfa devices
    
    Pull Request resolved: https://github.com/facebook/fboss/pull/508
    
    Test Plan:
    Imported from GitHub, without a `Test Plan:` line.
    
    Rollback Plan:
    
    Reviewed By: Scott8440
    
    Differential Revision: D80293368
    
    Pulled By: somasun
    
    fbshipit-source-id: b70b53ee1fc86eaf2cad866a0a96a0ee6f499c68

--- fboss/platform/showtech/Main.cpp
+++ fboss/platform/showtech/Main.cpp
@@ -1,25 +1,40 @@
 // Copyright (c) 2004-present, Meta Platforms, Inc. and affiliates.
 // All Rights Reserved.
 #include <fb303/FollyLoggingHandler.h>
+#include <thrift/lib/cpp2/protocol/Serializer.h>
 
+#include <folly/logging/xlog.h>
+
+#include "fboss/platform/config_lib/ConfigLib.h"
 #include "fboss/platform/helpers/InitCli.h"
+#include "fboss/platform/helpers/PlatformNameLib.h"
 #include "fboss/platform/showtech/Utils.h"
+#include "fboss/platform/showtech/gen-cpp2/showtech_config_types.h"
 
 using namespace facebook;
 using namespace facebook::fboss::platform;
-
-DEFINE_bool(verbose, false, "Run in verbose mode");
+using namespace facebook::fboss::platform::showtech_config;
 
 int main(int argc, char** argv) {
   helpers::initCli(&argc, &argv, "showtech");
 
-  Utils().printHostDetails();
-  Utils().printFbossDetails();
-  Utils().printWeutilDetails();
-  Utils().printFwutilDetails();
-  Utils().printLspciDetails(FLAGS_verbose);
-  Utils().printPortDetails(FLAGS_verbose);
-  Utils().printSensorDetails();
-  Utils().printI2cDetails(FLAGS_verbose);
+  auto platformName = helpers::PlatformNameLib().getPlatformName();
+  std::string showtechConfJson = ConfigLib().getShowtechConfig(platformName);
+  auto config =
+      apache::thrift::SimpleJSONSerializer::deserialize<ShowtechConfig>(
+          showtechConfJson);
+  XLOG(INFO) << apache::thrift::SimpleJSONSerializer::serialize<std::string>(
+      config);
+
+  Utils showtechUtil(config);
+
+  showtechUtil.printHostDetails();
+  showtechUtil.printFbossDetails();
+  showtechUtil.printWeutilDetails();
+  showtechUtil.printFwutilDetails();
+  showtechUtil.printLspciDetails();
+  showtechUtil.printPortDetails();
+  showtechUtil.printSensorDetails();
+  showtechUtil.printI2cDetails();
   return 0;
 }
