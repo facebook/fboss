136914ea53d968887a61580ff0e6368e13843db6
Huy Chu <huyc@arista.com>
2025-09-02 14:30:37 +0000

    refactor showtech i2c helper to use bimap for caching

--- fboss/platform/showtech/I2cHelper.h
+++ fboss/platform/showtech/I2cHelper.h
@@ -4,16 +4,27 @@
 
 #include "fboss/platform/helpers/PlatformUtils.h"
 
-#include <map>
+#include <boost/bimap.hpp>
+#include <string>
 
 namespace facebook::fboss::platform {
 
 class I2cHelper {
  public:
-  std::map<uint16_t, std::string> findI2cBuses();
+  // Map of i2cBusNum to adapterName
+  using I2cBusMap = boost::bimap<uint16_t, std::string>;
+
+  I2cHelper();
+  int findI2cBusForScdI2cDevice(
+      std::string scdPciAddr,
+      uint16_t master,
+      uint16_t bus);
+  const I2cBusMap& getI2cBusMap() const;
 
  private:
+  void findI2cBuses();
   PlatformUtils platformUtils_{};
+  I2cBusMap busMap_{};
 };
 
 } // namespace facebook::fboss::platform
