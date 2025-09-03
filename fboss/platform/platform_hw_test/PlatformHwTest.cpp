fc2be4d3da53b5f6092dc957e696dd0647fc1d12
Roman Choporov <romanc@meta.com>
2025-09-03 15:28:26 -0700

    Implement PlatformHwTest.PCIDevicesPresent gtest
    Summary: Implement test that checks if PCI devices provided in platform_manager config are presented on a device.
    
    Reviewed By: Scott8440
    
    Differential Revision: D81438772
    
    fbshipit-source-id: 754a38ba4d2d62d4c03693a2b5dba85d7a189b37

--- fboss/platform/platform_hw_test/PlatformHwTest.cpp
+++ fboss/platform/platform_hw_test/PlatformHwTest.cpp
@@ -14,9 +14,14 @@
 #include <net/if.h>
 #include <net/if_arp.h>
 #include <sys/ioctl.h>
+#include <filesystem>
 #include <memory>
+#include <set>
+#include <sstream>
 
 #include "fboss/platform/helpers/Init.h"
+#include "fboss/platform/helpers/PlatformFsUtils.h"
+#include "fboss/platform/platform_manager/ConfigUtils.h"
 #include "fboss/platform/weutil/FbossEepromInterface.h"
 #include "fboss/platform/weutil/Weutil.h"
 
@@ -26,10 +31,12 @@ class PlatformHwTest : public ::testing::Test {
  public:
   void SetUp() override {
     weutilConfig_ = getWeUtilConfig();
+    platformConfig_ = platform_manager::ConfigUtils().getConfig();
   }
 
  protected:
   weutil_config::WeutilConfig weutilConfig_;
+  platform_manager::PlatformConfig platformConfig_;
 
   folly::MacAddress getMacAddress(const std::string& interface) {
     auto sock_deleter = [](int* fd) {
@@ -78,6 +85,74 @@ TEST_F(PlatformHwTest, CorrectMacX86) {
 
   EXPECT_NE(eepromMac, folly::MacAddress::ZERO);
   EXPECT_EQ(eepromMac, eth0Mac);
+}
+
+TEST_F(PlatformHwTest, PCIDevicesPresent) {
+  struct PciDevice {
+    std::string vendor;
+    std::string device;
+    std::string subsystemVendor;
+    std::string subsystemDevice;
+  };
+
+  std::vector<PciDevice> missingDevices;
+
+  // Get expected devices from config
+  for (const auto& [_, pmUnitConfig] : *platformConfig_.pmUnitConfigs()) {
+    for (const auto& pciDeviceConfig : *pmUnitConfig.pciDeviceConfigs()) {
+      PciDevice pciDevice;
+      pciDevice.vendor = *pciDeviceConfig.vendorId();
+      pciDevice.device = *pciDeviceConfig.deviceId();
+      pciDevice.subsystemVendor = *pciDeviceConfig.subSystemVendorId();
+      pciDevice.subsystemDevice = *pciDeviceConfig.subSystemDeviceId();
+
+      bool deviceFound = false;
+      for (const auto& entry :
+           std::filesystem::directory_iterator("/sys/bus/pci/devices")) {
+        auto file = PlatformFsUtils(entry.path());
+        auto vendor = file.getStringFileContent("vendor");
+        auto device = file.getStringFileContent("device");
+        auto subsystemVendor = file.getStringFileContent("subsystem_vendor");
+        auto subsystemDevice = file.getStringFileContent("subsystem_device");
+        if (!(vendor && device && subsystemVendor && subsystemDevice)) {
+          EXPECT_TRUE(false) << fmt::format(
+              "Failed to read PCI device info for {}:{}:{}:{}",
+              vendor.value_or(""),
+              device.value_or(""),
+              subsystemVendor.value_or(""),
+              subsystemDevice.value_or(""));
+          continue;
+        }
+        if (std::tie(*vendor, *device, *subsystemVendor, *subsystemDevice) ==
+            std::tie(
+                pciDevice.vendor,
+                pciDevice.device,
+                pciDevice.subsystemVendor,
+                pciDevice.subsystemDevice)) {
+          deviceFound = true;
+          break;
+        }
+      }
+      if (!deviceFound) {
+        missingDevices.push_back(pciDevice);
+      }
+    }
+  }
+
+  EXPECT_TRUE(missingDevices.empty())
+      << "Missing PCI devices:\n"
+      << [&missingDevices]() {
+           std::stringstream ss;
+           for (const auto& device : missingDevices) {
+             ss << fmt::format(
+                 "\t- {}:{}:{}:{}\n",
+                 device.vendor,
+                 device.device,
+                 device.subsystemVendor,
+                 device.subsystemDevice);
+           }
+           return ss.str();
+         }();
 }
 
 } // namespace facebook::fboss::platform
