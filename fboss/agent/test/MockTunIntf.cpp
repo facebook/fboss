07de9a5297508a452788e521ff77ad4eba5ed16a
Ashutosh Grewal <agrewal@meta.com>
2025-09-05 01:53:28 -0700

    Add MockTunIntf for unit testing TUN interface functionality
    Summary:
    Introduce a mock implementation of the TUN interface to enable comprehensive unit testing without requiring actual system calls.
    
    The MockTunIntf class derives from TunIntfInterface and provides controllable behavior for testing TUN interface operations in isolation.
    
    Reviewed By: shri-khare
    
    Differential Revision:
    D80831091
    
    Privacy Context Container: L1297311
    
    fbshipit-source-id: 3af218b07871dd368d4d5eeba15ffb71bec51174

---
+++ fboss/agent/test/MockTunIntf.cpp
@@ -0,0 +1,35 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+/**
+ * @file MockTunIntf.cpp
+ * @brief Implementation of mock TUN interface for unit testing
+ * @author ashutosh grewal
+ *
+ * This file implements the MockTunIntf class which provides a mock
+ * implementation of the TunIntfInterface. It enables unit testing of TUN
+ * interface functionality without requiring actual system calls or kernel
+ * interactions.
+ */
+
+#include "fboss/agent/test/MockTunIntf.h"
+
+namespace facebook::fboss {
+
+MockTunIntf::MockTunIntf(
+    const InterfaceID& ifID,
+    const std::string& name,
+    int ifIndex,
+    int mtu)
+    : ifID_(ifID), name_(name), ifIndex_(ifIndex) {
+  // No system calls made in this constructor
+}
+
+} // namespace facebook::fboss
---
+++ fboss/agent/test/MockTunIntf.h
@@ -0,0 +1,77 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+/**
+ * @file MockTunIntf.h
+ * @brief Mock implementation of TUN interface for unit testing
+ * @author ashutosh grewal
+ *
+ * This file provides a mock implementation of the TunIntfInterface that enables
+ * comprehensive unit testing of TUN interface functionality without requiring
+ * actual system calls.
+ */
+
+#pragma once
+
+#include <gmock/gmock.h>
+#include "fboss/agent/TunIntfInterface.h"
+#include "fboss/agent/types.h"
+
+namespace facebook::fboss {
+
+class MockTunIntf : public TunIntfInterface {
+ public:
+  MockTunIntf(
+      const InterfaceID& ifID,
+      const std::string& name,
+      int ifIndex,
+      int mtu = 1500);
+
+  ~MockTunIntf() override = default;
+
+  // Mock methods not called in deleteProbedAddressesAndRules
+  MOCK_METHOD0(start, void());
+  MOCK_METHOD0(stop, void());
+  MOCK_METHOD0(setDelete, void());
+  MOCK_METHOD1(setStatus, void(bool status));
+  MOCK_METHOD2(addAddress, void(const folly::IPAddress& addr, uint8_t mask));
+  MOCK_METHOD1(setAddresses, void(const Interface::Addresses& addrs));
+  MOCK_METHOD1(sendPacketToHost, bool(std::unique_ptr<RxPacket> pkt));
+  MOCK_METHOD1(setMtu, void(int mtu));
+  MOCK_CONST_METHOD0(getInterfaceID, InterfaceID());
+  MOCK_CONST_METHOD0(getMtu, int());
+  MOCK_CONST_METHOD0(getStatus, bool());
+
+  // Real implementations for methods called in deleteProbedAddressesAndRules
+  int getIfIndex() const override {
+    return ifIndex_;
+  }
+
+  std::string getName() const override {
+    return name_;
+  }
+
+  Interface::Addresses getAddresses() const override {
+    return addrs_;
+  }
+
+  // Helper methods for tests to set state
+  void setTestAddresses(const Interface::Addresses& addrs) {
+    addrs_ = addrs;
+  }
+
+ private:
+  InterfaceID ifID_;
+  std::string name_;
+  int ifIndex_;
+  Interface::Addresses addrs_;
+};
+
+} // namespace facebook::fboss
