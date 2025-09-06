6a7e0a698e2bbcd603c23b001702eaf445a36bb4
Shrikrishna (Shri) Khare <skhare@meta.com>
2025-09-05 21:19:48 -0700

    MultiNodeTests: Start adding dedicated thrift handler
    Summary:
    Multi node tests require supporting disruptive thrift methods. Such methods
    will be unsafe for prod.
    
    Thus, start adding dedicated thrift handler.
    This inherits from Agent ThriftHandler, so all Agent methods continue to be
    available.
    
    Reviewed By: jasmeetbagga
    
    Differential Revision:
    D81511798
    
    Privacy Context Container: L1297311
    
    fbshipit-source-id: 54f5bd56f68b61e1e113b8e992018e0e2897cae1

---
+++ fboss/agent/if/multinode_test_ctrl.thrift
@@ -0,0 +1,12 @@
+namespace cpp2 facebook.fboss
+namespace go neteng.fboss.multinode_test_ctrl
+namespace php fboss
+namespace py neteng.fboss.multinode_test_ctrl
+namespace py3 neteng.fboss
+namespace py.asyncio neteng.fboss.asyncio.multinode_test_ctrl
+
+include "fboss/agent/if/ctrl.thrift"
+
+service MultiNodeTestCtrl extends ctrl.FbossCtrl {
+  void triggerGracefulExit();
+}
