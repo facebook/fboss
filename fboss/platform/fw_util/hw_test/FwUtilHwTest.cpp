10885aca9797c3a4d20a5408c20efb8643246c7c
Bin Huang <binhuang@meta.com>
2025-09-09 09:54:50 -0700

    Add validating config to hw test
    Summary: -astitle
    
    Reviewed By: Scott8440
    
    Differential Revision: D81733347
    
    fbshipit-source-id: ab055d68cf84bd616ea6b15ee56c86bfadaa46a3

--- fboss/platform/fw_util/hw_test/FwUtilHwTest.cpp
+++ fboss/platform/fw_util/hw_test/FwUtilHwTest.cpp
@@ -2,9 +2,9 @@
 
 #include "fboss/platform/fw_util/hw_test/FwUtilHwTest.h"
 
-#include <gtest/gtest.h>
-
-#include "fboss/platform/fw_util/FwUtilImpl.h"
+#include <thrift/lib/cpp2/protocol/Serializer.h>
+#include "fboss/platform/config_lib/ConfigLib.h"
+#include "fboss/platform/fw_util/ConfigValidator.h"
 #include "fboss/platform/helpers/Init.h"
 
 namespace facebook::fboss::platform::fw_util {
@@ -14,6 +14,19 @@ FwUtilHwTest::~FwUtilHwTest() {}
 void FwUtilHwTest::SetUp() {
   // TODO: Add arguments for testing
   fwUtilImpl_ = std::make_unique<FwUtilImpl>("", "", true, true);
+
+  auto fwUtilConfJson = ConfigLib().getFwUtilConfig();
+  try {
+    apache::thrift::SimpleJSONSerializer::deserialize<FwUtilConfig>(
+        fwUtilConfJson, fwUtilConfig_);
+  } catch (const std::exception& e) {
+    throw std::runtime_error(fmt::format(
+        "fw_util_hw_test failed to deserialize config: {}", e.what()));
+  }
+}
+
+TEST_F(FwUtilHwTest, validateConfig) {
+  EXPECT_TRUE(ConfigValidator().isValid(fwUtilConfig_));
 }
 
 TEST_F(FwUtilHwTest, GetAllFirmwareVersionInfo) {
