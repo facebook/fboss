cd118a51c6a95883e1bd030247a48b31a71ac167
Huy Chu <huyc@arista.com>
2025-09-11 22:48:10 +0000

    showtech: add nvme debug

--- fboss/platform/showtech/Utils.h
+++ fboss/platform/showtech/Utils.h
@@ -22,6 +22,7 @@ class Utils {
   void printSensorDetails();
   void printI2cDetails();
   void printPsuDetails();
+  void printNvmeDetails();
   void runFbossCliCmd(const std::string& cmd);
 
  private:
