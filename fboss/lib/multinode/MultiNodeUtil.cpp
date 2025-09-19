9099b3c9990d7f97cd2c29af06da6a7fd8d45950
Shrikrishna (Shri) Khare <skhare@meta.com>
2025-09-18 18:08:35 -0700

    MultiNodeUtil: verify session flaps on FSDB graceful restart
    Summary: As titled.
    
    Reviewed By: jasmeetbagga
    
    Differential Revision: D82580169
    
    fbshipit-source-id: 2e0307f84bcc2282f4df1881c4c8988ee2e0df33

--- fboss/lib/multinode/MultiNodeUtil.cpp
+++ fboss/lib/multinode/MultiNodeUtil.cpp
@@ -1550,6 +1550,7 @@ bool MultiNodeUtil::verifyGracefulFsdbDownUp() {
 
   auto myHostname = network::NetworkUtil::getLocalHost(
       true /* stripFbDomain */, true /* stripTFbDomain */);
+  auto baselinePeerToDsfSession = getPeerToDsfSession(myHostname);
 
   // For any one RDSW in every remote cluster issue graceful FSDB restart
   for (const auto& [_, rdsws] : std::as_const(clusterIdToRdsws_)) {
@@ -1559,7 +1560,25 @@ bool MultiNodeUtil::verifyGracefulFsdbDownUp() {
       }
       triggerGracefulFsdbRestart(rdsw);
 
-      // TODO verify
+      // Wait for FSDB to come up
+      if (!verifyFsdbIsUp(rdsw)) {
+        XLOG(DBG2) << "FSDB failed to come up post restart: " << rdsw;
+        return false;
+      }
+
+      // Sessions to RDSW whose FSDB just restarted are expected to flap.
+      // Verify no other sessions flap.
+      auto expectedPeerToDsfSession = baselinePeerToDsfSession;
+      expectedPeerToDsfSession.erase(rdsw);
+      if (!verifyNoSessionsFlap(
+              myHostname /* rdswToVerify */,
+              expectedPeerToDsfSession,
+              rdsw /* rdswToExclude */)) {
+        return false;
+      }
+
+      // Verify all DSF sessions are established for the RDSW that was restarted
+      verifyAllSessionsEstablished(rdsw);
 
       // Restart only one remote RDSW FSDB per cluster
       break;
