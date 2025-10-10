bba2df26e2ca6f519423f120c8018a936300dfdb
Wei Dai <daiweix@meta.com>
2025-10-10 15:48:31 -0700

    enable sending LLDP packets from hyper port members
    Summary: as titled
    
    Reviewed By: nivinl
    
    Differential Revision:
    D84360210
    
    Privacy Context Container: L1297311
    
    fbshipit-source-id: 29d4a68316b14a595fe3b847ddc4ddd2fd06a92e

--- fboss/agent/LldpManager.cpp
+++ fboss/agent/LldpManager.cpp
@@ -179,6 +179,7 @@ void LldpManager::sendLldpOnAllPorts() {
       switch (port->getPortType()) {
         case cfg::PortType::INTERFACE_PORT:
         case cfg::PortType::MANAGEMENT_PORT:
+        case cfg::PortType::HYPER_PORT_MEMBER:
           sendLldp = true;
           break;
         case cfg::PortType::FABRIC_PORT:
@@ -186,7 +187,6 @@ void LldpManager::sendLldpOnAllPorts() {
         case cfg::PortType::RECYCLE_PORT:
         case cfg::PortType::EVENTOR_PORT:
         case cfg::PortType::HYPER_PORT:
-        case cfg::PortType::HYPER_PORT_MEMBER:
           break;
       }
       if (sendLldp && port->isPortUp()) {
