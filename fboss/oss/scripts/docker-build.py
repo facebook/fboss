facebook/fboss

commit d3ea1e20f1a10abb308143f0305602e680e33f54

Mount fboss in Docker container instead of copying (#538)
Summary:
Pull Request resolved: https://github.com/facebook/fboss/pull/538

When building the fboss image, we used to copy the contents of the fboss repo on the host machine into the container. The user would have to re-build the image each time they make changes on their host machine in order to test them. We should instead mount the repository for ease of use and so that changes can persist on the host machine.

We still need to copy the fboss repo during image creation so we can use scripts for fetching dependencies, but this will be deleted afterwards so that the user will be using the mounted repository when entering the container.

Reviewed By: joseph5wu

Differential Revision: D81816484

fboss/oss/scripts/docker-build.py
@@ -23,6 +23,7 @@ OPT_ARG_NUM_JOBS = "--num-jobs"
 FBOSS_IMAGE_NAME = "fboss_image"
 FBOSS_CONTAINER_NAME = "FBOSS_BUILD_CONTAINER"
 CONTAINER_SCRATCH_PATH = "/var/FBOSS/tmp_bld_dir"
+CONTAINER_WORKDIR = "/var/FBOSS/fboss"
 
 
 def get_linux_type() -> Tuple[str, Optional[str], Optional[str]]:
@@ -174,6 +175,27 @@ def get_docker_path():
     return docker_dir_path
 
 
+def get_repo_path():
+    scripts_path = os.path.dirname(__file__)
+    return Path(scripts_path).parent.parent.parent.absolute()
+
+
+def use_stable_hashes():
+    cwd = os.getcwd()
+    os.chdir(get_repo_path())
+    cmd = [
+        "rm",
+        "-rf",
+        "build/deps/github_hashes/",
+        "&&",
+        "tar",
+        "xvzf",
+        "fboss/oss/stable_commits/latest_stable_hashes.tar.gz",
+    ]
+    subprocess.run(cmd)
+    os.chdir(cwd)
+
+
 def build_docker_image(docker_dir_path: str):
     fd, log_path = tempfile.mkstemp(suffix="docker-build.log")
     print(
@@ -210,6 +232,8 @@ def run_fboss_build(
     use_local: bool,
     num_jobs: Optional[int],
 ):
+    use_stable_hashes()
+
     cmd_args = ["sudo", "docker", "run"]
     # Add build environment variables, if any.
     for ev in env_vars:
@@ -221,6 +245,9 @@ def run_fboss_build(
             errMsg = f"Ignoring environment variable string {ev} as it does not match a supported pattern."
             print(errMsg, file=sys.stderr)
 
+    # Mount fboss repository in container
+    cmd_args.append("-v")
+    cmd_args.append(f"{get_repo_path()}:{CONTAINER_WORKDIR}:z")
     # Add args for directory mount for build output.
     cmd_args.append("-v")
     cmd_args.append(f"{scratch_path}:{CONTAINER_SCRATCH_PATH}:z")

docs/static/code_snips/start_forwarding_stack_container.sh
@@ -1,3 +1,7 @@
+# Use stable commits
+rm -rf build/deps/github_hashes/
+tar xvzf fboss/oss/stable_commits/latest_stable_hashes.tar.gz
+
 # If you know your host has enough space for the build, just make the SDK
 # artifacts available by mounting them with the -v flag.
 #
@@ -12,11 +16,17 @@
 # this will mount lib/ and include/ to the container like so:
 # /path_to_sdk/lib/     -> /opt/sdk/lib/
 # /path_to_sdk/include/ -> /opt/sdk/include/
-sudo docker run -d -v /path_to_sdk:/opt/sdk:z -it \
---name=FBOSS_DOCKER_CONTAINER fboss_docker:latest bash
+sudo docker run -d \
+    -it --name=FBOSS_DOCKER_CONTAINER \
+    -v $PWD:/var/FBOSS/fboss:z \
+    -v /path_to_sdk:/opt/sdk:z \
+    fboss_docker:latest bash
 
 # A full FBOSS build may take significant space (>50GB of storage). You
 # can mount a volume with more storage for building by using the -v flag
-sudo docker run -d -v /path_to_sdk:/opt/sdk:z \
--v /opt/app/localbuild:/var/FBOSS/tmp_bld_dir:z -it \
---name=FBOSS_DOCKER_CONTAINER fboss_docker:latest bash
+sudo docker run -d \
+    -it --name=FBOSS_DOCKER_CONTAINER \
+    -v $PWD:/var/FBOSS/fboss:z \
+    -v /path_to_sdk:/opt/sdk:z \
+    -v /opt/app/localbuild:/var/FBOSS/tmp_bld_dir:z \
+    fboss_docker:latest bash

docs/static/code_snips/start_platform_stack_container.sh
@@ -1,7 +1,17 @@
+# Use stable commits
+rm -rf build/deps/github_hashes/
+tar xvzf fboss/oss/stable_commits/latest_stable_hashes.tar.gz
+
 # If you know your host has enough space for the build
-sudo docker run -d -it --name=FBOSS_DOCKER_CONTAINER fboss_docker:latest bash
+sudo docker run -d \
+    -it --name=FBOSS_DOCKER_CONTAINER \
+    -v $PWD:/var/FBOSS/fboss:z \
+    fboss_docker:latest bash
 
 # A full FBOSS build may take significant space (>50GB of storage). You
 # can mount a volume with more storage for building by using the -v flag
-sudo docker run -d -v /opt/app/localbuild:/var/FBOSS/tmp_bld_dir:z -it \
---name=FBOSS_DOCKER_CONTAINER fboss_docker:latest bash
+sudo docker run -d \
+    -it --name=FBOSS_DOCKER_CONTAINER \
+    -v $PWD:/var/FBOSS/fboss:z \
+    -v /opt/app/localbuild:/var/FBOSS/tmp_bld_dir:z \
+    fboss_docker:latest bash

fboss/oss/docker/Dockerfile
@@ -33,6 +33,8 @@ RUN dnf install gperf libcap-devel libmount-devel -y
 RUN dnf install gcc-toolset-12 -y
 RUN echo "source /opt/rh/gcc-toolset-12/enable" >> ~/.bashrc
 
+RUN rm -rf /var/FBOSS/fboss/*
+
 # The following libraries are needed for building Broadcom SDK
 RUN dnf install -y doxygen aspell libyaml-devel vim-common libnsl libnsl2-devel
 # Perl modules needed for Broadcom SDK build

fboss/oss/scripts/github_actions/docker-unittest.py
@@ -18,6 +18,7 @@ FBOSS_CONTAINER_NAME = "FBOSS_BUILD_CONTAINER"
 
 TEST_PATH_REGEX = re.compile(".*tests?$")
 HW_TEST_PATH_REGEX = re.compile(".*hw_tests?$")
+CONTAINER_WORKDIR = "/var/FBOSS/fboss"
 
 
 # TODO: paulcruz74 - deduplicate this from docker-build.py
@@ -27,6 +28,28 @@ def get_docker_path():
     oss_dir_path = Path(scripts_path).parent.absolute()
     docker_dir_path = os.path.join(oss_dir_path, "docker")
     return docker_dir_path
+
+
+def get_repo_path():
+    github_actions_path = os.path.dirname(__file__)
+    scripts_path = Path(github_actions_path).parent.absolute()
+    return Path(scripts_path).parent.parent.parent.absolute()
+
+
+def use_stable_hashes():
+    cwd = os.getcwd()
+    os.chdir(get_repo_path())
+    cmd = [
+        "rm",
+        "-rf",
+        "build/deps/github_hashes/",
+        "&&",
+        "tar",
+        "xvzf",
+        "fboss/oss/stable_commits/latest_stable_hashes.tar.gz",
+    ]
+    subprocess.run(cmd)
+    os.chdir(cwd)
 
 
 def build_docker_image(docker_dir_path: str):
@@ -102,9 +125,12 @@ def is_hw_test(path: str) -> bool:
 
 
 def run_test(test: str, output_dir: str) -> bool:
+    use_stable_hashes()
     cmd_args = ["sudo", "docker", "run"]
     lib_path = os.path.join(output_dir, "lib")
     cmd_args.extend(["-e", f"LD_LIBRARY_PATH={lib_path}"])
+    # Mount fboss repository in container
+    cmd_args.extend(["-v", f"{get_repo_path()}:{CONTAINER_WORKDIR}:z"])
     cmd_args.extend(["-v", f"{output_dir}:{output_dir}:z"])
     # Add required capability for sudo permissions
     cmd_args.append("--cap-add=CAP_AUDIT_WRITE")

commit 72be4c679bdd02ac2b1a698a07d995a31c0a3b34

Properly unpack stable hashes for pinned dependencies
When the build starts it typically uses pinned dependencies from 
`fboss/oss/stable_commits/latest_stable_hashes.tar.gz` however due to a 
bug introduced in #538 the stable hashes were not extracted and were 
actually deleted, leading the build to expressly NOT use stable hashes 
when it was in fact supposed to.

The consequence of this is that builds were typically relying on the 
latest upstream sha1 for the various dependencies fetched from GitHub, 
which lead to unstable and non reproducible builds.

fboss/oss/scripts/docker-build.py
@@ -183,16 +183,8 @@ def get_repo_path():
 def use_stable_hashes():
     cwd = os.getcwd()
     os.chdir(get_repo_path())
-    cmd = [
-        "rm",
-        "-rf",
-        "build/deps/github_hashes/",
-        "&&",
-        "tar",
-        "xvzf",
-        "fboss/oss/stable_commits/latest_stable_hashes.tar.gz",
-    ]
-    subprocess.run(cmd)
+    subprocess.run(["rm", "-rf", "build/deps/github_hashes/"], check=True)
+    subprocess.run(["tar", "xvzf", "fboss/oss/stable_commits/latest_stable_hashes.tar.gz"], check=True)
     os.chdir(cwd)
 
 