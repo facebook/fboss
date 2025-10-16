de18122dacfe380f823bb3ca6eaae0542cf15699
Manikandan Somasundaram <manikandan@meta.com>
2025-10-15 20:40:18 -0700

    Ensure the runCommand & execCommand exceptions are handled
    Summary:
    The Subprocess invocation can throw for a couple of reasons like
    *   Command not found
    *   Permission denied to execute
    *   Fork/exec system call failure
    *   Resource limits (too many processes, out of memory)
    *   Invalid file descriptors
    
    Wrap these with try/catch to provide a **Consistent Interface**: Callers always
    get a `std::pair<int, std::string>` without worrying about exceptions.
    
    Reviewed By: RomanChoporov-Meta
    
    Differential Revision: D84655766
    
    fbshipit-source-id: 9016193d16ed381c12b552d1017df14dd7eea6ea

--- fboss/platform/helpers/PlatformUtils.cpp
+++ fboss/platform/helpers/PlatformUtils.cpp
@@ -14,33 +14,50 @@ namespace facebook::fboss::platform {
 std::pair<int, std::string> PlatformUtils::execCommand(
     const std::string& cmd) const {
   XLOG(DBG2) << "Executing command: " << cmd;
-  auto shellCmd = "/bin/sh -c {}"_shellify(cmd);
-  folly::Subprocess p(shellCmd, folly::Subprocess::Options().pipeStdout());
-  auto [standardOut, standardErr] = p.communicate();
-  int exitStatus = p.wait().exitStatus();
-  return std::make_pair(exitStatus, standardOut);
+  try {
+    auto shellCmd = "/bin/sh -c {}"_shellify(cmd);
+    folly::Subprocess p(shellCmd, folly::Subprocess::Options().pipeStdout());
+    auto [standardOut, standardErr] = p.communicate();
+    int exitStatus = p.wait().exitStatus();
+    return std::make_pair(exitStatus, standardOut);
+  } catch (const std::exception& ex) {
+    XLOG(ERR) << "Exception executing command '" << cmd << "': " << ex.what();
+    return std::make_pair(-1, ex.what());
+  }
 }
 
 std::pair<int, std::string> PlatformUtils::runCommand(
     const std::vector<std::string>& args) const {
   XLOG(DBG2) << "Running command: " << folly::join(" ", args);
-  folly::Subprocess p(args, folly::Subprocess::Options().pipeStdout());
-  auto [standardOut, standardErr] = p.communicate();
-  int exitStatus = p.wait().exitStatus();
-  return std::make_pair(exitStatus, standardOut);
+  try {
+    folly::Subprocess p(args, folly::Subprocess::Options().pipeStdout());
+    auto [standardOut, standardErr] = p.communicate();
+    int exitStatus = p.wait().exitStatus();
+    return std::make_pair(exitStatus, standardOut);
+  } catch (const std::exception& ex) {
+    XLOG(ERR) << "Exception running command '" << folly::join(" ", args)
+              << "': " << ex.what();
+    return std::make_pair(-1, ex.what());
+  }
 }
 
 std::pair<int, std::string> PlatformUtils::runCommandWithStdin(
     const std::vector<std::string>& args,
     const std::string& input) const {
   XLOG(DBG2) << "Running command: " << folly::join(" ", args);
-  folly::Subprocess::Options options;
-  options.pipeStdin();
-  options.pipeStdout();
-  folly::Subprocess proc(args, options);
-  auto [output1, output2] = proc.communicate(input);
-  int exitStatus = proc.wait().exitStatus();
-  return std::make_pair(exitStatus, output1);
+  try {
+    folly::Subprocess::Options options;
+    options.pipeStdin();
+    options.pipeStdout();
+    folly::Subprocess proc(args, options);
+    auto [output1, output2] = proc.communicate(input);
+    int exitStatus = proc.wait().exitStatus();
+    return std::make_pair(exitStatus, output1);
+  } catch (const std::exception& ex) {
+    XLOG(ERR) << "Exception running command '" << folly::join(" ", args)
+              << "' : " << ex.what();
+    return std::make_pair(-1, ex.what());
+  }
 }
 
 } // namespace facebook::fboss::platform
