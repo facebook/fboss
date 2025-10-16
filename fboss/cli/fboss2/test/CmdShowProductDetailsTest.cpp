988f87e0b25e328ed4c54e469ff21891b52dedea
benoit-nexthop <benoit@nexthop.ai>
2025-10-16 12:47:45 +0200

    Build the unit tests for `fboss2` in the OSS build.

--- fboss/cli/fboss2/test/CmdShowProductDetailsTest.cpp
+++ fboss/cli/fboss2/test/CmdShowProductDetailsTest.cpp
@@ -6,7 +6,7 @@
 #include "fboss/cli/fboss2/commands/show/product/CmdShowProductDetails.h"
 #include "fboss/cli/fboss2/commands/show/product/gen-cpp2/model_types.h"
 #include "fboss/cli/fboss2/test/CmdHandlerTestBase.h"
-#include "nettools/common/TestUtils.h"
+#include <thrift/lib/cpp2/reflection/testing.h>
 
 using namespace ::testing;
 
--- fboss/cli/fboss2/test/FilterTest.cpp
+++ fboss/cli/fboss2/test/FilterTest.cpp
@@ -6,7 +6,7 @@
 #include "fboss/cli/fboss2/CmdGlobalOptions.h"
 #include "fboss/cli/fboss2/commands/show/port/CmdShowPort.h"
 #include "fboss/cli/fboss2/test/CmdHandlerTestBase.h"
-#include "nettools/common/TestUtils.h"
+#include <thrift/lib/cpp2/reflection/testing.h>
 
 using namespace ::testing;
 namespace facebook::fboss {
