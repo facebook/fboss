d7864e12a3ab51f5c4f4f1a5f8a26055c3c112ef
Shrikrishna (Shri) Khare <skhare@meta.com>
2025-10-22 18:42:24 -0700

    Introduce outline of TopologyInfo
    Summary:
    As titled.
    Refactoring code in MultiNodeAgentVoqSwitchTests.cpp, lib/multinode for better
    maintainabilty, readability. The old code will be removed post this refactor.
    
    Reviewed By: jasmeetbagga
    
    Differential Revision:
    D85154417
    
    Privacy Context Container: L1297311
    
    fbshipit-source-id: f9daba16d028a42b5e0b167c6d54be7bee00e7fc

---
+++ fboss/agent/test/agent_multinode_tests/TopologyInfo.cpp
@@ -0,0 +1,17 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#include "fboss/agent/test/agent_multinode_tests/TopologyInfo.h"
+
+namespace facebook::fboss::utility {
+
+TopologyInfo::TopologyInfo(const std::shared_ptr<SwitchState>& switchState) {}
+
+} // namespace facebook::fboss::utility
