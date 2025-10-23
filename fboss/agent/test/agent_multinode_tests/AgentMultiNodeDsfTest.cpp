5e4f4396b1ad4b9bf543a3527467ed40436eb1f5
Shrikrishna (Shri) Khare <skhare@meta.com>
2025-10-23 04:14:50 -0700

    Create DSF specific test class: AgentMultiNodeDsfTest
    Summary:
    As titled.
    Refactoring code in MultiNodeAgentVoqSwitchTests.cpp, lib/multinode for better
    maintainabilty, readability. The old code will be removed post this refactor.
    
    Reviewed By: jasmeetbagga
    
    Differential Revision: D85176996
    
    fbshipit-source-id: d0b4332e1a071380e2bf36e91f883b3f2a07cd9f

---
+++ fboss/agent/test/agent_multinode_tests/AgentMultiNodeDsfTest.cpp
@@ -0,0 +1,11 @@
+// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+#include "fboss/agent/test/agent_multinode_tests/AgentMultiNodeTest.h"
+
+namespace facebook::fboss {
+
+class AgentMultiNodeDsfTest : public AgentMultiNodeTest {};
+
+TEST_F(AgentMultiNodeDsfTest, verifySelfHealingECMPLag) {}
+
+} // namespace facebook::fboss
