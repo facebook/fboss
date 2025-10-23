09c78c0a0b8be9f9920acd845ae02470201519a8
Shrikrishna (Shri) Khare <skhare@meta.com>
2025-10-22 18:42:24 -0700

    Populate initialConfig from file, set loopback mode on NIF ports
    Summary:
    As titled.
    Refactoring code in MultiNodeAgentVoqSwitchTests.cpp, lib/multinode for better
    maintainabilty, readability. The old code will be removed post this refactor.
    
    Reviewed By: jasmeetbagga
    
    Differential Revision: D85155688
    
    fbshipit-source-id: a232eb0e72034310b7f4481700783a61d6364762

--- fboss/agent/test/agent_multinode_tests/AgentMultiNodeTest.cpp
+++ fboss/agent/test/agent_multinode_tests/AgentMultiNodeTest.cpp
@@ -2,9 +2,34 @@
 
 #include "fboss/agent/test/AgentHwTest.h"
 
+#include "fboss/agent/AsicUtils.h"
+
 namespace facebook::fboss {
 
 class AgentMultiNodeTest : public AgentHwTest {
+  cfg::SwitchConfig initialConfig(
+      const AgentEnsemble& ensemble) const override {
+    XLOG(DBG0) << "initialConfig() loaded config from file " << FLAGS_config;
+
+    auto hwAsics = ensemble.getSw()->getHwAsicTable()->getL3Asics();
+    auto asic = checkSameAndGetAsic(hwAsics);
+
+    auto it = asic->desiredLoopbackModes().find(cfg::PortType::INTERFACE_PORT);
+    CHECK(it != asic->desiredLoopbackModes().end());
+    auto desiredLoopbackModeForInterfacePort = it->second;
+
+    auto agentConfig = AgentConfig::fromFile(FLAGS_config);
+    auto config = *agentConfig->thrift.sw();
+    for (auto& port : *config.ports()) {
+      if (port.portType() == cfg::PortType::INTERFACE_PORT) {
+        // TODO determine loopback mode based on ASIC type
+        port.loopbackMode() = desiredLoopbackModeForInterfacePort;
+      }
+    }
+
+    return config;
+  }
+
   // TODO: Factor out to VOQ specific Test subclass and implement there
   std::vector<ProductionFeature> getProductionFeaturesVerified()
       const override {
