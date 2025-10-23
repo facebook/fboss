67e7ad67cfb8d6332a26e13f6e73754c25c8d3c1
Shrikrishna (Shri) Khare <skhare@meta.com>
2025-10-23 04:14:50 -0700

    Introduce ThriftClientUtils: reusable wrappers for thrift calls
    Summary:
    As titled.
    Refactoring code in MultiNodeAgentVoqSwitchTests.cpp, lib/multinode for better
    maintainabilty, readability. The old code will be removed post this refactor.
    
    Reviewed By: jasmeetbagga
    
    Differential Revision: D85159699
    
    fbshipit-source-id: 727b51d7d7447d4c945111a36fc7dbae1e1a4946

---
+++ fboss/agent/test/thrift_client_utils/ThriftClientUtils.cpp
@@ -0,0 +1,32 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#include "fboss/agent/test/thrift_client_utils/ThriftClientUtils.h"
+
+#include <thrift/lib/cpp2/async/RocketClientChannel.h>
+#include "common/network/NetworkUtil.h"
+
+namespace facebook::fboss::utility {
+
+using facebook::fboss::TestCtrl;
+
+std::unique_ptr<apache::thrift::Client<TestCtrl>> getSwAgentThriftClient(
+    const std::string& switchName) {
+  folly::EventBase* eb = folly::EventBaseManager::get()->getEventBase();
+  auto remoteSwitchIp =
+      facebook::network::NetworkUtil::getHostByName(switchName);
+  folly::SocketAddress agent(remoteSwitchIp, 5909);
+  auto socket = folly::AsyncSocket::newSocket(eb, agent);
+  auto channel =
+      apache::thrift::RocketClientChannel::newChannel(std::move(socket));
+  return std::make_unique<apache::thrift::Client<TestCtrl>>(std::move(channel));
+}
+
+} // namespace facebook::fboss::utility
---
+++ fboss/agent/test/thrift_client_utils/ThriftClientUtils.h
@@ -0,0 +1,20 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#pragma once
+
+#include "fboss/agent/if/gen-cpp2/TestCtrlAsyncClient.h"
+
+namespace facebook::fboss::utility {
+
+std::unique_ptr<apache::thrift::Client<facebook::fboss::TestCtrl>>
+getSwAgentThriftClient(const std::string& switchName);
+
+} // namespace facebook::fboss::utility
