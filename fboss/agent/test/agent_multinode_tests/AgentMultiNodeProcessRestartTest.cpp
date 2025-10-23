ba6bbc7e024334f3a286b94f4d11be3bd454ad6d
Shrikrishna (Shri) Khare <skhare@meta.com>
2025-10-23 04:14:50 -0700

    Outline for process restart tests
    Summary:
    As titled.
    Refactoring code in MultiNodeAgentVoqSwitchTests.cpp, lib/multinode for better
    maintainabilty, readability. The old code will be removed post this refactor.
    
    Reviewed By: jasmeetbagga
    
    Differential Revision: D85184712
    
    fbshipit-source-id: ae2b9b9eff6dfb456b0cb79412d7629e484b91a4

---
+++ fboss/agent/test/agent_multinode_tests/AgentMultiNodeProcessRestartTest.cpp
@@ -0,0 +1,11 @@
+// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+#include "fboss/agent/test/agent_multinode_tests/AgentMultiNodeTest.h"
+
+namespace facebook::fboss {
+
+class AgentMultiNodeProcessRestartTest : public AgentMultiNodeTest {};
+
+TEST_F(AgentMultiNodeProcessRestartTest, verifyAgentWarmboot) {}
+
+} // namespace facebook::fboss
