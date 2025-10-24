9794269ecefd2c2f617239c460c16b22c13cada5
Tanmaya Udupa <tanmayaudupa@meta.com>
2025-10-24 16:06:08 -0700

    Add NextHop ID manager class header
    Summary: Adding NextHopIDManager class header with basic structure. Each attributes and member functions descriptions are added in comments.
    
    Reviewed By: zechengh09
    
    Differential Revision:
    D85076722
    
    Privacy Context Container: L1297311
    
    fbshipit-source-id: 42a2c113fd85cf4173ba5ee2e8085e7b59965db7

---
+++ fboss/agent/rib/NextHopIDManager.h
@@ -0,0 +1,96 @@
+// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+#pragma once
+
+#include <cstdint>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+
+#include "fboss/agent/state/RouteNextHop.h"
+#include "fboss/agent/types.h"
+
+namespace facebook::fboss {
+
+// A set of NextHopIDs (used as key for NextHopIDSetID mapping)
+using NextHopIDSet = std::set<NextHopID>;
+
+/**
+ * NextHopIDManager is responsible for generating and managing unique IDs
+ * for NextHops and NextHopSets. It maintains reference counts for each
+ * assigned ID to track usage.
+ */
+class NextHopIDManager {
+ public:
+  NextHopIDManager() = default;
+
+  // Get or allocate a NextHopID for the given NextHop
+  // Returns pair of (NextHopID, bool) where bool is true if new ID was
+  // allocated
+  std::pair<NextHopID, bool> getOrAllocateNextHopID(const NextHop& nextHop);
+
+  // Get or allocate a NextHopSetID for the given set of NextHopIDs
+  // Returns pair of (NextHopSetID, bool) where bool is true if new ID was
+  // allocated
+  std::pair<NextHopSetID, bool> getOrAllocateNextHopSetID(
+      const NextHopIDSet& nextHopIDSet);
+
+  // Decrement reference count for a NextHop and deallocate if count reaches 0
+  void decrOrDeallocateNextHop(const NextHop& nextHop);
+
+  // Decrement reference count for a set of NextHopIDs and deallocate if count
+  // reaches 0
+  void decrOrDeallocateNextHopIDSet(const NextHopIDSet& nextHopIDSet);
+
+  // Get the reverse lookup map from NextHopID to NextHop
+  const std::unordered_map<NextHopID, NextHop>& getIdToNextHop() const {
+    return idToNextHop_;
+  }
+
+  // Get the reverse lookup map from NextHopSetID to NextHopIDSet
+  const std::unordered_map<NextHopSetID, NextHopIDSet>& getIdToNextHopIdSet()
+      const {
+    return idToNextHopIdSet_;
+  }
+
+ private:
+  // Structure to hold ID and reference count for NextHops
+  // Count will be 0 when initialised
+  struct NextHopIDInfo {
+    NextHopID id;
+    uint32_t count = 0;
+
+    NextHopIDInfo(NextHopID id_, uint32_t count_) : id(id_), count(count_) {}
+  };
+
+  // Structure to hold ID and reference count for NextHopSets
+  struct NextHopSetIDInfo {
+    NextHopSetID id;
+    uint32_t count;
+
+    NextHopSetIDInfo(NextHopSetID id_, uint32_t count_)
+        : id(id_), count(count_) {}
+  };
+
+  // Counter for generating NextHop IDs, starting from 1
+  // allocate 0 - (2^63 -1) IDs for NextHops.
+  NextHopID nextAvailableNextHopID_{1};
+
+  // Counter for generating NextHopSet IDs, starting from 2^63
+  // Allocate 2^63 - (2^64 - 1) IDs for NextHopIDSets.
+  NextHopSetID nextAvailableNextHopSetID_{1ULL << 63};
+
+  // Mapping from NextHop to its ID and reference count
+  std::unordered_map<NextHop, NextHopIDInfo> nextHopToIDInfo_;
+
+  // Mapping from set of NextHopIDs to its NextHopSetID and reference count
+  std::unordered_map<NextHopIDSet, NextHopSetIDInfo> nextHopIdSetToIDInfo_;
+
+  // Reverse lookup maps for clients to convert IDs back to NextHops
+  std::unordered_map<NextHopID, NextHop> idToNextHop_;
+
+  // Map from NextHopSetID to set of NextHopIDs
+  std::unordered_map<NextHopSetID, NextHopIDSet> idToNextHopIdSet_;
+};
+
+} // namespace facebook::fboss
