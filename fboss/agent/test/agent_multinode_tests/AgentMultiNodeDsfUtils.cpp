12964f08978e54a5cf94e065bd88962432e1da5c
Shrikrishna (Shri) Khare <skhare@meta.com>
2025-10-23 21:04:41 -0700

    Outline to verify Fabric connectivity
    Summary:
    As titled.
    Refactoring code in MultiNodeAgentVoqSwitchTests.cpp, lib/multinode for better
    maintainabilty, readability. The old code will be removed post this refactor.
    
    Reviewed By: jasmeetbagga
    
    Differential Revision:
    D85189746
    
    Privacy Context Container: L1297311
    
    fbshipit-source-id: d734f44a832925360ae16e3dce61f6821d6a45a9

--- fboss/agent/test/agent_multinode_tests/AgentMultiNodeDsfUtils.cpp
+++ fboss/agent/test/agent_multinode_tests/AgentMultiNodeDsfUtils.cpp
@@ -2,9 +2,37 @@
 
 #include "fboss/agent/test/agent_multinode_tests/AgentMultiNodeDsfUtils.h"
 
+#include "fboss/lib/CommonUtils.h"
+
 namespace facebook::fboss::utility {
 
-void verifyDsfCluster(const std::unique_ptr<TopologyInfo>& topologyInfo) {}
+bool verifyFabricConnectivityForRdsws(
+    const std::unique_ptr<TopologyInfo>& topologyInfo) {
+  return true;
+}
+
+bool verifyFabricConnectivityForFdsws(
+    const std::unique_ptr<TopologyInfo>& topologyInfo) {
+  return true;
+}
+
+bool verifyFabricConnectivityForSdsws(
+    const std::unique_ptr<TopologyInfo>& topologyInfo) {
+  return true;
+}
+
+bool verifyFabricConnectivity(
+    const std::unique_ptr<TopologyInfo>& topologyInfo) {
+  return verifyFabricConnectivityForRdsws(topologyInfo) &&
+      verifyFabricConnectivityForFdsws(topologyInfo) &&
+      verifyFabricConnectivityForSdsws(topologyInfo);
+}
+
+void verifyDsfCluster(const std::unique_ptr<TopologyInfo>& topologyInfo) {
+  WITH_RETRIES_N_TIMED(10, std::chrono::milliseconds(5000), {
+    verifyFabricConnectivity(topologyInfo);
+  });
+}
 
 void verifyDsfAgentDownUp() {}
 
