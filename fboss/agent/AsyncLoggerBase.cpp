db6ccbe727fa0b56de2a4ec778dfd545992e6c13
Ron He <zecheng@meta.com>
2025-10-28 22:25:52 -0700

    Move unchanged implemention to base async logger
    Summary:
    **Background**
    
    While debugging issues with ECMP Resource Manager stress testing, we noticed several crashes that are hard to reproduced when there is constant BGP flaps.
    The inability to reproduce such issue makes it very hard to debug - we are only relying on our luck reproducing edge cases with ixia etc.
    Rather, we can log all the state delta transitions happening on the box and trace every single delta transition. This would help us step-by-step to reproduce the issue.
    
    To re-use the same async logger we built for BCM Cinter/SAI Replayer, refactor async logger such that it can also accommodate state delta logger.
    
    **This Diff**
    Move unchanged implementation from AsyncLogger to AsyncLoggerBase.
    
    Differential Revision:
    D85477709
    
    Privacy Context Container: L1297311
    
    fbshipit-source-id: dadcf5522d1efb9e3b44b59b1ebd02548fcf5138

--- fboss/agent/AsyncLoggerBase.cpp
+++ fboss/agent/AsyncLoggerBase.cpp
@@ -8,6 +8,8 @@
  *
  */
 
+#include <iomanip>
+
 #include "fboss/agent/AsyncLoggerBase.h"
 
 #include <fb303/ServiceData.h>
@@ -16,7 +18,15 @@
 
 DECLARE_bool(disable_async_logger);
 
-namespace {} // namespace
+namespace {
+
+constexpr auto kBuildRevision = "build_revision";
+constexpr auto kVerboseSdkVersion = "Verbose SDK Version";
+
+bool isWarmBoot;
+std::mutex bootTypeLatch_;
+
+} // namespace
 
 namespace facebook::fboss {
 
@@ -35,6 +45,64 @@ AsyncLoggerBase::AsyncLoggerBase(
     flushBuffer_ = flushBuf;
     logTimeout_ = std::chrono::milliseconds(logTimeout);
   }
+}
+
+void AsyncLoggerBase::setBootType(bool canWarmBoot) {
+  bootTypeLatch_.lock();
+  isWarmBoot = canWarmBoot;
+  bootTypeLatch_.unlock();
+}
+
+void AsyncLoggerBase::startFlushThread() {
+  enableLogging_ = true;
+  if (!FLAGS_disable_async_logger) {
+    flushThread_ = new std::thread(&AsyncLoggerBase::workerThread, this);
+  }
+  writeNewBootHeader();
+}
+
+void AsyncLoggerBase::stopFlushThread() {
+  if (!FLAGS_disable_async_logger && enableLogging_) {
+    enableLogging_ = false;
+    flushThread_->join();
+    delete flushThread_;
+  }
+}
+void AsyncLoggerBase::forceFlush() {
+  if (!FLAGS_disable_async_logger) {
+    future_ = promise_.get_future();
+    forceFlush_ = true;
+    cv_.notify_one();
+    future_.get();
+  }
+}
+
+void AsyncLoggerBase::writeNewBootHeader() {
+  auto now = std::chrono::system_clock::now();
+  auto timer = std::chrono::system_clock::to_time_t(now);
+  std::tm tm;
+  localtime_r(&timer, &tm);
+  auto bootType = isWarmBoot ? "warmboot" : "coldboot";
+  std::ostringstream oss;
+  oss << "// Start of a " << bootType << " "
+      << std::put_time(&tm, "%Y-%m-%d %T") << std::endl;
+  auto ossString = oss.str();
+  appendLog(ossString.c_str(), ossString.size());
+  std::string commitId;
+  fb303::fbData->getExportedValue(commitId, kBuildRevision);
+  std::string verboseSdkVersion;
+  fb303::fbData->getExportedValue(verboseSdkVersion, kVerboseSdkVersion);
+  verboseSdkVersion.erase(
+      std::remove(verboseSdkVersion.begin(), verboseSdkVersion.end(), '\n'),
+      verboseSdkVersion.end());
+  std::string agentVersion = folly::to<std::string>(
+      "// Commit id : ",
+      commitId,
+      "\n",
+      "// SDK version : ",
+      verboseSdkVersion,
+      "\n");
+  appendLog(agentVersion.c_str(), agentVersion.size());
 }
 
 } // namespace facebook::fboss
