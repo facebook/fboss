715130b67d814a2fd5c6c65037e5aee7ac6b1dc4
Ron He <zecheng@meta.com>
2025-10-29 23:18:31 -0700

    Create static buffers for async logging
    Summary:
    **Background**
    
    While debugging issues with ECMP Resource Manager stress testing, we noticed several crashes that are hard to reproduced when there is constant BGP flaps.
    The inability to reproduce such issue makes it very hard to debug - we are only relying on our luck reproducing edge cases with ixia etc.
    Rather, we can log all the state delta transitions happening on the box and trace every single delta transition. This would help us step-by-step to reproduce the issue.
    
    To re-use the same async logger we built for BCM Cinter/SAI Replayer, refactor async logger such that it can also accommodate state delta logger.
    
    **This Diff**
    Similar to AsyncLogger, create static buffers such that, in the case of catastrophic failures we can still flush it in the terminate handler.
    
    As the lifetime of static variable is longer than member variables, define those as static and will be used by AsyncLoggerbase.
    
    Differential Revision: D85684990
    
    fbshipit-source-id: ccc756745f9a985553797e46e95bc9bf38eb8344

---
+++ fboss/agent/StateDeltaLogger.cpp
@@ -0,0 +1,46 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#include "fboss/agent/StateDeltaLogger.h"
+
+#include <chrono>
+#include <sstream>
+
+#include "fboss/agent/AgentFeatures.h"
+#include "fboss/agent/state/StateDelta.h"
+#include "fboss/agent/state/SwitchState.h"
+#include "fboss/fsdb/if/gen-cpp2/fsdb_oper_types.h"
+
+#include <folly/logging/xlog.h>
+
+// Static buffers for async logging
+static auto constexpr kBufferSize = 409600;
+static facebook::fboss::AsyncLoggerBase::BufferToWrite currentBuffer =
+    facebook::fboss::AsyncLoggerBase::BufferToWrite::BUFFER0;
+static uint32_t bufOffset = 0;
+static std::array<char, kBufferSize> buffer0;
+static std::array<char, kBufferSize> buffer1;
+
+namespace facebook::fboss {
+
+uint32_t StateDeltaLogger::getOffset() {
+  return bufOffset;
+}
+
+void StateDeltaLogger::setOffset(uint32_t offset) {
+  bufOffset = offset;
+}
+
+void StateDeltaLogger::swapCurBuf() {
+  currentBuffer = currentBuffer == AsyncLoggerBase::BUFFER0
+      ? AsyncLoggerBase::BUFFER1
+      : AsyncLoggerBase::BUFFER0;
+}
+} // namespace facebook::fboss
