42bf5bf4a470e13aca810e5649ef45a70de278c5
Shrikrishna (Shri) Khare <skhare@meta.com>
2025-11-01 18:47:29 -0700

    Util to verify if a switch runstate is as specified
    Summary:
    As titled.
    Refactoring code in MultiNodeAgentVoqSwitchTests.cpp, lib/multinode for better
    maintainabilty, readability. The old code will be removed post this refactor.
    
    Reviewed By: jasmeetbagga
    
    Differential Revision:
    D85508875
    
    Privacy Context Container: L1297311
    
    fbshipit-source-id: 90eea98ffdae3725e2072ee1fe65af84c739ea27

--- fboss/agent/test/agent_multinode_tests/AgentMultiNodeUtils.cpp
+++ fboss/agent/test/agent_multinode_tests/AgentMultiNodeUtils.cpp
@@ -2,12 +2,27 @@
 
 #include "fboss/agent/test/agent_multinode_tests/AgentMultiNodeUtils.h"
 
+#include "fboss/agent/test/thrift_client_utils/ThriftClientUtils.h"
+#include "fboss/lib/CommonUtils.h"
+
 namespace facebook::fboss::utility {
 
 bool verifySwSwitchRunState(
     const std::string& switchName,
     const SwitchRunState& expectedSwitchRunState) {
-  return true;
+  auto switchRunStateMatches = [switchName, expectedSwitchRunState] {
+    auto multiSwitchRunState = getMultiSwitchRunState(switchName);
+    auto gotSwitchRunState = multiSwitchRunState.swSwitchRunState();
+    return gotSwitchRunState == expectedSwitchRunState;
+  };
+
+  // Thrift client queries will throw exception while the Agent is
+  // initializing. Thus, continue to retry while absorbing exceptions.
+  return checkWithRetryErrorReturn(
+      switchRunStateMatches,
+      30 /* num retries */,
+      std::chrono::milliseconds(5000) /* sleep between retries */,
+      true /* retry on exception */);
 }
 
 } // namespace facebook::fboss::utility
