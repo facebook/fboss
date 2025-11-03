a515d00475f138817f8e8acd0f3c3f1ff036140b
Shrikrishna (Shri) Khare <skhare@meta.com>
2025-11-03 14:47:48 -0800

    Introduce AgentMultiNodeVoqSwitchTests for voq specific tests
    Summary:
    As titled.
    Refactoring code in MultiNodeAgentVoqSwitchTests.cpp, lib/multinode for better
    maintainabilty, readability. The old code will be removed post this refactor.
    
    Reviewed By: jasmeetbagga
    
    Differential Revision: D86051608
    
    fbshipit-source-id: c3518aa02c0457e6362dd02d71fe33d5afbacb3d

---
+++ fboss/agent/test/agent_multinode_tests/AgentMultiNodeVoqSwitchTests.cpp
@@ -0,0 +1,12 @@
+// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+#include "fboss/agent/test/agent_multinode_tests/AgentMultiNodeDsfUtils.h"
+#include "fboss/agent/test/agent_multinode_tests/AgentMultiNodeTest.h"
+
+namespace facebook::fboss {
+
+class AgentMultiNodeVoqSwitchTest : public AgentMultiNodeTest {};
+
+TEST_F(AgentMultiNodeVoqSwitchTest, verifyGracefulFabricLinkDownUp) {}
+
+} // namespace facebook::fboss
