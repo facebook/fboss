ea09b7bd538fc39a3cb4f1d57d6cb7019363feef
Shrikrishna (Shri) Khare <skhare@meta.com>
2025-11-03 14:47:48 -0800

    AgentMultiNode VoqSwitch struct for NeighborInfo
    Summary:
    As titled.
    Refactoring code in MultiNodeAgentVoqSwitchTests.cpp, lib/multinode for better
    maintainabilty, readability. The old code will be removed post this refactor.
    
    Reviewed By: jasmeetbagga
    
    Differential Revision: D86058181
    
    fbshipit-source-id: 0ec69e778861771f3952840ef3122012c174274d

--- fboss/agent/test/agent_multinode_tests/AgentMultiNodeVoqSwitchNeighborTests.cpp
+++ fboss/agent/test/agent_multinode_tests/AgentMultiNodeVoqSwitchNeighborTests.cpp
@@ -5,7 +5,15 @@
 
 namespace facebook::fboss {
 
-class AgentMultiNodeVoqSwitchNeighborTest : public AgentMultiNodeTest {};
+class AgentMultiNodeVoqSwitchNeighborTest : public AgentMultiNodeTest {
+ private:
+  struct NeighborInfo {
+    int32_t portID = 0;
+    int32_t intfID = 0;
+    folly::IPAddress ip = folly::IPAddress("::");
+    folly::MacAddress mac = folly::MacAddress("00:00:00:00:00:00");
+  };
+};
 
 TEST_F(AgentMultiNodeVoqSwitchNeighborTest, verifyNeighborAddRemove) {}
 
