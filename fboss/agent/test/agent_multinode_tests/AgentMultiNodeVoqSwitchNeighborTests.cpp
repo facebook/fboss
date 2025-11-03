64b357b32c78188116eede4abd1994bc4fa2ac22
Shrikrishna (Shri) Khare <skhare@meta.com>
2025-11-03 14:47:48 -0800

    Outline of MultiNodeVoqSwitch Neighbor tests
    Summary:
    As titled.
    Refactoring code in MultiNodeAgentVoqSwitchTests.cpp, lib/multinode for better
    maintainabilty, readability. The old code will be removed post this refactor.
    
    Reviewed By: jasmeetbagga
    
    Differential Revision: D86058180
    
    fbshipit-source-id: f3942e374f9283f7232717afb6383f07957582a0

---
+++ fboss/agent/test/agent_multinode_tests/AgentMultiNodeVoqSwitchNeighborTests.cpp
@@ -0,0 +1,12 @@
+// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+#include "fboss/agent/test/agent_multinode_tests/AgentMultiNodeDsfUtils.h"
+#include "fboss/agent/test/agent_multinode_tests/AgentMultiNodeTest.h"
+
+namespace facebook::fboss {
+
+class AgentMultiNodeVoqSwitchNeighborTest : public AgentMultiNodeTest {};
+
+TEST_F(AgentMultiNodeVoqSwitchNeighborTest, verifyNeighborAddRemove) {}
+
+} // namespace facebook::fboss
