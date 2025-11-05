13c41166b0ce3d4702142244a708be16314d8e64
Parvez Shaikh <pshaikh@meta.com>
2025-11-04 21:52:25 -0800

    setup correct kvp to reclaim buffer
    Summary:
    as titled
    
    in latest sdk SAI_KEY_RECLAIM_PG0_BUFFER_DISABLED replaces SAI_KEY_RECLAIM_BUFFER_ENABLED
    
    this is as per vendor guidance
    
    Differential Revision:
    D86235622
    
    Privacy Context Container: L1297311
    
    fbshipit-source-id: 7e3d705b9cd628971cfd6975b7e8a7bc30d7a09a

--- fboss/agent/platforms/sai/SaiYangraPlatform.cpp
+++ fboss/agent/platforms/sai/SaiYangraPlatform.cpp
@@ -63,7 +63,11 @@ SaiYangraPlatform::getSaiProfileVendorExtensionValues() const {
   std::unordered_map<std::string, std::string> kv_map;
   kv_map.insert(std::make_pair("SAI_KEY_PORT_AUTONEG_DEFAULT_OFF", "1"));
   kv_map.insert(std::make_pair("SAI_KEY_NOT_DROP_SMAC_DMAC_EQUAL", "1"));
+#if !defined(CHENAB_SAI_SDK_VERSION_2505_34_0_1)
   kv_map.insert(std::make_pair("SAI_KEY_RECLAIM_BUFFER_ENABLED", "0"));
+#else
+  kv_map.insert(std::make_pair("SAI_KEY_RECLAIM_PG0_BUFFER_DISABLED", "1"));
+#endif
   kv_map.insert(std::make_pair("SAI_KEY_TRAP_PACKETS_USING_CALLBACK", "1"));
   kv_map.insert(std::make_pair("SAI_KEY_ROUTE_METADATA_FIELD_SIZE", "5"));
   kv_map.insert(
