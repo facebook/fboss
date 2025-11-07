61d3030040d317f9050b3a9cc6025ab0e3f0756e
Shrikrishna (Shri) Khare <skhare@meta.com>
2025-11-06 16:19:01 -0800

    Helper to print struct Neighbor objects
    Summary: As titled.
    
    Reviewed By: jasmeetbagga
    
    Differential Revision:
    D86269931
    
    Privacy Context Container: L1297311
    
    fbshipit-source-id: 20ea7afedd9dc7af473d7cd5e6b8cf1a593d2176

--- fboss/agent/test/agent_multinode_tests/AgentMultiNodeUtils.h
+++ fboss/agent/test/agent_multinode_tests/AgentMultiNodeUtils.h
@@ -22,6 +22,18 @@ struct Neighbor {
   int32_t intfID = 0;
   folly::IPAddress ip = folly::IPAddress("::");
   folly::MacAddress mac = folly::MacAddress("00:00:00:00:00:00");
+
+  std::string str() const {
+    return folly::to<std::string>(
+        "portID: ",
+        portID,
+        ", intfID: ",
+        intfID,
+        ", ip: ",
+        ip.str(),
+        ", mac: ",
+        mac.toString());
+  }
 };
 
 // Invoke the provided func on every element of a given container
