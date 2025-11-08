e354594571d12087334fd4bed1378f27706c0da6
Shrikrishna (Shri) Khare <skhare@meta.com>
2025-11-08 15:04:31 -0800

    Leverage the generalized util to restart FSDB
    Summary: As titled.
    
    Reviewed By: jasmeetbagga
    
    Differential Revision:
    D86453579
    
    Privacy Context Container: L1297311
    
    fbshipit-source-id: e5cda0922f4a38890c3eef79fb00783856b73c97

--- fboss/agent/test/agent_multinode_tests/AgentMultiNodeDsfUtils.cpp
+++ fboss/agent/test/agent_multinode_tests/AgentMultiNodeDsfUtils.cpp
@@ -1157,25 +1157,14 @@ bool verifyDsfFSDBRestart(
         continue;
       }
 
-      std::map<std::string, std::set<SwitchID>> switchNameToSwitchIds = {
-          {rdsw, topologyInfo->getSwitchNameToSwitchIds().at(rdsw)}};
-      auto baselineSwitchNameToFsdbAliveSinceEpoch =
-          getSwitchNameToFsdbAliveSinceEpoch(switchNameToSwitchIds);
-
-      // Trigger graceful or ungraceful FSDB restart
-      triggerGracefulRestart ? triggerGracefulFsdbRestart(rdsw)
-                             : triggerUngracefulFsdbRestart(rdsw);
-
-      // Wait for FSDB to restart
-      if (!verifyFsdbRestarted(
-              switchNameToSwitchIds, baselineSwitchNameToFsdbAliveSinceEpoch)) {
-        XLOG(DBG2) << "FSDB failed to restart on: " << rdsw;
-        return false;
-      }
-
-      // Wait for FSDB to come up
-      if (!verifyFsdbIsUp(rdsw)) {
-        XLOG(DBG2) << "FSDB failed to come up post restart: " << rdsw;
+      if (!restartServiceForSwitches(
+              {rdsw},
+              triggerGracefulRestart ? triggerGracefulFsdbRestart
+                                     : triggerUngracefulFsdbRestart,
+              getFsdbAliveSinceEpoch,
+              verifyFsdbIsUp)) {
+        XLOG(ERR) << "Failed to restart FSDB on: "
+                  << folly::join(",", topologyInfo->getAllSwitches());
         return false;
       }
 
