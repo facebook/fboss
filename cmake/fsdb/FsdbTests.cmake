e5e489c504a257bb72672baecfdedb4d517c4d5d
Elangovan Natarajan <elangovan@meta.com>
2025-11-10 11:41:51 -0800

    Enable fsdb pubsubmgr test in OSS
    Summary: As titled. portOperStateToggleTest is the main test we need in OSS for AMD's usecase
    
    Differential Revision: D85891874
    
    fbshipit-source-id: 98ae74c790dc00dae6df0be3d45687999d5e510a

--- cmake/fsdb/FsdbTests.cmake
+++ cmake/fsdb/FsdbTests.cmake
@@ -19,7 +19,7 @@ add_library(fsdb_test_server
 
 target_link_libraries(fsdb_test_server
   fsdb_handler
-  fsdb_oper
+  fsdb_oper_cpp2
   Folly::folly
   FBThrift::thriftcpp2
 )
@@ -45,3 +45,43 @@ target_link_libraries(fsdb_utils_benchmark
   ${LIBGMOCK}
   ${GFLAGS}
 )
+
+add_library(fsdb_test_clients
+  fboss/fsdb/tests/client/FsdbTestClients.h
+  fboss/fsdb/tests/client/FsdbTestClients.cpp
+)
+
+target_link_libraries(fsdb_test_clients
+  fsdb_pub_sub
+  fsdb_stream_client
+  fsdb_model_cpp2
+  common_utils
+  Folly::folly
+  FBThrift::thriftcpp2
+  FBThrift::thriftprotocol
+  ${GTEST}
+)
+
+add_executable(fsdb_pub_sub_tests
+  fboss/fsdb/tests/client/FsdbPubSubManagerTest.cpp
+  fboss/agent/test/oss/Main.cpp
+)
+
+target_link_libraries(fsdb_pub_sub_tests
+  log_thrift_call
+  fsdb_test_clients
+  fsdb_test_server
+  fsdb_pub_sub
+  fsdb_model_cpp2
+  fsdb_oper_cpp2
+  patch_cpp2
+  extended_path_builder
+  common_utils
+  thrift_service_client
+  Folly::folly
+  FBThrift::thriftcpp2
+  ${GTEST}
+  ${LIBGMOCK_LIBRARIES}
+)
+
+gtest_discover_tests(fsdb_pub_sub_tests)
