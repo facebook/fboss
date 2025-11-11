5a2a8ee19ab0a21a4ddf65613168a8e2cf0aedb3
Shrikrishna (Shri) Khare <skhare@meta.com>
2025-11-10 22:42:07 -0800

    Document how injectTraffic creates a loop
    Summary: As titled.
    
    Reviewed By: jasmeetbagga
    
    Differential Revision: D86607541
    
    fbshipit-source-id: 0ccee446837eb8880830d5a906933e959aa26c50

--- fboss/agent/test/agent_multinode_tests/AgentMultiNodeVoqSwitchTrafficTests.cpp
+++ fboss/agent/test/agent_multinode_tests/AgentMultiNodeVoqSwitchTrafficTests.cpp
@@ -86,6 +86,26 @@ class AgentMultiNodeVoqSwitchTrafficTest
   }
 
   void injectTraffic(const utility::Neighbor& neighbor) const {
+    // configureNeighborsAndRoutesForTrafficLoop configures
+    //  o RDSW A route to point to RDSW B's neighbor,
+    //  o RDSW B route to point to RDSW C's neighbor,
+    //  o ...
+    //  o last RDSW's route to point to RDSW A's neighbor.
+    //
+    // Send packet froms self (say RDSW A) with dstMAC as RouterMAC:
+    //  o The packet is routed to RDSW B's neighbor.
+    //  o RDSW B neighbor is resolved on a port in loopback mode.
+    //  o The packet thus gets looped back.
+    //  o Since the packet carries dstMac = Router MAC, it gets routed.
+    //  o This packet routes to RDSW C's neighbor
+    //  ....
+    //  o The last RDSW routes the packet to RDSW A and loop continues.
+    //
+    // Forwarding is enabled for TTL0 packets so the packet continues to be
+    // forwarded even after TTL is 0. Thus, we get traffic flood.
+    //
+    // Injecting 1000 packets on one 400G NIF port is sufficient to create a
+    // loop that saturates the 400G RDSW links on every RDSW.
     auto static kSrcIP = folly::IPAddressV6("2001:0db8:85a0::");
     auto [prefix, _] = kGetRoutePrefixAndPrefixLength();
     for (int i = 0; i < 1000; i++) {
