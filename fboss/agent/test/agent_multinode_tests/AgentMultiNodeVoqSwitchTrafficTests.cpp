ffb8f99050a3e25da15d680f92a3c379174761ff
Shrikrishna (Shri) Khare <skhare@meta.com>
2025-11-10 21:32:01 -0800

    Verify traffic spray: add neighbors
    Summary: As titled.
    
    Reviewed By: jasmeetbagga
    
    Differential Revision: D86603672
    
    fbshipit-source-id: 39ea7b93ef7910013d98dd64638169f3af7a4678

--- fboss/agent/test/agent_multinode_tests/AgentMultiNodeVoqSwitchTrafficTests.cpp
+++ fboss/agent/test/agent_multinode_tests/AgentMultiNodeVoqSwitchTrafficTests.cpp
@@ -2,10 +2,39 @@
 
 #include "fboss/agent/test/agent_multinode_tests/AgentMultiNodeVoqSwitchNeighborTests.h"
 
+#include "fboss/agent/test/thrift_client_utils/ThriftClientUtils.h"
+
 namespace facebook::fboss {
 
 class AgentMultiNodeVoqSwitchTrafficTest
     : public AgentMultiNodeVoqSwitchNeighborTest {
+ protected:
+  std::map<std::string, utility::Neighbor>
+  configureNeighborsAndRoutesForTrafficLoop(
+      const std::unique_ptr<utility::TopologyInfo>& topologyInfo) const {
+    std::map<std::string, utility::Neighbor> rdswToNeighbor;
+
+    // Add a neighbor for every RDSW in the cluster
+    for (const auto& rdsw : topologyInfo->getRdsws()) {
+      auto neighbors = computeNeighborsForRdsw(
+          topologyInfo, rdsw, 1 /* number of neighbors */);
+      CHECK_EQ(neighbors.size(), 1);
+      auto neighbor = neighbors[0];
+      rdswToNeighbor[rdsw] = neighbor;
+
+      XLOG(DBG2) << "Adding neighbor: " << neighbor.str() << " to " << rdsw;
+      utility::addNeighbor(
+          rdsw, neighbor.intfID, neighbor.ip, neighbor.mac, neighbor.portID);
+      if (!utility::verifyNeighborsPresent(
+              topologyInfo->getRdsws(), rdsw, {neighbor})) {
+        XLOG(DBG2) << "Neighbor add verification failed: " << rdsw
+                   << " neighbor: " << neighbor.str();
+        return rdswToNeighbor;
+      }
+    }
+    return rdswToNeighbor;
+  }
+
  public:
   bool verifyTrafficSpray(
       const std::unique_ptr<utility::TopologyInfo>& topologyInfo) const {
