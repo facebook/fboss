fe0e6c394328b45c877a6392478dffe108be8d4c
Shrikrishna (Shri) Khare <skhare@meta.com>
2025-11-11 00:28:49 -0800

    Helper to run through series of scenarios, verify no drops
    Summary: As titled.
    
    Reviewed By: jasmeetbagga
    
    Differential Revision: D86616271
    
    fbshipit-source-id: 8498473ebaf0780f6ea69c2e029a05c2eac56995

--- fboss/agent/test/agent_multinode_tests/AgentMultiNodeVoqSwitchNoTrafficDropsTests.cpp
+++ fboss/agent/test/agent_multinode_tests/AgentMultiNodeVoqSwitchNoTrafficDropsTests.cpp
@@ -2,10 +2,40 @@
 
 #include "fboss/agent/test/agent_multinode_tests/AgentMultiNodeVoqSwitchTrafficTests.h"
 
+#include "fboss/agent/test/agent_multinode_tests/AgentMultiNodeDsfUtils.h"
+
 namespace facebook::fboss {
 
 class AgentMultiNodeVoqSwitchNoTrafficDropTest
     : public AgentMultiNodeVoqSwitchTrafficTest {
+ protected:
+  struct Scenario {
+    std::string name;
+    std::function<bool()> setup;
+  };
+
+  // Return true only if all scenarios are successful
+  bool runScenariosAndVerifyNoDrops(
+      const std::unique_ptr<utility::TopologyInfo>& topologyInfo,
+      const std::vector<Scenario>& scenarios) const {
+    for (const auto& scenario : scenarios) {
+      XLOG(DBG2) << "Running scenario: " << scenario.name;
+      if (!scenario.setup()) {
+        XLOG(DBG2) << "Scenario: " << scenario.name << " failed";
+        return false;
+      }
+
+      if (!utility::verifyNoReassemblyErrorsForAllSwitches(topologyInfo)) {
+        // TODO query drop counters to root cause reason for reassembly errors
+        XLOG(DBG2) << "Scenario: " << scenario.name
+                   << " unexpected reassembly errors";
+        return false;
+      }
+    }
+
+    return true;
+  }
+
  public:
   bool verifyNoTrafficDropOnProcessRestarts(
       const std::unique_ptr<utility::TopologyInfo>& topologyInfo) {
