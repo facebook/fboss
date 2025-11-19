afdfe2c09f261c7e080c1c0d0ac439447b6e510c
Tanmaya Udupa <tanmayaudupa@meta.com>
2025-11-18 16:27:16 -0800

    Add MultiSwitchFibsInfoMap and FibInfo thrift COW wrappers
    Summary: Adding Thrift COW wrapper classes FibsInfoMap thrift map.
    
    Reviewed By: zechengh09
    
    Differential Revision:
    D87097042
    
    Privacy Context Container: L1297311
    
    fbshipit-source-id: 28b4b2740606a111fea236aad9e34943c55bc09b

---
+++ fboss/agent/state/FibInfoMap.cpp
@@ -0,0 +1,42 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+#include "fboss/agent/state/FibInfoMap.h"
+#include "fboss/agent/HwSwitchMatcher.h"
+#include "fboss/agent/state/SwitchState.h"
+
+namespace facebook::fboss {
+
+MultiSwitchFibInfoMap* MultiSwitchFibInfoMap::modify(
+    std::shared_ptr<SwitchState>* state) {
+  if (!isPublished()) {
+    CHECK(!(*state)->isPublished());
+    return this;
+  }
+
+  SwitchState::modify(state);
+  auto newFibInfoMap = clone();
+  auto* rtn = newFibInfoMap.get();
+  (*state)->resetFibsInfoMap(std::move(newFibInfoMap));
+
+  return rtn;
+}
+
+void MultiSwitchFibInfoMap::updateFibInfo(
+    const std::shared_ptr<FibInfo>& fibInfo,
+    const HwSwitchMatcher& matcher) {
+  auto matcherStr = matcher.matcherString();
+  if (getNodeIf(matcherStr)) {
+    updateNode(matcherStr, fibInfo);
+  } else {
+    addNode(matcherStr, fibInfo);
+  }
+}
+
+} // namespace facebook::fboss
