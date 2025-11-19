de88fa950f3140c620b06bd79f659ab155d390d3
chee-zhang <chee.zhang@celestica.com>
2025-11-19 10:05:51 +0800

    PhyId is 0-based, PhyCoreId is set to 0

--- fboss/lib/bsp/ladakh800bcls/Ladakh800bclsBspPlatformMapping.cpp
+++ fboss/lib/bsp/ladakh800bcls/Ladakh800bclsBspPlatformMapping.cpp
@@ -79,259 +79,259 @@ constexpr auto kJsonBspPlatformMappingStr = R"(
         }
       },
       "phyMapping": {
-        "1": {
-          "phyId": 1,
+        "0": {
+          "phyId": 0,
           "phyIOControllerId": 1,
           "phyAddr": 0,
-          "phyCoreId": 83361,
+          "phyCoreId": 0,
+          "pimId": 1,
+          "phyResetPath": ""
+        },
+        "1": {
+          "phyId": 1,
+          "phyIOControllerId": 2,
+          "phyAddr": 0,
+          "phyCoreId": 0,
           "pimId": 1,
           "phyResetPath": ""
         },
         "2": {
           "phyId": 2,
-          "phyIOControllerId": 2,
+          "phyIOControllerId": 3,
           "phyAddr": 0,
-          "phyCoreId": 83361,
+          "phyCoreId": 0,
           "pimId": 1,
           "phyResetPath": ""
         },
         "3": {
           "phyId": 3,
-          "phyIOControllerId": 3,
+          "phyIOControllerId": 4,
           "phyAddr": 0,
-          "phyCoreId": 83361,
+          "phyCoreId": 0,
           "pimId": 1,
           "phyResetPath": ""
         },
         "4": {
           "phyId": 4,
-          "phyIOControllerId": 4,
+          "phyIOControllerId": 5,
           "phyAddr": 0,
-          "phyCoreId": 83361,
+          "phyCoreId": 0,
           "pimId": 1,
           "phyResetPath": ""
         },
         "5": {
           "phyId": 5,
-          "phyIOControllerId": 5,
+          "phyIOControllerId": 6,
           "phyAddr": 0,
-          "phyCoreId": 83361,
+          "phyCoreId": 0,
           "pimId": 1,
           "phyResetPath": ""
         },
         "6": {
           "phyId": 6,
-          "phyIOControllerId": 6,
+          "phyIOControllerId": 7,
           "phyAddr": 0,
-          "phyCoreId": 83361,
+          "phyCoreId": 0,
           "pimId": 1,
           "phyResetPath": ""
         },
         "7": {
           "phyId": 7,
-          "phyIOControllerId": 7,
+          "phyIOControllerId": 8,
           "phyAddr": 0,
-          "phyCoreId": 83361,
+          "phyCoreId": 0,
           "pimId": 1,
           "phyResetPath": ""
         },
         "8": {
           "phyId": 8,
-          "phyIOControllerId": 8,
+          "phyIOControllerId": 9,
           "phyAddr": 0,
-          "phyCoreId": 83361,
+          "phyCoreId": 0,
           "pimId": 1,
           "phyResetPath": ""
         },
         "9": {
           "phyId": 9,
-          "phyIOControllerId": 9,
+          "phyIOControllerId": 10,
           "phyAddr": 0,
-          "phyCoreId": 83361,
+          "phyCoreId": 0,
           "pimId": 1,
           "phyResetPath": ""
         },
         "10": {
           "phyId": 10,
-          "phyIOControllerId": 10,
+          "phyIOControllerId": 11,
           "phyAddr": 0,
-          "phyCoreId": 83361,
+          "phyCoreId": 0,
           "pimId": 1,
           "phyResetPath": ""
         },
         "11": {
           "phyId": 11,
-          "phyIOControllerId": 11,
+          "phyIOControllerId": 12,
           "phyAddr": 0,
-          "phyCoreId": 83361,
+          "phyCoreId": 0,
           "pimId": 1,
           "phyResetPath": ""
         },
         "12": {
           "phyId": 12,
-          "phyIOControllerId": 12,
+          "phyIOControllerId": 13,
           "phyAddr": 0,
-          "phyCoreId": 83361,
+          "phyCoreId": 0,
           "pimId": 1,
           "phyResetPath": ""
         },
         "13": {
           "phyId": 13,
-          "phyIOControllerId": 13,
+          "phyIOControllerId": 14,
           "phyAddr": 0,
-          "phyCoreId": 83361,
+          "phyCoreId": 0,
           "pimId": 1,
           "phyResetPath": ""
         },
         "14": {
           "phyId": 14,
-          "phyIOControllerId": 14,
+          "phyIOControllerId": 15,
           "phyAddr": 0,
-          "phyCoreId": 83361,
+          "phyCoreId": 0,
           "pimId": 1,
           "phyResetPath": ""
         },
         "15": {
           "phyId": 15,
-          "phyIOControllerId": 15,
+          "phyIOControllerId": 16,
           "phyAddr": 0,
-          "phyCoreId": 83361,
+          "phyCoreId": 0,
           "pimId": 1,
           "phyResetPath": ""
         },
         "16": {
           "phyId": 16,
-          "phyIOControllerId": 16,
+          "phyIOControllerId": 17,
           "phyAddr": 0,
-          "phyCoreId": 83361,
+          "phyCoreId": 0,
           "pimId": 1,
           "phyResetPath": ""
         },
         "17": {
           "phyId": 17,
-          "phyIOControllerId": 17,
+          "phyIOControllerId": 18,
           "phyAddr": 0,
-          "phyCoreId": 83361,
+          "phyCoreId": 0,
           "pimId": 1,
           "phyResetPath": ""
         },
         "18": {
           "phyId": 18,
-          "phyIOControllerId": 18,
+          "phyIOControllerId": 19,
           "phyAddr": 0,
-          "phyCoreId": 83361,
+          "phyCoreId": 0,
           "pimId": 1,
           "phyResetPath": ""
         },
         "19": {
           "phyId": 19,
-          "phyIOControllerId": 19,
+          "phyIOControllerId": 20,
           "phyAddr": 0,
-          "phyCoreId": 83361,
+          "phyCoreId": 0,
           "pimId": 1,
           "phyResetPath": ""
         },
         "20": {
           "phyId": 20,
-          "phyIOControllerId": 20,
+          "phyIOControllerId": 21,
           "phyAddr": 0,
-          "phyCoreId": 83361,
+          "phyCoreId": 0,
           "pimId": 1,
           "phyResetPath": ""
         },
         "21": {
           "phyId": 21,
-          "phyIOControllerId": 21,
+          "phyIOControllerId": 22,
           "phyAddr": 0,
-          "phyCoreId": 83361,
+          "phyCoreId": 0,
           "pimId": 1,
           "phyResetPath": ""
         },
         "22": {
           "phyId": 22,
-          "phyIOControllerId": 22,
+          "phyIOControllerId": 23,
           "phyAddr": 0,
-          "phyCoreId": 83361,
+          "phyCoreId": 0,
           "pimId": 1,
           "phyResetPath": ""
         },
         "23": {
           "phyId": 23,
-          "phyIOControllerId": 23,
+          "phyIOControllerId": 24,
           "phyAddr": 0,
-          "phyCoreId": 83361,
+          "phyCoreId": 0,
           "pimId": 1,
           "phyResetPath": ""
         },
         "24": {
           "phyId": 24,
-          "phyIOControllerId": 24,
+          "phyIOControllerId": 25,
           "phyAddr": 0,
-          "phyCoreId": 83361,
+          "phyCoreId": 0,
           "pimId": 1,
           "phyResetPath": ""
         },
         "25": {
           "phyId": 25,
-          "phyIOControllerId": 25,
+          "phyIOControllerId": 26,
           "phyAddr": 0,
-          "phyCoreId": 83361,
+          "phyCoreId": 0,
           "pimId": 1,
           "phyResetPath": ""
         },
         "26": {
           "phyId": 26,
-          "phyIOControllerId": 26,
+          "phyIOControllerId": 27,
           "phyAddr": 0,
-          "phyCoreId": 83361,
+          "phyCoreId": 0,
           "pimId": 1,
           "phyResetPath": ""
         },
         "27": {
           "phyId": 27,
-          "phyIOControllerId": 27,
+          "phyIOControllerId": 28,
           "phyAddr": 0,
-          "phyCoreId": 83361,
+          "phyCoreId": 0,
           "pimId": 1,
           "phyResetPath": ""
         },
         "28": {
           "phyId": 28,
-          "phyIOControllerId": 28,
+          "phyIOControllerId": 29,
           "phyAddr": 0,
-          "phyCoreId": 83361,
+          "phyCoreId": 0,
           "pimId": 1,
           "phyResetPath": ""
         },
         "29": {
           "phyId": 29,
-          "phyIOControllerId": 29,
+          "phyIOControllerId": 30,
           "phyAddr": 0,
-          "phyCoreId": 83361,
+          "phyCoreId": 0,
           "pimId": 1,
           "phyResetPath": ""
         },
         "30": {
           "phyId": 30,
-          "phyIOControllerId": 30,
+          "phyIOControllerId": 31,
           "phyAddr": 0,
-          "phyCoreId": 83361,
+          "phyCoreId": 0,
           "pimId": 1,
           "phyResetPath": ""
         },
         "31": {
           "phyId": 31,
-          "phyIOControllerId": 31,
-          "phyAddr": 0,
-          "phyCoreId": 83361,
-          "pimId": 1,
-          "phyResetPath": ""
-        },
-        "32": {
-          "phyId": 32,
           "phyIOControllerId": 32,
           "phyAddr": 0,
-          "phyCoreId": 83361,
+          "phyCoreId": 0,
           "pimId": 1,
           "phyResetPath": ""
         }
