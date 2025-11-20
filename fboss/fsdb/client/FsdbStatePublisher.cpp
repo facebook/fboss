3c9c741cacc9474412d8fc2bf8428d62da9cdef9
Priyank Warkhede <priyankw@meta.com>
2025-11-20 13:20:14 -0800

    PathPublisher: always coalesce pending updates
    Summary:
    In FSDB Path publisher, coalesce updates if there are more
    than 1 pending chunks enqueued.
    
    The publisher client will try to flush any pending (coalesced) update
    either on next update or after next heartbeat can be enqueued.
    
    Differential Revision: D87358869
    
    fbshipit-source-id: 5f10e20477a7f9f0f9c0db9ee51a36ada9440ac9

--- fboss/fsdb/client/FsdbStatePublisher.cpp
+++ fboss/fsdb/client/FsdbStatePublisher.cpp
@@ -24,6 +24,37 @@ FsdbStatePublisher::setupStream() {
   co_return std::move(result.sink);
 }
 
+bool FsdbStatePublisher::tryWrite(
+    folly::coro::BoundedAsyncPipe<OperState, false>& pipe,
+    OperState&& pubUnit) {
+  if (nextUpdate_.has_value()) {
+    coalescedUpdates_.addValue(1);
+  }
+  nextUpdate_ = std::move(pubUnit);
+  return flush(pipe);
+}
+
+bool FsdbStatePublisher::flush(
+    folly::coro::BoundedAsyncPipe<OperState, false>& pipe) {
+  if (!nextUpdate_.has_value()) {
+    return true;
+  }
+
+  auto queuedChunks = pipe.getOccupiedSpace();
+  if (queuedChunks > 1) {
+    XLOG(DBG2) << "fsdb publish stream has pending chunks, skip flush";
+    return true;
+  }
+
+  if (pipe.try_write(*nextUpdate_)) {
+    nextUpdate_.reset();
+    chunksWritten_.addValue(1);
+    return true;
+  } else {
+    return false;
+  }
+}
+
 folly::coro::Task<void> FsdbStatePublisher::serveStream(StreamT&& stream) {
   CHECK(std::holds_alternative<StatePubStreamT>(stream));
   auto finalResponseReceiver =
