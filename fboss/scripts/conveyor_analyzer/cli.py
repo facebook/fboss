42ac831196a108adb15e148916c6e57ecefe1e5b
Pranav Raghavan <praghava@meta.com>
2025-11-25 16:54:26 -0800

    Parse the conveyor argument
    Summary: click infra uses decorators to parse command line arguments
    
    Differential Revision: D86687976
    
    fbshipit-source-id: 61e016d0e346160af643315161039e489b186d31

--- fboss/scripts/conveyor_analyzer/cli.py
+++ fboss/scripts/conveyor_analyzer/cli.py
@@ -2,5 +2,32 @@
 
 
 # pyre-strict
+import click
+
+
+@click.group()
+def cli() -> None:
+    """
+    click decorator will take over and run the analyze function after parsing the arguments
+    """
+    pass
+
+
+@cli.command(name="analyze")
+@click.option("--conveyor", required=True, help="Conveyor ID")
+def analyze(conveyor: str) -> None:
+    """
+    Analyze conveyor releases and nodes
+    """
+    print("Conveyor parsed: ", conveyor)
+
+
 def main() -> None:
-    print("Hello world!")
+    """
+    Entry point in BUCK target fboss_conveyor
+    """
+    cli()
+
+
+if __name__ == "__main__":
+    cli()
