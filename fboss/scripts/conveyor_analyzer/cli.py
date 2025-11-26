3701db88c44138b958ce8576060a73997f138b2c
Pranav Raghavan <praghava@meta.com>
2025-11-25 16:54:26 -0800

    Print out all the nodes and status in one release
    Differential Revision: D86688003
    
    fbshipit-source-id: 9201bb5f7b0f6bc9c5320024ea6d439f665d6873

--- fboss/scripts/conveyor_analyzer/cli.py
+++ fboss/scripts/conveyor_analyzer/cli.py
@@ -6,6 +6,8 @@ from typing import Optional
 
 import click
 
+from . import conveyor_api
+
 
 @click.group()
 def cli() -> None:
@@ -22,12 +24,26 @@ def analyze(
     conveyor: str,
     release: Optional[str],
 ) -> None:
-    """
+    """n
     Analyze conveyor releases and nodes
     """
     print("Conveyor parsed: ", conveyor)
     if release:
-        print("Release parsed: ", release)
+        handle_single_release_command(conveyor, release)
+
+
+def handle_single_release_command(conveyor: str, release: str) -> None:
+    """
+    Handle command 1: Single release instance analyzis
+    """
+    click.echo(f"📊 Analyzing release {release} for conveyor {conveyor}...\n")
+
+    release_status = conveyor_api.get_release_status(conveyor, release_instance=release)
+
+    for release_data in release_status:
+        for node_name, node_data in release_data.get("runs", {}).items():
+            if node_data:
+                print(node_name, node_data.get("status"))
 
 
 def main() -> None:
