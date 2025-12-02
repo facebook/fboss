23d9d54563b46ace42e472de96cb9b022e8ba900
Shrikrishna (Shri) Khare <skhare@meta.com>
2025-12-02 09:44:46 -0800

    Document HwAsic feature ECMP_HASH_v4
    Summary:
    This is towards cleaning up the HwAsic features to:
    - group them according to functionality,
    - document the behavior,
    - remove unncessary features.
    
    These feature enums are not stored anywhere.
    Thus, it should be safe to renumber these warmboots over warmboot.
    
    Differential Revision:
    D88039785
    
    Privacy Context Container: L1297311
    
    fbshipit-source-id: 4c037d1189a337e49e522cfb0bd13a88243c7c0c

--- fboss/agent/hw/switch_asics/HwAsic.h
+++ fboss/agent/hw/switch_asics/HwAsic.h
@@ -228,9 +228,19 @@ class HwAsic {
     //  - Rename to carry ECMP_ prefix.
     BULK_CREATE_ECMP_MEMBER,
 
+    // Set to true if the SAI implementation supports associating a specific
+    // hashing policy for IPv4 packets.
+    // This maps to SAI_SWITCH_ATTR_ECMP_HASH_IPV4.
+    // Some ASICs support hashing natively and thus do not support this
+    // configuration.
+    // TODO:
+    //  - Candidate for removal: YES, but non-trivial.
+    //  - Extend SAI spec to introduce sai_hash_capability_t to return the list
+    //    of hash configuraton supported.
+    ECMP_HASH_V4,
+
     HASH_FIELDS_CUSTOMIZATION,
     TRAFFIC_HASHING,
-    ECMP_HASH_V4,
     ECMP_HASH_V6,
     SAI_ECMP_HASH_ALGORITHM,
     SET_NEXT_HOP_GROUP_HASH_ALGORITHM,
