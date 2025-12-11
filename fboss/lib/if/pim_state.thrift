824ae5884683b468b554880ce6b857b3b4738205
Shiva Menta <smenta@meta.com>
2025-12-10 23:17:16 -0800

    Add getPimType to PIM Error Detection Logic
    Summary:
    Using existing PIM error framework to routinely check if getPimType fails (which generally indicates that some PIM communication exists).
    
    This will ensure we have proper PIM error handling in the case covered in S587991. We can now allow getTransceiverManagementInterface to fail gracefully.
    
    Error trace paste for reference: P2073911459.
    
    Reviewed By: harshitgulati18
    
    Differential Revision: D88770628
    
    fbshipit-source-id: 475dbfb6affb97ed0b1a6f6cd5cd55a1e9a4d108

--- fboss/lib/if/pim_state.thrift
+++ fboss/lib/if/pim_state.thrift
@@ -11,6 +11,8 @@ enum PimError {
   PIM_SCD_COUNTER_NOT_CLEARED = 0,
   # Same for Minipack / Fuji PIM, which uses a different scratchpad counter.
   PIM_MINIPACK_SCRATCHPAD_COUNTER_NOT_CLEARED = 1,
+  # PIM fails getPimType() check. This is a good indicator that communication with PIM is not working as expected.
+  PIM_GET_TYPE_FAILED = 2,
 }
 
 struct PimState {
