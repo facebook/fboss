3d77b71c2a413fce37d96becec1cdd9ccf063612
generatedunixname226714639793621 <noreply+226714639793621@fb.com>
2025-12-15 12:46:08 -0800

    (1/2) fbcode/fboss/agent/
    Reviewed By: aristidisp
    
    Differential Revision: D89061279
    
    fbshipit-source-id: 3323facd07aed69a50a7ea34ff3cb7f7f8b27821

--- fboss/agent/hw/bcm/packettrace.thrift
+++ fboss/agent/hw/bcm/packettrace.thrift
@@ -1,3 +1,8 @@
+include "thrift/annotation/thrift.thrift"
+
+@thrift.AllowLegacyMissingUris
+package;
+
 namespace cpp2 facebook.fboss
 namespace py neteng.fboss.packettrace
 namespace py3 neteng.fboss
