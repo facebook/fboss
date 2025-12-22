5179e1e11970273d8838db1bb852efcb28e30fcb
Roman Choporov <romanc@meta.com>
2025-12-22 11:48:38 -0800

    Initialize RuntimeEnvironment only during tests runs
    Summary:
    As titled.
    Implemented this approach to prevent calling platform-specific functions on non-platform systems.
    E.g. while calling `bsp_tests --gtest_list_tests`
    `bsp_tests --gtest_list_tests` is called by netcastle right before running actual gtests.
    
    Reviewed By: Scott8440
    
    Differential Revision: D89441321
    
    fbshipit-source-id: af7d93b1faa746b68fd71ed4e2e2b4568ae51007

--- fboss/platform/bsp_tests/BspTestRunner.cpp
+++ fboss/platform/bsp_tests/BspTestRunner.cpp
@@ -5,22 +5,12 @@
 #include <gtest/gtest.h>
 #include "fboss/platform/bsp_tests/BspTestEnvironment.h"
 
-#include "fboss/platform/config_lib/ConfigLib.h"
 #include "fboss/platform/helpers/Init.h"
-#include "fboss/platform/helpers/PlatformNameLib.h"
 
 DEFINE_bool(
     enable_stress_tests,
     false,
     "Enable stress tests that may take a long time to run");
-DEFINE_string(
-    bsp_tests_config_file,
-    "",
-    "[OSS-Only] Path to bsp_tests.json file.");
-DEFINE_string(
-    platform_manager_config_file,
-    "",
-    "[OSS-Only] Path to platform_manager.json file.");
 
 using namespace facebook;
 using namespace facebook::fboss::platform;
@@ -44,31 +34,11 @@ int main(int argc, char** argv) {
   ::testing::InitGoogleTest(&argc, argv);
   helpers::init(&argc, &argv);
 
-  auto platformName =
-      helpers::PlatformNameLib().getPlatformName().value_or("unknown");
-
-  XLOG(INFO) << "Starting BSP tests";
-  XLOG(INFO) << "Platform: " << platformName;
-
   ::testing::UnitTest::GetInstance()->listeners().Append(new StressTestFilter);
-
-  std::string jsonBspTestsConfig;
-  if (FLAGS_bsp_tests_config_file.empty()) {
-    jsonBspTestsConfig = ConfigLib().getBspTestConfig();
-  } else {
-    folly::readFile(FLAGS_bsp_tests_config_file.c_str(), jsonBspTestsConfig);
-  }
-  std::string jsonPMConfig;
-  if (FLAGS_platform_manager_config_file.empty()) {
-    jsonPMConfig = ConfigLib().getPlatformManagerConfig();
-  } else {
-    folly::readFile(FLAGS_platform_manager_config_file.c_str(), jsonPMConfig);
-  }
-
-  auto env = BspTestEnvironment::GetInstance(
-      platformName, jsonBspTestsConfig, jsonPMConfig);
+  ::testing::AddGlobalTestEnvironment(BspTestEnvironment::GetInstance());
 
   auto ret = RUN_ALL_TESTS();
+  auto env = BspTestEnvironment::GetInstance();
   env->printAllRecordedErrors();
   return ret;
 }
