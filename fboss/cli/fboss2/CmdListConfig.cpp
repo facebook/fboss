36568af2f6d03ae67b8b994702303cc8f3a46d08
benoit-nexthop <benoit@nexthop.ai>
2025-12-23 18:04:45 -0800

    Add a simple `fboss2 config reload` command. (#753)
    Summary:
    **Pre-submission checklist**
    - [x] I've ran the linters locally and fixed lint errors related to the files I modified in this PR. You can install the linters by running `pip install -r requirements-dev.txt && pre-commit install`
    - [x] `pre-commit run`
    
    This command just calls `reloadConfig()` on the wedge_agent.
    
    Note: this change is part of a series, the next one is https://github.com/facebook/fboss/issues/754.
    
    Pull Request resolved: https://github.com/facebook/fboss/pull/753
    
    Test Plan: Unit tests.
    
    Reviewed By: kevin645
    
    Differential Revision: D89698241
    
    Pulled By: joseph5wu
    
    fbshipit-source-id: 37c6cbe9ef175d91825a9d7b697422d25997d2e7

---
+++ fboss/cli/fboss2/CmdListConfig.cpp
@@ -0,0 +1,30 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#include "fboss/cli/fboss2/CmdList.h"
+
+#include "fboss/cli/fboss2/CmdHandler.h"
+#include "fboss/cli/fboss2/commands/config/CmdConfigReload.h"
+
+namespace facebook::fboss {
+
+const CommandTree& kConfigCommandTree() {
+  static CommandTree root = {
+      {"config",
+       "reload",
+       "Reload agent configuration",
+       commandHandler<CmdConfigReload>,
+       argTypeHandler<CmdConfigReloadTraits>},
+  };
+  sort(root.begin(), root.end());
+  return root;
+}
+
+} // namespace facebook::fboss
