c64dd21c1bac9eb5e947c07267da8f2df001e785
Benoit Sigoure <benoit@nexthop.ai>
2026-01-05 21:22:01 +0000

    Add a `fboss2 config interface <name> switchport access vlan <N>` command.
    This doesn't yet automatically create the VLAN if it doesn't exist.

---
+++ fboss/cli/fboss2/commands/config/interface/switchport/access/CmdConfigInterfaceSwitchportAccess.h
@@ -0,0 +1,43 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#pragma once
+
+#include "fboss/cli/fboss2/CmdHandler.h"
+#include "fboss/cli/fboss2/commands/config/interface/switchport/CmdConfigInterfaceSwitchport.h"
+#include "fboss/cli/fboss2/utils/CmdUtils.h"
+#include "fboss/cli/fboss2/utils/InterfaceList.h"
+
+namespace facebook::fboss {
+
+struct CmdConfigInterfaceSwitchportAccessTraits : public WriteCommandTraits {
+  using ParentCmd = CmdConfigInterfaceSwitchport;
+  static constexpr utils::ObjectArgTypeId ObjectArgTypeId =
+      utils::ObjectArgTypeId::OBJECT_ARG_TYPE_ID_NONE;
+  using ObjectArgType = std::monostate;
+  using RetType = std::string;
+};
+
+class CmdConfigInterfaceSwitchportAccess
+    : public CmdHandler<
+          CmdConfigInterfaceSwitchportAccess,
+          CmdConfigInterfaceSwitchportAccessTraits> {
+ public:
+  RetType queryClient(
+      const HostInfo& /* hostInfo */,
+      const utils::InterfaceList& /* interfaces */) {
+    throw std::runtime_error(
+        "Incomplete command, please use one of the subcommands");
+  }
+
+  void printOutput(const RetType& /* model */) {}
+};
+
+} // namespace facebook::fboss
