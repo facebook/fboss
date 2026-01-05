893888f394206e4a51f481566a134c4b517d6de0
Benoit Sigoure <benoit@nexthop.ai>
2026-01-05 21:22:01 +0000

    Add a `fboss2 config interface <name> switchport access vlan <N>` command.
    This doesn't yet automatically create the VLAN if it doesn't exist.

--- cmake/CliFboss2Test.cmake
+++ cmake/CliFboss2Test.cmake
@@ -38,6 +38,7 @@ add_executable(fboss2_cmd_test
   fboss/cli/fboss2/test/CmdConfigHistoryTest.cpp
   fboss/cli/fboss2/test/CmdConfigInterfaceDescriptionTest.cpp
   fboss/cli/fboss2/test/CmdConfigInterfaceMtuTest.cpp
+  fboss/cli/fboss2/test/CmdConfigInterfaceSwitchportAccessVlanTest.cpp
   fboss/cli/fboss2/test/CmdConfigQosBufferPoolTest.cpp
   fboss/cli/fboss2/test/CmdConfigReloadTest.cpp
   fboss/cli/fboss2/test/CmdConfigSessionDiffTest.cpp
