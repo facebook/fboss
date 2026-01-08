37d7b652b93121f1ac81a3bc74e2bcd3e008aff6
generatedunixname89002005232357 <generatedunixname89002005232357@meta.com>
2026-01-08 00:26:47 -0800

    Revert D89735114
    Summary:
    This diff reverts D89735114
    Breaking OSS
    
    Depends on D89735114
    
    Differential Revision: D90300227
    
    fbshipit-source-id: c1d5f0288034c227fe7a0f223a6c2cec0a42d881

--- fboss/lib/asic_config_v2/montblanc.py
+++ fboss/lib/asic_config_v2/montblanc.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-import neteng.fboss.asic_config_v2.thrift_types as asic_config_thrift
+import neteng.fboss.asic_config_v2.ttypes as asic_config_thrift
 import neteng.fboss.platform_mapping_config.ttypes as pm_types
 from fboss.lib.asic_config_v2.tomahawk5 import Tomahawk5AsicConfig
 from fboss.lib.platform_mapping_v2.asic_vendor_config import AsicVendorConfig
