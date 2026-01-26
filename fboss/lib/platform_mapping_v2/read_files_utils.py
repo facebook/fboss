1574453ee2e9c16bbd6bcafb0d2091a66544ebc8
Tanmaya Udupa <tanmayaudupa@meta.com>
2026-01-25 22:42:58 -0800

    use controlling port from CSV in platform_mapping.json for wedge800bact
    Summary: Add optional controlling_port field in wedge800bact_port_profile_mapping.csv and use it to generate platform_mapping.json until a good way to determine controlling port is determined.
    
    Reviewed By: Tianyu-Meta
    
    Differential Revision: D91363848
    
    fbshipit-source-id: 7922d728e12e5f65a58b6a1b1327ac3bd2787b3e

--- fboss/lib/platform_mapping_v2/read_files_utils.py
+++ fboss/lib/platform_mapping_v2/read_files_utils.py
@@ -135,7 +135,7 @@ def read_port_profile_mapping(
 ) -> PortProfileMapping:
     PORT_PROFILE_MAPPING_SUFFIX = "_port_profile_mapping.csv"
     Column = column_int_enum_generator(
-        "GLOBAL_PORT_ID LOGICAL_PORT_ID PORT_NAME SUPPORTED_PROFILES ATTACHED_COREID ATTACHED_CORE_PORTID VIRTUAL_DEVICE_ID PORT_TYPE SCOPE PARENT_PORT",
+        "GLOBAL_PORT_ID LOGICAL_PORT_ID PORT_NAME SUPPORTED_PROFILES ATTACHED_COREID ATTACHED_CORE_PORTID VIRTUAL_DEVICE_ID PORT_TYPE SCOPE PARENT_PORT CONTROLLING_PORT",
     )
     ports = {}
     for index, line in enumerate(
@@ -179,6 +179,10 @@ def read_port_profile_mapping(
             parent_port_id = int(row[Column.PARENT_PORT])
         else:
             parent_port_id = None
+        if Column.CONTROLLING_PORT < len(row) and row[Column.CONTROLLING_PORT]:
+            controlling_port = int(row[Column.CONTROLLING_PORT])
+        else:
+            controlling_port = None
         assert global_port_id not in ports
         ports[global_port_id] = Port(
             global_port_id=global_port_id,
@@ -193,6 +197,7 @@ def read_port_profile_mapping(
             # pyre-fixme[6]: Expected `Scope` for 8th param but got `int`.
             scope=int(scope),
             parent_port_id=parent_port_id,
+            controlling_port=controlling_port,
         )
     return PortProfileMapping(ports=ports)
 
