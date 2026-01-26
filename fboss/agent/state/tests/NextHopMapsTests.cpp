909cbda05acd7d20a23d13c8eb03d0c78eadca98
Tanmaya Udupa <tanmayaudupa@meta.com>
2026-01-25 22:03:56 -0800

    Add unittests for IdToNextHopSetIDMap as thriftCOW map
    Summary: as titled.
    
    Reviewed By: jasmeetbagga
    
    Differential Revision: D90476954
    
    fbshipit-source-id: 748192f3669f957cab4ec5d7c76fd37c79ef8057

--- fboss/agent/state/tests/NextHopMapsTests.cpp
+++ fboss/agent/state/tests/NextHopMapsTests.cpp
@@ -10,7 +10,6 @@
 
 #include "fboss/agent/AddressUtil.h"
 #include "fboss/agent/state/NextHopIdMaps.h"
-#include "fboss/agent/state/SwitchState.h"
 
 #include <folly/Format.h>
 #include <folly/logging/xlog.h>
@@ -67,4 +66,47 @@ TEST_F(IdToNextHopMapTest, AddGetNextHopAndIds) {
   map_->removeNextHop(2);
   EXPECT_EQ(map_->size(), 0);
 }
+
+class IdToNextHopIdSetMapTest : public ::testing::Test {
+ protected:
+  void SetUp() override {
+    nextHopIdSetMap_ = std::make_shared<IdToNextHopIdSetMap>();
+  }
+
+  std::shared_ptr<IdToNextHopIdSetMap> nextHopIdSetMap_;
+};
+
+TEST_F(IdToNextHopIdSetMapTest, AddGetNextHopIDSetAndSetIds) {
+  std::set<NextHopId> nextHopIds1{1, 2, 3};
+  std::set<NextHopId> nextHopIds2{4, 5};
+  std::set<NextHopId> emptySet;
+
+  // Add and retrieve
+  nextHopIdSetMap_->addNextHopIdSet(100, nextHopIds1);
+  nextHopIdSetMap_->addNextHopIdSet(200, nextHopIds2);
+  nextHopIdSetMap_->addNextHopIdSet(300, emptySet);
+  EXPECT_EQ(nextHopIdSetMap_->size(), 3);
+  EXPECT_EQ(nextHopIdSetMap_->getNextHopIdSet(100)->toThrift(), nextHopIds1);
+  EXPECT_EQ(nextHopIdSetMap_->getNextHopIdSet(200)->toThrift(), nextHopIds2);
+  EXPECT_TRUE(nextHopIdSetMap_->getNextHopIdSet(300)->toThrift().empty());
+
+  // GetIf returns nullptr for non-existent, doesn't throw
+  EXPECT_NE(nextHopIdSetMap_->getNextHopIdSetIf(100), nullptr);
+  EXPECT_EQ(nextHopIdSetMap_->getNextHopIdSetIf(999), nullptr);
+
+  // Throwing behavior
+  EXPECT_THROW(nextHopIdSetMap_->getNextHopIdSet(999), FbossError);
+  EXPECT_THROW(nextHopIdSetMap_->removeNextHopIdSet(999), FbossError);
+
+  // RemoveIf returns true/false
+  EXPECT_FALSE(nextHopIdSetMap_->removeNextHopIdSetIf(999));
+  EXPECT_TRUE(nextHopIdSetMap_->removeNextHopIdSetIf(100));
+  EXPECT_EQ(nextHopIdSetMap_->size(), 2);
+
+  // Remove remaining
+  nextHopIdSetMap_->removeNextHopIdSet(200);
+  nextHopIdSetMap_->removeNextHopIdSet(300);
+  EXPECT_EQ(nextHopIdSetMap_->size(), 0);
+}
+
 } // namespace facebook::fboss
