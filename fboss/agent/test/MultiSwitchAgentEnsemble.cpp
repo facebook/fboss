ccbede4d3becacb6badad26bf6c3dcebdcd2ad61
Nivin Lawrence <nivinl@meta.com>
2026-01-30 01:34:36 -0800

    Make the needed configs available early enough for HwSwitch create during tests
    Summary:
    Some tests require the hardware switch to be created with a specific DSF configuration (e.g., L2 fabric
    level). Previously, there was no way to inject such configs before switch creation. This change enables
    more flexible and accurate test setups. This change enables more precise hardware switch initialization
    in FBOSS agent tests which will be needed to avoid failures with `AgentBalancedInputModeTest` once
    the next diff in stack lands.
    
    Reviewed By: msomasundaran, jasmeetbagga
    
    Differential Revision: D91178596
    
    fbshipit-source-id: 9e333eb34871c12cd40413a24fb6f570a7bd4638

--- fboss/agent/test/MultiSwitchAgentEnsemble.cpp
+++ fboss/agent/test/MultiSwitchAgentEnsemble.cpp
@@ -53,7 +53,7 @@ std::unique_ptr<AgentEnsemble> createAgentEnsemble(
     bool disableLinkStateToggler,
     AgentEnsemblePlatformConfigFn platformConfigFn,
     uint32_t featuresDesired,
-    bool failHwCallsOnWarmboot) {
+    const TestEnsembleInitInfo& initInfo) {
   // Set multi switch flag to true for MultiSwitchAgentEnsemble
   FLAGS_multi_switch = true;
   std::unique_ptr<AgentEnsemble> ensemble =
@@ -63,7 +63,7 @@ std::unique_ptr<AgentEnsemble> createAgentEnsemble(
       disableLinkStateToggler,
       platformConfigFn,
       featuresDesired,
-      failHwCallsOnWarmboot);
+      initInfo);
   return ensemble;
 }
 
