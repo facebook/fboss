85ccbb79896d48482aa7313c0414a6a4e8874196
Tanmaya Udupa <tanmayaudupa@meta.com>
2026-02-09 23:49:56 -0800

    Unittests for exluding MPLS label_and_pop actions
    Summary: As titled. This is to ensure ID manager stays consistent with MPLS routes as well.
    
    Reviewed By: jasmeetbagga
    
    Differential Revision: D92569951
    
    fbshipit-source-id: e6c7e5631b99f12cdb03215717e7950f6101a509

--- fboss/agent/rib/test/NextHopMapPopulationTests.cpp
+++ fboss/agent/rib/test/NextHopMapPopulationTests.cpp
@@ -16,9 +16,13 @@
 #include "fboss/agent/AddressUtil.h"
 #include "fboss/agent/FibHelpers.h"
 #include "fboss/agent/SwSwitchRouteUpdateWrapper.h"
+#include "fboss/agent/if/gen-cpp2/ctrl_types.h"
+#include "fboss/agent/if/gen-cpp2/mpls_types.h"
 #include "fboss/agent/rib/NextHopIDManager.h"
 #include "fboss/agent/state/FibInfo.h"
 #include "fboss/agent/state/FibInfoMap.h"
+#include "fboss/agent/state/LabelForwardingAction.h"
+#include "fboss/agent/state/LabelForwardingInformationBase.h"
 #include "fboss/agent/state/NextHopIdMaps.h"
 #include "fboss/agent/state/RouteNextHopEntry.h"
 #include "fboss/agent/state/RouteTypes.h"
@@ -189,6 +193,30 @@ class NextHopMapPopulationTest : public ::testing::Test {
         prefix.mask(),
         kClientA,
         RouteNextHopEntry(RouteForwardAction::TO_CPU, DISTANCE));
+  }
+
+  // Add MPLS route with POP_AND_LOOKUP action.
+  // POP_AND_LOOKUP routes have a null address (::) as nexthop since forwarding
+  // is based on inner header lookup.
+  void addMplsPopAndLookupRoute(
+      SwSwitchRouteUpdateWrapper& updater,
+      MplsLabel label) {
+    MplsRoute mplsRoute;
+    mplsRoute.topLabel() = label;
+    NextHopThrift nexthop;
+    nexthop.mplsAction() = MplsAction();
+    *nexthop.mplsAction()->action() = MplsActionCode::POP_AND_LOOKUP;
+    folly::IPAddress nullAddr{"::"};
+    nexthop.address()->addr()->append(
+        reinterpret_cast<const char*>(nullAddr.bytes()),
+        folly::IPAddressV6::byteCount());
+    mplsRoute.nextHops()->emplace_back(nexthop);
+    updater.addRoute(kClientA, mplsRoute);
+  }
+
+  std::shared_ptr<MultiLabelForwardingInformationBase> getLabelFib() {
+    auto state = sw_->getState();
+    return state->getLabelForwardingInformationBase();
   }
 
   void delV4Route(
@@ -578,6 +606,47 @@ TEST_F(NextHopMapPopulationTest, SerializationTests) {
   EXPECT_EQ(
       deserializedNextHopIdSetMap->size(), originalIdToNextHopIdSetMap->size())
       << "IdToNextHopIdSetMap size should be preserved after deserialization";
+}
+
+// Test general addition of POP_AND_LOOKUP MPLS routes alongside normal
+// weighted routes. This verifies that both types of routes are handled
+// correctly
+TEST_F(NextHopMapPopulationTest, PopAndLookupWithNormalRouteAddition) {
+  // First, add standard IP routes
+  auto updater = sw_->getRouteUpdater();
+  addStandardTestRoutes(updater, false /* includeV6 */);
+
+  // Now add MPLS POP_AND_LOOKUP routes
+  addMplsPopAndLookupRoute(updater, 1001);
+  addMplsPopAndLookupRoute(updater, 1002);
+  addMplsPopAndLookupRoute(updater, 1003);
+  updater.program();
+
+  // Verify MPLS POP_AND_LOOKUP routes have resolvedNextHopSetID but NOT
+  // normalizedResolvedNextHopSetID
+  auto labelFib = getLabelFib();
+  ASSERT_NE(labelFib, nullptr);
+
+  auto labelEntry1 = labelFib->getNodeIf(1001);
+  ASSERT_NE(labelEntry1, nullptr);
+  const auto& labelFwdInfo1 = labelEntry1->getForwardInfo();
+  EXPECT_TRUE(labelFwdInfo1.getResolvedNextHopSetID().has_value())
+      << "POP_AND_LOOKUP MPLS route should have resolvedNextHopSetID";
+  EXPECT_FALSE(labelFwdInfo1.getNormalizedResolvedNextHopSetID().has_value())
+      << "POP_AND_LOOKUP MPLS route should NOT have "
+      << "normalizedResolvedNextHopSetID";
+
+  auto labelEntry2 = labelFib->getNodeIf(1002);
+  ASSERT_NE(labelEntry2, nullptr);
+  const auto& labelFwdInfo2 = labelEntry2->getForwardInfo();
+  EXPECT_TRUE(labelFwdInfo2.getResolvedNextHopSetID().has_value())
+      << "POP_AND_LOOKUP MPLS route should have resolvedNextHopSetID";
+  EXPECT_FALSE(labelFwdInfo2.getNormalizedResolvedNextHopSetID().has_value())
+      << "POP_AND_LOOKUP MPLS route should NOT have "
+      << "normalizedResolvedNextHopSetID";
+
+  verifyIdMapsMatchIdManager();
+  verifyIDMapsConsistency();
 }
 
 } // namespace facebook::fboss
