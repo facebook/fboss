2acfb5a335a05800711e24cf57286dabf21d787c
Gang Tao <gang.tao@celestica.com>
2026-02-10 08:08:00 +0000

    Ladakh800bcls: route packets to correct switch in AgentCoppTests

--- fboss/agent/MultiHwSwitchHandler.h
+++ fboss/agent/MultiHwSwitchHandler.h
@@ -80,7 +80,9 @@ class MultiHwSwitchHandler {
 
   bool sendPacketSwitchedSync(std::unique_ptr<TxPacket> pkt) noexcept;
 
-  bool sendPacketSwitchedAsync(std::unique_ptr<TxPacket> pkt) noexcept;
+  bool sendPacketSwitchedAsync(
+      std::unique_ptr<TxPacket> pkt,
+      std::optional<SwitchID> switchId = std::nullopt) noexcept;
 
   bool transactionsSupported() const;
 
