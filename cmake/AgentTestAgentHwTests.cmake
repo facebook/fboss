791bf2a23b4c5394dd0dd75c79b6fa5059f0a83f
Jasmeet Bagga <jasmeetbagga@meta.com>
2026-02-16 23:36:31 -0800

    Add AgentMPLSTests.cpp to cmake build
    Summary: Add the migrated AgentMPLSTests.cpp to AgentTestAgentHwTests.cmake so it is included in the open-source cmake build.
    
    Reviewed By: shri-khare
    
    Differential Revision: D93373495
    
    fbshipit-source-id: 755395128aaaa5c3973ad4a81de57a58ff5a32ab

--- cmake/AgentTestAgentHwTests.cmake
+++ cmake/AgentTestAgentHwTests.cmake
@@ -30,6 +30,7 @@ add_library(agent_hw_test_src
   fboss/agent/test/agent_hw_tests/AgentMacLearningAndNeighborResolutionTests.cpp
   fboss/agent/test/agent_hw_tests/AgentMirroringTests.cpp
   fboss/agent/test/agent_hw_tests/AgentMirroringScaleTests.cpp
+  fboss/agent/test/agent_hw_tests/AgentMPLSTests.cpp
   fboss/agent/test/agent_hw_tests/AgentNSFScaleTests.cpp
   fboss/agent/test/agent_hw_tests/AgentNeighborTests.cpp
   fboss/agent/test/agent_hw_tests/AgentOlympicQosTests.cpp
