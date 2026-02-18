d7286ede4723550fb2825a3604b8d2f839448913
Scott Smith <smithscott@meta.com>
2026-02-18 15:03:17 -0800

    Add StructredLogger for logging with network_event_logs
    Summary: Adds a StructuredLogger class to format logs in a way that are easily recognized and parsed by the network event logger.
    
    Reviewed By: somasun
    
    Differential Revision: D91353883
    
    fbshipit-source-id: f73321ad8af5baae6d251b90132951cf4aa289d3

---
+++ fboss/platform/helpers/StructuredLogger.h
@@ -0,0 +1,54 @@
+// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+#pragma once
+
+#include <string>
+#include <unordered_map>
+
+namespace facebook::fboss::platform::helpers {
+
+/**
+ * StructuredLogger provides utilities for logging structured events that can
+ * be parsed by net_event_log.
+ *
+ * Output format:
+ *   FBOSS_EVENT event_name <key1:value1> <key2:value2> ...
+ *   FBOSS_ALERT event_name <key1:value1> <key2:value2> ...
+ *
+ * Tags are to be extracted as key-value pairs
+ */
+class StructuredLogger {
+ public:
+  explicit StructuredLogger(const std::string& serviceName);
+
+  /**
+   * Log an event at INFO level with FBOSS_EVENT marker.
+   */
+  void logEvent(
+      const std::string& event,
+      const std::unordered_map<std::string, std::string>& tags = {});
+
+  /**
+   * Log an alert at ERR level with FBOSS_ALERT marker.
+   */
+  void logAlert(
+      const std::string& event,
+      const std::string& errorMsg,
+      const std::unordered_map<std::string, std::string>& tags = {});
+
+  /**
+   * Set persistent tags that will be included in all future logs.
+   */
+  void setTags(const std::unordered_map<std::string, std::string>& tags);
+
+ private:
+  std::string formatTags(
+      const std::string& event,
+      const std::unordered_map<std::string, std::string>& tags) const;
+
+  std::string serviceName_;
+  std::string platformName_;
+  std::unordered_map<std::string, std::string> persistentTags_;
+};
+
+} // namespace facebook::fboss::platform::helpers
