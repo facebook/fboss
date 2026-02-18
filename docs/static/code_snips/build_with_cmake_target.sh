329fbd39299e890025af196a345ef6d19f3a7717
Kevin Yakar <rxbevin@meta.com>
2026-02-18 12:22:14 -0800

    Enable clang by default in FBOSS OSS build (#938)
    Summary:
    Pull Request resolved: https://github.com/facebook/fboss/pull/938
    
    This diff contains changes that enable clang by default during an FBOSS OSS build.
    
    Now:
    - GHA workflows will use clang by default
    - Vendors will be using clang when building by following the instructions in our OSS wiki
      - if facing problems related to clang, they can easily use gcc by passing `--build-arg USE_CLANG=false` when building the docker image as we triage
    - Conveyor still uses gcc, future diff will include a clang build node which runs alongside the gcc build node
    
    Reviewed By: joseph5wu
    
    Differential Revision: D93245720
    
    fbshipit-source-id: a8125aea999a8a0f7018b6d9a27faa083a375aad

--- docs/static/code_snips/build_with_cmake_target.sh
+++ docs/static/code_snips/build_with_cmake_target.sh
@@ -3,5 +3,5 @@ cd /var/FBOSS/fboss
 
 # Build using a cmake target
 time ./fboss/oss/scripts/run-getdeps.py build --allow-system-packages \
---extra-cmake-defines='{"CMAKE_BUILD_TYPE": "MinSizeRel", "CMAKE_CXX_STANDARD": "20"}' \
---scratch-path /var/FBOSS/tmp_bld_dir --cmake-target $TARGET fboss
+  --extra-cmake-defines='{"CMAKE_BUILD_TYPE": "MinSizeRel", "CMAKE_CXX_STANDARD": "20", "RANGE_V3_TESTS": "OFF", "RANGE_V3_PERF": "OFF"}' \
+  --scratch-path /var/FBOSS/tmp_bld_dir --cmake-target $TARGET fboss
