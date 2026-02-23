1d2b71cb8ddc44c574b08044deae54f21a110e83
Scott Smith <smithscott@meta.com>
2026-02-23 10:32:41 -0800

    Add structured logging to data_corral_service
    Summary:
    Integrate the shared StructuredLogger into data_corral_service to enable
    structured logging to Scuba.
    
    Events logged:
    - fru_presence_detection_failure: When detecting FRU presence fails
    - fru_led_program_failure: When programming a FRU LED fails
    - system_led_program_failure: When programming the system LED fails
    
    Also adds crash logging via try-catch in Main.cpp to capture any
    unhandled exceptions.
    
    Reviewed By: somasun
    
    Differential Revision: D90884860
    
    fbshipit-source-id: 9e8f95a5b535dceec256f10e6fde2041043aea4b

--- fboss/platform/data_corral_service/Main.cpp
+++ fboss/platform/data_corral_service/Main.cpp
@@ -12,10 +12,12 @@
 #include "fboss/platform/data_corral_service/FruPresenceExplorer.h"
 #include "fboss/platform/helpers/Init.h"
 #include "fboss/platform/helpers/PlatformNameLib.h"
+#include "fboss/platform/helpers/StructuredLogger.h"
 
 using namespace facebook;
 using namespace facebook::fboss::platform;
 using namespace facebook::fboss::platform::data_corral_service;
+using facebook::fboss::platform::helpers::StructuredLogger;
 
 DEFINE_int32(
     refresh_interval_ms,
@@ -28,34 +30,41 @@ int main(int argc, char** argv) {
   fb303::registerFollyLoggingOptionHandlers();
   helpers::init(&argc, &argv);
 
-  LedManagerConfig config;
   auto platformName = helpers::PlatformNameLib().getPlatformName();
-  auto configJson = ConfigLib().getLedManagerConfig(platformName);
-  apache::thrift::SimpleJSONSerializer::deserialize<LedManagerConfig>(
-      configJson, config);
 
-  if (!ConfigValidator().isValid(config)) {
-    XLOG(ERR) << "Invalid LedManager config";
-    throw std::runtime_error("Invalid LedManager config");
+  try {
+    LedManagerConfig config;
+    auto configJson = ConfigLib().getLedManagerConfig(platformName);
+    apache::thrift::SimpleJSONSerializer::deserialize<LedManagerConfig>(
+        configJson, config);
+
+    if (!ConfigValidator().isValid(config)) {
+      XLOG(ERR) << "Invalid LedManager config";
+      throw std::runtime_error("Invalid LedManager config");
+    }
+
+    auto ledManager = std::make_shared<LedManager>(
+        *config.systemLedConfig(), *config.fruTypeLedConfigs());
+    auto fruPresenceExplorer =
+        std::make_shared<FruPresenceExplorer>(*config.fruConfigs(), ledManager);
+
+    folly::FunctionScheduler scheduler;
+    scheduler.addFunction(
+        [&]() { fruPresenceExplorer->detectFruPresence(); },
+        std::chrono::milliseconds(FLAGS_refresh_interval_ms),
+        "PresenceDetection");
+    scheduler.start();
+
+    auto server = std::make_shared<apache::thrift::ThriftServer>();
+    auto handler = std::make_shared<DataCorralServiceThriftHandler>();
+
+    helpers::runThriftService(
+        server, handler, "DataCorralService", FLAGS_thrift_port);
+  } catch (const std::exception& ex) {
+    StructuredLogger logger("data_corral_service");
+    logger.logAlert("unexpected_crash", ex.what());
+    throw;
   }
-
-  auto ledManager = std::make_shared<LedManager>(
-      *config.systemLedConfig(), *config.fruTypeLedConfigs());
-  auto fruPresenceExplorer =
-      std::make_shared<FruPresenceExplorer>(*config.fruConfigs(), ledManager);
-
-  folly::FunctionScheduler scheduler;
-  scheduler.addFunction(
-      [&]() { fruPresenceExplorer->detectFruPresence(); },
-      std::chrono::milliseconds(FLAGS_refresh_interval_ms),
-      "PresenceDetection");
-  scheduler.start();
-
-  auto server = std::make_shared<apache::thrift::ThriftServer>();
-  auto handler = std::make_shared<DataCorralServiceThriftHandler>();
-
-  helpers::runThriftService(
-      server, handler, "DataCorralService", FLAGS_thrift_port);
 
   XLOG(INFO) << "================ STOPPED PLATFORM BINARY ================";
   return 0;
