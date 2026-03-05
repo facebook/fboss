d524f73ddab9ab42b233f8beb0df8b1e3b9b3cae
Daniel Long <daniellong@meta.com>
2026-03-05 13:06:20 -0800

    added HwCpuLantency Benchmark Helper for organzie the common usage
    Summary:
    abstracted the helper method for hwcpulatency
    make .cpp as the thin file
    
    next step:
    
    change the sequence number from global count to batch based. (fixed)
    dig into latency miss match
    
    Reviewed By: tanmayaudupa
    
    Differential Revision: D94392384
    
    fbshipit-source-id: 56bddf566d793bd71b21feaf6443777c8606e9cf

--- fboss/agent/hw/benchmarks/HwCpuLatencyBenchmark.cpp
+++ fboss/agent/hw/benchmarks/HwCpuLatencyBenchmark.cpp
@@ -1,119 +1,13 @@
 // (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
 
-#include "fboss/agent/AsicUtils.h"
-#include "fboss/agent/SwSwitchRouteUpdateWrapper.h"
-#include "fboss/agent/TxPacket.h"
-#include "fboss/agent/benchmarks/AgentBenchmarks.h"
-#include "fboss/agent/packet/PktFactory.h"
-#include "fboss/agent/test/EcmpSetupHelper.h"
-#include "fboss/agent/test/utils/ConfigUtils.h"
-#include "fboss/agent/test/utils/CoppTestUtils.h"
-#include "fboss/agent/test/utils/OlympicTestUtils.h"
-#include "fboss/agent/test/utils/PacketSnooper.h"
-#include "fboss/agent/test/utils/QosTestUtils.h"
-#include "fboss/agent/test/utils/TrapPacketUtils.h"
-
-#include <folly/Benchmark.h>
-#include <folly/IPAddress.h>
-#include <folly/json/dynamic.h>
-#include <folly/json/json.h>
-#include <folly/logging/xlog.h>
-
-#include <chrono>
-#include <iostream>
+#include "fboss/agent/hw/benchmarks/HwCpuLatencyBenchmarkHelper.h"
 
 namespace facebook::fboss {
 
-namespace {
+// Single packet test - baseline latency measurement
+CPU_LATENCY_BENCHMARK(CpuLatency_1Batch_1Packet, 1, 1)
 
-// Constants
-const folly::IPAddressV6 kDstIp{"2620:0:1cfe:face:b00c::4"};
-[[maybe_unused]] constexpr std::chrono::seconds kPacketTimeout{60};
-[[maybe_unused]] constexpr uint8_t kNetworkControlDscp = 48;
-constexpr size_t kPayloadSize = 12;
-const folly::MacAddress kSrcMac{"fa:ce:b0:00:00:0c"};
-const folly::IPAddressV6 kSrcIp{"2620:0:1cfe:face:b00c::3"};
+// Multiple iterations of single packet - consistency measurement
+CPU_LATENCY_BENCHMARK(CpuLatency_10Batches_1Packet, 10, 1)
 
-// Types
-struct DecodedPayload {
-  bool valid{false};
-  uint64_t timestampNs{0};
-  uint32_t sequenceNumber{0};
-};
-
-struct LatencyResult {
-  bool success{false};
-  double latencyMs{0.0};
-  uint32_t expectedSeq{0};
-  uint32_t receivedSeq{0};
-  bool sequenceMatched{false};
-};
-
-// Payload encoding/decoding
-[[maybe_unused]] std::vector<uint8_t> encodePayload(uint32_t sequenceNumber) {
-  folly::IOBuf buf(folly::IOBuf::CREATE, kPayloadSize);
-  buf.append(kPayloadSize);
-
-  folly::io::RWPrivateCursor cursor(&buf);
-  uint64_t timestampNs =
-      std::chrono::duration_cast<std::chrono::nanoseconds>(
-          std::chrono::steady_clock::now().time_since_epoch())
-          .count();
-
-  cursor.writeBE(timestampNs);
-  cursor.writeBE(sequenceNumber);
-
-  return std::vector(buf.data(), buf.data() + buf.length());
-}
-
-[[maybe_unused]] DecodedPayload decodePayload(const folly::IOBuf& rxBuf) {
-  DecodedPayload result;
-
-  size_t kMinUdpPacketSize = EthHdr::UNTAGGED_PKT_SIZE + IPv6Hdr::SIZE +
-      UDPHeader::size() + kPayloadSize;
-  size_t totalLength = rxBuf.computeChainDataLength();
-
-  if (totalLength < kMinUdpPacketSize) {
-    XLOG(DBG4) << "Packet too small: " << totalLength
-               << " bytes, minimum required: " << kMinUdpPacketSize;
-    return result;
-  }
-
-  folly::io::Cursor cursor(&rxBuf);
-
-  size_t payloadOffset = totalLength - kPayloadSize;
-  cursor.skip(payloadOffset);
-  result.timestampNs = cursor.readBE<uint64_t>();
-  result.sequenceNumber = cursor.readBE<uint32_t>();
-  result.valid = true;
-
-  return result;
-}
-
-// Packet creation
-[[maybe_unused]] std::unique_ptr<TxPacket> createLatencyTestPacket(
-    AgentEnsemble* ensemble,
-    uint32_t sequenceNumber) {
-  auto vlanId = ensemble->getVlanIDForTx();
-  auto intfMac =
-      utility::getMacForFirstInterfaceWithPorts(ensemble->getProgrammedState());
-
-  auto payload = encodePayload(sequenceNumber);
-  uint8_t trafficClass = kNetworkControlDscp << 2;
-
-  return utility::makeUDPTxPacket(
-      ensemble->getSw(),
-      vlanId,
-      kSrcMac,
-      intfMac,
-      kSrcIp,
-      kDstIp,
-      8000,
-      8001,
-      trafficClass,
-      255,
-      payload);
-}
-
-} // namespace
 } // namespace facebook::fboss
