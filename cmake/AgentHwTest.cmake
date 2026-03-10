734394843cbdf38b2b9107b8db21ad777bdee2be
Ravi Vantipalli <rvantipalli@meta.com>
2026-03-10 10:04:05 -0700

    Remove fully-migrated HwPacketSendTests.cpp from old HwTest framework
    Summary:
    Delete HwPacketSendTests.cpp as all tests have been migrated to the new
    AgentHwTest framework in AgentPacketSendTests.cpp.
    
    Test matching:
    - HwPacketSendReceiveLagTest::LacpPacketReceiveSrcPort -> AgentPacketSendReceiveLagTest::LacpPacketReceiveSrcPort
    - HwPacketFloodTest::ArpRequestFloodTest -> AgentPacketFloodTest::ArpRequestFloodTest
    - HwPacketFloodTest::NdpFloodTest -> AgentPacketFloodTest::NdpFloodTest
    
    New file adds 7 additional tests not present in old file.
    No orphaned headers. No CMake references.
    
    Reviewed By: msomasundaran
    
    Differential Revision: D95567650
    
    fbshipit-source-id: 4705565da6c4203c4877a141d0a7ae8517def527

--- cmake/AgentHwTest.cmake
+++ cmake/AgentHwTest.cmake
@@ -241,7 +241,6 @@ set(hw_switch_test_srcs
   fboss/agent/hw/test/dataplane_tests/HwTeFlowTrafficTests.cpp
   fboss/agent/hw/test/dataplane_tests/HwProdInvariantHelper.cpp
   fboss/agent/hw/test/dataplane_tests/HwProdInvariantTests.cpp
-  fboss/agent/hw/test/dataplane_tests/HwPacketSendTests.cpp
   fboss/agent/hw/test/dataplane_tests/HwSflowTests.cpp
   fboss/agent/hw/test/dataplane_tests/HwTestAqmUtils.cpp
   fboss/agent/hw/test/dataplane_tests/HwTestQosUtils.cpp
