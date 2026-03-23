c4600a9c7172f1f6210001699203dfa0da31f1c8
generatedunixname89002005232357 <generatedunixname89002005232357@meta.com>
2026-03-23 15:02:08 -0700

    Revert D90707712
    Summary:
    This diff reverts D90707712
    The diff is breaking warm boot for Chenab. We always will end up recreating Port Serdes over warm boot causing traffic drop. This happens because SDK returns same value for PreEmphasis as that of Pre1. SDK does that whether or not FBOSS set the value for PreEmphasis. PreEmphasis is not part of Serdes config on chenab. As a result FBOSS doesn't set it. FBOSS sets Pre1. The warm boot ends up recreating Serdes because PreEmphasis is -15 in SDK but not in FBOSS. This can lead to packet drop on warm boot.
    
    Solutions in order of preference/turn around time
    - Fix serdes config so Pre1 and premphasis are same for FBOSS on Chenab
    - Fix FBOSS to silently program PreEmphasis same as that of Pre1
    - Fix SDK
    
    Depends on D90707712
    
    Reviewed By: birdsoup
    
    Differential Revision: D97767987
    
    fbshipit-source-id: e444c943802b5f61c56c3529555d79f8f4ccb83c

--- fboss/agent/hw/switch_asics/ChenabAsic.cpp
+++ fboss/agent/hw/switch_asics/ChenabAsic.cpp
@@ -424,7 +424,7 @@ uint32_t ChenabAsic::getSflowShimHeaderSize() const {
   return 9;
 }
 std::optional<uint32_t> ChenabAsic::getPortSerdesPreemphasis() const {
-  return std::nullopt;
+  return -15; // must be same as pre1
 }
 uint32_t ChenabAsic::getPacketBufferUnitSize() const {
   return 192;
