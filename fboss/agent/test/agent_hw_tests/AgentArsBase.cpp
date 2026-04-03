56e3fbb27c0fe883b7cb11103037ec2d680c9a22
Ruinan Hu <ruinanhu@meta.com>
2026-04-03 12:08:58 -0700

    temp fix to correct acl counter used in ars test
    Reviewed By: daiwei1983
    
    Differential Revision: D99372831
    
    fbshipit-source-id: 5067d88c4b8fb7256a813cde28e36ac0fa12689b

--- fboss/agent/test/agent_hw_tests/AgentArsBase.cpp
+++ fboss/agent/test/agent_hw_tests/AgentArsBase.cpp
@@ -329,8 +329,14 @@ auto AgentArsBase::verifyAclType(bool bumpOnHit, AclType aclType) {
       EXPECT_EVENTUALLY_GE(aclPktCountAfter, aclPktCountBefore + 1);
       // At most we should get a pkt bump of 2
       EXPECT_EVENTUALLY_LE(aclPktCountAfter, aclPktCountBefore + 2);
+      // TODO ruinanhu: Remove this once we have a fix for TH6 counter problem
+      auto hwAsic = checkSameAndGetAsic(getAgentEnsemble()->getL3Asics());
+      auto extraBytes =
+          (hwAsic->getAsicType() == cfg::AsicType::ASIC_TYPE_TOMAHAWK6) ? 4 : 0;
+
       EXPECT_EVENTUALLY_GE(
-          aclBytesCountAfter, aclBytesCountBefore + sizeOfPacketSent);
+          aclBytesCountAfter + extraBytes,
+          aclBytesCountBefore + sizeOfPacketSent);
       // On native BCM we see 4 extra bytes in the acl counter. This is
       // likely due to ingress vlan getting imposed and getting counted
       // when packet hits acl in ingress pipeline
