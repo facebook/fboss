a7c382b8bf33d74cb26dee564f2cf791f2258a14
Anna Komarova <anna@nexthop.ai>
2026-04-06 17:19:24 +0000

    Address review comments

--- fboss/oss/scripts/run_scripts/run_test.py
+++ fboss/oss/scripts/run_scripts/run_test.py
@@ -604,11 +604,9 @@ class TestRunner(abc.ABC):
         else:
             test_names = self._list_tests_to_run("*")
         test_filter = ""
-        known_bad_test_regexes = self._get_known_bad_test_regexes()
-        unsupported_test_regexes = self._get_unsupported_test_regexes()
         for test_name in test_names:
-            if any(re.match(r, test_name) for r in known_bad_test_regexes) or any(
-                re.match(r, test_name) for r in unsupported_test_regexes
+            if self._is_known_bad_test(test_name) or self._is_unsupported_test(
+                test_name
             ):
                 continue
             test_filter += f"{test_name}:"
