7563d549a47f6990e22552183631e293fbdd867c
Shrikrishna (Shri) Khare <skhare@meta.com>
2026-04-06 00:19:40 -0700

    Migrate ECMPHalfTrunkFull4X3WideTrunksV4MplsSwapFrontPanelTraffic MPLS test to AgentHwTest
    Summary:
    Migrate ECMPHalfTrunkFull4X3WideTrunksV4MplsSwapFrontPanelTraffic from
    HwTrunkLoadBalancerTests to AgentMplsTrunkLoadBalancerTest,
    moving from the old HwLinkStateDependentTest framework to
    the new AgentHwTest framework. Uses MPLS production feature
    gating to run only on MPLS-capable ASICs.
    
    Reviewed By: jasmeetbagga
    
    Differential Revision: D99619219
    
    fbshipit-source-id: 7f8fec4d7e50cd7d2c1e98d035e88a9592953e3e

--- fboss/agent/test/agent_hw_tests/AgentTrunkLoadBalancerTests.cpp
+++ fboss/agent/test/agent_hw_tests/AgentTrunkLoadBalancerTests.cpp
@@ -789,6 +789,16 @@ TEST_F(
       true /* loopThroughFrontPanelPort */);
 }
 
+TEST_F(
+    AgentMplsTrunkLoadBalancerTest,
+    ECMPHalfTrunkFull4X3WideTrunksV4MplsSwapFrontPanelTraffic) {
+  runLoadBalanceTest(
+      TrafficType::v4MPLS4Swap,
+      getEcmpHalfTrunkFullHashConfig(getAgentEnsemble()->getL3Asics()),
+      k4X3WideAggs,
+      true /* loopThroughFrontPanelPort */);
+}
+
 TEST_F(AgentSrv6TrunkLoadBalancerTest, Srv6TrunkEcmpLoadBalance) {
   auto setup = [this]() { setupSrv6TrunkECMP(); };
   auto verify = [this]() {
