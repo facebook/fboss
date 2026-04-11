79e5ced5e1e68fbfd4815a940d89b47a167da182
Ruinan Hu <ruinanhu@meta.com>
2026-04-11 04:12:08 -0700

    disable SRV6_MYSID_DISCARD_COUNTER on TH6
    Summary:
    SRV6_MYSID_DISCARD_COUNTER cause crash on TH6
    
    https://www.internalfb.com/phabricator/paste/view/P2269031889
    
    ___
    
    overriding_review_checks_triggers_an_audit_and_retroactive_review
    Oncall Short Name: fboss_agent
    
    Differential Revision: D100445241
    
    fbshipit-source-id: ef7eba83d9f3cd1aefd2ed2d29a0eb5fb1c7900e

--- fboss/agent/hw/switch_asics/Tomahawk6Asic.cpp
+++ fboss/agent/hw/switch_asics/Tomahawk6Asic.cpp
@@ -112,7 +112,6 @@ bool Tomahawk6Asic::isSupported(Feature feature) const {
     case HwAsic::Feature::ARS:
     case HwAsic::Feature::VIRTUAL_ARS_GROUP:
     case HwAsic::Feature::CUT_THROUGH_FORWARDING:
-    case HwAsic::Feature::SRV6_MYSID_DISCARD_COUNTER:
     case HwAsic::Feature::SAI_SERDES_RX_REACH:
     case HwAsic::Feature::SAI_SERDES_PRECODING:
     case HwAsic::Feature::PORT_SERDES_ZERO_PREEMPHASIS:
@@ -237,6 +236,7 @@ bool Tomahawk6Asic::isSupported(Feature feature) const {
     case HwAsic::Feature::PORT_LEVEL_BUFFER_CONFIGURATION_SUPPORT:
     case HwAsic::Feature::ARS_FUTURE_PORT_LOAD:
     case HwAsic::Feature::ECN_PROBABILISTIC_MARKING:
+    case HwAsic::Feature::SRV6_MYSID_DISCARD_COUNTER:
       return false;
   }
   return false;
