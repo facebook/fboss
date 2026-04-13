2730c47476ed86d83559bd130e408d935e872ac1
Jasmeet Bagga <jasmeetbagga@meta.com>
2026-04-13 14:56:44 -0700

    Add SRV6_MYSID_RESOURCE_COUNTER HwAsic feature
    Summary:
    Add SRV6_MYSID_RESOURCE_COUNTER feature flag to HwAsic::Feature enum.
    Disabled (false) on all ASICs. This will be used to gate querying
    SAI_SWITCH_ATTR_AVAILABLE_MY_SID_ENTRY for MySid resource usage tracking.
    
    ___
    
    overriding_review_checks_triggers_an_audit_and_retroactive_review
    Oncall Short Name: network_er_datacenter
    
    Differential Revision: D100659477
    
    fbshipit-source-id: db14cd04372e204aceb916dabbed919d5c983c9c

--- fboss/agent/hw/switch_asics/Agera3PhyAsic.cpp
+++ fboss/agent/hw/switch_asics/Agera3PhyAsic.cpp
@@ -25,6 +25,7 @@ bool Agera3PhyAsic::isSupported(Feature feature) const {
     case HwAsic::Feature::VIRTUAL_ARS_GROUP:
     case HwAsic::Feature::CUT_THROUGH_FORWARDING:
     case HwAsic::Feature::SRV6_MYSID_DISCARD_COUNTER:
+    case HwAsic::Feature::SRV6_MYSID_RESOURCE_COUNTER:
     case HwAsic::Feature::ECN_PROBABILISTIC_MARKING:
     case HwAsic::Feature::SAI_SERDES_PRECODING:
       return false;
--- fboss/agent/hw/switch_asics/ChenabAsic.cpp
+++ fboss/agent/hw/switch_asics/ChenabAsic.cpp
@@ -242,6 +242,7 @@ bool ChenabAsic::isSupportedNonFabric(Feature feature) const {
     case HwAsic::Feature::VIRTUAL_ARS_GROUP:
     case HwAsic::Feature::CUT_THROUGH_FORWARDING:
     case HwAsic::Feature::SRV6_MYSID_DISCARD_COUNTER:
+    case HwAsic::Feature::SRV6_MYSID_RESOURCE_COUNTER:
     case HwAsic::Feature::ECN_PROBABILISTIC_MARKING:
       return false;
   }
