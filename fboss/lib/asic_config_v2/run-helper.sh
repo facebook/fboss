fa615d92c247df081ac8952304fffcdef57faf8f
marif-nexthop <marif@nexthop.ai>
2026-04-23 00:09:00 +0000

    disable range-v3 tests via cmake defines
    range-v3's test suite fails to compile under clang's -Werror,-Wnrvo.
    Disable it at the cmake level via --extra-cmake-defines.

--- fboss/lib/asic_config_v2/run-helper.sh
+++ fboss/lib/asic_config_v2/run-helper.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-python3 fboss/lib/oss/run-helper.py --target fboss-asic-config-gen.GEN_PY_EXE "$@"
+python3 fboss/lib/oss/run-helper.py \
+  --target fboss-asic-config-gen.GEN_PY_EXE \
+  --extra-cmake-defines='{"RANGE_V3_TESTS": "OFF"}' \
+  "$@"
