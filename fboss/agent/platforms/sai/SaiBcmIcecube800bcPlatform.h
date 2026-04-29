f8ca2eb9a1561342671d872d5ebe34f108f87c90
Ravi Vantipalli <rvantipalli@meta.com>
2026-04-29 08:52:50 -0700

    Update available cells on all TH6 platforms
    Summary:
    as titled
    Also update same for queue/pg static limits in coop
    
    Reviewed By: nivinl
    
    Differential Revision: D102922114
    
    fbshipit-source-id: ada6ce28201653b397ac7b1e0fd864cdc0114d28

--- fboss/agent/platforms/sai/SaiBcmIcecube800bcPlatform.h
+++ fboss/agent/platforms/sai/SaiBcmIcecube800bcPlatform.h
@@ -28,7 +28,7 @@ class SaiBcmIcecube800bcPlatform : public SaiBcmPlatform {
   }
 
   uint32_t numCellsAvailable() const override {
-    return 610000;
+    return 616994;
   }
 
   bool isSerdesApiSupported() const override {
