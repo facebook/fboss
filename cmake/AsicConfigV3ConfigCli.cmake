ab8fddddbc7a2acb17c2532429caf62d015bb6e6
marif-nexthop <marif@nexthop.ai>
2026-05-13 14:43:12 -0700

    "asic_config_v3" part 1 - basic skeleton
    Implements `asic_config_v3` design.
    
    Add `fboss/lib/asic_config_v3/` Python package skeleton along
    with a fully defined CMake build target. This creates the basic infra of
    the data-driven ASIC config generator (`asic_config_v3`) that will
    replace the code-driven
    `asic_config_v2` path.
    
    The `gen.py` entry point is included with an empty `_GENERATOR_REGISTRY`
    so
    the `fboss-asic-config-v3-gen` target is buildable immediately; registry
    entries for concrete ASIC families will be added in follow-up commits.
    
    Ran `fboss/lib/asic_config_v3/run-helper.sh`.

---
+++ cmake/AsicConfigV3ConfigCli.cmake
@@ -0,0 +1,37 @@
+# CMake to build libraries and binaries in fboss/lib/asic_config_v3
+# Data-driven ASIC config generation
+
+include(FBPythonBinary)
+
+set(
+    ASIC_CONFIG_V3_PY_SRCS
+    "fboss/lib/asic_config_v3/__init__.py"
+    "fboss/lib/asic_config_v3/base_generator.py"
+    "fboss/lib/asic_config_v3/gen.py"
+    "fboss/lib/asic_config_v3/generators/__init__.py"
+    "fboss/lib/platform_mapping_v2/asic_vendor_config.py"
+    "fboss/lib/platform_mapping_v2/gen.py"
+    "fboss/lib/platform_mapping_v2/helpers.py"
+    "fboss/lib/platform_mapping_v2/platform_mapping_v2.py"
+    "fboss/lib/platform_mapping_v2/port_profile_mapping.py"
+    "fboss/lib/platform_mapping_v2/profile_settings.py"
+    "fboss/lib/platform_mapping_v2/read_files_utils.py"
+    "fboss/lib/platform_mapping_v2/si_settings.py"
+    "fboss/lib/platform_mapping_v2/static_mapping.py"
+)
+
+add_fb_thrift_python_executable(
+    fboss-asic-config-v3-gen
+    MAIN_MODULE fboss.lib.asic_config_v3.gen:generate_all_asic_configs
+    SOURCES ${ASIC_CONFIG_V3_PY_SRCS}
+    DEPENDS
+        platform_config_python
+        switch_config_python
+        transceiver_python
+        phy_python
+        platform_mapping_config_python
+        fboss_common_python
+        python-pyyaml::python-pyyaml
+)
+
+install_fb_python_executable(fboss-asic-config-v3-gen)
