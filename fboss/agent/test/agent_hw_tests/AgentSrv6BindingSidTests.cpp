3099225eb0f5d1ff2c849315a1f65ec1d11053d2
Jasmeet Bagga <jasmeetbagga@meta.com>
2026-05-15 20:58:54 -0700

    Add first binding sid test
    Summary:
    As titled. Test is not working right now, but
    sending it along so we can review and land for vendor debug.
    I am going to block enabling binding sid feature on requisite ASICs
    till this is resolved with vendor.
    
    Reviewed By: zechengh09
    
    Differential Revision: D105289282
    
    fbshipit-source-id: 24db4bf82227f24ad3920c1dfc509e57f05d1978

--- fboss/agent/test/agent_hw_tests/AgentSrv6BindingSidTests.cpp
+++ fboss/agent/test/agent_hw_tests/AgentSrv6BindingSidTests.cpp
@@ -4,6 +4,7 @@
 #include "fboss/agent/AgentFeatures.h"
 #include "fboss/agent/AsicUtils.h"
 #include "fboss/agent/SwSwitchRouteUpdateWrapper.h"
+#include "fboss/agent/ThriftHandler.h"
 #include "fboss/agent/gen-cpp2/switch_config_types.h"
 #include "fboss/agent/hw/test/ConfigFactory.h"
 #include "fboss/agent/if/gen-cpp2/common_types.h"
@@ -44,8 +45,8 @@ class AgentSrv6BindingSidTest : public AgentHwTest {
   static inline const folly::IPAddressV6 kSid0{"3001:db8:1:2:3:4:5:6"};
   static inline const folly::IPAddressV6 kSid1{"3001:db8:4:5:6::"};
 
-  const folly::IPAddressV6 kEncapRoutePrefix{"2800:2::"};
-  static constexpr uint8_t kEncapRoutePrefixLen{64};
+  const folly::IPAddressV6 kMySidPrefix{"fdad:ffff:1::"};
+  static constexpr uint8_t kMySidPrefixLen{48};
   static constexpr int kNumNextHops{4};
 
   std::vector<ProductionFeature> getProductionFeaturesVerified()
@@ -78,11 +79,9 @@ class AgentSrv6BindingSidTest : public AgentHwTest {
     }
     addSrv6TunnelConfig(cfg);
     auto asic = checkSameAndGetAsicForTesting(ensemble.getL3Asics());
-    utility::addTrapPacketAcl(
-        asic,
-        &cfg,
-        std::set<folly::CIDRNetwork>{
-            folly::CIDRNetwork{kSid0, 128}, folly::CIDRNetwork{kSid1, 128}});
+    std::set<folly::CIDRNetwork> trapPrefixes{
+        folly::CIDRNetwork{kSid0, 128}, folly::CIDRNetwork{kSid1, 128}};
+    utility::addTrapPacketAcl(asic, &cfg, trapPrefixes);
     utility::addOlympicQueueConfig(
         &cfg,
         ensemble.getL3Asics(),
@@ -142,5 +141,141 @@ class AgentSrv6BindingSidTest : public AgentHwTest {
 };
 
 TYPED_TEST_SUITE(AgentSrv6BindingSidTest, Srv6BindingSidPortTypes);
+
+TYPED_TEST(AgentSrv6BindingSidTest, recursiveResolutionPreservesSidList) {
+  auto setup = [this]() {
+    this->setupHelper();
+
+    auto ecmpHelper = this->makeEcmpHelper();
+    auto routeUpdater = this->getSw()->getRouteUpdater();
+
+    auto getNhopIp = [&ecmpHelper](int idx) {
+      auto nhop = ecmpHelper.nhop(idx);
+      if (nhop.linkLocalNhopIp.has_value()) {
+        return folly::IPAddress(nhop.linkLocalNhopIp.value());
+      }
+      return folly::IPAddress(nhop.ip);
+    };
+
+    // OpenR route A (2901::/48) -> nhop(0), nhop(1) with link-local nexthops
+    RouteNextHopSet openrNhopsA{
+        ResolvedNextHop(getNhopIp(0), ecmpHelper.nhop(0).intf, ECMP_WEIGHT),
+        ResolvedNextHop(getNhopIp(1), ecmpHelper.nhop(1).intf, ECMP_WEIGHT)};
+    routeUpdater.addRoute(
+        RouterID(0),
+        folly::IPAddressV6("2901::"),
+        48,
+        ClientID::OPENR,
+        RouteNextHopEntry(openrNhopsA, AdminDistance::OPENR));
+
+    // OpenR route B (2902::/48) -> nhop(2), nhop(3) with link-local nexthops
+    RouteNextHopSet openrNhopsB{
+        ResolvedNextHop(getNhopIp(2), ecmpHelper.nhop(2).intf, ECMP_WEIGHT),
+        ResolvedNextHop(getNhopIp(3), ecmpHelper.nhop(3).intf, ECMP_WEIGHT)};
+    routeUpdater.addRoute(
+        RouterID(0),
+        folly::IPAddressV6("2902::"),
+        48,
+        ClientID::OPENR,
+        RouteNextHopEntry(openrNhopsB, AdminDistance::OPENR));
+    routeUpdater.program();
+
+    // Add a BINDING_MICRO_SID via ThriftHandler with 2 nexthops carrying
+    // SID lists. Each nexthop resolves recursively over one of the OpenR
+    // routes above.
+    ThriftHandler handler(this->getSw());
+    auto entries = std::make_unique<std::vector<MySidEntry>>();
+    MySidEntry bindingEntry;
+    bindingEntry.type() = MySidType::BINDING_MICRO_SID;
+    facebook::network::thrift::IPPrefix mySidPrefix;
+    mySidPrefix.prefixAddress() =
+        facebook::network::toBinaryAddress(folly::IPAddressV6("fc00:100::1"));
+    mySidPrefix.prefixLength() = 48;
+    bindingEntry.mySid() = mySidPrefix;
+
+    NextHopThrift nhop0;
+    nhop0.address() =
+        facebook::network::toBinaryAddress(folly::IPAddressV6("2901::1"));
+    nhop0.srv6SegmentList() = {facebook::network::toBinaryAddress(this->kSid0)};
+    nhop0.tunnelType() = TunnelType::SRV6_ENCAP;
+    nhop0.tunnelId() = "srv6Tunnel0";
+
+    NextHopThrift nhop1;
+    nhop1.address() =
+        facebook::network::toBinaryAddress(folly::IPAddressV6("2902::1"));
+    nhop1.srv6SegmentList() = {facebook::network::toBinaryAddress(this->kSid1)};
+    nhop1.tunnelType() = TunnelType::SRV6_ENCAP;
+    nhop1.tunnelId() = "srv6Tunnel0";
+
+    bindingEntry.nextHops() = {nhop0, nhop1};
+    entries->push_back(bindingEntry);
+    handler.addMySidEntries(std::move(entries));
+  };
+
+  auto verify = [this]() {
+    auto ecmpHelper = this->makeEcmpHelper();
+    std::vector<PortID> egressPorts;
+    egressPorts.reserve(this->kNumNextHops);
+    for (int i = 0; i < this->kNumNextHops; ++i) {
+      egressPorts.push_back(this->getEgressPort(ecmpHelper.nhop(i).portDesc));
+    }
+
+    std::map<PortID, int64_t> bytesBefore;
+    for (auto port : egressPorts) {
+      bytesBefore[port] = *this->getLatestPortStats(port).outBytes_();
+    }
+
+    auto intfMac =
+        getMacForFirstInterfaceWithPortsForTesting(this->getProgrammedState());
+
+    // Send an IP-in-IP packet with outer dst matching the binding SID prefix.
+    auto txPacket = utility::makeIpInIpTxPacket(
+        this->getSw(),
+        this->getVlanIDForTx().value(),
+        intfMac,
+        intfMac,
+        folly::IPAddressV6("100::1"),
+        folly::IPAddressV6("fc00:100::1"),
+        folly::IPAddressV6("2001:db8::1"),
+        folly::IPAddressV6("2001:db8::2"),
+        8000,
+        8001,
+        0,
+        0,
+        64,
+        64);
+
+    utility::SwSwitchPacketSnooper snooper(this->getSw(), "bindingSidSnooper");
+    this->sendPacketSwitchedAsync(std::move(txPacket));
+
+    auto frameRx = snooper.waitForPacket(1);
+    WITH_RETRIES({
+      bool anyPortGotBytes = false;
+      for (auto port : egressPorts) {
+        auto bytesAfter = *this->getLatestPortStats(port).outBytes_();
+        if (bytesAfter > bytesBefore[port]) {
+          anyPortGotBytes = true;
+        }
+      }
+      EXPECT_EVENTUALLY_TRUE(anyPortGotBytes);
+      if (!frameRx.has_value()) {
+        frameRx = snooper.waitForPacket(1);
+      }
+      EXPECT_EVENTUALLY_TRUE(frameRx.has_value());
+    });
+    ASSERT_TRUE(frameRx.has_value());
+    folly::io::Cursor cursor((*frameRx).get());
+    utility::EthFrame frame(cursor);
+    auto v6Payload = frame.v6PayLoad();
+    ASSERT_TRUE(v6Payload.has_value());
+    auto v6Hdr = v6Payload->header();
+    bool sidMatch =
+        v6Hdr.dstAddr == this->kSid0 || v6Hdr.dstAddr == this->kSid1;
+    EXPECT_TRUE(sidMatch) << "Outer DA " << v6Hdr.dstAddr
+                          << " does not match kSid0 or kSid1";
+  };
+
+  this->verifyAcrossWarmBoots(setup, verify);
+}
 
 } // namespace facebook::fboss
