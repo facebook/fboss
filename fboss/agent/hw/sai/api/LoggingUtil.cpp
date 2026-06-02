2decba0ae739f1fe1a5d4c7bf35f17945865414c
Siva Muthusamy <simuthus@meta.com>
2026-06-02 12:23:54 -0700

    Guard SAI_SER_LOG_TYPE_NONE with BRCM_SAI_SDK_XGS_GTE_15_0
    Summary: SAI_SER_LOG_TYPE_NONE is only defined in 15.4+ SDK. Without the guard, builds for older SDKs (10.2, 14.2, etc.) fail with undeclared identifier error.
    
    Reviewed By: huruinan
    
    Differential Revision: D107275339
    
    fbshipit-source-id: 749bb4c6c4fca74ff090859f309ab986299c75e1

--- fboss/agent/hw/sai/api/LoggingUtil.cpp
+++ fboss/agent/hw/sai/api/LoggingUtil.cpp
@@ -541,8 +541,10 @@ std::string saiSerLogTypeToString(sai_ser_log_type_t sai_ser_log_type) {
       return "SAI_SER_LOG_TYPE_ENTRY_INFO";
     case SAI_SER_LOG_TYPE_CACHE:
       return "SAI_SER_LOG_TYPE_CACHE";
+#if defined(BRCM_SAI_SDK_XGS_GTE_15_0)
     case SAI_SER_LOG_TYPE_NONE:
       return "SAI_SER_LOG_TYPE_NONE";
+#endif
   }
   return folly::to<std::string>(static_cast<int>(sai_ser_log_type));
 }
