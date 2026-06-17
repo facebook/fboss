13bc3953c164bf87c8f9370313b69d50f3245650
Jasmeet Bagga <jasmeetbagga@meta.com>
2026-06-16 18:54:19 -0700

    Get rid of rollback option in test code
    Summary:
    This code was to only be used for rollbacks in HwSwitch
    using ERM. We ended up with a different design altogether
    https://docs.google.com/document/d/1NAG_GtQ4dwZvPRFYKrt4WKqAu8VZe41Z2fFJ7RqYb2o/edit?tab=t.0#heading=h.xfucm5k7g5ql
    
    So retire these tests. Will kill more code in stacked diffs
    
    ___
    
    Differential Revision: D108785212
    
    fbshipit-source-id: a20379e0e487c3b97c0254dac92d734233235409

--- fboss/agent/test/EcmpResourceMgrBackupGrpTests.cpp
+++ fboss/agent/test/EcmpResourceMgrBackupGrpTests.cpp
@@ -70,24 +70,8 @@ class EcmpBackupGroupTypeTest : public BaseEcmpResourceManagerTest {
     newState->publish();
     consolidate(newState);
     assertEndState(newState, {});
-    setupState = state_->clone();
-    setupState->publish();
     XLOG(DBG2) << "EcmpResourceMgrBackupGrpTest SetUp done";
   }
-  void TearDown() override {
-    StateDelta delta(setupState, sw_->getState());
-    if (delta.getFlowletSwitchingConfigDelta().getOld() ==
-        delta.getFlowletSwitchingConfigDelta().getNew()) {
-      // If test didn't change flowlet settings, check for rollbacks. During
-      // rollbacks we will explicitly fail updates and we don't allow
-      // for failures of updates across a backup ecmp mode (stored in
-      // flowlet switch settings) change
-      auto newEcmpResourceMgr = makeResourceMgr();
-      assertRollbacks(*newEcmpResourceMgr, setupState, sw_->getState());
-    }
-    BaseEcmpResourceManagerTest::TearDown();
-  }
-  std::shared_ptr<SwitchState> setupState;
 };
 
 TEST_F(EcmpBackupGroupTypeTest, addSingleNhopRoutesBelowEcmpLimit) {
