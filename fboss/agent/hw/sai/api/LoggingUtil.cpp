ac8550a739796f2db96e3e38c91038500376fe98
Siva Muthusamy <simuthus@meta.com>
2026-06-19 14:29:20 -0700

    Handle SAI_SER_LOG_TYPE_NONE in LoggingUtil.cpp for SDK 16.0
    Summary: Add `SAI_SER_LOG_TYPE_NONE` case to the `saiSerLogTypeToString` switch statement. This new enum value was introduced in SAI SDK 16.0.
    
    Reviewed By: shri-khare
    
    Differential Revision: D105380267
    
    fbshipit-source-id: 7367b1a78df7c1a991c018cef5feec5d549ba3ff

--- fboss/agent/hw/sai/api/LoggingUtil.cpp
+++ fboss/agent/hw/sai/api/LoggingUtil.cpp
@@ -541,7 +541,8 @@ std::string saiSerLogTypeToString(sai_ser_log_type_t sai_ser_log_type) {
       return "SAI_SER_LOG_TYPE_ENTRY_INFO";
     case SAI_SER_LOG_TYPE_CACHE:
       return "SAI_SER_LOG_TYPE_CACHE";
-#if defined(BRCM_SAI_SDK_XGS_GTE_15_0) || defined(TAJO_SDK_GTE_26_5)
+#if defined(BRCM_SAI_SDK_XGS_GTE_15_0) || defined(TAJO_SDK_GTE_26_5) || \
+    defined(BRCM_SAI_SDK_GTE_16_0)
     case SAI_SER_LOG_TYPE_NONE:
       return "SAI_SER_LOG_TYPE_NONE";
 #endif
