b1179ec53fc053af17a1d6700adf1202e85b8604
Nivin Lawrence <nivinl@meta.com>
2026-06-20 11:17:29 -0700

    Re-enable SAI_FEC_CODEWORDS_STATS on Tomahawk6
    Summary:
    Backs out D93289396, which moved `SAI_FEC_CODEWORDS_STATS` to unsupported on Tomahawk6 while the slow stats read (5-7 ms/port) was investigated. The root-cause fix is now integrated via the brcm-sai 14.2 patch for CS00012448707, so the feature is safe to re-enable on TH6.
    
    Stats being enabled are SAI_PORT_STAT_IF_IN_FEC_CODEWORD_ERRORS_S0 -  SAI_PORT_STAT_IF_IN_FEC_CODEWORD_ERRORS_S16, values 183 to 198.
    
    Reviewed By: harshitgulati18
    
    Differential Revision: D109167499
    
    fbshipit-source-id: 8d00f9b219e6643fd66e7933d84c27b82525d052

--- fboss/agent/hw/switch_asics/Tomahawk6Asic.cpp
+++ fboss/agent/hw/switch_asics/Tomahawk6Asic.cpp
@@ -83,6 +83,7 @@ bool Tomahawk6Asic::isSupported(Feature feature) const {
     case HwAsic::Feature::ACL_COUNTER_LABEL:
     case HwAsic::Feature::ECMP_DLB_OFFSET:
     case HwAsic::Feature::SAI_FEC_CORRECTED_BITS:
+    case HwAsic::Feature::SAI_FEC_CODEWORDS_STATS:
     case HwAsic::Feature::SAI_PORT_SERDES_PROGRAMMING:
     case HwAsic::Feature::MANAGEMENT_PORT:
     case HwAsic::Feature::PORT_WRED_COUNTER:
@@ -148,10 +149,7 @@ bool Tomahawk6Asic::isSupported(Feature feature) const {
     case HwAsic::Feature::SAI_MPLS_TTL_1_TRAP:
     case HwAsic::Feature::SAI_MPLS_LABEL_LOOKUP_FAIL_COUNTER:
     case HwAsic::Feature::FABRIC_PORTS:
-    // TODO(arunyerra): Moving SAI_FEC_CODEWORDS_STATS to unsupported while we
-    // work with Broadcom in CSP CS00012448707 to root cause and fix the time
-    // taken for this stats read.
-    case HwAsic::Feature::SAI_FEC_CODEWORDS_STATS:
+
     case HwAsic::Feature::SAI_FIRMWARE_PATH:
     case HwAsic::Feature::EXTENDED_FEC:
     case HwAsic::Feature::SAI_RX_REASON_COUNTER:
