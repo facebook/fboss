a5354b7326a891959dc793a6f3286acb51695c00
Siva Muthusamy <simuthus@meta.com>
2026-07-20 20:06:34 -0700

    Q4D: Enable SAI_PORT_ATTR_FDR_ENABLE
    Summary: Enable the `SAI_PORT_ATTR_FDR_ENABLE` port extension attribute on Broadcom DNX 16.x (Q4D/J4/R4) by removing the `!defined(BRCM_SAI_SDK_DNX_GTE_15_0)` exclusion in `SaiPortTraits::Attributes::AttributeFdrEnable::operator()`, now that the SDK supports it. SDK-version gated (no per-ASIC context at this layer), so this applies to all 16.x DNX.
    
    Reviewed By: jasmeetbagga
    
    Differential Revision: D112674137
    
    fbshipit-source-id: cd74594dbc232c4250cf7b7605c3b7376cd05b5c

--- fboss/agent/hw/sai/api/bcm/PortApi.cpp
+++ fboss/agent/hw/sai/api/bcm/PortApi.cpp
@@ -30,10 +30,7 @@ SaiPortTraits::Attributes::AttributeDiagModeEnable::operator()() {
 
 std::optional<sai_attr_id_t>
 SaiPortTraits::Attributes::AttributeFdrEnable::operator()() {
-  // TODO (Q4D/J4/R4): Enable SAI_PORT_ATTR_FDR_ENABLE for 15.x after SDK
-  // support for Q4DL/J4
-#if (defined(BRCM_SAI_SDK_GTE_10_0) || defined(BRCM_SAI_SDK_DNX_GTE_11_0)) && \
-    !defined(BRCM_SAI_SDK_DNX_GTE_15_0)
+#if (defined(BRCM_SAI_SDK_GTE_10_0) || defined(BRCM_SAI_SDK_DNX_GTE_11_0))
   return SAI_PORT_ATTR_FDR_ENABLE;
 #else
   return std::nullopt;
