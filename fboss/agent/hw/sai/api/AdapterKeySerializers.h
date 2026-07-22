9e47c645955652e79a64eb80b5cdcbbaf0e986df
Wei Dai <daiweix@meta.com>
2026-07-22 08:58:06 -0700

    Use concepts for adapter key helpers
    Summary:
    Replace return-type `std::enable_if_t` constraints in adapter-key serialization and object-key helpers with the named SAI object concepts.
    
    This keeps the overload behavior unchanged while making the object-ID and entry-struct cases easier to read.
    
    Reviewed By: simuthus-fb
    
    Differential Revision: D112746898
    
    fbshipit-source-id: 5c82f731658d7cde0355d0b549c6bbf5617e20d1

--- fboss/agent/hw/sai/api/AdapterKeySerializers.h
+++ fboss/agent/hw/sai/api/AdapterKeySerializers.h
@@ -18,34 +18,29 @@ namespace facebook::fboss {
 constexpr auto kKey = "adapterkey";
 constexpr auto kAdapterKeys = "adapterKeys";
 
-template <typename SaiObjectTraits>
-std::enable_if_t<AdapterKeyIsObjectId<SaiObjectTraits>::value, folly::dynamic>
-toFollyDynamic(const typename SaiObjectTraits::AdapterKey& adapterKey) {
+template <ObjectIdSaiObject SaiObjectTraits>
+folly::dynamic toFollyDynamic(
+    const typename SaiObjectTraits::AdapterKey& adapterKey) {
   folly::dynamic key = folly::dynamic::object;
   key[kKey] = static_cast<long>(adapterKey);
   return key;
 }
 
-template <typename SaiObjectTraits>
-std::enable_if_t<
-    AdapterKeyIsObjectId<SaiObjectTraits>::value,
-    typename SaiObjectTraits::AdapterKey>
-fromFollyDynamic(const folly::dynamic& obj) {
+template <ObjectIdSaiObject SaiObjectTraits>
+typename SaiObjectTraits::AdapterKey fromFollyDynamic(
+    const folly::dynamic& obj) {
   return typename SaiObjectTraits::AdapterKey(obj[kKey].asInt());
 }
 
-template <typename SaiObjectTraits>
-std::
-    enable_if_t<AdapterKeyIsEntryStruct<SaiObjectTraits>::value, folly::dynamic>
-    toFollyDynamic(const typename SaiObjectTraits::AdapterKey& adapterKey) {
+template <EntryStructSaiObject SaiObjectTraits>
+folly::dynamic toFollyDynamic(
+    const typename SaiObjectTraits::AdapterKey& adapterKey) {
   return adapterKey.toFollyDynamic();
 }
 
-template <typename SaiObjectTraits>
-std::enable_if_t<
-    AdapterKeyIsEntryStruct<SaiObjectTraits>::value,
-    typename SaiObjectTraits::AdapterKey>
-fromFollyDynamic(const folly::dynamic& obj) {
+template <EntryStructSaiObject SaiObjectTraits>
+typename SaiObjectTraits::AdapterKey fromFollyDynamic(
+    const folly::dynamic& obj) {
   return SaiObjectTraits::AdapterKey::fromFollyDynamic(obj);
 }
 
