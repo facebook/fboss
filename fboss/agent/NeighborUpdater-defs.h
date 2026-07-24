7eaa0c51df1e681e749dd4b35cb8e76f3ced4e7c
Wei Dai <daiweix@meta.com>
2026-07-24 12:32:43 -0700

    simplify `NeighborUpdater` method table macros
    Summary:
    Modernize the `NeighborUpdater` method table by replacing the old type/name-pair X-macro helper chain with normal parenthesized C++ parameter lists.
    
    Before this change, each method declaration encoded arguments as alternating macro tokens, for example `VlanID, vlan, folly::IPAddress, ip`. Supporting that shape required helper macros like `GET_MACRO`, `ARG_LIST_0`, `ARG_LIST_1`, ..., `ARG_LIST_8`, plus a separate no-argument macro variant. That made the method table hard to read because the argument list did not look like C++, and readers had to understand the macro expansion machinery before understanding the method signature.
    
    This diff keeps the centralized method table, but writes each method's arguments as an ordinary C++ parameter list, for example `(VlanID vlan, folly::IPAddress ip)` or `()` for no-argument methods. The method declarations for `NeighborUpdaterImpl` and `NeighborUpdaterNoopImpl` can then use that parameter list directly.
    
    For the forwarding methods on `NeighborUpdater`, this uses a C++20 feature: a lambda with an explicit template parameter list and a parameter pack. In simple terms, the helper receives the real method arguments as a pack, forwards them once, and preserves the existing behavior around checking `impl_` before dispatching to the implementation.
    
    The readability benefit is that the method table now shows real C++ signatures instead of macro-specific type/name pairs, and the repeated forwarding boilerplate is centralized in one helper. Future changes to `NeighborUpdater` methods should require editing the method table only, without also reasoning through a chain of argument-counting macros.
    
    Reviewed By: simuthus-fb
    
    Differential Revision: D113427036
    
    fbshipit-source-id: 8be5b0c07b1d8e67b95fb56c9387071c0751caf3

--- fboss/agent/NeighborUpdater-defs.h
+++ fboss/agent/NeighborUpdater-defs.h
@@ -3,10 +3,9 @@
  * `NeighborUpdater`, `NeighborUpdaterImpl` and `NeighborUpdaterNoopImpl`.
  * Having the definition in a centralized place ensures that these classes are
  * in sync and agree on the argument types being used. This file defines a
- * couple of utility macros, followed by multiple calls to
- * `NEIGHBOR_UPDATER_METHOD()`. This macro is *not* defined in this file; it's
- * the job of the caller to define a `NEIGHBOR_UPDATER_METHOD()` first, and
- * *then* include this file.
+ * list of calls to `NEIGHBOR_UPDATER_METHOD()`. This macro is *not* defined in
+ * this file; it's the job of the caller to define a `NEIGHBOR_UPDATER_METHOD()`
+ * first, and *then* include this file.
  *
  * See https://en.wikipedia.org/wiki/X_Macro for a more detailed explanation of
  * this technique.
@@ -14,90 +13,71 @@
 
 // clang-format off
 
-#define GET_MACRO(_IGNORED,_1,_2,_3,_4,_5,_6,_7,_8,_9,_10,_11,_12,_13,_14,_15,_16,NAME,...) NAME
+// The method table is intentionally expanded by includers that define
+// NEIGHBOR_UPDATER_METHOD differently.
+// NOLINTBEGIN(facebook-modularize-issue-check)
 
-#define ARG_LIST_0(ARG_LIST_ENTRY_CB)
-#define ARG_LIST_1(ARG_LIST_ENTRY_CB, TYPE, NAME) ARG_LIST_ENTRY_CB(TYPE, NAME)
-#define ARG_LIST_2(ARG_LIST_ENTRY_CB, TYPE, NAME, ...) ARG_LIST_ENTRY_CB(TYPE, NAME), ARG_LIST_1(ARG_LIST_ENTRY_CB, __VA_ARGS__)
-#define ARG_LIST_3(ARG_LIST_ENTRY_CB, TYPE, NAME, ...) ARG_LIST_ENTRY_CB(TYPE, NAME), ARG_LIST_2(ARG_LIST_ENTRY_CB, __VA_ARGS__)
-#define ARG_LIST_4(ARG_LIST_ENTRY_CB, TYPE, NAME, ...) ARG_LIST_ENTRY_CB(TYPE, NAME), ARG_LIST_3(ARG_LIST_ENTRY_CB, __VA_ARGS__)
-#define ARG_LIST_5(ARG_LIST_ENTRY_CB, TYPE, NAME, ...) ARG_LIST_ENTRY_CB(TYPE, NAME), ARG_LIST_4(ARG_LIST_ENTRY_CB, __VA_ARGS__)
-#define ARG_LIST_6(ARG_LIST_ENTRY_CB, TYPE, NAME, ...) ARG_LIST_ENTRY_CB(TYPE, NAME), ARG_LIST_5(ARG_LIST_ENTRY_CB, __VA_ARGS__)
-#define ARG_LIST_7(ARG_LIST_ENTRY_CB, TYPE, NAME, ...) ARG_LIST_ENTRY_CB(TYPE, NAME), ARG_LIST_6(ARG_LIST_ENTRY_CB, __VA_ARGS__)
-#define ARG_LIST_8(ARG_LIST_ENTRY_CB, TYPE, NAME, ...) ARG_LIST_ENTRY_CB(TYPE, NAME), ARG_LIST_7(ARG_LIST_ENTRY_CB, __VA_ARGS__)
-#define ARG_LIST(ARG_LIST_ENTRY_CB, ...) GET_MACRO("ignored", ##__VA_ARGS__, ARG_LIST_8, _, ARG_LIST_7, _, ARG_LIST_6, _, ARG_LIST_5, _, ARG_LIST_4, _, ARG_LIST_3, _, ARG_LIST_2, _, ARG_LIST_1, _, ARG_LIST_0)(ARG_LIST_ENTRY_CB, ##__VA_ARGS__)
-
-#if !defined(NEIGHBOR_UPDATER_METHOD_NO_ARGS)
-#define NEIGHBOR_UPDATER_METHOD_NO_ARGS(VISIBILITY, NAME, RETURN_TYPE) NEIGHBOR_UPDATER_METHOD(VISIBILITY, NAME, RETURN_TYPE)
-#define DEFINED_NO_ARGS_VARIANT
-#endif
-
-NEIGHBOR_UPDATER_METHOD(public, flushEntry, uint32_t, VlanID, vlan, folly::IPAddress, ip)
-NEIGHBOR_UPDATER_METHOD(public, flushEntryForIntf, uint32_t, InterfaceID, intfID, folly::IPAddress, ip)
+NEIGHBOR_UPDATER_METHOD(public, flushEntry, uint32_t, (VlanID vlan, folly::IPAddress ip))
+NEIGHBOR_UPDATER_METHOD(public, flushEntryForIntf, uint32_t, (InterfaceID intfID, folly::IPAddress ip))
 
 // Ndp events
 // TODO(skhare) Remove after completely migrating to intfCaches_
-NEIGHBOR_UPDATER_METHOD(public, sentNeighborSolicitation, void, VlanID, vlan, folly::IPAddressV6, ip)
-NEIGHBOR_UPDATER_METHOD(public, receivedNdpMine, void, VlanID, vlan, folly::IPAddressV6, ip, folly::MacAddress, mac, PortDescriptor, port, ICMPv6Type, type, uint32_t, flags)
-NEIGHBOR_UPDATER_METHOD(public, receivedNdpNotMine, void, VlanID, vlan, folly::IPAddressV6, ip, folly::MacAddress, mac, PortDescriptor, port, ICMPv6Type, type, uint32_t, flags)
+NEIGHBOR_UPDATER_METHOD(public, sentNeighborSolicitation, void, (VlanID vlan, folly::IPAddressV6 ip))
+NEIGHBOR_UPDATER_METHOD(public, receivedNdpMine, void, (VlanID vlan, folly::IPAddressV6 ip, folly::MacAddress mac, PortDescriptor port, ICMPv6Type type, uint32_t flags))
+NEIGHBOR_UPDATER_METHOD(public, receivedNdpNotMine, void, (VlanID vlan, folly::IPAddressV6 ip, folly::MacAddress mac, PortDescriptor port, ICMPv6Type type, uint32_t flags))
 
-NEIGHBOR_UPDATER_METHOD(public, sentNeighborSolicitationForIntf, void, InterfaceID, intfID, folly::IPAddressV6, ip)
-NEIGHBOR_UPDATER_METHOD(public, receivedNdpMineForIntf, void, InterfaceID, intfID, folly::IPAddressV6, ip, folly::MacAddress, mac, PortDescriptor, port, ICMPv6Type, type, uint32_t, flags)
-NEIGHBOR_UPDATER_METHOD(public, receivedNdpNotMineForIntf, void, InterfaceID, intfID, folly::IPAddressV6, ip, folly::MacAddress, mac, PortDescriptor, port, ICMPv6Type, type, uint32_t, flags)
+NEIGHBOR_UPDATER_METHOD(public, sentNeighborSolicitationForIntf, void, (InterfaceID intfID, folly::IPAddressV6 ip))
+NEIGHBOR_UPDATER_METHOD(public, receivedNdpMineForIntf, void, (InterfaceID intfID, folly::IPAddressV6 ip, folly::MacAddress mac, PortDescriptor port, ICMPv6Type type, uint32_t flags))
+NEIGHBOR_UPDATER_METHOD(public, receivedNdpNotMineForIntf, void, (InterfaceID intfID, folly::IPAddressV6 ip, folly::MacAddress mac, PortDescriptor port, ICMPv6Type type, uint32_t flags))
 
 // Arp events
 // TODO(skhare) Remove after completely migrating to intfCaches_
-NEIGHBOR_UPDATER_METHOD(public, sentArpRequest, void, VlanID, vlan, folly::IPAddressV4, ip)
-NEIGHBOR_UPDATER_METHOD(public, receivedArpMine, void, VlanID, vlan, folly::IPAddressV4, ip, folly::MacAddress, mac, PortDescriptor, port, ArpOpCode, op)
-NEIGHBOR_UPDATER_METHOD(public, receivedArpNotMine, void, VlanID, vlan, folly::IPAddressV4, ip, folly::MacAddress, mac, PortDescriptor, port, ArpOpCode, op)
+NEIGHBOR_UPDATER_METHOD(public, sentArpRequest, void, (VlanID vlan, folly::IPAddressV4 ip))
+NEIGHBOR_UPDATER_METHOD(public, receivedArpMine, void, (VlanID vlan, folly::IPAddressV4 ip, folly::MacAddress mac, PortDescriptor port, ArpOpCode op))
+NEIGHBOR_UPDATER_METHOD(public, receivedArpNotMine, void, (VlanID vlan, folly::IPAddressV4 ip, folly::MacAddress mac, PortDescriptor port, ArpOpCode op))
 
-NEIGHBOR_UPDATER_METHOD(public, sentArpRequestForIntf, void, InterfaceID, intfID, folly::IPAddressV4, ip)
-NEIGHBOR_UPDATER_METHOD(public, receivedArpMineForIntf, void, InterfaceID, intfID, folly::IPAddressV4, ip, folly::MacAddress, mac, PortDescriptor, port, ArpOpCode, op)
-NEIGHBOR_UPDATER_METHOD(public, receivedArpNotMineForIntf, void, InterfaceID, intfID, folly::IPAddressV4, ip, folly::MacAddress, mac, PortDescriptor, port, ArpOpCode, op)
+NEIGHBOR_UPDATER_METHOD(public, sentArpRequestForIntf, void, (InterfaceID intfID, folly::IPAddressV4 ip))
+NEIGHBOR_UPDATER_METHOD(public, receivedArpMineForIntf, void, (InterfaceID intfID, folly::IPAddressV4 ip, folly::MacAddress mac, PortDescriptor port, ArpOpCode op))
+NEIGHBOR_UPDATER_METHOD(public, receivedArpNotMineForIntf, void, (InterfaceID intfID, folly::IPAddressV4 ip, folly::MacAddress mac, PortDescriptor port, ArpOpCode op))
 
-NEIGHBOR_UPDATER_METHOD(public, portDown, void, PortDescriptor, port)
-NEIGHBOR_UPDATER_METHOD(public, portFlushEntries, void, PortDescriptor, port)
+NEIGHBOR_UPDATER_METHOD(public, portDown, void, (PortDescriptor port))
+NEIGHBOR_UPDATER_METHOD(public, portFlushEntries, void, (PortDescriptor port))
 
 // TODO(skhare) Remove after completely migrating to intfCaches_
-NEIGHBOR_UPDATER_METHOD_NO_ARGS(public, getArpCacheData, std::list<ArpEntryThrift>)
-NEIGHBOR_UPDATER_METHOD_NO_ARGS(public, getNdpCacheData, std::list<NdpEntryThrift>)
+NEIGHBOR_UPDATER_METHOD(public, getArpCacheData, std::list<ArpEntryThrift>, ())
+NEIGHBOR_UPDATER_METHOD(public, getNdpCacheData, std::list<NdpEntryThrift>, ())
 
-NEIGHBOR_UPDATER_METHOD_NO_ARGS(public, getArpCacheDataForIntf, std::list<ArpEntryThrift>)
-NEIGHBOR_UPDATER_METHOD_NO_ARGS(public, getNdpCacheDataForIntf, std::list<NdpEntryThrift>)
+NEIGHBOR_UPDATER_METHOD(public, getArpCacheDataForIntf, std::list<ArpEntryThrift>, ())
+NEIGHBOR_UPDATER_METHOD(public, getNdpCacheDataForIntf, std::list<NdpEntryThrift>, ())
 
-NEIGHBOR_UPDATER_METHOD(public, getProbesLeft, uint32_t, const InterfaceID&, intfID, const folly::IPAddressV6&, ip)
-NEIGHBOR_UPDATER_METHOD(public, getProbesLeftIPv4, uint32_t, const InterfaceID&, intfID, const folly::IPAddressV4&, ip)
-NEIGHBOR_UPDATER_METHOD(public, getMaxNeighborProbes, uint32_t,const InterfaceID&, intfID)
+NEIGHBOR_UPDATER_METHOD(public, getProbesLeft, uint32_t, (const InterfaceID& intfID, const folly::IPAddressV6& ip))
+NEIGHBOR_UPDATER_METHOD(public, getProbesLeftIPv4, uint32_t, (const InterfaceID& intfID, const folly::IPAddressV4& ip))
+NEIGHBOR_UPDATER_METHOD(public, getMaxNeighborProbes, uint32_t, (const InterfaceID& intfID))
 
 // Enable access to neighbor caches from NeighborUpdater to help imitate neighbor learning during testing
-NEIGHBOR_UPDATER_METHOD(private, getArpCacheForIntf, std::shared_ptr<ArpCache>, InterfaceID, intfID)
-NEIGHBOR_UPDATER_METHOD(private, getNdpCacheForIntf, std::shared_ptr<NdpCache>, InterfaceID, intfID)
+NEIGHBOR_UPDATER_METHOD(private, getArpCacheForIntf, std::shared_ptr<ArpCache>, (InterfaceID intfID))
+NEIGHBOR_UPDATER_METHOD(private, getNdpCacheForIntf, std::shared_ptr<NdpCache>, (InterfaceID intfID))
 
-NEIGHBOR_UPDATER_METHOD(private, getArpCacheFor, std::shared_ptr<ArpCache>, VlanID, vlan)
-NEIGHBOR_UPDATER_METHOD(private, getNdpCacheFor, std::shared_ptr<NdpCache>, VlanID, vlan)
+NEIGHBOR_UPDATER_METHOD(private, getArpCacheFor, std::shared_ptr<ArpCache>, (VlanID vlan))
+NEIGHBOR_UPDATER_METHOD(private, getNdpCacheFor, std::shared_ptr<NdpCache>, (VlanID vlan))
 
 // State update helpers
 // TODO(skhare) Remove after completely migrating to intfCaches_
-NEIGHBOR_UPDATER_METHOD(private, vlanAdded, void, VlanID, vlanID, const std::shared_ptr<SwitchState>, state)
-NEIGHBOR_UPDATER_METHOD(private, vlanDeleted, void, VlanID, vlanID)
-NEIGHBOR_UPDATER_METHOD(private, vlanChanged, void, VlanID, vlanID, InterfaceID, intfID, std::string, vlanName)
+NEIGHBOR_UPDATER_METHOD(private, vlanAdded, void, (VlanID vlanID, const std::shared_ptr<SwitchState> state))
+NEIGHBOR_UPDATER_METHOD(private, vlanDeleted, void, (VlanID vlanID))
+NEIGHBOR_UPDATER_METHOD(private, vlanChanged, void, (VlanID vlanID, InterfaceID intfID, std::string vlanName))
 
-NEIGHBOR_UPDATER_METHOD(private, interfaceAdded, void, InterfaceID, intfID, const std::shared_ptr<SwitchState>, state)
-NEIGHBOR_UPDATER_METHOD(private, interfaceRemoved, void, InterfaceID, intfID)
+NEIGHBOR_UPDATER_METHOD(private, interfaceAdded, void, (InterfaceID intfID, const std::shared_ptr<SwitchState> state))
+NEIGHBOR_UPDATER_METHOD(private, interfaceRemoved, void, (InterfaceID intfID))
 
-NEIGHBOR_UPDATER_METHOD(private, timeoutsChanged, void, std::chrono::seconds, arpTimeout, std::chrono::seconds, ndpTimeout, std::chrono::seconds, staleEntryInterval, uint32_t, maxNeighborProbes)
+NEIGHBOR_UPDATER_METHOD(private, timeoutsChanged, void, (std::chrono::seconds arpTimeout, std::chrono::seconds ndpTimeout, std::chrono::seconds staleEntryInterval, uint32_t maxNeighborProbes))
 
 // Lookup class updaters
 // TODO(skhare) Remove after completely migrating to intfCaches_
-NEIGHBOR_UPDATER_METHOD(private, updateArpEntryClassID, void, VlanID, vlan, folly::IPAddressV4, ip, std::optional<cfg::AclLookupClass>, classID);
-NEIGHBOR_UPDATER_METHOD(private, updateNdpEntryClassID, void, VlanID, vlan, folly::IPAddressV6, ip, std::optional<cfg::AclLookupClass>, classID);
+NEIGHBOR_UPDATER_METHOD(private, updateArpEntryClassID, void, (VlanID vlan, folly::IPAddressV4 ip, std::optional<cfg::AclLookupClass> classID))
+NEIGHBOR_UPDATER_METHOD(private, updateNdpEntryClassID, void, (VlanID vlan, folly::IPAddressV6 ip, std::optional<cfg::AclLookupClass> classID))
 
-NEIGHBOR_UPDATER_METHOD(private, updateArpEntryClassIDForIntf, void, InterfaceID, intfID, folly::IPAddressV4, ip, std::optional<cfg::AclLookupClass>, classID);
-NEIGHBOR_UPDATER_METHOD(private, updateNdpEntryClassIDForIntf, void, InterfaceID, intfID, folly::IPAddressV6, ip, std::optional<cfg::AclLookupClass>, classID);
+NEIGHBOR_UPDATER_METHOD(private, updateArpEntryClassIDForIntf, void, (InterfaceID intfID, folly::IPAddressV4 ip, std::optional<cfg::AclLookupClass> classID))
+NEIGHBOR_UPDATER_METHOD(private, updateNdpEntryClassIDForIntf, void, (InterfaceID intfID, folly::IPAddressV6 ip, std::optional<cfg::AclLookupClass> classID))
 
-#undef ARG_LIST
-#undef GET_MACRO
-#if defined(DEFINED_NO_ARGS_VARIANT)
-#undef NEIGHBOR_UPDATER_METHOD_NO_ARGS
-#undef DEFINED_NO_ARGS_VARIANT
-#endif
+// NOLINTEND(facebook-modularize-issue-check)
