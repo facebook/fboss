2067035c66103c0c7dfbcfd4006b7034ee88a4c6
Ram Singamala <saisinga@cisco.com>
2026-09-08 13:30:42 -0700

    Enable ECN cache priming for P200

--- fboss/agent/test/agent_hw_tests/AgentAqmTests.cpp
+++ fboss/agent/test/agent_hw_tests/AgentAqmTests.cpp
@@ -100,6 +100,7 @@ void verifyEcnMarkedPacketCount(
 bool requiresCiscoDequeueEcnPriming(const HwAsic* hwAsic) {
   switch (hwAsic->getAsicType()) {
     case cfg::AsicType::ASIC_TYPE_EBRO:
+    case cfg::AsicType::ASIC_TYPE_P200:
     case cfg::AsicType::ASIC_TYPE_YUBA:
     case cfg::AsicType::ASIC_TYPE_G202X:
       return true;
